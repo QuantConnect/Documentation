b8f5eac64229588bbd89f74463e8f46a5c238e25
AlexCatarino <alexandre.catarino@gmail.com>
2017-09-29 15:49:35 +0100

    Fixes code snippets links

--- 02 Algorithm Reference/12 Reality Modelling/03 Transaction Fee Models/02 code.py
+++ 02 Algorithm Reference/12 Reality Modelling/03 Transaction Fee Models/02 code.py
@@ -1,7 +1,5 @@
-# Assigning securities custom fee models:
-self.SetFeeModel(CustomFeeModel(), self.Securities["SPY"]) 
+# Set IBM to use a fixed $1.5 per trade fee model.
+self.Securities["IBM"].FeeModel = ConstantFeeTransactionModel(1.5)
 
-# Custom fee implementation
-class CustomFeeModel:
-    def GetOrderFee(self, security, order):
-        return 1
+# Set EURUSD to use FXCM's transaction fees:
+self.Securities["EURUSD"].FeeModel = new FxcmTransactionModel()
