d6127cd8a91c7c1188c3642ba661cb0380799411
AlexCatarino <alexandre.catarino@gmail.com>
2017-12-05 18:02:00 +0000

    Adds SetFilter examples using lambda expressions

--- 03 Asset Classes/03 Futures/01 Futures/01 code.cs
+++ 03 Asset Classes/03 Futures/01 Futures/01 code.cs
@@ -1,3 +1,5 @@
 // In Initialize
 var future = AddFuture(Futures.Indices.SP500EMini, Resolution.Minute);
 future.SetFilter(TimeSpan.Zero, TimeSpan.FromDays(182));
+// or Linq
+future.SetFilter(universe => universe.Expiration(TimeSpan.Zero, TimeSpan.FromDays(182)));
--- 03 Asset Classes/03 Futures/01 Futures/02 code.py
+++ 03 Asset Classes/03 Futures/01 Futures/02 code.py
@@ -1,3 +1,5 @@
 # In Initialize
 future = self.AddFuture(Futures.Indices.SP500EMini, Resolution.Minute)
-futureSetFilter(timedelta(0), timedelta(182))
+future.SetFilter(timedelta(0), timedelta(182))
+# or Lambda
+future.SetFilter(universe => universe.Expiration(timedelta(0), timedelta(182)))
