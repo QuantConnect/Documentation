cfef906ccee1a2a978cff026058107a33af5cfd1
Jing Wu <wujingwj@outlook.com>
2018-01-30 11:32:51 -0500

    add csharp code of custom chart

--- 02 Algorithm Reference/13 Charting/02 Manually Creating Charts/01 code.cs
+++ 02 Algorithm Reference/13 Charting/02 Manually Creating Charts/01 code.cs
@@ -6,6 +6,12 @@ var stockPlot = new Chart("Trade Plot");
 var buyOrders = new Series("Buy", SeriesType.Scatter, 0);
 var sellOrders = new Series("Sell", SeriesType.Scatter, 0);
 var assetPrice = new Series("Price", SeriesType.Line, 0);
+// Or Using Custom Chart
+// Import the necessary module before using Custom color
+using System.Drawing;
+var buyOrders = new Series("Buy", SeriesType.Scatter, "$", Color.Red, ScatterMarkerSymbol.Triangle);
+var sellOrders = new Series("Sell", SeriesType.Scatter, "$", Color.Blue, ScatterMarkerSymbol.TriangleDown);
+var assetPrice = new Series("Price", SeriesType.Line, "$", Color.Green);
 stockPlot.AddSeries(buyOrders);
 stockPlot.AddSeries(sellOrders);
 stockPlot.AddSeries(assetPrice);
