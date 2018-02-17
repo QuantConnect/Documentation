dfa49ead7167bb82478579879fee2cdc31cac23d
Jared <jaredbroad@gmail.com>
2018-02-17 02:05:02 -0500

    Update 02 code.py

--- 02 Algorithm Reference/04 Importing Custom Data/04 Loading Reference Data/02 code.py
+++ 02 Algorithm Reference/04 Importing Custom Data/04 Loading Reference Data/02 code.py
@@ -1,2 +1,2 @@
 # If using dropbox remember to add the &dl=1 to trigger a download
-file = self.Download("http://www.dropbox.com?....&dl=1");
+file = self.Download("https://www.dropbox.com?....&dl=1")
