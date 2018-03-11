8408c9adfe33ef55eac903920dbf18ca0fa9dc05
Jared <jaredbroad@gmail.com>
2018-03-11 13:39:24 -0400

    Update 02 code.py

--- 04 Live Trading/02 Notifications/01 Notifications/02 code.py
+++ 04 Live Trading/02 Notifications/01 Notifications/02 code.py
@@ -1,2 +1,7 @@
 #Send yourself an email on live trade executions
 self.Notify.Email("myEmailAddress@gmail.com", "Live Trade Executed", "Bought 100 Shares of AAPL")
+
+# Example sending notification with price with slice obect from inside OnData
+# Try not to do this; it'll send hundreds of sms!
+def OnData(self, slice):
+  self.Notify.Sms("+1234567890", "BTCUSD Price at " + str(slice["BTCUSD"].Close))
