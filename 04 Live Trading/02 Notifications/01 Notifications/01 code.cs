b1f72032fba847e9dea54e6ddc2a07f284f257e4
Jared <jaredbroad@gmail.com>
2018-03-11 13:27:16 -0400

    Update 01 code.cs

--- 04 Live Trading/02 Notifications/01 Notifications/01 code.cs
+++ 04 Live Trading/02 Notifications/01 Notifications/01 code.cs
@@ -1,2 +1,3 @@
 //Send yourself an email on live trade executions
 Notify.Email("myEmailAddress@gmail.com", "Live Trade Executed", "Bought 100 Shares of AAPL");
+Notify.Sms("+1 1234 5678", "SPY Trade at " + data.Bars["SPY"].Close);
