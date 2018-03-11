77983831d5ad80eb22ade07595904b06775cab48
Jared <jaredbroad@gmail.com>
2018-03-11 09:57:30 -0400

    Update 01 code.cs

--- 02 Algorithm Reference/09 Indicators/03 Reference Table/01 code.cs
+++ 02 Algorithm Reference/09 Indicators/03 Reference Table/01 code.cs
@@ -1,6 +1,7 @@
 // 1. Using basic indicator at the same resolution as source security:
 // TIP -> You can use string "IBM" or the security.Symbol object
 var ema = EMA("IBM", 14);
+var rsi = RSI("IBM", 14);
 
 //2. Using indicator at different (higher) resolution to the source security:
 var emaDaily = EMA("IBM", 14, Resolution.Daily);
