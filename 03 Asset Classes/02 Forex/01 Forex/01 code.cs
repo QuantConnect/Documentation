15696d9c51ecb4e65ff3462c3f8d10f98e675e7f
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 15:01:31 -0400

    Merge 03/02/01 Forex

--- 03 Asset Classes/02 Forex/01 Forex/01 code.cs
+++
@@ -1,6 +0,0 @@
-// In Initialize
-AddForex("EURUSD", Resolution.Minute);   // or
-AddForex("EURUSD", Resolution.Minute, Market.FXCM);
-
-// For OANDA, we need to explictly define the market
-AddForex("EURUSD", Resolution.Minute, Market.Oanda);
