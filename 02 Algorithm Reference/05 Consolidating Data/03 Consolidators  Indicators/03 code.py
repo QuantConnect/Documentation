89709ddf83de4da07fd601d06842d73251252fb0
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 13:10:46 -0400

    Merge 02/05/03 Consolidators Indicators

--- 02 Algorithm Reference/05 Consolidating Data/03 Consolidators  Indicators/03 code.py
+++
@@ -1,3 +0,0 @@
-# Consolidating minute SPY into Daily SMA/EMA.
-ema = self.EMA("SPY", 14, Resolution.Daily)
-sma = self.SMA("SPY", 14, Resolution.Daily)
--- 02 Algorithm Reference/05 Consolidating Data/03 Consolidators  Indicators/06 code.py
+++
@@ -1,8 +0,0 @@
-# Create, initialize a consolidator and add an indicator
-def Initialize(self):
-    # ...other initialization...
-    self.AddEquity("SPY", Resolution.Minute)
-    consolidator = TradeBarConsolidator(30)
-    self._sma = SimpleMovingAverage(10)
-    self.RegisterIndicator("SPY", self._sma, consolidator)
-    self.SubscriptionManager.AddConsolidator("SPY", consolidator)
