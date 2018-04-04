c11a924893f8f7bbf404fad6a9a521c00ba8e4a3
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 15:04:18 -0400

    Merge 03/04/01 Options

--- 03 Asset Classes/04 Options/01 Options/02 code.py
+++
@@ -1,5 +0,0 @@
-# In Initialize
-option = self.AddOption("GOOG");
-option.SetFilter(-2, 2, timedelta(0), timedelta(182))
-# or Lambda
-option.SetFilter(lambda universe: universe.IncludeWeeklys().Strikes(-2, +2).Expiration(timedelta(0), timedelta(182)))
