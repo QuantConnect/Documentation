8204d9b05757300696a3206cb06f7dfded8852c1
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 14:40:59 -0400

    Merge 02/13/03 Transaction Fee Models

--- 02 Algorithm Reference/13 Reality Modelling/03 Transaction Fee Models/02 code.py
+++
@@ -1,5 +0,0 @@
-# Set IBM to use a fixed $1.5 per trade fee model.
-self.Securities["IBM"].FeeModel = ConstantFeeTransactionModel(1.5)
-
-# Set EURUSD to use FXCM's transaction fees:
-self.Securities["EURUSD"].FeeModel = new FxcmTransactionModel()
