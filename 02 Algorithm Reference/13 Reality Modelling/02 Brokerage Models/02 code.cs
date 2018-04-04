528ed9ccc56b298c3f4410e669565f3f3bee8546
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 14:33:38 -0400

    Merge 02/13/02 Brokerage Models

--- 02 Algorithm Reference/13 Reality Modelling/02 Brokerage Models/02 code.cs
+++
@@ -1,6 +0,0 @@
-// You can also create your own brokerage model: IBrokerageModel
-class MyBrokerage: DefaultBrokerage {
-   // Custom implementation of brokerage here.
-} 
-
-SetBrokerageModel(new MyBrokerage());
