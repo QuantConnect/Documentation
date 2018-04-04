33ad93d45e99e9160d7a052fcc615b13fc92f860
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 12:02:55 -0400

    Merge 02/02/04 Selecting Asset Data

--- 02 Algorithm Reference/02 Initializing Algorithms/04 Selecting Asset Data/03 code.fs
+++
@@ -1,2 +0,0 @@
-this.AddEquity("SPY", Resolution.Minute) |> ignore
-this.AddForex("EURUSD", Resolution.Second) |> ignore
--- 02 Algorithm Reference/02 Initializing Algorithms/04 Selecting Asset Data/06 code.py
+++
@@ -1,2 +0,0 @@
-# Setting the data normalization mode for the MSFT security to raw (pay dividends as cash)
-self.Securities["SPY"].SetDataNormalizationMode(DataNormalizationMode.Raw);
