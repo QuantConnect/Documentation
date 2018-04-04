70aff55c14c4b19cf8019271c08e1564e0c7ebe4
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 12:41:37 -0400

    Merge 02/02/06 Cash and Brokerage Models

--- 02 Algorithm Reference/02 Initializing Algorithms/06 Cash and Brokerage Models/02 code.py
+++
@@ -1,6 +0,0 @@
-#Brokerage model and account type:
-self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash)
-
-//Add securities and if required set custom margin models 
-spy = self.AddEquity("SPY") # Default to minute bars
-spy.MarginModel = PatternDayTradingMarginModel()
--- 02 Algorithm Reference/02 Initializing Algorithms/06 Cash and Brokerage Models/03 code.fs
+++
@@ -1,5 +0,0 @@
-(* In Initialize method, set the brokerage models *)
-this.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash);
-this.AddEquity("SPY", Resolution.Second) |> ignore
-(* Then if required, you can set specific margin models for your secutities *)
-this.Securities.Item("SPY").MarginModel <- new PatternDayTradingMarginModel()
