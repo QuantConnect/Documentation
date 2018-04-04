6479a25d097d0d7775bc1834b7ef92bfa5eef89c
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 14:42:38 -0400

    Merge 02/13/04 Slippage Models

--- 02 Algorithm Reference/13 Reality Modelling/04 Slippage Models/01 code.cs
+++
@@ -1,9 +0,0 @@
-// Assigning securities custom slippage models:
-Securities["SPY"].SetSlippageModel(new CustomSlippageModel());
-
-// Custom slippage implementation
-public class CustomSlippageModel : ISlippageModel {
-    public decimal GetSlippageApproximation(Security asset, Order order) {
-        return 0.1m;
-    }
-}
