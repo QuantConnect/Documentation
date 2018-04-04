02ec19141a47271b355e33f141a129bd71dc5d23
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 13:51:49 -0400

    Merge 02/11/05 Custom Universe Selection

--- 02 Algorithm Reference/11 Universes/05 Custom Universe Selection/01 code.cs
+++
@@ -1,5 +0,0 @@
-AddUniverse<NyseTopGainers>("myCustomUniverse", Resolution.Daily, nyseTopGainersList => {
-      return from singleStockData in nyseTopGainersList 
-             where singleStockData.Rank > 5
-             select singleStockData.Symbol;
-});
