33f40d1b7c0b3200d5d5004db05fadb3796c7893
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 15:06:33 -0400

    Merge 03/06/01 Crypto

--- 03 Asset Classes/06 Crypto/01 Crypto/01 code.cs
+++
@@ -1,2 +0,0 @@
-// In Initialize
-AddCrypto("BTCUSD", Resolution.Minute);
--- 03 Asset Classes/06 Crypto/01 Crypto/04 code.py
+++
@@ -1,5 +0,0 @@
-# In OnData(self, data)
-bar = data["BTCUSD"]
-
-# Anywhere in the code
-crypto = self.Securities["BTCUSD"]
