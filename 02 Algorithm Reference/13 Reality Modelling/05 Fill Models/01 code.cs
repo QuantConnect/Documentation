bca8c159e6f05e14a4351550861df5b354eb3fc3
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 14:44:16 -0400

    Merge 02/13/05 Fill Models

--- 02 Algorithm Reference/13 Reality Modelling/05 Fill Models/01 code.cs
+++
@@ -1,9 +0,0 @@
-// Set the fill models in initialize:
-Securities["IBM"].SetFillModel(new PartialFillModel());
-
-// Custom fill model implementation stub
-public class PartialFillModel : ImmediateFillModel {
-    public override OrderEvent MarketFill(Security asset, MarketOrder order) {
-        //Override order event handler and return partial order fills, 
-    }
-}
--- 02 Algorithm Reference/13 Reality Modelling/05 Fill Models/02 code.py
+++
@@ -1,8 +0,0 @@
-# Set the fill models in initialize:
-self.Securities["IBM"].SetFillModel(PartialFillModel())
-
-# Custom fill model implementation stub
-class PartialFillModel(ImmediateFillModel):
-    def MarketFill(self, asset, order):
-        # Override order event handler and return partial order fills
-        pass
