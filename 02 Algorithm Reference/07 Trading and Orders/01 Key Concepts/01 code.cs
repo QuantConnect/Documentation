39c3f2d4380dab07e92adaf56d7b927c00719d53
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 13:15:46 -0400

    Merge 02/07/01 Key Concepts

--- 02 Algorithm Reference/07 Trading and Orders/01 Key Concepts/01 code.cs
+++
@@ -1,8 +0,0 @@
-// Creating an Order:
-OrderTicket limitOrderTicket = LimitOrder("SPY", 100, 205);
-
-// Updating an Order:
-limitOrderTicket.Update(new UpdateOrderFields{LimitPrice = 207.50};
-
-// Cancel an Order:
-limitOrderTicket.Cancel();
