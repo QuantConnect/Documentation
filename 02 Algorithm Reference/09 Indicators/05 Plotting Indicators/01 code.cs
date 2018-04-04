21585b4083e955ac129f34dcdc82a9bd7641fb8a
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 13:27:31 -0400

    Remove 02/09/05 Plotting Indicators

--- 02 Algorithm Reference/09 Indicators/05 Plotting Indicators/01 code.cs
+++
@@ -1,7 +0,0 @@
-//Plot array of indicator objects; extending "Indicator" type.
-Plot("Indicators", sma, rsi); 
-
-//Plot array of indicator objects; extending "TradeBarIndicator" type.
-Plot("Indicators", atr, aroon); 
-
-//For complex plotting it might be easiest to simply plot your indicators individually.
