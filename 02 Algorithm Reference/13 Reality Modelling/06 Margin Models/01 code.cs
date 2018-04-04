8b2a8d9fbd4edf76059dcf76633e033ad96cbf5c
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 14:45:42 -0400

    Remove 02/13/06 Margin Models

--- 02 Algorithm Reference/13 Reality Modelling/06 Margin Models/01 code.cs
+++
@@ -1,3 +0,0 @@
-// Example of setting a security to use PDT margin models:
-// Generally you do not need to adjust margin models
-Securities["AAPL"].MarginModel = new PatternDayTradingMarginModel();
