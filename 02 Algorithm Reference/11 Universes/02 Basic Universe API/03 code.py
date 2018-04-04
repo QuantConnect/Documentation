0d88ca5b79a49ea847d1870a8ec6ebdf337dd674
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 13:46:29 -0400

    Merge 02/11/02 Basic Universe API

--- 02 Algorithm Reference/11 Universes/02 Basic Universe API/03 code.py
+++
@@ -1,3 +0,0 @@
-# Overriding the on changes event handler.
-def OnSecuritiesChanged(self, changes):
-     self._changes = changes
--- 02 Algorithm Reference/11 Universes/02 Basic Universe API/05 code.py
+++
@@ -1,8 +0,0 @@
-# Use UniverseManager to find symbols from the Universe Selection
-for universe in self.UniverseManager.Values:
-
-    # User defined universe has symbols from AddSecurity/AddEquity calls
-    if universe is UserDefinedUniverse:
-        continue
-    
-    symbols = universe.Members.Keys
