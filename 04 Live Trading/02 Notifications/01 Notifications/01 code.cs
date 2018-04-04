f2df0684e1ef5d8791bc5304e8e497ed1d6a05d4
LiXiang618 <lixiang.lx618@gmail.com>
2018-04-04 15:12:24 -0400

    Merge 04/02/01 Notifications

--- 04 Live Trading/02 Notifications/01 Notifications/01 code.cs
+++
@@ -1,3 +0,0 @@
-//Send yourself an email on live trade executions
-Notify.Email("myEmailAddress@gmail.com", "Live Trade Executed", "Bought 100 Shares of AAPL");
-Notify.Sms("+1 1234 5678", "SPY Trade at " + data.Bars["SPY"].Close);
