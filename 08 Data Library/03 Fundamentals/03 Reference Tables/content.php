839b49f9efd15c76d364bff904a2e76104f4c8d0
www-data <www-data@qc-web-beta.c.continual-bay-418.internal>
2018-05-08 19:52:12 +0000

    Automated push from server made by Jared Broad

--- 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
+++ 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
@@ -1,7 +1,11 @@
+<p>
+The following reference tables detail the specific objects available for use in your QuantConnect Fine Universe filter. These properties are subsets of the <code>FineFundamental</code> object presented to your universe filter each day.
+</p>
 <?php
 $fields = file_get_contents("https://www.quantconnect.com/services/fundamentals");
 $decoded = json_decode($fields, true);
 foreach ($decoded['fundamentals'] as $field) {
+    $fieldName = $field['name'];
     ?>
 <table class="table qc-table table-itemized table-reflow">
 	<thead>
@@ -15,6 +19,7 @@ foreach ($decoded['fundamentals'] as $field) {
 		<?php
         foreach ($field['properties'] as $property)
         {
+            $periodExample = "";
             $name = $property['name'];
             $type = $property['type'];
             $isMultiPeriod = strpos($type, "QuantConnect.Data.Fundamental.") !== false;
@@ -22,16 +27,22 @@ foreach ($decoded['fundamentals'] as $field) {
             $type = str_replace("System.", "", $type);
             $type = str_replace("QuantConnect.Data.Fundamental.", "", $type);
             
-            if ($isMultiPeriod) $type = "<a href='https://github.com/QuantConnect/Lean/tree/master/Common/Data/Fundamental/Generated'>$type : MultiPeriodField</a>";
+            if ($isMultiPeriod) {
+               $type = "<a href='https://github.com/QuantConnect/Lean/tree/master/Common/Data/Fundamental/Generated'>$type (MultiPeriodField)</a>";
+               $periodExample = ".OneMonth";
+            }
             
             $tooltip = htmlentities($property['description']);
             
             echo "
 <tr>
-			<td width='30%'>$name <br/>
-				<small>$type</small>
+			<td width='30%'><code>$name</code><br/><i style='font-size: 0.8em'>$type</i>
 			</td>
-			<td>$tooltip</td>
+			<td>
+				<p>$tooltip</p>
+				<p><code>fine.$fieldName.$name{$periodExample}</code>
+				</p>
+			</td>
 		</tr>";
         }
     ?>
