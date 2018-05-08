f900f2ec395827a852ee25687e3c07a2e4386475
www-data <www-data@qc-web-beta.c.continual-bay-418.internal>
2018-05-08 19:38:51 +0000

    Automated push from server made by Jared Broad

--- 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
+++ 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
@@ -15,11 +15,22 @@ foreach ($decoded['fundamentals'] as $field) {
 		<?php
         foreach ($field['properties'] as $property)
         {
-            $name = $property['name'];    
+            $name = $property['name'];
+            $type = $property['type'];
+            $isMultiPeriod = strpos($type, "QuantConnect.Data.Fundamental.") !== false;
+            
+            $type = str_replace("System.", "", $type);
+            $type = str_replace("QuantConnect.Data.Fundamental.", "", $type);
+            
+            if ($isMultiPeriod) $type = "<a href='https://github.com/QuantConnect/Lean/tree/master/Common/Data/Fundamental/Generated'>$type : MultiPeriodField</a>";
+            
             $tooltip = htmlentities($property['description']);
+            
             echo "
 <tr>
-			<td width='30%'>$name</td>
+			<td width='30%'>$name <br/>
+				<small>$type</small>
+			</td>
 			<td>$tooltip</td>
 		</tr>";
         }
