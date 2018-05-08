0ecfba2d4c6786ac61fe8ee8307da096b91c5502
www-data <www-data@qc-web-beta.c.continual-bay-418.internal>
2018-05-08 16:54:07 +0000

    Automated push from server made by Jared Broad

--- 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
+++ 08 Data Library/03 Fundamentals/03 Reference Tables/content.php
@@ -1,2 +1,29 @@
-x
-<?php    echo "test";   $fun = file_get_contents("http://www.quantconnect.com/services/fundamentals");   $fun = json_decode($fun);   print_r($fun);?>
+<?php
+$fields = file_get_contents("https://www.quantconnect.com/services/fundamentals");
+$decoded = json_decode($fields, true);
+foreach ($decoded['fundamentals'] as $field) {
+    ?>
+<table class="table qc-table table-itemized table-reflow">
+	<thead>
+		<tr>
+			<th colspan="2">
+				<?php echo $field['name']; ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+        foreach ($field['properties'] as $property)
+        {
+            $name = $property['name'];    
+            $tooltip = htmlentities($property['description']);
+            echo "
+		<tr>
+			<td>$name</td>
+			<td>$tooltip</td>
+		</tr>";
+        }
+    ?>
+	</tbody>
+</table>
+<?php } ?>
