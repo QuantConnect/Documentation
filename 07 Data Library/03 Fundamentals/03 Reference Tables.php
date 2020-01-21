924ec28da441359f5f8da40dd8dd85dbfc95fd92
Gustavo Aviles <gaviles@users.noreply.github.com>
2020-01-20 17:55:47 -0800

    Add dummy element with an id that can be reference from a hash link
    This feature is required in order to make the table searchable and referable from out of the docs links.

--- 07 Data Library/03 Fundamentals/03 Reference Tables.php
+++ 07 Data Library/03 Fundamentals/03 Reference Tables.php
@@ -1,87 +1,107 @@
 <p>
-The following reference tables detail the specific objects available for use in your QuantConnect Fine Universe filter. These properties are subsets of the <code>FineFundamental</code> object presented to your universe filter each day.
+    The following reference tables detail the specific objects available for use in your QuantConnect Fine Universe
+    filter. These properties are subsets of the <code>FineFundamental</code> object presented to your universe filter
+    each day.
 </p>
 <style>
-.fundamental-type {
-    width: 30% !important;
-    word-wrap: break-word !important;
-}
-.fundamental-type > code, .fundamental-type > p {
-    word-break: break-all;
-}
+    .fundamental-type {
+        width: 30% !important;
+        word-wrap: break-word !important;
+    }
+
+    .fundamental-type > code, .fundamental-type > p {
+        word-break: break-all;
+    }
+
+    .fundamental-table .dummy-reference {
+        position: relative;
+        top: -100px;
+        display: block;
+    }
 </style>
-<?php 
-$fields = file_get_contents("https://cdn.quantconnect.com/docs/fundamentals.json"); 
+<?php
+$fields = file_get_contents("https://cdn.quantconnect.com/docs/fundamentals.json");
 $decoded = json_decode($fields, true);
-foreach ($decoded['fundamentals'] as $field) 
-{
+foreach ($decoded['fundamentals'] as $field) {
     $fieldName = trim($field['name']);
     ?>
     <table class="table qc-table fundamental-table table-itemized table-reflow">
         <thead>
-		<tr>
-			<th colspan="2">
-				<?php echo $field['name']; ?>
-			</th>
-		</tr>
+        <tr>
+            <th colspan="2">
+                <?php echo $field['name']; ?>
+            </th>
+        </tr>
         </thead>
         <tbody>
-		<?php
-        foreach ($field['properties'] as $property)
-        {
+        <?php
+        foreach ($field['properties'] as $property) {
             $periodExample = "";
             $propertyName = $property['name'];
             $isMultiPeriod = $property['multiperiodfield'];
             $children = $property['child-properties'];
-            
+
             $type = $property['type'];
             $type = str_replace("System.", "", $type);
             $type = str_replace("QuantConnect.Data.Fundamental.", "", $type);
-            
+
             if ($isMultiPeriod) {
-               $type = "<a style='color: #747f8e !important;' href='https://raw.githubusercontent.com/QuantConnect/Lean/master/Common/Data/Fundamental/Generated/MultiPeriodValueTypes.cs'>MultiPeriodField</a>";
-               $periodExample = ".OneMonth";
+                $type = "<a style='color: #747f8e !important;' href='https://raw.githubusercontent.com/QuantConnect/Lean/master/Common/Data/Fundamental/Generated/MultiPeriodValueTypes.cs'>MultiPeriodField</a>";
+                $periodExample = ".OneMonth";
             }
-            
+
             $description = htmlentities($property['description']);
-            
-            if ( count($children) == 0) {
+
+            if (count($children) == 0) {
+
+                $htmlId = "{$fieldName}-{$propertyName}";
+                $htmlId = strtolower($htmlId);
+                $htmlId = preg_replace('/\s+/m', '-', $htmlId);
+
                 ?>
                 <tr>
                     <td class="fundamental-type">
-                        <code><?=$propertyName?></code><br/>
-                        <p><?=$type?></p>
+                        <span class="dummy-reference" id="<?= $htmlId ?>"></span><code><?= $propertyName ?></code><br/>
+                        <p><?= $type ?></p>
                     </td>
                     <td>
-                        <p><?=$description?></p>
+                        <p><?= $description ?></p>
                         <pre class='prettyprint' style='border: none !important; background: transparent; font-size: 1em;'><?php echo "fine.{$fieldName}.{$propertyName}{$periodExample}"; ?></pre>
                     </td>
                 </tr>
                 <?php
             } else {
-                foreach($children as $child) {
-                
+                foreach ($children as $child) {
+
                     $childType = trim($child['type']);
                     $childName = trim($child['name']);
-                
+
                     if ($child['multiperiodfield']) {
-                       $childType = "<a style='color: #747f8e !important;' href='https://raw.githubusercontent.com/QuantConnect/Lean/master/Common/Data/Fundamental/Generated/MultiPeriodValueTypes.cs'>MultiPeriodField</a>";
-                       $periodExample = ".OneMonth";
-                    } 
-                
+                        $childType = "<a style='color: #747f8e !important;' href='https://raw.githubusercontent.com/QuantConnect/Lean/master/Common/Data/Fundamental/Generated/MultiPeriodValueTypes.cs'>MultiPeriodField</a>";
+                        $periodExample = ".OneMonth";
+                    }
+
+                    $htmlId = "{$fieldName}-{$propertyName}-{$childName}";
+                    $htmlId = strtolower($htmlId);
+                    $htmlId = preg_replace('/\s+/m', '-', $htmlId);
+
                     ?>
                     <tr>
-                        <td class="fundamental-type"><code><?="{$propertyName}.{$childName}"; ?></code><br/><p><?=$childType; ?></p>
+                        <td class="fundamental-type">
+                            <span class="dummy-reference" id="<?= $htmlId ?>"></span>
+                            <code><?= "{$propertyName}.{$childName}"; ?></code><br/>
+                            <p><?= $childType; ?></p>
                         </td>
                         <td>
-                            <p><?=$child['description']?></p>
-<pre class='prettyprint' style='border: none !important; background: transparent; font-size: 1em;'><?php echo "fine.{$fieldName}.{$propertyName}.{$childName}{$periodExample}"; ?></pre></td>
+                            <p><?= $child['description'] ?></p>
+                            <pre class='prettyprint' style='border: none !important; background: transparent; font-size: 1em;'><?php echo "fine.{$fieldName}.{$propertyName}.{$childName}{$periodExample}"; ?></pre>
+                        </td>
                     </tr>
                     <?php
                 }
             }
         }
-    ?>
-	</tbody>
-</table>
+        ?>
+        </tbody>
+    </table>
 <?php } ?>
