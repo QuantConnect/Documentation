9226ab0e93098eb6da6183468070452c5d4fdec9
Jared Broad <jaredbroad@gmail.com>
2021-12-07 18:19:28 -0800

    SCandir

--- 02 Writing Algorithms/02 User Guides/07 Trading and Orders/06 Trade Models/03 Slippage/02 What is Slippage%3F.php
+++ 02 Writing Algorithms/02 User Guides/07 Trading and Orders/06 Trade Models/03 Slippage/02 What is Slippage%3F.php
@@ -1,3 +1,3 @@
 <?php
-	echo print_r(scandir("../../../"), true); // ; // file_get_contents("__DIR__/../../../15 Reality Modelling/03 Slippage/02 What is Slippage%3F.html");
+	echo print_r(scandir("."), true); // ; // file_get_contents("__DIR__/../../../15 Reality Modelling/03 Slippage/02 What is Slippage%3F.html");
 ?>
