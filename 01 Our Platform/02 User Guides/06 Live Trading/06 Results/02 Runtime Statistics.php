13ee6528e3c204c4be802a434935a1322f0ecd80
Web Editor <web.editor@quantconnect.com>
2021-12-08 19:05:46 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
@@ -1,4 +1,4 @@
-<p>Runtime statistics of your live performance are displayed in the banner at the top of the live performance page. You can create your own custom runtime statistics, but the following statistics are included by default:</p>
+<p>Runtime statistics of your live algorithm are displayed in the banner at the top of the live performance page. You can create your own custom runtime statistics, but the following statistics are included by default:</p>
 
 <ul>
     <li><b>PSR</b>: Probabilistic Sharpe Ratio</li>
@@ -12,7 +12,7 @@
 </ul>
 
 <img style="max-width: 100%; margin-bottom: 20px" src="https://cdn.quantconnect.com/i/tu/runtime-statistics.png">
-<p>Call <code>SetRuntimeStatistic</code> with <code>name</code> and <code>value</code> arguments to add a run-time statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
+<p>Call the <code>SetRuntimeStatistic</code> method with a <code>name</code> and <code>value</code> to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
 
 <div class="section-example-container">
     <pre class="csharp">SetRuntimeStatistic(name, value);</pre>
