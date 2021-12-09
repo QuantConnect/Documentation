186780c5ec906f409f4d0bc8a25681f9316c967f
Web Editor <web.editor@quantconnect.com>
2021-12-08 19:08:04 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
@@ -14,7 +14,7 @@
 
 <img style="max-width: 100%; margin-bottom: 20px" src="https://cdn.quantconnect.com/i/tu/runtime-statistics.png">
 
-<p>Call <code>SetRuntimeStatistic</code> with <code>name</code> and <code>value</code> arguments to add a run-time statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
+<p>Call <code>SetRuntimeStatistic</code> with <code>name</code> and <code>value</code> arguments to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
 
 <div class="section-example-container">
     <pre class="csharp">SetRuntimeStatistic("My Custom statistic", myCustomStatisticsValue);</pre>
