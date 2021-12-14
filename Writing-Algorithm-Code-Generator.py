5a2466e3d47d5d76d5da66b040aae0599ad92807
Louis Szeto <hke0073@icloud.com>
2021-12-15 01:26:39 +0800

    Refactor API

--- Writing-Algorithm-Code-Generator.py
+++ Writing-Algorithm-Code-Generator.py
@@ -2,27 +2,32 @@ import os
 import pathlib
 from urllib.request import urlopen
 
-sources = ["http://cdn.quantconnect.com.s3.us-east-1.amazonaws.com/terminal/cache/api/csharp_tree.json"]
+source = "http://cdn.quantconnect.com.s3.us-east-1.amazonaws.com/terminal/cache/api/csharp_tree.json"
 
 base = "02 Writing Algorithms/04 API Reference/"
-dir_ = {"Adding Data": "01 Adding Data/",
-       "Algorithm Framework": "02 Algorithm Framework/",
-       "Charting": "03 Charting/",
-       "Consolidating Data": "04 Consolidating Data/",
-       "Handling Data": "05 Handling Data/",
-       "Historical Data": "06 Historical Data/",
-       "Indicators": "07 Indicators/",
-       "Live Trading": "08 Live Trading/",
-       "Logging": "09 Logging/",
-       "MachineLearning": "10 Machine Learning/",
-       "Modeling": "11 Modeling/",
-       "Parameter and Optimization": "12 Parameter and Optimization/",
-       "Scheduled Events": "13 Scheduled Events/",
-       "Securities and Portfolio": "14 Securities and Portfolio/",
-       "Trading and Orders": "15 Trading and Orders/",
-       "Universes": "16 Universes/"}
+dir_ = {"Adding Data": "02 Adding Data/",
+       "Algorithm Framework": "03 Algorithm Framework/",
+       "Charting": "04 Charting/",
+       "Consolidating Data": "05 Consolidating Data/",
+       "Handling Data": "06 Handling Data/",
+       "Historical Data": "07 Historical Data/",
+       "Indicators": "08 Indicators/",
+       "Live Trading": "09 Live Trading/",
+       "Logging": "10 Logging/",
+       "MachineLearning": "11 Machine Learning/",
+       "Modeling": "12 Modeling/",
+       "Parameter and Optimization": "13 Parameter and Optimization/",
+       "Scheduled Events": "14 Scheduled Events/",
+       "Securities and Portfolio": "15 Securities and Portfolio/",
+       "Trading and Orders": "16 Trading and Orders/",
+       "Universes": "17 Universes/"}
 
 counter = {key: 0 for key in dir_.keys()}
+
+path_ = pathlib.Path(base)
+path_.mkdir(parents=True, exist_ok=True)
+with open(path_ / "01 Overview.html", "w", encoding="utf-8") as html_file:
+    html_file.write(f'<p>Available API References:<br/></p>')
 
 def Table(input_, previous_name, n, type_map):
     if "DocumentationAttributes" not in input_ or not "DocumentationAttributes":
@@ -76,6 +81,13 @@ def Table(input_, previous_name, n, type_map):
             path = pathlib.Path(base + dir_[tag] + f"{counter[tag]:02} " + name)
             
             i = n
+            
+        with open(path_ / "01 Overview.html", "rb+") as html_file:
+            html_file.seek(-4, os.SEEK_END)
+            html_file.truncate()
+            
+        with open(path_ / "01 Overview.html", "a", encoding="utf-8") as html_file:
+            html_file.write(f'<ul><li><a href="#{call}"><i class="fa fa-link"></i>{call}</a></li></ul></p>')
             
         with open(path / f'{i:02} {call}.html', "w", encoding="utf-8") as html_file:
             html_file.write(Box(input_, type_map))
@@ -156,10 +168,12 @@ def Box(input_, type_map):
             
     else:
         ret += "This method provides no return."
+        
+    slash = '\"'
     
     write_up = f"""<div style="padding: 10px; border: 1px solid #ccc; margin-bottom: 25px; border-radius: 3px">
-<a href="#jump"><code>{call}</code></a>
-<p>{input_["Description"] if "Description" in input_ else ""}</p>
+<a id={call}><code>{call}</code></a>
+<p>{input_["Description"].replace("{slash}", "") if "Description" in input_ else ""}</p>
 <h4>Parameters</h4>
 {params}
 
@@ -170,25 +184,24 @@ def Box(input_, type_map):
     return write_up
 
 
-for source in sources:
-    json_file = urlopen(source).read().decode("utf-8")
-    json_file = json_file.replace("true", "True").replace("false", "False").replace("null", "None")
-    doc = eval(json_file)
-    
-    keys = doc["keys"]
-    
-    type_map = {}
-    for key in keys.items():
-        if "GenericParameters" in key[1]:
-            type_map[key[0]] = key[1]["Type"].split(".")[-1][:-1]
-        else:
-            type_map[key[0]] = key[1]["ShortType"]
+json_file = urlopen(source).read().decode("utf-8")
+json_file = json_file.replace("true", "True").replace("false", "False").replace("null", "None")
+doc = eval(json_file)
 
-    algo_methods = [doc["tree"]["core"]["data"][0]["children"], keys.values()]
+keys = doc["keys"]
+
+type_map = {}
+for key in keys.items():
+    if "GenericParameters" in key[1]:
+        type_map[key[0]] = key[1]["Type"].split(".")[-1][:-1]
+    else:
+        type_map[key[0]] = key[1]["ShortType"]
+
+algo_methods = [doc["tree"]["core"]["data"][0]["children"], keys.values()]
+
+for branch in algo_methods:
+    previous_name = ""
+    i = 4
     
-    for branch in algo_methods:
-        previous_name = ""
-        i = 4
-        
-        for item in branch:
-            previous_name, i = Table(item, previous_name, i, type_map)
+    for item in branch:
+        previous_name, i = Table(item, previous_name, i, type_map)
