89d2d9856bef5d6c7f83b0e035f1e36a1af5d0a9
Jared <jaredbroad@gmail.com>
2021-12-21 10:36:37 -0800

    Update 02 Available Resources.php

--- 01 Our Platform/02 User Guides/01 Organizations/02 Resources/02 Available Resources.php
+++ 01 Our Platform/02 User Guides/01 Organizations/02 Resources/02 Available Resources.php
@@ -3,7 +3,7 @@
 <h4>Backtesting Nodes</h4>
 <p>Organizations need backtesting nodes to run backtests. The more backtesting nodes an organization has, the more concurrent backtests the members can run. Several models of backtesting nodes are available. Backtesting nodes that are more powerful can run faster backtests and backtest nodes with more RAM can handle more memory-intensive operations like training machine learning models, processing Options data, and managing large universes. The following table shows the specifications of the backtesting node models:<br></p>
 
-<?php echo file_get_contents("{__DIR__}/../../../../Resources/backtest-nodes-table.html"); ?>
+<?php echo file_get_contents(__DIR__."/../../../../Resources/backtest-nodes-table.html"); ?>
 
 <p>Refer to the <a href="https://www.quantconnect.com/pricing"><b>Pricing</b></a> page to see the price of each backtesting node model. The first organization on each account is given one free B-MICRO backtesting node. It incurs a 20-second delay when launching backtests, but the delay is removed and the node is replaced when subscribing to a new backtesting node in the organization.<br></p>
 
