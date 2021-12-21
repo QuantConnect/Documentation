e679115ad41d3d575bc8ae9bef66f9802dc4daa4
Web Editor <web.editor@quantconnect.com>
2021-12-20 19:35:22 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/09 Alpha Streams Market/05 Authors/05 Listings.php
+++ 01 Our Platform/02 User Guides/09 Alpha Streams Market/05 Authors/05 Listings.php
@@ -1,2 +1,3 @@
-What is the My Listings page:<br>- A page that shows all of your alphas that are listed in the marketplace<br>- A page to submit a new alpha<br><div><br></div><div>Why use the My Listings page:</div>- To see the performance of your alphas, including all of the key metrics mentioned earlier in the Navigation &gt; Strategies list<br><div>
-</div> 
+<?php
+	echo file_get_contents(__DIR__."/../03 Navigation/05 My Listings.html");
+?>
