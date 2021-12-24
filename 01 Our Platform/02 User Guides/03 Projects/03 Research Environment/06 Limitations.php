8560244a16ce855dbdf61e8400fcc0db7edd0f39
Derek Melchin <melchin.derek@gmail.com>
2021-12-23 17:53:25 -0700

    Add node blocks

--- 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/06 Limitations.php
+++ 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/06 Limitations.php
@@ -1,7 +1,9 @@
 <p>There are several limitations to note when working with the Research Environment.</p>
 
 <h4>Node Specifications</h4>
-<p>Research nodes are not as powerful as backtesting and live trading nodes because the Research Environment only works with static data. In the Research Environment, you can control the amount of data that you're analyzing. In backtesting, there is much more data to process.</p>
+<p>Research nodes are not as powerful as backtesting and live trading nodes because the Research Environment only works with static data. In the Research Environment, you can control the amount of data that you're analyzing. In backtesting, there is much more data to process. The following table shows the specifications of the research node models:</p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/research-nodes-table.html"); ?>
 
 <h4>Collaboration</h4>
 <p>It's not currently possible to simultaneously work on the same notebook with other members in your organization because the functionality is not supported by Jupyter. Jupyter doesn't recognize an edit as a change to the file until after you run the cell.</p>
---
+++ 01 Our Platform/02 User Guides/06 Live Trading/03 Deployment/02 Resources.php
@@ -0,0 +1,5 @@
+<p>You need a live trading node for each algorithm you deploy on our co-located servers. Several models of live trading nodes are available. More powerful live trading nodes allow you to run algorithms with larger universes, increase your notification limit, and give you more time for machine learning training. The following table shows the specifications of the live trading node models:</p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/live-trading-nodes-table.html"); ?>
+
+<p>Refer to the <a href="/pricing"><b>Pricing</b></a> page to see the price of each live trading node model.</p>
