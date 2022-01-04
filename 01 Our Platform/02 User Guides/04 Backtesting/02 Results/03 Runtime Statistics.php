516256bb033613c227ae23360c25ab9919082ca4
Web Editor <web.editor@quantconnect.com>
2022-01-04 13:44:03 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
@@ -1,4 +1,8 @@
-<p>Runtime statistics of your backtest performance are displayed in the banner at the top of the backtest results page. You can create your own custom runtime statistics, but the following statistics are included by default:<br></p>
+<p>Performance statistics of your backtest are displayed in the banner at the top of the backtest results page.</p>
+
+<img style="max-width: 100%; margin-bottom: 20px" src="https://cdn.quantconnect.com/i/tu/runtime-statistics.png">
+
+<p>You can create your own custom runtime statistics, but the following statistics are included by default:<br></p>
 
 <ul>
     <li><b>PSR</b>: Probabilistic Sharpe Ratio<br></li>
@@ -11,8 +15,6 @@
     <li><b>Volume</b>: Total sale volume since the start of algorithm operations</li>
     <li><b>Capacity</b>: The maximum amount of capital the strategy can trade.<br></li>
 </ul>
-
-<img style="max-width: 100%; margin-bottom: 20px" src="https://cdn.quantconnect.com/i/tu/runtime-statistics.png">
 
 <p>Call <code>SetRuntimeStatistic</code> with <code>name</code> and <code>value</code> arguments to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
 
