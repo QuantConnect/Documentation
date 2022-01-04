0bb3ba6fae4a5eec07b8b8de4b68112708744525
Web Editor <web.editor@quantconnect.com>
2022-01-03 19:34:25 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/06 Limitations.php
+++ 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/06 Limitations.php
@@ -1,7 +1,7 @@
 <p>There are several limitations to note when working with the Research Environment.</p>
 
 <h4>Node Specifications</h4>
-<p>Research nodes are not as powerful as backtesting and live trading nodes because the Research Environment only works with static data. In the Research Environment, you can control the amount of data that you're analyzing. In backtesting, there is much more data to process. The following table shows the specifications of the research node models:</p>
+<p>Research nodes are not as powerful as backtesting and live trading nodes because the Research Environment only works with static data. In the Research Environment, you can control the amount of data that you're analyzing. In backtesting, there is much more data to process so you need more powerful nodes. The following table shows the specifications of the research node models:</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/research-nodes-table.html"); ?>
 
