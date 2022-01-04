59cf6f483ef9243800cd23c49415be9088b5b64a
Web Editor <web.editor@quantconnect.com>
2022-01-04 15:31:27 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/02 Results/03 Runtime Statistics.php
@@ -15,7 +15,7 @@
     <li><b>Volume</b>: Total sale volume since the start of backtest</li>
     <li><b>Capacity</b>: The maximum amount of capital the strategy can trade.<br></li>
 </ul>
-<p>Call <code>SetRuntimeStatistic</code> with <code>name</code> and <code>value</code> arguments to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
+<p>Call the <code>SetRuntimeStatistic</code> method with a <code>name</code> and <code>value</code> to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
 <div class="section-example-container">
     <pre class="csharp">SetRuntimeStatistic("My Custom statistic", myCustomStatisticsValue);</pre>
     <pre class="python">self.SetRuntimeStatistic("My Custom statistic", my_custom_statistics_value)</pre>
