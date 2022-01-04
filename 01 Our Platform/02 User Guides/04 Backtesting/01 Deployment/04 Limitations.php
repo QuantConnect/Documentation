7aec54b1f74f5260230bdc34bc40b38927066a80
Derek Melchin <melchin.derek@gmail.com>
2022-01-04 10:25:42 -0700

    Use blocks for log limits and supported packages

---
+++ 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/04 Limitations.php
@@ -0,0 +1,26 @@
+<p>There are limitations on the number of nodes your organization can have, the number of orders you complete in a backtest, and the amount of logs that you can produce. The limitations are eased as your organization grows to higher tiers.</p>
+
+<h4>Nodes</h4>
+<p>The number of backtesting nodes you can have in your organization depends on the tier of your organization. The maximum number of backtesting nodes for each tier is the following:</p>
+<ul>
+    <li><b>Free</b>: 1</li>
+    <li><b>Quant Researcher</b>: 2</li>
+    <li><b>Team</b>: 10</li>
+    <li><b>Trading Firm</b>: Unlimited</li>
+    <li><b>Institution</b>: Unlimited</li>
+</ul>
+
+<h4>Orders</h4>
+<p>The number of orders you can place in a single backtest depends on the tier of your organization. The maximum number of orders per backtest for each tier is the following:<br></p>
+<ul>
+    <li><b>Free</b>: 10K</li>
+    <li><b>Quant Researcher</b>: 10M</li>
+    <li><b>Team</b>: Unlimited</li>
+    <li><b>Trading Firm</b>: Unlimited</li>
+    <li><b>Institution</b>: Unlimited</li>
+</ul>
+
+<h4>Logs</h4>
+<p>The amount of logs that you can generate per backtest and per day depends on the tier of your organization. The following table describes the amount of logs that you can generate on each tier:<br></p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/log-limits.html"); ?>
