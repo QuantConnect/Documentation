326e05aaafa8a9d92ef7dc5682b609fb17a02834
Web Editor <web.editor@quantconnect.com>
2022-01-06 16:18:14 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
@@ -7,12 +7,12 @@
 <ul>
     <li><b>PSR</b>: Probabilistic Sharpe Ratio</li>
     <li><b>Unrealized</b>: Unrealized profit</li>
-    <li><b>Fees</b>: Total fees paid during the algorithm operation across all securities in the portfolio</li>
+    <li><b>Fees</b>: Total fees paid during the live deployment across all securities in the portfolio</li>
     <li><b>Net Profit</b>: Sum of all gross profit across all securities in the portfolio</li>
     <li><b>Return</b>: Return = (current equity - starting equity) / starting equity</li>
-    <li><b>Equity</b>: Total portfolio value if we sold all holdings at current market rates</li>
+    <li><b>Equity</b>: Total portfolio value if you sold all holdings at current market rates</li>
     <li><b>Holdings</b>: Absolute sum of the items in the portfolio</li>
-    <li><b>Volume</b>: Total sale volume since the start of algorithm operations</li>
+    <li><b>Volume</b>: Total sale volume since the start of the live deployment</li>
 </ul>
 
 <p>Call the <code>SetRuntimeStatistic</code> method with a <code>name</code> and <code>value</code> to add a runtime statistic to the banner. The <code>value</code> argument can be a <code>string</code> or a number. If you pass a number, it is cast to a <code>string</code>.</p>
