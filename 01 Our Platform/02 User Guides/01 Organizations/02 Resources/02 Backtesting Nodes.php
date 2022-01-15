0db705d159a1431d194349dac4a713d02ad50c47
Derek Melchin <melchin.derek@gmail.com>
2022-01-14 18:15:21 -0700

    Update Organization Resources page

---
+++ 01 Our Platform/02 User Guides/01 Organizations/02 Resources/02 Backtesting Nodes.php
@@ -0,0 +1,5 @@
+<p>You need backtesting nodes to run backtests. The more backtesting nodes your organization has, the more concurrent backtests the you can run. Several models of backtesting nodes are available. Backtesting nodes that are more powerful can run faster backtests and backtest nodes with more RAM can handle more memory-intensive operations like training machine learning models, processing Options data, and managing large universes. The following table shows the specifications of the backtesting node models:<br></p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/backtest-nodes-table.html"); ?>
+
+<p>Refer to the <a href="https://www.quantconnect.com/pricing"><b>Pricing</b></a> page to see the price of each backtesting node model. You get one free B-MICRO backtesting node in your first organization. This node incurs a 20-second delay when you launch backtests, but the delay is removed and the node is replaced when you subscribe to a new backtesting node in the organization.<br></p>
---
+++ 01 Our Platform/02 User Guides/01 Organizations/02 Resources/03 Research Nodes.php
@@ -0,0 +1,5 @@
+<p>You need research nodes to use the Research Environment. Several models of research nodes are available. More powerful research nodes allow you to handle more data and run faster computations in your notebooks. The following table shows the specifications of the research node models: </p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/research-nodes-table.html"); ?>
+
+<p>Refer to the <a href="https://www.quantconnect.com/pricing"><b>Pricing</b></a> page to see the price of each research node model. You get one free R1-4 research node in your first organization, but the node is replaced when you subscribe to a new research node in the organization.</p>
