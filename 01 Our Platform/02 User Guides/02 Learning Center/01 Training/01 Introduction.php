3bd530ef408b6f5540b58513570d977af89b0a41
Web Editor <web.editor@quantconnect.com>
2022-01-17 18:43:23 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/02 Learning Center/01 Training/01 Introduction.php
+++ 01 Our Platform/02 User Guides/02 Learning Center/01 Training/01 Introduction.php
@@ -2,4 +2,4 @@
 	echo file_get_contents(DOCS_RESOURCES."/introduce-learning-center.html");
 ?>
 
-<p>We support onboarding team members with the Learning Center content. The Learning Center enables you to systematically track and monitor the progress of your team members on the courses that you purchase or create. If you purchase or create a course, you can access it from the <b>Organization > Resources</b> page in the Algorithm Lab.</p>
+<p>Onboard new team members to your organization through the content in the Learning Center. The Learning Center enables you to systematically track and monitor the progress of your team members on the courses that you purchase or create. If you purchase or create a course, you can access it from the <b>Organization > Resources</b> page in the Algorithm Lab.</p>
