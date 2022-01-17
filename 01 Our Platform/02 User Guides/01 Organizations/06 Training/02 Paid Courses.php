3a0e1fe9af77dacdc721b8a388f4f46483970e53
Web Editor <web.editor@quantconnect.com>
2022-01-17 18:45:15 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/01 Organizations/06 Training/02 Paid Courses.php
+++ 01 Our Platform/02 User Guides/01 Organizations/06 Training/02 Paid Courses.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../../02 Learning Center/01 Training/02 Paid Courses.html");
+    echo file_get_contents(__DIR__."/../02 Learning Center/01 Training/02 Paid Courses.html");
 ?>
