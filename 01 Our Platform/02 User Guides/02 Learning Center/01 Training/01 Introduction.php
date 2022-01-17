91be17457344dfd95172fc826d2d6eebae163b36
Web Editor <web.editor@quantconnect.com>
2022-01-17 18:25:06 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/02 Learning Center/01 Training/01 Introduction.php
+++ 01 Our Platform/02 User Guides/02 Learning Center/01 Training/01 Introduction.php
@@ -1,3 +1,5 @@
 <?php
 	echo file_get_contents(DOCS_RESOURCES."/introduce-learning-center.html");
 ?>
+
+<p>We support onboarding team members with the Learning Center content. The Learning Center enables you to systematically track and monitor the progress of your team members on the courses that you purchase or create.</p>
