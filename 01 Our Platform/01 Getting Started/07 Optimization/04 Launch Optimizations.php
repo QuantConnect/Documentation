28cb55345a52208f284730a1e2eda30032669ba2
Web Editor <web.editor@quantconnect.com>
2022-01-18 12:30:24 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/07 Optimization/04 Launch Optimizations.php
+++ 01 Our Platform/01 Getting Started/07 Optimization/04 Launch Optimizations.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../03 Tutorials/07 Optimization/01 Running Optimizations/02 Launch Optimization.html");
+    echo file_get_contents(__DIR__."/../../03 Tutorials/07 Optimization/01 Running Optimizations/02 Launch Optimization.html");
 ?>
