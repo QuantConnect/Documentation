e767be21dfb809b61c087d9ac79c1efe23f7365c
Web Editor <web.editor@quantconnect.com>
2022-01-17 22:19:22 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/02 Research Nodes.php
+++ 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/02 Research Nodes.php
@@ -1,5 +1,1 @@
-<p>The following table shows the specifications of the research node models:</p>
-
-<?php echo file_get_contents(DOCS_RESOURCES."/research-nodes-table.html"); ?>
-
-<p>We recommend using more powerful nodes when requesting data for large universes or when training machine learning models.</p>
+<?php echo file_get_contents(__DIR__."/..//01 Organizations/02 Resources/03 Research Nodes.php"); ?>
