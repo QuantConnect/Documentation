f2087cea033c924a6c68729326ad97b0c598a694
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2022-01-18 23:57:48 +0000

    Refactors Working with Indicators Tutorials
    Creates one tutoral for each type of indicator: DataPoint, Bar, TradeBar,
    and Composite to simplify the tutorials.

--- 03 Research Environment/03 Tutorials/03 Working with Indicators/03 Custom Resolutions/03 Get Historical Data.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo file_get_contents(__DIR__."/../01 Basic Usage/03 Get Historical Data.html");
-?>
---
+++ 03 Research Environment/03 Tutorials/03 Working with Indicators/06 Custom Resolutions/03 Get Historical Data.php
@@ -0,0 +1,3 @@
+<?php
+	echo file_get_contents(__DIR__."/../01 DataPoint Indicator/03 Get Historical Data.html");
+?>
