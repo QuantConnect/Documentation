97046d6c2ae49739034dcc4330fc8e4da1a02be3
Web Editor <web.editor@quantconnect.com>
2022-01-18 12:29:43 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/08 Alpha Streams Market/04 Place New Bids.php
+++ 01 Our Platform/01 Getting Started/08 Alpha Streams Market/04 Place New Bids.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../03 Tutorials/08 Alpha Streams Market/02 Licensing Alphas/04 Place New Bids.html");
+    echo file_get_contents(__DIR__."/../../03 Tutorials/08 Alpha Streams Market/02 Licensing Alphas/04 Place New Bids.html");
 ?>
