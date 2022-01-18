0949c31d6dc703cea750ca0ae20f44fdcbf0a454
Web Editor <web.editor@quantconnect.com>
2022-01-18 12:29:58 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/07 Optimization/02 Purchase QCC.php
+++ 01 Our Platform/01 Getting Started/07 Optimization/02 Purchase QCC.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../03 Tutorials/01 Organizations/05 Billing/05 Purchase QCC.html");
+    echo file_get_contents(__DIR__."/../../03 Tutorials/01 Organizations/05 Billing/05 Purchase QCC.html");
 ?>
