3fc0d8d585e0f060b9b18a33df224c64d1615889
Web Editor <web.editor@quantconnect.com>
2022-01-18 17:15:37 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/06 Live Trading/03 Deployment/02 Resources.php
+++ 01 Our Platform/02 User Guides/06 Live Trading/03 Deployment/02 Resources.php
@@ -1,5 +1,9 @@
+###
+<br> TODO: merge this into the Organization > Resources > Live Trading Nodes section
+<br>###
+
 <p>You need a live trading node for each algorithm that you deploy to our co-located servers. Several models of live trading nodes are available. More powerful live trading nodes allow you to run algorithms with larger universes and give you <a href='../organizations/resources#04-Resource-Limits'>more time for machine learning training</a>. The following table shows the specifications of the live trading node models:</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/live-trading-nodes-table.html"); ?>
 
-<p>Refer to the <a href="/pricing"><b>Pricing</b></a> page to see the price of each live trading node model.</p>
+<p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each live trading node model.</p>
