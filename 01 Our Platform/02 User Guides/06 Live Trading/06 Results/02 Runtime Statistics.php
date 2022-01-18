8a80d29c076698fea9ca459e0c3270648c8a8214
Web Editor <web.editor@quantconnect.com>
2022-01-18 17:27:44 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/06 Live Trading/06 Results/02 Runtime Statistics.php
@@ -5,14 +5,14 @@
 <p>You can create your own custom runtime statistics, but the following statistics are included by default:</p>
 
 <ul>
-    <li><b>PSR</b>: <a href='../optimization/objectives#05-PSR'>Probabilistic Sharpe Ratio</a></li>
-    <li><b>Unrealized</b>: Unrealized profit</li>
-    <li><b>Fees</b>: Total fees paid during the live deployment across all securities in the portfolio</li>
-    <li><b>Net Profit</b>: Sum of all gross profit across all securities in the portfolio</li>
-    <li><b>Return</b>: Return = (current equity - starting equity) / starting equity</li>
-    <li><b>Equity</b>: Total portfolio value if you sold all holdings at current market rates</li>
-    <li><b>Holdings</b>: Absolute sum of the items in the portfolio</li>
-    <li><b>Volume</b>: Total sale volume since the start of the live deployment</li>
+    <li><span class='ul-option'>PSR</span>: <a href='../optimization/objectives#05-PSR'>Probabilistic Sharpe Ratio</a></li>
+    <li><span class='ul-option'>Unrealized</span>: Unrealized profit</li>
+    <li><span class='ul-option'>Fees</span>: Total fees paid during the live deployment across all securities in the portfolio</li>
+    <li><span class='ul-option'>Net Profit</span>: Sum of all gross profit across all securities in the portfolio</li>
+    <li><span class='ul-option'>Return</span>: Return = (current equity - starting equity) / starting equity</li>
+    <li><span class='ul-option'>Equity</span>: Total portfolio value if you sold all holdings at current market rates</li>
+    <li><span class='ul-option'>Holdings</span>: Absolute sum of the items in the portfolio</li>
+    <li><span class='ul-option'>Volume</span>: Total sale volume since the start of the live deployment</li>
 </ul>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/create-custom-runtime-statistic.html"); ?>
