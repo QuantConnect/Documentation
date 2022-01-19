a610ccb26208a4a1afce46b9432c49d6a74197cd
Web Editor <web.editor@quantconnect.com>
2022-01-18 19:34:00 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/03 Tutorials/01 Organizations/01 Managing Organizations/08 Change Organization Tiers.php
+++ 01 Our Platform/03 Tutorials/01 Organizations/01 Managing Organizations/08 Change Organization Tiers.php
@@ -5,15 +5,13 @@
     echo file_get_contents(DOCS_RESOURCES."/upgrade-organization-tier.html");
 ?>
 
-
-
 <h4>Free Tier</h4>
 <p>Follow these steps to downgrade to the Free tier:</p>
 
 <ol>
     <li>Log in to the Algorithm Lab.</li>
-    <li>In the left navigation bar, click <b>Organization &gt; Home</b>.
-    </li><li>On the organization homepage, click <b>Downgrade to Free</b>.</li>
-    <li>Select the <b>I understand that my subscription will be terminated immediately</b> check box and then click <b>Continue</b>.</li>
-    <li>Select the reason for downgrading and then click <b>Downgrade</b></li>
+    <li>In the left navigation bar, click <span class='menu-name'>Organization &gt; Home</span>.</li>
+    <li>On the organization homepage, click <span class='button-name'>Downgrade to Free</span>.</li>
+    <li>Select the <span class='button-name'>I understand that my subscription will be terminated immediately</span> check box and then click <span class='button-name'>Continue</span>.</li>
+    <li>Select the reason for downgrading and then click <span class='button-name'>Downgrade</span></li>
 </ol>
