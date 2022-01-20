368c8edaf6b6ad0f1707a1dffec9b19a3b327363
Web Editor <web.editor@quantconnect.com>
2022-01-19 19:06:17 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/03 Tutorials/08 Alpha Streams Market/02 Licensing Alphas/06 Manage Subscriptions.php
+++ 01 Our Platform/03 Tutorials/08 Alpha Streams Market/02 Licensing Alphas/06 Manage Subscriptions.php
@@ -7,11 +7,11 @@
 ?>
 
 <h4>Update Alpha</h4>
-<p>If the author of an Alpha that you're running live updates the Alpha code, follow these steps to update the version you have deployed:</p>
+<p>If the author of an Alpha that you're running live updates the Alpha code, follow these steps to update the version that you have deployed:</p>
 <ol>
     <li>Stop the algorithm.</li>
-    <p>Refer to <a href="../live-trading/brokerages">the tutorial of your brokerage</a> for instructions.</p>
-    <li>In the project panel, click <b>Delete</b>.</li>
+    <p>For instructions on stopping the algorithm, refer to <a href="../live-trading/brokerages">the tutorial of your brokerage</a>.</p>
+    <li>In the project panel, click <span class='button-name'>Delete</span>.</li>
     <li>Redeploy the Alpha.</li>
 </ol>
 
@@ -19,11 +19,11 @@
 <p>Follow these steps to adjust the amount of capital that you allocate to an Alpha:</p>
 <ol>
     <li>Stop the algorithm.</li>
-    <p>Refer to <a href="../live-trading/brokerages">the tutorial of your brokerage</a> for instructions.</p>
-    <li>If you want to increase your allocation to the Alpha passed the allocation size that your subscription allows or you want to reduce your allocation to the Alpha passed your subscription period, <a href="#04-Manage-Existing-Bids">update your bid</a> with the new allocation size.</li>
+    <p>For instructions on stopping the algorithm, refer to <a href="../live-trading/brokerages">the tutorial of your brokerage</a>.</p>
+    <li>If you want to increase your allocation to the Alpha passed the allocation size that your subscription allows or you want to reduce your allocation to the Alpha passed your subscription period, <a href="#05-Manage-Existing-Bids">update your bid</a> with the new allocation size.</li>
     <li>Adjust the amount of capital in your brokerage account to the amount of capital that you want to allocate to the Alpha.</li>
     <li>Redeploy the Alpha.</li>
 </ol>
 
 <h4>Cancel Subscription</h4>
-<p>You can not cancel an Alpha subscription, but you may <a href="#04-Manage-Existing-Bids">cancel the auto-renewing bid</a> that you have for the Alpha. If you have strong reasons to request a cancellation and refund, <a href="/contact">contact us</a> with those reasons for evaluation.</p>
+<p>You can not cancel an Alpha subscription, but you may <a href="#05-Manage-Existing-Bids">cancel the auto-renewing bid</a> that you have for the Alpha. If you have strong reasons to request a cancellation and refund, <a href="/contact">contact us</a> with those reasons for evaluation.</p>
