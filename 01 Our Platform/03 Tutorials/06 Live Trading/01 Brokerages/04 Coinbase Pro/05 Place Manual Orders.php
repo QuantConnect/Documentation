3cd392c515194527f9ca16b9b18ff3709df1772c
Derek Melchin <melchin.derek@gmail.com>
2022-01-21 13:35:19 -0700

    Remove relative links from brokerage tutorials

--- 01 Our Platform/03 Tutorials/06 Live Trading/01 Brokerages/04 Coinbase Pro/05 Place Manual Orders.php
+++ 01 Our Platform/03 Tutorials/06 Live Trading/01 Brokerages/04 Coinbase Pro/05 Place Manual Orders.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../01 Atreyu/05 Place Manual Orders.html");
+    echo file_get_contents(DOCS_RESOURCES."/brokerages/place-manual-orders.html"");
 ?>
--- 01 Our Platform/03 Tutorials/06 Live Trading/01 Brokerages/05 FTX/06 Stop Execution.php
+++ 01 Our Platform/03 Tutorials/06 Live Trading/01 Brokerages/05 FTX/06 Stop Execution.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents(__DIR__."/../01 Atreyu/06 Stop Execution.html");
+    echo file_get_contents(DOCS_RESOURCES."/brokerages/stop-execution.html"");
 ?>
