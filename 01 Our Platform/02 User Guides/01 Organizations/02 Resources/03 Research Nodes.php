d7fee23443ff8973a6ad06fd8ceb2297c2873301
Jared <jaredbroad@gmail.com>
2022-01-21 11:08:31 -0800

    Update 03 Research Nodes.php

--- 01 Our Platform/02 User Guides/01 Organizations/02 Resources/03 Research Nodes.php
+++ 01 Our Platform/02 User Guides/01 Organizations/02 Resources/03 Research Nodes.php
@@ -1,5 +1,5 @@
 <p>You need research nodes to use the Research Environment. Several models of research nodes are available. More powerful research nodes allow you to handle more data and run faster computations in your notebooks. The following table shows the specifications of the research node models: </p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/research-nodes-table.html"); ?>
+<?php echo include(DOCS_RESOURCES."/research-nodes-table.html"); ?>
 
 <p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each research node model. You get one free R1-4 research node in your first organization, but the node is replaced when you subscribe to a new research node in the organization.</p>
