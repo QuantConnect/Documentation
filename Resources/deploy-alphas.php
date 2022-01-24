ef809bd623c58394c94bf7bf7f2174d42d27cd01
Derek Melchin <melchin.derek@gmail.com>
2022-01-24 15:51:51 -0700

    Mention Alpha Streams bundle in deploy Alphas block

--- Resources/deploy-alphas.php
+++ Resources/deploy-alphas.php
@@ -4,5 +4,6 @@
     <li>In the left navigation bar, click <span class='menu-name'>Strategies &gt; Subscriptions &amp; Bids</span>.</li>
     <li>On the Subscriptions &amp; Bids page, click <span class='button-name'>Deploy</span> next to the Alpha that you want to deploy.</li>
     <li>Follow the <a href="<?= DOCS_ROOT_URL ?>/our-platform/tutorials/live-trading/brokerages">live trading tutorial</a> of the brokerage that you want to use when deploying the Alpha.</li>
-    <p>If the capital in your brokerage account that you use to deploy the Alpha exceeds the allocation size granted by your Alpha subscription, the deployment will fail.</p>
+    <p>To enable <a href='/docs/v2/our-platform/user-guides/live-trading/notifications'>live trading notifications</a> on Alpha deployments, you need a subscription to the Alpha Analytics, S3, and API Bundle. Subscribe to the bundle on the <a href='/pricing'>Pricing</a> page.</p>
+    <p>If the capital in your brokerage account that you use to deploy the Alpha exceeds the allocation size granted by your Alpha subscription, the deployment fails.</p>
 </ol>
