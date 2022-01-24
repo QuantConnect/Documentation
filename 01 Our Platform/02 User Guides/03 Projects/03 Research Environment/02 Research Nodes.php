132782e999e1996adc5a4612b5fe50a6556272e6
Web Editor <web.editor@quantconnect.com>
2022-01-24 16:59:31 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/02 Research Nodes.php
+++ 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/02 Research Nodes.php
@@ -1,4 +1,3 @@
-
-<?php echo file_get_contents(__DIR__."/../../01 Organizations/02 Resources/03 Research Nodes.php"); ?>
+<?php include(__DIR__."/../../01 Organizations/02 Resources/03 Research Nodes.php"); ?>
 
 <!-- <?php include(DOCS_ROOT_URL."/01 Our Platform/02 User Guides/01 Organizations/02 Resources/03 Research Nodes.php"); ?> -->
