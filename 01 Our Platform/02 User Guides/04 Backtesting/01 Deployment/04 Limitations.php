1aab4b673351c0e07415d75d1a2031e53449f402
Web Editor <web.editor@quantconnect.com>
2022-01-25 12:30:11 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/04 Limitations.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/04 Limitations.php
@@ -1,24 +1,45 @@
-<p>There are limitations on the number of nodes your organization can have, the number of orders you complete in a backtest, and the amount of logs that you can produce. The limitations are eased as your organization grows to higher tiers.</p>
-
-<h4>Nodes</h4>
-<p>The number of backtesting nodes that you can have in your organization depends on the tier of your organization. The maximum number of backtesting nodes for each tier is the following:</p>
-<ul>
-    <li><span class='ul-option'>Free</span>: 1</li>
-    <li><span class='ul-option'>Quant Researcher</span>: 2</li>
-    <li><span class='ul-option'>Team</span>: 10</li>
-    <li><span class='ul-option'>Trading Firm</span>: Unlimited</li>
-    <li><span class='ul-option'>Institution</span>: Unlimited</li>
-</ul>
 
 <h4>Orders</h4>
 <p>The number of orders that you can place in a single backtest depends on the tier of your organization. The maximum number of orders per backtest for each tier is the following:<br></p>
-<ul>
-    <li><span class='ul-option'>Free</span>: 10K</li>
-    <li><span class='ul-option'>Quant Researcher</span>: 10M</li>
-    <li><span class='ul-option'>Team</span>: Unlimited</li>
-    <li><span class='ul-option'>Trading Firm</span>: Unlimited</li>
-    <li><span class='ul-option'>Institution</span>: Unlimited</li>
-</ul>
+
+<table class="qc-table table" id='order-quotas-table'>
+   <thead>
+      <tr>
+         <th style="width: 50%;">Tier</th>
+         <th style="width: 50%;">Orders Quota</th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td>Free</td>
+         <td>10K</td>
+      </tr>
+      <tr>
+         <td>Quant Researcher</td>
+         <td>10M</td>
+      </tr>
+      <tr>
+         <td>Team</td>
+         <td>Unlimited</td>
+      </tr>
+      <tr>
+         <td>Trading Firm</td>
+         <td>Unlimited</td>
+      </tr>
+      <tr>
+         <td>Institution</td>
+         <td>Unlimited</td>
+      </tr>
+   </tbody>
+</table>
+
+<style>
+#order-quotas-table td:last-child, 
+#order-quotas-table th:last-child {
+    text-align: right;
+}
+</style>
+
 
 <h4>Logs</h4>
 <p>The amount of logs that you can generate per backtest and per day depends on the tier of your organization. The following table describes the amount of logs that you can generate on each tier:<br></p>
