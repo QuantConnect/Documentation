4b158e5b1d700b4fc81107833adcd21c2bece155
Derek Melchin <melchin.derek@gmail.com>
2022-01-25 10:52:34 -0700

    Fix copy-paste errors

--- 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/05 Orders.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/05 Orders.php
@@ -1,3 +1,3 @@
-<p>The amount of logs that you can generate per backtest and per day depends on the tier of your organization. The following table describes the amount of logs that you can generate on each tier:<br></p>
+<p>The number of orders that you can place in a single backtest depends on the tier of your organization. The maximum number of orders per backtest for each tier is the following:<br></p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/log-limits.html"); ?>
+<?php include(DOCS_RESOURCES."/quotas/orders-table.html"); ?>
