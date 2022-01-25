0c79a818a911d3af54ea66a95f6bd854ee11d2b7
Web Editor <web.editor@quantconnect.com>
2022-01-25 13:43:07 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/07 Limitations.php
+++ 01 Our Platform/02 User Guides/03 Projects/03 Research Environment/07 Limitations.php
@@ -1,10 +1,4 @@
 <p>There are several limitations to note when working with the Research Environment.</p>
 
-<h4>Collaboration</h4>
-<p>It's not currently possible to simultaneously work on the same notebook with other members in your organization because the functionality is not supported by Jupyter. Jupyter doesn't recognize an edit as a change to the file until after you run the cell.</p>
-
-<h4>Universe Selection</h4>
-<p>Universe selection is not currently supported in the Research Environment. Subscribe to <a href="https://github.com/QuantConnect/Lean/issues/1614">GitHub Issue #1614</a> to track the feature progress.</p>
-
 <h4>File Size</h4>
 <p>Research notebooks are limited to 128KB to ensure a fast, successful, and smooth experience for uploading and downloading files. If your notebook files exceed the limit, you can't save the notebook. To avoid issues with file size errors, break up your notebook files into manageable sizes.</p>
