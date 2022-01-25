9092cf53a1106b71669cbd8f0c5f3a6c4a245200
Web Editor <web.editor@quantconnect.com>
2022-01-25 12:58:56 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/05 Orders.php
+++ 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/05 Orders.php
@@ -1,3 +1,3 @@
-<p>The number of orders that you can place in a single backtest depends on the tier of your organization. The following table shows the maximum number of orders per backtest for each tier:<br></p>
+<p>The number of orders you can place in a single backtest depends on the tier of your organization. The maximum number of orders per backtest for each tier is the following:<br></p>
 
 <?php include(DOCS_RESOURCES."/quotas/orders-table.html"); ?>
