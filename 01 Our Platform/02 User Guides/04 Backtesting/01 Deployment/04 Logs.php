c92a0e81ee8c217ae66812335d71d50d5c35c462
Derek Melchin <melchin.derek@gmail.com>
2022-01-25 10:49:30 -0700

    Restructure Backtesting Deployment page

---
+++ 01 Our Platform/02 User Guides/04 Backtesting/01 Deployment/04 Logs.php
@@ -0,0 +1,9 @@
+<p>The number of orders that you can place in a single backtest depends on the tier of your organization. The maximum number of orders per backtest for each tier is the following:<br></p>
+
+<?php include(DOCS_RESOURCES."/quotas/log-table.html"); ?>
+
+
+<h4>Logs</h4>
+<p>The amount of logs that you can generate per backtest and per day depends on the tier of your organization. The following table describes the amount of logs that you can generate on each tier:<br></p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/log-limits.html"); ?>
