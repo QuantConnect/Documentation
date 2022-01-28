e82dce205292ff6da3f7ebe525482e7da61178d5
Web Editor <web.editor@quantconnect.com>
2022-01-28 11:17:29 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/11 Community/01 Forum/01 Introduction.php
+++ 01 Our Platform/02 User Guides/11 Community/01 Forum/01 Introduction.php
@@ -1,3 +1,3 @@
-<?php include(DOCS_RESOURCES."/introduce-community.html"); ?>
+<?php include(DOCS_RESOURCES."/introduce-community.php"); ?>
 
 <p>The QuantConnect forum is a place to discuss with other community members, the core QuantConnect team, and our <a href="/docs/v2/our-platform/user-guides/community/integration-partners">Integration Partners</a>. Use the forum to spark interesting discussions, ask for assistance from other members, and voice your opinion on our products. You must complete 30% of the Bootcamp lessons in the <a href="/docs/v2/our-platform/user-guides/learning-center">Learning Center</a> to unlock the ability to post to the forum.</p>
