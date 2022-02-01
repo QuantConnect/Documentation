255ce0f97b17d59c1968272dcc7f17c1eb368088
Web Editor <web.editor@quantconnect.com>
2022-02-01 13:35:33 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/01 Introduction.php
@@ -1,3 +1,3 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 
-<p>Binance was founded by Changpeng Zhao in 2017 with the goal to "increase the freedom of money globally"<sup><a href="https://www.binance.com/en/about">1</a></sup>. Binance provides access to trading Crypto through spot markets and perpetual Futures. They serve clients with no minimum deposit when depositing Crypto. Binance also provides an NFT marketplace, a mining pool, and services to deposit Crypto coins in liquidity pools to earn rewards.</p>
+<p>Binance was founded by Changpeng Zhao in 2017 with the goal to "increase the freedom of money globally". Binance provides access to trading Crypto through spot markets and perpetual Futures. They serve clients with no minimum deposit when depositing Crypto. Binance also provides an NFT marketplace, a mining pool, and services to deposit Crypto coins in liquidity pools to earn rewards.</p>
