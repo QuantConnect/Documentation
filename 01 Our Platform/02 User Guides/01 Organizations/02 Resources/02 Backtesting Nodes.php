71f55b3732ded2c37d57790ab63bfb3fabc7ca26
Web Editor <web.editor@quantconnect.com>
2022-02-03 16:57:51 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/01 Organizations/02 Resources/02 Backtesting Nodes.php
+++ 01 Our Platform/02 User Guides/01 Organizations/02 Resources/02 Backtesting Nodes.php
@@ -1,3 +1,12 @@
+<?php
+
+function getSettlementText($brokerageName) {
+    echo "<p>Trades through $brokerageName are settled immediately after the transaction.</p>"; 
+}
+getSettlementText("Oanda");
+
+?>
+
 <p>Backtesting nodes enable you to run backtests. The more backtesting nodes your organization has, the more concurrent backtests that you can run. Several models of backtesting nodes are available. Backtesting nodes that are more powerful can run faster backtests and backtest nodes with more RAM can handle more memory-intensive operations like training machine learning models, processing Options data, and managing large universes. The following table shows the specifications of the backtesting node models:<br></p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/backtest-nodes-table.html"); ?>
