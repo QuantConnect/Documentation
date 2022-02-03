484f303809747e35df6d8fa44ad4be42478188cd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-03 16:10:33 -0700

    Fix typo

--- Resources/brokerages/slippage.php
+++ Resources/brokerages/slippage.php
@@ -1,11 +1,11 @@
 <?php
 
 function getSlippageText($brokerageName, $paperTradingSupported) {
-	$result = "<p>Orders through $brokerageName do not experience slippage in backtests. In ";
+	$result = "<p>Orders through $brokerageName do not experience slippage in backtests. In";
 	if ($paperTradingSupported) {
-		$result .= "paper trading and";
+		$result .= " paper trading and";
 	}
-	$result .= "live trading, your orders may experience slippage.";
+	$result .= " live trading, your orders may experience slippage.";
 	echo $result;
 }
 
