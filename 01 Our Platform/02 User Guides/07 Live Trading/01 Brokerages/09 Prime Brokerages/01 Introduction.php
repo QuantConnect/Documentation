2a82b1a483ff3b78abd40796dfbaaf896160560b
Web Editor <web.editor@quantconnect.com>
2022-02-04 13:55:47 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
@@ -2,4 +2,4 @@
 
 <img class='img-responsive' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg'>
 
-<p>Terminal Link is an add-on that unlocks access to the 1,300+ prime brokerages in the Bloomberg Execution Management System network. Terminal Link is only supported in live mode, so there is no backtesting brokerage model. However, you can use Terminal Link in paper trading to test your algorithm using the Bloomberg live data feeds.</p>
+<p>LEAN can connect to the Bloomberg™ Desktop API (DAPI) through the Terminal Link plug-in. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
