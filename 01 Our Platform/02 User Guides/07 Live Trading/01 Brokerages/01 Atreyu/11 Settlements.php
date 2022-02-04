ce0c5d26a14f34e93dd87b86be432ae1ab2ba37e
Derek Melchin <melchin.derek@gmail.com>
2022-02-04 09:52:08 -0700

    Add trade settlement resource

---
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/01 Atreyu/11 Settlements.php
@@ -0,0 +1,4 @@
+<?php
+include(DOCS_RESOURCES."/brokerages/settlements.php");
+$getSettlementText(true, true, true, true, false);
+?>
---
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/10 QuantConnect Paper Trading/10 Settlements.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/brokerages/settlements.php");
+$getSettlementText(true, true, true, true, false);
+?>
+
+<p>If you set the brokerage model to a model other than the <code>DefaultBrokerageModel</code>, the new brokerage model defines how trades settle.</p>
