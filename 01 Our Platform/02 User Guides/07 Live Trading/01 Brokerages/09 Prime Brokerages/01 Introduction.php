37a1ddcf01ff532c7385898697cbc55f43577778
Web Editor <web.editor@quantconnect.com>
2022-02-04 12:46:16 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
@@ -1,3 +1,5 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 
+<img class='img-responsive' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg'>
+
 <p>Terminal Link is an add-on that unlocks access to the 1,300+ prime brokerages in the Bloomberg Execution Management System network. Terminal Link is only supported in live mode, so there is no backtesting brokerage model. However, you can use Terminal Link in paper trading to test your algorithm using the Bloomberg live data feeds.</p>
