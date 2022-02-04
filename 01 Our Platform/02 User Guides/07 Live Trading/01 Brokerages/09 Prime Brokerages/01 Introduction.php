ce79d318221f4f2a3b9d4480439608c64e1c11a4
Web Editor <web.editor@quantconnect.com>
2022-02-04 13:58:45 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/09 Prime Brokerages/01 Introduction.php
@@ -2,4 +2,4 @@
 
 <img class='img-responsive' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg'>
 
-<p>LEAN can connect to the Bloomberg™ Desktop API (DAPI) through the Terminal Link plug-in. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
+<p>LEAN can connect to the Bloomberg™ Desktop API (DAPI) through our Terminal Link plug-in. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
