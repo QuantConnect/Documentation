1a9473d10372cde9e5875b7c0bc8c8dc28f5d48d
Web Editor <web.editor@quantconnect.com>
2022-02-07 13:02:22 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/11 Tradier/07 Margin.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/11 Tradier/07 Margin.php
@@ -1,7 +1,7 @@
 <p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
 
 <h4>Buying Power</h4>
-<p>Tradier's margin accounts allow up to 2x leverage for Equity trades.</p>
+<p>In the US, Tradier allows up to 2x leverage on Equity trades for margin accounts. In other countries, Tradier may offer different amounts of leverage. To figure out how much leverage you can access, check with your local legislation or contact a Tradier representative. We model the US version of Tradier leverage by default.</p>
 
 <?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
 
