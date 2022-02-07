11843d392b0477ea4bf80f554d1b278e6ed57c1e
Web Editor <web.editor@quantconnect.com>
2022-02-07 12:36:34 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/06 Interactive Brokers/08 Margin.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/06 Interactive Brokers/08 Margin.php
@@ -1,7 +1,7 @@
 <p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
 
 <h4>Buying Power</h4>
-<p>IB allows up to 2x leverage on Equity trades for margin accounts.</p>
+<p>In the US, IB allows up to 2x leverage on Equity trades for margin accounts. In other countries, IB may offer different amounts of leverage. To figure out how much leverage you can access, check with your local legislation or contact an IB representative.</p>
 
 <?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
 
