d5f21a7721b21d21485a9e8fef3976fa45f438bc
Derek Melchin <melchin.derek@gmail.com>
2022-02-08 11:08:57 -0700

    Add dataset misconception resources

---
+++ 01 Our Platform/02 User Guides/06 Datasets/04 Misconceptions/07 Live and Backtesting Differences.php
@@ -0,0 +1,1 @@
+<?php echo file_get_contents(DOCS_RESOURCES."/datasets/live-and-backtest-differences.html"); ?>
--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/04 Bar Building.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/04 Bar Building.php
@@ -1,5 +1,7 @@
 <p>Ticks are aggregated to build bars. If a tick is flagged as suspicious, it's not included it in the bar price, but it's included in the bar volume. In backtesting, ticks are collected into slices that span 1 millisecond before they are injected into your algorithm. In live trading, ticks are collected into slices that span up to 70 milliseconds before they are injected into your algorithm.</p>
 
 <h4>Discrepancies</h4>
+<?php echo file_get_contents(DOCS_RESOURCES."/datasets/live-and-backtest-differences.html"); ?>
 
 <h4>Opening and Closing Auctions</h4>
+<?php echo file_get_contents(DOCS_RESOURCES."/datasets/opening-and-closing-auctions.html"); ?>
