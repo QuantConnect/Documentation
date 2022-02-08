0f5e55f5961ec8d8f5bf9d8006ede144a43e391d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-08 16:55:55 -0700

    Fix typo

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/05 Update Frequency.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/05 Update Frequency.php
@@ -1,4 +1,4 @@
 <?php 
-include(DOCS_RESOURCES."/data-feeds/update-frequnecy.php"); 
+include(DOCS_RESOURCES."/data-feeds/update-frequency.php"); 
 $getUpdateFrequencyText('/docs/v2/our-platform/user-guides/live-trading/data-feeds/us-equities#04-Bar-Building');
 ?>
