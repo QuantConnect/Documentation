8fee1d2921fcdfb723c3898568b4030a2761ddf0
Gustavo Aviles <gaviles@users.noreply.github.com>
2022-02-07 18:27:46 -0800

    Fic php bug

--- Resources/brokerages/latency.php
+++ Resources/brokerages/latency.php
@@ -2,7 +2,7 @@
 $getBrokerageLatencyText = function($brokerageName, $brokerageFileName) {
    echo "<p>Brokerage latency is amount of time it takes for messages like orders to travel between your algorithm and the brokerage. The brokerage latency of $brokerageName is "
 
-   echo file_get_contents(DOCS_RESOURCES."/brokerages/latency-by-brokerage/$brokerageFileName.php")
+   echo file_get_contents(DOCS_RESOURCES."/brokerages/latency-by-brokerage/$brokerageFileName.php");
 
    echo ".</p>";
 }
