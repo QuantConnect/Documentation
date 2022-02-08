025daa4a1cdbfa411418d0e90d0ba72ed1e4f029
Derek Melchin <melchin.derek@gmail.com>
2022-02-08 16:37:51 -0700

    Fix typo

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/06 Data Delay.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/06 Data Delay.php
@@ -1,4 +1,4 @@
 <?php 
 include(DOCS_RESOURCES."/data-feeds/data-delay.php"); 
-$getUpdateFrequencyText('IB has a 400 millisecond order fill time.');
+$getDataFeedDataDelayText('IB has a 400 millisecond order fill time.');
 ?>
