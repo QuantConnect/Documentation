45cf05fafe811de9c77f8aeb615741920fc31dca
Web Editor <web.editor@quantconnect.com>
2022-02-08 17:41:22 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/04 Update Frequency.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/04 Update Frequency.php
@@ -1,5 +1,1 @@
-<p><a href="/docs/v2/our-platform/user-guides/datasets/vendors">Dataset Vendors</a> send us data points throughout the day. If your algorithm subscribes to alternative datasets, Lean checks the storage system for new data points at various frequencies, depending on the resolution of the dataset subscription. When Lean finds new data points, they are injected into your algorithm. The following table shows the frequency of which Lean checks the storage system:</p>
-
-<?php include(DOCS_RESOURCES."/live-dataset-polling-frequency-table.html"); ?>
-
-<p>For example, if we receive a new data point at 9:51am for a dataset for which your algorithm has a daily subscription, your algorithm will discover the new data point between 9:51am and 10:21am. The timestamps of alternative data can be in various timezones, including UTC, NY, and Chicago. <a href="/docs/v2/our-platform/tutorials/datasets/inspecting-datasets#02-View-Dataset-Listing">Check the dataset listing page</a> to see the timezone of the dataset that you're using.</p>
+<p>For most alternative data feeds, the data is updated on a daily basis. For intraday alternative data feeds, the data points are delivered streaming.</p>
