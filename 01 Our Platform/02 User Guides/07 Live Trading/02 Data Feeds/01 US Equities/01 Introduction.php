1b9fb6ee91a79d3a7417e18b17f80059e6e185fa
Web Editor <web.editor@quantconnect.com>
2022-02-09 18:43:42 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/01 Introduction.php
@@ -2,3 +2,5 @@
 include(DOCS_RESOURCES."/data-feeds/introductions.php"); 
 $getDataFeedIntroText("US Equities", "security trades, quotes, and corporate actions");
 ?>
+
+<p>The US Equity Security Master data feed provides a live stream of corporate actions. The US Fundamentals data feed provides daily updates on company fundamentals. The US Equities Short Availability data feed provides the number of shares that are available for short sellers to borrow.</p>
