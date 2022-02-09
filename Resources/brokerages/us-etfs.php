8ed664989cd88e3b94bd932af55d75d72ccdc8f1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 11:35:14 -0700

    Add paragraph tags

--- Resources/brokerages/us-etfs.php
+++ Resources/brokerages/us-etfs.php
@@ -1,7 +1,7 @@
 <?php
 
 $getUSETFText = function($brokerageName) {
-   echo "You may not be able to trade all assets with $brokerageName. For example, if you live in the EU, you can’t trade US ETFs. Check with your local regulators to know which assets you are allowed to trade. You may need to adjust settings in your brokerage account to live trade some assets.";
+   echo "<p>You may not be able to trade all assets with $brokerageName. For example, if you live in the EU, you can’t trade US ETFs. Check with your local regulators to know which assets you are allowed to trade. You may need to adjust settings in your brokerage account to live trade some assets.</p>";
 }
 
 ?>
