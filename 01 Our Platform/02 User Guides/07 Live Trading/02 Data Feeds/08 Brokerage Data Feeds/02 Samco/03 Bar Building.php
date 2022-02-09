af89d72cc9ea0919d0727311946b570f9097cd1c
Web Editor <web.editor@quantconnect.com>
2022-02-09 12:48:17 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/03 Bar Building.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/03 Bar Building.php
@@ -1,1 +1,1 @@
-<p></p>
+<p>The data feed consolidates prices and quotes across all of the Indian exchanges. For a complete list of exchange and securities, see the <a rel='nofollow' href='https://developers.stocknote.com/doc/ScripMaster.csv'>ScripMaster</a> file from the StockNote API documentation.</p>
