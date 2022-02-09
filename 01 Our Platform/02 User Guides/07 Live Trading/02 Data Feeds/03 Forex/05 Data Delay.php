eca3baa7956a8eb51c0ef305a4fd95dd087de134
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 12:13:51 -0700

    Add missing argument

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/03 Forex/05 Data Delay.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/03 Forex/05 Data Delay.php
@@ -1,4 +1,4 @@
 <?php 
 include(DOCS_RESOURCES."/data-feeds/data-delay.php"); 
-$getUpdateFrequencyText();
+$getUpdateFrequencyText("/docs/v2/our-platform/user-guides/live-trading/data-feeds/forex#03-Bar-Building");
 ?>
