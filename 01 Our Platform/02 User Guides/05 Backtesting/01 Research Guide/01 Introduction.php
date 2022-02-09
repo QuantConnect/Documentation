a8253ac8aee48e20cbe0cb642c9589e5b47e9e21
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-08 18:45:23 -0700

    Fix typo

--- 01 Our Platform/02 User Guides/05 Backtesting/01 Research Guide/01 Introduction.php
+++ 01 Our Platform/02 User Guides/05 Backtesting/01 Research Guide/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php
-	echo file_get_contents(DOCS_RESOURCES."/introduce-backtesting.html");
+    echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/backtesting.html");
 ?>
 
 <p>QuantConnect aims to teach and inspire our community to create high performing algorithmic trading strategies. We measure our success by the profits created by the community through their live trading. As such, we try to build the best quantitative research techniques possible into the product to encourage a robust research process.</p>
