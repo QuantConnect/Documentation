1e556279c8a7576d8d2e98f3bf977badc798d27e
Web Editor <web.editor@quantconnect.com>
2022-02-08 19:32:18 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/04 Update Frequency.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/04 Update Frequency.php
@@ -1,1 +1,1 @@
-<p>For most alternative data feeds, the data is updated on a daily basis. For intraday alternative data feeds, the data points are delivered streaming.</p>
+<p>The update frequency of alternative data feeds depends on the specific data feed you're using. We deliver the data at the update frequency of the vendor. For most alternative data feeds, the data is updated on a daily basis. For intraday alternative data feeds, the data points are delivered streaming.</p>
