7ffd97262b982c3eefdda4e08a6310b06e96927e
Web Editor <web.editor@quantconnect.com>
2022-02-08 20:12:36 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/06 Latency.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/06 Latency.php
@@ -1,1 +1,1 @@
-<p>Data feed latency is the amount of time it takes for a data point in the data feed to travel to your algorithm. The Samco data feed has a latency of <?php echo file_get_contents(DOCS_RESOURCES."/data-feeds-latency/samco.php"); ?>.</p>
+<p></p>
