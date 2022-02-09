edb0f977d0165b6c815ce441fb14c22eab6a4cde
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 16:44:45 -0700

    Remove corporate actions from price data feed

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/01 Introduction.php
@@ -1,6 +1,6 @@
 <?php 
 include(DOCS_RESOURCES."/data-feeds/introductions.php"); 
-$getDataFeedIntroText("US Equities", "security trades, quotes, and corporate actions");
+$getDataFeedIntroText("US Equities", "security trades and quotes");
 ?>
 
 <p>The US Equity Security Master data feed provides a live stream of corporate actions. The US Fundamentals data feed provides daily updates on company fundamentals. The US Equities Short Availability data feed provides the number of shares that are available for short sellers to borrow.</p>
