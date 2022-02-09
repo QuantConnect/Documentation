9ee52ecbdd26363e179510d29aad6a75a798af5f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 12:24:50 -0700

    Call correct fuction

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/04 CFD/05 Data Delay.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/04 CFD/05 Data Delay.php
@@ -1,4 +1,4 @@
 <?php 
 include(DOCS_RESOURCES."/data-feeds/data-delay.php"); 
-$getUpdateFrequencyText("/docs/v2/our-platform/user-guides/live-trading/data-feeds/cfd#03-Bar-Building");
+$getDataFeedDataDelayText();
 ?>
