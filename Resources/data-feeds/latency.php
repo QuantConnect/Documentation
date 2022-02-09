53cbd1a7627e15f64dc2c33e9183dea3fa0a8dcf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-08 20:27:59 -0700

    Simplify wording

--- Resources/data-feeds/latency.php
+++ Resources/data-feeds/latency.php
@@ -1,6 +1,6 @@
 <?php
 $getDataFeedLatencyText = function($dataFeedName, $dataFeedPathName) {
-    echo "<p>Data feed latency is the amount of time it takes for a data point in the data feed to travel to your algorithm. The $dataFeedName data feed has a latency of ";
+    echo "<p>Data feed latency is the time it takes for a data point to travel from the source to your algorithm. The $dataFeedName data feed has a latency of ";
     echo file_get_contents(DOCS_RESOURCES."/data-feeds/latency-by-data-feed/$dataFeedPathName.php");
     echo ".</p>";
 }
