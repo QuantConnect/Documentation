445cfabadcce66c43e53b5accc076b7e98816642
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-10 09:26:51 -0700

    Simplify wording

--- Resources/data-feeds/data-delay.php
+++ Resources/data-feeds/data-delay.php
@@ -5,6 +5,6 @@ $getDataFeedDataDelayText = function($prependText=null) {
 		echo $prependText;
 		echo " ";
 	}
-    echo "We run your live trading algorithms on co-located servers racked in Equinix, but QuantConnect is not designed for high-frequency trading. Co-location reduces several factors that can interfere with your algorithm, including downtime from internet outages, equipment repairs, and natural disasters.</p>";
+    echo "Live trading algorithms run on co-located servers racked in Equinix, but QuantConnect is not designed for high-frequency trading. Co-location reduces several factors that can interfere with your algorithm, including downtime from internet outages, equipment repairs, and natural disasters.</p>";
 }
 ?>
