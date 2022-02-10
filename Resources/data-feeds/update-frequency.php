2cd33c98154619abe9ce8b7b994f934df5023841
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-10 09:25:39 -0700

    Simplify wording

--- Resources/data-feeds/update-frequency.php
+++ Resources/data-feeds/update-frequency.php
@@ -8,7 +8,7 @@ $getUpdateFrequencyText = function($barBuildingPath, $brokerageName=null) {
         echo "receive data from $brokerageName</a>, we publish it";
     }    
     
-    echo " to the cloud storage system. Lean checks the storage system for new data points at various frequencies, depending on the resolution of your data subscription. When new data points are available, they are injected into your algorithm. The following table shows the frequency of which Lean checks the storage system:</p>";
+    echo " to the cloud storage system. Lean checks the storage system for new data points at various frequencies, depending on the resolution of your data subscription. When new data points are available, they are injected into your algorithm. The following table shows how often Lean checks the storage system:</p>";
     
     include(DOCS_RESOURCES."/live-dataset-polling-frequency-table.html");
 
