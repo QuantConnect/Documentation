e6bac3e9de0346dbcfb61ba2b17868b10b5c91c6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 18:22:29 -0700

    Add brokerage argument

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/04 Zerodha/04 Update Frequency.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/04 Zerodha/04 Update Frequency.php
@@ -1,4 +1,4 @@
 <?php 
 include(DOCS_RESOURCES."/data-feeds/update-frequency.php"); 
-$getUpdateFrequencyText('/docs/v2/our-platform/user-guides/live-trading/data-feeds/brokerage-data-feeds/zerodha#03-Bar-Building');
+$getUpdateFrequencyText('/docs/v2/our-platform/user-guides/live-trading/data-feeds/brokerage-data-feeds/zerodha#03-Bar-Building', 'Zerodha');
 ?>
