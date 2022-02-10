d9e91dd37e4de66be756cca61ce44caadf4960d7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 17:50:00 -0700

    Fix indexing bug

--- Resources/data-feeds/latency.php
+++ Resources/data-feeds/latency.php
@@ -3,7 +3,9 @@ $getDataFeedLatencyText = function($dataFeedPathByDataFeed) {
     echo "<p>Data feed latency is the time it takes for a data point to travel from the source to your algorithm. ";
     if (sizeof($dataFeedPathByDataFeed) <= 1) {
         echo "The $dataFeedName data feed has a latency of ";
-        echo file_get_contents(DOCS_RESOURCES."/data-feeds/latency-by-data-feed/$dataFeedPathName.php");
+        foreach($dataFeedPathByDataFeed as $dataFeedName => $dataFeedPathName) {
+            echo file_get_contents(DOCS_RESOURCES."/data-feeds/latency-by-data-feed/$dataFeedPathName.php");
+        }
         echo ".</p>";
     }
     else {
