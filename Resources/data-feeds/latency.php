ca316a18c667f86d60fd0a86b8be3f01da2a0318
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 17:41:53 -0700

    Replace h4's with a table

--- Resources/data-feeds/latency.php
+++ Resources/data-feeds/latency.php
@@ -1,18 +1,35 @@
 <?php
 $getDataFeedLatencyText = function($dataFeedPathByDataFeed) {
     echo "<p>Data feed latency is the time it takes for a data point to travel from the source to your algorithm. ";
-    if (sizeof($dataFeedPathByDataFeed) > 1) {
-        echo "</p>";
-    }
-
-    foreach($dataFeedPathByDataFeed as $dataFeedName => $dataFeedPathName) {
-        if (sizeof($dataFeedPathByDataFeed) > 1) {
-            echo "<h4>$dataFeedName</h4>";
-            echo "<p>";
-        }
+    if (sizeof($dataFeedPathByDataFeed) <= 1) {
         echo "The $dataFeedName data feed has a latency of ";
         echo file_get_contents(DOCS_RESOURCES."/data-feeds/latency-by-data-feed/$dataFeedPathName.php");
         echo ".</p>";
+    }
+    else {
+        echo "The following table shows the latency of each US Equity data feed:</p>";
+
+
+        echo "<table class='qc-table table'>";
+        echo     "<thead>";
+        echo         "<tr>";
+        echo             "<th style='width: 50%;'>Data Feed</th>";
+        echo             "<th>Latency</th>";
+        echo         "</tr>";
+        echo     "</thead>";
+        echo     "<tbody>";
+
+        foreach($dataFeedPathByDataFeed as $dataFeedName => $dataFeedPathName) {
+            echo     "<tr>";
+            echo         "<td>$dataFeedName</td>";
+            echo         "<td>";
+            echo             file_get_contents(DOCS_RESOURCES."/data-feeds/latency-by-data-feed/$dataFeedPathName.php");
+            echo         "</td>";
+            echo     "</tr>";
+        }
+
+        echo     "</tbody>";
+        echo "</table>";
     }
 }
 ?>
