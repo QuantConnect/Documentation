03bcd0b8cc6bb102ca5a010fefecf76976ef68f0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-09 18:20:31 -0700

    Add support for brokerage data feeds

--- Resources/data-feeds/update-frequency.php
+++ Resources/data-feeds/update-frequency.php
@@ -1,6 +1,14 @@
 <?php
-$getUpdateFrequencyText = function($barBuildingPath) {
-    echo "<p>As we <a href='$barBuildingPath'>build slices of ticks and bars</a>, we publish them to the cloud storage system. Lean checks the storage system for new data points at various frequencies, depending on the resolution of your data subscription. When new data points are available, they are injected into your algorithm. The following table shows the frequency of which Lean checks the storage system:</p>";
+$getUpdateFrequencyText = function($barBuildingPath, $brokerageName=null) {
+    echo "<p>As we <a href='$barBuildingPath'>";
+    if ($brokerageName == null) {
+        echo "build slices of ticks and bars</a>, we publish them"
+    }
+    else {
+        echo "receive data from $brokerageName</a>, we publish it";
+    }    
+    
+    echo " to the cloud storage system. Lean checks the storage system for new data points at various frequencies, depending on the resolution of your data subscription. When new data points are available, they are injected into your algorithm. The following table shows the frequency of which Lean checks the storage system:</p>";
     
     include(DOCS_RESOURCES."/live-dataset-polling-frequency-table.html");
 
