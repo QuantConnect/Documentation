86170bbe7ac1e6f9e8ce6bac77c899a14faa2e4f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-11 11:38:29 -0700

    Move suspicious ticks further down the page

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/03 Bar Building.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/01 US Equities/03 Bar Building.php
@@ -9,7 +9,7 @@
 <h4>Excluded Ticks</h4>
 <p>The bar building process can exclude ticks. If a tick is excluded, its volume is aggregated in the bar but its price is not aggregated in the bar. Ticks are excluded if any of the the following statements are true:</p>
 <ul>
-    <li>The tick is <a href='/docs/v2/our-platform/user-guides/live-trading/data-feeds/us-equities#03-Suspicious-Ticks'>suspicious</a>.</li>
+    <li>The tick is suspicious.</li>
     <li>The tick is from the FINRA exchange and meets our price and volume thresholds.</li>
 <p>FINRA is a government-authorized not-for-profit organization that oversees U.S. broker-dealers. The ticks that have FINRA as the exchange represent trades like internal crosses from brokers, dealers and dark pools. We can miss a complete day of aggregated trades in low volume securities because the flag filtering is quite strict.</p>
     <li>The tick is a trade, the tick has none of the included <code>TradeConditionFlags</code>, and the tick has at least one of the excluded <code>TradeConditionFlags</code>.</li>
