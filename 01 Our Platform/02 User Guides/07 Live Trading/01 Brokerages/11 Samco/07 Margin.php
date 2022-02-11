5c55b9111135dfc06a9cce2766ef951bbe9e9816
Web Editor <web.editor@quantconnect.com>
2022-02-11 12:50:29 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/11 Samco/07 Margin.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/11 Samco/07 Margin.php
@@ -1,6 +1,6 @@
 <p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
 
 <h4>Buying Power</h4>
-<p>Samco allows up to 5x leverage for margin accounts, but the amount of margin available depends on the Equity and product type. To check the amount of margin for each asset, see the <a href="https://www.samco.in/span/equity">Equities Margin Calculator</a> on the Samco website.</p> 
+<p>Samco allows up to 5x leverage for margin accounts, but the amount of margin available depends on the Equity and product type. To check the amount of margin available for each asset, see the <a rel='nofollow' target='_blank' href="https://www.samco.in/span/equity">Equities Margin Calculator</a> on the Samco website.</p> 
 
 <?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
