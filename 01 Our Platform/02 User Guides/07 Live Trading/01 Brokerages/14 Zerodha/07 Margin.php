6185ee3bdf112a0ec6685ba6224222f5f4dbd379
Web Editor <web.editor@quantconnect.com>
2022-02-11 12:51:28 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/14 Zerodha/07 Margin.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/14 Zerodha/07 Margin.php
@@ -1,5 +1,5 @@
 <p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
 
 <h4>Buying Power</h4>
-<p>Zerodha allows maximum 5x leverage for margin trades, but the amount of margin available depends on the Equity and product type. To check the amount of margin available for each asset, see the <a rel='nofollow' target='_blank' href="https://zerodha.com/margin-calculator/Equity/">Margin Calculator</a> on the Zerodha website.</p> 
+<p>Zerodha allows up to 5x leverage for margin accounts, but the amount of margin available depends on the Equity and product type. To check the amount of margin available for each asset, see the <a rel='nofollow' target='_blank' href="https://zerodha.com/margin-calculator/Equity/">Margin Calculator</a> on the Zerodha website.</p> 
 <?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
