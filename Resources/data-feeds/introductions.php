58bced7eeee585dc415644ed7c26a3f10082d214
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-11 12:44:07 -0700

    Remove redundant sentences

--- Resources/data-feeds/introductions.php
+++ Resources/data-feeds/introductions.php
@@ -1,5 +1,5 @@
 <?php
 $getDataFeedIntroText = function($dataFeedName, $streamOfText) {
-    echo "<p>The $dataFeedName data feed is a stream of $streamOfText delivered to your trading algorithm during live execution. Live data feeds enable you to make real-time trades and update the value of the securities in your portfolio. If you trade $dataFeedName, your position values will usually be read from your brokerage account upon deployment, but your position values can only be updated as the algorithm progresses if you have the $dataFeedName data feed.</p>";
+    echo "<p>The $dataFeedName data feed is a stream of $streamOfText delivered to your trading algorithm during live execution. Live data feeds enable you to make real-time trades and update the value of the securities in your portfolio.</p>";
 }
 ?>
