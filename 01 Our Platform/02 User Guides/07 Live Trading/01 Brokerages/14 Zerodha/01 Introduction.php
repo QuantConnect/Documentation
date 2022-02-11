03bcd4c6ab2e0cfd4c4b539931ffe424e3916707
Louis Szeto <hke0073@icloud.com>
2022-02-11 21:52:29 +0800

    Add Zerodha Brokerage to Our Platform/User Guides/Live Trading/Brokerages

---
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/14 Zerodha/01 Introduction.php
@@ -0,0 +1,3 @@
+<?php include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
+
+<p>Zerodha was founded by Nithin Kamath on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. They have over 8 million users contributing over 15% of all Indian retail trading volumes.</p>
---
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/14 Zerodha/07 Margin.php
@@ -0,0 +1,5 @@
+<p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
+
+<h4>Buying Power</h4>
+<p>Zerodha allows maximum 5x leverage for margin trades, we model the margin leverage with this. However, the margin requirements are various among different equity and product type. Please refer to <a href="https://zerodha.com/margin-calculator/Equity/">Margin Calculator</a> page of Zerodha for details.</p> 
+<?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
