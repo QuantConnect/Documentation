dc29d6f1ecf5e3a29f033dd0c3907306f41cca86
Web Editor <web.editor@quantconnect.com>
2022-02-12 01:31:08 +0000

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/06 Interactive Brokers/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/06 Interactive Brokers/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php 
 include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); 
-echo file_get_contents(DOCS_RESOURCES."introduction-by-brokerage/interactive-brokers.php");
+echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.php");
 ?>
 
