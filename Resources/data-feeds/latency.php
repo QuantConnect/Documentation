30f9b7c1fc7e559a6e99aec574482748651afd6a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-11 17:23:11 -0700

    Add HFT text

--- Resources/data-feeds/latency.php
+++ Resources/data-feeds/latency.php
@@ -1,5 +1,5 @@
 <?php
 $getDataFeedLatencyText = function($dataFeedName) {
-    echo "<p>Live data takes time to travel from the source to your algorithm. The $dataFeedName data feed has a latency of 20-50 milliseconds.</p>";
+    echo "<p>Live data takes time to travel from the source to your algorithm. The $dataFeedName data feed has a latency of 20-50 milliseconds. QuantConnect is not designed for high-frequency trading.</p>";
 }
 ?>
