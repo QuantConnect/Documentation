0bd32b05cab426fa5213650433ee49f2b9918ed8
Web Editor <web.editor@quantconnect.com>
2022-02-12 01:11:35 +0000

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/03 Delivery.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/07 Alternative Data/03 Delivery.php
@@ -1,5 +1,4 @@
-<p>The update frequency of alternative data feeds depends on the specific data feed you're using. We deliver the data at the update frequency of the vendor. For most alternative data feeds, the data is updated on a daily basis. For intraday alternative data feeds, the data points are delivered streaming.</p>
-
+<p>The delivery schedule of alternative data feeds depends on the specific data feed you're using. We inject the data into your algorithms when the vendor provides the data. For most alternative data feeds, the data is updated on a daily or hourly basis. Some data feeds, like the <a href='/datasets/tiingo-news-feed'>Tiingo News Feed</a> or <a href='/datasets/benzinga-news-feed'>Benzinga News Feed</a>, include a live stream. In these cases, we deliver the data as a live stream to your algorithm.</p>
 
 <?php 
 include(DOCS_RESOURCES."/data-feeds/data-delay.php"); 
