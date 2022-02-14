aa84481d8c4f9cc90f1c28c3d9ebbc1ba124b8ba
Web Editor <web.editor@quantconnect.com>
2022-02-14 18:42:09 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/09 Community/01 Introduction.php
+++ 01 Our Platform/01 Getting Started/09 Community/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php
-    echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/community.html");
+    echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/community.php");
 ?>
 
 <p>This page explains how to post forum comments, give rewards, and follow discussions.</p>
