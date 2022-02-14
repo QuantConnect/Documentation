554f9b9ad6e6a7519735540eb08605745464120f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-14 10:48:12 -0700

    Add alternative data feed page

---
+++ Resources/data-feeds/alternative-data.php
@@ -0,0 +1,9 @@
+<?php
+$getAltDataText = function($brokerageName, $hybridSupported) {
+    echo "<p>Brokerage data feeds support most alternative data feeds, except feeds that stream real-time intraday data. Streaming data feeds, like the <a href='/datasets/tiingo-news-feed'>Tiingo News Feed</a> and <a href='/datasets/benzinga-news-feed'>Benzinga News Feed</a>, require the QuantConnect data feed. ";
+    if ($$hybridSupported) {
+        echo "The hybrid QuantConnect-$brokerageName data feed supports streaming data feeds."; 
+    }
+    echo "</p>";
+}
+?>
