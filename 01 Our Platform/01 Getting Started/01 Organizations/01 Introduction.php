4f7632b535116bb232edea3117a6040b17643c7b
Web Editor <web.editor@quantconnect.com>
2022-02-14 18:27:47 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/01 Organizations/01 Introduction.php
+++ 01 Our Platform/01 Getting Started/01 Organizations/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php
-	echo file_get_contents(DOCS_RESOURCES."/introduce-organizations.html");
+	echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/organizations.html");
 ?>
 <p>This page explains how to create new organizations, edit organization profiles, and set a preferred organization.</p>
