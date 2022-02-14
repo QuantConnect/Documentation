069d9c647062b960e2c6637ea4d0b0b7574d557d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-14 16:39:28 -0700

    Add missing space

--- Resources/landing-page-introductions/live-trading.php
+++ Resources/landing-page-introductions/live-trading.php
@@ -3,5 +3,5 @@
     echo file_get_contents(DOCS_RESOURCES."/kpis/live-algorithms-count.php");
     echo " live algorithms and have had more than ";
     echo file_get_contents(DOCS_RESOURCES."/kpis/volume-traded.php");
-    echo "in volume traded on our servers since 2015. The algorithms that our members create are run on co-located servers and the trading infrastructure is maintained at all times by our team of engineers. It's common for members to achieve 6-months of uptime with no interruptions.</p>";
+    echo " in volume traded on our servers since 2015. The algorithms that our members create are run on co-located servers and the trading infrastructure is maintained at all times by our team of engineers. It's common for members to achieve 6-months of uptime with no interruptions.</p>";
 ?>
