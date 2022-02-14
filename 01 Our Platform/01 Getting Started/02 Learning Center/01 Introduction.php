9a05d37b968fa022b423b39f001ed15acf5b1d6a
Web Editor <web.editor@quantconnect.com>
2022-02-14 18:28:28 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/02 Learning Center/01 Introduction.php
+++ 01 Our Platform/01 Getting Started/02 Learning Center/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php
-	echo file_get_contents(DOCS_RESOURCES."/introduce-learning-center.html");
+    echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/learning-center.html");
 ?>
 <p>This page explains how to enroll in Learning Center courses and launch the course IDE to complete courses.</p>
