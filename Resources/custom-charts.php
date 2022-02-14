f4ecebdb4445ec865a8f0f62acdb1b46b6b25ae3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-14 12:25:58 -0700

    Fix text alignment

--- Resources/custom-charts.php
+++ Resources/custom-charts.php
@@ -3,12 +3,13 @@ $getCustomChartText = function($backtest) {
 
     echo "<p>Create custom charts and they will display on the results page of your algorithm. We support the following types of plots:</p>";
     echo "<div class='section-example-container'>";
-    echo "<pre class='all prettyprint prettyprinted'><span class='pln'>   </span><span class='typ'>SeriesType</span><span class='pun'>.</span><span class='typ'>Line</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Scatter</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Candle</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Bar</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Flag</span></pre>";
-    echo "</div>";
+    echo "<pre class='all prettyprint prettyprinted'>";
+    echo     "<span class='typ'>SeriesType</span><span class='pun'>.</span><span class='typ'>Line</span><br>";
+    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Scatter</span><br>";
+    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Candle</span><br>";
+    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Bar</span><br>";
+    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Flag</span>";
+    echo "</pre></div>";
 
     if ($backtest) {
         echo "<p>To create other types of charts, save the plot data in the ObjectStore and then load it into the Research Environment. In the Research Environment, you can <a href='/docs/v2/research-environment/tutorials/plotting-in-research'>create other types of charts with third-party charting packages</a>.</p>";
@@ -16,12 +17,13 @@ $getCustomChartText = function($backtest) {
 
     echo "<p>When you create scatter plots, you can set a marker symbol. We support the following marker symbols:</p>";
     echo "<div class='section-example-container'>";
-    echo "<pre class='all prettyprint prettyprinted'><span class='pln'>   </span><span class='typ'>ScatterMarkerSymbol</span><span class='pun'>.</span><span class='typ'>Circle</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Diamond</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Square</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>Triangle</span><br><span class='pln'>";
-    echo "             </span><span class='pun'>.</span><span class='typ'>TriangleDown</span></pre>";
-    echo "</div>";
+    echo "<pre class='all prettyprint prettyprinted'>";
+    echo     "<span class='typ'>ScatterMarkerSymbol</span><span class='pun'>.</span><span class='typ'>Circle</span><br>";
+    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Diamond</span><br>";
+    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Square</span><br>";
+    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Triangle</span><br>";
+    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>TriangleDown</span>";
+    echo "</pre></div>";
 
     if ($backtest) {
         echo "<p>Custom charts are limited to 4,000 data points. Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. If you exceed the limit, the Console displays the following message:</p>";
