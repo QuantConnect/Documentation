89e8556657aee17e1209b2a85794b6900b562c57
Web Editor <web.editor@quantconnect.com>
2022-02-14 12:52:26 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/05 Alternative Data.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/02 Samco/05 Alternative Data.php
@@ -1,1 +1,4 @@
-<p>Brokerage data feeds support most alternative data feeds, except feeds that stream real-time intraday data. Streaming data feeds, like the <a href='/datasets/tiingo-news-feed'>Tiingo News Feed</a> and <a href='/datasets/benzinga-news-feed'>Benzinga News Feed</a>, require the QuantConnect data feed.</p>
+<?php
+include(DOCS_RESOURCES."/data-feeds/alternative-data.php");
+$getAltDataText("Samco", false);
+?>
