b05f02b7242c55d82d079aa07dddb0c46ff07761
Web Editor <web.editor@quantconnect.com>
2022-02-14 18:29:26 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/01 Getting Started/05 Datasets/01 Introduction.php
+++ 01 Our Platform/01 Getting Started/05 Datasets/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php
-	echo file_get_contents(DOCS_RESOURCES."/introduce-datasets.html");
+    echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/datasets.html");
 ?>
 
 <p>This page explains how to view datasets, inspect their properties, and add cloud access.</p>
