8098ab839923d0d0bf556e076c419814817c16fa
Web Editor <web.editor@quantconnect.com>
2022-02-15 17:15:43 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/10 Alpha Streams Market/01 Alpha Life Cycle/01 Introduction.php
+++ 01 Our Platform/02 User Guides/10 Alpha Streams Market/01 Alpha Life Cycle/01 Introduction.php
@@ -2,4 +2,4 @@
 	echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/alpha-streams-market.html");
 ?>
 
-<p>An Alpha is the core signal creation module of an algorithm. The Alpha predicts the expected return in the coming moments of time. Alpha's predict the direction of the price curve and, ideally, the magnitude of the movement. The member who researches and develops the algorithm is the Alpha Author and the investor or fund that licenses the algorithm is the Licensee. The Alpha life cycle is the process that an Alpha undertakes to go from discovery to live trading to delisting.</p>
+<p>An Alpha is the core signal creation module of an algorithm. The Alpha predicts the expected return in the coming moments of time. Alpha's predict the direction of the price curve and, ideally, the magnitude of the movement.</p>
