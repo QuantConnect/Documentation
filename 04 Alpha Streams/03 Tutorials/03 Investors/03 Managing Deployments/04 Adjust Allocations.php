f0adc160eda2b1469e4c04bf825f88c513dc614a
Web Editor <web.editor@quantconnect.com>
2022-02-15 19:36:40 -0500

    Automated push from server made by Derek Melchin

--- 04 Alpha Streams/03 Tutorials/03 Investors/03 Managing Deployments/04 Adjust Allocations.php
+++ 04 Alpha Streams/03 Tutorials/03 Investors/03 Managing Deployments/04 Adjust Allocations.php
@@ -2,7 +2,7 @@
 <ol>
     <li>Stop the algorithm.</li>
     <p>For instructions on stopping the algorithm, refer to <a href="/docs/v2/our-platform/tutorials/live-trading/brokerages">the tutorial of your brokerage</a>.</p>
-    <li>If you want to increase your allocation to the Alpha passed the allocation size that your subscription allows or you want to reduce your allocation to the Alpha passed your subscription period, <a href="/docs/v2/our-platform/tutorials/alpha-streams-market/licensing-alphas#05-Manage-Existing-Bids">update your bid</a> with the new allocation size.</li>
+    <li>If you want to increase your allocation to the Alpha passed the allocation size that your subscription allows or you want to reduce your allocation to the Alpha passed your subscription period, <a href="/docs/v2/alpha-streams/tutorials/investors/licensing-alphas#05-Edit-Bids">update your bid</a> with the new allocation size.</li>
     <li>Adjust the amount of capital in your brokerage account to the amount of capital that you want to allocate to the Alpha.</li>
     <li>Redeploy the Alpha.</li>
 </ol>
