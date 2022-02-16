a990e5e232d7c85b7f2a9d5e64e1ba0e251ed13e
Web Editor <web.editor@quantconnect.com>
2022-02-15 19:51:47 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/10 Community/01 Code of Conduct/01 Introduction.php
+++ 01 Our Platform/02 User Guides/10 Community/01 Code of Conduct/01 Introduction.php
@@ -1,1 +1,5 @@
-<p></p>
+<?php include(DOCS_RESOURCES."/landing-page-introductions/community.php"); ?>
+
+<p>Our community forum was created to be a hub for sharing quality quantitative science insights, discussions on quantitative philosophies, and solving problems. Our goal is to embrace new ways of thinking while keeping a friendly, welcoming environment where people feel comfortable amongst their peers. Our community's exceptional and diverse range of opinions and experiences make us a unique platform. Coming together with mutual respect and courtesy can unite us in growing together as quants.</p>
+
+<p>We ask that our users adhere to the community code of conduct to ensure QuantConnect remains a safe, healthy environment for high quality quantitative trading discussions</p>
