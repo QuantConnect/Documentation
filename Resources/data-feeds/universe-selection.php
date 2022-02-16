dff8b3917a3ef9a2a69ade87292ec4057fd24b85
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-02-16 09:19:39 -0700

    Reduce duplicate code

--- Resources/data-feeds/universe-selection.php
+++ Resources/data-feeds/universe-selection.php
@@ -1,17 +1,16 @@
 <?php
 $getUniverseSelectionText = function($dataFeedName, $availability=true) {
+    echo "<p>Universe selection is";
+    if (!$availability) {
+        echo "n't";
+    }
+    echo " available with the $dataFeedName data feed.</p>";
     if ($availability)
     {
-        echo "<p>Universe selection is compatible with the $dataFeedName data feed.</p>
-        
-<div class='section-example-container'>
-    <pre class='csharp'>AddUniverse(CoarseUniverseSelection, FineUniverseSelection);</pre>
-    <pre class='python'>self.AddUniverse(self.CoarseUniverseSelection, self.FineUniverseSelection)</pre>
-</div>";
-    }
-    else
-    {
-        echo "<p>Universe selection isn't available with the $dataFeedName data feed.";
+        echo "<div class='section-example-container'>
+                  <pre class='csharp'>AddUniverse(CoarseUniverseSelection, FineUniverseSelection);</pre>
+                  <pre class='python'>self.AddUniverse(self.CoarseUniverseSelection, self.FineUniverseSelection)</pre>
+              </div>";
     }
 }
 ?>
