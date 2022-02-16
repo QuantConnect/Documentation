45b801ebfa9cbb4d40c2debce2458a7ccae4ebf2
Web Editor <web.editor@quantconnect.com>
2022-02-16 12:49:00 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/03 Tutorials/05 Datasets/04 Contributing Datasets/09 Update Documentation.php
+++ 01 Our Platform/03 Tutorials/05 Datasets/04 Contributing Datasets/09 Update Documentation.php
@@ -1,1 +1,9 @@
+<p>You must <a href='/docs/v2/our-platform/tutorials/datasets/contributing-datasets#08-Create-Dataset-Listing'>create your dataset listing</a> before you update the documentation.</p>
 
+<p>Make a pull request to the <a href='https://github.com/QuantConnect/Documentation'>Documentation repository</a> with the following changes to add your dataset to the documentation:</p>
+<ol>
+    <li>Update the <span class='page-section-name'>Asset Classes</span> and <span class='page-section-name'>Assets Available</span> sections of the <a href='/docs/v2/our-platform/user-guides/live-trading/brokerages'>brokerage user guides</a>.</li>
+    <li>Update the <span class='page-section-name'>Sourcing</span> section of the <a href='/docs/v2/our-platform/user-guides/live-trading/data-feeds'>data feed user guides</a>.</li>
+</ol>
+
+<p>After we merge your dataset into production, we will update the <a href='https://www.quantconnect.com/docs/v2/our-platform/getting-started/quantconnect#04-Data-Library'>Data Library image</a> if this is your organization's first dataset contribution and we will update the <a href='https://www.quantconnect.com/docs/v2/our-platform/user-guides/datasets/categories'>Categories</a> documentation if your dataset falls under a new category.</p>
