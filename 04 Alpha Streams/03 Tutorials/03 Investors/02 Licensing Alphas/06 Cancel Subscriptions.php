def9c338dae44864620e4e49d030e216b9660c01
Web Editor <web.editor@quantconnect.com>
2022-02-15 19:35:40 -0500

    Automated push from server made by Derek Melchin

--- 04 Alpha Streams/03 Tutorials/03 Investors/02 Licensing Alphas/06 Cancel Subscriptions.php
+++ 04 Alpha Streams/03 Tutorials/03 Investors/02 Licensing Alphas/06 Cancel Subscriptions.php
@@ -1,2 +1,2 @@
-<p>You can't cancel an Alpha subscription, but you may <a href="/docs/v2/our-platform/tutorials/alpha-streams-market/licensing-alphas#05-Manage-Existing-Bids">cancel the auto-renewing bid</a> that you have for the Alpha. If you have strong reasons to request a cancellation and refund, <a href="/contact">contact us</a> with those reasons for evaluation.</p>
+<p>You can't cancel an Alpha subscription, but you may <a href="/docs/v2/alpha-streams/tutorials/investors/licensing-alphas#05-Edit-Bids">cancel the auto-renewing bid</a> that you have for the Alpha. If you have strong reasons to request a cancellation and refund, <a href="/contact">contact us</a> with those reasons for evaluation.</p>
 
