fa12a5d744723adb014b4d6974814b49033142cd
Web Editor <web.editor@quantconnect.com>
2022-02-16 19:05:03 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/06 Results/02 Runtime Statistics.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/06 Results/02 Runtime Statistics.php
@@ -4,49 +4,52 @@
 
 <p>The following table shows the performance statistics that display by default:</p>
 
+<?php
+include(DOCS_RESOURCES."/glossary.php");
+echo "
+<table class='qc-table table'>
+  <thead>
+    <tr>
+      <th style='width: 25%'>Statistic</th>
+      <th style='width: 75%'>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>PSR</td>
+      <td>{$defintionByTerm['probabilistic sharpe ratio (PSR)']}</td>
+    </tr>
+    <tr>
+      <td>Unrealized</td>
+      <td>{$defintionByTerm['unrealized']}</td>
+    </tr>
+    <tr>
+      <td>Fees</td>
+      <td>{$defintionByTerm['total fees']}</td>
+    </tr>
+    <tr>
+      <td>Net Profit</td>
+      <td>{$defintionByTerm['net profit']['dollar-value']}</td>
+    </tr>
+    <tr>
+      <td>Return</td>
+      <td>{$defintionByTerm['net profit']['percent']}</td>
+    </tr>
+    <tr>
+      <td>Equity</td>
+      <td>Total portfolio value if you sold all holdings at current market rates</td>
+    </tr>
+    <tr>
+      <td>Holdings</td>
+      <td>{$defintionByTerm['holdings']}</td>
+    </tr>
+    <tr>
+      <td>Volume</td>
+      <td>{$defintionByTerm['volume']}</td>
+    </tr>
+  </tbody>
+</table>";
 
-<table class="table qc-table">
-    <thead>
-        <tr>
-            <th style="width: 25%">Statistic</th>
-            <th style="width: 75%">Description</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><a href='/docs/v2/our-platform/user-guides/optimization/objectives#05-PSR'>PSR</a></td>
-            <td>Probabilistic Sharpe Ratio</td>
-        </tr>
-        <tr>
-            <td>Unrealized</td>
-            <td>Unrealized profit</td>
-        </tr>
-        <tr>
-            <td>Fees</td>
-            <td>Total fees paid during the live deployment across all securities in the portfolio</td>
-        </tr>
-        <tr>
-            <td>Net Profit</td>
-            <td>Sum of all gross profit across all securities in the portfolio</td>
-        </tr>
-        <tr>
-            <td>Return</td>
-            <td>Return = (current equity - starting equity) / starting equity</td>
-        </tr>
-        <tr>
-            <td>Equity</td>
-            <td>Total portfolio value if you sold all holdings at current market rates</td>
-        </tr>
-        <tr>
-            <td>Holdings</td>
-            <td>Absolute sum of the items in the portfolio</td>
-        </tr>
-        <tr>
-            <td>Volume</td>
-            <td>Total sale volume since the start of the live deployment</td>
-        </tr>
-    </tbody>
-</table>
 
-
-<?php echo file_get_contents(DOCS_RESOURCES."/create-custom-runtime-statistic.html"); ?>
+echo file_get_contents(DOCS_RESOURCES."/create-custom-runtime-statistic.html"); 
+?>
