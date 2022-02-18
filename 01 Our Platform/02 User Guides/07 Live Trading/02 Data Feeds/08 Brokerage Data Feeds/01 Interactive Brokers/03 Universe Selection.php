6de207fc7cc8c1f69b1e2eacb70958475acf9325
Web Editor <web.editor@quantconnect.com>
2022-02-18 18:34:43 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/03 Universe Selection.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/03 Universe Selection.php
@@ -2,3 +2,10 @@
 include(DOCS_RESOURCES."/data-feeds/universe-selection.php");
 $getUniverseSelectionText("IB", true);
 ?>
+
+<p>The IB data feed can stream data for up to 100 assets by default, but IB may enable you to stream more than 100 assets based on your commissions and equity value. For more information about data feed limits from IB, see the <a rel='nofollow' target='_blank' href='https://www.interactivebrokers.com/en/pricing/research-news-marketdata.php'>Market Data Pricing Overview</a> page on the IB website. If IB enables you to stream more than 100 assets, set the <code>DataSubscriptionLimit</code> of your algorithm to the limit from IB.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">Settings.DataSubscriptionLimit = 150;</pre>
+    <pre class="python">self.Settings.DataSubscriptionLimit = 150</pre>
+</div>
