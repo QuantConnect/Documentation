7a2720e5088481207d20d3d02fcc40e7e67f6936
Web Editor <web.editor@quantconnect.com>
2022-02-21 13:09:06 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/01 Introduction.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.php"); ?>
 
-<p>The Interactive Brokers (IB) data feed streams live asset prices from IB.</p>
+<p>The Interactive Brokers (IB) data feed streams live asset prices from IB. If you use the IB data feed and request historical data, the historical data comes from IB.</p>
