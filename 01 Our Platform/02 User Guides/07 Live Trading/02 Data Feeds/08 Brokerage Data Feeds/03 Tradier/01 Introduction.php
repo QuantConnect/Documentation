af1a8179c5ff5a5509045069df13dd8552bc6eb0
Web Editor <web.editor@quantconnect.com>
2022-02-21 13:11:27 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/03 Tradier/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/03 Tradier/01 Introduction.php
@@ -2,4 +2,4 @@
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/tradier.php");
 ?>
 
-<p>The Tradier data feeds are streams of Equity and Option prices directly from Tradier. If you use the Tradier data feed and request historical data, the historical data comes from Tradier.</p>
+<p>The Tradier data feeds are streams of Equity and Option prices directly from Tradier. If you use this data feed and request historical data, the historical data comes from Tradier.</p>
