6619700caa7af4ec09abe7dacb570c7f5e69f4ac
Web Editor <web.editor@quantconnect.com>
2022-02-21 13:10:15 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/04 Zerodha/01 Introduction.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/02 Data Feeds/08 Brokerage Data Feeds/04 Zerodha/01 Introduction.php
@@ -2,4 +2,4 @@
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/zerodha.php");
 ?>
 
-<p>The Zerodha data feed streams live asset prices from Zerodha.</p>
+<p>The Zerodha data feed streams live asset prices from Zerodha. If you use the Zerodha data feed and request historical data, the historical data comes from Zerodha.</p>
