7d291a61384e78db551574fef4fe81716f0c6535
Web Editor <web.editor@quantconnect.com>
2022-02-23 14:30:39 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/07 Margin.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/07 Margin.php
@@ -1,7 +1,7 @@
 <p>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</p>
 
 <h4>Buying Power</h4>
-<p>Binance allows up to 3x leverage for margin accounts.</p>
+<p>Binance allows up to 3x leverage for margin accounts, but Binance US doesn't currently support margin accounts.</p>
 
 <?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
 
