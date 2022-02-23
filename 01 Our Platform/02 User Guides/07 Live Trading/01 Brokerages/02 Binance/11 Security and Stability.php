8c001c84de42fbfcca08eafca24260bd0f73b224
Web Editor <web.editor@quantconnect.com>
2022-02-23 14:31:53 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/11 Security and Stability.php
+++ 01 Our Platform/02 User Guides/07 Live Trading/01 Brokerages/02 Binance/11 Security and Stability.php
@@ -1,4 +1,4 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/security-and-stability.php");
-$getBrokerageStabilityText("Binance", "/docs/v2/our-platform/tutorials/live-trading/brokerages/binance#04-Deploy-Live-Algorithms");
+$getBrokerageStabilityText("Binance or Binance US", "/docs/v2/our-platform/tutorials/live-trading/brokerages/binance#04-Deploy-Live-Algorithms");
 ?>
