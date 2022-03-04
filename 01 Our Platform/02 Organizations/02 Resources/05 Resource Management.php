29901c30cf8af4075c355f82f6e9d9c64c95aec3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-04 09:18:16 -0700

    Delete duplicate content

--- 01 Our Platform/02 Organizations/02 Resources/05 Resource Management.php
+++
@@ -1,1 +0,0 @@
-<p>We assign a default name to hardware nodes that includes the model name and an arbitrary string of characters, but you can rename the nodes in your organization. You can also stop running nodes and select specific nodes to use when you run backtests, launch research notebooks, or deploy algorithms to live trading.</p>
