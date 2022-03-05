98b5f1d62f11f76a4f4e9980b160eb2c3a42238f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-04 18:19:11 -0700

    update to vscode

--- Resources/tutorials/projects/rename-projects.php
+++ Resources/tutorials/projects/rename-projects.php
@@ -2,8 +2,7 @@
 
 <ol>
     <li><a href="/docs/v2/our-platform/tutorials/projects/managing-projects#05-Open-Existing-Projects">Open the project</a> that you want to rename.</li>
-    <li>In the project panel, hover over the project name and then click the <span class='icon-name'>pencil</span> icon that appears.</li>
-    <img class='docs-image' style='max-height: 120px' src="https://cdn.quantconnect.com/i/tu/rename-project.jpg">
-    <li>Enter the new project name and then click <span class='button-name'>OK</span>.</li>
-    <p>"Project renamed successfully" displays.</p>
+    <li>In the Project panel, hover over the project name and then click the <span class='icon-name'>pencil</span> icon that appears.</li>
+    <img class='docs-image' style='max-height: 120px' src="https://cdn.quantconnect.com/i/tu/rename-project-1.png">
+    <li>In the <span class='field-name'>Name</span> field, enter the new project name and then click <span class='button-name'>Save Changes</span>.</li>
 </ol>
