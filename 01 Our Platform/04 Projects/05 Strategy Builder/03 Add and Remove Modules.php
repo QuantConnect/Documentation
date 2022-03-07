3cf29f641b0db79caa60ba86da220f865431e1e2
Web Editor <web.editor@quantconnect.com>
2022-03-07 18:23:58 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/05 Strategy Builder/03 Add and Remove Modules.php
+++ 01 Our Platform/04 Projects/05 Strategy Builder/03 Add and Remove Modules.php
@@ -1,5 +1,3 @@
-<p>You need to <a href='/docs/v2/our-platform/community/profile#03-Code-Settings'>enable the Strategy Builder code setting</a> to use the Strategy Builder.</p>
-
 <?php
 echo file_get_contents(DOCS_RESOURCES."/tutorials/projects/auto-generate-algorithm-code.php");
 ?>
