67eeed6711954ba170b1eb7e6bb474c9d8ec34e5
Web Editor <web.editor@quantconnect.com>
2022-03-08 17:12:56 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/06 Backtesting/03 Results/15 Download Results.php
+++ 01 Our Platform/06 Backtesting/03 Results/15 Download Results.php
@@ -1,4 +1,10 @@
-<?php
-include(DOCS_RESOURCES."/tutorials/backtesting/download-backtest-results.php");
-$getDownloadBacktestResultsText("Open a <a href='/docs/v2/our-platform/tutorials/backtesting/accessing-results#02-View-Backtest-Results'>backtest results page</a>");
-?>
+<p>You can download the following information from the backtest results page:</p>
+
+<ul>
+    <li>Runtime statistics</li>
+    <li>Charts</li>
+    <li>The data in the <span class='tab-name'>Overview</span> tab</li>
+    <li>The data in the <span class='tab-name'>Orders</span> tab</li>
+</ul>
+
+<p>To download the preceding information, open the backtest results page, click the <span class='tab-name'>Overview</span> tab, and then click <span class='button-name'>Download Results</span>.</p>
