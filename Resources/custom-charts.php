9dd6b11956b2230b751a04f6051d2d78d009f6ef
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-09 16:49:29 -0700

    Add headings

--- Resources/custom-charts.php
+++ Resources/custom-charts.php
@@ -1,33 +1,48 @@
 <?php
 $getCustomChartText = function($backtest) {
 
-    echo "<p>Create custom charts and they will display on the results page of your algorithm. We support the following types of plots:</p>";
-    echo "<div class='section-example-container'>";
-    echo "<pre class='all prettyprint prettyprinted'>";
-    echo     "<span class='typ'>SeriesType</span><span class='pun'>.</span><span class='typ'>Line</span><br>";
-    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Scatter</span><br>";
-    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Candle</span><br>";
-    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Bar</span><br>";
-    echo     "<span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Flag</span>";
-    echo "</pre></div>";
+    echo "
+        <p>The results page shows the custom charts that you create.</p>
+        <h4>Supported Plot Types</h4>
+        <p>We support the following types of plots:</p>
+        <div class='section-example-container'>
+        <pre class='all prettyprint prettyprinted'>
+             <span class='typ'>SeriesType</span><span class='pun'>.</span><span class='typ'>Line</span><br>
+             <span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Scatter</span><br>
+             <span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Candle</span><br>
+             <span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Bar</span><br>
+             <span class='pln'>          </span><span class='pun'>.</span><span class='typ'>Flag</span>
+         </pre></div>
+     ";
 
     if ($backtest) {
         echo "<p>To create other types of charts, save the plot data in the ObjectStore and then load it into the Research Environment. In the Research Environment, you can <a href='/docs/v2/research-environment/charting/key-concepts'>create other types of charts with third-party charting packages</a>.</p>";
     }
 
-    echo "<p>When you create scatter plots, you can set a marker symbol. We support the following marker symbols:</p>";
-    echo "<div class='section-example-container'>";
-    echo "<pre class='all prettyprint prettyprinted'>";
-    echo     "<span class='typ'>ScatterMarkerSymbol</span><span class='pun'>.</span><span class='typ'>Circle</span><br>";
-    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Diamond</span><br>";
-    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Square</span><br>";
-    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Triangle</span><br>";
-    echo     "<span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>TriangleDown</span>";
-    echo "</pre></div>";
+    echo "
+        <h4>Supported Markers</h4>
+        <p>When you create scatter plots, you can set a marker symbol. We support the following marker symbols:</p>
+        <div class='section-example-container'>
+        <pre class='all prettyprint prettyprinted'>
+            <span class='typ'>ScatterMarkerSymbol</span><span class='pun'>.</span><span class='typ'>Circle</span><br>
+            <span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Diamond</span><br>
+            <span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Square</span><br>
+            <span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>Triangle</span><br>
+            <span class='pln'>                   </span><span class='pun'>.</span><span class='typ'>TriangleDown</span>
+        </pre></div>
+    ";
 
     if ($backtest) {
-        echo "<p>Custom charts are limited to 4,000 data points. Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. If you exceed the limit, the Console displays the following message:</p>";
-        echo "<p><span class='error-messages'>Exceeded maximum points per chart, data skipped</span></p>";
+        echo "
+            <h4>Chart Quotas</h4>
+            <p>Custom charts are limited to 4,000 data points. Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. If you exceed the limit, the Console displays the following message:</p>
+            <p><span class='error-messages'>Exceeded maximum points per chart, data skipped</span></p>
+        ";
     }
+    
+    echo "
+        <h4>Demonstration</h4>
+        <p>For more information about creating custom charts, see <a href='/docs/v2/writing-algorithms/user-guides/charting'>Charting</a>.
+    ";
 }
 ?>
