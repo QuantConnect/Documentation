d9c464896a424f4b60da391e43cc6def6e9c9060
Web Editor <web.editor@quantconnect.com>
2022-03-09 18:10:57 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/05 Results/10 Logs.php
+++ 01 Our Platform/08 Live Trading/05 Results/10 Logs.php
@@ -1,3 +1,9 @@
-<p>The live performance page has a <span class='tab-name'>Logs</span> tab that displays all of the logs, debug messages, and status messages that your algorithm creates. It's good practice to add logs in a live deployment because then you can see what is happening during execution. The status messages include all of the points in time when your algorithm deployed, encountered an error, sent an order, or quit execution. You can view the logs directly in the <span class='tab-name'>Logs</span> tab or download them for further analysis. If you stop and redeploy your algorithm, the logs are retained. <br></p>
+<p>The <span class='tab-name'>Logs</span> tab on the live results page displays all of the <a href='/docs/v2/our-platform/projects/debugging#04-Logging-Statements'>logging statements</a> and status messages your algorithm creates. The status messages include all of the points in time when your algorithm deployed, encountered an error, sent an order, or quit executing. It's good practice to add logs in live algorithms because then you can see what is happening while it executes. If you stop and redeploy your algorithm, the logs are retained. You can view the log file on the live results page or download them to your local machine. </p>
 
 
+<h4>View in the GUI</h4>
+<p>To see the log file your algorithm has created, open the live results page and then click the <span class="tab-name">Logs</span> tab.</p>
+
+
+<h4>Download Log File</h4>
+<p>To download the log file, open the live result page, click the <span class="tab-name">Logs</span> tab, and then click <span class="button-name">Download Logs</span>.</p>
