76c1d097f25e327774d611ba034440d4c556e61c
Derek Melchin <melchin.derek@gmail.com>
2022-03-09 13:59:32 -0700

    Restructure Live Trading Results page

---
+++ 01 Our Platform/06 Backtesting/03 Results/10 Orders.php
@@ -0,0 +1,4 @@
+<?php 
+include(DOCS_RESOURCES."/algorithm-results/orders.php"); 
+$getOrdersText(false);
+?>
---
+++ 01 Our Platform/06 Backtesting/03 Results/11 Insights.php
@@ -0,0 +1,4 @@
+<?php 
+include(DOCS_RESOURCES."/algorithm-results/insights.php"); 
+$getOrdersText(false);
+?>
