7a95552b97abc1a18c58ea40ae371f0096376bfa
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-09 17:12:23 -0700

    Rename live performance to live results

--- Resources/brokerages/stop-execution.php
+++ Resources/brokerages/stop-execution.php
@@ -1,9 +1,9 @@
 <?php
-$getStopExecutionText = function($livePerformanceLink) {
+$getStopExecutionText = function($liveResultsLink) {
     echo "
         <p>Follow these steps to stop a live algorithm:</p>
         <ul>
-            <li>Open the <a href='{$livePerformanceLink}'>live performance page</a> of the algorithm.</li>
+            <li>Open the <a href='{$liveResultsLink}'>live results page</a> of the algorithm.</li>
             <li>Click <span class='button-name'>Stop</span>.</li>
             <li>Click <span class='button-name'>Yes</span></li>
         </ul>
