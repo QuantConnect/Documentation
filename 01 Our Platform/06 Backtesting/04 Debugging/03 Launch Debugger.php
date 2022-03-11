773201c50ac8c2660e857db0a618ceb742a691b1
Web Editor <web.editor@quantconnect.com>
2022-03-11 18:55:00 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/06 Backtesting/04 Debugging/03 Launch Debugger.php
+++ 01 Our Platform/06 Backtesting/04 Debugging/03 Launch Debugger.php
@@ -3,18 +3,6 @@
     <li><a href="/docs/v2/our-platform/tutorials/projects/managing-projects#05-Open-Existing-Projects">Open the project</a> you want to debug.</li>
     <li>In your project's code files, add atleast one <a href='/docs/v2/our-platform/backtesting/debugging#02-Breakpoints'>breakpoint</a>.</li>
     <li>Click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/backtest-button.png'> <span class='icon-name'>Backtest</span> icon.</li>
-    <p>If the Run and Debug panel is not open, it opens when the first breakpoint is hit.</p>
-    <li>If you want to set watch variables, follow these steps:</li>
-    <ol>
-    	<li>In the Debug panel, click the <span class='icon-name'>plus</span> icon.</li>
-    	<li>Enter the name of the variable to watch and then click <span class='button-name'>OK</span>.</li>
-    	<li>If you want to add more watch variables, continue from step 4.1.</li>
-    	<li>If you want to update your watch variables, follow these steps:</li>
-    	<ol>
-    		<li>In the Debug panel, click the variable name that you want to update.</li>
-    		<li>Enter the new variable name and then click <span class='button-name'>OK</span>.</li>
-    	</ol>
-    	<li>If you want to remove a watch variable, click the <span class='icon-name'>X</span> icon next to the watch variable that you want to remove.</li>
-    </ol>
-    <li>Click <span class='button-name'>Backtest</span> to start debugging.</li>
 </ol>
+
+<p>If the Run and Debug panel is not open, it opens when the first breakpoint is hit.</p>
