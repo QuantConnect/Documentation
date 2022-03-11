035a5c741ec780b088c50d4cf717b1af56c1479c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-11 11:38:35 -0700

    Merge in info from the introduction

--- Resources/tutorials/projects/auto-generate-algorithm-code.php
+++ Resources/tutorials/projects/auto-generate-algorithm-code.php
@@ -8,6 +8,7 @@
     <li>On the Strategy Builder page, click <span class="button-name">Add Module</span>.</li>
     <p>The modules catalog displays.</p>
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/strategy-builder-catalog.png">
+    <p>Each of the modules in the catalog contains a description and list of the pre-selected settings for the module.</p>
     <li><span class="qualifier">(Optional)</span> At the bottom of the page, click the pagination buttons to view other modules.</li>
     <li><span class="qualifier">(Optional)</span> Click a module in the table to view more information about it.</li>
     <li>To apply filters to the module catalog, follow these steps:</li>
@@ -49,7 +50,7 @@
     </ol>
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/add-strategy-builder-module.gif">
     <li>Click <span class="button-name">Add</span> next to the modules you want to add.</li>
-    <p>The modules you add are added to your code files.</p>
+    <p>The modules you add are added to your code files, but their class definitions aren't. To view the definitions, see the <a rel="nofollow" target="_blank" href="https://github.com/QuantConnect/Lean">Lean GitHub repository</a>.</p>
     <li>If you want to remove modules that you have added, click <span class="button-name">Remove</span> next to the module.</li>
     <p>The modules you remove are removed from your code files.</p>
 </ol>
