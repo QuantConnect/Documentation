1a6aa40921e904dffd698b216a1346eaf8321e91
Web Editor <web.editor@quantconnect.com>
2022-03-14 19:28:34 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/05 Assets Available.php
+++ 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/05 Assets Available.php
@@ -32,7 +32,7 @@
 </table>
 
 <h4>Data Feeds</h4>
-<p>You might need to purchase an IB data subscription for your trading. For more information about live data feeds, see <a href='/docs/v2/our-platform/user-guides/live-trading/data-feeds'>Data Feeds</a>.</p>
+<p>You might need to purchase an IB data subscription for your trading. For more information about live data feeds, see <a href='/docs/v2/our-platform/live-trading/data-feeds'>Data Feeds</a>.</p>
 
 <h4>ETFs</h4>
 <?php
