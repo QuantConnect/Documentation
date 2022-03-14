c7ac1cdd77c64420b9326bd70faac34429cbf02f
Web Editor <web.editor@quantconnect.com>
2022-03-14 16:31:54 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/02 Project Management/06 Rename Projects.php
+++ 01 Our Platform/04 Projects/02 Project Management/06 Rename Projects.php
@@ -1,6 +1,6 @@
 <p>Follow these steps to rename projects:</p>
 <ol>
-    <li><a href="/docs/v2/our-platform/tutorials/projects/managing-projects#05-Open-Existing-Projects">Open the project</a> that you want to rename.</li>
+    <li><a href="/docs/v2/our-platform/projects/project-management#02-View-All-Projects">Open the project</a> you want to rename.</li>
     <li>In the Project panel, hover over the project name and then click the <span class='icon-name'>pencil</span> icon that appears.</li>
     <img class='docs-image' style='max-height: 120px' src="https://cdn.quantconnect.com/i/tu/rename-project-1.png">
     <li>In the <span class='field-name'>Name</span> field, enter the new project name and then click <span class='button-name'>Save Changes</span>.</li>
