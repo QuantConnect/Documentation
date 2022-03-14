65127eebab025dfde8e1b40a797e89736947e9c6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-14 15:29:32 -0600

    remove fluff words

--- Resources/libraries/project-libraries.php
+++ Resources/libraries/project-libraries.php
@@ -1,2 +1,2 @@
-<p>Project libraries are QuantConnect projects that you can merge into your project to avoid duplicating code files. If you have tools that you use across several projects, create a library.</p>
+<p>Project libraries are QuantConnect projects you can merge into your project to avoid duplicating code files. If you have tools that you use across several projects, create a library.</p>
 
