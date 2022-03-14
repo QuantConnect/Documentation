cedc0c6d1402bbe1498ec1673b04193ae1567916
Web Editor <web.editor@quantconnect.com>
2022-03-14 18:11:36 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/06 Backtesting/02 Deployment/04 Logs.php
+++ 01 Our Platform/06 Backtesting/02 Deployment/04 Logs.php
@@ -1,3 +1,3 @@
-<p>The amount of logs you can generate per backtest and per day depends on the tier of your organization. The following table describes the amount of logs that you can generate on each tier:<br></p>
+<p>The amount of logs you can generate per backtest and per day depends on the tier of your organization. The following table shows the amount of logs you can generate on each tier:</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/quotas/logs.html"); ?>
