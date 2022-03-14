995ba8077bc672eaa8031ff7211601f55724e53b
Web Editor <web.editor@quantconnect.com>
2022-03-14 18:59:56 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/07 Datasets/05 Licensing/03 Cloud.php
+++ 01 Our Platform/07 Datasets/05 Licensing/03 Cloud.php
@@ -1,13 +1,13 @@
-<p>If you have a Cloud license for a dataset, you can access the dataset in the Algorithm Lab for research, backtests, and live trading. The cost of the license is added to your monthly bill, which you can pay with your organization's credit card or <a href='/docs/v2/our-platform/user-guides/organizations/credit'>QuantConnect Credit</a> balance. With one Cloud license for a dataset, all of the members in your organization can access the dataset in the cloud. If you have more than 3 Alphas listed in the Alpha Streams market, you can run backtests in the cloud without paying the Cloud license fee.</p>
+<p>If you have a Cloud license for a dataset, you can access the dataset in the Algorithm Lab for research, backtests, and live trading. The cost of the license is added to your monthly bill, which you can pay with your organization's credit card or <a href='/docs/v2/our-platform/organizations/credit'>QuantConnect Credit</a> balance. With one Cloud license for a dataset, all of the members in your organization can access the dataset in the cloud. If you have more than 3 Alphas listed in the Alpha Streams market, you can run backtests in the cloud without paying the Cloud license fee.</p>
 
 <h4>Add Cloud Access</h4>
-<p>You need an organization above the <a href='/docs/v2/our-platform/user-guides/organizations/tier-features#02-Free-Tier'>Free tier</a> to purchase cloud access to datasets.</p>
+<p>You need an organization above the <a href='/docs/v2/our-platform/organizations/tier-features#02-Free-Tier'>Free tier</a> to purchase cloud access to datasets.</p>
 
 <p>Follow these steps to add cloud access to datasets:</p>
 
 <ol>
     <li>Log in to the Algorithm Lab.</li>
-    <li>Open the <a href='/docs/v2/our-platform/tutorials/datasets/inspecting-datasets#02-View-Dataset-Listing'>listing page of a dataset</a> for which you want to gain cloud access.</li>
+    <li>Open the <a href='/docs/v2/our-platform/datasets/navigating-the-market#02-View-All-Listings'>listing page of a dataset</a> for which you want to gain cloud access.</li>
     <li>On the dataset listing page, click the <span class='tab-name'>Pricing</span> tab.</li>
     <li>Under the <span class='page-section-name'>Cloud Access</span> section, click <span class='button-name'>SUBSCRIBE</span>.</li>
     <li>On the Pricing page, click <span class='button-name'>Update Subscriptions</span>.</li>
@@ -18,7 +18,7 @@
 <p>Follow these steps to remove cloud access to datasets:</p>
 
 <ol>
-    <li>Open the <a href="/docs/v2/our-platform/tutorials/organizations/managing-organizations#02-View-Organization-Profile">organization homepage</a>.</li>
+    <li>Open the <a href="/docs/v2/our-platform/organizations/administration#05-View-the-Organization-Homepage">organization homepage</a>.</li>
     <li>Click <span class='button-name'>Edit Plan</span>.</li>
     <li>On the Pricing page, under the <span class='page-section-name'>Market Subscriptions</span> section, click <span class='button-name'>Cancel Renew</span> next to the dataset to which you no longer want to have cloud access.</li>
     <li>Click <span class='button-name'>Update Subscriptions</span>.</li>
