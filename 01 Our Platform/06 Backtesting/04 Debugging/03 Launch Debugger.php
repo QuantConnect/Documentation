30c3432da620303c7395ba8e7c35d1ed3366bdd6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-14 16:42:04 -0600

    Update to include the new debug button

--- 01 Our Platform/06 Backtesting/04 Debugging/03 Launch Debugger.php
+++ 01 Our Platform/06 Backtesting/04 Debugging/03 Launch Debugger.php
@@ -2,7 +2,7 @@
 <ol>
     <li><a href="/docs/v2/our-platform/projects/project-management#02-View-All-Projects">Open the project</a> you want to debug.</li>
     <li>In your project's code files, add at least one breakpoint.</li>
-    <li>Click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/backtest-button.png'> <span class='icon-name'>Backtest</span> icon.</li>
+    <li>Click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/debug-icon.png'> <span class='icon-name'>Debug</span> icon.</li>
 </ol>
 
 <p>If the Run and Debug panel is not open, it opens when the first breakpoint is hit.</p>
