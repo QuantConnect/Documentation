9f5dc165e4631d657a175f7dac41e704727da9be
Web Editor <web.editor@quantconnect.com>
2022-03-14 14:23:18 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 Organizations/02 Resources/09 Log Quotas.php
+++ 01 Our Platform/02 Organizations/02 Resources/09 Log Quotas.php
@@ -3,4 +3,4 @@
 
 <p>If you delete a backtest or project that produced logs, your quotas aren't restored. Additionally, daily log quotas aren't fully restored at midnight. They are restored according to a 24-hour following window. </p>
 
-<p>To avoid reaching the limits, we recommend logging sparsely, focusing on the change events instead of logging every time loop. You can use the <a href='/docs/v2/our-platform/user-guides/projects/debugging#03-Debugger'>debugger</a> to inspect objects during runtime. If you use the debugger, you should rarely reach the log limits.</p>
+<p>To avoid reaching the limits, we recommend logging sparsely, focusing on the change events instead of logging every time loop. You can use the <a href='/docs/v2/our-platform/backtesting/debugging'>debugger</a> to inspect objects during runtime. If you use the debugger, you should rarely reach the log limits.</p>
