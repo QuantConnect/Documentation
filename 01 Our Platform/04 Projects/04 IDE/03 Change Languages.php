1c698a46e00f10bdd82ed45e26ac69d2f194cef9
Web Editor <web.editor@quantconnect.com>
2022-03-14 16:54:35 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/04 IDE/03 Change Languages.php
+++ 01 Our Platform/04 Projects/04 IDE/03 Change Languages.php
@@ -2,6 +2,6 @@
 <ol>
     <li>Log in to the Algorithm Lab.</li>
     <li>In the top navigation bar, click <span class='menu-name'><span class='placeholder-text'>yourUsername</span> &gt; My Account</span>.</li>
-    <li>On the Your Account page, in the <span class='page-section-name'>Account Settings</span> section, click <span class='button-name'>C#</span> or <span class='button-name'>Python</span>.</li>
+    <li>On your Account page, in the <span class='page-section-name'>Account Settings</span> section, click <span class='button-name'>C#</span> or <span class='button-name'>Python</span>.</li>
     <p>"Preferred language setting has been updated" displays.</p>
 </ol>
