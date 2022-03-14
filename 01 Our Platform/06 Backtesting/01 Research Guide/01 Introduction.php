cf2ae316363a86e90e0a327a6476eb8afc4a6c35
Web Editor <web.editor@quantconnect.com>
2022-03-14 18:03:19 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/06 Backtesting/01 Research Guide/01 Introduction.php
+++ 01 Our Platform/06 Backtesting/01 Research Guide/01 Introduction.php
@@ -2,5 +2,5 @@
     echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/backtesting.html");
 ?>
 
-<p>QuantConnect aims to teach and inspire our community to create high performing algorithmic trading strategies. We measure our success by the profits created by the community through their live trading. As such, we try to build the best quantitative research techniques possible into the product to encourage a robust research process.</p>
+<p>QuantConnect aims to teach and inspire our community to create high-performing algorithmic trading strategies. We measure our success by the profits created by the community through their live trading. As such, we try to build the best quantitative research techniques possible into the product to encourage a robust research process.</p>
 
