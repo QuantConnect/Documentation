45b5d6e06b3cda9590b6cb91606f90083340017c
Web Editor <web.editor@quantconnect.com>
2022-03-14 17:42:55 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/05 Research/01 Deployment/01 Introduction.php
+++ 01 Our Platform/05 Research/01 Deployment/01 Introduction.php
@@ -1,6 +1,6 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/research-environment.html"); ?>
 
-<p>Before you run backtests, we recommend testing your hypothesis in the Research Environment. It's easier to perform data analysis and <a href="/docs/v2/research-environment/tutorials/plotting-in-research">produce plots in the Research Environment</a> than in a backtest. In the Research Environment, you can use the QuantConnect API to <a href="/docs/v2/our-platform/api-reference/backtest-management/read-backtest">import your backtest results</a> for further analysis.</p>
+<p>Before you run backtests, we recommend testing your hypothesis in the Research Environment. It's easier to perform data analysis and <a href="/docs/v2/research-environment/charting">produce plots in the Research Environment</a> than in a backtest. In the Research Environment, you can use the QuantConnect API to <a href="/docs/v2/our-platform/api-reference/backtest-management/read-backtest/backtest-statistics">import your backtest results</a> for further analysis.</p>
 
 <p>Before backtesting or live trading with machine learning models, you may find it beneficial to train them in the Research Environment, save them in the ObjectStore, and then load them from the ObjectStore into the backtesting and live trading environment</p>
 
