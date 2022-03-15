2ea43e8417e624b26b37f1b64c0076652a0285b4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-14 18:27:36 -0600

    fix typo

--- Resources/brokerages/latency.php
+++ Resources/brokerages/latency.php
@@ -1,6 +1,6 @@
 <?php
 $getBrokerageLatencyText = function($brokerageName, $brokerageFileName) {
-   echo "<p>Brokerage latency is amount of time it takes for messages like orders to travel between your algorithm and the brokerage. The brokerage latency of $brokerageName is ";
+   echo "<p>Brokerage latency is the amount of time it takes for messages like orders to travel between your algorithm and the brokerage. The brokerage latency of $brokerageName is ";
 
    echo file_get_contents(DOCS_RESOURCES."/brokerages/latency-by-brokerage/$brokerageFileName.php");
 
