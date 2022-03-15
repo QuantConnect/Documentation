d4e4952a219b60ef91e791241daf7d91ceb1b864
Web Editor <web.editor@quantconnect.com>
2022-03-14 20:01:15 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/09 Optimization/04 Deployment/02 Resources.php
+++ 01 Our Platform/09 Optimization/04 Deployment/02 Resources.php
@@ -1,4 +1,4 @@
-<p>The optimization nodes that backtest your algorithm are not the <a href="/docs/v2/our-platform/user-guides/organizations/resources#02-Backtesting-Nodes">backtesting nodes</a> in your organization. The optimization nodes are a cluster of nodes that exclusively run optimization jobs. The optimization can run with up to 24 nodes. If you use multiple nodes, the backtests run concurrently and all of the nodes are the same model. The following table shows the specifications of the optimization node models:</p> 
+<p>The optimization nodes that backtest your algorithm are not the <a href="/docs/v2/our-platform/organizations/resources#02-Backtesting-Nodes">backtesting nodes</a> in your organization. The optimization nodes are a cluster of nodes that exclusively run optimization jobs. The optimization can run with up to 24 nodes. If you use multiple nodes, the backtests run concurrently and all of the nodes are the same model. The following table shows the specifications of the optimization node models:</p> 
 
 <table class="qc-table table">
    <thead>
@@ -60,7 +60,7 @@
 </style>
 
 
-<p>The following table shows the <a href="/docs/v2/our-platform/user-guides/organizations/resources#08-Training-Quotas">training quotas</a> of the optimization nodes:</p>
+<p>The following table shows the <a href="/docs/v2/our-platform/organizations/resources#08-Training-Quotas">training quotas</a> of the optimization nodes:</p>
 
 <table class="qc-table table">
    <thead>
