ef4e1cd704e43646eb3be4ed316a88ba52539f39
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-17 12:58:52 -0600

    Update to display tables and include the module subscription step

--- 05 Lean CLI/09 Live Trading/14 Zerodha/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/14 Zerodha/02 Deploy Local Algorithms.php
@@ -3,6 +3,17 @@ include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php")
 
 
 $brokerageDetails = "
+<li>Enter the number of the organization that has a subscription for the Zerodha module.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Select the organization with the Samco module subscription:
+1) Organization 1
+2) Organization 2
+3) Organization 3
+Enter an option: 1</pre>
+</div>
+</li>
+
 <li>Enter your <a href='https://kite.trade/' target='_blank'>Kite Connect</a> API key and access token.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
@@ -11,23 +22,64 @@ Access token: ********************</pre>
 </div>
 </li>
 
-<li>Enter the product type. This property must be <code>MIS</code> if you are targeting intraday products, <code>CNC</code> if you are targeting delivery products, or <code>NRML</code> if you are targeting carry forward products.
+<li>Enter the product type.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
-Product type (MIS, CNC, NRML): MIS</pre>
+Product type (MIS, CNC, NRML):</pre>
 </div>
+<p>The following table describes the product types:</p>
+<table class='qc-table table'>
+    <thead>
+        <tr>
+            <th style='width: 25%'>Product Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>MIS</code></td>
+            <td>Intraday products</td>
+        </tr>
+        <tr>
+            <td><code>CNC</code></td>
+            <td>Delivery products</td>
+        </tr>
+        <tr>
+            <td><code>NRML</code></td>
+            <td>Carry forward products</td>
+        </tr>
+    </tbody>
+</table>
 </li>
 
-<li>Enter the trading segment. This property must be <code>EQUITY</code> if you are trading equities on NSE or BSE, or <code>COMMODITY</code> if you are trading commodities on MCX.
+<li>Enter the trading segment.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
-Trading segment (EQUITY, COMMODITY): EQUITY</pre>
+Trading segment (EQUITY, COMMODITY):</pre>
 </div>
+<p>The following table describes when to use each trading segment:</p>
+<table class='qc-table table'>
+    <thead>
+        <tr>
+            <th style='width: 25%'>Trading Segment</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>EQUITY</code></td>
+            <td>For trading Equities on the National Stock Exchange of India (NSE) or the Bombay Stock Exchange (BSE)</td>
+        </tr>
+        <tr>
+            <td><code>COMMODITY</code></td>
+            <td>For trading commodities on the Multi Commodity Exchange of India (MCX)</td>
+        </tr>
+    </tbody>
+</table>
 </li>
 ";
 
 $dataFeedDetails = "
-<li>Follow the steps of configuring Zerodha as brokerage above if you chose paper trading as the brokerage.</li>
 <li>Enter whether you have a history API subscription.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
