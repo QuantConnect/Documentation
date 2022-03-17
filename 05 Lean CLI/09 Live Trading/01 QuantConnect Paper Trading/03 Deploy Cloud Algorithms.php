588de4e0d3013ceb1a5b619817eb3114b8dca979
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-17 10:27:02 -0600

    Add cloud deployment support

--- 05 Lean CLI/09 Live Trading/01 QuantConnect Paper Trading/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 QuantConnect Paper Trading/03 Deploy Cloud Algorithms.php
@@ -1,5 +1,6 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
+$isSupported = true;
 $brokerageDetails = "";
-$getDeployCloudAlgorithmsText("QuantConnect Paper Trading", $brokerageDetails);
+$getDeployCloudAlgorithmsText("QuantConnect Paper Trading", $isSupported, $brokerageDetails);
 ?>
