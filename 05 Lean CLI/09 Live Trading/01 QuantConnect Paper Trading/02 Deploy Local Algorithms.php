aeacf826ddfbe401e5d5168d453a088f47f307ad
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-17 11:25:00 -0600

    Add custom data issue workaround

--- 05 Lean CLI/09 Live Trading/01 QuantConnect Paper Trading/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 QuantConnect Paper Trading/02 Deploy Local Algorithms.php
@@ -4,11 +4,16 @@ include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php")
 
 $brokerageDetails = "";
 $dataFeedDetails = "
-	<p>
-	    If your algorithm only uses custom data, you can select the \"Custom data only\" data feed option.
-	    This data feed doesn't require any brokerage credentials, but only works if your algorithm doesn't subscribe to non-custom data.
-	    Your algorithm crashes if it attempts to subscribe to non-custom data with this data feed in place.
-	</p>
+        <p>
+            If your algorithm only uses custom data, you can select the \"Custom data only\" data feed option.
+            This data feed doesn't require any brokerage credentials, but only works if your algorithm doesn't subscribe to non-custom data.
+            Your algorithm crashes if it attempts to subscribe to non-custom data with this data feed in place, including the benchmark security.
+            To avoid data issues with the benchmark security, either set the benchmark to the subscribed custom data or a constant.
+        </p>
+        <div class='section-example-container'>
+            <pre class='csharp'>SetBenchmark(x => 0);</pre>
+            <pre class='python'>self.SetBenchmark(lambda x: 0)</pre>
+        </div>
 ";
 $supportsIQFeed = true;
 
