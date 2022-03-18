af79d3d685570353a10b5c668bed7d241d5187d4
Web Editor <web.editor@quantconnect.com>
2022-03-18 13:43:59 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/12 Security and Stability.php
+++ 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/12 Security and Stability.php
@@ -7,7 +7,7 @@ $getBrokerageStabilityText("IB", "https://www.interactivebrokers.com/en/software
 <p>IB only supports 1 connection at a time to your account. If you interfere with your brokerage account while an algorithm is connected to it, the algorithm may stop executing. To run more than 1 algorithm, <a rel="nofollow" target="_blank" href="https://www.interactivebrokers.com.hk/en/software/ptgstl/topics/ptgsubaccounts.htm">open an IB subaccount</a> for each additional algorithm.</p>
 
 <h4>SMS 2FA</h4>
-<p>Our IB integration doesn't support Two-Factor Authentication (2FA) via SMS. Use the <a rel="nofollow" target="_blank" href="https://guides.interactivebrokers.com/iphone/log_in/using_ios.htm">IB Key protocol in IBKR Mobile</a> instead.</p>
+<p>Our IB integration doesn't support Two-Factor Authentication (2FA) via SMS. Use the <a rel="nofollow" target="_blank" href="https://guides.interactivebrokers.com/iphone/log_in/ibkey.htm?tocpath=IB%20Key%20Security%20Protocol%7C_____0">IB Key Security via IBKR Mobile</a> instead.</p>
 
 <h4>System Resets</h4>
 <p>If your IB account has 2FA enabled, you'll need to re-authenticate once a week. The reset schedule is available on the <a rel="nofollow" target="_blank" href="https://www.interactivebrokers.com/en/software/systemStatus.php">IB Status page</a>. If you miss the timeout period, your algorithm quits executing.</p>
