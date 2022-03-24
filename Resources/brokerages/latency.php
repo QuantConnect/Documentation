dfceafbe998f0dd77a0412cb009865e640610b20
Derek Melchin <melchin.derek@gmail.com>
2022-03-24 15:20:27 -0600

    Remove empty page section

--- Resources/brokerages/latency.php
+++
@@ -1,9 +0,0 @@
-<?php
-$getBrokerageLatencyText = function($brokerageName, $brokerageFileName) {
-   echo "<p>Brokerage latency is the amount of time it takes for messages like orders to travel between your algorithm and the brokerage. The brokerage latency of $brokerageName is ";
-
-   echo file_get_contents(DOCS_RESOURCES."/brokerages/latency-by-brokerage/$brokerageFileName.php");
-
-   echo ".</p>";
-}
-?>
