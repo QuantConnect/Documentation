b5812ea9274a67c007306d631fb5a1d124e21b88
Web Editor <web.editor@quantconnect.com>
2022-03-24 18:09:59 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/07 Datasets/07 Contributing Datasets/09 Update Documentation.php
+++ 01 Our Platform/07 Datasets/07 Contributing Datasets/09 Update Documentation.php
@@ -6,4 +6,4 @@
     <li>Update the <span class='page-section-name'>Sourcing</span> section of the <a href='/docs/v2/our-platform/live-trading/data-feeds'>data feed guides</a>.</li>
 </ol>
 
-<p>After we merge your dataset into production, we will update the <a href='/docs/v2/our-platform/quantconnect#04-Data-Library'>Data Library image</a> if this is your organization's first dataset contribution and we will update the <a href='/docs/v2/our-platform/datasets/categories'>Categories</a> documentation if your dataset falls under a new category.</p>
+<p>After we merge your dataset into production, we will update the <a href='/docs/v2/our-platform/welcome#04-Data-Library'>Data Library image</a> if this is your organization's first dataset contribution and we will update the <a href='/docs/v2/our-platform/datasets/categories'>Categories</a> documentation if your dataset falls under a new category.</p>
