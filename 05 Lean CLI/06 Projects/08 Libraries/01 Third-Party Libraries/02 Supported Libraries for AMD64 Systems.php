b653b73d345613531bd928e700a7b5d5234190db
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-24 18:24:41 -0600

    Make the list of libraries the same resource as in Our Platform

---
+++ 05 Lean CLI/06 Projects/08 Libraries/01 Third-Party Libraries/02 Supported Libraries for AMD64 Systems.php
@@ -0,0 +1,10 @@
+<p>
+    The CLI supports many of the most popular C# and Python open-source libraries.
+    On AMD64-based systems, the CLI supports the same C# and Python libraries as are supported on QuantConnect.
+    If you're unsure about the architecture of your system, it's most likely AMD64.
+    The following libraries are available on AMD64-based systems:
+</p>
+
+<?php
+echo file_get_contents(DOCS_RESOURCES."/libraries/supported-libraries.php");
+?>
