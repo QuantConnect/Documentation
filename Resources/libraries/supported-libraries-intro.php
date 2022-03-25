a10015c1ba574635bec2913bec58113ee16b0889
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-03-24 18:28:38 -0600

    Add intro text resource

---
+++ Resources/libraries/supported-libraries-intro.php
@@ -0,0 +1,3 @@
+<p>
+We support various libraries for machine learning, plotting, and data processing. As members often request new libraries, we frequently add new libraries to the underlying docker image that runs the Lean engine. The following list comes directly from the underlying docker image and is the exhaustive set of libraries that we support. To use these libraries, add the relevant <code class="csharp">using</code><code class="python">import</code> statement at the top of your file.
+</p>
