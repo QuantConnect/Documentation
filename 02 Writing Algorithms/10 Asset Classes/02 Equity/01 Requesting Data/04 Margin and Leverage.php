60f3da0b53dc8172d95b8b60a30b863238305eb4
Web Editor <web.editor@quantconnect.com>
2022-04-04 18:22:19 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 Equity/01 Requesting Data/04 Margin and Leverage.php
+++ 02 Writing Algorithms/10 Asset Classes/02 Equity/01 Requesting Data/04 Margin and Leverage.php
@@ -1,1 +1,5 @@
-<div>What is margin/leverage:</div><div>-</div><div><br></div><div>Why is there margin/leverage:</div><div>-PDT</div><div><br></div><div>What margin/leverage is available:</div><div>-The default margin is<br></div><div><br></div><div>How do I set the margin/leverage:<br></div><div>- Pass to AddEquity or configure in the UniverseSettings<br></div>
+<div>We model buying power and margin calls to ensure your algorithm stays within the margin requirements.</div><div><br></div><div>Why is there margin/leverage:</div><div>-PDT</div><div><br></div><div>What margin/leverage is available:</div><div>-The default leverage is 2x leverage for margin accounts. If you trade live, the brokerage determines the amount of leverage you may use.<br>-Leverage is not available for cash accounts.</div><div><br></div><div>How do I set the margin/leverage:<br></div><div>- Pass to AddEquity or configure in the UniverseSettings<br></div>
+
+<?php include(DOCS_RESOURCES."/brokerages/margin-calls.html"); ?>
+
+<?php include(DOCS_RESOURCES."/brokerages/pattern-day-trader-rule.html"); ?>
