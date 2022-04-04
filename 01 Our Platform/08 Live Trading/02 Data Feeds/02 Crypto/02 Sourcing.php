60c9a741a2846d2e0679ba1631da9fd50c77a0d9
Web Editor <web.editor@quantconnect.com>
2022-04-04 12:53:42 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Data Feeds/02 Crypto/02 Sourcing.php
+++ 01 Our Platform/08 Live Trading/02 Data Feeds/02 Crypto/02 Sourcing.php
@@ -1,6 +1,6 @@
 <p>The Crypto data feed uses WebSockets to gathers market data from the following sources:</p>
 <ul>
-    <li>Binance</li>
+    <li>Binance & Binance US</li>
     <li>Bitfinex</li>
     <li>Coinbase Pro</li>
     <li>FTX & FTX US</li>
