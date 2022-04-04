d33361279df0c7a29df02c53daf3f16fdf7888c0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-04-04 16:38:26 -0600

    import data normalization table

---
+++ 01 Our Platform/07 Datasets/04 Misconceptions/05 Data Normalization.php
@@ -0,0 +1,3 @@
+<p>The data normalization mode defines how historical data is adjusted to accommodate for splits and dividends.  When you compare the data in the Dataset Market to data that's hosted on other platforms, the data may have different values because a different data normalization mode is being used to adjust the data. Ensure datasets are using the same normalization mode before reporting data issues. The most common way to recognize this bug is by comparing the two price series and seeing them significantly deviate in the past. The following table explains the data normalization modes that we support:</p>
+
+<?php include(DOCS_RESOURCES."/datasets/data-normalization.html"); ?>
