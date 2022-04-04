a7397111d7610a50afd02ea55e687d103d68cfd7
Web Editor <web.editor@quantconnect.com>
2022-04-04 18:44:26 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 Equity/01 Requesting Data/09 Data Normalization.php
+++ 02 Writing Algorithms/10 Asset Classes/02 Equity/01 Requesting Data/09 Data Normalization.php
@@ -1,1 +1,12 @@
+What is data normalization:
+<br>- A technique of how the data is adjusted for corporate actions.
+
+<br><br>
+What data normalization options are available:
 <?php include(DOCS_RESOURCES."/datasets/data-normalization.html"); ?>
+
+
+<br><br>
+How to set the data normalization mode:
+<br>-Either with &lt;equity&gt;.SetDataNormalizationMode, UniverseSettings.DataNormalizationMode, or in a security initialize
+<br>-Example snippets
