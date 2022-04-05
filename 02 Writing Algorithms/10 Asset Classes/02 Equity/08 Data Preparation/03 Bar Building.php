2df61ff3c8abc5f5e4fc0069cfc664d114c9f0db
Web Editor <web.editor@quantconnect.com>
2022-04-04 20:19:01 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 Equity/08 Data Preparation/03 Bar Building.php
+++ 02 Writing Algorithms/10 Asset Classes/02 Equity/08 Data Preparation/03 Bar Building.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/data-feeds/bar-building.html"); ?>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/datasets/excluded-ticks.html"); ?>
+<?php echo file_get_contents(DOCS_RESOURCES."/data-feeds/excluded-ticks.html"); ?>
