e4a0a4c5aa1a19568151a600fca5d12c0e196bbd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-04-05 16:48:32 -0600

    Remove dangling div tag

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/06 Corporate Fundamentals/02 Properties.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/06 Corporate Fundamentals/02 Properties.php
@@ -1,4 +1,4 @@
-</div><br>How do I access the properties:
+<br>How do I access the properties:
 <br>-You need to add a coarse-fine universe to your algorithm. See Universes &amp; Universe Selection.
 <br>-After you have added the universe, you can access fundamentals through &lt;security&gt;.Fundamentals.
 <br>-You can also access the fundamentals through the universe selection method/class. This is covered in the Universes &amp; Universe Selection docs.
