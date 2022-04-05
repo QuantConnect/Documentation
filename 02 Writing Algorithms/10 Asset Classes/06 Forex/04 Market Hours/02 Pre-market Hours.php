68dc663d60c098892b3da100ab0836540b986619
Web Editor <web.editor@quantconnect.com>
2022-04-05 18:23:03 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/06 Forex/04 Market Hours/02 Pre-market Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/06 Forex/04 Market Hours/02 Pre-market Hours.php
@@ -1,2 +1,1 @@
-<p>The following table shows the pre-market hours for the Forex market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/202 Forex oanda generic/02 Pre-market Hours.html"); ?>
