195cf6e8150935dd691694c0de5615d916e77179
Web Editor <web.editor@quantconnect.com>
2022-04-06 16:42:38 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/05 Trades.php
+++ 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/05 Trades.php
@@ -1,2 +1,4 @@
 <div>TradeBars</div><div>-Definition: individual trades from the exchanges consolidated into price bars<br></div><div>- Add image from Handling Data docs<br></div><div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, the time-slice may not have any data. <br></div>
+
+<br>## Copy from US Equity
