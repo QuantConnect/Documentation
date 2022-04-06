e69b367929a52b15f9e9fad8ac297c9290dde4b0
Web Editor <web.editor@quantconnect.com>
2022-04-06 16:43:02 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/07 Ticks.php
+++ 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/07 Ticks.php
@@ -6,3 +6,5 @@
 <div>-Tick quote definition: a bid or offer to purchase the security for a specific price</div><div>-Quote ticks contain non-zero BidPrice, BidSize, AskPrice, and AskSize properties</div><div></div>
 
 <div>-Tick data is raw and unfiltered</div><div>-may contain bad ticks which skew your trade results</div><div>-Some ticks come from dark pools, not tradable</div><div>-We recommend only using tick data if you understand the risks and are able to perform your own online tick filtering.</div><div>-Add image of tick going outside of bid/ask and tick crashing far outside normal price (see image from Jared)</div><div>-Marked by QC using a model (? - add a link to the source code)<br></div><div></div>
+
+<br>## Copy from US Equity
