14aa2a11070747a2bdf2c1f6837dd76ec0036a33
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:08:11 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/03 India Equity/05 Market Hours/03 Regular Trading Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/03 India Equity/05 Market Hours/03 Regular Trading Hours.php
@@ -1,2 +1,1 @@
-<p>The following table shows the regular trading hours for the India Equity market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/equity/india/generic/market-opening-hours.html"); ?>
