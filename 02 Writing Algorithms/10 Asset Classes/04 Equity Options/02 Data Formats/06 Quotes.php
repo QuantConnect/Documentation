f67c53892d605fea73cee6ed30e461e7a11436d4
Web Editor <web.editor@quantconnect.com>
2022-04-06 16:32:19 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/04 Equity Options/02 Data Formats/06 Quotes.php
+++ 02 Writing Algorithms/10 Asset Classes/04 Equity Options/02 Data Formats/06 Quotes.php
@@ -1,2 +1,4 @@
 <div>QuoteBars</div><div>-Definition: built by consolidating the bid and ask ticks from the exchanges into bars</div><div>- Add image from Handling Data docs</div><div>-Properties:</div><div>&nbsp;&nbsp;&nbsp; - Open, High, Low, Close, Bid, Ask, LastBidSize, and LastAskSize</div><div>&nbsp;&nbsp;&nbsp; - Bid and the Ask properties are Bar objects</div><div>&nbsp;&nbsp;&nbsp; - The QuoteBar Open, High, Low, and Close properties values are the mean of the respective Bid and Ask properties.</div><div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, the time-slice may not have any data.</div><div>-Used to fill trades in backtesting</div><div>&nbsp;&nbsp;&nbsp;&nbsp; - Makes trades realistic by accounting for spread costs<br></div><div></div>
+
+<br> ## Copy from US Equity
