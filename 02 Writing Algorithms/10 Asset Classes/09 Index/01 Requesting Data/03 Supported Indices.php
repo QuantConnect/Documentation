b09440d5fbe1c6357ab79500bf39edadbdf049d0
Web Editor <web.editor@quantconnect.com>
2022-04-06 14:32:03 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/09 Index/01 Requesting Data/03 Supported Indices.php
+++ 02 Writing Algorithms/10 Asset Classes/09 Index/01 Requesting Data/03 Supported Indices.php
@@ -1,1 +1,1 @@
-<p>To view the supported indices, see <a href='/docs/v2/writing-algorithms/datasets/tickdata/us-cash-indices#05-US-Cash-Indices'>US Cash Indices</a>.</p>
+## TODO: Waiting for <a href='/docs/v2/writing-algorithms/datasets/tickdata/us-cash-indices#05-US-Cash-Indices'>supported Indices</a> to be added to the Resources.
