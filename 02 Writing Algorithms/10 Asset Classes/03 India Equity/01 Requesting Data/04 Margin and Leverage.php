aecd94937747ed23c2282bb7439478de424a7c8c
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:51:18 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/03 India Equity/01 Requesting Data/04 Margin and Leverage.php
+++ 02 Writing Algorithms/10 Asset Classes/03 India Equity/01 Requesting Data/04 Margin and Leverage.php
@@ -1,4 +1,4 @@
--Depends on the brokerage model you use. 
+-Depends on the brokerage model you use and the assets you trade. 
 <br>Refer to Samco and Zerodha brokerage guides
 <br>https://www.quantconnect.com/docs/v2/our-platform/live-trading/brokerages/samco#07-Margin
 <br>https://www.quantconnect.com/docs/v2/our-platform/live-trading/brokerages/zerodha#07-Margin
