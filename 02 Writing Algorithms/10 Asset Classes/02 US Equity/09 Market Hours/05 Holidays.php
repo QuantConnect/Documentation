cc9a052d45a0caa0a0e9a58bbc3b4e1c9003f720
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:06:52 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/05 Holidays.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/05 Holidays.php
@@ -1,3 +1,3 @@
-<p>LEAN uses the <a target="_blank" rel='nofollow' href='https://www.nyse.com/markets/hours-calendars'>NYSE trading holidays</a>. The following table shows the dates of all the holidays:</p>
+<p>LEAN uses the <a target="_blank" rel='nofollow' href='https://www.nyse.com/markets/hours-calendars'>NYSE trading holidays</a>.</p>
 
 <?php include(DOCS_RESOURCES."/datasets/market-hours/equity/usa/generic/holidays.html"); ?>
