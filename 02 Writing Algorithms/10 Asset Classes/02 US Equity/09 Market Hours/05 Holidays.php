bb5a9705854d3542ba4f1fc3d42c48370c8b1471
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:07:34 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/05 Holidays.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/05 Holidays.php
@@ -1,3 +1,3 @@
-<p>LEAN uses the <a target="_blank" rel='nofollow' href='https://www.nyse.com/markets/hours-calendars'>NYSE trading holidays</a>.</p>
+<p>LEAN uses the <a target="_blank" rel='nofollow' href='https://www.nyse.com/markets/hours-calendars'>trading holidays</a> from the NYSE website.</p>
 
 <?php include(DOCS_RESOURCES."/datasets/market-hours/equity/usa/generic/holidays.html"); ?>
