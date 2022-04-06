182391d27e5bb784727db1dc48e49d829cd5b970
Web Editor <web.editor@quantconnect.com>
2022-04-06 17:16:51 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/10 Delistings.php
+++ 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/10 Delistings.php
@@ -1,4 +1,5 @@
 -For contract expiry
+<br>-Positions are liquidated when contract expires
 <br>-Example of detecting delisting in OnData
 
 <div data-tree='QuantConnect.Data.Market.Delisting'></div>
