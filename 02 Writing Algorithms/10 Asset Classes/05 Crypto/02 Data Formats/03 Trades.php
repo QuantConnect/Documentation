929966c9ea2b26cf98d73d7c6291839ebd34d479
Web Editor <web.editor@quantconnect.com>
2022-04-06 16:40:43 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/05 Crypto/02 Data Formats/03 Trades.php
+++ 02 Writing Algorithms/10 Asset Classes/05 Crypto/02 Data Formats/03 Trades.php
@@ -1,2 +1,4 @@
 <div>TradeBars</div><div>-Definition: individual trades from the exchanges consolidated into price bars<br></div><div>- Add image from Handling Data docs<br></div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, time time-slice may not have any data. <br><div>-Prices from tradebars are used fill trades in backtests (hour, daily resolutions)</div>
+
+<br>## Copy from US Equity
