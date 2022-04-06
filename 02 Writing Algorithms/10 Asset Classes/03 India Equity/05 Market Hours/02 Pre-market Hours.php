38650cba31143e8958d587a7192726a86dfc1231
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:08:02 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/03 India Equity/05 Market Hours/02 Pre-market Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/03 India Equity/05 Market Hours/02 Pre-market Hours.php
@@ -1,2 +1,1 @@
-<p>The following table shows the pre-market hours for the India Equity market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/equity/india/generic/pre-market-hours.html"); ?>
