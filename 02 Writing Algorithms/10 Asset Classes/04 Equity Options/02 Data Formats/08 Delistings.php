c4a8acbb8aa95bb85e53b9c34b6047c4833b6d1d
Web Editor <web.editor@quantconnect.com>
2022-04-06 16:36:22 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/04 Equity Options/02 Data Formats/08 Delistings.php
+++ 02 Writing Algorithms/10 Asset Classes/04 Equity Options/02 Data Formats/08 Delistings.php
@@ -1,1 +1,6 @@
-## For contract expiration
+- For contract expiration
+<br>- Contracts are liquidated on expiration
+<br>- Show example of accessing delistings in OnData
+
+
+<div data-tree="QuantConnect.Data.Market.OpenInterest"></div>
