e5500a1ab18ac97f548be8a57bb46dadc4866fbf
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:08:54 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/02 Pre-market Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/02 Pre-market Hours.php
@@ -1,3 +1,1 @@
-
-<p>The following table shows the pre-market hours for the US Option market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/option/usa/generic/pre-market-hours.html"); ?>
