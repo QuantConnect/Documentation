bb00091137ff39033567a67cc03404160034f2b3
Web Editor <web.editor@quantconnect.com>
2022-04-06 19:41:04 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/10 Index Options/02 Data Formats/06 Open Interest.php
+++ 02 Writing Algorithms/10 Asset Classes/10 Index Options/02 Data Formats/06 Open Interest.php
@@ -1,1 +1,1 @@
-<div>-Definition: Number of outstanding contracts that haven't been settled<br></div><div>-A measure of investor interest.</div><div>&nbsp;&nbsp;&nbsp; - Provides insight into the liquidity of an option<br></div><div>-Calculated once per day</div><div>-Used for selecting a contract</div><div><br></div><div><div>-Include a code snippet of filtering contracts by OI</div>-Link to full example of using OI to select a contract to trade</div>
+## Copy from Equity Options
