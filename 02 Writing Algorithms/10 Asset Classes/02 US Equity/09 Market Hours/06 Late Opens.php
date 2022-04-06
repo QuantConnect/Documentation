1031ebbee8eedcc0e52b7d69bb9093c2857816f1
Web Editor <web.editor@quantconnect.com>
2022-04-06 11:50:45 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/06 Late Opens.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/06 Late Opens.php
@@ -1,1 +1,1 @@
-<?php include(DOCS_RESOURCES."/datasets/market-hours/01 Equity usa generic/07 Late Opens.html"); ?>
+<?php include(DOCS_RESOURCES."/datasets/market-hours/equity/usa/generic/late-opens.html"); ?>
