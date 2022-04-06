8cf068cacb9a47479d9b9ea9a2d7d8199d67edfd
Web Editor <web.editor@quantconnect.com>
2022-04-06 12:33:41 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/05 Holidays.php
+++ 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/05 Holidays.php
@@ -1,1 +1,3 @@
+<p>The following table shows the dates of all the holidays for the Equity Options market:</p>
 
+<?php include(DOCS_RESOURCES."/datasets/market-hours/equity/usa/generic/holidays.html"); ?>
