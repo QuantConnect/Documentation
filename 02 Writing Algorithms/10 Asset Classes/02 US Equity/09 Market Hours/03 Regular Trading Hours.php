f7f67a480de7ed922dd44f21310b4c0dc9d16284
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:06:06 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/03 Regular Trading Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/09 Market Hours/03 Regular Trading Hours.php
@@ -1,2 +1,1 @@
-<p>The following table shows the regular trading hours for the US Equity market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/equity/usa/generic/market-opening-hours.html"); ?>
