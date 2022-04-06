fec8752d063dc42cfb1f68f2dae903394d5d9ec9
Web Editor <web.editor@quantconnect.com>
2022-04-06 13:09:05 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/03 Regular Trading Hours.php
+++ 02 Writing Algorithms/10 Asset Classes/04 Equity Options/04 Market Hours/03 Regular Trading Hours.php
@@ -1,2 +1,1 @@
-<p>The following table shows the regular trading hours for the US Option market:</p>
 <?php include(DOCS_RESOURCES."/datasets/market-hours/option/usa/generic/market-opening-hours.html"); ?>
