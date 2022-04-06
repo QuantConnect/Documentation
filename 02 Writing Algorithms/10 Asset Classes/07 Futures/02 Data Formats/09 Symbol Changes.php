51844433b5535edfd42b344f30ea6fe1b60563f0
Web Editor <web.editor@quantconnect.com>
2022-04-06 17:14:04 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/09 Symbol Changes.php
+++ 02 Writing Algorithms/10 Asset Classes/07 Futures/02 Data Formats/09 Symbol Changes.php
@@ -1,4 +1,5 @@
 -For contract rollover of the continuous contract
+<br>-Determined by the dataMappingMode when you call AddFuture
 <br>-Example of detecting symbol changes in OnData
 
 <div data-tree='QuantConnect.Data.Market.SymbolChangedEvent'></div>
