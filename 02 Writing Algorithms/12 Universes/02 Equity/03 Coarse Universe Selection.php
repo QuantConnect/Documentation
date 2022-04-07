4ed4360f70807564480adfa689e5cd680e0b7921
Web Editor <web.editor@quantconnect.com>
2022-04-07 13:46:28 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/12 Universes/02 Equity/03 Coarse Universe Selection.php
+++ 02 Writing Algorithms/12 Universes/02 Equity/03 Coarse Universe Selection.php
@@ -29,7 +29,7 @@ To use a coarse universe, you must request it using an $[AddUniverse(), M:QuantC
 </div>
 
 <p>
-The coarse filter function is provided a list of $[CoarseFundamental,T:QuantConnect.Data.UniverseSelection.CoarseFundamental] objects and must return a list of Symbol objects. If you don't want to make any changes to the current universe, you can return <code>Universe.Unchanged</code></p>
+The coarse filter function is provided a list of $[CoarseFundamental,T:QuantConnect.Data.UniverseSelection.CoarseFundamental] objects and must return a list of Symbol objects. If you don't want to make any changes to the current universe, you can return <code>Universe.Unchanged</code>. If you only use a coarse filter in your universe selection process, the Equities you return from the function are automatically added to your universe. You shouldn't call <code>AddEquity</code>.</p>
 
 <p>The total number of stocks in the <a href='/datasets/quantconnect-us-equity-security-master'>US Equity Security Master dataset</a> is <?php include(DOCS_RESOURCES."/kpis/us-equity-security-master-size.php");?>, but your coarse filter function won't receive all of these at one time because the US Equity Security Master dataset is free of survivorship-bias and some of the securities have delisted over time. The number of securities that are passed into your coarse filter function depends on the date of your algorithm. Currently, there are about <?php include(DOCS_RESOURCES."/kpis/coarse-universe-size.php"); ?> securities passed into your coarse filter function. The most important properties of the $[CoarseFundamental,T:QuantConnect.Data.UniverseSelection.CoarseFundamental] object are: <code>Price</code> (raw), <code>DollarVolume</code> and <code>HasFundamentaData</code>. <br></p>
 
