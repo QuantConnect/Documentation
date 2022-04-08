17596b2c358f543210c319f0e2b1a7b1263a3ac0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-04-08 15:21:02 -0600

    add default keyboard shortcuts

--- 01 Our Platform/06 Backtesting/04 Debugging/04 Control Debugger.php
+++ 01 Our Platform/06 Backtesting/04 Debugging/04 Control Debugger.php
@@ -5,6 +5,7 @@
     <tr>
       <th>Button</th>
       <th>Name</th>
+      <th>Default Keyboard Shortcut</th>
       <th>Description</th>
     </tr>
   </thead>
@@ -12,26 +13,31 @@
     <tr>
       <td><img src="https://cdn.quantconnect.com/i/tu/debugger-continue-button.png"></td>
       <td>Continue</td>
+      <td></td>
       <td>Continue execution until the next breakpoint</td>
     </tr>
     <tr>
       <td><img src="https://cdn.quantconnect.com/i/tu/debugger-stop-over-button.png"></td>
       <td>Step Over</td>
+      <td><span class='key-combinations'>Alt+F10</span></td>
       <td>Step to the next line of code in the current or parent scope</td>
     </tr>
     <tr>
       <td><img src="https://cdn.quantconnect.com/i/tu/debugger-step-into-button.png"></td>
       <td>Step Into</td>
+      <td><span class='key-combinations'>Alt+F11</span></td>
       <td>Step into the definition of the function call on the current line</td>
     </tr>
     <tr>
       <td><img src="https://cdn.quantconnect.com/i/tu/debugger-restart-button.png"></td>
       <td>Restart</td>
+      <td><span class='key-combinations'>Shift+F11</span></td>
       <td>Restart the debugger</td>
     </tr>
     <tr>
       <td><img src="https://cdn.quantconnect.com/i/tu/debugger-disconnect-button.png"></td>
       <td>Disconnect</td>
+      <td><span class='key-combinations'>Shift+F5</span></td>
       <td>Exit the debugger</td>
     </tr>
   </tbody>
