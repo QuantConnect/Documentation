1b30017172049b6d1c9b3dff7dcfd3ede023c43b
Web Editor <web.editor@quantconnect.com>
2022-04-12 17:32:34 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/03 Count Consolidators/03 Updating Aggregated Bars.php
+++ 02 Writing Algorithms/18 Consolidating Data/03 Count Consolidators/03 Updating Aggregated Bars.php
@@ -1,1 +1,6 @@
+<p>You can update the consolidator manually or automatically.</p><p>- To manually update a consolidator, pass a Tick, TradeBar, or QuoteBar (depending on the type of consolidator) to it's `Update` method.</p>
 
+<div class="section-example-container">
+<pre class="python">self.consolidator.Update(data)</pre>
+<pre class="csharp">_consolidator.Update(data);</pre>
+</div>
