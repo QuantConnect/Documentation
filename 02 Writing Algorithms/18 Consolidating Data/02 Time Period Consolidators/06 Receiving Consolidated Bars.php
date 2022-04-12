4adfe6b38af0fc196a9af8832a7ab2fa21b797b8
Web Editor <web.editor@quantconnect.com>
2022-04-12 17:24:09 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/02 Time Period Consolidators/06 Receiving Consolidated Bars.php
+++ 02 Writing Algorithms/18 Consolidating Data/02 Time Period Consolidators/06 Receiving Consolidated Bars.php
@@ -1,1 +1,3 @@
 
+
+<p>The consolidation handler accepts two arguments, the sender (consolidator object) and the consolidated bar. The consolidated bars are automatically passed to your consolidation handler when the consolidated bar closes.<br>
