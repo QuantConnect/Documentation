0980486645d7df0f6bc21fb569f653e8995ef50f
Web Editor <web.editor@quantconnect.com>
2022-04-12 17:32:49 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/03 Count Consolidators/03 Updating Aggregated Bars.php
+++ 02 Writing Algorithms/18 Consolidating Data/03 Count Consolidators/03 Updating Aggregated Bars.php
@@ -1,6 +1,36 @@
+
 <p>You can update the consolidator manually or automatically.</p><p>- To manually update a consolidator, pass a Tick, TradeBar, or QuoteBar (depending on the type of consolidator) to it's `Update` method.</p>
 
 <div class="section-example-container">
 <pre class="python">self.consolidator.Update(data)</pre>
 <pre class="csharp">_consolidator.Update(data);</pre>
+</div>
+
+<p>- To automatically update a consolidator with a data subscription, create the subscription and then register the consolidator with the Subscription Manager.</p>
+
+<div class="section-example-container">
+<pre class="python">def Initialize(self):
+    # Make sure you have the data you need
+    self.symbol = self.AddEquity("QQQ", Resolution.Minute).Symbol
+
+    # Create consolidator you need and attach event handler
+    self.consolidator = TradeBarConsolidator(30)
+    self.consolidator.DataConsolidated += self.consolidation_handler
+
+    # Register consolidator to get automatically updated with minute data
+    self.SubscriptionManager.AddConsolidator(self.symbol, self.consolidation_handler)
+</pre>
+<pre class="csharp">public override void Initialize()
+{ 
+     // Make sure you have the data you need
+    _symbol = AddEquity("QQQ", Resolution.Minute).Symbol;
+
+    // Create consolidator you need and attach event handler
+    _consolidator = new TradeBarConsolidator(30);
+    _consolidator.DataConsolidated += ConsolidationHandler;
+
+    // Register consolidator to get automatically updated with minute data
+    SubscriptionManager.AddConsolidator(_symbol, _consolidator);
+}
+</pre>
 </div>
