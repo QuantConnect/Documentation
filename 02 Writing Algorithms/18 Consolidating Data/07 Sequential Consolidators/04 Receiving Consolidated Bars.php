cc9eb1f9f2a1e6b381586baf054cdd5b4aba72b7
Web Editor <web.editor@quantconnect.com>
2022-04-12 19:47:06 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/07 Sequential Consolidators/04 Receiving Consolidated Bars.php
+++ 02 Writing Algorithms/18 Consolidating Data/07 Sequential Consolidators/04 Receiving Consolidated Bars.php
@@ -1,1 +1,2 @@
-
+The consolidated bars are automatically passed to your consolidation handler when the second internal consolidator in the sequential consolidator closes.
+<br>## Import from Time Period Consolidators 
