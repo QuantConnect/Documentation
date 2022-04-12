c1735d733c464c7dc3dc24ee787b8fc05958a4bc
Web Editor <web.editor@quantconnect.com>
2022-04-12 17:49:16 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/04 Mixed Mode Consolidators/04 Receiving Consolidated Bars.php
+++ 02 Writing Algorithms/18 Consolidating Data/04 Mixed Mode Consolidators/04 Receiving Consolidated Bars.php
@@ -1,1 +1,3 @@
+The consolidated bars are automatically passed to your consolidation handler when the max count or time period is reached, whichever comes first.
 
+<br>## Import from Time Period Consolidators 
