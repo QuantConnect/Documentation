b64770a7662846b9b2608c8366672b16e4363edf
Web Editor <web.editor@quantconnect.com>
2022-04-13 14:20:11 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/05 Removing Indicator Consolidators.php
+++ 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/05 Removing Indicator Consolidators.php
@@ -1,8 +1,7 @@
 What: 
-<br>- If your algorithm has a dynamic universe, remove the consolidator that updates the indicator when you remove securities from your universe.
+<br>- If your algorithm has a dynamic universe, when you remove securities from your universe, remove the consolidator that updates the security's indicator.
 <br><br>Why:
 <br>Remove the consolidators in dynamic universes so that the list of consolidators doesn't compound over time and slow down your algorithm.
-
 <br><br>How:
 <br>-To remove the consolidator that's updating an indicator, you need to save a reference to the consolidator.
 <br>- To save a reference to the consolidator, either:
@@ -19,7 +18,6 @@ self.RegisterIndicator(self.symbol, self.indicator, self.consolidator)</pre>
 self.consolidator += self.consolidation_handler # Update the indicator in the consolidation handler</pre>
 </div>
 <br>-Now that you have a reference to the consolidator, you can remove it
-
 <div class="section-example-container">
 <pre class="csharp"></pre>
 <pre class="python">algorithm.SubscriptionManager.RemoveConsolidator(self.symbol, self.consolidator)</pre>
