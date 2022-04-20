7aaf5f1c7063b1023ff439f2a7ea066ec48a6027
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-04-20 12:12:44 -0600

    Update brokerage-restrictions.php

--- Resources/order-types/brokerage-restrictions.php
+++ Resources/order-types/brokerage-restrictions.php
@@ -1,5 +1,5 @@
 <?php
 $getBrokerageRestrictionsText = function($orderType) {
-    echo "<p>To check if the brokerage you're using has any special requirements for {$orderType}, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>documentation for your brokerage</a>.</p>";
+    echo "<p>To check if the brokerage you're using has any special requirements for {$orderType}, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>integration documentation for your brokerage</a>.</p>";
 }
 ?>
