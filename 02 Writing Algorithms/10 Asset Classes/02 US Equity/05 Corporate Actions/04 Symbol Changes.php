b23ffd214d4a1ae8a9debd18fb6f895e0d8a20bf
Web Editor <web.editor@quantconnect.com>
2022-04-25 11:13:28 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/05 Corporate Actions/04 Symbol Changes.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/05 Corporate Actions/04 Symbol Changes.php
@@ -1,4 +1,5 @@
 <div>-SymbolChangedEvents provides notice of new ticker names for stocks, or mergers of two tickers into one</div><div>&nbsp;&nbsp;&nbsp; - Example: AOL TimeWarner became TimeWarner and changed their symbol from AOL to TWX.</div><div>-Show properties of SymbolChangedEvent class</div><div>&nbsp;&nbsp;&nbsp; - OldSymbol, NewSymbol</div><div>-accessed in the time-slice<br></div><div>-The benefit of our Symbol class is that ticker changes don't really matter, we're not passing strings of company ticker<br></div><div>-Full example: There are none in the repo (add example?)<br></div><div>-Data saved in map files</div><div></div>
+<br>-Open Orders get canceled in case of a SymbolChanged event. In this case, we recommend using the OnOrderEvent handler to get the order quantity and the symbol associated with the canceled order and resubmitting a new order.
 
 
 <div class="section-example-container">
