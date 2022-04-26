8449125a06e8ff2e7fc468cd8a5589bab9b8c0ef
LouisSzeto <hke0073@icloud.com>
2022-04-26 20:36:18 +0800

    Script for Order Error Code

---
+++ order-error-code-generator.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+
+raw = urlopen("https://raw.githubusercontent.com/QuantConnect/Lean/master/Common/Orders/OrderResponseErrorCode.cs").read().decode("utf-8").split('\n')
+
+description = ""
+code = ""
+enum = ""
+html = '''<p>When an order fails to process it returns with a negative order-id. These error codes mean different things as described in the table below.</p>
+<table>
+<thead>
+<th style="width: 5%;">Enum</th><th style="width: 20%;">Error Code</th><th style="width: 75%;">Description</th>
+</thead>
+<tbody>
+'''
+
+for line in raw:
+    if "        /// " in line and "<" not in line:
+        description = line.split("        /// ")[-1]
+        
+    if " = " in line:
+        item = line.split(" = ")
+        code = item[0].split(" ")[-1]
+        enum = item[-1].split(",")[0]
+        
+    if line == "" and code != "":
+        html += f'''<tr><td>{enum}</td><td>{code}</td><td>{description}</td></tr>
+'''
+
+html += """</tbody>
+<table>"""
+
+with open("02 Writing Algorithms/22 Trading and Orders/10 Order Errors/19 Order Error Code Reference.html", "w", encoding="utf-8") as file:
+    file.write(html)
