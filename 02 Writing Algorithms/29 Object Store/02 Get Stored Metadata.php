f9b9177658d8a68aa54266771d977735f2bd4c3e
Web Editor <web.editor@quantconnect.com>
2022-05-09 18:00:52 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/29 Object Store/02 Get Stored Metadata.php
+++ 02 Writing Algorithms/29 Object Store/02 Get Stored Metadata.php
@@ -1,1 +1,10 @@
 # Import from https://www.quantconnect.com/docs/v2/research-environment/object-store/storing-data
+
+<br><br>-What is gathering keys:
+<br>-Gathering all of the keys stored in the ObjectStore key-value dictionary
+<br>-Why gather keys:
+<br>&nbsp;&nbsp;&nbsp;&nbsp;- So you can see what is available in the ObjectStore
+<br>&nbsp;&nbsp;&nbsp;&nbsp;- So you don't over-write a key-value pair in the OS
+<br>-How(?):
+ <br>&nbsp;&nbsp;&nbsp;&nbsp;-can be gathered by using the GetEnumerator method
+<br>         keys = [str(j).split(',')[0][1:] for _, j in enumerate(self.ObjectStore.GetEnumerator())]
