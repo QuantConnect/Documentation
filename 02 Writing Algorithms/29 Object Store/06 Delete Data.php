7ca49c1b16b6bfdbdd4b5a71ecc5e42f3ce46533
Web Editor <web.editor@quantconnect.com>
2022-05-09 18:04:40 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/29 Object Store/06 Delete Data.php
+++ 02 Writing Algorithms/29 Object Store/06 Delete Data.php
@@ -1,1 +1,12 @@
 # Import from https://www.quantconnect.com/docs/v2/research-environment/object-store/storing-data
+
+
+<br>-What is deleting data:
+<br>&nbsp;&nbsp;&nbsp;&nbsp;     - Deleting a key-value pair in the ObjectStore by providing the object's key
+<br>-Why delete data:
+<br>&nbsp;&nbsp;&nbsp;&nbsp;      - To stay within organization's limits (OS storage usage is displayed in Algorithm Lab)
+<br>&nbsp;&nbsp;&nbsp;&nbsp;      - To remove unneeded objects that are in the ObjectStore
+<br>-How(?):
+<br>&nbsp;&nbsp;&nbsp;&nbsp;      -Use Delete method or delete the project
+<br>&nbsp;&nbsp;&nbsp;&nbsp;      -Need to provide the key to delete
+<br>&nbsp;&nbsp;&nbsp;&nbsp;      -<snippet example of using Delete>
