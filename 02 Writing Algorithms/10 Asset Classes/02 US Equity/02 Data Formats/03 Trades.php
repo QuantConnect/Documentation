8224663ea14276ea74b9c5cef02003108dd9c3a3
Web Editor <web.editor@quantconnect.com>
2022-05-17 11:20:13 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/02 Data Formats/03 Trades.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/02 Data Formats/03 Trades.php
@@ -4,8 +4,10 @@ official auction open (close) price. If there is no official auction
 price received before we must emit the bar, we use the first/last trade</div><div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, the time-slice may not have any data. <br></div><div>-Prices from tradebars are used fill trades in backtests (hour, daily resolutions)</div><div>&nbsp;&nbsp;&nbsp; - Opening &amp; closing prices are updated to the official opening/closing auction prices,&nbsp; used to fill MOO and MOC orders<br></div>
 
+<img src="https://cdn.quantconnect.com/docs/i/dataformat-tradebar.png" class="img-responsive">
 
 <div data-tree="QuantConnect.Data.Market.TradeBar"></div>
 
 <br>
 -Add snippet of accessing TradeBars in OnData
+<br>-The examples on this page should check if the slice contains the data before indexing it. (https://github.com/QuantConnect/Documentation/issues/204).
