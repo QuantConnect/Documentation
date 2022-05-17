bb954371966f884b0362ccd5c88c14994801e3af
Web Editor <web.editor@quantconnect.com>
2022-05-17 11:21:44 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/02 Data Formats/04 Quotes.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/02 Data Formats/04 Quotes.php
@@ -1,5 +1,7 @@
-<div>QuoteBars</div><div>-Definition: built by consolidating the bid and ask ticks from the exchanges into bars</div><div>- Add image from Handling Data docs</div><div>-Properties:</div><div>&nbsp;&nbsp;&nbsp; - Open, High, Low, Close, Bid, Ask, LastBidSize, and LastAskSize</div><div>&nbsp;&nbsp;&nbsp; - Bid and the Ask properties are Bar objects</div><div>&nbsp;&nbsp;&nbsp; - The QuoteBar Open, High, Low, and Close properties values are the mean of the respective Bid and Ask properties.</div><div>-If there is little trading, or you are in the same time loop as when 
+<div>QuoteBars</div><div>-Definition: built by consolidating the bid and ask ticks from the exchanges into bars</div><div>- Add image from Handling Data docs</div><div>-Properties:</div><div>&nbsp;&nbsp;&nbsp; - Open, High, Low, Close, Bid, Ask, LastBidSize, and LastAskSize</div><div>&nbsp;&nbsp;&nbsp; - Bid and the Ask properties are Bar objects</div><div>&nbsp;&nbsp;&nbsp; - The QuoteBar Open, High, Low, and Close properties values are the mean of the respective Bid and Ask properties. (The Low of a QuoteBar is not always the mean. If there is no data for the bid/ask at the current slice, it selects either the AskLow or the BidLow) </div><div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, the time-slice may not have any data.</div><div>-Used to fill trades for second &amp; minute resolutions in backtesting</div><div>&nbsp;&nbsp;&nbsp;&nbsp; - Makes trades realistic by accounting for spread costs</div><div>-Quotes are only provided for tick-minute resolutions<br></div><div></div>
+
+<img src="https://cdn.quantconnect.com/docs/i/dataformat-quotebar.png" class="img-responsive">
 
 <div data-tree="QuantConnect.Data.Market.QuoteBar"></div>
 
