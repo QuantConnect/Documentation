5445b050d41d8acf74c6e617979831acd83ea346
Web Editor <web.editor@quantconnect.com>
2022-05-17 12:52:00 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/01 Requesting Data/09 Data Normalization.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/01 Requesting Data/09 Data Normalization.php
@@ -1,5 +1,6 @@
 What is data normalization:
 <br>- A technique of how the data is adjusted for corporate actions.
+<br>-By default equity data in QuantConnect is Split and Dividend adjusted backwards in time to give smooth continuous prices. This allows easy use for indicators. Some algorithms need raw or partially adjusted price data. You can control this with the { SetDataNormalizationMode()} method. The { DataNormalizationMode} enum has the values Adjusted (default), Raw, SplitAdjusted, and TotalReturn. When data is set to Raw mode the dividends are paid as cash into your algorithm, and the splits are directly applied to your holding quantity. 
 
 <br><br>
 What data normalization options are available:
