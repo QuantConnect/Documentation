2d3225bff59319df260a5cd457dbb5bb0b7084e1
Web Editor <web.editor@quantconnect.com>
2022-05-17 11:25:23 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/10 Asset Classes/02 US Equity/05 Corporate Actions/05 Delistings.php
+++ 02 Writing Algorithms/10 Asset Classes/02 US Equity/05 Corporate Actions/05 Delistings.php
@@ -1,3 +1,4 @@
+-{ Delisting} events provide notice that an asset is no longer trading on the exchange. A delisting warning is issued on the final trading day for a stock delisting event to give your algorithm time to gracefully exit out of positions before forced termination. 
 <div>-Our data is survivorship-bias free<br></div><div>-Check if there are delistings in the current slice with data.Delistings</div><div>&nbsp;&nbsp;&nbsp; - returns key-value pairs</div><div>&nbsp;&nbsp;&nbsp; - value has a `.Value` that you can check against&nbsp; the DelistingType enum<br></div><div>- &lt;security&gt;.IsDelisted<br></div><div>-DelistingType (.Warning &amp; .Delisted)</div><div>-Accessed in the time-slice<br></div><div>-Example: DelistingEventsAlgorithm.py</div><div>-"We could also make a short tutorial on delisting. Some people save the symbols in list (to make history requests), they should remove the security from that list too."<br></div><div></div>
 
 <div class="section-example-container">
@@ -8,5 +9,6 @@
         <pre class="python">if data.ContainsKey(self.msft):
     delisting = data.Delistings[self.msft]</pre>
     </div>
+-^ Add SetOrderTicket method and add the Ticket member to get the order ticket that liquidates the position
 
 <div data-tree="QuantConnect.Data.Market.Delisting"></div>
