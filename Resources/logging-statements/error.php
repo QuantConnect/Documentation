1e9ae7a0195c2ae7235719c55b195559948de368
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:40:40 -0600

    Update error.php

--- Resources/logging-statements/error.php
+++ Resources/logging-statements/error.php
@@ -1,1 +1,7 @@
 
+<p>Error statements are the same as log statements, but <code>Error</code> statements are displayed in red text in the Cloud Terminal. Use these statements when you want to give the most attention to a message in the Cloud Terminal. Error statements are rate-limited like debug statements.<br></p>
+
+<div class="section-example-container">
+    <pre class="csharp">Error("My error message");</pre>
+    <pre class="python">self.Error("My error message")</pre>
+</div>
