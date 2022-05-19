64dbe54bf8d7670062d99adcdd395a524ee8c10f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:39:20 -0600

    Update debug.php

--- Resources/logging-statements/debug.php
+++ Resources/logging-statements/debug.php
@@ -1,1 +1,1 @@
-
+<p>Debug statements are the same as log statements, but <code>Debug</code> statements are orange in the Cloud Terminal. Use these statements when you want to give more attention to a message in the Cloud Terminal. Debug messages are capped at 200 characters in length. If you send multiple debug statements within one second, your messages are rate-limited to avoid crashing your browser.<br></p>
