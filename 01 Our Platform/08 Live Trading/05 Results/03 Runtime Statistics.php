69f057154faa08755896b754c776e29c92aa2636
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 12:15:58 -0600

    Update 03 Runtime Statistics.php

--- 01 Our Platform/08 Live Trading/05 Results/03 Runtime Statistics.php
+++ 01 Our Platform/08 Live Trading/05 Results/03 Runtime Statistics.php
@@ -4,52 +4,9 @@
 
 <p>The following table shows the performance statistics that display by default:</p>
 
-<?php
-include(DOCS_RESOURCES."/glossary.php");
-echo "
-<table class='qc-table table'>
-  <thead>
-    <tr>
-      <th style='width: 25%'>Statistic</th>
-      <th style='width: 75%'>Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>PSR</td>
-      <td>{$definitionByTerm['probabilistic sharpe ratio (PSR)']}</td>
-    </tr>
-    <tr>
-      <td>Unrealized</td>
-      <td>{$definitionByTerm['unrealized']}</td>
-    </tr>
-    <tr>
-      <td>Fees</td>
-      <td>{$definitionByTerm['total fees']}</td>
-    </tr>
-    <tr>
-      <td>Net Profit</td>
-      <td>{$definitionByTerm['net profit']['dollar-value']}</td>
-    </tr>
-    <tr>
-      <td>Return</td>
-      <td>{$definitionByTerm['net profit']['percent']}</td>
-    </tr>
-    <tr>
-      <td>Equity</td>
-      <td>{$definitionByTerm['equity']}</td>
-    </tr>
-    <tr>
-      <td>Holdings</td>
-      <td>{$definitionByTerm['holdings']}</td>
-    </tr>
-    <tr>
-      <td>Volume</td>
-      <td>{$definitionByTerm['volume']}</td>
-    </tr>
-  </tbody>
-</table>";
-
+<?php 
+include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
+$getRuntimeStatisticsTable(true);
 
 echo file_get_contents(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.html"); 
 ?>
