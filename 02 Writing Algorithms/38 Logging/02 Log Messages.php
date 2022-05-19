c28803781ecaec71d5683344ed2d65ed2418277c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:52:17 -0600

    Update 02 Log Messages.php

--- 02 Writing Algorithms/38 Logging/02 Log Messages.php
+++ 02 Writing Algorithms/38 Logging/02 Log Messages.php
@@ -2,6 +2,3 @@
 include(DOCS_RESOURCES."/logging-statements/log.php");
 $getLogText(false);
 ?>
-
-Add "It's a good practice to add debug/log statements to algorithms running in live mode so that we can understand its behavior and keep records to compare against backtests." Some people deploy silent algorithms in live mode and when it doesn't work as expected, we don't have information to evaluate the problem. Maybe this should go as a Best Practices page in Live Trading too?
--OnEndOfAlgorithm ("intended for closing out logs")
