bf0231c95773f952236198b8d5d1bc5a02a8fdf6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 12:20:11 -0600

    Update 03 Runtime Statistics.php

--- 01 Our Platform/06 Backtesting/03 Results/03 Runtime Statistics.php
+++ 01 Our Platform/06 Backtesting/03 Results/03 Runtime Statistics.php
@@ -2,57 +2,9 @@
 
 <img class='docs-image' src="https://cdn.quantconnect.com/i/tu/runtime-statistics-1.png">
 
-<p>The following table describes the runtime statistics:</p>
-
-<?php
-include(DOCS_RESOURCES."/glossary.php");
-echo "
-<table class='qc-table table'>
-  <thead>
-    <tr>
-      <th style='width: 25%'>Statistic</th>
-      <th style='width: 75%'>Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>PSR</td>
-      <td>{$definitionByTerm['probabilistic sharpe ratio (PSR)']}</td>
-    </tr>
-    <tr>
-      <td>Unrealized</td>
-      <td>{$definitionByTerm['unrealized']}</td>
-    </tr>
-    <tr>
-      <td>Fees</td>
-      <td>{$definitionByTerm['total fees']}</td>
-    </tr>
-    <tr>
-      <td>Net Profit</td>
-      <td>{$definitionByTerm['net profit']['dollar-value']}</td>
-    </tr>
-    <tr>
-      <td>Return</td>
-      <td>{$definitionByTerm['net profit']['percent']}</td>
-    </tr>
-    <tr>
-      <td>Equity</td>
-      <td>{$definitionByTerm['equity']}</td>
-    </tr>
-    <tr>
-      <td>Holdings</td>
-      <td>{$definitionByTerm['holdings']}</td>
-    </tr>
-    <tr>
-      <td>Volume</td>
-      <td>{$definitionByTerm['volume']}</td>
-    </tr>
-    <tr>
-      <td>Capacity</td>
-      <td>{$definitionByTerm['capacity']}</td>
-    </tr>
-  </tbody>
-</table>";
+<?php 
+include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
+$getRuntimeStatisticsTable(false);
 
 echo file_get_contents(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.html"); 
 ?>
