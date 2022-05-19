f1831f606dd7a5f903350e66afc660d181571966
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 10:40:38 -0600

    Update 05 Telegram.php

--- 02 Writing Algorithms/40 Live Notifications/05 Telegram.php
+++ 02 Writing Algorithms/40 Live Notifications/05 Telegram.php
@@ -1,2 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-intro.html"); ?>
 <?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-setup.html"); ?>
+<p>The <code>Id</code> argument can be your Telegram user Id or group Id. <?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
