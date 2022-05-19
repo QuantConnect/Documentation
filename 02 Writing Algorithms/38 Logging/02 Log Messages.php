1125619965561eabae3cd180630c05b6bd4720d1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:45:23 -0600

    Update 02 Log Messages.php

--- 02 Writing Algorithms/38 Logging/02 Log Messages.php
+++ 02 Writing Algorithms/38 Logging/02 Log Messages.php
@@ -3,4 +3,5 @@ include(DOCS_RESOURCES."/logging-statements/log.php");
 $getLogText(false);
 ?>
 
-## TODO: Add "It's a good practice to add debug/log statements to algorithms running in live mode so that we can understand its behavior and keep records to compare against backtests." Some people deploy silent algorithms in live mode and when it doesn't work as expected, we don't have information to evaluate the problem. Maybe this should go as a Best Practices page in Live Trading too?</div><div>-OnEndOfAlgorithm ("intended for closing out logs")</div><div><br></div><div><br></div><div>-- Add runtime statistics (you're logging in the banner)<br></div>
+Add "It's a good practice to add debug/log statements to algorithms running in live mode so that we can understand its behavior and keep records to compare against backtests." Some people deploy silent algorithms in live mode and when it doesn't work as expected, we don't have information to evaluate the problem. Maybe this should go as a Best Practices page in Live Trading too?
+-OnEndOfAlgorithm ("intended for closing out logs")
