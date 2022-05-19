50e0188ee0b6a436cbda8959f7ebd24b51a5232a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:36:52 -0600

    Update log.php

--- Resources/logging-statements/log.php
+++ Resources/logging-statements/log.php
@@ -1,1 +1,26 @@
+<?php
+
+$getLogText = function($isOnOurPlatform) {
+    $result = "<p>Log statements are added to the log file while your algorithm continues executing. Logging dataset information is not permitted. Use <code>Log</code> statements to debug your backtests and live trading algorithms. ";
+    if ($isOnOurPlatform) {
+        $result .= "L";
+    }
+    else
+    {
+        $result .= "If you execute algorithm in QC Cloud, l" ;
+    }
+    $result .= "og length is <a href='/docs/v2/our-platform/organizations/resources#09-Log-Quotas'>capped by organization tier</a>. If your organization hits the daily limit, <a href="/contact">contact us</a>. If you log the same content multiple times, only the first instance is added to the log file. Add a timestamp to your log messages to bypass this rate-limiting.</p>";
+     
+    $result .= "<div class='section-example-container'>
+                    <pre class='csharp'>Log(\"My log message\");</pre>
+                    <pre class='python'>self.Log(\"My log message\")</pre>
+                </div>";
+    echo $result;
+}
+
+?>
+
+
+
+
 
