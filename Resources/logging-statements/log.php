ced12273e4e45626962ddce580a2f97631d0508e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-19 11:56:27 -0600

    Update log.php

--- Resources/logging-statements/log.php
+++ Resources/logging-statements/log.php
@@ -8,7 +8,7 @@ $getLogText = function($isOnOurPlatform) {
     }
     else
     {
-        $result .= "If you execute algorithm in QC Cloud, l" ;
+        $result .= "If you execute algorithms in QC Cloud, l" ;
     }
     $result .= "og length is <a href='/docs/v2/our-platform/organizations/resources#09-Log-Quotas'>capped by organization tier</a>. If your organization hits the daily limit, <a href=\"/contact\">contact us</a>.</p> <p>If you log the same content multiple times, only the first instance is added to the log file. To bypass this rate-limiting, add a timestamp to your log messages.</p> <p>If you need to record the algorithm state when the algorithm finishes executing, add log statements to the <code>OnEndOfAlgorithm</code> event handler.</p>";
      
