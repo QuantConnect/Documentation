9b05d52f6ffe8b2cd6964dd15c6e0a0c31ac600b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 17:34:52 -0600

    Update and rename equal-weighting-pcm-arguments.html to equal-weighting-pcm-arguments.php

--- Resources/algorithm-framework/equal-weighting-pcm-arguments.php
+++ Resources/algorithm-framework/equal-weighting-pcm-arguments.php
@@ -1,6 +1,10 @@
-<p>The following table describes the arguments the model accepts:</p>
+<?php
 
-<table class="qc-table table">
+$getPCMArgumentText = function($supportsPortfolioBias) {
+    $result = "<p>The following table describes the arguments the model accepts:</p>";
+	
+    $result .= "
+<table class=\"qc-table table\">
     <thead>
         <tr>
             <th>Argument</th>
@@ -11,21 +15,29 @@
     </thead>
     <tbody>
         <tr>
-            <td><code class="python">rebalance</code><code class="csharp">resolution</code></td>
-	    <td><code class="csharp">Resolution</code>
-                 <span class="python">Any of the following types:
+            <td><code class=\"python\">rebalance</code><code class=\"csharp\">resolution</code></td>
+	    <td><code class=\"csharp\">Resolution</code>
+                 <span class=\"python\">Any of the following types:
                      <ul>
                          <li><code>timedelta</code></li>
                          <li><code>Resolution</code></li>
-                         <li><a href="/docs/v2/writing-algorithms/scheduled-events#04-Date-Rules">DateRules</a></li>
+                         <li><a href=\"/docs/v2/writing-algorithms/scheduled-events#04-Date-Rules\">DateRules</a></li>
                          <li><code>None</code></li>
                          <li>A function</li>
                      </ul>
                  </span>
             </td>
-            <td><span class="csharp">Rebalancing frequency</span><span class="python">Rebalancing parameter. If it's a <code>timedelta</code>, <code>DateRules</code> or <code>Resolution</code>, it's converted into a function. If it's <code>None</code>, it's ignored. The function returns the next expected rebalance time for a given algorithm UTC DateTime. The function returns </span><span class="python"><span class="python"><code>None </code></span>if unknown, in which case the function will be called again in the
+";
+    if ($supportsPortfolioBias)
+    {
+        $result .= "
+            <td><span class=\"csharp\">Rebalancing frequency</span><span class=\"python\">Rebalancing parameter. If it's a <code>timedelta</code>, <code>DateRules</code> or <code>Resolution</code>, it's converted into a function. If it's <code>None</code>, it's ignored. The function returns the next expected rebalance time for a given algorithm UTC DateTime. The function returns </span><span class=\"python\"><span class=\"python\"><code>None </code></span>if unknown, in which case the function will be called again in the
                               next loop. If the function returns the current time, the portfolio rebalances.</span></td>
             <td><code>Resolution.Daily</code></td>
+";
+    }
+
+    $result .= "
         </tr>
         <tr>
             <td><code>portfolioBias</code></td>
@@ -36,10 +48,10 @@
     </tbody>
 </table>
 
-<p class="csharp">The following table describes the arguments that can replace the <code>rebalance</code> argument:</p>
+<p class=\"csharp\">The following table describes the arguments that can replace the <code>rebalance</code> argument:</p>
 
 
-<table class="csharp qc-table table">
+<table class=\"csharp qc-table table\">
     <thead>
         <tr>
             <th>Argument</th>
@@ -50,7 +62,7 @@
     <tbody>
         <tr>
             <td><code>rebalancingDateRules</code></td>
-	    <td><a href="/docs/v2/writing-algorithms/scheduled-events#04-Date-Rules">IDateRules</a></td>
+	    <td><a href=\"/docs/v2/writing-algorithms/scheduled-events#04-Date-Rules\">IDateRules</a></td>
             <td>The date rules used to define the next expected rebalance time</td>
         </tr>
         <tr>
@@ -70,3 +82,7 @@
         </tr>
     </tbody>
 </table>
+";
+	
+    echo $result;
+}
