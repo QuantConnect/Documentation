0fe341bdff13297d4596cfba713cb339f7d418b0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 11:56:50 -0600

    Update and rename 07 Limitations.html to 07 Quotas.php

---
+++ 02 Writing Algorithms/36 Charting/07 Quotas.php
@@ -0,0 +1,5 @@
+<p>Custom charts are limited to 4,000 data points. Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. If you exceed the quota, the following message displays:</p>
+<p><span class='error-messages'>Exceeded maximum points per chart, data skipped</span></p>
+
+<p>You can create up to 10 custom chart series per algorithm. If you exceed the quota, your algorithm stops executing and the following message displays:</p>
+<p><span class='error-messages'>Exceeded maximum series count: Each backtest can have up to 10 series in total.</span></p>
