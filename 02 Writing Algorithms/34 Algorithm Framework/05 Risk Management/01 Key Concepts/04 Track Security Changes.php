dffc4e6294d505680a749484b47e413109f4a1a0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 14:44:06 -0600

    Update 04 Track Security Changes.php

--- 02 Writing Algorithms/34 Algorithm Framework/05 Risk Management/01 Key Concepts/04 Track Security Changes.php
+++ 02 Writing Algorithms/34 Algorithm Framework/05 Risk Management/01 Key Concepts/04 Track Security Changes.php
@@ -3,5 +3,5 @@ include(DOCS_RESOURCES."/algorithm-framework/track-security-changes.php");
 $modelName = "Risk Management";
 $modelClassName = "RiskManagementModel";
 $callsBaseClass = true;
-$getTrackSecurityChangesText($modelName, $modelClassName, $callsBaseClass);
+//$getTrackSecurityChangesText($modelName, $modelClassName, $callsBaseClass);
 ?>
