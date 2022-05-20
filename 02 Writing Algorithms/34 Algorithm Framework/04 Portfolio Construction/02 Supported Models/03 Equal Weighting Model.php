6b38e378c443273049db94d9a8fa1681844d1120
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 16:46:02 -0600

    Update and rename 03 Equal Weighting Model.html to 03 Equal Weighting Model.php

---
+++ 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/03 Equal Weighting Model.php
@@ -0,0 +1,10 @@
+<p>The <code>EqualWeightingPortfolioConstructionModel</code> assigns an equal share of the portfolio to the securities with active insights. This weighting scheme is useful for universe rotation based on simple portfolio strategies.</p>
+
+<div class="section-example-container">
+	<pre class="csharp">SetPortfolioConstruction(new EqualWeightingPortfolioConstructionModel());</pre>
+	<pre class="python">self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())</pre>
+</div>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/algorithm-framework/equal-weighting-pcm-arguments.html"); 
+
+<p>To view the implementation of this model, see the <span class="csharp"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/EqualWeightingPortfolioConstructionModel.cs">LEAN GitHub repository</a></span><span class="python"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/EqualWeightingPortfolioConstructionModel.py">LEAN GitHub repository</a></span>.</p>
