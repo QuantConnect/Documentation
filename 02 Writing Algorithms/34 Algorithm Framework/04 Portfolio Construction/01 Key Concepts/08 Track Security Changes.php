af86dad99f6968151ac29b89bfe20f88801f7175
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 14:48:00 -0600

    Update and rename 08 Track Security Changes.html to 08 Track Security Changes.php

---
+++ 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/01 Key Concepts/08 Track Security Changes.php
@@ -0,0 +1,7 @@
+<?php
+include(DOCS_RESOURCES."/algorithm-framework/track-security-changes.php"); 
+$modelName = "Portfolio Construction";
+$modelClassName = "PortfolioConstructionModel";
+$callsBaseClass = true;
+$getTrackSecurityChangesText($modelName, $modelClassName, $callsBaseClass);
+?>
