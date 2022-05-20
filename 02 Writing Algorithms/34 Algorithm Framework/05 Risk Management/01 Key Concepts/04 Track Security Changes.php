b8bf3621c370e41a3a9c0504f8309435a93a5107
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 14:41:47 -0600

    Update and rename 04 Track Security Changes.html to 04 Track Security Changes.php

---
+++ 02 Writing Algorithms/34 Algorithm Framework/05 Risk Management/01 Key Concepts/04 Track Security Changes.php
@@ -0,0 +1,7 @@
+<?php
+include(DOCS_RESOURCES."/algorithm-framework/track-security-changes.php"); 
+$modelName = "Risk Management";
+$modelClassName = "RiskManagementModel";
+$callsBaseClass = true;
+$getTrackSecurityChangesText($modelName, $modelClassName, $callsBaseClass);
+?>
