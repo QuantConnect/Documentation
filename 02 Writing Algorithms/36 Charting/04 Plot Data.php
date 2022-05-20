7bb343a75fddd108d59203d4bf96296acceda6f6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-20 11:58:51 -0600

    Update 04 Plot Data.php

--- 02 Writing Algorithms/36 Charting/04 Plot Data.php
+++ 02 Writing Algorithms/36 Charting/04 Plot Data.php
@@ -1,8 +1,8 @@
 <p>To add a data point to a chart series, call the <code>Plot</code> method. If you haven't already created a chart and series with the names you pass to the <code>Plot</code> method, the chart and/or series is automatically created.</p>
 
 <div class="section-example-container">
-    <pre class="csharp">Plot("Chart name", "Series name", value);</pre>
-    <pre class="python">self.Plot("Chart name", "Series name", value)</pre>
+    <pre class="csharp">Plot("&lt;chartName&gt;", &lt;seriesName&gt;, value);</pre>
+    <pre class="python">self.Plot("&lt;chartName&gt;", &lt;seriesName&gt;, value)</pre>
 </div>
 
 <p>The <code>value</code> argument can be an integer for decimal number. If the chart is a time series, the value is added to the chart using the algorithm time as the x-coordinate.</p>
