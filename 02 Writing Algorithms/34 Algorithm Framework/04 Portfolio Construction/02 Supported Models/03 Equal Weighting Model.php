6e19079d8ee69c40a9f7b1d1ccb1f522360d97e0
Web Editor <web.editor@quantconnect.com>
2022-05-21 00:04:00 +0000

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/03 Equal Weighting Model.php
+++ 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/03 Equal Weighting Model.php
@@ -5,10 +5,23 @@
 	<pre class="python">self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())</pre>
 </div>
 
-<?php 
-include(DOCS_RESOURCES."/algorithm-framework/equal-weighting-pcm-arguments.php"); 
-$supportedPortfolioBias = true;
-$getPCMArgumentText($supportedPortfolioBias);
-?>
+<table class=\"qc-table table\">
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>Data Type</th>
+            <th>Description</th>
+            <th>Default Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code class=\"python\">rebalance</code><code class=\"csharp\">resolution</code></td>
+	    <td><code>Resolution</code></td>
+	    <td>Rebalancing frequency</td>
+            <td><code>Resolution.Daily</code></td>
+        </tr>
+    </tbody>
+</table>
 
 <p>To view the implementation of this model, see the <span class="csharp"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/EqualWeightingPortfolioConstructionModel.cs">LEAN GitHub repository</a></span><span class="python"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/EqualWeightingPortfolioConstructionModel.py">LEAN GitHub repository</a></span>.</p>
