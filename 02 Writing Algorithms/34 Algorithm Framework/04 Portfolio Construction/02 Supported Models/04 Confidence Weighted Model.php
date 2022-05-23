28c42dcac06a2a6b5cc0e5ab8d02dc6b309f0588
Web Editor <web.editor@quantconnect.com>
2022-05-23 18:28:02 +0000

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/04 Confidence Weighted Model.php
+++ 02 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/04 Confidence Weighted Model.php
@@ -31,4 +31,6 @@
         </tr>
     </tbody>
 </table>
+<p>This model supports other data types for the rebalancing frequency argument. For more information about the supported types, see <a href='/docs/v2/writing-algorithms/algorithm-framework/portfolio-construction/key-concepts#06-Rebalance-Frequency'>Rebalance Frequency</a>.</p>
+
 <p>To view the implementation of this model, see the <span class='csharp'><a target="_blank" rel='nofollow' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/ConfidenceWeightedPortfolioConstructionModel.cs'>LEAN GitHub repository</a></span><span class='python'><a target="_blank" rel='nofollow' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/ConfidenceWeightedPortfolioConstructionModel.py'>LEAN GitHub repository</a></span>.</p>
