07486f42a7fff49ded16baaefbb5ffdad41017a8
Web Editor <web.editor@quantconnect.com>
2022-05-25 18:27:49 +0000

    Automated push from server made by Alexandre Catarino

--- 02 Writing Algorithms/22 Trading and Orders/00 Key Concepts/04 Symbol Properties.php
+++ 02 Writing Algorithms/22 Trading and Orders/00 Key Concepts/04 Symbol Properties.php
@@ -1,7 +1,6 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/securities/symbolproperties.html"); ?>
-
 <p>
-
+The <code>SymbolProperties</code> is a property of the <code>Security</code> object that provides three important values for calculating the order quantity: <code>LotSize</code>, <code>MinimumOrderSize</code>, and <code>MinimumPriceVariation</code>:
 </p>
 
 <div class="section-example-container">
@@ -10,10 +9,12 @@ var lotSize = symbolProperties.LotSize;
 var minimumOrderSize = symbolProperties.MinimumOrderSize;
 var minimumPriceVariation = symbolProperties.MinimumPriceVariation;
 	</pre>
-	<pre class="python">symbol_properties = Securities["BTCUSD"].SymbolProperties
+	<pre class="python">symbol_properties = self.Securities["BTCUSD"].SymbolProperties
 lot_size = symbol_properties.LotSize
 minimum_order_size = symbol_properties.MinimumOrderSize
 minimum_price_variation = symbol_properties.MinimumPriceVariation
 	</pre>
 </div>
-
+<p>
+These properties are used by the algorithm to prevent invalid orders from being placed or by helper methods to calculate order quantities for a given target.
+</p>
