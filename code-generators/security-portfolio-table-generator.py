1500f9a07f7a5e917c77c0afcd95d2060439f81b
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2022-05-25 23:34:53 +0100

    Applies New Format for Properties Tables

--- code-generators/security-portfolio-table-generator.py
+++ code-generators/security-portfolio-table-generator.py
@@ -11,6 +11,19 @@ links = {
     "SymbolProperties": "https://www.quantconnect.com/services/inspector?type=T:QuantConnect.Securities.SymbolProperties"
 }
 
+filenames = {
+    "Security": "security.html",
+    "SecurityPortfolioManager": "security_portfolio_manager.html",
+    "SecurityHolding": "security_holding.html",
+    "SymbolProperties": "symbol_properties.html"
+}
+
+cs2py = {
+    "decimal": "float",
+    "DateTime": "datetime",
+    "TimeSpan": "timedelta",
+}
+
 for item, url in links.items():
     json = urlopen(url).read().decode("utf-8") \
             .replace("true", "True") \
@@ -18,19 +31,21 @@ for item, url in links.items():
             .replace("null", "None")
     json_dict = eval(json)
 
-    html_code = f"""<p>{json_dict["description"]}</p>
-    
+    html_code = f"""<p>The following table describes the properties of the <code>{item}</code> class:</p>
+
 <table class="table qc-table">
-<thead>
-<tr><th colspan="2"><code>{item}</code></th></tr>
-</thead>
+<thead><tr><th style="white-space:nowrap">Property</th><th style="white-space:nowrap">Data Type</th><th>Description</th></tr></thead>
 """
+    for attr in sorted(json_dict["properties"], key=lambda attr: attr['property-name']):
+        type_name = attr['property-short-type-name']
+        if type_name in cs2py:
+            type_name = f'<code class="csharp">{type_name}</code><code class="python">{cs2py[type_name]}</code>'
+        else:
+            type_name = f'<code>{type_name}</code>'
 
-    for attr in json_dict["properties"]:
-        html_code += f"""<tr><td width="20%">{attr['property-name']}</td> <td><code>{attr['property-short-type-name']}</code> <br/> {attr['property-description']}</td></tr>
+        html_code += f"""<tr><td><code>{attr['property-name']}</code></td><td>{type_name}</td><td>{attr['property-description']}</td></tr>
 """
-
     html_code += "</table>"
     
-    with open(OUTPUT_DIR / f"{item.lower()}.html", "w", encoding="utf-8") as html_file:
+    with open(OUTPUT_DIR / filenames[item], "w", encoding="utf-8") as html_file:
         html_file.write(html_code)
