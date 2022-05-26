ef0fa8da7b3eac858330734e58e43b9c470f82a5
Web Editor <web.editor@quantconnect.com>
2022-05-26 17:21:32 +0000

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/29 Object Store/06 Read Data.php
+++ 02 Writing Algorithms/29 Object Store/06 Read Data.php
@@ -1,14 +1,94 @@
-# Import from https://www.quantconnect.com/docs/v2/research-environment/object-store/storing-data
+<p>Read data from the Object Store to import algorithm variables between deployments, import data from the Research Environment, or load trained machine learning models. To read data from the Object Store, you need to provide the key you used to store the object.</p>
+
+<p class='csharp'>You can load the following types of objects from the Object Store:</p>
+
+<ul class='csharp'>
+    <li><code>Bytes</code> objects</li>
+    <li><code>string</code> objects</li>
+    <li>JSON objects</li>
+    <li>XML-formatted objects</li>
+</ul>
+
+<p class='python'>You can load <code>Bytes</code> and <code>string</code> objects from the Object Store.</p>
+
+<p>Before you read data from the Object Store, check if the key exists.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">if (ObjectStore.ContainsKey("key"))
+{
+    // Read data
+}</pre>
+    <pre class="python">if self.ObjectStore.ContainsKey("key"):
+    # Read data</pre>
+</div>
 
 
-<br>-What is reading data:
-<br>&nbsp;&nbsp;&nbsp;&nbsp;- Reading an entry in the ObjectStore by providing the key it was saved under
-<br>-Why read data:
-<br>&nbsp;&nbsp;&nbsp;&nbsp;- Saving algorithm variable values between deployments
-<br>&nbsp;&nbsp;&nbsp;&nbsp;- Transporting data between the backtesting environment and the research environment
-<br>&nbsp;&nbsp;&nbsp;&nbsp;- Training machine learning models in the research environment before deploying them to live trading
-<br>-How(?):
-<br>&nbsp;&nbsp;&nbsp;&nbsp; -Using Read, ReadBytes, ReadJson, ReadXml
-<br>&nbsp;&nbsp;&nbsp;&nbsp; -Method depends on data being stored
-<br>&nbsp;&nbsp;&nbsp;&nbsp; -Need to provide key
-<br>&nbsp;&nbsp;&nbsp;&nbsp;-<snippet example of using each method>
+<h4>Bytes</h4>
+<ol>
+    <p>Call <code>ReadBytes</code> method to read data from a stored bytes object.</p>
+    <div class="section-example-container">
+        <pre class="csharp">var bytesData = objectStore.Read("saveBytes");</pre>
+        <pre class="python">byte_data = object_store.Read("save_bytes")</pre>
+    </div>
+
+    <li class="csharp">Convert to string.</li>
+    <li class="python">Call <code>json.loads</code> to restore the data string.</li>
+    <div class="section-example-container">
+        <pre class="csharp">var stringData = Encoding.UTF8.GetString(bytesData);</pre>
+        <pre class="python">string_data = json.loads(bytearray(byte_data).decode('utf-8'))</pre>
+    </div>
+
+    <li>Restore as dictionary.</li>
+    <div class="section-example-container">
+        <pre class="csharp">var dict = stringFromBytes.Split(new[] {'\n'}, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => part.Split('[')[1]).Select(part => part.Split(']')[0]).Select(part => part.Split(','))
+               .ToDictionary(split => split[0], split => split[1]);</pre>
+        <pre class="python">recovered_dict = eval(string_data)
+recovered_dict, type(recovered_dict)</pre>
+    </div>
+    <img class="csharp" src="https://cdn.quantconnect.com/i/tu/storing-cs-6.png">
+    <img class="python" src="https://cdn.quantconnect.com/i/tu/storing-py-4.png">
+</ol>
+
+<h4>Strings</h4>
+<ol>
+    <p>Call <code>Read</code> or <code>ReadString</code> method to read the stored string object.</p>
+    <div class="section-example-container">
+        <pre class="csharp">var stringData = objectStore.Read("saveString");</pre>
+        <pre class="python">string_data = object_store.Read("save_string")</pre>
+    </div>
+
+    <li>Restore as dictionary.</li>
+    <div class="section-example-container">
+        <pre class="csharp">var dict = stringData.Split(new[] {'\n'}, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => part.Split('[')[1]).Select(part => part.Split(']')[0]).Select(part => part.Split(','))
+               .ToDictionary(split => split[0], split => split[1]);</pre>
+        <pre class="python">recovered_dict = eval(string_data)
+recovered_dict, type(recovered_dict)</pre>
+    </div>
+    <img class="csharp" src="https://cdn.quantconnect.com/i/tu/storing-cs-16.png">
+    <img class="python" src="https://cdn.quantconnect.com/i/tu/storing-py-6.png">
+</ol>
+
+<h4 class="csharp">JSON</h4>
+<ol class="csharp">
+    <li class="csharp">Call the <code>ReadJson&lt;T&gt;</code> method to read the stored JSON object.</li>
+    <div class="csharp section-example-container">
+        <pre class="csharp">var jsonData = objectStore.ReadJson&lt;Dictionary&lt;string, int&gt;&gt;("saveJson");</pre>
+    </div>
+    <img class="csharp" src="https://cdn.quantconnect.com/i/tu/storing-cs-10.png">
+</ol>
+
+<h4 class="csharp">XML</h4>
+<ol class="csharp">
+    <li class="csharp">Call the <code>ReadXml&lt;T&gt;</code> method to read the stored XML object.</li>
+    <div class="csharp section-example-container">
+        <pre class="csharp">var xmlData = objectStore.ReadXml&lt;XElement&gt;("saveXml");</pre>
+    </div>
+
+    <li class="csharp">Reconstruct as a dictionary.</li>
+    <div class="csharp section-example-container">
+        <pre class="csharp">var dict = xmlData.Elements().ToDictionary(x => x.Name.LocalName, x => int.Parse(x.Value));</pre>
+    </div>
+    <img class="csharp" src="https://cdn.quantconnect.com/i/tu/storing-cs-12.png">
+</ol>
