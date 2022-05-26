57c4a1899246e64cd5f8efe6f6b68d4954f9c7ab
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-05-26 15:34:09 -0600

    Update and rename 04 Naming Convention.html to 04 Naming Convention.php

---
+++ 02 Writing Algorithms/28 Indicators/00 Key Concepts/04 Naming Convention.php
@@ -0,0 +1,6 @@
+<?php 
+echo file_get_contents(DOCS_RESOURCES."/indicators/naming-convention-manual.html"); 
+echo file_get_contents(DOCS_RESOURCES."/indicators/naming-convention-automatic.html");
+?>
+
+<p>To view the class name and shortcut method of each indicator, see the Indicator Reference.</p>
