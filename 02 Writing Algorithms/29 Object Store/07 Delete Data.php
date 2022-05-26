4dfc7ae83c23b8430ed38d6dfba67b4c15107244
Web Editor <web.editor@quantconnect.com>
2022-05-26 17:43:15 +0000

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/29 Object Store/07 Delete Data.php
+++ 02 Writing Algorithms/29 Object Store/07 Delete Data.php
@@ -1,12 +1,22 @@
-# Import from https://www.quantconnect.com/docs/v2/research-environment/object-store/storing-data
+<p>Delete objects in the Object Store to remove objects that you no longer need. If you run algorithms in QC Cloud, you must stay within your <a href='/docs/v2/our-platform/projects/data-storage#03-Storage-Sizes'>storage quota</a> to avoid issues. To delete data in the cloud Object Store, you need <a href='/docs/v2/our-platform/organizations/members#06-Permissions'>storage delete permissions</a>.</p>
 
+<p>To delete objects from the Object Store, call the <code>Delete</code> method. Before you delete data, check if the key exists. If you try to delete an object with a key that doesn't exist in the Object Store, the method raises an exception.</p>
 
-<br>-What is deleting data:
-<br>&nbsp;&nbsp;&nbsp;&nbsp;     - Deleting a key-value pair in the ObjectStore by providing the object's key
-<br>-Why delete data:
-<br>&nbsp;&nbsp;&nbsp;&nbsp;      - To stay within organization's limits (OS storage usage is displayed in Algorithm Lab)
-<br>&nbsp;&nbsp;&nbsp;&nbsp;      - To remove unneeded objects that are in the ObjectStore
-<br>-How(?):
-<br>&nbsp;&nbsp;&nbsp;&nbsp;      -Use Delete method or delete the project
-<br>&nbsp;&nbsp;&nbsp;&nbsp;      -Need to provide the key to delete
-<br>&nbsp;&nbsp;&nbsp;&nbsp;      -<snippet example of using Delete>
+<div class="section-example-container">
+    <pre class="csharp">if (ObjectStore.ContainsKey(key))
+{
+    ObjectStore.Delete(key);
+}</pre>
+    <pre class="python">if self.ObjectStore.ContainsKey(key):
+    self.ObjectStore.Delete(key)</pre>
+</div>
+
+<p>To delete all of the content in the Object Store, iterate through all the stored data.</p>
+<div class="section-example-container">
+    <pre class="csharp">foreach (var kvp in ObjectStore)
+{
+    ObjectStore.Delete(kvp.Key);
+}</pre>
+    <pre class="python">for kvp in self.ObjectStore:
+    self.ObjectStore.Delete(kvp.Key)</pre>
+</div>
