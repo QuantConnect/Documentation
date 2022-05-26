fda7aaf92f19e8f0f55cb00e28b3ec460a8e52fa
Web Editor <web.editor@quantconnect.com>
2022-05-26 15:42:10 +0000

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/29 Object Store/02 Get Stored Metadata.php
+++ 02 Writing Algorithms/29 Object Store/02 Get Stored Metadata.php
@@ -1,36 +1,14 @@
-<p>Follow these steps to get all of the keys, values, and file paths in the Object Store:</p>
+<p>To get all of the keys, values, and file paths in the Object Store, iterate through the <code>ObjectStore</code> object.</p>
 
-<ol>
-    <li class="csharp">Load the required libraries and files.</li>
-    <div class="csharp section-example-container">
-        <pre class="csharp">#load "../Initialize.csx"
-#load "../QuantConnect.csx"
-
-using System.Xml.Linq;
-using QuantConnect;
-using QuantConnect.Data;
-using QuantConnect.Algorithm;
-using QuantConnect.Research;</pre>
-    </div>
-
-    <li>Instantiate a <code>QuantBook</code> object.</li>
-    <div class="section-example-container">
-        <pre class="csharp">var qb = new QuantBook();</pre>
-        <pre class="python">qb = QuantBook()</pre>
-    </div>
-    <li>Iterate the <code>ObjectStore</code> member:</li>
-    <div class="section-example-container">
-        <pre class="csharp">foreach (var kvp in qb.ObjectStore)
+<div class="section-example-container">
+    <pre class="csharp">foreach (var kvp in ObjectStore)
 {
     var key = kvp.Key;
     var value = kvp.Value;
-    var filePath = objectStore.GetFilePath(key);
-    Console.WriteLine($"{key} : {filePath}");
+    var filePath = ObjectStore.GetFilePath(key);
 }</pre>
-        <pre class="python">for kvp in qb.ObjectStore:
+    <pre class="python">for kvp in self.ObjectStore:
     key = kvp.Key
     value = kvp.Value
-    file_path = object_store.GetFilePath(key)
-    print(f"{key} : {file_path}")</pre>
-    </div>
-</ol>
+    file_path = self.ObjectStore.GetFilePath(key)</pre>
+</div>
