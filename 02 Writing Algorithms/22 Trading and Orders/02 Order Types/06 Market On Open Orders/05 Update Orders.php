ef0dd6d5eadb1eede363befeff4d3768d21b2e76
Web Editor <web.editor@quantconnect.com>
2022-06-03 14:40:05 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/22 Trading and Orders/02 Order Types/06 Market On Open Orders/05 Update Orders.php
+++ 02 Writing Algorithms/22 Trading and Orders/02 Order Types/06 Market On Open Orders/05 Update Orders.php
@@ -1,5 +1,5 @@
 <p>
-You can update the quantity and tag of market on open orders. To update an order, pass a $[UpdateOrderFields, T:QuantConnect.Orders.UpdateOrderFields] object to the Update method on the $[OrderTicket, T:QuantConnect.Orders.OrderTicket]. The Update method returns an $[OrderResponse, T:QuantConnect.Orders.OrderResponse] to signal the success or failure of the update request. You can update orders until the last two minutes before close.
+You can update the quantity and tag of market on open orders until the order fills or the brokerage prevents modifications. To update an order, pass an <code>UpdateOrderFields</code> object to the <code>Update</code> method on the <code>OrderTicket</code>. The <code>Update</code> method returns an <code>OrderResponse</code> to signal the success or failure of the update request.
 </p>
 
 <div class="section-example-container">
