e47e388e2e67a3550749965b97cbeffaf0548393
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-03 12:25:41 -0600

    Create update-individual-fields.php

---
+++ Resources/order-types/update-individual-fields.php
@@ -0,0 +1,53 @@
+<?php
+$getUpdateIndividualFieldsText = function($supportedMethods) {
+    $fieldsToMethod = array(
+      "UpdateLimitPrice" => array("C#" => "var limitResponse = ticket.UpdateLimitPrice(limitPrice, tag);", "Python" => "limit_response = ticket.UpdateLimitPrice(limitPrice, tag)"), 
+      "UpdateQuantity"  => array("C#" => "var quantityResponse = ticket.UpdateQuantity(quantity, tag);", "Python" => "limit_response = ticket.UpdateQuantity(quantity, tag)"), 
+      "UpdateStopPrice"  => array("C#" => "var stopResponse = ticket.UpdateStopPrice(stopPrice, tag);", "Python" => "limit_response = ticket.UpdateStopPrice(stopPrice, tag)"), 
+      "UpdateTag" => array("C#" => "var tagResponse = ticket.UpdateTag(tag);", "Python" => "limit_response = ticket.UpdateTag(tag)")
+    )
+  
+    echo "
+        <p>To update individual fields of an order, call any of the following methods:</p>
+        <ul>
+    ";
+    
+    foreach ($fieldsToMethod as $key => $value)
+    {
+        if (in_array($key, $supportedMethods)
+        {
+            echo "<li><code>{$key}</code></li>";
+        }
+    }
+  
+    echo "</ul>";
+    
+            
+    echo "<div class=\"section-example-container\">
+              <pre class=\"csharp\">";
+        
+    foreach ($fieldsToMethod as $key => $value)
+    {
+        if (in_array($key, $supportedMethods)
+        {
+            echo $value["C#"];
+            echo "
+
+";
+        }
+    }
+    echo "</pre>
+<pre class=\"python\">";
+    foreach ($fieldsToMethod as $key => $value)
+    {
+        if (in_array($key, $supportedMethods)
+        {
+            echo $value["Python"];
+            echo "
+
+";
+        }
+    }
+    echo "</pre></div>";
+}
+?>
