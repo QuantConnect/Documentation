ba5d7ca6c46a72dd244109551c2d7cf54de2aec0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-06 11:38:55 -0600

    Update 06 Order Response.php

--- 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/06 Order Response.php
+++ 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/06 Order Response.php
@@ -2,3 +2,9 @@
 <div data-tree="QuantConnect.Orders.OrderResponse"></div>
 
 <p>If your order changes fail, check the <code>ErrorCode</code> or <code>ErrorMessage</code>. For more information about specific order errors, see the <a href="/docs/v2/writing-algorithms/trading-and-orders/order-errors#19-Error-Code-Reference">Error Code Reference</a>.</p>
+
+<p>To get most recent order response, call the <code>GetMostRecentOrderResponse</code> method.</p>
+<div class="section-example-container">
+<pre class="csharp">var response = orderTicket.GetMostRecentOrderResponse();</pre>
+<pre class="python">response = order_ticket.GetMostRecentOrderResponse()</pre>
+</div>
