da1ac77b6ac5602c2077904088947b0b77f68970
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-06 12:39:27 -0600

    Update 04 Symbol Properties.php

--- 02 Writing Algorithms/22 Trading and Orders/00 Key Concepts/04 Symbol Properties.php
+++ 02 Writing Algorithms/22 Trading and Orders/00 Key Concepts/04 Symbol Properties.php
@@ -1,4 +1,4 @@
-<p>The <code>SymbolProperties</code> is a property of the <code>Security</code> object. LEAN uses some of the <code>SymbolProperties</code> to prevent invalid orders and to <a href='/docs/v2/writing-algorithms/trading-and-orders/position-sizing'>calculate order quantities</a> for a given target. </p>
+<p>The <code>SymbolProperties</code> are a property of the <code>Security</code> object. LEAN uses some of the <code>SymbolProperties</code> to prevent invalid orders and to <a href='/docs/v2/writing-algorithms/trading-and-orders/position-sizing'>calculate order quantities</a> for a given target. </p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/securities/symbol_properties.html"); ?>
 <p>To get the <code>SymbolProperties</code>, check the property on the <code>Security</code> object.</p>
