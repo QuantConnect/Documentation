6dd7a8791e95aac317f0a1982f2caf035c208335
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-06 17:53:02 -0600

    Create execution-sequence.php

---
+++ Resources/scheduled-events/execution-sequence.php
@@ -0,0 +1,28 @@
+<?php
+$getExecutionText = function($linkScheduledEvents) {
+    $result = "<p>The algorithm manager calls events in the following order:</p>
+<ol>";
+  
+    if ($linkScheduledEvents)
+    {
+        $result .= "
+<li><a href='/docs/v2/writing-algorithms/scheduled-events'>Scheduled Events</a></li>
+<li>Consolidation event handlers</li>";
+    }
+    else
+    {
+        $result .= "
+<li>Scheduled Events</li>
+<li><a href='/docs/v2/writing-algorithms/consolidating-data/key-concepts#05-Receive-Consolidated-Bars'>Consolidation event handlers</a></li>";
+    }
+  
+    $result .= "
+    <li><code>OnData</code> event handler</li>
+</ol>
+
+<p>This event flow is important to note. For instance, if your consolidation handlers or <code>OnData</code> event handler appends data to a <code>RollingWindow</code> and you use that <code>RollingWindow</code> in your Scheduled Event, when the Scheduled Event executes, the <code>RollingWindow</code> won't contain the most recent data.</p>"; 
+    
+  
+    echo $result;
+}
+?>
