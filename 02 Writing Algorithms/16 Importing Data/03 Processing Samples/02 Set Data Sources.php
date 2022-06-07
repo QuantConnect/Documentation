9cae24b1f576aaf8cf49095d8b86a092c9973926
Web Editor <web.editor@quantconnect.com>
2022-06-07 17:40:26 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/16 Importing Data/03 Processing Samples/02 Set Data Sources.php
+++ 02 Writing Algorithms/16 Importing Data/03 Processing Samples/02 Set Data Sources.php
@@ -1,11 +1,7 @@
-<p>
-	In your custom data class, the $[GetSource,M:QuantConnect.Data.BaseData.Reader] method instructs LEAN where to find your data. It must return a $[SubscriptionDataSource,T:QuantConnect.Data.SubscriptionDataSource] object
-	containing the location to find your data, and the format of the data (SubscriptionTransportMedium). You can even change source locations for backtesting and live modes. We support many different data types and formats. <br></p>
+<p>The <code>GetSource</code> method in your custom data class instructs LEAN where to find the data. This method must return a <code>SubscriptionDataSource</code> object, which contains the data location and format (<code>SubscriptionTransportMedium</code>). You can even change source locations for backtesting and live modes. We support many different data sources.</p>
 
 <h4>Local Files</h4>
-<p>
-To retreive data from your local disk, use $[SubscriptionTransportMedium.LocalFile,T:QuantConnect.SubscriptionTransportMedium].
-</p>
+<p>To retrieve data from your local disk, use <code>SubscriptionTransportMedium.LocalFile</code>.</p>
 
 <div class="section-example-container">
 <pre class="csharp">public class MyCustomDataType : BaseData
@@ -20,13 +16,10 @@ To retreive data from your local disk, use $[SubscriptionTransportMedium.LocalFi
         return SubscriptionDataSource("&lt;filePath&gt;", SubscriptionTransportMedium.LocalFile)</pre>
 </div>
 
-
 ## Import example of local path from Key Concepts
 
 <h4>Remote Files</h4>
-<p>
-To download data from a remote URL like GitHub and Dropbox, use $[SubscriptionTransportMedium.RemoteFile,T:QuantConnect.SubscriptionTransportMedium].
-</p>
+<p>To download data from a remote URL like GitHub and Dropbox, use <code>SubscriptionTransportMedium.RemoteFile</code>.</p>
 
 <div class="section-example-container">
 <pre class="csharp">public class MyCustomDataType : BaseData
@@ -44,9 +37,7 @@ To download data from a remote URL like GitHub and Dropbox, use $[SubscriptionTr
 ## Import examples of github and dropbox urls from Key Concepts
 
 <h4>REST APIs</h4>
-<p>
-To retrieve data from a REST endpoint, use $[SubscriptionTransportMedium.Rest,T:QuantConnect.SubscriptionTransportMedium]. The endpoint is polled at each <code>Resolution</code> time step that you set when you initialize the data subscription. This is generally intended for live data sources.
-</p>
+<p>To retrieve data from a REST endpoint, use <code>SubscriptionTransportMedium.Rest</code>. LEAN polls the endpoint at each <code>Resolution</code> time step that you set when you initialize the data subscription. This <code>SubscriptionTransportMedium</code> is generally for live data sources.</p>
 
 <div class="section-example-container">
 <pre class="csharp">public class MyCustomDataType : BaseData
