911a04b6b98f2683b95c1b546fb91cfb2378e589
Web Editor <web.editor@quantconnect.com>
2022-06-07 16:39:27 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/04 Custom Indicator Values.php
+++ 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/04 Custom Indicator Values.php
@@ -1,13 +1,32 @@
--By default, the consolidators update the indicator with the close price of the security.
+<p>Data point indicators use only a single price data in their calculations. By default, those indicators use the closing price. For 
+assets with <code>TradeBar</code> data, that price is the <code>TradeBar</code> close price. For assets with <code>QuoteBar</code> data, that price is the mid-price of the bid closing price and the ask closing price. To create an indicator with the other fields like the <code>Open</code>, <code>High</code>, <code>Low</code>, or <code>Close</code>, provide a <code>selector</code> argument to the <code>RegisterIndicator</code> method.</p>
 
-<p>The data point indicators use only a single price data in their calculations. By default, those indicators use the closing price. For Equity, that price is the trade bar closing price. For other asset classes with quote bar data (bid/ask price), those indicators are calculated with the mid-price of the bid closing price and the ask closing price. </p><p>If you want to create an indicator with the other fields like Open, High, Low, or Close, you can specify the selector argument in the indicator helper method with the available fields. </p>
 <div class="section-example-container">
-<pre class="csharp">// define a 10-period RSI with indicator constructor
+<pre class="csharp">// Define a 10-period RSI with indicator the constructor
 _rsi = new RelativeStrengthIndex(10, MovingAverageType.Simple);
-// register the daily High price data to automatically update the indicator
+
+// Register the daily High price data to automatically update the indicator
 RegisterIndicator(symbol, _rsi, Resolution.Daily, Field.High);</pre>
-<pre class="python"># define a 10-period RSI with indicator constructor
+<pre class="python"># Define a 10-period RSI with indicator constructor
 self.rsi = RelativeStrengthIndex(10, MovingAverageType.Simple)
-# register the daily High price data to automatically update the indicator
+
+# Register the daily High price data to automatically update the indicator
 self.RegisterIndicator(self.symbol, self.rsi, Resolution.Daily, Field.High)</pre>
+</div>
+
+
+<?php 
+echo file_get_contents(DOCS_RESOURCES."/enumerations/field.html"); 
+?>
+
+<p class='csharp'>To create a custom <code>selector</code>, define a function that calculates the value.</p>
+
+<div class='csharp'>
+    <div class="section-example-container">
+        <pre class="csharp">_rsi = RegisterIndicator(_symbol, _rsi,  Resolution.Daily, x =>
+{
+    var bar = x as IBaseDataBar;
+    return (bar.Low + bar.High) / 2;
+});</pre>
+    </div>
 </div>
