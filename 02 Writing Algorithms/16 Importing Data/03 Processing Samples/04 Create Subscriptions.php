9912adafdc251623d5c25a429b5c20dc95136de8
Web Editor <web.editor@quantconnect.com>
2022-06-07 18:14:57 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/16 Importing Data/03 Processing Samples/04 Create Subscriptions.php
+++ 02 Writing Algorithms/16 Importing Data/03 Processing Samples/04 Create Subscriptions.php
@@ -1,11 +1,8 @@
-<p>
-To receive your custom data in the OnData method, after you define your custom data type, call 
-	<span class="csharp"><code>AddData&lt;T&gt;(string ticker, Resolution resolution = Resolution.Daily)</code> in the <code>Initialize</code> method of your algorithm. 
-		This gives LEAN the T-type factory to create the objects, the name of the data, and the resolution at which to poll 
-		the data to check for updates.
-	</span><span class="python"><code>self.AddData(Type class, string ticker, Resolution resolution = Resolution.Daily)</code>. in the <code>Initialize</code> method of your algorithm. This gives LEAN the 
-		type factory to create the data objects and the resolution to poll the remote data source for updates. 
-	</span>
+<p>To receive your custom data in the <code>OnData</code> method, after you define the custom data class, call 
+
+<span class="csharp"><code>AddData&lt;T&gt;(string ticker, Resolution resolution = Resolution.Daily)</code> in the <code>Initialize</code> method of your algorithm. This method gives LEAN the T-type factory to create the objects, the name of the data, and the resolution at which to poll the data source for updates.</span>
+
+<span class="python"><code>self.AddData(Type class, string ticker, Resolution resolution = Resolution.Daily)</code>. in the <code>Initialize</code> method of your algorithm. This method gives LEAN the type factory to create the data objects, the name of the data, and the resolution to poll the data source for updates. </span>
 </p>
 
 <div class="section-example-container">
@@ -23,12 +20,6 @@ To receive your custom data in the OnData method, after you define your custom d
 </pre>
 </div>
 
-## TODO: 
-<br>-What resolution should you set?
-<br>&nbsp;&nbsp;&nbsp;- Match the resolution of your custom dataset
-<br>&nbsp;&nbsp;&nbsp;- The max reasonable resolute is every minute. Anything more frequent than minute will be very slow to execute.
-
-<p>
-	The framework checks for new data as instructed by the <code>Resolution</code> period. The following table shows the polling frequency of each resolution:</p> 
+<p>The <code>resolution</code> argument should match the resolution of your custom dataset. The lowest reasonable resolution is every minute. Anything more frequent than minute is very slow to execute. The frequency that LEAN checks the data source depends on the <code>resolution</code> argument. The following table shows the polling frequency of each resolution:</p> 
 
 <?php include(DOCS_RESOURCES."/datasets/live-dataset-polling-frequency-table.html"); ?>
