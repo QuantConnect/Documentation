439b8c999e43db68c9e01d10dae1881550a3f3a4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-07 17:36:17 -0600

    Update 04 Create Subscriptions.php

--- 02 Writing Algorithms/16 Importing Data/03 Processing Samples/04 Create Subscriptions.php
+++ 02 Writing Algorithms/16 Importing Data/03 Processing Samples/04 Create Subscriptions.php
@@ -20,6 +20,6 @@
 </pre>
 </div>
 
-<p>The <code>resolution</code> argument should match the resolution of your custom dataset. The lowest reasonable resolution is every minute. Anything more frequent than minute is very slow to execute. The frequency that LEAN checks the data source depends on the <code>resolution</code> argument. The following table shows the polling frequency of each resolution:</p> 
+<p>The <code>resolution</code> argument should match the resolution of your custom dataset. The lowest reasonable resolution is every minute. Anything more frequent than every minute is very slow to execute. The frequency that LEAN checks the data source depends on the <code>resolution</code> argument. The following table shows the polling frequency of each resolution:</p> 
 
 <?php include(DOCS_RESOURCES."/datasets/live-dataset-polling-frequency-table.html"); ?>
