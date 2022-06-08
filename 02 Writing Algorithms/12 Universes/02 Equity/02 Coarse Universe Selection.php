6106e89d9cecf9aa29f19ac3b51b0bcf1127e739
Web Editor <web.editor@quantconnect.com>
2022-06-08 17:29:27 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/12 Universes/02 Equity/02 Coarse Universe Selection.php
+++ 02 Writing Algorithms/12 Universes/02 Equity/02 Coarse Universe Selection.php
@@ -1,10 +1,5 @@
-<p>Coarse Universe selection lets you pick a set of stocks based on <code>CoarseFundamental</code> data. <code>CoarseFundamental</code> objects have the following attributes:</p>
+<p>Coarse Universe Selection enables you pick a set of stocks based on their trading volume, price, or whether they have fundamental data. To add a coarse universe, in the <code>Initialize</code> method, pass a filter function to the <code>AddUniverse</code> method. The coarse filter function receives a list of <code>CoarseFundamental</code> objects and must return a list of <code>Symbol</code> objects. LEAN automatically subscribes to the <code>Symbol</code> objects you return from the function. You shouldn't call <code>AddEquity</code> in the filter function.</p>
 
-<div data-tree='QuantConnect.Data.UniverseSelection.CoarseFundamental'></div>
-
-<p>
-To use a coarse universe, you must request it using an $[AddUniverse(), M:QuantConnect.Algorithm.QCAlgorithm.AddUniverse] call from the Initialize() method of your algorithm. You should pass in a function that will be used to filter the stocks down to the assets you are interested in using.
-</p>
 <div class="section-example-container">
 <pre class="csharp">public class MyCoarseUniverseAlgorithm : QCAlgorithm
 {
@@ -12,31 +7,24 @@ To use a coarse universe, you must request it using an $[AddUniverse(), M:QuantC
     {
         AddUniverse(MyCoarseFilterFunction);
     }
+
+    private IEnumerable&lt;Symbol&gt; MyCoarseFilterFunction(IEnumerable&lt;CoarseFundamental&gt; coarse)
+    {
+        return ;
+    }
 }
 </pre>
 <pre class="python">class MyCoarseUniverseAlgorithm(QCAlgorithm):
     def Initialize(self):
         self.AddUniverse(self.MyCoarseFilterFunction)
-</pre>
-</div>
 
-<p>
-The coarse filter function is provided a list of $[CoarseFundamental,T:QuantConnect.Data.UniverseSelection.CoarseFundamental] objects and must return a list of Symbol objects. If you don't want to make any changes to the current universe, you can return <code>Universe.Unchanged</code>. If you only use a coarse filter in your universe selection process, the Equities you return from the function are automatically added to your universe. Don't call <code>AddEquity</code> in this method.</p>
-
-<div class="section-example-container">
-<pre class="csharp">public class MyCoarseUniverseAlgorithm : QCAlgorithm
-{
-    // Coarse Filter Function accepts a list of CoarseFundamental Objects. 
-    private IEnumerable&lt;Symbol&gt; MyCoarseFilterFunction(IEnumerable&lt;CoarseFundamental&gt; coarse)
-    {
-        return Universe.Unchanged;
-    }
-}
-</pre>
-<pre class="python">class MyCoarseUniverseAlgorithm(QCAlgorithm):
     def MyCoarseFilterFunction(self, coarse):
-         return Universe.Unchanged
-</pre>
+        sorted_by_dollar_volume = sorted(coarse, key=lambda x: x.DollarVolume, reverse=True) 
+        return [c.Symbol for c in sorted_by_dollar_volume[:100]]</pre>
 </div>
 
-<p>The total number of stocks in the <a href='/datasets/quantconnect-us-equity-security-master'>US Equity Security Master dataset</a> is <?php include(DOCS_RESOURCES."/kpis/us-equity-security-master-size.php");?>, but your coarse filter function won't receive all of these at one time because the US Equity Security Master dataset is free of survivorship-bias and some of the securities have delisted over time. The number of securities that are passed into your coarse filter function depends on the date of your algorithm. Currently, there are about <?php include(DOCS_RESOURCES."/kpis/coarse-universe-size.php"); ?> securities passed into your coarse filter function. The most important properties of the $[CoarseFundamental,T:QuantConnect.Data.UniverseSelection.CoarseFundamental] object are: <code>Price</code> (raw), <code>DollarVolume</code> and <code>HasFundamentaData</code>.</p>
+<p><code>CoarseFundamental</code> objects have the following attributes:</p>
+
+<div data-tree='QuantConnect.Data.UniverseSelection.CoarseFundamental'></div>
+
+<p>The total number of stocks in the <a href='/datasets/quantconnect-us-equity-security-master'>US Equity Security Master dataset</a> is <?php include(DOCS_RESOURCES."/kpis/us-equity-security-master-size.php");?>, but your coarse filter function won't receive all of these at one time because the US Equity Security Master dataset is free of survivorship-bias and some of the securities have delisted over time. The number of securities that are passed into your coarse filter function depends on the date of your algorithm. Currently, there are about <?php include(DOCS_RESOURCES."/kpis/coarse-universe-size.php"); ?> securities that LEAN passes into your coarse filter function.</p>
