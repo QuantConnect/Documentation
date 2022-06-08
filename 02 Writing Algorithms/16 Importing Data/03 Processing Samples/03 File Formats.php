e46cb6c551e2e4ae57af78df57b8b7f9660655e7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-08 13:01:48 -0600

    Update 03 File Formats.php

--- 02 Writing Algorithms/16 Importing Data/03 Processing Samples/03 File Formats.php
+++ 02 Writing Algorithms/16 Importing Data/03 Processing Samples/03 File Formats.php
@@ -1,1 +1,3 @@
+<p>The <code>SubscriptionDataSource</code> constructor can accept a <code>format</code> argument.</p>
 
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/file_format.html"); ?>
