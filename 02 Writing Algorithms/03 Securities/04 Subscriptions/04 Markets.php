f1192e324f1e4cfcf8a5cbb0dbcae84453efeb61
Web Editor <web.editor@quantconnect.com>
2022-06-13 19:01:38 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/04 Subscriptions/04 Markets.php
+++ 02 Writing Algorithms/03 Securities/04 Subscriptions/04 Markets.php
@@ -1,1 +1,10 @@
+<p>The datasets integrated into the Dataset Market cover the following markets:</p>
 
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market.html"); ?>
+
+<p>LEAN can usually determine the correct market based on the ticker you provide when you create the security subscription. To manually set the market for a security, pass a <code>market</code> argument when you create the security subscription.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">AddEquity("SPY", market: Market.USA);</pre>
+    <pre class="python">self.AddEquity("SPY", market=Market.USA)</pre>
+</div>
