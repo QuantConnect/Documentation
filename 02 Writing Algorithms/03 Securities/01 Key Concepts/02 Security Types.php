fda7283218f5e8b5cdac62a17ee02df3a00969c5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-13 11:54:40 -0600

    Update and rename 02 Security Types.html to 02 Security Types.php

---
+++ 02 Writing Algorithms/03 Securities/01 Key Concepts/02 Security Types.php
@@ -0,0 +1,5 @@
+<p>LEAN uses the <code>SecurityType</code> enumeration as a synonym for asset classes.</p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/security_type.html"); ?>
+
+<p>The <code>Base</code> type is for non-financial assets like custom and alternative data objects.</p>
