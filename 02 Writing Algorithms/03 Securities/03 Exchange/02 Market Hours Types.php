a231ad0000b22d2936139d275465c83e969e25dd
Web Editor <web.editor@quantconnect.com>
2022-06-13 17:11:16 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/03 Exchange/02 Market Hours Types.php
+++ 02 Writing Algorithms/03 Securities/03 Exchange/02 Market Hours Types.php
@@ -1,1 +1,13 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market_hours_state.html"); ?>
+<p>By default, your security subscriptions only cover regular trading hours. To subscribe to pre- and post-market trading hours for a specific asset, enable the <code>extendedMarketHours</code> argument when you create the security subscription.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">AddEquity("SPY", extendedMarketHours: true);</pre>
+    <pre class="python">self.AddEquity("SPY", extendedMarketHours=True)</pre>
+</div>
+
+<p>To subscribe to pre- and post-market trading hours for all assets, enable the <code>ExtendedMarketHours</code> <a href='/docs/v2/writing-algorithms/universes/key-concepts#05-Universe-Settings'>universe setting</a> before you create the security subscriptions.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">UniverseSettings.ExtendedMarketHours = true;</pre>
+    <pre class="python">self.UniverseSettings.ExtendedMarketHours = True</pre>
+</div>
