790a5dc0f1bc3af21ffb819e07ae6b72d785b88a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-14 10:28:15 -0600

    Update 05 Security Market.php

--- 02 Writing Algorithms/03 Securities/01 Key Concepts/05 Security Market.php
+++ 02 Writing Algorithms/03 Securities/01 Key Concepts/05 Security Market.php
@@ -1,5 +1,3 @@
-<?php echo "<p>The following table describes the supported <code>Market</code> values:</p>"; ?>
-
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market.html"); ?>
 
-<?php echo "<p>LEAN groups all of the US Equity exchanges under <code>Market.USA</code>. In live mode, the brokerage routes the orders to the exchange that provides the best price.</p>"; ?>
+<p>LEAN groups all of the US Equity exchanges under <code>Market.USA</code>. In live mode, the brokerage routes the orders to the exchange that provides the best price.</p>
