f1d3d3b2d23d8e088fda0450f51a1594dc43452c
Web Editor <web.editor@quantconnect.com>
2022-06-14 13:18:33 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/01 Requesting Data/09 Data Normalization.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/01 Requesting Data/09 Data Normalization.php
@@ -1,17 +1,22 @@
-What is data normalization:
-<br>- A technique of how the data is adjusted for corporate actions.
-<br>-By default equity data in QuantConnect is Split and Dividend adjusted backwards in time to give smooth continuous prices. This allows easy use for indicators. Some algorithms need raw or partially adjusted price data. You can control this with the { SetDataNormalizationMode()} method. The { DataNormalizationMode} enum has the values Adjusted (default), Raw, SplitAdjusted, and TotalReturn. When data is set to Raw mode the dividends are paid as cash into your algorithm, and the splits are directly applied to your holding quantity. 
+<p>The data normalization modes defines how historical data is adjusted for corporate actions. By default, our US Equity data is adjusted for splits and dividends to produce a smooth price curve. The following table describes the data normalization modes:</p>
 
-<br><br>
-What data normalization options are available:
 <?php include(DOCS_RESOURCES."/datasets/data-normalization.html"); ?>
 
 
-<br><br>
-How to set the data normalization mode:
-<br>-Either with &lt;equity&gt;.SetDataNormalizationMode, UniverseSettings.DataNormalizationMode, or in a security initialize
-<br>-Example snippets
+<p>To set the data normalization mode for a security, call the <code>SetDataNormalizationMode</code> method.</p>
 
-<br><br>
-Ramifications:
-<br>-The normalization of the data returned from a History request matches the normalization of the data subscription.
+ <div class="section-example-container">
+    <pre class="csharp">var equity = AddEquity("SPY");
+equity.SetDataNormalizationMode(DataNormalizationMode.Raw);</pre>
+    <pre class="python">equity = self.AddEquity("SPY", leverage=3)
+equity.SetDataNormalizationMode(DataNormalizationMode.Raw)</pre>
+</div>
+
+<p>To set the data normalization mode for all securities in an algorithm, set the <code>DataNormalizationMode</code> <a href='/docs/v2/writing-algorithms/universes/key-concepts#05-Universe-Settings'>universe setting</a> before you create the security subscriptions.</p>
+
+ <div class="section-example-container">
+    <pre class="csharp">UniverseSettings.DataNormalizationMode = DataNormalizationMode.Raw;</pre>
+    <pre class="python">self.UniverseSettings.DataNormalizationMode = DataNormalizationMode.Raw</pre>
+</div>
+
+<p>The data normalization mode you set configures the data that LEAN passes to <code>OnData</code> and the data from <a href='/docs/v2/writing-algorithms/historical-data/history-requests'>history request</a>.</p>
