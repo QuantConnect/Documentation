866bbf439c7b1bd566b645f88e95b769990d95b2
Web Editor <web.editor@quantconnect.com>
2022-06-14 14:12:26 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/01 Requesting Data/04 Margin and Leverage.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/01 Requesting Data/04 Margin and Leverage.php
@@ -1,7 +1,6 @@
-## Copy from US Equity
+<p>LEAN models buying power and margin calls to ensure your algorithm stays within the margin requirements. The amount of margin that's available depends on the <a href='/docs/v2/writing-algorithms/reality-modeling/brokerages'>brokerage model</a> you use. For more information about the margin requirements of each brokerage, see the <span class='page-section-name'>Margin and Leverage</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage guides</a>. To change the amount of leverage you can use for a security, pass a <code>leverage</code> argument to the <code>AddEquity</code> method.</p>
 
-<br><br>
--Depends on the brokerage model you use and the assets you trade. 
-<br>Refer to Samco and Zerodha brokerage guides
-<br>https://www.quantconnect.com/docs/v2/our-platform/live-trading/brokerages/samco#07-Margin
-<br>https://www.quantconnect.com/docs/v2/our-platform/live-trading/brokerages/zerodha#07-Margin
+<div class="section-example-container">
+    <pre class="csharp">_symbol = AddEquity("YESBANK", market: Market.India, leverage: 3).Symbol;</pre>
+    <pre class="python">self.symbol = self.AddEquity("YESBANK", market=Market.India, leverage=3).Symbol</pre>
+</div>
