1e4d33d366cc3d379dc618d64ef5481f5b823ea5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-15 14:27:10 -0600

    Update 04 Supported Markets.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
@@ -1,1 +1,10 @@
-## TODO: Waiting for the <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/datasets/algoseek/us-future-options#04-Data-Summary'>supported contracts table</a> to be in the Resources
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market-futureoption.html"); ?>
+
+<p>To set the market for a security, pass a <code>market</code> argument to the <code>AddOptionContract</code> method.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">AddOptionContract(self.contract_symbol, market: Market.COMEX);</pre>
+    <pre class="python">self.AddOptionContract(self.contract_symbol, market=Market.COMEX)</pre>
+</div>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); ?>
