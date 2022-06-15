9ae7f9908c97fa60a5e6776393bd549a92b4e3cd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-15 10:39:11 -0600

    Update 04 Supported Markets.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/04 Supported Markets.php
@@ -1,1 +1,3 @@
-<?php include(DOCS_RESOURCES."/datasets/supported-securities/future/supported-contracts.html"); ?>
+<?php include(DOCS_RESOURCES."/enumerations/market-future.html"); ?>
+
+<p>You don't need to pass a <code>market</code> argument to the <code>AddFutureContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
