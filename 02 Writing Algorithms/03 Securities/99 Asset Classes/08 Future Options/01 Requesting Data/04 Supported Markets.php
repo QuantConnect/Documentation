4e878979cc56c7b2adf6edd1f62bf02d71a1291a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-15 14:28:55 -0600

    Update 04 Supported Markets.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
@@ -1,10 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market-futureoption.html"); ?>
 
-<p>To set the market for a security, pass a <code>market</code> argument to the <code>AddOptionContract</code> method.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">AddOptionContract(self.contract_symbol, market: Market.COMEX);</pre>
-    <pre class="python">self.AddOptionContract(self.contract_symbol, market=Market.COMEX)</pre>
-</div>
-
-<?php echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); ?>
+<p>You don't need to pass a <code>Market</code> argument to the <code>AddOptionContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
