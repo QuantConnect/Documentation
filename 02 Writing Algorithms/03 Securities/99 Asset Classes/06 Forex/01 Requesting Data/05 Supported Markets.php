bf49c9837266fbf6462f7b2a84dd1b495d4cc8fa
Web Editor <web.editor@quantconnect.com>
2022-06-14 20:03:42 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/01 Requesting Data/05 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/01 Requesting Data/05 Supported Markets.php
@@ -1,6 +1,4 @@
-<p>LEAN groups all of the Forex exchanges under <code>Market.Oanda</code>. In live mode, the brokerage routes the orders to the exchange that provides the best price.</p>
-
-<p>To set the market for a Forex pair, pass a <code>market</code> argument to the <code>AddForex</code> method.</p>
+<p>The only market available for Forex pairs is <code>Market.Oanda</code>. To set the market for a Forex pair, pass a <code>market</code> argument to the <code>AddForex</code> method.</p>
 
 <div class="section-example-container">
     <pre class="csharp">_symbol = AddForex("EURUSD", market: Market.Oanda).Symbol;</pre>
