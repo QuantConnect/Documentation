eb43574a84d45c09922f4a37c2e3a3dc4872ca17
Web Editor <web.editor@quantconnect.com>
2022-06-16 11:56:04 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/03 Trades.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/03 Trades.php
@@ -1,19 +1,21 @@
-<p><code>TradeBar</code> objects are individual trades from the exchanges that LEAN consolidates into price bars.</p>
+<p><code>TradeBar</code> objects are price bars that consolidate individual trades from the exchanges. They contain the open, high, low, close, and volume of trading activity over a period of time.</p>
 
 <img src="https://cdn.quantconnect.com/docs/i/dataformat-tradebar.png" class="img-responsive">
 
 <p><code>TradeBar</code> objects have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.TradeBar"></div>
 
-<p>To get the <code>TradeBar</code> objects in the <code>Slice</code>, </p>
+<p>To get the <code>TradeBar</code> objects in the <code>Slice</code>, index the <code>Slice</code> or index the <code>Bars</code> property of the <code>Slice</code> with a <code>Symbol</code>. If the security doesn't actively trade or you are in the same time step as when you add the security subscription, the <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Slice</code> contains data for your security before you index the <code>Slice</code> with the security <code>Symbol</code>.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Example of accessing TradeBar objects in OnData
+// The examples on this page should check if the slice contains the data before indexing it.</pre>
+    <pre class="python"># Example of accessing TradeBar objects in OnData
+# The examples on this page should check if the slice contains the data before indexing it.</pre>
+</div>
+
+<p>We adjust the open and close price of bars to reflect the official <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/data-preparation#05-Market-Auction-Prices'>opening and closing auction prices</a>.</p>
+
+<div>-Prices from tradebars are used fill trades in backtests (hour, daily resolutions)</div>
 
 
-Daily/hour/minute bars are adjusted to open (close) with with the 
-official auction open (close) price. If there is no official auction 
-price received before we must emit the bar, we use the first/last trade</div><div>-If there is little trading, or you are in the same time loop as when 
-	you added the security, the time-slice may not have any data. <br></div><div>-Prices from tradebars are used fill trades in backtests (hour, daily resolutions)</div><div>&nbsp;&nbsp;&nbsp; - Opening &amp; closing prices are updated to the official opening/closing auction prices,&nbsp; used to fill MOO and MOC orders<br>
-
-
-<br>
--Add snippet of accessing TradeBars in OnData
-<br>-The examples on this page should check if the slice contains the data before indexing it. (https://github.com/QuantConnect/Documentation/issues/204).
