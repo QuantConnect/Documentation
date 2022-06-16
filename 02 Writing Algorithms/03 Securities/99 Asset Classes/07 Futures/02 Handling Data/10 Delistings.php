13fd3e84f563e660258843930a31bc7f31d0834d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 17:56:29 -0600

    Delete 10 Delistings.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/10 Delistings.php
+++
@@ -1,5 +0,0 @@
--For contract expiry
-<br>-Positions are liquidated when contract expires
-<br>-Example of detecting delisting in OnData
-
-<div data-tree='QuantConnect.Data.Market.Delisting'></div>
