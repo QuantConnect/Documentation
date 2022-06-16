758fb6ae39ef093b1c171116dfc16d93d254fc09
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 12:35:42 -0600

    Update 04 Quotes.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/04 Quotes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/04 Quotes.php
@@ -5,8 +5,6 @@
 <p><code>QuoteBar</code> objects have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.QuoteBar"></div>
 
-<p><code>QuoteBar</code> objects are only available for second and minute resolution data. They make simulated trade fills more realistic by accounting for spread costs.</p>
-
 <p>To get the <code>QuoteBar</code> objects in the <code>Slice</code>, index <code>QuoteBars</code> property of the <code>Slice</code> with a <code>Symbol</code>. If the security doesn't actively get quotes or you are in the same time step as when you added the security subscription, the <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Slice</code> contains data for your security before you index the <code>Slice</code> with the security <code>Symbol</code>.</p>
 <div class="section-example-container">
     <pre class="csharp">// Example of accessing QuoteBar objects in OnData
@@ -14,3 +12,5 @@
     <pre class="python"># Example of accessing QuoteBar objects in OnData
 # The examples on this page should check if the slice contains the data before indexing it.</pre>
 </div>
+
+<p><code>QuoteBar</code> make simulated trade fills more realistic by accounting for spread costs.</p>
