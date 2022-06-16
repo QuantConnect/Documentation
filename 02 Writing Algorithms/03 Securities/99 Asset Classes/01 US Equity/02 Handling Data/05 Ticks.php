dfc235ea1ccf31c774d24f71f4f4272bb1e42733
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 14:53:37 -0600

    Update 05 Ticks.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/05 Ticks.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/05 Ticks.php
@@ -1,13 +1,11 @@
-<p><code>Tick</code> objects represent a single trade or quote at a moment in time. A trade tick is a record of a transaction or sale for the security. A quote tick is an offer to buy or sell a security for a specific price. In backtests, LEAN groups ticks into one millisecond buckets. In live trading, LEAN groups ticks into ~70 millisecond buckets.</p>
-
-<p><code>Tick</code> objects have the following properties:</p>
+<p><code>Tick</code> objects represent a single trade or quote at a moment in time. A trade tick is a record of a transaction or sale for the security. A quote tick is an offer to buy or sell a security for a specific price. <code>Tick</code> objects have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.Tick"></div>
 
 <p>Trade ticks have a non-zero value for the <code>Quantity</code> and <code>Price</code> properties but they a zero value for the <code>BidPrice</code>, <code>BidSize</code>, <code>AskPrice</code>, and <code>AskSize</code> properties. Quote ticks have non-zero values for <code>BidPrice</code> and <code>BidSize</code> properties or have non-zero values for <code>AskPrice</code> and <code>AskSize</code> properties. To check if a tick is a trade or a quote, use the <code>TickType</code> property.</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/tick_type.html"); ?>
 
-<p>To get the <code>Tick</code> objects in the <code>Slice</code>, index the <code>Ticks</code> property of the <code>Slice</code> with a <code>Symbol</code>. If the security doesn't actively trade or you are in the same time step as when you added the security subscription, the <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Slice</code> contains data for your security before you index the <code>Slice</code> with the security <code>Symbol</code>.</p>
+<p>To get the <code>Tick</code> objects in the <code>Slice</code>, index the <code>Ticks</code> property of the <code>Slice</code> with a <code>Symbol</code>. If the security doesn't actively trade or you are in the same time step as when you added the security subscription, the <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Slice</code> contains data for your security before you index the <code>Slice</code> with the security <code>Symbol</code>. In backtests, LEAN groups ticks into one millisecond buckets. In live trading, LEAN groups ticks into ~70 millisecond buckets.</p>
 
 <div class="section-example-container">
     <pre class="csharp">// Example of accessing Tick objects in OnData
