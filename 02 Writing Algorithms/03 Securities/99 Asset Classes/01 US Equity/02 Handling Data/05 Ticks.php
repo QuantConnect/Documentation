80cec347e4878b4a82714fd9d28604e8a077b9fa
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 16:03:19 -0600

    Update 05 Ticks.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/05 Ticks.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/05 Ticks.php
@@ -1,1 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/securities/tick.html"); ?>
+
+<p>If <a href='/docs/v2/our-platform/live-trading/data-feeds/us-equities#05-Suspicious-Ticks'>we detect a tick that may be suspicious</a>, we set its <code>Suspicious</code> flag to true.</p>
