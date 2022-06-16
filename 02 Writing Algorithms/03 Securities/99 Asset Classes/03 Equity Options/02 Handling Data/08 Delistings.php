3aa3235e9b66009e57e459c60ed3f3505fff675b
Web Editor <web.editor@quantconnect.com>
2022-06-16 19:31:54 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/08 Delistings.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/08 Delistings.php
@@ -1,6 +1,10 @@
-- For contract expiration
-<br>- Contracts are liquidated on expiration
-<br>- Show example of accessing delistings in OnData
-
+<p>When a contract expires, LEAN adds a <code>Delisting</code> object to the <code>Slice</code> and liquidates your holdings. <code>Delisting</code> objects have the following properties:</p>
 
 <div data-tree="QuantConnect.Data.Market.Delisting"></div>
+
+<p>To get the <code>Delisting</code> objects, loop through the <code>Delistings</code> property of the <code>Slice</code>.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Example of looping through Delistings</pre>
+    <pre class="python"># Example of looping through Delistings</pre>
+</div>
