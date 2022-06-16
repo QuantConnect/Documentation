f6f65bfc0601f0565e8ea39d8b5b27a21c125fe1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 15:57:02 -0600

    Update 01 Introduction.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/02 Handling Data/01 Introduction.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/02 Handling Data/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php 
-include(DOCS_RESOURCES."/securities/handling-data-intro.html"); 
+include(DOCS_RESOURCES."/securities/handling-data-php.html"); 
 $dataFormat = "TradeBar";
 $getIntroText($dataFormat);
 ?>
