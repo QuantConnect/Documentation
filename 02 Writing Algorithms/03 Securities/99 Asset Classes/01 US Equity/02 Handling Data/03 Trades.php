5595eafc3577abb2a494bc987b9ad39b6a3a2deb
Web Editor <web.editor@quantconnect.com>
2022-06-15 20:27:52 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/03 Trades.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/03 Trades.php
@@ -1,12 +1,18 @@
-<div>TradeBars</div><div>-Definition: individual trades from the exchanges consolidated into price bars<br></div><div>- Add image from Handling Data docs<br>- 
+<p><code>TradeBar</code> objects are individual trades from the exchanges that LEAN consolidates into price bars.</p>
+
+<img src="https://cdn.quantconnect.com/docs/i/dataformat-tradebar.png" class="img-responsive">
+
+<p><code>TradeBar</code> objects have the following properties:</p>
+<div data-tree="QuantConnect.Data.Market.TradeBar"></div>
+
+<p>To get the <code>TradeBar</code> objects in the <code>Slice</code>, </p>
+
+
 Daily/hour/minute bars are adjusted to open (close) with with the 
 official auction open (close) price. If there is no official auction 
 price received before we must emit the bar, we use the first/last trade</div><div>-If there is little trading, or you are in the same time loop as when 
 	you added the security, the time-slice may not have any data. <br></div><div>-Prices from tradebars are used fill trades in backtests (hour, daily resolutions)</div><div>&nbsp;&nbsp;&nbsp; - Opening &amp; closing prices are updated to the official opening/closing auction prices,&nbsp; used to fill MOO and MOC orders<br></div>
 
-<img src="https://cdn.quantconnect.com/docs/i/dataformat-tradebar.png" class="img-responsive">
-
-<div data-tree="QuantConnect.Data.Market.TradeBar"></div>
 
 <br>
 -Add snippet of accessing TradeBars in OnData
