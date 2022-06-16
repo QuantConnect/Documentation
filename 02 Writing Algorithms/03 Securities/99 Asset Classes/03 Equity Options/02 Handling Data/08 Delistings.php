4aa41c65d182ef39e95d95b9d4edbac78693d6b7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-16 17:54:53 -0600

    Update 08 Delistings.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/08 Delistings.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/08 Delistings.php
@@ -1,10 +1,1 @@
-<p>When a contract expires, LEAN adds a <code>Delisting</code> object to the <code>Slice</code> and liquidates your holdings. <code>Delisting</code> objects have the following properties:</p>
-
-<div data-tree="QuantConnect.Data.Market.Delisting"></div>
-
-<p>To get the <code>Delisting</code> objects, loop through the <code>Delistings</code> property of the <code>Slice</code>.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">// Example of looping through Delistings</pre>
-    <pre class="python"># Example of looping through Delistings</pre>
-</div>
+<?php echo file_get_contents(DOCS_RESOURCES."/securities/delistings.html"); ?>
