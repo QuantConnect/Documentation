9ecb94b7f620c036b4e6f5d099ec8e59e77b1a97
Web Editor <web.editor@quantconnect.com>
2022-06-17 18:27:41 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/03 Corporate Actions/02 Splits.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/03 Corporate Actions/02 Splits.php
@@ -1,1 +1,6 @@
-<?php include(DOCS_RESOURCES."/securities/corporate-actions/splits.php"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/corporate-actions/splits.php"); 
+$dataNormalizationModeLink = "/docs/v2/writing-algorithms/securities/asset-classes/india-equity/requesting-data#07-Data-Normalization";
+$factorFilesLink = "https://github.com/QuantConnect/Lean/tree/master/Data/equity/india/factor_files";
+$getSplitsText($dataNormalizationModeLink, $factorFilesLink);
+?>
