785c7a884251bb4f5d2180c35b20109937400db9
Web Editor <web.editor@quantconnect.com>
2022-06-17 13:22:28 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/09 Index/02 Handling Data/03 Ticks.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/09 Index/02 Handling Data/03 Ticks.php
@@ -1,7 +1,7 @@
 <p><code>Tick</code> objects represent a price for the Index at a moment in time. <code>Tick</code> objects have the following properties:</p>
 <div data-tree='QuantConnect.Data.Market.Tick'></div>
 
-<p>Index ticks have a non-zero value for the <code>Quantity</code> and <code>Price</code> properties, but they have a zero value for the <code>BidPrice</code>, <code>BidSize</code>, <code>AskPrice</code>, and <code>AskSize</code> properties.</p>
+<p>Index ticks have a non-zero value for the <code>Price</code> property, but they have a zero value for the <code>BidPrice</code>, <code>BidSize</code>, <code>AskPrice</code>, and <code>AskSize</code> properties.</p>
 
 <p> In backtests, LEAN groups ticks into one millisecond buckets. In live trading, LEAN groups ticks into ~70 millisecond buckets. To get the <code>Tick</code> objects in the <code>Slice</code>, index the <code>Ticks</code> property of the <code>Slice</code> with a <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code> at every time step. To avoid issues, check if the <code>Slice</code> contains data for your Index before you index the <code>Slice</code> with the Index <code>Symbol</code>.</p>
 
