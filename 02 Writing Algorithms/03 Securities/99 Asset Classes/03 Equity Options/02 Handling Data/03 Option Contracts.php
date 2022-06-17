9342497187dee2e3a71cc0a907dbff424175a9a7
Web Editor <web.editor@quantconnect.com>
2022-06-16 20:27:45 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/03 Option Contracts.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/03 Option Contracts.php
@@ -1,4 +1,4 @@
-<p><code>OptionContract</code> objects represent a single Option contract in the market. They have the following properties:</p>
+<p><code>OptionContract</code> objects represent the data of a single Option contract in the market. They have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.OptionContract"></div>
 
 <p>For more information about Greeks, see <a href='/tutorials/introduction-to-options/the-greek-letters'>The Greek Letters</a>.</p>
