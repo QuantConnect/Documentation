4f5e480cde7b7168b1a49edfce58665779a31a05
Web Editor <web.editor@quantconnect.com>
2022-06-16 20:13:40 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/09 Symbol Changes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/09 Symbol Changes.php
@@ -3,3 +3,10 @@
 <br>-Example of detecting symbol changes in OnData
 
 <div data-tree='QuantConnect.Data.Market.SymbolChangedEvent'></div>
+
+<div class="section-example-container">
+    <pre class="csharp"></pre>
+    <pre class="python">def OnData(self, data):
+    for changed_event in data.SymbolChangedEvents.Values:
+        self.Log(f"Contract rollover from {changed_event.OldSymbol} to {changed_event.NewSymbol}")</pre>
+</div>
