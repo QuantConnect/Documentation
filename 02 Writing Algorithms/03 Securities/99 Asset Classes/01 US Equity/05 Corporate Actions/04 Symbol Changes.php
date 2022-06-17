f970afe0e92bce4df0d80f635c1e9fd8d0420d54
Web Editor <web.editor@quantconnect.com>
2022-06-17 17:26:07 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/04 Symbol Changes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/04 Symbol Changes.php
@@ -1,6 +1,8 @@
-<div>-SymbolChangedEvents provides notice of new ticker names for stocks, or mergers of two tickers into one or reverse mergers</div><div>&nbsp;&nbsp;&nbsp; - Example: AOL TimeWarner became TimeWarner and changed their symbol from AOL to TWX.</div><div>- It provides the OldSymbol and NewSymbol tickers ; Show properties of SymbolChangedEvent class</div><div>&nbsp;&nbsp;&nbsp; - OldSymbol, NewSymbol</div><div>-accessed in the time-slice<br></div><div>-The benefit of our Symbol class is that ticker changes don't really matter, we're not passing strings of company ticker<br></div><div>-Full example: There are none in the repo (add example?)<br></div><div>-Data saved in map files</div><div></div>
-<br>-Open Orders get canceled in case of a SymbolChanged event. In this case, we recommend using the OnOrderEvent handler to get the order quantity and the symbol associated with the canceled order and resubmitting a new order.
+<p>When a company changes their trading ticker, LEAN sends a <code>SymbolChangedEvent</code> to the <code>OnData</code> method. <code>SymbolChangedEvent</code> objects have the following properties:</p>
 
+<div data-tree="QuantConnect.Data.Market.SymbolChangedEvent"></div>
+
+<p>The benefit of the <code>Symbol</code> class is that it always maps to the same security, regardless of their trading ticker.</p>
 
 <div class="section-example-container">
         <pre class="csharp">if (data.SymbolChangedEvents.ContainsKey(_googl))
@@ -11,4 +13,13 @@
     symbolChangedEvent = data.SymbolChangedEvents[self.googl]</pre>
     </div>
 
-<div data-tree="QuantConnect.Data.Market.SymbolChangedEvent"></div>
+<p>LEAN stores the data for ticker changes in map files. To view some example map files, see the <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/tree/master/Data/equity/usa/map_files'>LEAN GitHub repository</a>.</p>
+
+<p>If you have an open order for a security when they change their ticker, LEAN cancels your orders. To maintain your position, in the <code>OnOrderEvent</code> method, get the quantity and <code>Symbol</code> of the liquidation order and submit a new order.</p> 
+
+<div class="section-example-container">
+        <pre class="csharp">// Example ^^</pre>
+        <pre class="python">## Example ^^</pre>
+    </div>
+
+<div>-SymbolChangedEvents provides notice of new ticker names for stocks, or mergers of two tickers into one or reverse mergers</div><div>&nbsp;&nbsp;&nbsp; - Example: AOL TimeWarner became TimeWarner and changed their symbol from AOL to TWX.</div>
