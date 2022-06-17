fc20455591a9adf05ce09a22dbc181c72d5bc946
Web Editor <web.editor@quantconnect.com>
2022-06-17 18:27:03 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
@@ -1,1 +1,6 @@
-<?php include(DOCS_RESOURCES."/securities/corporate-actions/splits.php"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/corporate-actions/splits.php"); 
+$dataNormalizationModeLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#09-Data-Normalization";
+$factorFilesLink = "https://github.com/QuantConnect/Lean/tree/master/Data/equity/usa/factor_files";
+$getSplitsText($dataNormalizationModeLink, $factorFilesLink);
+?>
