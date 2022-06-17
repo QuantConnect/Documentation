2fcc9dbffcce23736a48ff6cdd6be583cf7c5c12
Web Editor <web.editor@quantconnect.com>
2022-06-16 20:12:19 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/03 Future Contracts.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/03 Future Contracts.php
@@ -1,7 +1,7 @@
-<p><code>FuturesContract</code> objects represent a single Future contract in the market. They have the following properties:</p>
+<p><code>FuturesContract</code> objects represent a single Futures contract in the market. They have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.FuturesContract"></div>
 
-<p>To get the Option contracts in the <code>Slice</code>, use the <code>Contracts</code> property of the <code>FuturesChain</code>.</p>
+<p>To get the Futures contracts in the <code>Slice</code>, use the <code>Contracts</code> property of the <code>FuturesChain</code>.</p>
 
 <div class="section-example-container">
     <pre class="csharp">// Example of accessing FuturesContract objects in OnData</pre>
