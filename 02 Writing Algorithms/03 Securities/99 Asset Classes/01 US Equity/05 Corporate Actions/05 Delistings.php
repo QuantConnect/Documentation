89200c907a7b6630e8873a063aa3a58aaf67a1b7
Web Editor <web.editor@quantconnect.com>
2022-06-17 17:58:08 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/05 Delistings.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/05 Delistings.php
@@ -1,15 +1,26 @@
--{ Delisting} events provide notice that an asset is no longer trading on the exchange. A delisting warning is issued on the final trading day for a stock delisting event to give your algorithm time to gracefully exit out of positions before forced termination. 
-<div>-Our data is survivorship-bias free<br></div><div>-Check if there are delistings in the current slice with data.Delistings</div><div>&nbsp;&nbsp;&nbsp; - returns key-value pairs</div><div>&nbsp;&nbsp;&nbsp; - value has a `.Value` that you can check against&nbsp; the DelistingType enum<br></div><div>- &lt;security&gt;.IsDelisted<br></div><div>-DelistingType (.Warning &amp; .Delisted)</div><div>-Accessed in the time-slice<br></div><div>-Example: DelistingEventsAlgorithm.py</div><div>-"We could also make a short tutorial on delisting. Some people save the symbols in list (to make history requests), they should remove the security from that list too."<br></div><div></div>
+<p>When a company stops trading on an exchange, it's delisted. <code>Delisting</code> objects have the following properties:</p>
+<div data-tree="QuantConnect.Data.Market.Delisting"></div>
+
+
+<p>You receive <code>Delisting</code> objects when a delisting is in the near future and when it occurs. To know if the delisting occurs in the near future or now, check the <code>Type</code> property.</p>
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/delisting_type.html"); ?>
+
+<p>To get the <code>Delisting</code> objects in the <code>Slice</code>, index the <code>Delistings</code> property of the <code>Slice</code> with the security <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Delistings</code> property contains data for your security before you index it with the security <code>Symbol</code>.</p>
 
 <div class="section-example-container">
-        <pre class="csharp">if (data.Delistings.ContainsKey(_msft))
+        <pre class="csharp">if (data.Delistings.ContainsKey(_symbol))
 {
-    var delisting = data.Delistings[_msft];
+    var delisting = data.Delistings[_symbol];
 }</pre>
-        <pre class="python">if data.ContainsKey(self.msft):
-    delisting = data.Delistings[self.msft]</pre>
-    </div>
--^ Add SetOrderTicket method and add the Ticket member to get the order ticket that liquidates the position
-<br>-For a full example, see the DelistingEventsAlgorithm.
+        <pre class="python">if data.ContainsKey(self.symbol):
+    delisting = data.Delistings[self.symbol]</pre>
+</div>
 
-<div data-tree="QuantConnect.Data.Market.Delisting"></div>
+<p>The delist warning occurs on the final trading day of the stock to give you time to gracefully exit out of positions before LEAN automatically liquidates them. To set the ticket for the order that liquidates the position, call the <code>SetOrderTicket</code> method on the <code>Delisting</code> object.</p>
+
+<div class="section-example-container">
+        <pre class="csharp">// Example ^^</pre>
+        <pre class="python"># Example ^^</pre>
+</div>
+
+<p>For a full example, see the <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.CSharp/DelistingEventsAlgorithm.cs' class='csharp'>DelistingEventsAlgorithm</a><a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.Python/DelistingEventsAlgorithm.py' class='python'>DelistingEventsAlgorithm</a> in the LEAN GitHub repository.</p>
