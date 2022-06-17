20293951a70b43779fd8180eeb7d42dc74bdea1d
Web Editor <web.editor@quantconnect.com>
2022-06-17 12:46:31 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/02 Futures Chains.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/02 Futures Chains.php
@@ -1,4 +1,4 @@
-<p><code>FuturesChain</code> objects represent and entire chain of contracts for a single underlying Future. They have the following properties:</p>
+<p><code>FuturesChain</code> objects represent an entire chain of contracts for a single underlying Future. They have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.FuturesChain"></div>
 
 <p>To get the <code>FuturesChain</code>, index the <code>FuturesChains</code> property of the <code>Slice</code> with the continuous contract <code>Symbol</code>.</p>
