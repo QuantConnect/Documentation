ccd1dc7e6b3025cd922fbac06d0a1157d04a4ce0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-17 10:19:17 -0600

    Update 07 Quotes.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/07 Quotes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/07 Quotes.php
@@ -1,1 +1,7 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/securities/quotebar.html"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/quotebar.php"); 
+$securityName = "Option contract";
+$pythonVariable = "self.option_contract_symbol";
+$cSharpVariable = "_optionContractSymbol";
+$getQuoteBarText($securityName, $pythonVariable, $cSharpVariable);
+?>
