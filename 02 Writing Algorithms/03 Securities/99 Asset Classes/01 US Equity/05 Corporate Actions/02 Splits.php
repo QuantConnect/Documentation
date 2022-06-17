8f01414c2af45e5025a1550f47e0aed203daec46
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-17 14:18:57 -0600

    Update 02 Splits.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
@@ -1,12 +1,10 @@
-<p>When a company does a stock split, it increases the number of shares each shareholder owns and decreases the price of each share. When a company does a reverse stock split, it decreases the number of shares each shareholder owns and increases the price of each share. A company may perform a stock split or a reverse stock split to adjust the price of their stock so that more investors trade it and the liquidity increases.</p>
+<p>When a company does a stock split, the number of shares each shareholder owns increases and the price of each share decreases. When a company does a reverse stock split, the number of shares each shareholder owns decreases and the price of each share increases. A company may perform a stock split or a reverse stock split to adjust the price of their stock so that more investors trade it and the liquidity increases.</p>
 
 <p>When a stock split or reverse stock split occurs for an Equity in your algorithm, LEAN sends a <code>Split</code> object to the <code>OnData</code> method. </code>Split</code> objects have the following properties:</p>
 <div data-tree='QuantConnect.Data.Market.Split'></div>
 
-<p>To check if a <code>Split</code> is a regular stock split or a reverse stock split, use its <code>Type</code> property.</p>
+<p>You receive <code>Split</code> objects when a split is in the near future and when it occurs. To know if the split occurs in the near future or now, check the <code>Type</code> property.</p>
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/split_type.html"); ?>
-
-
 
 <p>To get the <code>Split</code> objects in the <code>Slice</code>, index the <code>Splits</code> property of the <code>Split</code> with the security <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Split</code> contains data for your security before you index the <code>Split</code> with the security <code>Symbol</code>.</p>
 
@@ -19,10 +17,13 @@
     split = data.Splits[self.symbol]</pre>
 </div>
 
+<p>LEAN stores the data for stock splits in factor files. To view some example factor files, see the <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/tree/master/Data/equity/usa/factor_files'>LEAN GitHub repository</a>.</p>
+
+<p>In backtests, your algorithm receives <code>Split</code> objects at midnight. In live trading, your algorithm receives <code>Split</code> objects when the factor files are ready.</p>
 
 
 
-<div>- In backtesting, they are streamed to your algorithm in a Slice object that is emitted at midnight</div><div>- Splits are handled by the engine depeding on the data normalization mode or whether the algorithm is running in live mode<br>&nbsp;&nbsp; - Raw data and live mode:<br>&nbsp;&nbsp; - It's information is used by the engine to adjust the quantity of the positions accordingly ("SplitOccurred")<br>&nbsp;&nbsp; - If the quantity is not a valid lot size, the remaining value is credited to your account currency.<br>
+<div>- Splits are handled by the engine depeding on the data normalization mode or whether the algorithm is running in live mode<br>&nbsp;&nbsp; - Raw data and live mode:<br>&nbsp;&nbsp; - It's information is used by the engine to adjust the quantity of the positions accordingly ("SplitOccurred")<br>&nbsp;&nbsp; - If the quantity is not a valid lot size, the remaining value is credited to your account currency.<br>
 - Other mode with backtesting:<br>&nbsp;&nbsp; - The splts are factored into the price and volume.<br>
 
 Splits close all options positions. <br></div>
