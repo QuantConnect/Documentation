b0a8c6d42fc7b67fe7056559e421d3049c25a117
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-17 10:55:34 -0600

    Update 02 Option Chains.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/02 Option Chains.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/02 Option Chains.php
@@ -1,16 +1,1 @@
-<p><code>OptionChain</code> objects represent and entire chain of Option contracts for a single underlying security. They have the following properties:</p>
-<div data-tree="QuantConnect.Data.Market.OptionChain"></div>
-
-
-<p>To get the <code>OptionChain</code>, index the <code>OptionChains</code> property of the <code>Slice</code> with the canonical <code>Symbol</code>.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">// Example of getting the OptionChain in OnData</pre>
-    <pre class="python"># Example of getting the OptionChain in OnData</pre>
-</div>
-
-<p>You can also loop through the <code>OptionChains</code> property to get each <code>OptionChain</code>.</p>
-<div class="section-example-container">
-    <pre class="csharp">// Example of looping through OptionChains</pre>
-    <pre class="python"># Example of looping through OptionChains</pre>
-</div>
+<?php echo file_get_contents(DOCS_RESOURCES."/securities/option-chains.html"); ?>
