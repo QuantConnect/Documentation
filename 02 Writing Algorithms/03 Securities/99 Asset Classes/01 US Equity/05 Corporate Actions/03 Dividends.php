4a55aee2eb63f21da7f9327771975b27dda506c8
Web Editor <web.editor@quantconnect.com>
2022-06-17 16:56:16 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/03 Dividends.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/03 Dividends.php
@@ -1,36 +1,17 @@
-<p>
-    $[Dividend,T:QuantConnect.Data.Market.Dividend] events are triggered on payment of a dividend. It provides the Distribution per share.
-</p>
-<div class="section-example-container">
-<pre class="python">def Initialize(self) -&gt; None:
-    self.SetStartDate(2017, 6, 1)
-    self.SetEndDate(2017, 6, 28)
-    self.spy = self.AddEquity("SPY", Resolution.Hour) 
-    
-def OnData(self, data: Slice) -&gt; None:
-    if not self.Portfolio.Invested:
-        self.Buy("SPY", 100)
-    
-    ## Condition to see if SPY is in the Dividend DataDictionary
-    if data.Dividends.ContainsKey("SPY"):
-        ## Log the dividend distribution
-        self.Log(f"SPY paid a dividend of {data.Dividends['SPY'].Distribution}")
-   
-</pre>
-</div>
-
-## TODO: <br>
-- Update to use Symbol
-<br>- ToString()
-<br><div>- Add ReferencePrice member <br></div><div><br></div><div>-By default, data is dividend adjusted. When using Raw or SplitAdjusted data, divdend payments are added as cash to your portfolio</div><div>-Include a high-level definition of what a dividend is (?)<br></div><div>-Dividend events are triggered on payment of a dividend.</div><div>-Show properties of dividend class</div><div>&nbsp;&nbsp;&nbsp; - Includes Distribution and ReferencePrice</div><div>-Access from the time-slice<br></div><div>-Full example: DividendAlgorithm.Py</div><div>-Data saved in factor files<br></div><div></div>
-
-<div class="section-example-container">
-        <pre class="csharp">if (data.Dividends.ContainsKey(_googl))
-{
-    var dividend = data.Dividends[_googl];
-}</pre>
-        <pre class="python">if self.googl in data.Dividends:<br>    dividend = data.Dividends[self.googl]</pre>
-    </div>
-- For a full example, see the DividendAlgorithm
-
+<p>A dividend is a payment that a company gives to shareholders to distribute profits. When a dividend payment occurs for an Equity in your algorithm, LEAN sends a <code>Dividend</code> object to the <code>OnData</code> method. <code>Dividend</code> objects have the following properties:</p>
 <div data-tree="QuantConnect.Data.Market.Dividend"></div>
+
+<p>By default, data is adjusted for dividends. If you use the <code>Raw</code> or <code>SplitAdjusted</code> data normalization mode, LEAN adds dividend payments to your cashbook.</p>
+
+<p>To get the <code>Dividend</code> objects in the <code>Slice</code>, index the <code>Dividends</code> property of the <code>Slice</code> with the security <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Dividends</code> property contains data for your security before you index the <code>Dividends</code> property with the security <code>Symbol</code>.</p>
+
+<div class="section-example-container">
+        <pre class="csharp">if (data.Dividends.ContainsKey(_symbol))
+{
+    var dividend = data.Dividends[_symbol];
+}</pre>
+        <pre class="python">if self.symbol in data.Dividends:
+    dividend = data.Dividends[self.symbol]</pre>
+    </div>
+
+<p>For a full example, see the <a rel='nofollow' target='_blank' class='csharp' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.CSharp/DividendAlgorithm.cs'>DividendAlgorithm</a><a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Algorithm.Python/DividendAlgorithm.py' class='python'>DividendAlgorithm</a> in the LEAN GitHub repository.</p>
