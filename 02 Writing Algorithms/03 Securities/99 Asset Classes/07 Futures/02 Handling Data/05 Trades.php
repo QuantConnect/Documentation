8569ecea89a0e36965c32cbf95474fb1294edad0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-17 11:07:30 -0600

    Update 05 Trades.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/05 Trades.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/05 Trades.php
@@ -1,7 +1,9 @@
 <?php 
 include(DOCS_RESOURCES."/securities/tradebar.php"); 
 $securityName = "contract";
-$pythonVariable = "self.contract_symbol";
+$pythonVariable = "self.contract_symbol
+data[future.Symbol].Price # raw contract price
+data[future.Mapped] # continous contract price w/ adjustments";
 $cSharpVariable = "_contractSymbol";
 $getTradeBarText($securityName, $pythonVariable, $cSharpVariable);
 ?>
