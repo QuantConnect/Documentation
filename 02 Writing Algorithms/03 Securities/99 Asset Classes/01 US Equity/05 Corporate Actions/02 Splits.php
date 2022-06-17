cf4e4ad65c0492cc785cc545d80bdfcee9fc92e6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-17 14:57:42 -0600

    Update 02 Splits.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/02 Splits.php
@@ -6,6 +6,8 @@
 <p>You receive <code>Split</code> objects when a split is in the near future and when it occurs. To know if the split occurs in the near future or now, check the <code>Type</code> property.</p>
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/split_type.html"); ?>
 
+<p>If you backtest without the <code>Raw</code> <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#09-Data-Normalization'>data normalization mode</a>, the splits are factored into the price and volume. If you backtest with the <code>Raw</code> data normalization mode or trade live, when a split occurs, LEAN automatically adjusts your positions based on the <code>SplitFactor</code>. If the post-split quantity isn't a valid <a href='/docs/v2/writing-algorithms/securities/properties#50-Symbol-Properties'>lot size</a>, LEAN credits the remaining value to your <a href='/docs/v2/writing-algorithms/portfolio/cashbook'>cashbook</a> in your account currency. If you have indicators in your algorithm, <a href='/docs/v2/writing-algorithms/indicators/key-concepts#09-Reset-Indicators'>reset your indicators</a> when split occur so that the data in your indicators account for the price adjustments that the split causes.</p>
+
 <p>To get the <code>Split</code> objects in the <code>Slice</code>, index the <code>Splits</code> property of the <code>Slice</code> with the security <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code>. To avoid issues, check if the <code>Split</code> contains data for your security before you index the <code>Split</code> with the security <code>Symbol</code>.</p>
 
 <div class="section-example-container">
@@ -19,7 +21,6 @@
 
 <p>LEAN stores the data for stock splits in factor files. To view some example factor files, see the <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/tree/master/Data/equity/usa/factor_files'>LEAN GitHub repository</a>. In backtests, your algorithm receives <code>Split</code> objects at midnight. In live trading, your algorithm receives <code>Split</code> objects when the factor files are ready.</p>
 
-<p>If you backtest without the <code>Raw</code> <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#09-Data-Normalization'>data normalization mode</a>, the splits are factored into the price and volume. If you backtest with the <code>Raw</code> data normalization mode or trade live, when a split occurs, LEAN automatically adjusts your positions based on the <code>SplitFactor</code>. If the post-split quantity isn't a valid <a href='/docs/v2/writing-algorithms/securities/properties#50-Symbol-Properties'>lot size</a>, LEAN credits the remaining value to your <a href='/docs/v2/writing-algorithms/portfolio/cashbook'>cashbook</a> in your account currency. If you have indicators in your algorithm, <a href='/docs/v2/writing-algorithms/indicators/key-concepts#09-Reset-Indicators'>reset your indicators</a> when split occur so that the data in your indicators account for the price adjustments that the split causes.</p>
 
 <p>If you hold an Option contract for an underlying Equity when a split occurs, LEAN closes your Option contract position.</p>
 
