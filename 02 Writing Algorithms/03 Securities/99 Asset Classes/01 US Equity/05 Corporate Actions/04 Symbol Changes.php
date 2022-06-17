f464c3b2a87a2747294bcce8c8649800584756ed
Web Editor <web.editor@quantconnect.com>
2022-06-17 18:30:56 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/04 Symbol Changes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/05 Corporate Actions/04 Symbol Changes.php
@@ -1,1 +1,5 @@
-<?php include(DOCS_RESOURCES."/securities/corporate-actions/symbol-changes.php"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/corporate-actions/symbol-changes.php"); 
+$mapFilesLink = "https://github.com/QuantConnect/Lean/tree/master/Data/equity/usa/map_files";
+$getSymbolChangesText($mapFilesLink);
+?>
