d3ca47af08d85e1db921a888f19cd4b10a3fea63
Web Editor <web.editor@quantconnect.com>
2022-06-17 12:44:41 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/09 Symbol Changes.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/09 Symbol Changes.php
@@ -1,8 +1,8 @@
--For contract rollover of the continuous contract
-<br>-Determined by the dataMappingMode when you call AddFuture
-<br>-Example of detecting symbol changes in OnData
+<p>When the <a href='/docs/v2/writing-algorithms/universes/futures#12-Continous-Contracts'>continuous contract</a> rolls over, LEAN passes a <code>SymbolChangedEvent</code> to your <code>OnData</code> method, which contains the old contract <code>Symbol</code> and the new contract <code>Symbol</code>. <code>SymbolChangedEvent</code> have the following properties:</p>
 
 <div data-tree='QuantConnect.Data.Market.SymbolChangedEvent'></div>
+
+<p>To get the <code>SymbolChangedEvent</code>, use the <code>SymbolChangedEvents</code> property of the <code>Slice</code>.</p>
 
 <div class="section-example-container">
     <pre class="csharp"></pre>
