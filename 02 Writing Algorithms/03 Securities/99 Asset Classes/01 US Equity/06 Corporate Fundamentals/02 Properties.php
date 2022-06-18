dd0e779457d6d3550587d854b90c5999de06903a
Web Editor <web.editor@quantconnect.com>
2022-06-17 20:21:48 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/06 Corporate Fundamentals/02 Properties.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/06 Corporate Fundamentals/02 Properties.php
@@ -1,8 +1,8 @@
-<br>How do I access the properties:
-<br>-You need to add a coarse-fine universe to your algorithm. See Universes &amp; Universe Selection.
-<br>-After you have added the universe, you can access fundamentals through &lt;security&gt;.Fundamentals.
-<br>-You can also access the fundamentals through the universe selection method/class. This is covered in the Universes &amp; Universe Selection docs.
-<br><br>-Add code snippet of accessing the fundamental properties
+<p>To get fundamental data, access the <code>FineFundamental</code> properties in your fine universe selection function or access the <code>Fundamentals</code> property of the <code>Equity</code> objects in your fundamental universe.
 
+<div class='section-example-container'>
+        <pre class='csharp'>var fundamentals = Securities[_symbol].Fundamentals;</pre>
+        <pre class='python'>fundamentals = self.Securities[self.symbol].Fundamentals</pre>
+</div>
 
 <?php include(DOCS_RESOURCES."/datasets/data-point-attributes/fundamentals/enums.html"); ?>
