c9a6fa88518a55e0e190005d7306f80c8a5af6fa
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-20 11:24:31 -0600

    Delete 04 Universe Delivery.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/99 Live Trading/04 Universe Delivery.php
+++
@@ -1,4 +0,0 @@
-^^ Move to Universes > Equity > Live Trading Considerations
-<br>
-
-## Import Corase-Fine Universe text from https://www.quantconnect.com/docs/v2/our-platform/live-trading/data-feeds/us-equities#03-Universe-Selection
