5f2c80a05664e22c02027d7da94c576ac9167d72
LouisSzeto <hke0073@icloud.com>
2022-06-21 23:42:53 +0800

    Add example snippets

--- 02 Writing Algorithms/12 Universes/10 Custom Universes/03 Define Custom Universe Types.php
+++ 02 Writing Algorithms/12 Universes/10 Custom Universes/03 Define Custom Universe Types.php
@@ -2,48 +2,54 @@
 
 <p>The <code>GetSource</code> method in your custom data class instructs LEAN where to find the data. This method must return a <code>SubscriptionDataSource</code> object, which contains the data location and format (<code>SubscriptionTransportMedium</code>). You can even change source locations for backtesting and live modes. We support many different data sources.</p>
 
-
 <p>The <code>Reader</code> method of your custom data class takes one line of data from the source location and parses it into one of your custom objects. You can add as many properties to your custom data objects as you need, but must set <code>Symbol</code> and <code>EndTime</code> properties. When there is no useable data in a line, the method should return <code class="csharp">null</code><code class="python">None</code>. LEAN repeatedly calls the <code>Reader</code> method until the date/time advances or it reaches the end of the file. <br></p>
 <div class="section-example-container">
 <pre class="csharp">//Example custom universe data; it is virtually identical to other custom data types.
-public class NyseTopGainers : BaseData 
+public class MyCustomUniverseDataClass : BaseData 
 {
-    public int TopGainersRank;
+    public int CustomAttribute1;
+    public decimal CustomAttribute2;
     public override DateTime EndTime {
         // define end time as exactly 1 day after Time
-	get { return Time + QuantConnect.Time.OneDay; }
-	set { Time = value - QuantConnect.Time.OneDay; }
+	    get { return Time + QuantConnect.Time.OneDay; }
+	    set { Time = value - QuantConnect.Time.OneDay; }
     }
 
     public override SubscriptionDataSource GetSource(SubscriptionDataConfig config, DateTime date, bool isLiveMode) {
         return new SubscriptionDataSource(@"your-remote-universe-data", SubscriptionTransportMedium.RemoteFile);
-     }
+    }
 
-     public override BaseData Reader(SubscriptionDataConfig config, string line, DateTime date, bool isLiveMode) {
-         // Generate required data, then return an instance of your class.
-        return new NyseTopGainers {
-            Symbol = Symbol.Create(symbolString, SecurityType.Equity, Market.USA),
+    public override BaseData Reader(SubscriptionDataConfig config, string line, DateTime date, bool isLiveMode) {
+        var items = line.Split(",");
+
+        // Generate required data, then return an instance of your class.
+        return new MyCustomUniverseDataClass {
+            Symbol = Symbol.Create(items[0], SecurityType.Crypto, Market.Bitfinex),
             Time = date,
-            TopGainersRank = rank
+            CustomAttribute1 = int.Parse(items[1]),
+            CustomAttribute2 = decimal.Parse(items[2], NumberStyles.Any, CultureInfo.InvariantCulture)
         };
     }
 }
 </pre>
 <pre class="python"># Example custom universe data; it is virtually identical to other custom data types.
-class NyseTopGainers(PythonData):
+class MyCustomUniverseDataClass(PythonData):
 
     def GetSource(self, config: SubscriptionDataConfig, date: datetime, isLiveMode: bool) -&gt; SubscriptionDataSource:
         return SubscriptionDataSource(@"your-remote-universe-data", SubscriptionTransportMedium.RemoteFile)
 
     def Reader(self, config: SubscriptionDataConfig, line: str, date: datetime, isLiveMode: bool) -&gt; BaseData:
+        items = line.split(",")
+    
         # Generate required data, then return an instance of your class.
-        nyse = NyseTopGainers()
-        nyse.Time = date
+        data = MyCustomUniverseDataClass()
+        data.Time = date
         # define end time as exactly 1 day after Time
-        nyse.EndTime = nyse.Time + timedelta(1)
-        nyse.Symbol = Symbol.Create(symbolString, SecurityType.Equity, Market.USA)
-        nyse["Rank"] = rank
-        return nyse
+        data.EndTime = data.Time + timedelta(1)
+        data.Symbol = Symbol.Create(items[0], SecurityType.Crypto, Market.Bitfinex)
+        data["CustomAttribute1"] = int(items[1])
+        data["CustomAttribute2"] = float(items[2])
+        return data
 </pre>
 </div>
 
