efda4bb1658dfd29e0f319b845b117e67d66c642
Web Editor <web.editor@quantconnect.com>
2022-06-21 19:39:22 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
+++ 02 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
@@ -1,11 +1,18 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/time-zone.html"); ?>
 
-<p>To get the time zone of your algorithm, use the <code>TimeZone</code> property of your algorithm. To get the algorithm time in the Coordinated Universal Time (UTC), use the <code>UtcTime</code> property.</p>
+<p>To get the time zone of your algorithm, use the <code>TimeZone</code> property of your algorithm.</p>
 
 <div class="section-example-container">
-<pre class="python">time_zone = self.TimeZone
-utc_time = self.UtcTime</pre>
-<pre class="csharp">var timeZone = TimeZone;
-var utcTime = UtcTime;</pre>
+<pre class="python">time_zone = self.TimeZone</pre>
+<pre class="csharp">var timeZone = TimeZone;</pre>
 </div> 
+
+ <p>To get the algorithm time in the Coordinated Universal Time (UTC), use the <code>UtcTime</code> property.</p>
+
+
+<div class="section-example-container">
+<pre class="python">utc_time = self.UtcTime</pre>
+<pre class="csharp">var utcTime = UtcTime;</pre>
+</div> 
+
 <p>The <code>Time</code> and <code>UtcTime</code> objects have no time zone. LEAN maintains their state to be consistent.</p>
