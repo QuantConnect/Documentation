940e0f708868846e4932c66d2e3cec568d8660e4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-20 18:30:24 -0600

    Update hypothesis-driven-research.php

--- Resources/research-guide/hypothesis-driven-research.php
+++ Resources/research-guide/hypothesis-driven-research.php
@@ -81,5 +81,5 @@
    </tbody>
 </table>
 <p>
-   There are millions of potential alpha strategies to explore, each of them a candidate for an algorithm. Once you have chosen a strategy, we recommend exploring it no more than 8-32 hours, depending on your coding ability.
+   There are millions of potential alpha strategies to explore, each of them a candidate for an algorithm. Once you have chosen a strategy, we recommend exploring it for no more than 8-32 hours, depending on your coding ability.
 </p>
