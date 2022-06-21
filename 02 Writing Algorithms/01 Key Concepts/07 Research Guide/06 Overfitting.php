a55875f620676007dcbde43c77534d33b32448d3
Web Editor <web.editor@quantconnect.com>
2022-06-20 20:32:18 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/01 Key Concepts/07 Research Guide/06 Overfitting.php
+++ 02 Writing Algorithms/01 Key Concepts/07 Research Guide/06 Overfitting.php
@@ -1,6 +1,6 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/research-guide/overfitting.php"); ?>
 
-<p>If you have a collection of factors, you can backtest over a period of time to find the best performing factors for the time period. If you then narrow the collection of factors to just the best performing ones and backtest over the same period, the backtest will show great results. However, if you take the same best-performing factors and backtest them on an out-of-sample dataset, the performance will almost always underperform the in-sample period. To avoid issues with overfitting, follow these guidelines:</p>
+<p>If you have a collection of factors, you can backtest over a period of time to find the best-performing factors for the time period. If you then narrow the collection of factors to just the best-performing ones and backtest over the same period, the backtest will show great results. However, if you take the same best-performing factors and backtest them on an out-of-sample dataset, the performance will almost always underperform the in-sample period. To avoid issues with overfitting, follow these guidelines:</p>
 
 <ul>
     <li>Use walk-forward optimization to train your models on historical data and test them on future data.</li>
