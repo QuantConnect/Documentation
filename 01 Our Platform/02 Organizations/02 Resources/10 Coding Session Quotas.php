6a9d9704238c183c59df5a63eff69a2504bab0a4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-23 14:21:04 -0600

    Update 10 Coding Session Quotas.php

--- 01 Our Platform/02 Organizations/02 Resources/10 Coding Session Quotas.php
+++ 01 Our Platform/02 Organizations/02 Resources/10 Coding Session Quotas.php
@@ -1,5 +1,1 @@
-<p>If you have a project open, it uses a coding session. Your account can have up to one coding session across all of your free organizations. If you connect as a paid organization, your account can have multiple active coding sessions. The following table shows how many active coding sessions you can have on each organization tier:</p>
-
-<?php echo file_get_contents(DOCS_RESOURCES."/quotas/coding-sessions.html"); ?>
-
-<p>The quota for free organizations is a global quota, so you can have one active coding session across all of your free organizations. The quotas for paid organizations are at the organization level. Therefore, if you are in two Quant Researcher organizations, you can have two active coding sessions in one of those organizations and another two active sessions in the other organization. These paid tier quotas are for each account, not for the organization as a whole. For instance, a Trading Firm organization can have more than eight members and all of the members can simultaneously work on projects within the organization.</p>
+<?php echo file_get_contents(DOCS_RESOURCES."/environments/quota.html"); ?>
