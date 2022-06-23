83dcd2d4371d0e581bbbd95280a4b19e0da529f8
Web Editor <web.editor@quantconnect.com>
2022-06-23 12:15:04 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 Organizations/02 Resources/10 Coding Session Quotas.php
+++ 01 Our Platform/02 Organizations/02 Resources/10 Coding Session Quotas.php
@@ -1,3 +1,5 @@
-<p>When you <a href='/docs/v2/our-platform/projects/project-management#02-View-All-Projects'>open a project</a>, it starts a coding session. When you close the project, the coding session stops. You can have up to one coding session across all of your free organizations. If you connect as an paid organization, you can have multiple coding sessions open. The following table shows how many coding sessions you can have open on each organization tier:</p>
+<p>If you have a project open, it uses a coding session. You can have up to one coding session across all of your free organizations. If you connect as an paid organization, you can have multiple coding sessions open. The following table shows how many coding sessions you can have open on each organization tier:</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/quotas/coding-sessions.html"); ?>
+
+<p>The quota for free organizations is a global quota, so you can open one coding session for all of your free organizations. The quotas for paid organizations are at the organization level. For instance, if you are in two Quant Researcher organizations, you can open two coding sessions in one of those organizations and another two sessions in the other organization.</p>
