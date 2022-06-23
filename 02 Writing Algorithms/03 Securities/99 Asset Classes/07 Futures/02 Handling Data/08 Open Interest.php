3e5d9af8b16543ee6063c8d8359ccf8fa4a73589
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-23 15:28:14 -0600

    Update 08 Open Interest.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/08 Open Interest.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/08 Open Interest.php
@@ -8,4 +8,4 @@ $getOpenInterestText($contractTypeName, $chainTypeName, $variableName);
 
 
 
-<p class='csharp'>If you define an <code>OnData</code> method that accepts a <code>FuturesChains</code> objects, LEAN doesn't currently call it. We have an open GitHub Issue to add this functionality. Subscribe to <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/issues/6419'>GitHub Issue #6419</a> to track the feature progress.</p>
+<p class='csharp'>If you define an <code>OnData</code> method that accepts a <code>FuturesChains</code> object, LEAN doesn't currently call it. We have an open GitHub Issue to add this functionality. Subscribe to <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/issues/6419'>GitHub Issue #6419</a> to track the feature progress.</p>
