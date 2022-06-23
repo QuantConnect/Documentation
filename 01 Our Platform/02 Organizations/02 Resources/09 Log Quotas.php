e7e37ba90e445fceaae9579f520836e04b515879
Web Editor <web.editor@quantconnect.com>
2022-06-23 12:55:13 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/02 Organizations/02 Resources/09 Log Quotas.php
+++ 01 Our Platform/02 Organizations/02 Resources/09 Log Quotas.php
@@ -3,4 +3,6 @@
 
 <p>If you delete a backtest or project that produced logs, your quotas aren't restored. Additionally, daily log quotas aren't fully restored at midnight. They are restored according to a 24-hour following window. </p>
 
+<p>The log files of each project can store up to 100,000 lines for up to one year. If you log more than 100,000 lines or some lines become older than one year, we remove the oldest lines in the files so your project stays within the quota.</p>
+
 <p>To avoid reaching the limits, we recommend logging sparsely, focusing on the change events instead of logging every time loop. You can use the <a href='/docs/v2/our-platform/user-guides/projects/debugging#03-Debugger'>debugger</a> to inspect objects during runtime. If you use the debugger, you should rarely reach the log limits.</p>
