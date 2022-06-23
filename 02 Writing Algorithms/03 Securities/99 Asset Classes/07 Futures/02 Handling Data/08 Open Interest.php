752f4c295df5e0bdb25f4ce2681c64984a42c309
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-23 15:25:39 -0600

    Update 08 Open Interest.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/08 Open Interest.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/08 Open Interest.php
@@ -5,3 +5,7 @@ $chainTypeName = "FuturesChains";
 $variableName = "futuresChains";
 $getOpenInterestText($contractTypeName, $chainTypeName, $variableName);
 ?>
+
+
+
+<p class='csharp'>If you define an <code>OnData</code> method that accepts a <code>FuturesChains</code> objects, LEAN doesn't currently call it. We have an open GitHub Issue to add this functionality. Subscribe to <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/issues/6419'>GitHub Issue #6419</a> to track the feature progress.</p>
