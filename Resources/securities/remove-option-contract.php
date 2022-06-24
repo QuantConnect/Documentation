75bc4591b12864f5c54591832754bc0417eb6578
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 15:04:50 -0600

    Update and rename remove-option-contract.html to remove-option-contract.php

--- Resources/securities/remove-option-contract.php
+++ Resources/securities/remove-option-contract.php
@@ -1,8 +1,16 @@
+<?php
+$getRemoveOptionContractText = function($pythonVariableName, $cSharpVariableName)
+{
+    echo "
 <p>To remove a contract subscription that you created with <code>AddOptionContract</code>, call the <code>RemoveOptionContract</code> method. This method is an alias for <code>RemoveSecurity</code>.</p>
 
-<div class="section-example-container">
-    <pre class="csharp">RemoveOptionContract(_contractSymbol);</pre>
-    <pre class="python">self.RemoveOptionContract(self.contract_symbol)</pre>
+<div class='section-example-container'>
+    <pre class='csharp'>RemoveOptionContract(_contractSymbol);</pre>
+    <pre class='python'>self.RemoveOptionContract(self.contract_symbol)</pre>
 </div>
 
 <p>The <code>RemoveOptionContract</code> method cancels your open orders for the contract and liquidates your holdings.</p>
+";
+    
+}
+?>
