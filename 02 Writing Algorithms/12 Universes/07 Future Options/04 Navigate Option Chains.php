76d44c68db597e7fa0e47383507770e7829ab0ef
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 14:21:35 -0600

    Update and rename 04 Navigate Option Chains.html to 04 Navigate Option Chains.php

---
+++ 02 Writing Algorithms/12 Universes/07 Future Options/04 Navigate Option Chains.php
@@ -0,0 +1,7 @@
+<?php 
+include(DOCS_RESOURCES."/securities/option-chains.php"); 
+$isFutureOptionUniverse = true;
+$pythonMemberName = "";
+$cSharpMemberName = "";
+$getOptionChainsText($isFutureOptionUniverse, $pythonMemberName, $cSharpMemberName);
+?>
