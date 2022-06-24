d37772079137cb4c60d6af9f4c328b28ad66b194
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 10:51:54 -0600

    Update and rename 02 Future Option Universes.php to 02 Create Universes.php

---
+++ 02 Writing Algorithms/12 Universes/07 Future Options/02 Create Universes.php
@@ -0,0 +1,40 @@
+<p>To add a universe of Future Option contracts, in the <code>Initialize</code> method, <a href="/docs/v2/writing-algorithms/universes/futures#11-Future-Universes">define a Future universe</a> and then pass the canonical <code>Symbol</code> to the <code>AddFutureOption</code> method.<br></p>
+
+<div class="section-example-container">
+    <pre class="csharp">var future = AddFuture(Futures.Currencies.BTC);
+future.SetFilter(0, 90);
+AddFutureOption(future.Symbol);</pre>
+    <pre class="python">future = self.AddFuture(Futures.Currencies.BTC)
+future.SetFilter(0, 90)
+self.AddFutureOption(future.Symbol)</pre>
+</div>
+
+
+<p>The following table describes the <code>AddFutureOption</code> method arguments:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>Data Type</th>
+            <th>Description</th>
+            <th>Default Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>symbol</code></td>
+	        <td><code>Symbol</code></td>
+            <td>The Future canonical symbol</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><code>optionFilter</code></td>
+	        <td><code class="csharp">Func&lt;OptionFilterUniverse, OptionFilterUniverse&gt;</code><code class="python">callable[OptionFilterUniverse, OptionFilterUniverse]</code></td>
+            <td>A function that selects Future Option contracts</td>
+            <td><code class='csharp'>null</code><code class='python'>None</code></td>
+        </tr>
+    </tbody>
+</table>
+
+
