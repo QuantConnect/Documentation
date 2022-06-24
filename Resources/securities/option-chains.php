951f8ef66d1bdd573126dd847958acdcf56a660d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 14:27:38 -0600

    Update option-chains.php

--- Resources/securities/option-chains.php
+++ Resources/securities/option-chains.php
@@ -1,19 +1,22 @@
 <?php
-$getOptionChainsText = function($isFutureOptionUniverse, $cSharpMemberName, $pythonMemberName, $cSharpVariableName="canonicalSymbol", $pythonVariableName="canonical_symbol")
+$getOptionChainsText = function($path, $cSharpMemberName, $pythonMemberName, $cSharpVariableName="canonicalSymbol", $pythonVariableName="canonical_symbol")
 {
     echo "
 <p><code>OptionChain</code> objects represent and entire chain of Option contracts for a single underlying security. They have the following properties:</p>
 <div data-tree='QuantConnect.Data.Market.OptionChain'></div>";
     
-    
-    if ($isFutureOptionUniverse)
-    {
-        echo file_get_contents(DOCS_RESOURCES."/securities/future-option-chains.html");
-    }
-    else
+    if ($path == "regular")
     {
         include(DOCS_RESOURCES."/securities/regular-option-chains.php");
         $getRegularOptionChainsText($cSharpMemberName, $pythonMemberName, $cSharpVariableName, $pythonVariableName);
+    }
+    else if ($path == "universe")
+    {
+        echo file_get_contents(DOCS_RESOURCES."/securities/universe-option-chains.html");
+    }
+    else if ($path == "future-option")
+    {
+        echo file_get_contents(DOCS_RESOURCES."/securities/future-option-chains.html");
     }
 }
 ?>
