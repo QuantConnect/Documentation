3839f626f5c97256800ea1333fbdd12b82e28b43
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 11:57:43 -0600

    Update option-chains.php

--- Resources/securities/option-chains.php
+++ Resources/securities/option-chains.php
@@ -1,5 +1,5 @@
 <?php
-$getOptionChainsText = function($isFutureOptionUniverse, $variableName)
+$getOptionChainsText = function($isFutureOptionUniverse, $cSharpVariableName, $pythonVariableName)
 {
     echo "
 <p><code>OptionChain</code> objects represent and entire chain of Option contracts for a single underlying security. They have the following properties:</p>
@@ -13,7 +13,7 @@ $getOptionChainsText = function($isFutureOptionUniverse, $variableName)
     else
     {
         include(DOCS_RESOURCES."/securities/regular-option-chains.html");
-        $getRegularOptionChainsText($variableName);
+        $getRegularOptionChainsText($cSharpVariableName, $pythonVariableName);
     }
 }
 ?>
