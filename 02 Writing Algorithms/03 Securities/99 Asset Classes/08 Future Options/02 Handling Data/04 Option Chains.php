59948734b1c8db23170cf7e8be3bdfacc0e287c5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-24 12:52:31 -0600

    Update 04 Option Chains.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/04 Option Chains.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/04 Option Chains.php
@@ -1,7 +1,9 @@
 <?php 
 include(DOCS_RESOURCES."/securities/option-chains.php"); 
 $isFutureOptionUniverse = false;
-$cSharpVariableName = "_optionContractSymbol.Canonical";
-$pythonVariableName = "self.option_contract_symbol.Canonical";
+$cSharpMemberName = "_optionContractSymbol.Canonical";
+$pythonMemberName = "self.option_contract_symbol.Canonical";
+$cSharpVariableName = "underlyingFutureContractSymbol";
+$cSharpVariableName = "underlying_future_contract_symbol";
 $getOptionChainsText($isFutureOptionUniverse, $cSharpVariableName, $pythonVariableName);
 ?>
