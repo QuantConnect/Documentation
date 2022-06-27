b71539388ade89b475abea65025655764008375d
Web Editor <web.editor@quantconnect.com>
2022-06-27 13:51:06 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/04 Option Chains.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/04 Option Chains.php
@@ -3,7 +3,7 @@ include(DOCS_RESOURCES."/securities/option-chains.php");
 $path = "regular";
 $cSharpMemberName = "_optionContractSymbol.Canonical";
 $pythonMemberName = "self.option_contract_symbol.Canonical";
-$cSharpVariableName = "underlyingFutureContractSymbol";
-$pythonVariableName = "underlying_future_contract_symbol";
+$cSharpVariableName = "canonicalFOPSymbol";
+$pythonVariableName = "canonical_fop_symbol";
 $getOptionChainsText($path, $cSharpMemberName, $pythonMemberName, $cSharpVariableName, $pythonVariableName);
 ?>
