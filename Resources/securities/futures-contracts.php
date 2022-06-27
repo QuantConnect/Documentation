801ca687180312c00be9aee2ae86edbdc121064b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-27 13:46:09 -0600

    Update and rename futures-contracts.html to futures-contracts.php

--- Resources/securities/futures-contracts.php
+++ Resources/securities/futures-contracts.php
@@ -1,14 +1,18 @@
+<?php
+$getFutureContractText = function($pythonMemberName, $cSharpMemberName)    
+{
+    echo "
 <p><code>FuturesContract</code> objects represent the data of a single Futures contract in the market. They have the following properties:</p>
-<div data-tree="QuantConnect.Data.Market.FuturesContract"></div>
+<div data-tree='QuantConnect.Data.Market.FuturesContract'></div>
 
 <p>To get the Futures contracts in the <code>Slice</code>, use the <code>Contracts</code> property of the <code>FuturesChain</code>.</p>
 
 <div class='section-example-container'>
     <pre class='csharp'>public override void OnData(Slice slice)
 {
-    if (slice.FuturesChains.TryGetValue(_contractSymbol.Canonical, out var chain))
+    if (slice.FuturesChains.TryGetValue({$cSharpMemberName}.Canonical, out var chain))
     {
-        if (chain.Contracts.TryGetValue(_contractSymbol, out var contract))
+        if (chain.Contracts.TryGetValue({$cSharpMemberName}, out var contract))
         {
             //
         }
@@ -17,18 +21,21 @@
 
 public void OnData(FuturesChains futuresChains)
 {
-    if (futuresChains.TryGetValue(_contractSymbol.Canonical, out var chain))
+    if (futuresChains.TryGetValue({$cSharpMemberName}.Canonical, out var chain))
     {
-        if (chain.Contracts.TryGetValue(_contractSymbol, out var contract))
+        if (chain.Contracts.TryGetValue({$cSharpMemberName}, out var contract))
         {
             //
         }
     }
 }</pre>
     <pre class='python'>def OnData(self, slice: Slice) -> None:
-    chain = slice.FuturesChains.get(self.contract_symbol.Canonical)
+    chain = slice.FuturesChains.get({$pythonMemberName}.Canonical)
     if chain:
-        contract = chain.Contracts.get(self.contract_symbol)
+        contract = chain.Contracts.get({$pythonMemberName})
         if contract:
             pass</pre>
-</div>
+</div>    
+";
+}
+?>
