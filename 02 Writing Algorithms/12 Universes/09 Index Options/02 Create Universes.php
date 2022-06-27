85bfea66b8c880a0e1111a3df2d463d83fafb772
Web Editor <web.editor@quantconnect.com>
2022-06-27 13:03:07 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/12 Universes/09 Index Options/02 Create Universes.php
+++ 02 Writing Algorithms/12 Universes/09 Index Options/02 Create Universes.php
@@ -49,3 +49,10 @@ self.symbol = option.Symbol</pre>
 
 <p>If you add an Option universe for an underlying Index that you don't have a subscription for, LEAN automatically subscribes to the underlying Index.</p>
 
+
+<p>To set the <a href='/docs/v2/writing-algorithms/reality-modeling/options-models/pricing'>price model</a> of the Option, set its <code>PriceModel</code> property.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">option.PriceModel =  OptionPriceModels.CrankNicolsonFD();</pre>
+    <pre class="python">option.PriceModel = OptionPriceModels.CrankNicolsonFD()</pre>
+</div>
