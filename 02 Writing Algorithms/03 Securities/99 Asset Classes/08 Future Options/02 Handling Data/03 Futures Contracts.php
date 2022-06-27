564a806883018d8dc98fb8ab9166d13c60115f35
Web Editor <web.editor@quantconnect.com>
2022-06-27 15:47:48 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/03 Futures Contracts.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/03 Futures Contracts.php
@@ -1,1 +1,6 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/securities/futures-contracts.html"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/futures-contracts.php"); 
+$pythonMemberName = "self.future_contract_symbol";
+$cSharpMemberName = "_futureContractSymbol";
+$getFutureContractText($pythonMemberName, $cSharpMemberName);
+?>
