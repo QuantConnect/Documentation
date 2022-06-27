8cdc8ba84191a9f31780b86571b5866f84723aea
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-27 13:47:10 -0600

    Update 03 Futures Contracts.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/03 Futures Contracts.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/03 Futures Contracts.php
@@ -1,1 +1,6 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/securities/futures-contracts.html"); ?>
+<?php 
+include(DOCS_RESOURCES."/securities/futures-contracts.php"); 
+$pythonMemberName = "self.contract_symbol";
+$cSharpMemberName = "_contractSymbol";
+$getFutureContractText($pythonMemberName, $cSharpMemberName);
+?>
