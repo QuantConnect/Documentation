be79595fe95e9c540f12c25b7e1584c4f0e92ad2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-28 16:52:31 -0600

    Update 02 Get All Stored Data.php

--- 03 Research Environment/08 Object Store/02 Get All Stored Data.php
+++ 03 Research Environment/08 Object Store/02 Get All Stored Data.php
@@ -2,6 +2,6 @@
 include(DOCS_RESOURCES."/object-store/get-all-stored-data.php");
 $cSharpPrefix = "qb.";
 $pythonPrefix = "qb.";
-$getAllStoredDataText($cSharpPrefix, $pythonPrefix)
+$getAllStoredDataText($cSharpPrefix, $pythonPrefix);
 ?>
 
