554d1a0a26b0a383ef9ea845d3975b14b7d56e90
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-28 16:54:23 -0600

    Update 05 Save Data.php

--- 03 Research Environment/08 Object Store/05 Save Data.php
+++ 03 Research Environment/08 Object Store/05 Save Data.php
@@ -1,1 +1,8 @@
-
+<?php
+include(DOCS_RESOURCES."/object-store/save-data.php");
+$cSharpPrefix = "qb.";
+$pythonPrefix = "qb.";
+$sampleDataLink = "/docs/v2/research-environment/object-store#03-Create-Sample-Data";
+$writingAlgorithmsText = "";
+$getSaveDataText($cSharpPrefix, $pythonPrefix, $sampleDataLink, $writingAlgorithmsText);
+?>
