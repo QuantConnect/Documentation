0943963505c86ce8f6328ec15ce93c74bb2933aa
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-28 17:02:20 -0600

    Update 03 Create Sample Data.php

--- 02 Writing Algorithms/29 Object Store/03 Create Sample Data.php
+++ 02 Writing Algorithms/29 Object Store/03 Create Sample Data.php
@@ -1,31 +1,5 @@
-<p>You need some data to store data in the Object Store.</p>
-
-<p>Follow these steps to create some sample data:</p>
-
-<ol>
-    <li>Create a dictionary.</li>
-    <div class="section-example-container">
-        <pre class="csharp">var dictSample = new Dictionary&lt;string, int&gt; { {"One", 1}, {"Two", 2}, {"Three", 3} };</pre>
-        <pre class="python">dict_sample = {"One": 1, "Two": 2, "Three": 3}</pre>
-    </div>
-
-    <li>Create a <code>string</code>.</li>
-    <div class="section-example-container">
-        <pre class="csharp">var stringSample = "My string";</pre>
-        <pre class="python">string_sample = "My string"</pre>
-    </div>
-
-    <li>Create a <code>Bytes</code> object.</li>
-    <div class="section-example-container">
-        <pre class="csharp">var bytesSample = Encoding.UTF8.GetBytes("My String");</pre>
-        <pre class="python">bytes_sample = str.encode("My String")</pre>
-    </div>
-
-    <li class="csharp">Convert the dictionary to an <code>XML</code>-formatted object.</li>
-    <div class="csharp section-example-container">
-        <pre class="csharp">var xmlSample = new XElement("sample",
-    sample.Select(kvp => new XElement(kvp.Key, kvp.Value)));
-Log(xmlSample.ToString());</pre>
-    </div>
-    <img class="csharp" src="https://cdn.quantconnect.com/i/tu/store-data-xml-cs.png">
-</ol>
+<?php
+include(DOCS_RESOURCES."/object-store/create-sample-data.php");
+$cSharpMethodName = "Log";
+$getCreateSampleDataText($cSharpMethodName);
+?>
