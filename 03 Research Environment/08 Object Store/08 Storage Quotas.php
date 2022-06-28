e45727fceb02ef039602e07e72635e98463d30e7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-28 17:06:17 -0600

    Update 08 Storage Quotas.php

--- 03 Research Environment/08 Object Store/08 Storage Quotas.php
+++ 03 Research Environment/08 Object Store/08 Storage Quotas.php
@@ -1,5 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/object-store/storage-quotas.php");
+$cSharpPrefix="qb.";
+$pythonPrefix="qb.";
 $context = "use the Research Environment";
-$getStorageQuotasText($context);
+$getStorageQuotasText($cSharpPrefix, $pythonPrefix, $context);
 ?>
