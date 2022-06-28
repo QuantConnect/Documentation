7bf4aa668d798918653bedfbc472ccc6a31e4272
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-28 16:58:17 -0600

    Update 07 Delete Data.php

--- 03 Research Environment/08 Object Store/07 Delete Data.php
+++ 03 Research Environment/08 Object Store/07 Delete Data.php
@@ -1,1 +1,7 @@
-
+<?php
+include(DOCS_RESOURCES."/object-store/delete-data.php");
+$cSharpPrefix = "qb.";
+$pythonPrefix = "qb.";
+$context = "use the Research Environment"
+$getDeleteDataText($cSharpPrefix, $pythonPrefix, $context);
+?>
