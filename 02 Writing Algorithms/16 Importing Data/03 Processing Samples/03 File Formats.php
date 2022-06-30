ed9e717f31b23e1781f9d7c195fb78862103e997
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-30 15:21:39 -0600

    Update 03 File Formats.php

--- 02 Writing Algorithms/16 Importing Data/03 Processing Samples/03 File Formats.php
+++ 02 Writing Algorithms/16 Importing Data/03 Processing Samples/03 File Formats.php
@@ -1,4 +1,4 @@
 <p>The <code>SubscriptionDataSource</code> constructor can accept a <code>format</code> argument. The <code>FileFormat</code> enumeration has the following members:</p>
 
-<div data-tree="QuantConnect.FileFormat"></div>
+<div data-tree="QuantConnect.Data.FileFormat"></div>
 
