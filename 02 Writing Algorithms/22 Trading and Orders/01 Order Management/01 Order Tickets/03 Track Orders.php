6d37b8d3c7181778013dde1d2449a2d38109908c
Web Editor <web.editor@quantconnect.com>
2022-06-30 13:58:52 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/03 Track Orders.php
+++ 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/03 Track Orders.php
@@ -7,7 +7,8 @@
 <pre class="python">limit_price = order_ticket.Get(OrderField.LimitPrice)</pre>
 </div>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/order_field.html"); ?>
+<p>The <code>OrderField</code> enumeration has the following members:</p>
+<div data-tree="QuantConnect.OrderField"></div>
 
 
 <p>In addition to using order tickets to track orders, you can receive <a href='/docs/v2/writing-algorithms/trading-and-orders/order-events'>order events</a> through the <code>OnOrderEvent</code> event handler.</p>
