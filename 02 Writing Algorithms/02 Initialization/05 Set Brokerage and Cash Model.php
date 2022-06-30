5488d7a79332c08f85695cb1be96577f66cda230
Web Editor <web.editor@quantconnect.com>
2022-06-30 13:25:24 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/02 Initialization/05 Set Brokerage and Cash Model.php
+++ 02 Writing Algorithms/02 Initialization/05 Set Brokerage and Cash Model.php
@@ -1,9 +1,9 @@
 <p>We model your algorithm with margin modeling by default, but you can select a cash account type. Cash accounts don't allow leveraged trading, whereas Margin accounts can support leverage on your account value. To set your brokerage and account type, call the <code>SetBrokerageModel</code> method. For more information about each brokerage and the account types they support, see the <a href="/docs/v2/our-platform/live-trading/brokerages">brokerage integration</a> documentation. For more information about the reality models that the brokerage models set, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages">Brokerages</a>.<br></p>
-
-
 <div class="section-example-container">
 	<pre class="csharp">SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Margin);</pre>
 	<pre class="python">self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash)</pre>
 </div>
+
+<p>The <code>AccountType</code> enumeration has the following members:</p>
 
 <div data-tree="QuantConnect.AccountType"></div>
