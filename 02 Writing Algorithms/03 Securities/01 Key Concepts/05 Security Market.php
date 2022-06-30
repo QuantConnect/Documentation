a1e245a772c55c55ff96bdf1ba43ea936b7556b2
Web Editor <web.editor@quantconnect.com>
2022-06-30 13:29:38 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/01 Key Concepts/05 Security Market.php
+++ 02 Writing Algorithms/03 Securities/01 Key Concepts/05 Security Market.php
@@ -1,3 +1,7 @@
+<p>The <code>Market</code> enumeration has the following members:</p>
+<div data-tree="QuantConnect.Market"></div>
+
+
 <?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market.html"); ?>
 
 <p>LEAN groups all of the US Equity exchanges under <code>Market.USA</code>. In live mode, the brokerage routes the orders to the exchange that provides the best price.</p>
