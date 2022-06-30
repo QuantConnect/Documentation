ce9b2db1d6ef1b36b9085f8b735c4f824ff0b4f4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-30 14:12:29 -0600

    Update 04 Custom Indicator Values.php

--- 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/04 Custom Indicator Values.php
+++ 02 Writing Algorithms/18 Consolidating Data/08 Updating Indicators/04 Custom Indicator Values.php
@@ -16,8 +16,7 @@ self.RegisterIndicator(self.symbol, self.rsi, Resolution.Daily, Field.High)</pre
 
 <p>The <code>RegisterIndicator</code> method can accept a <code class="python">timedelta</code><code class="csharp">TimeSpan</code>, <code>Resolution</code>, or an unregistered consolidator. If you apply the indicator to a security in a dynamic universe, provide a consolidator so that you can <a href="/docs/v2/writing-algorithms/consolidating-data/key-concepts#07-Remove-Consolidators">remove it</a> when your algorithm removes the security from the universe.</p>
 
-<p>The <code>Field</code> enumeration has the following members:</p>
-<div data-tree="QuantConnect.Field"></div>
+<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/field.html"); ?>
 
 
 <p class='csharp'>To create a custom <code>selector</code>, define a function that calculates the value.</p>
