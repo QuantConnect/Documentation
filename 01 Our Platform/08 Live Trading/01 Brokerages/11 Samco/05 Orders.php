4cc834c020c44028e38b419d5b1654b36c3d6912
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-06-30 12:26:44 -0600

    Update 05 Orders.php

--- 01 Our Platform/08 Live Trading/01 Brokerages/11 Samco/05 Orders.php
+++ 01 Our Platform/08 Live Trading/01 Brokerages/11 Samco/05 Orders.php
@@ -35,6 +35,8 @@ self.LimitIfTouchedOrder(self.symbol, quantity, trigger_price, limit_price)</pre
 
 <p>We model custom order properties from the Samco API. The following table describes the members of the <code>IndiaOrderProperties</code> object that you can set to customize order execution:</p>
 
+<div data-tree="QuantConnect.Orders.IndiaOrderProperties.IndiaProductType"></div>
+
 <table class="table qc-table">
     <thead>
         <tr>
