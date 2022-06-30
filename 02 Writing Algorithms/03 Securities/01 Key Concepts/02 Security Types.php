7208b1d4fa2444687ada9e96d7cff750142008a5
Web Editor <web.editor@quantconnect.com>
2022-06-30 13:27:32 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/01 Key Concepts/02 Security Types.php
+++ 02 Writing Algorithms/03 Securities/01 Key Concepts/02 Security Types.php
@@ -1,5 +1,5 @@
-<p>LEAN uses the <code>SecurityType</code> enumeration as a synonym for asset classes.</p>
+<p>LEAN uses the <code>SecurityType</code> enumeration as a synonym for asset classes. The <code>SecurityType</code> enumeration has the following members:</p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/security_type.html"); ?>
+<div data-tree="QuantConnect.SecurityType"></div>
 
 <p>The <code>Base</code> type is for non-financial assets like <a href='/docs/v2/writing-algorithms/importing-data/processing-samples'>custom</a> and alternative data objects.</p>
