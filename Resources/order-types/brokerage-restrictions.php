bb73a15e8e6a0c3197effc1f2dbcbb51fbe752d4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-01 14:53:27 -0600

    Update brokerage-restrictions.php

--- Resources/order-types/brokerage-restrictions.php
+++ Resources/order-types/brokerage-restrictions.php
@@ -1,5 +1,14 @@
 <?php
-$getBrokerageRestrictionsText = function($orderType) {
-    echo "<p>To check if your brokerage has any special requirements for {$orderType}, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>";
+$getBrokerageRestrictionsText = function($orderType, $brokerageName="Default") {
+    echo "
+    <p>Each brokerage has a set of assets and order types they support. To avoid issues with {$orderType}, <a href='/docs/v2/writing-algorithms/initialization#05-Set-Brokerage-and-Cash-Model'>set the brokerage model</a> to a brokerage that supports them.</p>
+    
+<div class='section-example-container'>
+<pre class='csharp'>SetBrokeargeModel(BrokerageName.{$brokerageName});</pre>
+<pre class='python'>self.SetBrokeargeModel(BrokerageName.{$brokerageName})</pre>
+</div>
+
+<p>To check if your brokerage has any special requirements for {$orderType}, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>
+    ";
 }
 ?>
