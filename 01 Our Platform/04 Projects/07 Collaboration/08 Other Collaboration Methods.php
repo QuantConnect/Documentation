322f26bdfe9bcdefb19b1a495e0b593d0d23aeb7
Web Editor <web.editor@quantconnect.com>
2022-07-01 17:38:45 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/07 Collaboration/08 Other Collaboration Methods.php
+++ 01 Our Platform/04 Projects/07 Collaboration/08 Other Collaboration Methods.php
@@ -5,7 +5,7 @@
 <p>To clone projects, <a href="/docs/v2/our-platform/projects/project-management#02-View-All-Projects">open the project</a> you want to clone and then, in the Project panel, click <span class="button-name">Clone</span>. "Project cloned successfully" displays.</p>
 
 <h4>Share Projects</h4>
-<p><a href="/docs/v2/our-platform/backtesting/deployment#08-Run-Backtests">Run a backtest</a> and then <a href="/docs/v2/our-platform/backtesting/results#14-Share-Results">make the backtest results public</a> to share a project. Once a backtest is made public, a link is generated for you that opens the backtest results and the project files. You can directly give the link to others, attach the backtest to a forum discussion, or embed the backtest into a website. However, note that when you make a backtest public, the project files are accessible to anyone who visits the link, even after you delete the project. As a result, we do not recommend collaborating on projects by making backtests public and sharing the link with the people with which you are collaborating. Instead, we recommend adding team members to your project since doing so protects your intellectual property.</p>
+<p><a href="/docs/v2/our-platform/backtesting/deployment#08-Run-Backtests">Run a backtest</a> and then <a href="/docs/v2/our-platform/backtesting/results#14-Share-Results">make the backtest results public</a> to share a project. Once a backtest is made public, a link is generated for you that opens the backtest results and the project files. You can directly give the link to others, attach the backtest to a forum discussion, or embed the backtest into a website. However, note that when you make a backtest public, the project files are accessible to anyone who visits the link, even after you delete the project. As a result, we don't recommend collaborating on projects by making backtests public and sharing the link with your collaborators. Instead, add team members to your project since it protects your intellectual property.</p>
 
 <p>You can share a backtest at any time when it's executing. Although, if you generate a link to share the backtest before the backtest completes, the link that's generated will not contain all of the backtest results. Some reasons to share your project include the following:</p>
 <ul>
@@ -14,6 +14,7 @@
     <li>Share a link to the project with others to give them a copy of the project files and the backtest results.</li>
 </ul>
 
+<p>To share a research notebook, save the notebook and run a backtest.</p>
 
 <h4>Migrate Projects</h4>
 <?php echo file_get_contents(DOCS_RESOURCES."/ide/migrate-projects.html"); ?>
