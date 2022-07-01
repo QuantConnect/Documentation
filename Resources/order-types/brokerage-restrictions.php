c9f97cbba011f32db0e5538eb745e6280bc1dacf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-01 14:55:07 -0600

    Update brokerage-restrictions.php

--- Resources/order-types/brokerage-restrictions.php
+++ Resources/order-types/brokerage-restrictions.php
@@ -4,8 +4,8 @@ $getBrokerageRestrictionsText = function($orderType, $brokerageName="QuantConnec
     <p>Each brokerage has a set of assets and order types they support. To avoid issues with {$orderType}, <a href='/docs/v2/writing-algorithms/initialization#05-Set-Brokerage-and-Cash-Model'>set the brokerage model</a> to a brokerage that supports them.</p>
     
 <div class='section-example-container'>
-<pre class='csharp'>SetBrokeargeModel(BrokerageName.{$brokerageName});</pre>
-<pre class='python'>self.SetBrokeargeModel(BrokerageName.{$brokerageName})</pre>
+<pre class='csharp'>SetBrokerageModel(BrokerageName.{$brokerageName});</pre>
+<pre class='python'>self.SetBrokerageModel(BrokerageName.{$brokerageName})</pre>
 </div>
 
 <p>To check if your brokerage has any special requirements for {$orderType}, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>
