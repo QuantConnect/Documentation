77263480171098463de5ab6ebb39071ee3089040
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-05 11:38:40 -0600

    Update 16 net profit.php

--- 02 Writing Algorithms/01 Key Concepts/09 Glossary/16 net profit.php
+++ 02 Writing Algorithms/01 Key Concepts/09 Glossary/16 net profit.php
@@ -1,9 +1,4 @@
 <?php 
 include(DOCS_RESOURCES."/glossary.php");
-echo "<p><span class='qualifier'>(Percent)</span> ";
-echo $definitionByTerm['net profit']['percent'];
-echo "</p>";
-echo "<p><span class='qualifier'>(Dollar-value)</span> ";
-echo $definitionByTerm['net profit']['dollar-value'];
-echo "</p>";
+$getGlossaryTermHTML("net profit");
 ?>
