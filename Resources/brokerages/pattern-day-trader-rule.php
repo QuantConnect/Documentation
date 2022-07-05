41c0f54082b5c44123cb30b8c855558e5d820380
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-05 10:59:56 -0600

    Update pattern-day-trader-rule.php

--- Resources/brokerages/pattern-day-trader-rule.php
+++ Resources/brokerages/pattern-day-trader-rule.php
@@ -16,7 +16,7 @@ $getPDTText = function($includeh4=true)
     <li>Your intraday US Equity trades represent more than 6% of your total trades.</li>
 </ul>
 
-<p>Pattern day traders must maintain a minimum equity of $25,000 in their margin account to continue trading. For more information about pattern day trading, see <a rel="nofollow" target="_blank" href='https://www.finra.org/investors/learn-to-invest/advanced-investing/day-trading-margin-requirements-know-rules'>Am I a Pattern Day Trader?</a> on the FINRA website.</p>
+<p>Pattern day traders must maintain a minimum equity of $25,000 in their margin account to continue trading. For more information about pattern day trading, see <a rel='nofollow' target='_blank' href='https://www.finra.org/investors/learn-to-invest/advanced-investing/day-trading-margin-requirements-know-rules'>Am I a Pattern Day Trader?</a> on the FINRA website.</p>
 
 <p>The <code>PatternDayTradingMarginModel</code> doesn't enforce minimum equity rules and doesn't limit your trade, but it adjusts your available leverage based on the market state. During regular market hours, you can use up to 4x leverage. During extended market hours, you can use up to 2x leverage.</p>
 
