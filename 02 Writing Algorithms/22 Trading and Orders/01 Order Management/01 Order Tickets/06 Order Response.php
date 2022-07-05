356e6644a430b595591bae8a1f035427ebba3d50
Web Editor <web.editor@quantconnect.com>
2022-07-05 14:39:35 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/06 Order Response.php
+++ 02 Writing Algorithms/22 Trading and Orders/01 Order Management/01 Order Tickets/06 Order Response.php
@@ -1,7 +1,7 @@
 <p>When you update or cancel an order, LEAN returns an <code>OrderReponse</code> object, which have the following attributes:</p>
 <div data-tree="QuantConnect.Orders.OrderResponse"></div>
 
-<p>If your order changes fail, check the <code>ErrorCode</code> or <code>ErrorMessage</code>. For more information about specific order errors, see the <a href="/docs/v2/writing-algorithms/trading-and-orders/order-errors#19-Error-Code-Reference">Error Code Reference</a>.</p>
+<p>If your order changes fail, check the <code>ErrorCode</code> or <code>ErrorMessage</code>. For more information about specific order errors, see the <a href="/docs/v2/writing-algorithms/trading-and-orders/order-errors#20-Order-Response-Error-Reference">Order Response Error Reference</a>.</p>
 
 <p>To get most recent order response, call the <code>GetMostRecentOrderResponse</code> method.</p>
 <div class="section-example-container">
