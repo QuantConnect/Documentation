0f5f252b035b06a276d12d945b30e92bff7e7571
Web Editor <web.editor@quantconnect.com>
2022-07-05 20:04:15 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/28 Indicators/00 Key Concepts/04 Naming Convention.php
+++ 02 Writing Algorithms/28 Indicators/00 Key Concepts/04 Naming Convention.php
@@ -3,4 +3,4 @@ echo file_get_contents(DOCS_RESOURCES."/indicators/naming-convention-manual.html
 echo file_get_contents(DOCS_RESOURCES."/indicators/naming-convention-automatic.html");
 ?>
 
-<p>To view the class name and shortcut method of each indicator, see the Indicator Reference.</p>
+<p>To view the class name and shortcut method of each indicator, see the <a href='/docs/v2/writing-algorithms/indicators/indicator-reference'>Indicator Reference</a>.</p>
