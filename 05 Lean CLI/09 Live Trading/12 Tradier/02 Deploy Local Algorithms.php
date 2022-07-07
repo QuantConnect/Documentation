6df2a28dba92e5b05d695086f61fd73a64b00256
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 14:57:55 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/12 Tradier/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/12 Tradier/02 Deploy Local Algorithms.php
@@ -5,18 +5,19 @@ $brokerageName = "Tradier";
 $dataFeedName = "";
 $isBrokerage = true;
 $brokerageDetails = "
-<li>Enter your Tradier account id and access token. You can find these credentials on your <a href='https://dash.tradier.com/settings/api' target='_blank'>Settings/API Access</a> page.
+<li>Enter your Tradier account ID and access token.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 Account id: VA000001
 Access token: ****************</pre>
 </div>
+To get these credentials, see your <a href='https://dash.tradier.com/settings/api' target='_blank' rel='nofollow'>Settings/API Access page</a> on the Tradier website.
 </li>
 
 <li>Enter whether the developer sandbox should be used.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
-Use the developer sandbox? [y/N]: y</pre>
+Use the developer sandbox? (live, paper): live</pre>
 </div>
 </li>
 ";
