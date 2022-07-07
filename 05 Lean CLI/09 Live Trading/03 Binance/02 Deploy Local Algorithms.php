d4a3bbaf53398630eac347b32c13f5779c826775
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 15:12:45 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/03 Binance/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/03 Binance/02 Deploy Local Algorithms.php
@@ -19,12 +19,13 @@ Use the testnet? (live, paper): live</pre>
 </div>
 </li>
 
-<li>Enter your API key and API secret. To create a new API key, see the API Management page on <a rel='nofollow' href='https://www.binance.com/en/my/settings/api-management' target='_blank'>Binance</a> or <a rel='nofollow' href='https://www.binance.us/en/usercenter/settings/api-management' target='_blank'>Binance US</a>.
+<li>Enter your API key and API secret.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 API key: 6d3ef5ca2d2fa52e4ee55624b0471261
 API secret: ********************************</pre>
 </div>
+To create a new API key, see the API Management page on <a rel='nofollow' href='https://www.binance.com/en/my/settings/api-management' target='_blank'>Binance</a> or <a rel='nofollow' href='https://www.binance.us/en/usercenter/settings/api-management' target='_blank'>Binance US</a>.
 </li>
 ";
 
