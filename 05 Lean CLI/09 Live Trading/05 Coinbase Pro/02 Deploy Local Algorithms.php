b4d7fd551f8198cd30953f93eab6cabd07056cb0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 12:43:14 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/05 Coinbase Pro/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/05 Coinbase Pro/02 Deploy Local Algorithms.php
@@ -5,6 +5,13 @@ $brokerageName = "Coinbase Pro";
 $dataFeedName = "";
 $isBrokerage = true;
 $brokerageDetails = "
+<li>Enter the environment to use.</li>
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Use sandbox? (live, paper):</pre>
+</div>
+</li>
+
 <li>Enter your API key, API secret, and passphrase. You can generate new API credentials on the <a href='https://pro.coinbase.com/profile/api' target='_blank'>API settings page</a>.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
@@ -24,7 +31,7 @@ Use the sandbox? [y/N]: n</pre>
 ";
 $dataFeedDetails = "";
 $supportsIQFeed = false;
-$requiresSubscription = false;
+$requiresSubscription = true;
 
 $getDeployLocalAlgorithmsText($brokerageName, $dataFeedName, $isBrokerage, $brokerageDetails, $dataFeedDetails, $supportsIQFeed, $requiresSubscription);
 ?>
