9b372ca7f904df8c0d0c22d788bc3607f95a5391
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 15:16:44 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/05 Coinbase Pro/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/05 Coinbase Pro/02 Deploy Local Algorithms.php
@@ -12,13 +12,14 @@ Use sandbox? (live, paper): live</pre>
 </div>
 </li>
 
-<li>Enter your API key, API secret, and passphrase. You can generate new API credentials on the <a href='https://pro.coinbase.com/profile/api' target='_blank'>API settings page</a>.
+<li>Enter your API key, API secret, and passphrase.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 API key: 6d3ef5ca2d2fa52e4ee55624b0471261
 API secret: ****************************************************************************************
 Passphrase: ****************</pre>
 </div>
+To create new API credentials, see the <a href='https://pro.coinbase.com/profile/api' target='_blank' rel='nofollow'>API settings page</a> on the Coinbase Pro website.
 </li>
 ";
 $dataFeedDetails = "";
