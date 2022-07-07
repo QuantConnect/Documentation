d5438ca9580835286b7bdaeb2b1ca80f83978912
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 14:48:18 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/08 Kraken/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/08 Kraken/02 Deploy Local Algorithms.php
@@ -5,15 +5,16 @@ $brokerageName = "Kraken";
 $dataFeedName = "";
 $isBrokerage = true;
 $brokerageDetails = "
-<li>Enter your API key and API secret. You can gather these credentials from the <a target='_blank' rel='nofollow' href='https://www.kraken.com/u/security/api'>API Management Settings</a> page on the Kraken website.
+<li>Enter your API key and API secret.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 API key: 
 API secret: </pre>
 </div>
+To get your API credentials, see the <a target='_blank' rel='nofollow' href='https://www.kraken.com/u/security/api'>API Management Settings</a> page on the Kraken website.
 </li>
 
-<li>Enter your verification tier. For more information about verification tiers, see <a href='https://support.kraken.com/hc/en-us/articles/360001395743-Verification-levels-explained' target='_blank' rel='nofollow'>Verification levels explained</a> on the Kraken website.
+<li>Enter your verification tier.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 Select the Verification Tier:
@@ -22,6 +23,7 @@ Select the Verification Tier:
 3) Pro
 Enter an option:</pre>
 </div>
+For more information about verification tiers, see <a href='https://support.kraken.com/hc/en-us/articles/360001395743-Verification-levels-explained' target='_blank' rel='nofollow'>Verification levels explained</a> on the Kraken website.
 </li>
 ";
 $dataFeedDetails = "";
