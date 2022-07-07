ce98dc1c1591609cc519d2cecd1fda49240a7352
Web Editor <web.editor@quantconnect.com>
2022-07-07 18:34:25 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/14 Zerodha/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/14 Zerodha/03 Deploy Cloud Algorithms.php
@@ -1,4 +1,73 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
-$getDeployCloudAlgorithmsText("Zerodha", false);
+
+$brokerageDetails = "
+<li>Enter your <a href='https://kite.trade/' target='_blank'>Kite Connect</a> API key and access token.
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+API key: hp9erb9ct0lqaxpm
+Access token: ********************</pre>
+</div>
+</li>
+
+
+<li>Enter the product type.
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Product type (MIS, CNC, NRML):</pre>
+</div>
+<p>The following table describes the product types:</p>
+<table class='qc-table table'>
+    <thead>
+        <tr>
+            <th style='width: 25%'>Product Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>MIS</code></td>
+            <td>Intraday products</td>
+        </tr>
+        <tr>
+            <td><code>CNC</code></td>
+            <td>Delivery products</td>
+        </tr>
+        <tr>
+            <td><code>NRML</code></td>
+            <td>Carry forward products</td>
+        </tr>
+    </tbody>
+</table>
+</li>
+
+
+<li>Enter the trading segment.
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Trading segment (EQUITY, COMMODITY):</pre>
+</div>
+<p>The following table describes when to use each trading segment:</p>
+<table class='qc-table table'>
+    <thead>
+        <tr>
+            <th style='width: 25%'>Trading Segment</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>EQUITY</code></td>
+            <td>For trading Equities on the National Stock Exchange of India (NSE) or the Bombay Stock Exchange (BSE)</td>
+        </tr>
+        <tr>
+            <td><code>COMMODITY</code></td>
+            <td>For trading commodities on the Multi Commodity Exchange of India (MCX)</td>
+        </tr>
+    </tbody>
+</table>
+</li>
+";
+
+$getDeployCloudAlgorithmsText("Zerodha", true, $brokerageDetails);
 ?>
