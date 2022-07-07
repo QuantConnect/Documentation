3a243f5316c305a6fa8a402422e6eaaa1e3bcd00
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 15:21:55 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
@@ -13,10 +13,10 @@ Account password: ****************</pre>
 </div>
 </li>
 
-<li>Enter whether you want to use the price data feed from Interactive Brokers instead of the one from QuantConnect. Enabling this feature requires you to have active Interactive Brokers market data subscriptions for all data required by your algorithm.
+<li>Enter whether you want to use the <a href='/docs/v2/our-platform/live-trading/data-feeds/brokerage-data-feeds/interactive-brokers'>price data feed from Interactive Brokers</a> instead of the one from QuantConnect. Enabling this feature requires you to have active Interactive Brokers market data subscriptions for all data required by your algorithm.
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
-Do you want to use the Interactive Brokers price data feed instead of the QuantConnect price data feed? [y/N]: y</pre>
+Do you want to use the Interactive Brokers price data feed instead of the QuantConnect price data feed? (yes/no): y</pre>
 </div>
 </li>
 ";
