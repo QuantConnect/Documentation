bb4d4ddbde1bb7285d650f3f1b2cf8d9fa9da601
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 12:56:14 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/03 Binance/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/03 Binance/02 Deploy Local Algorithms.php
@@ -5,18 +5,25 @@ $brokerageName = "Binance";
 $dataFeedName = "";
 $isBrokerage = true;
 $brokerageDetails = "
+<li>Enter the Exchange to use.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Binance Exchange (Binance, BinanceUS): BinanceUS</pre>
+</div>
+</li>
+
+<li>Enter the environment to use.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Use the testnet? (live, paper): live</pre>
+</div>
+</li>
+
 <li>Enter your API key and API secret. You can create a new API key on the <a href='https://www.binance.com/en/my/settings/api-management' target='_blank'>API Management page</a>.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 API key: 6d3ef5ca2d2fa52e4ee55624b0471261
 API secret: ********************************</pre>
-</div>
-</li>
-
-<li>Enter whether the testnet should be used.
-<div class='cli section-example-container'>
-<pre>$ lean live \"My Project\"
-Use the testnet? [y/N]: n</pre>
 </div>
 </li>
 ";
