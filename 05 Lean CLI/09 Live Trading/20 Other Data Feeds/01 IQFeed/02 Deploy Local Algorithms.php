8b30998cc9e1ab9e16d77adc9014dcced9ceac36
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 11:03:49 -0600

    Update and rename 02 Set Up Data Feed.html to 02 Deploy Local Algorithms.php

---
+++ 05 Lean CLI/09 Live Trading/20 Other Data Feeds/01 IQFeed/02 Deploy Local Algorithms.php
@@ -0,0 +1,41 @@
+<?php
+include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php");
+
+$brokerageName = "QuantConnect Paper Trading";
+$dataFeedName = "IQ Feed";
+$isBrokerage = false;
+
+$brokerageDetails = "";
+
+$dataFeedDetails = "
+<ol>
+    <li>Enter the path to the IQConnect binary. The default path is <span class='private-file-name'>C:/Program Files (x86)/DTN/IQFeed/iqconnect.exe</span> if you used the default settings when installing the IQFeed client.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+IQConnect binary location [C:/Program Files (x86)/DTN/IQFeed/iqconnect.exe]:</pre>
+</div>
+    </li>
+
+    <li>Enter your IQFeed username and password.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Username: 123456
+Password: **********</pre>
+</div>
+    </li>
+
+    <li>Enter the product id and version of your IQFeed developer account.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Product id: &lt;yourID&gt;
+Product version: 1.0</pre>
+</div>
+    </li>
+</ol>
+";
+
+$supportsIQFeed = true;
+$requiresSubscription = false;
+
+$getDeployLocalAlgorithmsText($brokerageName, $dataFeedName, $isBrokerage, $brokerageDetails, $dataFeedDetails, $supportsIQFeed, $requiresSubscription);
+?>
