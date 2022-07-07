584313ec4474b7e679690b35f5ab5320ee21ae1c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 15:50:13 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/08 Kraken/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/08 Kraken/03 Deploy Cloud Algorithms.php
@@ -1,19 +1,21 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
 $brokerageDetails = "
-<li>Enter your API key and API secret. You can gather these credentials from the <a href='https://www.kraken.com/u/security/api'>API Management Settings</a> page on the Kraken website.
+<li>Enter your API key and API secret.
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 API key: 
 Secret key: </pre>
 </div>
+To get your API credentials, see the <a target='_blank' rel='nofollow' href='https://www.kraken.com/u/security/api'>API Management Settings</a> page on the Kraken website.
 </li>
 
-<li>Enter your verification tier. For example, <code>Starter</code>. For more information about verification tiers, see <a href='https://support.kraken.com/hc/en-us/articles/360001395743-Verification-levels-explained' target='_blank' rel='nofollow'>Verification levels explained</a> on the Kraken website.
+<li>Enter your verification tier.
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
-Verification Tier:</pre>
+Select the Verification Tier (Starter, Intermediate, Pro):</pre>
 </div>
+For more information about verification tiers, see <a href='https://support.kraken.com/hc/en-us/articles/360001395743-Verification-levels-explained' target='_blank' rel='nofollow'>Verification levels explained</a> on the Kraken website.
 </li>
 ";
 $getDeployCloudAlgorithmsText("Kraken", true, $brokerageDetails);
