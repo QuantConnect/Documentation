a5a2718c9d5adc365658543c6c479966a5a3ef30
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-07 14:52:27 -0600

    Update 02 Deploy Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/09 Oanda/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/09 Oanda/02 Deploy Local Algorithms.php
@@ -5,25 +5,27 @@ $brokerageName = "Oanda";
 $dataFeedName = "";
 $isBrokerage = true;
 $brokerageDetails = "
-<li>Enter your OANDA account id. You can find this number on your <a href='https://www.oanda.com/account/statement/' target='_blank'>Account Statement page</a>.
+<li>Enter the environment to use. Enter <code>Trade</code> for fxTrade or <code>Practice</code> for fxTrade Practice.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Environment? (Practice, Trade): Trade</pre>
+</div>
+</li>
+
+<li>Enter your OANDA account ID. 
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 Account id: 001-011-5838423-001</pre>
 </div>
+To get your account ID, see your <a href='https://www.oanda.com/account/statement/' target='_blank'>Account Statement page</a> on the OANDA website.
 </li>
 
-<li>Enter your OANDA API token. You can generate a token on the <a href='https://www.oanda.com/account/tpa/personal_token' target='_blank'>Manage API Access page</a>.
+<li>Enter your OANDA API token.
 <div class='cli section-example-container'>
 <pre>$ lean live \"My Project\"
 API token: ****************</pre>
 </div>
-</li>
-
-<li>Enter which environment you want to use. You can either choose <code>trade</code> for fxTrade or <code>practice</code> for fxTrade Practice.
-<div class='cli section-example-container'>
-<pre>$ lean live \"My Project\"
-Environment (practice, trade): practice</pre>
-</div>
+To create a token, see the <a href='https://www.oanda.com/account/tpa/personal_token' target='_blank'>Manage API Access page</a> on the OANDA website.
 </li>
 ";
 
