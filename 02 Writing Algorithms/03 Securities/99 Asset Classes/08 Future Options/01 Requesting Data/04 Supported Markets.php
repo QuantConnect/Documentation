48c0d4197c4b520d790223df6f0fea605d3f53fc
Web Editor <web.editor@quantconnect.com>
2022-07-14 19:09:44 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
@@ -1,3 +1,5 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/enumerations/market-futureoption.html"); ?>
+<p>The following <code>Market</code> enumeration members are available for Future Options:</p>
+
+<div data-tree='' data-fields='CBOT,COMEX,NYSELIFFE'></div>
 
 <p>You don't need to pass a <code>Market</code> argument to the <code>AddOptionContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
