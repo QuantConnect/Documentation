1fad7587b921255451be6e6a3fb178dd6ec66b18
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-14 17:10:13 -0600

    Update 04 Supported Markets.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
@@ -1,5 +1,5 @@
 <p>The following <code>Market</code> enumeration members are available for Future Options:</p>
 
-<div data-tree='' data-fields='CBOT,COMEX,NYSELIFFE'></div>
+<div data-tree='QuantConnect.Market' data-fields='CBOT,COMEX,NYSELIFFE'></div>
 
 <p>You don't need to pass a <code>Market</code> argument to the <code>AddOptionContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
