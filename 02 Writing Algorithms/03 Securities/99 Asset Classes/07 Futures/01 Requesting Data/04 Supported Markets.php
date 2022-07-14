b0383a5efe1d76c9ce0d72411428c700e832a648
Web Editor <web.editor@quantconnect.com>
2022-07-14 19:13:45 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/04 Supported Markets.php
@@ -1,3 +1,5 @@
-<?php include(DOCS_RESOURCES."/enumerations/market-future.html"); ?>
+<p>The following <code>Market</code> enumeration members are available for Futures:</p>
+
+<div data-tree='QuantConnect.Market' data-fields='NYMEX,ICE,CFE,India,CME,SGX,HKFE'></div>
 
 <p>You don't need to pass a <code>market</code> argument to the <code>AddFutureContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
