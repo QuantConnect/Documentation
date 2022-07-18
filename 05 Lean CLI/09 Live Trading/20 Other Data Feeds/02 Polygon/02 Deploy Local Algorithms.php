522589da0693a19dfb71615f514244b51ff1c4b1
Web Editor <web.editor@quantconnect.com>
2022-07-18 13:06:25 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/20 Other Data Feeds/02 Polygon/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/20 Other Data Feeds/02 Polygon/02 Deploy Local Algorithms.php
@@ -1,1 +1,27 @@
+<?php
+include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php");
 
+$brokerageName = "QuantConnect Paper Trading";
+$dataFeedName = "Polygon";
+$isBrokerage = false;
+
+$brokerageDetails = "";
+
+$dataFeedDetails = "
+    <li>Enter your Polygon API key.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Configure credentials for Polygon Data Feed
+
+Your Polygon data feed API Key:</pre>
+</div>
+
+<p>To get your API key, see the <a href='https://polygon.io/dashboard/api-keys'>API Keys page</a> on the Polygon website.</p>
+    </li>
+";
+
+$supportsIQFeed = false;
+$requiresSubscription = false;
+
+$getDeployLocalAlgorithmsText($brokerageName, $dataFeedName, $isBrokerage, $brokerageDetails, $dataFeedDetails, $supportsIQFeed, $requiresSubscription);
+?>
