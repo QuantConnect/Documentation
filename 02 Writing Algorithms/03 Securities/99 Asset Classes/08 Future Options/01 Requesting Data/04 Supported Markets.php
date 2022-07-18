4b277e1feaf0ffb920f68ed1ec35ddb2841f4b99
Web Editor <web.editor@quantconnect.com>
2022-07-18 14:52:06 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/04 Supported Markets.php
@@ -2,4 +2,4 @@
 
 <div data-tree='QuantConnect.Market' data-fields='CBOT,COMEX,NYSELIFFE'></div>
 
-<p>You don't need to pass a <code>Market</code> argument to the <code>AddOptionContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
+<p>You don't need to pass a <code>Market</code> argument to the <code>AddFutureOptionContract</code> method because the contract <code>Symbol</code> already contains the market.</p>
