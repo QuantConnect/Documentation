c976273af60150580a2bcb35b9c0aa3efcb74b1d
Web Editor <web.editor@quantconnect.com>
2022-07-18 14:52:57 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/10 Remove Subscriptions.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/10 Remove Subscriptions.php
@@ -2,5 +2,6 @@
 include(DOCS_RESOURCES."/securities/remove-option-contract.php"); 
 $pythonVariableName = "self.option_contract_symbol";
 $cSharpVariableName = "_optionContractSymbol";
+$addContractMethodName = "AddFutureOptionContract";
 $getRemoveOptionContractText($pythonVariableName, $cSharpVariableName); 
 ?>
