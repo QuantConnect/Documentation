0a72d5487d47b426c99ade989a5c01713091d240
LouisSzeto <hke0073@icloud.com>
2022-07-22 01:15:03 +0800

    dataset page orders and bug fix

--- code-generators/Alternative-Datasets-Code-Generator.py
+++ code-generators/Alternative-Datasets-Code-Generator.py
@@ -26,7 +26,7 @@ vendor_names = priority + sorted([x for x in [dataset["vendorName"].strip() for 
 for vendor in vendor_names:
     if vendor not in vendors:
         vendors[vendor] = vendor_count
-        product_count[vendor] = 1
+        product_count[vendor] = {dataset["name"].strip(): m+1 for m, dataset in enumerate(sorted([x for x in doc if x["vendorName"].strip() == vendor], key=lambda x: x['name']))}
         vendor_count += 1
         
         json_00[vendor] = {
@@ -48,13 +48,13 @@ for dataset in doc:
     datasetName = dataset["name"].strip()
         
     # Create path if not exist
-    main_dir = f'02 Writing Algorithms/14 Datasets/{vendors[vendorName]:02} {vendorName}/{product_count[vendorName]:02} {datasetName}'
+    main_dir = f'02 Writing Algorithms/14 Datasets/{vendors[vendorName]:02} {vendorName}/{product_count[vendorName][datasetName]:02} {datasetName}'
     destination_folder = pathlib.Path(main_dir)
     destination_folder.mkdir(parents=True, exist_ok=True)
     
-    for item in dataset["about"]:
-        if not item["title"]: continue
-        
+    all_sections = {**{item["title"]: item for item in dataset["about"] if item["title"]}, **{item["title"]: item for item in dataset["documentation"] if item["title"]}}
+    
+    for _, item in all_sections.items():
         content = item["content"].replace("\/", "/") \
                     .replace('<div class="qc-embed-frame"><div class="qc-embed-dummy"></div><div class="qc-embed-element"><iframe class="qc-embed-backtest"',
                                             '<div class="qc-embed-frame python" style="display: inline-block; position: relative; width: 100%; min-height: 100px; min-width: 300px;"><div class="qc-embed-dummy" style="padding-top: 56.25%;"></div><div class="qc-embed-element" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"><iframe class="qc-embed-backtest"') \
@@ -67,7 +67,7 @@ for dataset in doc:
                     
         if item["title"].lower() == "about the provider":
             json_00[vendorName]["content"] = content
-            json_00[vendorName]["featureShortDescription"][f"{product_count[vendorName]:02}"] = ""
+            json_00[vendorName]["featureShortDescription"][f"{product_count[vendorName][datasetName]:02}"] = ""
             
             with open(f'02 Writing Algorithms/14 Datasets/{vendors[vendorName]:02} {vendorName}/00.json', 'w', encoding='utf-8') as html_file:
                 html_file.write(str(json_00[vendorName]).replace('"', '\\"').replace("'", '"'))
@@ -75,44 +75,25 @@ for dataset in doc:
         if item["title"].lower() == "example applications":
             with open(destination_folder / f'99 {item["title"].strip()}.html', "w", encoding="utf-8") as html_file:
                 html_file.write(content)
-        else: 
-            if item["title"] == "Data Point Attributes":
-                attr = True
                 
-            elif item["title"].lower() == "introduction":
+            continue
+        
+        else: 
+            if item["title"].lower() == "introduction":
                 backslash = '\\'
                 content += f"""
                 
 <p>For more information about the {datasetName} dataset, including CLI commands and pricing, see the <a href=\"https://www.quantconnect.com{dataset['url'].lower().replace(backslash, '')}\">dataset listing</a>.<p>"""
                 
-            with open(destination_folder / f'{i:02} {item["title"].strip()}.html', "w", encoding="utf-8") as html_file:
-                html_file.write(content)
-                i += 1
-                
-    for item in dataset["documentation"]:
-        content = item["content"].replace("\/", "/") \
-                    .replace('<div class="qc-embed-frame"><div class="qc-embed-dummy"></div><div class="qc-embed-element"><iframe class="qc-embed-backtest"',
-                                            '<div class="qc-embed-frame python" style="display: inline-block; position: relative; width: 100%; min-height: 100px; min-width: 300px;"><div class="qc-embed-dummy" style="padding-top: 56.25%;"></div><div class="qc-embed-element" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"><iframe class="qc-embed-backtest"') \
-                    .replace('<pre><code class="language-python">', 
-                                '<pre class="python">') \
-                    .replace('<pre><code class="language-cs">',
-                                '<pre class="csharp">') \
-                    .replace('</code>',
-                                '')
-                
         if item["title"].strip() == "Universe Selection" and vendorName not in priority:
             universe_html += f"""    <li><a href="https://www.quantconnect.com/docs/v2/writing-algorithms/datasets/{vendorName.lower().replace(' ', '-')}/{datasetName.lower().replace(' ', '-')}#{i:02}-Universe-Selection">{datasetName}</a></li>
 """
         
-        if item["title"] == "Data Point Attributes" and attr:
-            continue
-        else:
-            with open(destination_folder / f'{i:02} {item["title"].strip()}.html', "w", encoding="utf-8") as html_file:
-                html_file.write(content)
-                i += 1
+        with open(destination_folder / f'{i:02} {item["title"].strip()}.html', "w", encoding="utf-8") as html_file:
+            html_file.write(content)
+            i += 1
             
     print(f'Documentation of {dataset["name"]} is generated and inplace!')
-    product_count[vendorName] = product_count[vendorName] + 1
     
 universe_html += "</ul>"
 with open('Resources/datasets/supported-alternative-dataset-universe.html', "w", encoding="utf-8") as html_file:
