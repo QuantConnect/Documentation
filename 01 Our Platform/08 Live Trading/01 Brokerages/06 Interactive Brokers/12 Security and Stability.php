1bbb6c0b0081e1869ca9cf6a22a4a8ac14358b7d
Web Editor <web.editor@quantconnect.com>
2022-07-20 20:26:12 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/12 Security and Stability.php
+++ 01 Our Platform/08 Live Trading/01 Brokerages/06 Interactive Brokers/12 Security and Stability.php
@@ -4,7 +4,7 @@ $getBrokerageStabilityText("IB", "https://www.interactivebrokers.com/en/software
 ?>
 
 <h4>Connections</h4>
-<p>IB only supports 1 connection at a time to your account. If you interfere with your brokerage account while an algorithm is connected to it, the algorithm may stop executing. To run more than 1 algorithm, <a rel="nofollow" target="_blank" href="https://www.interactivebrokers.com.hk/en/software/ptgstl/topics/ptgsubaccounts.htm">open an IB subaccount</a> for each additional algorithm.</p>
+<p>By default, IB only supports one connection at a time to your account. If you interfere with your brokerage account while an algorithm is connected to it, the algorithm may stop executing. If you deploy a live running algorithm with your IB account and want to open Trader Workstation (TWS) with the same IB account, <a rel='nofollow' target="_blank" href='https://ibkr.info/node/1004'>create a second user on your IB account</a> and log in to TWS with the new user credentials. To run more than one algorithm with IB, <a rel="nofollow" target="_blank" href="https://www.interactivebrokers.com.hk/en/software/ptgstl/topics/ptgsubaccounts.htm">open an IB subaccount</a> for each additional algorithm.</p>
 
 <h4>SMS 2FA</h4>
 <p>Our IB integration doesn't support Two-Factor Authentication (2FA) via SMS. Use the <a rel="nofollow" target="_blank" href="https://guides.interactivebrokers.com/iphone/log_in/ibkey.htm?tocpath=IB%20Key%20Security%20Protocol%7C_____0">IB Key Security via IBKR Mobile</a> instead.</p>
