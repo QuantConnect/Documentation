b522bfbf5ce0dbaaac1fab8d0747e27379784d41
Web Editor <web.editor@quantconnect.com>
2022-07-25 19:03:21 -0400

    Automated push from server made by Derek Melchin

--- 03 Research Environment/02 Datasets/01 Key Concepts/07 Extended Market Hours.php
+++ 03 Research Environment/02 Datasets/01 Key Concepts/07 Extended Market Hours.php
@@ -8,6 +8,6 @@ $getExtendedMarketHoursText($isWritingAlgorithms);
 <p>When you request historical data, the <code>History</code> method uses the extended market hours setting of your security subscription. To get historical data with a different extended market hours setting, pass an <code>extendedMarket</code> argument to the <code>History</code> method.</p>
 
 <div class="section-example-container">
-    <pre class="csharp">var history = qb.History(spy, 10, extendedMarket: false);</pre>
-    <pre class="python">history = qb.History(spy, 10, extendedMarket=False)</pre>
+    <pre class="csharp">var history = qb.History(qb.Securities.Keys, qb.Time-TimeSpan.FromDays(10), qb.Time, extendedMarket: false);</pre>
+    <pre class="python">history = qb.History(qb.Securities.Keys, qb.Time-timedelta(days=10), qb.Time, extendedMarket=False)</pre>
 </div>
