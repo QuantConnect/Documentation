5944193aff4234b66752291843c9e1ed924dc066
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 16:02:54 -0600

    Update resolutions.php

--- Resources/datasets/resolutions.php
+++ Resources/datasets/resolutions.php
@@ -8,7 +8,7 @@ $getResolutionsText = function($isWritingAlgorithms)
     
 <p>Resolution is the duration of time that's used to sample a data source. The <code>Resolution </code> enumeration has the following members:</p>
 <div data-tree='QuantConnect.Resolution'></div>
-<p>The default resolution for market data is <code>Minute</code>. To set the resolution for a security, set the <code>resolution</code> argument when you create the security subscription.</p>
+<p>The default resolution for market data is <code>Minute</code>. To set the resolution for a security, pass the <code>resolution</code> argument when you create the security subscription.</p>
 <div class='section-example-container'>
     <pre class='csharp'>{$cVariableName}AddEquity(\"SPY\", Resolution.Daily);</pre>
     <pre class='python'>{$pyVariableName}.AddEquity(\"SPY\", Resolution.Daily)</pre>
