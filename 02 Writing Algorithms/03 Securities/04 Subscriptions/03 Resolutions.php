af0dabae71c25fcf97753235a92570f29e89488a
Web Editor <web.editor@quantconnect.com>
2022-07-25 18:00:12 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/04 Subscriptions/03 Resolutions.php
+++ 02 Writing Algorithms/03 Securities/04 Subscriptions/03 Resolutions.php
@@ -1,19 +1,14 @@
-<p>Resolution is the duration of time that's used to sample a data source. The <code>Resolution </code> enumeration has the following members:</p>
-
-<div data-tree="QuantConnect.Resolution"></div>
-
-<p>The default resolution for market data is <code>Minute</code>. To set the resolution for a security, set the <code>resolution</code> argument when you create the security subscription.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">AddEquity("SPY", Resolution.Daily);</pre>
-    <pre class="python">self.AddEquity("SPY", Resolution.Daily)</pre>
-</div>
+<?php 
+include(DOCS_RESOURCES."/datasets/resolutions.php");
+$isWritingAlgorithms = true;
+$getResolutionsText($isWritingAlgorithms);
+?>
 
 <p>To set the resolution for all securities, set the <code>Resolution</code> <a href='/docs/v2/writing-algorithms/universes/key-concepts#05-Universe-Settings'>universe setting</a> before you create security subscriptions.</p>
 
 <div class="section-example-container">
     <pre class="csharp">UniverseSettings.Resolution = Resolution.Daily;</pre>
-    <pre class="python">self.UniverseSettings.Resolutio = Resolution.Daily</pre>
+    <pre class="python">self.UniverseSettings.Resolution = Resolution.Daily</pre>
 </div>
 
 <p>To see which resolutions of data are available for a dataset, see the dataset listing in the <a href="/datasets">Data Market</a>. To create custom resolution periods, see <a href='/docs/v2/writing-algorithms/consolidating-data/key-concepts'>Consolidating Data</a>.</p>
