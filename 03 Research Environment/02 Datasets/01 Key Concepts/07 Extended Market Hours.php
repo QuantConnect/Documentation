f03a1aac06e7e3f5eb0210bcfeeeeca6be5f7ccf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 16:54:04 -0600

    Update 07 Extended Market Hours.php

--- 03 Research Environment/02 Datasets/01 Key Concepts/07 Extended Market Hours.php
+++ 03 Research Environment/02 Datasets/01 Key Concepts/07 Extended Market Hours.php
@@ -5,7 +5,7 @@ $isWritingAlgorithms = false;
 $getExtendedMarketHoursText($isWritingAlgorithms);
 ?>
 
-<p>When you request historical data, the <code>History</code> method uses the extended market hours setting of your security subscription. To get historical data with a different fill forward setting, pass an <code>extendedMarket</code> argument to the <code>History</code> method.</p>
+<p>When you request historical data, the <code>History</code> method uses the extended market hours setting of your security subscription. To get historical data with a different extended market hours setting, pass an <code>extendedMarket</code> argument to the <code>History</code> method.</p>
 
 <div class="section-example-container">
     <pre class="csharp">var history = qb.History(spy, 10, extendedMarket: false);</pre>
