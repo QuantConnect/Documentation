0d3c5847d452bf2f6fa9b2b74812dbf93496db75
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 17:12:54 -0600

    Update common-errors.php

--- Resources/datasets/common-errors.php
+++ Resources/datasets/common-errors.php
@@ -7,10 +7,15 @@ $getCommonErrorsText = function($isWritingAlgorithms)
     $pyPrintFunction = $isWritingAlgorithms ? "self.Log" : "print";
     $envName = $isWritingAlgorithms ? "algorithms" : "the Research Environment";
     
-    echo "
+    if ($isWritingAlgorithms)
+    {
+        echo "
 <p>Errors can occur when you request historical data.</p>    
- 
-<h4>Empty Data Errors</h4>
+<h4>Empty Data Errors</h4>      
+";
+    }
+    
+    echo "
 <p class='python'>If the history request returns an empty DataFrame and you try to slice it, it throws an exception. To avoid issues, check if the DataFrame contains data before slicing it.</p>
 <div class='python section-example-container'>
 <pre class='python'>df = {$pyVarName}.History(symbol, 10).close    # raises exception if the request is empty
@@ -27,13 +32,13 @@ def GetSafeHistoryCloses({$pyFuncVariables}symbols):
 </div>
 
 <p>If you run {$envName} on your local machine and history requests return no data, check if your <span class='private-file-name'>data</span> directory contains the data you request. To download datasets, see <a href='/docs/v2/our-platform/datasets/licensing#04-Download'>Download</a>.</p>
-
-<h4>Numerical Precision Errors</h4>
-<p>Some factor files have INF split values, which indicate that the stock has so many splits that prices can't be calculated with correct numerical precision. To allow history requests with these symbols, we need to move the starting date forward when reading the data. If there are numerical precision errors in the factor files for a security in your history request, LEAN throws the following error:</p>
-
-<div class='error-messages'>\"Warning: when performing history requests, the start date will be adjusted if there are numerical precision errors in the factor files.\"</div>
-
 ";
+    
+    if ($isWritingAlgorithms)
+    {
+        echo "<h4>Numerical Precision Errors</h4>";
+        echo file_get_contents(DOCS_RESOURCES."/datasets/equity-common-errors.html");
+    }
 }
 
 ?>
