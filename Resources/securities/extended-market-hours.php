7252792a4f9bc72371ff3d21621f2fce6dba7656
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 16:48:05 -0600

    Update and rename extended-market-hours.html to extended-market-hours.php

--- Resources/securities/extended-market-hours.php
+++ Resources/securities/extended-market-hours.php
@@ -1,9 +1,8 @@
-<p>By default, your security subscriptions only cover regular trading hours. To subscribe to pre- and post-market trading hours for a specific asset, enable the <code>extendedMarketHours</code> argument when you create the security subscription.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">AddEquity("SPY", extendedMarketHours: true);</pre>
-    <pre class="python">self.AddEquity("SPY", extendedMarketHours=True)</pre>
-</div>
+<?php
+include(DOCS_RESOURCES."/datasets/extended-market-hours.php");
+$isWritingAlgorithms = true;
+$getExtendedMarketHoursText($isWritingAlgorithms);
+?>
 
 <p>To subscribe to pre- and post-market trading hours for all assets, enable the <code>ExtendedMarketHours</code> <a href='/docs/v2/writing-algorithms/universes/key-concepts#05-Universe-Settings'>universe setting</a> before you create the security subscriptions.</p>
 
