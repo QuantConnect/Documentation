5c623ac1dca2c8f72f056074be9bb68aa8fe6861
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 16:14:36 -0600

    Update 04 Markets.php

--- 02 Writing Algorithms/03 Securities/04 Subscriptions/04 Markets.php
+++ 02 Writing Algorithms/03 Securities/04 Subscriptions/04 Markets.php
@@ -1,9 +1,5 @@
-<p>The datasets integrated into the Dataset Market cover many markets. The <code>Market</code> enumeration has the following members:</p>
-<div data-tree="QuantConnect.Market"></div>
-
-<p>LEAN can usually determine the correct market based on the ticker you provide when you create the security subscription. To manually set the market for a security, pass a <code>market</code> argument when you create the security subscription.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">AddEquity("SPY", market: Market.USA);</pre>
-    <pre class="python">self.AddEquity("SPY", market=Market.USA)</pre>
-</div>
+<?php 
+include(DOCS_RESOURCES."/datasets/resolutions.php");
+$isWritingAlgorithms = true;
+$getResolutionsText($isWritingAlgorithms);
+?>
