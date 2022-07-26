5e4415b061530300cae58ad32c0f5f320967452d
LouisSzeto <hke0073@icloud.com>
2022-07-26 17:43:14 +0800

    fix broken links, add alternative datasets supported assets

---
+++ code-generators/alt-dataset-supported-assets-generator.py
@@ -0,0 +1,47 @@
+import glob
+from pathlib import Path
+import os
+import shutil
+
+DESTINATION = "Resources/datasets/supported-securities/alternatives"
+PROCESSED_DIR = "/nas/alternative"
+DATASETS = {
+    "atreyu": [""],
+    "brain": [""]
+}
+
+def TableCreation(files):
+    html = """<table class="table qc-table table-reflow ticker-table hidden-xs">
+<thead>
+<tr><th colspan="6">Assets Available</th></tr>
+</thead>
+<tbody>
+<tr>"""
+
+    i = 0
+
+    for file in files:
+        html += f"<td>{file.split('.')[0]}</td>"
+        i += 1
+        
+        if i == 6:
+            html += "</tr>\n<tr>"
+            i = 0
+    
+    html += "\n</tbody>\n</table>"
+    
+    return html
+
+if __name__ == '__main__':
+    DESTINATION = Path(DESTINATION)
+    if os.path.exists(DESTINATION):
+        shutil.rmtree(DESTINATION)
+    DESTINATION.mkdir(parents=True, exist_ok=True)
+        
+    for vendor, datasets in DATASETS.items():
+        for dataset in datasets:
+            files = glob.glob(f"{PROCESSED_DIR}/{vendor.lower()}/{dataset.lower()}/*.csv")
+            html_table = TableCreation(files)
+            
+            with open(DESTINATION / vendor.lower() / dataset.lower() / "support-assets.html") as html_file:
+                html_file.write(html_table)
