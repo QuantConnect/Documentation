e2471de8f87707d8b0980b9fdfc65596b2b2b326
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-25 18:01:50 -0600

    Update data-normalization.php

--- Resources/datasets/data-normalization.php
+++ Resources/datasets/data-normalization.php
@@ -2,7 +2,7 @@
 $getDataNormalizationAdjustmentText = function($isWritingAlgorithms)
 {
     $endDateWording = $isWritingAlgorithms ? "backtest end date"  : "<code>QuantBook</code> time";
-    echo "<p>We use the entire split and dividend history to adjust historical prices. This process ensures your algorithms receive the same adjusted prices, regardless of the {$endDateWording}.</p>";
+    echo "<p>We use the entire split and dividend history to adjust historical prices. This process ensures you get the same adjusted prices, regardless of the {$endDateWording}.</p>";
 }
 
 ?>
