9c434dd68970bbac0bf031a6ced26f7bac1f604f
Web Editor <web.editor@quantconnect.com>
2022-07-26 17:31:17 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/06 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/06 Supported Markets.php
@@ -1,6 +1,10 @@
-<?php 
-include(DOCS_RESOURCES."/securities/markets/crypto.php"); 
-$getMarketsText(true);
+<?php echo file_get_contents(DOCS_RESOURCES."/securities/markets/crypto.html"); ?>
 
-echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); 
-?>
+<p>To set the market for a security, pass a <code>market</code> argument to the <code>AddCrypto</code> method.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">_symbol = AddCrypto("BTCUSD", market: Market.GDAX).Symbol;</pre>
+    <pre class="python">self.symbol = self.AddCrypto("BTCUSD", market=Market.GDAX).Symbol</pre>
+</div>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); ?>
