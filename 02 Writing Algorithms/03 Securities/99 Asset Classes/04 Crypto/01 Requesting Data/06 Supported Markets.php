0132ae353483580e846555e1d1c2a46b2cbcdef6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-07-26 15:18:58 -0600

    Update 06 Supported Markets.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/06 Supported Markets.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/06 Supported Markets.php
@@ -1,12 +1,4 @@
-<p>The following <code>Market</code> enumeration members are available for Crypto:</p>
-
-<div data-tree='QuantConnect.Market' data-fields='Bitfinex,GDAX,Kraken,Binance,FTX,FTXUS,BinanceUS'></div>
-
-<p>To set the market for a security, pass a <code>market</code> argument to the <code>AddCrypto</code> method.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">_symbol = AddCrypto("BTCUSD", market: Market.GDAX).Symbol;</pre>
-    <pre class="python">self.symbol = self.AddCrypto("BTCUSD", market=Market.GDAX).Symbol</pre>
-</div>
-
-<?php echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); ?>
+<?php 
+echo file_get_contents(DOCS_RESOURCES."/securities/markets/crypto.html"); 
+echo file_get_contents(DOCS_RESOURCES."/securities/supported-markets.html"); 
+?>
