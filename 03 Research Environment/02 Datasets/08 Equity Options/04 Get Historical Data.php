9279206cea0b6804044bf4247091a83cc8615ee0
Web Editor <web.editor@quantconnect.com>
2022-08-15 19:23:08 -0400

    Automated push from server made by Derek Melchin

--- 03 Research Environment/02 Datasets/08 Equity Options/04 Get Historical Data.php
+++ 03 Research Environment/02 Datasets/08 Equity Options/04 Get Historical Data.php
@@ -1,5 +1,5 @@
 <?php
-include(DOCS_RESOURCES."/dataset/research-environment/get-historical-options-data.php");
+include(DOCS_RESOURCES."/datasets/research-environment/get-historical-options-data.php");
 $createSubscriptionsLink = "/docs/v2/research-environment/datasets/equity-options#03-Create-Subscriptions";
 $optionType = "Equity";
 $underlyingSymbolVariable = "equity_symbol";
