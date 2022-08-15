5b9e6db5a4898e86edaa10a3fcb5eb0204e4b744
Derek Melchin <melchin.derek@gmail.com>
2022-08-15 15:09:11 -0600

    Update data feeds page

--- 02 Writing Algorithms/40 Live Trading/02 Data Feeds/03 Historical Data.php
+++
@@ -1,4 +0,0 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/datasets/live-history-requests.html"); ?>
-
-<h4>Warm Up</h4>
-<?php echo file_get_contents(DOCS_RESOURCES."/datasets/warm-up-introduction.html"); ?>
