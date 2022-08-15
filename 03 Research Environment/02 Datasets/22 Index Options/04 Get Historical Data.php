57a9ad35ec32219b378d808f2c5226c7e5056497
Web Editor <web.editor@quantconnect.com>
2022-08-15 19:26:05 -0400

    Automated push from server made by Derek Melchin

--- 03 Research Environment/02 Datasets/22 Index Options/04 Get Historical Data.php
+++ 03 Research Environment/02 Datasets/22 Index Options/04 Get Historical Data.php
@@ -1,5 +1,5 @@
 <?php
-include(DOCS_RESOURCES."/dataset/research-environment/get-historical-options-data.php");
+include(DOCS_RESOURCES."/datasets/research-environment/get-historical-options-data.php");
 $createSubscriptionsLink = "/docs/v2/research-environment/datasets/index-options#03-Create-Subscriptions";
 $optionType = "Index";
 $underlyingSymbolVariable = "index_symbol";
