d81f4db8675f460d85df7d0a5782e7e6b79498cd
Web Editor <web.editor@quantconnect.com>
2022-08-16 14:25:55 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/05 Research/01 Getting Started/01 Introduction.php
+++ 01 Our Platform/05 Research/01 Getting Started/01 Introduction.php
@@ -1,3 +1,1 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/research-environment-2.php"); ?>
-
-<p>A Jupyter notebook is an web-based interactive computing platform. Notebooks let you run and edit individual blocks of code.</p>
