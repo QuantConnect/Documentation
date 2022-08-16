8096c8c52062adbf4e841417ab123b59dad35b23
Web Editor <web.editor@quantconnect.com>
2022-08-16 16:57:59 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/09 Optimization/01 Getting Started/02 Launch Optimization Jobs.php
+++ 01 Our Platform/09 Optimization/01 Getting Started/02 Launch Optimization Jobs.php
@@ -1,3 +1,6 @@
+<p>The following video demonstrates how to launch an optimization job:</p>
+<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/optimization-gif.gif'>
+
 <?php
 	echo file_get_contents(DOCS_RESOURCES."/getting-started/optimization/launch-optimization-jobs.html");
 ?>
