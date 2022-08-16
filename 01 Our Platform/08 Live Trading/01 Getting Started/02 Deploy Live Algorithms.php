f6961a782650398ac1825563688e2ed50d3c3ebe
Web Editor <web.editor@quantconnect.com>
2022-08-16 15:22:15 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/01 Getting Started/02 Deploy Live Algorithms.php
+++ 01 Our Platform/08 Live Trading/01 Getting Started/02 Deploy Live Algorithms.php
@@ -1,3 +1,6 @@
+<p>The following video demonstrates how to deploy live paper trading algorithms:</p>
+<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/deploy-paper-trading.gif'>
+
 <?php echo file_get_contents(DOCS_RESOURCES."/getting-started/live-trading/deploy-live-algorithms.html"); ?>
 
 <p>To deploy a live algorithm with a different brokerage, see the <span class='page-section-name'>Deploy Live Algorithms</span> section of the <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>
