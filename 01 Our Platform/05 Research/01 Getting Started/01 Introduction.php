8d81ea4b54438f115ad59030db17c306efb97445
Web Editor <web.editor@quantconnect.com>
2022-08-16 20:14:38 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/05 Research/01 Getting Started/01 Introduction.php
+++ 01 Our Platform/05 Research/01 Getting Started/01 Introduction.php
@@ -1,5 +1,7 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/research-environment-2.php"); ?>
 
+<img src='https://cdn.quantconnect.com/i/tu/run-research-cell-py-1.gif' class='python docs-image'>
+
 <div class="highlight">
     <p>Note: This chapter is an introduction to the Research Environment for the Algorithm Lab. For more comprehensive information on using research notebooks, see the <a href="/docs/v2/research-environment">Research Environment</a> documentation product.</p>
 </div>
