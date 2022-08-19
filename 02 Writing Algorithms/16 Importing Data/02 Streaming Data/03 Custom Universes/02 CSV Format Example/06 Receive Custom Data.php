65b862931c9e97e1f34fb7e616ee4c255fd38395
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-19 11:21:21 -0600

    Update 06 Receive Custom Data.php

--- 02 Writing Algorithms/16 Importing Data/02 Streaming Data/03 Custom Universes/02 CSV Format Example/06 Receive Custom Data.php
+++ 02 Writing Algorithms/16 Importing Data/02 Streaming Data/03 Custom Universes/02 CSV Format Example/06 Receive Custom Data.php
@@ -1,1 +1,1 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/datasets/custom-data/receive-custom-universe-example.html"); ?>
+<?php echo file_get_contents(DOCS_RESOURCES."/datasets/custom-data/universe/receive-custom-universe-example.html"); ?>
