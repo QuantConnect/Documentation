cc409c9308e9790396530628ac697745dca8cf75
Web Editor <web.editor@quantconnect.com>
2022-08-19 13:26:32 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/16 Importing Data/02 Streaming Data/03 Custom Universes/02 CSV Format Example/05 Initialize Universe.php
+++ 02 Writing Algorithms/16 Importing Data/02 Streaming Data/03 Custom Universes/02 CSV Format Example/05 Initialize Universe.php
@@ -1,12 +1,1 @@
-<p>To perform a universe selection with custom data, in the <code>Initialize</code> method, call the <code>AddUniverse</code> method.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">public class MyAlgorithm : QCAlgorithm
-{
-    AddUniverse&lt;StockDataSource&gt;("myStockDataSource", Resolution.Daily, FilterFunction);
-}</pre>
-    <pre class="python">class MyAlgorithm(QCAlgorithm): 
-    def Initialize(self) -&gt; None:
-        self.AddUniverse(StockDataSource, "my-stock-data-source", Resolution.Daily, self.FilterFunction)
-    </pre>
-</div>
+<?php echo file_get_contents(DOCS_RESOURCES."/datasets/custom-data/universe/init-custom-universe-example.html"); ?>
