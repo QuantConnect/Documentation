c8640fa1a732de7a07262a0122ae654ec18ccf8f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-22 16:06:49 -0600

    Update telegram-setup.php

--- Resources/notifications/telegram-setup.php
+++ Resources/notifications/telegram-setup.php
@@ -5,3 +5,5 @@
 </div>
 
 <p>The <code>id</code> argument is your Telegram user Id or group Id. <?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
+
+<p>To add emoticons to your <code>message</code>, convert the emoticon character to UTF-32 with the <a href='https://www.branah.com/unicode-converter' rel='nofollow' target="_blank">Unicode Converter</a> on the Branah website. For example, the rocket emoji is <code>0001f680</code> in UTF-32 format, so use <code>\U001f680</code> in your message.</p>
