21157ab471431ef923884c2cf7397800f5f31c2b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-24 15:28:08 -0600

    Update key-history-concepts.php

--- Resources/datasets/key-history-concepts.php
+++ Resources/datasets/key-history-concepts.php
@@ -14,6 +14,8 @@ $getKeyHistoryConceptsText = function($isWritingAlgorithms)
 <h4>Return Formats</h4>
 <p>Each asset class supports slightly different data formats. When you make a history request, consider what data returns. Depending on how you request the data, history requests return a specific data type. For example, if you don't provide <code>Symbol</code> objects, you get <code>Slice</code> objects that contain {$universeText}.</p>
 
+<p class='python'>The most popular return type is a <code>DataFrame</code>. If you request a <code>DataFrame</code>, LEAN unpacks the data from <code>Slice</code> objects to populate the <code>DataFrame</code>. If you intend to use the data in the <code>DataFrame</code> to create <code>TradeBar</code> or <code>QuoteBar</code> objects, request that the history request returns the data type you need. Otherwise, LEAN will waste computational resources populating the <code>DataFrame</code>.</p>
+
 
 <h4>Time Index</h4>
 
