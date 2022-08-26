c4176caca54e19b5239413af77f516c28425c44f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-25 18:27:44 -0600

    Update 05 Define Algorithm Settings.php

--- 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/04 Coinbase Pro/05 Define Algorithm Settings.php
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/04 Coinbase Pro/05 Define Algorithm Settings.php
@@ -1,4 +1,4 @@
-<p>To use the Coinbase Pro brokerage, create an <code>GDAXLiveAlgorithmSettings</code> object.</p>
+<p>To use the Coinbase Pro brokerage, create a <code>GDAXLiveAlgorithmSettings</code> object.</p>
 
 <div class="section-example-container">
     <pre class="csharp">var liveSettings = new GDAXLiveAlgorithmSettings(key: "", secret: "", passphrase: "");</pre>
