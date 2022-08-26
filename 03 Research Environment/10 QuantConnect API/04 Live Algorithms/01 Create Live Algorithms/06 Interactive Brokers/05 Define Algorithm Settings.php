e6e0f86a2c881d9ca5e9556cea9e4f90e04fd574
Web Editor <web.editor@quantconnect.com>
2022-08-25 20:16:10 -0400

    Automated push from server made by Derek Melchin

---
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/06 Interactive Brokers/05 Define Algorithm Settings.php
@@ -0,0 +1,10 @@
+<p>To use the Interactive Brokers brokerage, create an <code>InteractiveBrokersLiveAlgorithmSettings</code> object.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">var liveSettings = new InteractiveBrokersLiveAlgorithmSettings(user: "",
+                                                    password: "",
+                                                    account: "&lt;accountId&gt;");</pre>
+    <pre class="python">live_settings = InteractiveBrokersLiveAlgorithmSettings(user="",
+                                             password="",
+                                             account="&lt;account_id&gt;")</pre>
+</div>
