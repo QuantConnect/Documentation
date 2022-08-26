02e65b076f84b11743889a92969d2db14af88ef2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 12:55:07 -0600

    Update 03 Control Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/03 Algorithm Control/03 Control Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/03 Algorithm Control/03 Control Cloud Algorithms.php
@@ -6,6 +6,11 @@ include(DOCS_RESOURCES."/trading-and-orders/liquidate-positions.php");
 $getLiquidatePositionsText(true);
 ?>
 
+<p>To stop an algorithm, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean cloud live liquidate "My Project"</code>.</p>
+<div class="cli section-example-container">
+<pre>$ lean cloud live liquidate "My Project"</pre>
+</div>
+
 <p>For more information about the command options, see <a href='/docs/v2/lean-cli/api-reference/lean-cloud-live-liquidate#04-Options'>Options</a>.</p>
 
 <h4>Stop Algorithms</h4>
