4fc451e1e547a45b3af10d0a1159ba46c7610989
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 11:45:27 -0600

    Update and rename 03 Place Manual Trades.html to 03 Place Manual Trades.php

--- 01 Our Platform/08 Live Trading/07 Algorithm Control/03 Place Manual Trades.php
+++ 01 Our Platform/08 Live Trading/07 Algorithm Control/03 Place Manual Trades.php
@@ -1,11 +1,7 @@
-<p>The live results page lets you manually place orders instead of calling the automated methods in your project files. You can use any order type that is supported by the brokerage that you used when deploying the algorithm. To view the supported order types of your brokerage, see the <span class='page-section-name'>Orders</span> section of your <a href='/docs/v2/our-platform/live-trading/brokerages'>brokerage guide</a>. Some example situations where it may be helpful to place manual orders instead of stopping and redeploying the algorithm include the following:</p>
-
-<ul>
-    <li>Your brokerage account had holdings in it before you deployed your algorithm</li>
-    <li>Your algorithm had bugs in it that caused it to purchase the wrong security</li>
-    <li>You want to add a hedge to your portfolio without adjusting the algorithm code</li>
-    <li>You want to rebalance your portfolio before the rebalance date</li>
-</ul>
+<?php
+include(DOCS_RESOURCES."/trading-and-orders/place-manual-trades.php");
+$getManualTradesText(false);
+?>
 
 <p>Note that it's not currently possible to cancel manual orders.</p>
 
