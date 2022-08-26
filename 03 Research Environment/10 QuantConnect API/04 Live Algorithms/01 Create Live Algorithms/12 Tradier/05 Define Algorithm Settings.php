1ef050366836c0d654b3e01fb783be1ffb6d5b7a
Web Editor <web.editor@quantconnect.com>
2022-08-25 20:23:42 -0400

    Automated push from server made by Derek Melchin

---
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/12 Tradier/05 Define Algorithm Settings.php
@@ -0,0 +1,12 @@
+<p>To use the Tradier brokerage, create an <code>TradierLiveAlgorithmSettings</code> object.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">var liveSettings = new TradierLiveAlgorithmSettings(accessToken: "",
+                                                    dateIssued: "",
+                                                    refreshToken: "",
+                                                    account: "");</pre>
+    <pre class="python">live_settings = TradierLiveAlgorithmSettings(accessToken="",
+                                             dateIssued="",
+                                             refreshToken="",
+                                             account="")</pre>
+</div>
