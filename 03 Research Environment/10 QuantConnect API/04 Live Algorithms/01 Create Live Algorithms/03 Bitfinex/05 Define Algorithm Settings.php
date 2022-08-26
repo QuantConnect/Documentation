399b5aba1891ee227687070f47aa840163926847
Web Editor <web.editor@quantconnect.com>
2022-08-25 20:25:19 -0400

    Automated push from server made by Derek Melchin

---
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/03 Bitfinex/05 Define Algorithm Settings.php
@@ -0,0 +1,6 @@
+<p>To use the Bitfinex brokerage, create an <code>BitfinexLiveAlgorithmSettings</code> object.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">var liveSettings = new BitfinexLiveAlgorithmSettings(key: "", secret: "");</pre>
+    <pre class="python">live_settings = BitfinexLiveAlgorithmSettings(key="", secret="")</pre>
+</div>
