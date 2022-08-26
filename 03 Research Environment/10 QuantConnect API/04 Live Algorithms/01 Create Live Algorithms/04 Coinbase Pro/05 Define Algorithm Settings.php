6f2479a251459c1615cef6a357a25c6c92605272
Web Editor <web.editor@quantconnect.com>
2022-08-25 20:26:45 -0400

    Automated push from server made by Derek Melchin

---
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/04 Coinbase Pro/05 Define Algorithm Settings.php
@@ -0,0 +1,6 @@
+<p>To use the Coinbase Pro brokerage, create an <code>GDAXLiveAlgorithmSettings</code> object.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">var liveSettings = new GDAXLiveAlgorithmSettings(key: "", secret: "", passphrase: "");</pre>
+    <pre class="python">live_settings = GDAXLiveAlgorithmSettings(key="", secret="", passphrase="")</pre>
+</div>
