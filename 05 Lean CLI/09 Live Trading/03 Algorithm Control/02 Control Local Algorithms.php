7afcbea0e8e9022d43edd41080c573614b7a4bdb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 12:22:51 -0600

    Update 02 Control Local Algorithms.php

--- 05 Lean CLI/09 Live Trading/03 Algorithm Control/02 Control Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/03 Algorithm Control/02 Control Local Algorithms.php
@@ -1,5 +1,5 @@
 <h4>Add Security Subscriptions</h4>
-<p>In local deployments, you can manually create security subscriptions for your algorithm instead of calling the <code>Add<span class="placeholder-text">securityType</span></code> methods in your code files. If you add security subscriptions to your algorithm, you can place manual trades without having to edit and redeploy the algorithm. To add security subscriptions, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean live add-security "My Project"</code>.</p>
+<p>You can manually create security subscriptions for your algorithm instead of calling the <code>Add<span class="placeholder-text">securityType</span></code> methods in your code files. If you add security subscriptions to your algorithm, you can place manual trades without having to edit and redeploy the algorithm. To add security subscriptions, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean live add-security "My Project"</code>.</p>
 
 <div class="cli section-example-container">
 <pre>$ lean live add-security "My Project" --ticker "SPY" --market "usa" --security-type "equity"</pre>
@@ -26,9 +26,31 @@ $getManualTradesText(true);
 
 
 <h4>Update Orders</h4>
+<p>To update an existing order, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean live update-order "My Project"</code>.</p>
+<div class="cli section-example-container">
+<pre>$ lean live update-order "My Project" --order-id 1 --quantity 5</pre>
+</div>
+
+<p>For more information about the command options, see <a href='/docs/v2/lean-cli/api-reference/lean-live-update-order#04-Options'>Options</a>.</p>
 
 <h4>Cancel Orders</h4>
 
+<p>To cancel an existing order, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean live cancel-order "My Project"</code>.</p>
+<div class="cli section-example-container">
+<pre>$ lean live cancel-order "My Project" --order-id 1</pre>
+</div>
+
+<p>For more information about the command options, see <a href='/docs/v2/lean-cli/api-reference/lean-live-cancel-order#04-Options'>Options</a>.</p>
+
 <h4>Liquidate Positions</h4>
+<p>To liquidate a specific asset in your algorithm, open a terminal in your <a href='/docs/v2/lean-cli/initialization/directory-structure#02-lean-init'>CLI root directory</a> and then run <code>lean live liquidate "My Project"</code>.</p>
+
+<div class="cli section-example-container">
+<pre>$ lean live liquidate "My Project" --ticker "SPY" --market "usa" --security-type "equity"</pre>
+</div>
+
+<p>When you run the command, if the market is open for the asset, the algorithm liquidates it with market orders. If the market is not open, the algorithm places market on open orders.</p>
+
+<p>For more information about the command options, see <a href='/docs/v2/lean-cli/api-reference/lean-live-liquidate#04-Options'>Options</a>.</p>
 
 <h4>Stop the Algorithm</h4>
