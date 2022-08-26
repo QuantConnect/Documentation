d6821c07d125cf7fbfe7a9598a8b249f10b03aba
derekmelchin <melchin.derek@gmail.com>
2022-08-26 10:56:01 -0600

    Add Algorithm Control page to CLI docs

--- Resources/brokerages/cli-deployment/deploy-local-algorithms.php
+++ Resources/brokerages/cli-deployment/deploy-local-algorithms.php
@@ -82,8 +82,8 @@ To enter multiple options, separate them with comma.:</pre>
     if ($isBrokerage)
     {
         echo "
-        <p>If you select IQFeed, see <a href='/docs/v2/lean-cli/live-trading/other-data-feeds/iqfeed'>IQFeed</a> for set up instructions.</p>
-        <p>If you select Polygon Data Feed, see <a href='/docs/v2/lean-cli/live-trading/other-data-feeds/polygon'>Polygon</a> for set up instructions.</p>
+        <p>If you select IQFeed, see <a href='/docs/v2/lean-cli/live-trading/data-feeds/iqfeed'>IQFeed</a> for set up instructions.</p>
+        <p>If you select Polygon Data Feed, see <a href='/docs/v2/lean-cli/live-trading/data-feeds/polygon'>Polygon</a> for set up instructions.</p>
         ";
     }
 
