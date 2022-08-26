1156c2799fdd2f9debbf41ecc90def69b765d0bf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 10:23:45 -0600

    Update 05 Define Algorithm Settings.php

--- 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/08 Oanda/05 Define Algorithm Settings.php
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/08 Oanda/05 Define Algorithm Settings.php
@@ -2,11 +2,11 @@
 
 <div class="section-example-container">
     <pre class="csharp">var liveSettings = new OandaLiveAlgorithmSettings(accessToken: "",
-                                                    environment: BrokerageEnvironment.Live,
-                                                    account: "&lt;account_id&gt;");</pre>
+                                                  environment: BrokerageEnvironment.Live,
+                                                  account: "&lt;account_id&gt;");</pre>
     <pre class="python">live_settings = OandaLiveAlgorithmSettings(accessToken="",
-                                             environment=BrokerageEnvironment.Live,
-                                             account="&lt;account_id&gt;")</pre>
+                                           environment=BrokerageEnvironment.Live,
+                                           account="&lt;account_id&gt;")</pre>
 </div>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/oanda.html"); ?>
