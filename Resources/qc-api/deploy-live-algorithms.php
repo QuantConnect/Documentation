c406650dffc85063fdd17ee087e3cce98c4a0358
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-25 18:13:06 -0600

    Update deploy-live-algorithms.php

--- Resources/qc-api/deploy-live-algorithms.php
+++ Resources/qc-api/deploy-live-algorithms.php
@@ -7,15 +7,15 @@ $getDeployText = function($compileProjectsLink, $selectNodeLink, $defineAlgorith
 <p>To deploy live algorithms, call the <code>CreateLiveAlgorithm</code> method.</p>
 
 <div class='section-example-container'>
-    <pre class='csharp'>var liveAlgorithm = api.CreateLiveAlgorithm(projectId, compileId, nodeId, liveSettings);</pre>
-    <pre class='python'>new_live_algorithm = api.CreateLiveAlgorithm(project_id, compile_id, node_id, live_settings)</pre>
+    <pre class='csharp'>var liveAlgorithm = api.CreateLiveAlgorithm(projectId, compile.CompileId, nodeId, liveSettings);</pre>
+    <pre class='python'>new_live_algorithm = api.CreateLiveAlgorithm(project_id, compile.CompileId, node_id, live_settings)</pre>
 </div>
 
 <p>By default, LEAN uses the latest master branch. If the latest master branch causes issues with your live deployment, pass a LEAN version to the <code>CreateLiveAlgorithm</code> method.</p>
 
 <div class='section-example-container'>
-    <pre class='csharp'>var liveAlgorithm = api.CreateLiveAlgorithm(projectId, compileId, nodeId, liveSettings, versionId);</pre>
-    <pre class='python'>new_live_algorithm = api.CreateLiveAlgorithm(project_id, compile_id, node_id, live_settings, version_id)</pre>
+    <pre class='csharp'>var liveAlgorithm = api.CreateLiveAlgorithm(projectId, compile.CompileId, nodeId, liveSettings, versionId);</pre>
+    <pre class='python'>new_live_algorithm = api.CreateLiveAlgorithm(project_id, compile.CompileId, node_id, live_settings, version_id)</pre>
 </div>
 
 <p>The <code>CreateLiveAlgorithm</code> method returns a <code>LiveAlgorithm</code> object, which have the following attributes:</p>
