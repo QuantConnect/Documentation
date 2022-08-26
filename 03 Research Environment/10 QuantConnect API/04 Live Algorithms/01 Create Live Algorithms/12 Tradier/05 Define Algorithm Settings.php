c88ae2ca784bf071577652bd67cbed07b9a7a50e
Web Editor <web.editor@quantconnect.com>
2022-08-26 12:47:42 -0400

    Automated push from server made by Derek Melchin

--- 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/12 Tradier/05 Define Algorithm Settings.php
+++ 03 Research Environment/10 QuantConnect API/04 Live Algorithms/01 Create Live Algorithms/12 Tradier/05 Define Algorithm Settings.php
@@ -1,15 +1,19 @@
-<p>To use the Tradier brokerage, create a <code>TradierLiveAlgorithmSettings</code> object.</p>
+<p>To use the Tradier brokerage, create a <code>TradierLiveAlgorithmSettings</code> object and then set its <code>Environment</code> property.</p>
 
 <div class="section-example-container">
     <pre class="csharp">var liveSettings = new TradierLiveAlgorithmSettings(accessToken: "",
                                                     dateIssued: "",
                                                     refreshToken: "",
-                                                    account: "");</pre>
+                                                    account: "");
+liveSettings.Environment = BrokerageEnvironment.Live;</pre>
     <pre class="python">live_settings = TradierLiveAlgorithmSettings(accessToken="",
                                              dateIssued="",
                                              refreshToken="",
-                                             account="")</pre>
+                                             account="")
+live_settings.Environment = BrokerageEnvironment.Live</pre>
 </div>
 
-
 <?php echo file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/tradier.html"); ?>
+
+<p>The <code>BrokerageEnvironment</code> enumeration has the following members:</p>
+<div data-tree='QuantConnect.BrokerageEnvironment'></div>
