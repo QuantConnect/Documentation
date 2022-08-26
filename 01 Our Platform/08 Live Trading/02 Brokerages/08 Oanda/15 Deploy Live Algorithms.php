c727a607391f5b63e7fe0c69f55f35962ce42a73
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 10:22:56 -0600

    Update and rename 15 Deploy Live Algorithms.html to 15 Deploy Live Algorithms.php

--- 01 Our Platform/08 Live Trading/02 Brokerages/08 Oanda/15 Deploy Live Algorithms.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/08 Oanda/15 Deploy Live Algorithms.php
@@ -7,7 +7,7 @@
     <li>Click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/deploy-live-icon.png"> <span class="icon-name">Deploy Live</span> icon.</li>
     <li>On the Deploy Live page, click the <span class="field-name">Brokerage</span> field and then click <span class="button-name">Oanda</span> from the drop-down menu.</li>
     <li>Enter your Oanda account ID and access token.</li>
-    <p>To get this information, see <a href="/docs/v2/our-platform/live-trading/brokerages/oanda#02-Account-Types">Account Types</a>. Your account details are not saved on QuantConnect.<br></p>
+    <?php echo file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/oanda.html"); ?>
     <li>Click the <span class="field-name">Environment</span> field and then click one of the environments.</li>
 
     <p>The following table shows the supported environments:</p>
