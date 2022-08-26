a7d5dd333a71e2b2467905f633ee0d49cdcd1ea8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-08-26 12:24:51 -0600

    Update 03 Control Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/03 Algorithm Control/03 Control Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/03 Algorithm Control/03 Control Cloud Algorithms.php
@@ -1,3 +1,9 @@
 <h4>Liquidate Positions</h4>
+<?php
+include(DOCS_RESOURCES."/trading-and-orders/liquidate-positions.php");
+$getManualTradesText(true);
+?>
+
+<p>For more information about the command options, see <a href='/docs/v2/lean-cli/api-reference/lean-cloud-live-liquidate#04-Options'>Options</a>.</p>
 
 <h4>Stop Algorithms</h4>
