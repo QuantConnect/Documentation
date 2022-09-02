5882aad435fd908c112ceb4037a7979f29a974ad
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:18:20 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Brokerages/11 Samco/01 Introduction.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/11 Samco/01 Introduction.php
@@ -3,3 +3,4 @@ include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php");
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/samco.php");
 ?>
 
+<p>To view the implementation of the Samco brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Samco' rel='nofollow' target="_blank">Lean.Brokerages.Samco repository</a>.</p>
