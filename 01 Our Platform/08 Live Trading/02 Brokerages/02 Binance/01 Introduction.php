e510d3b2bf01d8b43828f18f82df166d6770f7e5
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:11:47 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Brokerages/02 Binance/01 Introduction.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/02 Binance/01 Introduction.php
@@ -1,3 +1,5 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 
 <p>Binance was founded by Changpeng Zhao in 2017 with the goal to "increase the freedom of money globally". Binance provides access to trading Crypto through spot markets and perpetual Futures. They serve clients with no minimum deposit when depositing Crypto. Binance also provides an NFT marketplace, a mining pool, and services to deposit Crypto coins in liquidity pools to earn rewards.</p>
+
+<p>To view the implementation of the Binance brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Binance' rel='nofollow' target="_blank">Lean.Brokerages.Binance repository</a>.</p>
