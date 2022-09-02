836274243c9f76805a82dd16c55733169aef040f
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:14:17 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
@@ -3,3 +3,4 @@ include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php");
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.php");
 ?>
 
+<p>To view the implementation of the IB brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.InteractiveBrokers' rel='nofollow' target="_blank">Lean.Brokerages.InteractiveBrokers repository</a>.</p>
