177f91518ddfbb9c5fa75d8488bf2498ad836f91
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:22:43 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/01 Brokerages/04 Bitfinex/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/04 Bitfinex/01 Introduction.php
@@ -2,3 +2,5 @@
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 $getCLIDeploymentIntroText("Bitfinex");
 ?>
+
+<p>To view the implementation of the Bitfinex brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Bitfinex' rel='nofollow' target="_blank">Lean.Brokerages.Bitfinex repository</a>.</p>
