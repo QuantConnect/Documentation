ad87a1aa82b291112169a279fea1bca7ea511af3
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:18:52 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Brokerages/12 Tradier/01 Introduction.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/12 Tradier/01 Introduction.php
@@ -2,3 +2,5 @@
 include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php");
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/tradier.php");
 ?>
+
+<p>To view the implementation of the Tradier brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Tradier' rel='nofollow' target="_blank">Lean.Brokerages.Tradier repository</a>.</p>
