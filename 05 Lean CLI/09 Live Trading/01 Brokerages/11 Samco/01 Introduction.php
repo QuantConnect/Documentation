8db27b5dd5b92b33ebaf8c4e1da473278bbee252
Web Editor <web.editor@quantconnect.com>
2022-09-02 16:25:52 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/01 Brokerages/11 Samco/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/11 Samco/01 Introduction.php
@@ -2,3 +2,5 @@
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 $getCLIDeploymentIntroText("Samco");
 ?>
+
+<p>To view the implementation of the Samco brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Samco' rel='nofollow' target="_blank">Lean.Brokerages.Samco repository</a>.</p>
