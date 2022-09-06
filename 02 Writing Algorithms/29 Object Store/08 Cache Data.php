4244a2d7c9f2e35874aebf8f213a1c93babc34e3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-09-06 13:25:35 -0600

    Create 08 Cache Data.php

---
+++ 02 Writing Algorithms/29 Object Store/08 Cache Data.php
@@ -0,0 +1,7 @@
+<?php
+include(DOCS_RESOURCES."/object-store/cache-data.php");
+$isAlgorithm = true;
+$cSharpPrefix = "";
+$pythonPrefix = "self.";
+$getCacheDataText($isAlgorithm, $cSharpPrefix, $pythonPrefix);
+?>
