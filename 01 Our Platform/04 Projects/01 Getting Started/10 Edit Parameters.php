aa46a6b5194a0834ef25cf8fd6d1dc77d4daf5ed
Web Editor <web.editor@quantconnect.com>
2022-09-06 13:40:20 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/01 Getting Started/10 Edit Parameters.php
+++ 01 Our Platform/04 Projects/01 Getting Started/10 Edit Parameters.php
@@ -1,5 +1,10 @@
 <p>You can add parameters, set default parameter values, and remove parameters from your projects.</p>
 
+<?php 
+include(DOCS_RESOURCES."/get-vimeo-player.php"); 
+$getVimeoPlayerText(696287618);
+?>
+
 <h4>Add Parameters</h4>
 <p>Follow these steps to add an algorithm parameter:</p>
 <ol>
