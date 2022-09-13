50eb76e8405652d1e3cca3ee064b91cf226225b2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-09-13 17:13:19 -0600

    Update rolling-statistics.php

--- Resources/algorithm-results/report/rolling-statistics.php
+++ Resources/algorithm-results/report/rolling-statistics.php
@@ -3,11 +3,12 @@
 <h4>Rolling Portfolio Beta<br></h4>
 <img src="https://cdn.quantconnect.com/i/tu/report-rolling-beta.jpg" class='img-responsive'>
 <p>
-    This chart displays the rolling portfolio beta over trailing 6 and 12 month periods.
+    This chart displays the rolling portfolio <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/glossary#07-beta'>beta</a> over trailing 6 and 12 month periods.
     The light blue line represents the 6 month period and the dark blue line represents the 12 month period.
 </p>
 
 <h4>Rolling Sharpe Ratio</h4>
 <img src="https://cdn.quantconnect.com/i/tu/report-rolling-sharpe.jpg" class='img-responsive'>
 <p>
-    This chart displays the rolling portfolio Sharpe ratio over a trailing 6 month period.<br></p>
+    This chart displays the rolling portfolio <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/glossary#24-Sharpe-ratio'>Sharpe ratio</a> over trailing 6 and 12 month periods.
+    The light blue line represents the 6 month period and the dark blue line represents the 12 month period.<br></p>
