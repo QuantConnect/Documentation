89663c8998b2804927655231784371e76ea5eaf7
Web Editor <web.editor@quantconnect.com>
2022-09-16 17:21:10 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/07 Extended Market Hours.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/01 Requesting Data/07 Extended Market Hours.php
@@ -6,3 +6,29 @@ $supportedIntradayData = true;
 $marketHoursLink = "/docs/v2/writing-algorithms/securities/asset-classes/futures/market-hours";
 $getExtMarketHoursText($cCode, $pyCode, $supportedIntradayData, $marketHoursLink);
 ?>
+
+<p>In general, we model most Futures market hours with the following segments:</p>
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Market Segment</th>
+            <th>Time</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Pre-market</td>
+	    <td>00:00:00 to 09:30:00</td>
+        </tr>
+        <tr>
+            <td>Market</td>
+	    <td>09:30:00 to 17:00:00</td>
+        </tr>
+        <tr>
+            <td>Post-market</td>
+	    <td>18:00:00 to 00:00:00</td>
+        </tr>
+    </tbody>
+</table>
+
+<p>We model it this way because some Futures, like VIX, have pre- and post-market hours, so we standardized it. With this segmentation, if you set a <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/scheduled-events'>Scheduled Events</a> for the market open, it's set for 9:30 AM instead of midnight. Additionally, LEAN excludes market hours for other asset classes, so extended market hours for Futures is disabled by default to be consistent.</p>
