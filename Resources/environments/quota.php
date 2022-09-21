6a789755210d8e6131700e997cc3318180b3a02a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-09-21 12:38:01 -0600

    Update quota.php

--- Resources/environments/quota.php
+++ Resources/environments/quota.php
@@ -2,6 +2,6 @@
 
 <?php echo file_get_contents(DOCS_RESOURCES."/quotas/coding-sessions.html"); ?>
 
-<p>If the organization you're in has more <a href='/docs/v2/our-platform/organizations/resources#04-Live-Trading-Nodes'>live trading nodes</a> than your coding session quota allows, then your coding session quota increases to the number of live trading nodes you have in the organization.</p>
+<p>If the organization you're in has more <a href='/docs/v2/our-platform/organizations/resources#04-Live-Trading-Nodes'>live trading nodes</a> than your coding session quota, then your coding session quota increases to the number of live trading nodes you have in the organization.</p>
 
 <p>The quota for free organizations is a global quota, so you can have one active coding session across all of your free organizations. The quotas for paid organizations are at the organization level. Therefore, if you are in two Quant Researcher organizations, you can have two active coding sessions in one of those organizations and another two active sessions in the other organization. These paid tier quotas are for each account, not for the organization as a whole. For instance, a Trading Firm organization can have more than eight members and all of the members can simultaneously work on projects within the organization.</p>
