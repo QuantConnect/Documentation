5c3cd92b444cce88829d061b7897554d2577aac3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-09-21 12:21:06 -0600

    Update 03 Virtual Pairs.php

--- 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/03 Holdings/03 Virtual Pairs.php
+++ 02 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/03 Holdings/03 Virtual Pairs.php
@@ -1,5 +1,5 @@
 <?php 
 include(DOCS_RESOURCES."/brokerages/virutal-pairs.php");
-$appendedTest = "If you call the <a href='/docs/v2/writing-algorithms/trading-and-orders/liquidating-positions'>Liquidate</a> method with the <code>Symbol</code> of a virtual pair, the method only liquidates the quantity in your virtual pair. For more information about liquidating Crypto postitions, see <a href='/docs/v2/writing-algorithms/trading-and-orders/crypto-trades'>Crypto Trades</a>.";
-$getVirtualPairsText($appendedTest);
+$isBrokeragePage = false;
+$getVirtualPairsText($isBrokeragePage);
 ?>
