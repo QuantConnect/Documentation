731b5779f0b8cb27f7d237f29b67e35df9f6f3cd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-06 15:00:03 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/13 Trading Technologies/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/13 Trading Technologies/03 Deploy Cloud Algorithms.php
@@ -1,4 +1,22 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
-$getDeployCloudAlgorithmsText("Trading Technologies", false);
+
+$brokerageDetails = "
+<li>Enter your TT user name, session password, account name, app key, app secret, environment, and routing sender.
+
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+User name:
+Session password: 
+Account name: 
+REST app key:
+REST app secret:
+REST environment:
+Order routing sender comp id: </div>
+
+<p>Our TT integration routes orders via the TT FIX 4.4 Connection. <a rel='nofollow' target='_blank' href='https://www.tradingtechnologies.com/contact/'>Contact your TT representative</a> to set the exchange where you would like your orders sent. Your account details are not saved on QuantConnect.</p>
+</li>
+";
+
+$getDeployCloudAlgorithmsText("Trading Technologies", true, $brokerageDetails);
 ?>
