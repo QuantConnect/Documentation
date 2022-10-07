31d1af56723da2d068a6a6f830f5c7d73ed6265d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-07 14:32:34 -0600

    Update orders.php

--- Resources/algorithm-results/orders.php
+++ Resources/algorithm-results/orders.php
@@ -2,19 +2,9 @@
 $getOrdersText = function($isLiveMode) {
     $pageName = $isLiveMode ? "live" : "backtest";
     
-    echo "<p>The {$pageName} results page displays the orders of your algorithm and you can download them to your local machine. The order timestamps are in ";
-    
-    if ($isLiveMode)
-    {
-        echo "Coordinated Universal Time (UTC).";
-    }
-    else
-    {
-        echo "your algorithm time.";
-    }
-    echo "</p>";
-    
     echo "
+        <p>The {$pageName} results page displays the orders of your algorithm and you can download them to your local machine.</p>
+        
         <h4>View in the GUI</h4>
         <p>To see the orders that your algorithm created, open the {$pageName} results page and then click the <span class='tab-name'>Orders</span> tab. If there are more than 10 orders, use the pagination tools at the bottom of the Orders Summary table to see all of the orders. Click on an individual order in the Orders Summary table to reveal additional information regarding the following:</p>
 
@@ -26,6 +16,8 @@ $getOrdersText = function($isLiveMode) {
             <li>Cancellations</li>
             <li>Option contract exercising</li>
         </ul>
+        
+        <p>In the IDE, the timestamps of the orders are based in Eastern Time (ET).</p>
 
         <h4>Download CSV</h4>
         <p>To download the orders in CSV format, open the {$pageName} results page, click the <span class='tab-name'>Orders</span> tab, and then click <span class='button-name'>Download Orders</span>. The content of the CSV file is the content displayed in the Orders Summary table when the table rows are collapsed.
@@ -37,6 +29,6 @@ $getOrdersText = function($isLiveMode) {
         ";
     }
 
-    echo "</p>";
+    echo "When you download the orders CSV, the timestamps of the orders are based in Coordinated Universal Time (UTC).</p>";
 }
 ?>
