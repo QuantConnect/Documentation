c8d44ae21ee44321ded751b640bab2fe9b731393
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-07 15:27:24 -0600

    Update key-history-concepts.php

--- Resources/datasets/key-history-concepts.php
+++ Resources/datasets/key-history-concepts.php
@@ -11,7 +11,7 @@ $getKeyHistoryConceptsText = function($isWritingAlgorithms)
 
 
 <h4>Time Period Options</h4>
-<p>You can request historical data based on a trailing number of bars, a trailing period of time, or a defined period of time. If you request data in a defined period of time, the the <code class='csharp'>DateTime</code><code class='python'>datetime</code> objects you provide are based in the <a href='{$timeZoneLink}'>{$timeZoneName} time zone</a>.</p>
+<p>You can request historical data based on a trailing number of bars, a trailing period of time, or a defined period of time. If you request data in a defined period of time, the <code class='csharp'>DateTime</code><code class='python'>datetime</code> objects you provide are based in the <a href='{$timeZoneLink}'>{$timeZoneName} time zone</a>.</p>
 
 <h4>Return Formats</h4>
 <p>Each asset class supports slightly different data formats. When you make a history request, consider what data returns. Depending on how you request the data, history requests return a specific data type. For example, if you don't provide <code>Symbol</code> objects, you get <code>Slice</code> objects that contain {$universeText}.</p>
