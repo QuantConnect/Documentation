037885ed1815c32e89b90fd83e31c2f656192c0a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-07 15:26:47 -0600

    Update key-history-concepts.php

--- Resources/datasets/key-history-concepts.php
+++ Resources/datasets/key-history-concepts.php
@@ -2,6 +2,8 @@
 $getKeyHistoryConceptsText = function($isWritingAlgorithms)
 {
     $universeText = $isWritingAlgorithms ? "the entire universe" : "all of the assets you created subscriptions for in the notebook" ;
+    $timeZoneLink = $isWritingAlgorithms ? "/docs/v2/writing-algorithms/initialization#12-Set-Time-Zone" : "/docs/v2/research-environment/initialization#04-Set-Time-Zone" ;
+    $timeZoneName = $isWritingAlgorithms ? "algorithm" : "notebook" ;
     
     echo "
     
@@ -9,7 +11,7 @@ $getKeyHistoryConceptsText = function($isWritingAlgorithms)
 
 
 <h4>Time Period Options</h4>
-<p>You can request historical data based on a trailing number of bars, a trailing period of time, or a defined period of time.</p>
+<p>You can request historical data based on a trailing number of bars, a trailing period of time, or a defined period of time. If you request data in a defined period of time, the the <code class='csharp'>DateTime</code><code class='python'>datetime</code> objects you provide are based in the <a href='{$timeZoneLink}'>{$timeZoneName} time zone</a>.</p>
 
 <h4>Return Formats</h4>
 <p>Each asset class supports slightly different data formats. When you make a history request, consider what data returns. Depending on how you request the data, history requests return a specific data type. For example, if you don't provide <code>Symbol</code> objects, you get <code>Slice</code> objects that contain {$universeText}.</p>
@@ -19,8 +21,7 @@ $getKeyHistoryConceptsText = function($isWritingAlgorithms)
 
 <h4>Time Index</h4>
 
-<p>The data that a history request returns is indexed by the <code>EndTime</code> of each data point. For daily US Equity data, this results in data points appearing on Saturday and skipping Monday.</p>
-    
+<p><span class='python'>When your history request returns a <code>DataFrame</code>, the timestamps in the <code>DataFrame</code> are based on the <a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/time-zones#05-Data-Time-Zone'>data time zone</a>.</span> When your history request returns a <code>TradeBars</code>, <code>QuoteBars</code>, <code>Ticks</code>, or <code>Slice</code> object, the <code>Time</code> properties of these objects are based on the {$timeZoneName} time zone, but the <code>EndTime</code> properties of the individual <code>TradeBar</code>, <code>QuoteBar</code>, and <code>Tick</code> objects are based on the data time zone. The <code>EndTime</code> is the end of the sampling period and when the data is actually available. For daily US Equity data, this results in data points appearing on Saturday and skipping Monday.</p>
     
     
 ";
