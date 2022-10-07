8bb7d8a587564c4928ee2b7a3609b578b0045d8c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-07 14:36:45 -0600

    Update insights.php

--- Resources/algorithm-results/insights.php
+++ Resources/algorithm-results/insights.php
@@ -6,11 +6,11 @@ $getInsightsText = function($isLiveMode) {
         <p>The {$pageName} results page displays the insights of your algorithm and you can download them to your local machine.</p>
 
         <h4>View in the GUI</h4>
-        <p>To see the insights your algorithm has emitted, open the {$pageName} result page and then click the <span class='tab-name'>Insights</span> tab. If there are more than 10 insights, use the pagination tools at the bottom of the Insights Summary table to see all of the insights.</p>
+        <p>To see the insights your algorithm has emitted, open the {$pageName} result page and then click the <span class='tab-name'>Insights</span> tab. If there are more than 10 insights, use the pagination tools at the bottom of the Insights Summary table to see all of the insights. The timestamps in the Insights Summary table are based in Eastern Time (ET).</p>
 
 
         <h4>Download JSON</h4>
-        <p>To download the insights in JSON format, open the {$pageName} result page, click the <span class='tab-name'>Insights</span> tab, and then click <span class='button-name'>Download Insights</span>.</p>
+        <p>To download the insights in JSON format, open the {$pageName} result page, click the <span class='tab-name'>Insights</span> tab, and then click <span class='button-name'>Download Insights</span>. The timestamps in the CSV file are based in Coordinated Universal Time (UTC).</p>
 
     ";
 }
