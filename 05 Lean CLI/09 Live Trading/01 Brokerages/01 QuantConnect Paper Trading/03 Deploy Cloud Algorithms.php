d0a8efedfc9ea81a653554ccd63ac8be6c943299
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-11 15:40:37 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/01 QuantConnect Paper Trading/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/01 QuantConnect Paper Trading/03 Deploy Cloud Algorithms.php
@@ -3,5 +3,6 @@ include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php")
 $isSupported = true;
 $brokerageDetails = "";
 $supportsCashHoldings = true;
-$getDeployCloudAlgorithmsText("QuantConnect Paper Trading", $isSupported, $brokerageDetails, $supportsCashHoldings);
+$supportedPortfolioHoldings = true;
+$getDeployCloudAlgorithmsText("QuantConnect Paper Trading", $isSupported, $brokerageDetails, $supportsCashHoldings, $supportedPortfolioHoldings);
 ?>
