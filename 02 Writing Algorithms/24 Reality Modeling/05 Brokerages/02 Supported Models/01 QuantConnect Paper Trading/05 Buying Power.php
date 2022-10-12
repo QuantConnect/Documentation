df385b3235d561b6805e72dcd433ab6222c63417
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-12 17:30:52 -0600

    Update 05 Buying Power.php

--- 02 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/01 QuantConnect Paper Trading/05 Buying Power.php
+++ 02 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/01 QuantConnect Paper Trading/05 Buying Power.php
@@ -1,5 +1,5 @@
 <p>The <code>DefaultBrokerageModel</code> uses sets the buying power model based on the asset class of the security. The following table shows the default buying power model of each asset class:</p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-model.html"); ?>
+<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-models.html"); ?>
 
 <p>If you have a margin account, the <code>DefaultBrokerageModel</code> allows 2x leverage for Equities, 50x leverage for Forex and CFDs, and 1x leverage for the remaining asset classes.</p>
