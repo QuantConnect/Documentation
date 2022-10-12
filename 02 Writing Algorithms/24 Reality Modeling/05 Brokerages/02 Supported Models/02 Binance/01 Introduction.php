b64f65500fb05c0ff92d2e5362f49872e878e1a3
derekmelchin <melchin.derek@gmail.com>
2022-10-12 17:27:09 -0600

    Add brokerage model docs

---
+++ 02 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/02 Binance/01 Introduction.php
@@ -0,0 +1,5 @@
+<p>This page explains the default <a href='/docs/v2/writing-algorithms/reality-modeling/key-concepts#02-Security-Level-Models'>security-level models</a> and markets of the <code>BinanceBrokerageModel</code> and the <code>BinanceUSBrokerageModel</code>.</p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/set-brokerage-model/binance.html"); ?>
+
+<p>To view the implementation of these models, see <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/blob/master/Common/Brokerages/BinanceBrokerageModel.cs'>BinanceBrokerageModel.cs</a> or <a rel='nofollow' target="_blank" href='https://github.com/QuantConnect/Lean/blob/master/Common/Brokerages/BinanceUSBrokerageModel.cs'>BinanceUSBrokerageModel.cs</a> in the LEAN GitHub repository.</p>
