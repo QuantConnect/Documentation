d8bdad84f257b56fda487c3f02cda1684fdfe343
Web Editor <web.editor@quantconnect.com>
2022-10-12 19:36:17 -0400

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/06 Interactive Brokers/05 Buying Power.php
+++ 02 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/06 Interactive Brokers/05 Buying Power.php
@@ -1,5 +1,5 @@
 <p>The <code>InteractiveBrokersBrokerageModel</code> uses sets the buying power model based on the asset class of the security. The following table shows the default buying power model of each asset class:</p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-model.html"); ?>
+<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-models.html"); ?>
 
 <p>If you have a margin account, the <code>InteractiveBrokersBrokerageModel</code> allows 2x leverage for Equities, 50x leverage for Forex, and 1x leverage for the remaining asset classes.</p>
