6c67d54f584950d6a49cf44c0f925c6fc8ee0b65
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-10-31 15:28:10 -0600

    Update and rename 02 lean init.html to 02 lean init.php

---
+++ 05 Lean CLI/03 Initialization/01 Directory Structure/02 lean init.php
@@ -0,0 +1,25 @@
+<p>
+    After installing the CLI, open a terminal in an empty directory and run <code>lean init</code> to create your CLI root directory.
+    This command scaffolds a standard directory structure containing a data directory and a <a href="/docs/v2/lean-cli/initialization/configuration#03-Lean-Configuration">Lean configuration file</a>, both of which are required to run the LEAN engine locally.
+    We recommend running all Lean CLI commands in your CLI root directory.
+</p>
+
+<?php 
+include(DOCS_RESOURCES."/cli/init/wsl.php");
+$isCLIDocs = true;
+$getWSLText($isCLIDocs);
+?>
+
+<p>
+    <code>lean init</code> creates the following structure:
+</p>
+
+<?php echo file_get_contents(DOCS_RESOURCES."/cli/init/structure.html"); ?>
+
+<p>
+    We recommend running all Lean CLI commands in your CLI root directory.
+    Doing so ensures the directory structure is always kept consistent when synchronizing projects between the cloud and your local drive.
+    It also makes it possible for the CLI to automatically find the Lean configuration file when running the LEAN engine locally.
+</p>
+
+<p>To set the default language of new projects, run <code>lean init --language &lt;value&gt;</code> where the <code>&lt;value&gt;</code> is <code>python</code> or <code>csharp</code>.</p>
