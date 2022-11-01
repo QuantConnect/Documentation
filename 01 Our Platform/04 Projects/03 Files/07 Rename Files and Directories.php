5f77117c4cc0511279a017ae1402e84ce0d63f8f
Web Editor <web.editor@quantconnect.com>
2022-11-01 19:55:51 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/03 Files/07 Rename Files and Directories.php
+++ 01 Our Platform/04 Projects/03 Files/07 Rename Files and Directories.php
@@ -1,7 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/getting-started/projects/rename-files.php");
 $fileType = "file or directory";
-$openProjectLink = "";
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
 $isDesktopDocs = false;
 $videoLink = "https://cdn.quantconnect.com/i/tu/rename-file-gif.gif";
 $getRenameFileText($fileType);
