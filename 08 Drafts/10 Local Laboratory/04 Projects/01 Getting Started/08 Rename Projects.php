755bd5183bb78694560af887fa4b0cea4ab61756
Web Editor <web.editor@quantconnect.com>
2022-11-01 17:03:03 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/08 Rename Projects.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/08 Rename Projects.php
@@ -1,1 +1,6 @@
-
+<?php
+include(DOCS_RESOURCES."/ide/rename-projects.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isDesktopDocs = false;
+$getRenameProjectsText($openProjectLink, $isDesktopDocs);
+?>
