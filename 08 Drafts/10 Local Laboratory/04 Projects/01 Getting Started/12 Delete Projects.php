5f3ab045431cc76bfd1b2fe895da9b9c160fc7cc
Web Editor <web.editor@quantconnect.com>
2022-11-01 17:13:13 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/12 Delete Projects.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/12 Delete Projects.php
@@ -1,1 +1,7 @@
+<p>Follow these steps to delete a project:</p>
 
+<ol>
+    <li><a href='/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects'>Open the project</a>.</li>
+    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-projects-tab.jpg"> <span class="icon-name">Project</span> icon.</li>
+    <li>In the Project panel, click <span class='button-name'>Delete</span>.</li>
+</ol>
