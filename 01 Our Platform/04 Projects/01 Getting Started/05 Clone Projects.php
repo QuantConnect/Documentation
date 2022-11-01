9b6bc07c81989da745b296483b26775612238762
Web Editor <web.editor@quantconnect.com>
2022-11-01 17:40:34 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/01 Getting Started/05 Clone Projects.php
+++ 01 Our Platform/04 Projects/01 Getting Started/05 Clone Projects.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/ide/clone-projects.html"); ?>
 
-<p>To clone a project, in the Project panel, click <span class='button-name'>Clone</span>.</p>
+<p>To clone a project, <a href='/docs/v2/our-platform/projects/getting-started#02-View-All-Projects'>open the project</a> and then, in the Project panel, click <span class='button-name'>Clone</span>.</p>
