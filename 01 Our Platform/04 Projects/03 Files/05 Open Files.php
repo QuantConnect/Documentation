1ebe5906b997b7d1f633162163f105c5f1789910
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 16:56:27 -0600

    Update and rename 05 Open Files.html to 05 Open Files.php

---
+++ 01 Our Platform/04 Projects/03 Files/05 Open Files.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/projects/files/open-files.php");
+$isDesktopDocs = false;
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$getOpenFilesText($isDesktopDocs, $openProjectLink);
+?>
