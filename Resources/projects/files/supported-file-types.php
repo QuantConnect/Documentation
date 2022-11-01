d82083ad6dc222b9e2329e7491b552b61d645497
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 16:00:41 -0600

    Create supported-file-types.php

---
+++ Resources/projects/files/supported-file-types.php
@@ -0,0 +1,9 @@
+
+<?php
+$getFileTypesText = function($env)
+{
+    echo "
+<p>{$env} supports <span class='public-file-name'>.cs</span>, <span class='public-file-name'>.ipynb</span>, and <span class='public-file-name'>.py</span> files.</p> 
+    ";
+}
+?>
