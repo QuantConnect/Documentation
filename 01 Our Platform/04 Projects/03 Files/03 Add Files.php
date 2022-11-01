7ce490a11950ed7e66fc087d4135fd78f505e7b1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 16:19:52 -0600

    Update and rename 03 Add Files.html to 03 Add Files.php

---
+++ 01 Our Platform/04 Projects/03 Files/03 Add Files.php
@@ -0,0 +1,8 @@
+<?php
+include(DOCS_RESOURCES."projects/files/add-files.php");
+$videoLink = "https://cdn.quantconnect.com/i/tu/add-file-gif.gif";
+$isDesktopDocs = false;
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$getAddFilesText($videoLink, $isDesktopDocs, $openProjectLink);
+?>
+
