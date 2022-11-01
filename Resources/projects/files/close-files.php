37dc9a487ab249ac637e44f44c16ff3f777b1303
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 17:07:34 -0600

    Create close-files.php

---
+++ Resources/projects/files/close-files.php
@@ -0,0 +1,10 @@
+<?php
+$getCloseFilesText = function($isDesktopDocs, $openProjectLink)
+{
+    $env = $isDesktopDocs ? "VS Code" : "the IDE" ;
+    echo "
+<p>To close a file, at the top of {$env}, click the <span class='button-name'>x</span> button on the file tab you want to close.</p>
+<p>To close all of the files in a project, at the top of {$env}, right-click one of the file tabs and then click <span class='button-name'>Close All</span>.</p>
+    ";
+}
+?>
