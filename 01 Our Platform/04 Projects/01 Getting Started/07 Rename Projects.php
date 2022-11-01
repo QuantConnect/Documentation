447eab47ab6e4a46dde3570ca430b49def286c8a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 15:02:45 -0600

    Update 07 Rename Projects.php

--- 01 Our Platform/04 Projects/01 Getting Started/07 Rename Projects.php
+++ 01 Our Platform/04 Projects/01 Getting Started/07 Rename Projects.php
@@ -1,7 +1,6 @@
-<p>Follow these steps to rename projects:</p>
-<ol>
-    <li><a href="/docs/v2/our-platform/projects/getting-started#02-View-All-Projects">Open the project</a> you want to rename.</li>
-    <li>In the Project panel, hover over the project name and then click the <span class='icon-name'>pencil</span> icon that appears.</li>
-    <img class='docs-image' style='max-height: 120px' src="https://cdn.quantconnect.com/i/tu/rename-project-1.png">
-    <li>In the <span class='field-name'>Name</span> field, enter the new project name and then click <span class='button-name'>Save Changes</span>.</li>
-</ol>
+<?php
+include(DOCS_RESOURCES."/ide/rename-projects.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isDesktopDocs = false;
+$getRenameProjectsText($openProjectLink, $isDesktopDocs);
+?>
