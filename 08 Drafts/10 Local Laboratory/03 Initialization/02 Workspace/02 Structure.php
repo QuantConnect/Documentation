991570bce8812274e7be3445b89f329e9045136a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 12:09:26 -0600

    Create 02 Structure.php

---
+++ 08 Drafts/10 Local Laboratory/03 Initialization/02 Workspace/02 Structure.php
@@ -0,0 +1,11 @@
+<p>
+    The workspace directory initially has following structure:
+</p>
+
+<?php
+include(DOCS_RESOURCES."/cli/init/structure.php"); 
+$isCLIDocs = false;
+$getStructureText($isCLIDocs);
+?>
+
+<p>As you add <a href='/docs/v2/drafts/local-laboratory/projects'>projects</a> to Local Lab, the project files are added to your workspace directory. If you create and use project libraries in your projects, the library files are added to a <span class='private-directory-name'>Library</span> directory in your workspace.</p> 
