c63f7d8532ecf08d731ad64af401f7d1a1ae96b0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 16:45:52 -0600

    Update add-files.php

--- Resources/projects/files/add-files.php
+++ Resources/projects/files/add-files.php
@@ -4,7 +4,7 @@ $getAddFilesText = function($videoLink, $isDesktopDocs, $openProjectLink)
     $navSide = $isDesktopDocs ? "left" : "right";
     $sectionName = $isDesktopDocs ? "<span class='placeholder-text'>projectName</span>" : "Project (Workspace)";
     echo "
-<p>Follow these steps to a add file to a project:</p>
+<p>Follow these steps to add a file to a project:</p>
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
     <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
