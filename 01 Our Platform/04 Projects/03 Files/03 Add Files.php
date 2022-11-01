e29d1b3f3439a4a279836bbb45818bdfc1985179
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 16:21:45 -0600

    Update 03 Add Files.php

--- 01 Our Platform/04 Projects/03 Files/03 Add Files.php
+++ 01 Our Platform/04 Projects/03 Files/03 Add Files.php
@@ -1,5 +1,5 @@
 <?php
-include(DOCS_RESOURCES."projects/files/add-files.php");
+include(DOCS_RESOURCES."/projects/files/add-files.php");
 $videoLink = "https://cdn.quantconnect.com/i/tu/add-file-gif.gif";
 $isDesktopDocs = false;
 $openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
