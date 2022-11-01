3b2a06dc88b89f03b332582bf9b1856af9dd542d
Web Editor <web.editor@quantconnect.com>
2022-11-01 19:58:49 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/05 Research/01 Getting Started/07 Rename Notebooks.php
+++ 01 Our Platform/05 Research/01 Getting Started/07 Rename Notebooks.php
@@ -1,5 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/getting-started/projects/rename-files.php");
 $fileType = "notebook";
-$getRenameFileText($fileType);
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isDesktopDocs = false;
+$getRenameFileText($fileType, $openProjectLink, $isDesktopDocs);
 ?>
