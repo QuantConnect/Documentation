2f238a25fedb7543ac71cd5d2de962e43f012903
Web Editor <web.editor@quantconnect.com>
2022-11-01 17:41:00 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/01 Getting Started/06 Migrate Projects.php
+++ 01 Our Platform/04 Projects/01 Getting Started/06 Migrate Projects.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/ide/migrate-projects.html"); ?>
 
-<p>To migrate a project, in the Project panel, click <span class='button-name'>Migrate</span>.</p>
+<p>To migrate a project, <a href='/docs/v2/our-platform/projects/getting-started#02-View-All-Projects'>open the project</a> and then, in the Project panel, click <span class='button-name'>Migrate</span>.</p>
