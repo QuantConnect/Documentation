3627857c9348bc887d4bb32066ffe40ac053dab8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 17:50:07 -0600

    Update rename-files.php

--- Resources/getting-started/projects/rename-files.php
+++ Resources/getting-started/projects/rename-files.php
@@ -1,15 +1,24 @@
 <?php
-$getRenameFileText = function($fileType)
+$getRenameFileText = function($fileType, $openProjectLink, $isDesktopDocs=false, $videoLink=null)
 {
+    $navSide = $isDesktopDocs ? "left" : "right";
+    $sectionName = $isDesktopDocs ? "<span class='placeholder-text'>projectName</span>" : "Project (Workspace)";
 	echo "
 <p>Follow these steps to rename a {$fileType} in a project:</p>
 
 <ol>
-    <li><a href='/docs/v2/our-platform/projects/getting-started#02-View-All-Projects'>Open the project</a>.</li>
-    <li>In the right navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li><a href='{$openProjectLink}'>Open the project</a>.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, right-click the {$fileType} you want to rename and then click <span class='menu-name'>Rename</span>.</li>
     <li>Enter the new name and then press <span class='button-name'>Enter</span>.</li>
 </ol>
 	";
+	
+	if (!is_null($videoLink))
+	{
+		echo "
+<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/rename-file-gif.gif'>
+		";
+	}
 }
 ?>
