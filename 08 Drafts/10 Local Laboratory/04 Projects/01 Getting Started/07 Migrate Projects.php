a6b456dbab8dd69f6b63a5e32d7a5ad3c996a288
Web Editor <web.editor@quantconnect.com>
2022-11-01 16:48:14 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/07 Migrate Projects.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/07 Migrate Projects.php
@@ -1,6 +1,6 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/ide/migrate-projects.html"); ?>
 
-<p>Follow these steps to clone a project:</p>
+<p>Follow these steps to migrate a project:</p>
 <ol>
     <li><a href='/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects'>Open the project</a>.</li>
     <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-projects-tab.jpg"> <span class="icon-name">Project</span> icon.</li>
