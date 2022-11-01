01894fd30588e2cba9c52644e174e3c4d4479bb1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 12:09:53 -0600

    Create 03 Set the Workspace.php

---
+++ 08 Drafts/10 Local Laboratory/03 Initialization/02 Workspace/03 Set the Workspace.php
@@ -0,0 +1,18 @@
+<p>Follow these steps to set the workspace directory:</p>
+<ol>
+    <li><a href="/docs/v2/drafts/local-laboratory/initialization#02-Log-In">Log in to the Local Lab</a>.</li>
+    <li>In the Project panel, click <span class="button-name">Create LEAN Workspace</span> or <span class="button-name">Select LEAN Workspace</span>.</li>
+    <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/local-lab-workspace-menu.jpg">
+    <p>You can create a new workspace directory or, if you use the LEAN CLI, you can set it to <a href="/docs/v2/lean-cli/initialization/directory-structure#02-lean-init">the directory that lean init creates</a>.</p>
+    <li>If you clicked <span class="button-name">Create LEAN Workspace</span>, in the Create Lean CLI Workspace window, create a directory to serve as the workspace and then click <span class="button-name">Select</span>.</li>
+    <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/local-lab-create-new-workspace.jpg">
+</ol>
+
+<p>It takes a few minutes to create a new workspace directory. To view the progress, see the purple bar at the bottom of Visual Studio Code.</p>
+<img class="docs-image" src="https://cdn.quantconnect.com/i/tu/local-lab-workspace-progress.jpg">
+
+<?php 
+include(DOCS_RESOURCES."/cli/init/wsl.php");
+$isCLIDocs = false;
+$getWSLText($isCLIDocs);
+?>
