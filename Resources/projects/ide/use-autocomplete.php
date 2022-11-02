a2dccc4b80616cec1417943d188ef0d3f5801d6e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:12:45 -0600

    Update use-autocomplete.php

--- Resources/projects/ide/use-autocomplete.php
+++ Resources/projects/ide/use-autocomplete.php
@@ -5,8 +5,8 @@ $getUseAutocompleteText = function($projectLink)
 <p>Follow these steps to use autocomplete:</p>
 
 <ol>
-    <li><a href='/docs/v2/our-platform/projects/getting-started#02-View-All-Projects'>Open a project</a>.</li>
-    <li>Type the first few characters of a variable, function, class, or class member that you want to autocomplete (for example, <code class='python'>self.Set</code><code class="csharp">Set</code> or <code>SimpleMovingAverage.Upda</code>).</li>
+    <li><a href='{$projectLink}'>Open a project</a>.</li>
+    <li>Type the first few characters of a variable, function, class, or class member that you want to autocomplete (for example, <code class='python'>self.Set</code><code class='csharp'>Set</code> or <code>SimpleMovingAverage.Upda</code>).</li>
     <li>Press <span class='key-combinations'>CTRL+Space</span>.</li>
     <p>If there are class members that match the characters you provided, a list of class members displays.</p>
     <img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/auto-complete.jpg'>
