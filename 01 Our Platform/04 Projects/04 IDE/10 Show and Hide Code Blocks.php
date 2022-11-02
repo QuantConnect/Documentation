05b403095b40c72c508620a83f5500e166941956
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 15:33:38 -0600

    Update and rename 10 Show and Hide Code Blocks.html to 10 Show and Hide Code Blocks.php

---
+++ 01 Our Platform/04 Projects/04 IDE/10 Show and Hide Code Blocks.php
@@ -0,0 +1,5 @@
+<?php 
+include(DOCS_RESOURCES."/projects/ide/show-and-hide-code-blocks.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$getCodeBlockText($isDesktopDocs, $openProjectLink);
+?>
