f359ed8e0f81b8d4554a4aee2e0f4ebef4a7c9b1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 17:30:19 -0600

    Update and rename 02 Set Environment.html to 02 Set Environment.php

---
+++ 01 Our Platform/04 Projects/11 Package Environments/02 Set Environment.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/projects/package-environments/set-env.php");
+$isDesktopDocs = false;
+$projectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$getSetEnvText($isDesktopDocs, $projectLink);
+?>
