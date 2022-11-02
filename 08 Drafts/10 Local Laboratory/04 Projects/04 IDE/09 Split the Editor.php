117ea1e670dc3c3e3505c915ea770a015e3693db
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 15:19:32 -0600

    Create 09 Split the Editor.php

---
+++ 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/09 Split the Editor.php
@@ -0,0 +1,7 @@
+<?php 
+include(DOCS_RESOURCES."/projects/ide/split-the-editor.php");
+$isDesktopDocs = true; 
+$openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
+$gifLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-split-the-editor.gif";
+$getSplitText($isDesktopDocs, $openProjectLink, $gifLink);
+?>
