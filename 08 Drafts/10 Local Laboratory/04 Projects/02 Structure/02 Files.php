cbe440098697ee2bac5236f95cde08097abd1a51
Web Editor <web.editor@quantconnect.com>
2022-11-02 13:30:30 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/02 Files.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/02 Files.php
@@ -1,1 +1,6 @@
-
+<?php
+include(DOCS_RESOURCES."/projects/structure/files.php");
+$isDesktopDocs = true;
+$renameFilesLink = "/docs/v2/drafts/local-laboratory/projects/files#08-Rename-Files-and-Directories";
+$getFileStructureText($isDesktopDocs, $renameFilesLink);
+?>
