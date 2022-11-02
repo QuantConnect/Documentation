7a4641029ac02a459e2ec290a0fe13d433515040
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:57:16 -0600

    Update and rename 08 Navigate the File Outline.html to 08 Navigate the File Outline.php

---
+++ 01 Our Platform/04 Projects/04 IDE/08 Navigate the File Outline.php
@@ -0,0 +1,7 @@
+<?php 
+include(DOCS_RESOURCES."/projects/ide/cloud-terminal.php");
+$isDesktopDocs = false; 
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$gifLink = "https://cdn.quantconnect.com/i/tu/navigate-outline-demo.gif";
+$getOutlineText($isDesktopDocs, $openProjectLink, $gifLink);
+?>
