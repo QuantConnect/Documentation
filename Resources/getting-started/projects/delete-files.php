45a4ffd59d31f77b5c7c5461abbee726c667891a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 18:10:40 -0600

    Update delete-files.php

--- Resources/getting-started/projects/delete-files.php
+++ Resources/getting-started/projects/delete-files.php
@@ -1,15 +1,23 @@
 <?php
-$getDeleteFileText = function($fileType)
+$getDeleteFileText = function($fileType, $openProjectLink, $isDesktopDocs=false, $videoLink=null)
 {
-	echo "
+    $navSide = $isDesktopDocs ? "left" : "right";
+    $sectionName = $isDesktopDocs ? "<span class='placeholder-text'>projectName</span>" : "Project (Workspace)";
+    echo "
 <p>Follow these steps to delete a {$fileType} in a project:</p>
-
 <ol>
-    <li><a href='/docs/v2/our-platform/projects/getting-started#02-View-All-Projects'>Open the project</a>.</li>
-    <li>In the right navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li><a href='{$openProjectLink}'>Open the project</a>.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, right-click the {$fileType} you want to delete and then click <span class='menu-name'>Delete Permanently</span>.</li>
     <li>Click <span class='button-name'>Delete</span>.</li>
 </ol>
 	";
+	
+    if (!is_null($videoLink))
+    {
+        echo "
+<img class='docs-image' src='{$videoLink}'>
+        ";
+    }
 }
 ?>
