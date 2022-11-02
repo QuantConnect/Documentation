2eb6fef58fc649fb1fe1515b250b70c6af85afb2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:21:24 -0600

    Update 07 Manage Nodes.php

--- 01 Our Platform/04 Projects/04 IDE/07 Manage Nodes.php
+++ 01 Our Platform/04 Projects/04 IDE/07 Manage Nodes.php
@@ -1,6 +1,6 @@
 <?php 
 include(DOCS_RESOURCES."/projects/ide/manage-nodes.php");
-$isDesktopDocs = true;
+$isDesktopDocs = false;
 $projectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
 $imageLink = "https://cdn.quantconnect.com/i/tu/resources-panel.png";
 $getManageNodesText($isDesktopDocs, $projectLink, $imageLink);
