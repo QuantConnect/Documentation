bb82ea4d663f640904d8cc5f9f39d8a7458f3edb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 12:21:48 -0600

    Update 06 Parameters.php

--- 01 Our Platform/04 Projects/02 Structure/06 Parameters.php
+++ 01 Our Platform/04 Projects/02 Structure/06 Parameters.php
@@ -2,7 +2,7 @@
 include(DOCS_RESOURCES."/get-vimeo-player.php"); 
 $getVimeoPlayerText(696287618);
 
-include(DOCS_RESOURCES."/projects/structure/directories.php");
+include(DOCS_RESOURCES."/projects/structure/parameters.php");
 $addParametersLink = "/docs/v2/our-platform/projects/getting-started#10-Edit-Parameters";
 $parameterOptimizationsLink = "/docs/v2/our-platform/optimization";
 $getParametersText($addParametersLink, $parameterOptimizationsLink);
