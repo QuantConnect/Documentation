89bbfc404be3c539782a3383f15240db16405157
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 11:30:42 -0600

    Update and rename 02 Files.html to 02 Files.php

---
+++ 01 Our Platform/04 Projects/02 Structure/02 Files.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/projects/structure/files.php");
+$isDesktopDocs = false;
+$renameFilesLink = "/docs/v2/our-platform/projects/files#07-Rename-Files-and-Directories";
+$getFileStructureText($isDesktopDocs, $renameFilesLink);
+?>
