690c017ff34ec40c9e55c37c90e77a845000d2f8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 15:15:21 -0600

    Update and rename 09 Split the Editor.html to 09 Split the Editor.php

---
+++ 01 Our Platform/04 Projects/04 IDE/09 Split the Editor.php
@@ -0,0 +1,7 @@
+<?php 
+include(DOCS_RESOURCES."/projects/ide/split-the-editor.php");
+$isDesktopDocs = false; 
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$gifLink = "https://cdn.quantconnect.com/i/tu/split-editor.gif";
+$getSplitText($isDesktopDocs, $openProjectLink, $gifLink);
+?>
