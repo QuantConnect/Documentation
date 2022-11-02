69443e1635c5a7a566f28be0fe55b09ac88ee5bc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:21:10 -0600

    Create manage-nodes.php

---
+++ Resources/projects/ide/manage-nodes.php
@@ -0,0 +1,45 @@
+<?php
+$getManageNodesText = function($isDesktopDocs, $projectLink, $imgLink)
+{
+    $nodeType = $isDesktopDocs ? "cloud " : "";
+    echo "
+<p>The Resources panel shows the {$nodeType}backtesting, research, and live trading nodes within your organization.</p>
+    ";
+
+    if ($isDesktopDocs)
+    {
+        include(DOCS_RESOURCES."/get-vimeo-player.php"); 
+        $getVimeoPlayerText(696287022);   
+    }
+
+    $navSide = $isDesktopDocs ? "left" : "right";
+    echo "
+<p>To view the Resources panel, <a href='{$projectLink}'>open a project</a> and then, in the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/resources-icon.png'> <span class='icon-name'>Resources</span> icon.</p>
+
+<img class='docs-image' src='{$imgLink}'>
+
+<p>The panel displays the following information for each node:</p>
+<table class='qc-table table'>
+    <thead>
+        <tr>
+            <th>Column</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><span class='column-name'>Node</span></td>
+            <td>The node name and model.</td>
+        </tr>
+        <tr>
+            <td><span class='column-name'>In Use By</span></td>
+            <td>The owner and name of the project using the node.</td>
+        </tr>
+    </tbody>
+</table>
+
+<p>To stop a running node, click the <span class='button-name'>stop</span> button next to it. You can stop nodes that you are using, but you need <a href='/docs/v2/our-platform/organizations/members#08-Permissions'>stop node permissions</a> to stop nodes other members are using.</p>
+
+<p>By default, we select the best node available in your clusters when you launch a backtest or research notebook. To use a specific node, click the check box next to a node in the panel.</p>    ";
+}
+?>
