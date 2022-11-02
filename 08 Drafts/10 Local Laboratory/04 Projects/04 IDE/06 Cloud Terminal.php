010d2da6e81a8f5b84477721a906958ac36589ab
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:42:48 -0600

    Update 06 Cloud Terminal.php

--- 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/06 Cloud Terminal.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/06 Cloud Terminal.php
@@ -2,6 +2,6 @@
 include(DOCS_RESOURCES."/projects/ide/cloud-terminal.php");
 $cloudTermianlImage = "";
 $problemsImagePy = "https://cdn.quantconnect.com/i/tu/qc-desktop-cloud-terminal-problems.jpg";
-$problemsImageC = "";
+$problemsImageC = "https://cdn.quantconnect.com/i/tu/qc-desktop-cloud-terminal-problems-c.jpg";
 $getCloudTerminalText($cloudTermianlImage, $problemsImagePy, $problemsImageC)
 ?>
