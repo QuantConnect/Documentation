647201b3d05abc1d64628c708e036adf08e6e59c
Web Editor <web.editor@quantconnect.com>
2022-11-02 14:22:33 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/06 Parameters.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/06 Parameters.php
@@ -1,1 +1,6 @@
-
+<?php 
+include(DOCS_RESOURCES."/projects/structure/parameters.php");
+$addParametersLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#10-Set-Descriptions";
+$parameterOptimizationsLink = "";
+$getParametersText($addParametersLink, $parameterOptimizationsLink);
+?>
