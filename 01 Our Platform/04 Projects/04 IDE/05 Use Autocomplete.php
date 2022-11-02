549d34ae1fab3161b12f6572aecd5819fbc26b4c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:13:15 -0600

    Update and rename 05 Use Autocomplete.html to 05 Use Autocomplete.php

---
+++ 01 Our Platform/04 Projects/04 IDE/05 Use Autocomplete.php
@@ -0,0 +1,5 @@
+<?php 
+include(DOCS_RESOURCES."/projects/ide/use-autocomplete.php");
+$projectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$getUseAutocompleteText($projectLink);
+?>
