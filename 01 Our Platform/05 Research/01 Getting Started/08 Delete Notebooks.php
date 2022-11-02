3a5e72b93aacba1189f97967bbc82a2499acdc38
Web Editor <web.editor@quantconnect.com>
2022-11-01 20:12:42 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/05 Research/01 Getting Started/08 Delete Notebooks.php
+++ 01 Our Platform/05 Research/01 Getting Started/08 Delete Notebooks.php
@@ -1,5 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/getting-started/projects/delete-files.php");
 $fileType = "notebook";
-$getDeleteFileText($fileType);
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isDesktopDocs = false;
+$getDeleteFileText($fileType, $openProjectLink, $isDesktopDocs);
 ?>
