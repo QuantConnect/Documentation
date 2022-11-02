02573829f9519b4aca433f1f396bb4ebd5c5788b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 15:05:37 -0600

    Update 08 Navigate the File Outline.php

--- 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/08 Navigate the File Outline.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/08 Navigate the File Outline.php
@@ -1,5 +1,5 @@
 <?php 
-include(DOCS_RESOURCES."/projects/ide/cloud-terminal.php");
+include(DOCS_RESOURCES."/projects/ide/navigate-the-file-outline.php");
 $isDesktopDocs = true; 
 $openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
 $gifLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-file-outline.gif";
