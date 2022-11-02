76ec9e65a697dc832b33621b09d6c3773cca7ead
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 11:56:47 -0600

    Create description.php

---
+++ Resources/projects/structure/description.php
@@ -0,0 +1,9 @@
+<?php
+$getDescriptionText = function($isDesktopDocs)
+{
+    $reportLink = $isDesktopDocs ? "backtest reports" : "<a href='/docs/v2/our-platform/backtesting/report'>backtest reports</a>";
+    echo "
+<p>You can give a project a description to provide a high-level overview of the project and its functionality. Descriptions make it easier to return to old projects and understand what is going on at a high level without having to look at the code. The project description is also displayed at the top of {$reportLink}, which you can create after your backtest completes.</p>    
+    ";
+}
+?>
