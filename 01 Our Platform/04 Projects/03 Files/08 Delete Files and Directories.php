9a0762662e1f4be9ee116c5e9d0b9047f350cf79
Web Editor <web.editor@quantconnect.com>
2022-11-01 20:13:24 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/04 Projects/03 Files/08 Delete Files and Directories.php
+++ 01 Our Platform/04 Projects/03 Files/08 Delete Files and Directories.php
@@ -3,5 +3,5 @@ include(DOCS_RESOURCES."/getting-started/projects/delete-files.php");
 $fileType = "file or directory";
 $openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
 $isDesktopDocs = false;
-$getRenameFileText($fileType, $openProjectLink, $isDesktopDocs, $videoLink);
+$getDeleteFileText($fileType, $openProjectLink, $isDesktopDocs);
 ?>
