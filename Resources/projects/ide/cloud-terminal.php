3b49b6b13e5dd904f8ab211414143d724272e81d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 14:30:02 -0600

    Create cloud-terminal.php

---
+++ Resources/projects/ide/cloud-terminal.php
@@ -0,0 +1,13 @@
+<?php
+$getCloudTerminalText = function($cloudTermianlImage, $problemsImagePy, $problemsImageC)
+{
+    echo "
+<p>The Cloud Terminal panel at the bottom of the IDE shows API messages, errors, and the logs from your algorithms.</p>
+<img class='docs-image' src='{$cloudTermianlImage}'>
+
+<p>The <span class='tab-name'>Problems</span> tab of the panel highlights the coding errors in your algorithms.</p>
+<img class='python docs-image' src='{$problemsImagePy}'>
+<img class='csharp docs-image' src='{$problemsImageC}'>
+    ";
+}
+?>
