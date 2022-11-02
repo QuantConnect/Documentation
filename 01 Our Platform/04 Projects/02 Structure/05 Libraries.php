66b177a505e0c855adfa2bfabd751652233d20dd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 12:08:07 -0600

    Update and rename 05 Libraries.html to 05 Libraries.php

---
+++ 01 Our Platform/04 Projects/02 Structure/05 Libraries.php
@@ -0,0 +1,7 @@
+<?php
+include(DOCS_RESOURCES."/projects/structure/libraries.php");
+$isDesktopDocs = false;
+$addLibrariesLink = "/docs/v2/our-platform/projects/shared-libraries#03-Add-Libraries";
+$createLibrariesLink = "/docs/v2/our-platform/projects/shared-libraries#02-Create-Libraries";
+$getLibrariesText($isDesktopDocs, $addLibrariesLink, $createLibrariesLink);
+?>
