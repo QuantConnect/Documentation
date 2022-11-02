c240086915013efcb0551980a238e1fc3c7d1723
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-01 18:11:47 -0600

    Update 08 Delete Files and Directories.php

--- 01 Our Platform/04 Projects/03 Files/08 Delete Files and Directories.php
+++ 01 Our Platform/04 Projects/03 Files/08 Delete Files and Directories.php
@@ -1,5 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/getting-started/projects/delete-files.php");
 $fileType = "file or directory";
-$getDeleteFileText($fileType);
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isDesktopDocs = false;
+$getRenameFileText($fileType, $openProjectLink, $isDesktopDocs, $videoLink);
 ?>
