58f16c29da518c5a5c4a28dc869ed2544ca98095
Web Editor <web.editor@quantconnect.com>
2022-11-03 13:42:50 -0400

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/08 Shares Libraries/02 Create Libraries.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/08 Shares Libraries/02 Create Libraries.php
@@ -1,1 +1,15 @@
+<p>Follow these steps to create a library:</p>
 
+<ol>
+    <li><a href="/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects">Open a project</a>.</li>
+    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-projects-tab.jpg"> <span class="icon-name">Project</span> icon.</li>
+    <li>In the Project panel, click <span class="button-name">Add Library</span>.</li>
+    <li>Click <span class="button-name">Create New</span>.</li>
+    <li>In the <span class="field-name">Input Library Name</span> field, enter a name for the library.</li>
+    <li>Click <span class="button-name">Create Library</span>.</li>
+    <p>The template library files are added to a new project.</p>
+    <li>Open the new library project.</li>
+    <p>Libraries are stored in the <span class='private-file-name'>Library</span> directory in your <a href='/docs/v2/drafts/local-laboratory/initialization/workspace'>workspace</a>.</p>
+    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/explorer-icon.png"> <span class="icon-name">Explorer</span> icon.</li>
+    <li>In Explorer panel, open the <span class="public-file-name python">Library.py</span><span class="public-file-name csharp">Library.cs</span> file and implement your library.</li>
+</ol>
