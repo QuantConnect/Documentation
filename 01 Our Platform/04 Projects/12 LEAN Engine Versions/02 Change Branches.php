e2e6c2d987e7884f7d3b1a30a96ac4098790bc02
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 18:06:58 -0600

    Update 02 Change Branches.php

--- 01 Our Platform/04 Projects/12 LEAN Engine Versions/02 Change Branches.php
+++ 01 Our Platform/04 Projects/12 LEAN Engine Versions/02 Change Branches.php
@@ -1,6 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/projects/lean-engine-branches/change-branches.php");
+$isDesktopDocs = false;
 $openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
 $createProjectLink = "/docs/v2/our-platform/projects/getting-started#03-Create-Projects";
-$getChangeBranchesText($openProjectLink, $createProjectLink);
+$getChangeBranchesText($isDesktopDocs, $openProjectLink, $createProjectLink);
 ?>
