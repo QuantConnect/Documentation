a07ec5161ef1b32f97f80cebdad09190d5ac3d55
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-02 18:05:11 -0600

    Update and rename 02 Change Branches.html to 02 Change Branches.php

---
+++ 01 Our Platform/04 Projects/12 LEAN Engine Versions/02 Change Branches.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/projects/lean-engine-branches/change-branches.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$createProjectLink = "/docs/v2/our-platform/projects/getting-started#03-Create-Projects";
+$getChangeBranchesText($openProjectLink, $createProjectLink);
+?>
