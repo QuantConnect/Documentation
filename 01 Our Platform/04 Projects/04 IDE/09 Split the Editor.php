22962e8fa1a67866facfd377ef36ec4f08f5de4a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-03 12:04:28 -0600

    Update 09 Split the Editor.php

--- 01 Our Platform/04 Projects/04 IDE/09 Split the Editor.php
+++ 01 Our Platform/04 Projects/04 IDE/09 Split the Editor.php
@@ -5,3 +5,5 @@ $openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-P
 $gifLink = "https://cdn.quantconnect.com/i/tu/split-editor.gif";
 $getSplitText($isDesktopDocs, $openProjectLink, $gifLink);
 ?>
+
+<p>Use this feature instead of opening multiple browser tabs for a single project. If you open open multiple browser tabs, two <a href='/docs/v2/our-platform/projects/code-sessions'>code sessions</a> will be updating the same project, which will cause the code sessions to fall out of sync.</p>
