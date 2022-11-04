992725406e306cda16e0c215e678399bdbeb2e4a
Web Editor <web.editor@quantconnect.com>
2022-11-04 13:55:27 -0400

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/03 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/03 Universe Selection.php
+++ 01 Our Platform/08 Live Trading/03 Data Feeds/08 Brokerage Data Feeds/01 Interactive Brokers/03 Universe Selection.php
@@ -3,7 +3,7 @@ include(DOCS_RESOURCES."/data-feeds/universe-selection.php");
 $getUniverseSelectionText("IB", true);
 ?>
 
-<p>Universe selection with the IB data feed occurs around 6-7 AM Eastern Time (ET) on Tuesday to Friday and at 2 AM ET on Sunday. Universe selection data isn't available when the IB servers are closed. To check the IB server status, see the <a rel='nofollow' target='_blank' href='https://www.interactivebrokers.com/en/software/systemStatus.php'>Current System Status</a> page on the IB website.</p>
+<p>Universe selection with the IB data feed occurs around 6-7 AM Eastern Time (ET) on Tuesday to Friday and at 2 AM ET on Sunday. The universe selection data comes from our Dataset Market, not the <a rel='nofollow' target='_blank' href='https://interactivebrokers.github.io/tws-api/market_scanners.html'>TWS market scanners</a>. Universe selection data isn't available when the IB servers are closed. To check the IB server status, see the <a rel='nofollow' target='_blank' href='https://www.interactivebrokers.com/en/software/systemStatus.php'>Current System Status</a> page on the IB website.</p>
 
 <p>The IB data feed can stream data for up to 100 assets by default, but IB may let you stream more than 100 assets based on your commissions and equity value. For more information about data feed limits from IB, see the <a rel='nofollow' target='_blank' href='https://www.interactivebrokers.com/en/pricing/research-news-marketdata.php'>Market Data Pricing Overview</a> page on the IB website. If IB lets you stream more than 100 assets, set the <code>DataSubscriptionLimit</code> of your algorithm to the new limit from IB.</p>
 
