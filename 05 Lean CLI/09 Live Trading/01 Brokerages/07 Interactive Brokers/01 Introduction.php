144f7261035d65e150fcfd79f042e47f7ff24ecf
Web Editor <web.editor@quantconnect.com>
2022-11-08 13:33:47 -0500

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/01 Introduction.php
@@ -1,6 +1,6 @@
 <?php
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
-$getCLIDeploymentIntroText("Interactive Brokers");
+$getCLIDeploymentIntroText("Interactive Brokers (IB)");
 ?>
 
 <p>To view the implementation of the IB brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.InteractiveBrokers' rel='nofollow' target="_blank">Lean.Brokerages.InteractiveBrokers repository</a>.</p>
