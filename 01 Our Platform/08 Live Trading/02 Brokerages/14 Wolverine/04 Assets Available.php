ce14ddf2c69a6b1ddd23628dc0c7c15b3061d46a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-11 17:31:07 -0700

    Update 04 Assets Available.php

--- 01 Our Platform/08 Live Trading/02 Brokerages/14 Wolverine/04 Assets Available.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/14 Wolverine/04 Assets Available.php
@@ -1,6 +1,5 @@
 <p>Refer to the <a href="/datasets/algoseek-us-equities">US Equities</a> dataset to see the assets available.</p>
 
-<h4>ETFs</h4>
 <?php
 include(DOCS_RESOURCES."/brokerages/us-etfs.php");
 $getUSETFText("Wolverine");
