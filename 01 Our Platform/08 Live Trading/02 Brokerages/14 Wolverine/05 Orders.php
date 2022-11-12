d5b79c0c04242cf3b083f6f91a94205a8eff097d
Web Editor <web.editor@quantconnect.com>
2022-11-11 19:35:42 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/08 Live Trading/02 Brokerages/14 Wolverine/05 Orders.php
+++ 01 Our Platform/08 Live Trading/02 Brokerages/14 Wolverine/05 Orders.php
@@ -1,4 +1,4 @@
-<p>Currently, only <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-orders'>Market Order</a> is supported.</p>
+<p>Our Wolverine integration currently only supports <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-orders'>market orders</a>.</p>
 
 <div class="section-example-container">
     <pre class="csharp">MarketOrder(_symbol, quantity);</pre>
@@ -6,7 +6,7 @@
 </div>
 
 <h4>Updates</h4>
-<p>We cannot update market orders.</p>
+<p>Order updates aren't supported for market orders.</p>
 
 <h4>Extended Market Hours</h4>
 <p>Wolverine does not support extended market hours trading. If you place an order outside of regular trading hours, the order will be invalid.</p>
