9e3d3f6220881092fd98af1afd0858fe7cc36e52
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-11-22 11:31:16 -0700

    Update and rename filter-caveats.html to filter-caveats.php

---
+++ Resources/universes/option/filter-caveats.php
@@ -0,0 +1,4 @@
+<?php
+$getFilterCaveatText = function($assetClass)
+  echo "<p>Some of the preceding filter methods only set an internal enumeration in the <code>{$assetClass}FilterUniverse</code> that it uses later on in the filter process. This subset filter methods don't immediately reduce the number of contract <code>Symbol</code> objects in the <code>{$assetClass}FilterUniverse</code>.</p>";
+?>
