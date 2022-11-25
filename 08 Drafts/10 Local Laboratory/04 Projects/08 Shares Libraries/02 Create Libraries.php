1c4498de8e1e2a966c16234347fc972013d98bdc
Web Editor <web.editor@quantconnect.com>
2022-11-25 17:29:46 -0500

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/08 Shares Libraries/02 Create Libraries.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/08 Shares Libraries/02 Create Libraries.php
@@ -2,7 +2,7 @@
 
 <ol>
     <li><a href="/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects">Open a project</a>.</li>
-    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-projects-tab.jpg"> <span class="icon-name">Project</span> icon.</li>
+    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg"> <span class="icon-name">QuantConnect</span> icon.</li>
     <li>In the Project panel, click <span class="button-name">Add Library</span>.</li>
     <li>Click <span class="button-name">Create New</span>.</li>
     <li>In the <span class="field-name">Input Library Name</span> field, enter a name for the library.</li>
