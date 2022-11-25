675ac236fd76a8af1ec7953dba637b49fea6d1b6
Web Editor <web.editor@quantconnect.com>
2022-11-25 17:37:59 -0500

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/07 Migrate Projects.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/07 Migrate Projects.php
@@ -3,6 +3,6 @@
 <p>Follow these steps to migrate a project:</p>
 <ol>
     <li><a href='/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects'>Open the project</a>.</li>
-    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-projects-tab.jpg"> <span class="icon-name">Project</span> icon.</li>
+    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg"> <span class="icon-name">QuantConnect</span> icon.</li>
     <li>In the Project panel, click <span class='button-name'>Migrate</span>.</li>
 </ol>
