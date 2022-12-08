239c3eb25b932a5559df303431affe0c08e26fee
Web Editor <web.editor@quantconnect.com>
2022-12-08 18:23:55 -0500

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/12 Universes/07 Future Options/02 Create Universes.php
+++ 02 Writing Algorithms/12 Universes/07 Future Options/02 Create Universes.php
@@ -25,7 +25,7 @@ self.AddFutureOption(future.Symbol)</pre>
         <tr>
             <td><code>symbol</code></td>
 	        <td><code>Symbol</code></td>
-            <td>The Future canonical symbol</td>
+            <td>The continuous Future contract Symbol.</td>
             <td></td>
         </tr>
         <tr>
