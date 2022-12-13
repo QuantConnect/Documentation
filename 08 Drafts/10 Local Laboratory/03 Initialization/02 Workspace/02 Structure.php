b857f89699e05214210d1b0d7ce05b4e1618232d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 16:23:24 -0700

    Update 02 Structure.php

--- 08 Drafts/10 Local Laboratory/03 Initialization/02 Workspace/02 Structure.php
+++ 08 Drafts/10 Local Laboratory/03 Initialization/02 Workspace/02 Structure.php
@@ -8,4 +8,4 @@ $isCLIDocs = false;
 $getStructureText($isCLIDocs);
 ?>
 
-<p>As you add <a href='/docs/v2/drafts/local-laboratory/projects/getting-started'>projects</a> to Local Lab, the project files are added to your workspace directory. If you create and use <a href='/docs/v2/drafts/local-laboratory/projects/shares-libraries'>project libraries</a> in your projects, the library files are added to a <span class='private-directory-name'>Library</span> directory in your workspace.</p> 
+<p>As you add <a href='/docs/v2/drafts/local-laboratory/projects/getting-started'>projects</a> to Local Lab, the project files are added to your workspace directory. If you create and use <a href='/docs/v2/drafts/local-laboratory/projects/shares-libraries'>shared libraries</a> in your projects, the library files are added to a <span class='private-directory-name'>Library</span> directory in your workspace.</p> 
