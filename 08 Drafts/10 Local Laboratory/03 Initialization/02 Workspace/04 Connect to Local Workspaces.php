086101e6174ef4cc0ef98f17291ad15f2a02e129
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 15:53:46 -0700

    Update and rename 04 Connect to Local Workspaces.php to 04 Connect to Local Workspaces.html

--- 08 Drafts/10 Local Laboratory/03 Initialization/02 Workspace/04 Connect to Local Workspaces.php
+++
@@ -1,6 +0,0 @@
-<p>If you have pulled a cloud workspace before or have set up a <a href='/docs/v2/lean-cli/initialization/workspace'>CLI workspace</a>, you can click <span class="button-name">Use Existing Organization Workspace</span>.</p>
-
-<ol>
-    <li>In the Select Workspace panel, click <span class="button-name">Pull Organization Workspace</span> or <span class="button-name">Use Existing Organization Workspace</span>.</li>
-    <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/select-workspace.png">
-</ol>
