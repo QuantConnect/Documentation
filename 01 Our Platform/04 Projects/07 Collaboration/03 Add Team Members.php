f47e9c516d68238f3e305b7a0736af4bc7a14444
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 16:02:54 -0700

    Update and rename 03 Add Team Members.html to 03 Add Team Members.php

---
+++ 01 Our Platform/04 Projects/07 Collaboration/03 Add Team Members.php
@@ -0,0 +1,7 @@
+<?php
+include(DOCS_RESOURCES."/projects/collab/add-team-members.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$sharedLibraryLink = "/docs/v2/our-platform/projects/shared-libraries";
+$isLocal = false;
+$getAddMembersText($openProjectLink, $sharedLibraryLink, $isLocal);
+?>
