6d5004f9d33109389f1748820282ef4c9906904e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 11:25:05 -0700

    Delete 07 Manage Nodes.php

--- 08 Drafts/10 Local Laboratory/04 Projects/04 IDE/07 Manage Nodes.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include(DOCS_RESOURCES."/projects/ide/manage-nodes.php");
-$isDesktopDocs = true;
-$projectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
-$imageLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-resources-panel.jpg";
-$getManageNodesText($isDesktopDocs, $projectLink, $imageLink);
-?>
