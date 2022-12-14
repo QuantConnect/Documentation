ede1bf410c82b06b1f218cb79d64c885a69bddf3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 18:04:08 -0700

    Update add-files.php

--- Resources/projects/files/add-files.php
+++ Resources/projects/files/add-files.php
@@ -7,7 +7,7 @@ $getAddFilesText = function($videoLink, $isDesktopDocs, $openProjectLink)
 <p>Follow these steps to add a file to a project:</p>
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, expand the <span class='button-name'>{$sectionName}</span> section.</li>
     <li>Click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/new-file-icon.png'> <span class='icon-name'>New File</span> icon.</li>
     <li>Enter a file name and extension.</li>
