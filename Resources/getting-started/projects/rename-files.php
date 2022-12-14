21507a862b4642872534c4b0db81e0f711549cd3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 18:02:32 -0700

    Update rename-files.php

--- Resources/getting-started/projects/rename-files.php
+++ Resources/getting-started/projects/rename-files.php
@@ -8,7 +8,7 @@ $getRenameFileText = function($fileType, $openProjectLink, $isDesktopDocs=false,
 
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, right-click the {$fileType} you want to rename and then click <span class='menu-name'>Rename</span>.</li>
     <li>Enter the new name and then press <span class='button-name'>Enter</span>.</li>
 </ol>
