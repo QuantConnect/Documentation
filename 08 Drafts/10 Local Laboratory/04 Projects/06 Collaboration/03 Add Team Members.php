09dac06eebf57fefba1956cc9654d21dd9596cf9
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 16:03:56 -0700

    Update 03 Add Team Members.php

--- 08 Drafts/10 Local Laboratory/04 Projects/06 Collaboration/03 Add Team Members.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/06 Collaboration/03 Add Team Members.php
@@ -1,1 +1,7 @@
-
+<?php
+include(DOCS_RESOURCES."/projects/collab/add-team-members.php");
+$openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
+$sharedLibraryLink = "/docs/v2/drafts/local-laboratory/projects/shared-libraries";
+$isLocal = true;
+$getAddMembersText($openProjectLink, $sharedLibraryLink, $isLocal);
+?>
