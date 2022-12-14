d6a45709533c8fb0b5f4f685dc81e29b235259d3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 18:04:22 -0700

    Update open-files.php

--- Resources/projects/files/open-files.php
+++ Resources/projects/files/open-files.php
@@ -7,7 +7,7 @@ $getOpenFilesText = function($isDesktopDocs, $openProjectLink)
 
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, click the file you want to open.</li>
 </ol>
     ";
