c43ad55e52b12e1208e42887f3e5c26c8f902200
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 18:02:48 -0700

    Update delete-files.php

--- Resources/getting-started/projects/delete-files.php
+++ Resources/getting-started/projects/delete-files.php
@@ -7,7 +7,7 @@ $getDeleteFileText = function($fileType, $openProjectLink, $isDesktopDocs=false)
 <p>Follow these steps to delete a {$fileType} in a project:</p>
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, right-click the {$fileType} you want to delete and then click <span class='menu-name'>Delete Permanently</span>.</li>
     <li>Click <span class='button-name'>Delete</span>.</li>
 </ol>
