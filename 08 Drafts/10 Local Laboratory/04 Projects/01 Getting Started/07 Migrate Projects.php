1f9822f5d69b2371701efc9ffaaa1da8bfd78930
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 17:38:34 -0700

    Delete 07 Migrate Projects.php

--- 08 Drafts/10 Local Laboratory/04 Projects/01 Getting Started/07 Migrate Projects.php
+++
@@ -1,8 +0,0 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/ide/migrate-projects.html"); ?>
-
-<p>Follow these steps to migrate a project:</p>
-<ol>
-    <li><a href='/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects'>Open the project</a>.</li>
-    <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg"> <span class="icon-name">QuantConnect</span> icon.</li>
-    <li>In the Project panel, click <span class='button-name'>Migrate</span>.</li>
-</ol>
