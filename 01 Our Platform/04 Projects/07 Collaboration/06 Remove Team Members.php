a8ed67ccfc29f21ae7e4a8d620c4af1e893b1996
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 16:56:31 -0700

    Update 06 Remove Team Members.php

--- 01 Our Platform/04 Projects/07 Collaboration/06 Remove Team Members.php
+++ 01 Our Platform/04 Projects/07 Collaboration/06 Remove Team Members.php
@@ -1,6 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/projects/collab/remove-team-members.php");
 $openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$deleteProjectLink = "/docs/v2/our-platform/projects/getting-started#11-Delete-Projects";
 $isLocal = false;
-$getRemoveMemberText($openProjectLink, $isLocal);
+$getRemoveMemberText($openProjectLink, $deleteProjectLink, $isLocal);
 ?>
