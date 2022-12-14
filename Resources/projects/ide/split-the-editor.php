f0c681de858a99c53ab503bd9d542c5aa065f8a8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-13 18:05:02 -0700

    Update split-the-editor.php

--- Resources/projects/ide/split-the-editor.php
+++ Resources/projects/ide/split-the-editor.php
@@ -6,7 +6,7 @@ $getSplitText = function($isDesktopDocs, $openProjectLink, $gifLink)
 <p>The editor can split horizontally and vertically to display multiple files at once. Follow these steps to split the editor:</p>
 <ol>
     <li><a href='{$openProjectLink}'>Open a project</a>.</li>
-    <li>In the {$navSide} navigation bar, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/explorer-icon.png'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the {$navSide} navigation bar, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the <span class='page-section-name placeholder-text'>projectName</span> section, drag and drop the files you want to open.</li>
 </ol>
 
