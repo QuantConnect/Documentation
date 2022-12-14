ea01ebe1cf09593f65ff77a8c704cb30a13e644f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 16:51:22 -0700

    Update 05 Toggle Live Control.php

--- 08 Drafts/10 Local Laboratory/04 Projects/06 Collaboration/05 Toggle Live Control.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/06 Collaboration/05 Toggle Live Control.php
@@ -1,1 +1,6 @@
-
+<?php
+include(DOCS_RESOURCES."/projects/collab/toggle-live-control.php");
+$openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
+$isLocal = true;
+$getToggleControlText($openProjectLink, $isLocal);
+?>
