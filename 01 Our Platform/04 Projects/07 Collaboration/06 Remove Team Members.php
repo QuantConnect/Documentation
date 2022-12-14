0d30ac23eebc219aafd2f9f5cbfa26955837b6a7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 16:55:10 -0700

    Update and rename 06 Remove Team Members.html to 06 Remove Team Members.php

---
+++ 01 Our Platform/04 Projects/07 Collaboration/06 Remove Team Members.php
@@ -0,0 +1,6 @@
+<?php
+include(DOCS_RESOURCES."/projects/collab/remove-team-members.php");
+$openProjectLink = "/docs/v2/our-platform/projects/getting-started#02-View-All-Projects";
+$isLocal = false;
+$getRemoveMemberText($openProjectLink, $isLocal);
+?>
