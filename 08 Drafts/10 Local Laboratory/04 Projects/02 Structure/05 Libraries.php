60f47b3a5d4df8b4bcfa6b4ae812aed71f1c9959
Web Editor <web.editor@quantconnect.com>
2022-12-14 19:01:23 -0500

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/05 Libraries.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/02 Structure/05 Libraries.php
@@ -1,7 +1,7 @@
 <?php
 include(DOCS_RESOURCES."/projects/structure/libraries.php");
 $isDesktopDocs = true;
-$addLibrariesLink = "/docs/v2/drafts/local-laboratory/projects/shares-libraried#03-Add-Libraries";
-$createLibrariesLink = "/docs/v2/drafts/local-laboratory/projects/shares-libraried#02-Create-Libraries";
+$addLibrariesLink = "/docs/v2/drafts/local-laboratory/projects/shared-libraried#03-Add-Libraries";
+$createLibrariesLink = "/docs/v2/drafts/local-laboratory/projects/shared-libraried#02-Create-Libraries";
 $getLibrariesText($isDesktopDocs, $addLibrariesLink, $createLibrariesLink);
 ?>
