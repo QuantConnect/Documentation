0554f68b59ea60cf364edba07cfe78dbf1c53cd2
Web Editor <web.editor@quantconnect.com>
2022-12-15 15:38:58 -0500

    Automated push from server made by Derek Melchin

--- 01 Our Platform/09 Live Trading/02 Brokerages/12 TD Ameritrade/01 Introduction.php
+++ 01 Our Platform/09 Live Trading/02 Brokerages/12 TD Ameritrade/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 
-<p>TD Ameritrade was founded by Joe Ricketts in 1971 with the goal to make "smart investors smarter through award-winning trading technology, education, and service". TD Ameritrade provides access to trading Equities, Options, Futures, Forex, Crypto, and other assets for clients with a <a rel='nofollow' target='_blank' href='https://www.tdameritrade.com/account-types/faqs-opening.html'>no minimum deposit</a>. TD Ameritrade also provides a collateral lending program, education services, and a desktop trading platform.</p>
+<p>TD Ameritrade was founded by Joe Ricketts in 1971 with the goal to make "smart investors smarter through award-winning trading technology, education, and service". TD Ameritrade provides access to trading Equities, Options, Futures, Forex, Crypto, and other assets for clients with <a rel='nofollow' target='_blank' href='https://www.tdameritrade.com/account-types/faqs-opening.html'>no minimum deposit</a>. TD Ameritrade also provides a collateral lending program, education services, and a desktop trading platform.</p>
 
 <p>To view the implementation of the TD Ameritrade brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.TDAmeritrade' rel='nofollow' target="_blank">Lean.Brokerages.TDAmeritrade repository</a>.</p>
