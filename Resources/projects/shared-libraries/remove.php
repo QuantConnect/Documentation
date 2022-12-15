fb6e2ea2dc8292e3e8394e01087d1df1191c3cf2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 17:18:43 -0700

    Create remove.php

---
+++ Resources/projects/shared-libraries/remove.php
@@ -0,0 +1,24 @@
+<?php
+$getRemoveText = function($openProjectLink, $isLocal)
+{
+    echo "
+<p>Follow these steps to remove a library from your project:</p>
+<ol>
+    <li><a href='{$openProjectLink}'>Open the project</a> that contains the library that you want to remove.</li>
+    ";
+    
+    if ($isLocal)
+    {
+        echo "
+    <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li>
+        ";
+    }
+    
+    echo "
+    <li>In the Project panel, hover over the library name and then click the <span class='icon-name'>trash can</span> icon that appears.</li>
+    <img class='docs-image' src='https://cdn.quantconnect.com/i/tu/delete-libraries.png'>
+    <p>The library files are removed from your project.</p>
+</ol>
+    ";
+}
+?>
