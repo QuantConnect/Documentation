becc5c750300af1c1a995c30bc12ec8b72c69328
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 17:15:34 -0700

    Create rename.php

---
+++ Resources/projects/shared-libraries/rename.php
@@ -0,0 +1,9 @@
+<?php
+$getRenameText = function($openProjectLink, $renameProjectLink)
+{
+
+    echo "
+<p>To rename a library, <a href='{$openProjectLink}'>open the library project file</a> and then <a href='{$renameProjectLink}'>rename the project</a>.</p>
+    ";
+}
+?>
