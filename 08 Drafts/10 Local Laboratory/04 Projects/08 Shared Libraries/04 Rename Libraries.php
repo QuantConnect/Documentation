6cd91381c41d7a2fc7145e8dc068bfa5aeb7c7ff
Web Editor <web.editor@quantconnect.com>
2022-12-14 19:14:37 -0500

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/08 Shared Libraries/04 Rename Libraries.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/08 Shared Libraries/04 Rename Libraries.php
@@ -1,1 +1,6 @@
-
+<?php
+include(DOCS_RESOURCES."/projects/shared-libraries/rename.php");
+$openProjectLink = '/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects';
+$renameProjectLink = '/docs/v2/drafts/local-laboratory/projects/getting-started#08-Rename-Projects';
+$getRenameText($openProjectLink, $renameProjectLink);
+?>
