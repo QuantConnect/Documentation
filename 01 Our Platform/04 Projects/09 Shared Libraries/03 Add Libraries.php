c35a013432a0332352045f34db555a056cc9d2fe
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2022-12-14 17:09:15 -0700

    Update 03 Add Libraries.php

--- 01 Our Platform/04 Projects/09 Shared Libraries/03 Add Libraries.php
+++ 01 Our Platform/04 Projects/09 Shared Libraries/03 Add Libraries.php
@@ -1,6 +1,6 @@
 <?php
 include(DOCS_RESOURCES."/projects/shared-libraries/add.php");
 $openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
-$isLocal = true;
+$isLocal = false;
 $getAddText($openProjectLink, $isLocal);
 ?>
