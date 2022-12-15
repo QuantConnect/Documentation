219810434773ff85ff010f0ab10453f49a812684
Web Editor <web.editor@quantconnect.com>
2022-12-14 19:09:42 -0500

    Automated push from server made by Derek Melchin

--- 08 Drafts/10 Local Laboratory/04 Projects/08 Shared Libraries/03 Add Libraries.php
+++ 08 Drafts/10 Local Laboratory/04 Projects/08 Shared Libraries/03 Add Libraries.php
@@ -1,1 +1,6 @@
-
+<?php
+include(DOCS_RESOURCES."/projects/shared-libraries/add.php");
+$openProjectLink = "/docs/v2/drafts/local-laboratory/projects/getting-started#04-Open-Projects";
+$isLocal = true;
+$getAddText($openProjectLink, $isLocal);
+?>
