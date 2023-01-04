5fdbf2487149a042dcb9ea15eb64c3d7e1b40e30
Web Editor <web.editor@quantconnect.com>
2023-01-04 11:37:06 -0500

    Automated push from server made by Derek Melchin

--- 02 Writing Algorithms/40 Live Trading/41 Charting and Logging/03 Logging.php
+++ 02 Writing Algorithms/40 Live Trading/41 Charting and Logging/03 Logging.php
@@ -1,4 +1,7 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/logging-statements/introduction.html"); ?>
+<?php 
+include(DOCS_RESOURCES."/logging-statements/introduction.html"); 
+$getLogIntroText("");
+?>
 
 <p>If you run algorithms on QuantConnect, you must stay within the <a href="/docs/v2/our-platform/organizations/resources#09-Log-Quotas">log quota</a>. To only log when your algorithm is live, use the <code>LiveMode</code> property.</p>
 
