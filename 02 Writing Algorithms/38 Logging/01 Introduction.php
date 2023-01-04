a6958a72e16a6644943df7d97c541ac8bc3ee665
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-01-03 18:24:43 -0700

    Update 01 Introduction.php

--- 02 Writing Algorithms/38 Logging/01 Introduction.php
+++ 02 Writing Algorithms/38 Logging/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php 
-include file_get_contents(DOCS_RESOURCES."/logging-statements/introduction.php"); 
+include(DOCS_RESOURCES."/logging-statements/introduction.php"); 
 $getLogIntroText("");
 ?>
