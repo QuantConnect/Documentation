ef753eaa9de8fb0fecfbb75cf57baec8d14f87ba
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-01-06 16:46:53 -0700

    Update 03 Debugger.php

--- 01 Our Platform/05 Projects/06 Debugging/03 Debugger.php
+++ 01 Our Platform/05 Projects/06 Debugging/03 Debugger.php
@@ -1,6 +1,6 @@
 <?php 
 include(DOCS_RESOURCES."/projects/debugging/debugger.php");
 $link = "/docs/v2/our-platform/backtesting/debugging";
-$getDebuggerText($openProjectLink);
+$getDebuggerText($link);
 ?>
 
