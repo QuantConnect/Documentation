a2bda6c547b9cb0e0bc644e628d0354cc634d9bd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-01-12 15:21:33 -0700

    Update option-strategy-extra-args.php

--- Resources/trading-and-orders/option-strategy-extra-args.php
+++ Resources/trading-and-orders/option-strategy-extra-args.php
@@ -1,4 +1,4 @@
-<p>You can also provide a tag and <a href="/docs/v2/writing-algorithms/trading-and-orders/order-properties">order properties</a> to the 
+<p>Option strategies synchronously execute execute by default. To asynchronously execute Option strategies, set the <code>asynchronous</code> argument to <code class='python'>False</code><code class='csharp'>false</code>. You can also provide a tag and <a href="/docs/v2/writing-algorithms/trading-and-orders/order-properties">order properties</a> to the 
 <?php
 foreach ($methodNames as $i=>$methodName)
 {
@@ -23,14 +23,14 @@ else
 <pre class="csharp"><?php 
 foreach ($methodNames as $methodName) 
 {
-	echo "{$methodName}(optionStrategy, quantity, tag, orderProperties);
+	echo "{$methodName}(optionStrategy, quantity, asynchronous, tag, orderProperties);
 ";
 }
 ?></pre>
 <pre class="python"><?php 
 foreach ($methodNames as $methodName) 
 {
-	echo "self.{$methodName}(option_strategy, quantity, tag, order_properties)
+	echo "self.{$methodName}(option_strategy, quantity, asynchronous, tag, order_properties)
 ";
 }
 ?></pre>
