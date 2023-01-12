7f38b63f1d466af39f78e71e5a0c23476f2420ae
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-01-12 14:59:14 -0700

    Update 02 Implementation.php

--- 02 Writing Algorithms/22 Trading and Orders/07 Option Strategies/06 Call Butterfly/02 Implementation.php
+++ 02 Writing Algorithms/22 Trading and Orders/07 Option Strategies/06 Call Butterfly/02 Implementation.php
@@ -94,7 +94,7 @@ self.Sell(option_strategy, 1)   # if short call butterfly</pre>
     </div>
 
 <?php 
-$methodNames = list("Buy", "Sell");
+$methodNames = array("Buy", "Sell");
 include(DOCS_RESOURCES."/trading-and-orders/option-strategy-extra-args.php"); 
 ?>
 
