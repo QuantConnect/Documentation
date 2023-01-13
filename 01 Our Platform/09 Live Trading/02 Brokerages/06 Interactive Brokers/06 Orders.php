b1d17e34db2ff91265e0d898a00fbab063117489
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-01-12 17:58:56 -0700

    Update 06 Orders.php

--- 01 Our Platform/09 Live Trading/02 Brokerages/06 Interactive Brokers/06 Orders.php
+++ 01 Our Platform/09 Live Trading/02 Brokerages/06 Interactive Brokers/06 Orders.php
@@ -193,3 +193,106 @@ ticket.Update(update_fields)</pre>
 
 <h4>Handling Splits</h4>
 <p>In live trading, if you're using raw <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#11-Data-Normalization'>data normalization</a> and you have active limit, stop limit, or stop market orders in the market for a US Equity when a <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-actions#02-Splits'>stock split</a> occurs, the quantity, limit price, and stop price of your orders are automatically adjusted to reflect the stock split.</p>
+
+<h4>Order Size Limits</h4>
+
+<p>The following table shows the maximum number of units you can buy of each currency when the currency is the <a href='/docs/v2/writing-algorithms/securities/key-concepts#03-Quote-Currency'>base currency</a> in a currency pair:</p>
+
+<table class="table qc-table">
+    <thead>
+        <tr>
+            <th>Base Currency</th>
+            <th>Description</th>
+            <th>Maximum Order Size (Millions)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>USD</td>
+            <td>US Dollar</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>AUD</td>
+            <td>Australian Dollar</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>CAD</td>
+            <td>Canadian Dollar</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>CHF</td>
+            <td>Swiss Franc</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>CNH</td>
+            <td>China Renminbi (offshore)</td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>DKK</td>
+            <td>Danish Krone</td>
+            <td>35</td>
+        </tr>
+        <tr>
+            <td>EUR</td>
+            <td>Euro</td>
+            <td>5</td>
+        </tr>
+        <tr>
+            <td>GBP</td>
+            <td>British Pound Sterling</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>HKD</td>
+            <td>Hong Kong Dollar</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>KRW</td>
+            <td>Korean Won</td>
+            <td>750</td>
+        </tr>
+        <tr>
+            <td>JPY</td>
+            <td>Japanese Yen</td>
+            <td>550</td>
+        </tr>
+        <tr>
+            <td>MXN</td>
+            <td>Mexican Peso</td>
+            <td>70</td>
+        </tr>
+        <tr>
+            <td>NOK</td>
+            <td>Norwegian Krone</td>
+            <td>35</td>
+        </tr>
+        <tr>
+            <td>NZD</td>
+            <td>New Zealand Dollar</td>
+            <td>8</td>
+        </tr>
+        <tr>
+            <td>RUB</td>
+            <td>Russian Ruble</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>SEK</td>
+            <td>Swedish Krona</td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>SGD</td>
+            <td>Singapore Dollar</td>
+            <td>8</td>
+        </tr>
+    </tbody>
+</table>
+
+
