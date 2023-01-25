34fc83edadc7d944f0c5d7d5bbd9886f5dbea624
Web Editor <web.editor@quantconnect.com>
2023-01-25 12:11:14 -0500

    Automated push from server made by Jared Broad

--- 01 Cloud Platform/06 Projects/01 Getting Started/01 Introduction.php
+++ 01 Cloud Platform/06 Projects/01 Getting Started/01 Introduction.php
@@ -2,4 +2,4 @@
 	echo file_get_contents(DOCS_RESOURCES."/landing-page-introductions/projects.html");
 ?>
 
-<p>The Algorithm lab enables you to create, store, and manage your projects in the cloud. Only you can access your projects unless you share them or add collaborators.</p>
+<p>The Algorithm lab enables you to create, store, and manage your projects in the cloud. You can only access your own projects unless you share them with others, or add collaborators.</p>
