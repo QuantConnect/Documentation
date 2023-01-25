69edbad4094a9352bfe0ed46ddede71859d07769
Jared Broad <jaredbroad@gmail.com>
2023-01-25 17:19:53 -0500

    Log statements

--- Resources/logging-statements/introduction.php
+++ Resources/logging-statements/introduction.php
@@ -2,9 +2,9 @@
 $getLogIntroText = function($terminalLink)
 {
     echo "
-<p>Logging statements are statements you can add to your algorithm in order to add information to the log file, add information to the <a href='{$terminalLink}'>Cloud Terminal</a>, or even stop the algorithm. Use these statements to debug your backtests and live algorithms. Consider adding them in the code block of an <code>if</code> statement to signify an error has been caught.</p>
+<p>Algorithms can record string messages ("log statements") to a file for analysis after a backtest is complete, or as a live algorithm is running. These records can assist in debugging logical flow errors in the project code. Consider adding them in the code block of an <code>if</code> statement to signify an error has been caught.</p>
 
-<p>It's good practice to add logging statements to live algorithm so that you can understand its behavior and keep records to compare against backtest results. If you don't add logging statements to a live algorithm and the algorithm doesn't trade as you expect, it's difficult to evaluate the underlying problem.</p>
+<p>It's good practice to add logging statements to live algorithms so you can understand its behavior and keep records to compare against backtest results. If you don't add logging statements to a live algorithm and the algorithm doesn't trade as you expect, it's difficult to evaluate the underlying problem.</p>
     ";
 }
 ?>
