484ce9faef8f05a73a746af9ec8a2b81ef6ef3de
Gustavo Aviles <gustavo@quantconnect.com>
2023-01-25 17:38:49 -0800

    Add DOCS_URL and breadcrumbs to the docs.

---
+++ Resources/hooks/pre-hook.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Add if does not exist the a metod to get the docs url key form the breadcrumbs
+ */
+if (function_exists('DOCS_URL')) {
+
+    /**
+     * Example breadcrumb: ["cloud-platform","organizations","data-storage"]
+     *
+     * DOCS_URL() => "cloud-platform/organizations/data-storage"
+     * DOCS_URL(0) => "cloud-platform"
+     *
+     * @return mixed
+     */
+    function DOCS_URL($key = null)
+    {
+        if ($key === null) {
+            return join('/', $BREADCRUMBS);
+        }
+        return $BREADCRUMBS[$key];
+    }
+}
