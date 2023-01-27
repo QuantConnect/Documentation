d984a1bcc829adbc0d21c450a7b8e2cc9773cfe4
Web Editor <web.editor@quantconnect.com>
2023-01-27 10:30:02 -0500

    Automated push from server made by Jared Broad

--- 01 Cloud Platform/07 Research/02 Deployment/02 Resources.php
+++ 01 Cloud Platform/07 Research/02 Deployment/02 Resources.php
@@ -1,7 +1,7 @@
-<p>Research nodes enable you to use the Research Environment. Several models of research nodes are available. More powerful research nodes allow you to handle more data and run faster computations in your notebooks. The following table shows the specifications of the research node models: </p>
+<p>Research nodes enable you to spin up an interactive, command-line, Jupyter notebook environments. Several models of research nodes are available. More powerful research nodes allow you to handle more data and run faster computations in your notebooks. The following table shows the specifications of the research node models: </p>
 
 <?php include(DOCS_RESOURCES."/specs/research-nodes.html"); ?>
 
-<p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each research node model. You get one free R1-4 research node in your first organization, but the node is replaced when you <a href='/docs/v2/cloud-platform/organizations/resources#12-Add-Nodes'>subscribe to a new research node</a> in the organization.</p>
+<p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each research node model. You get one free R1-4 research node in your first organization, but the node is replaced when you <a href='/docs/v2/cloud-platform/organizations/resources#12-Add-Nodes'>subscribe to a paid research node</a> in the organization.</p>
 
 <p>You need an idle research node in your organization to launch the Research Environment. You can view all of your organization's nodes from the <a href='/docs/v2/cloud-platform/organizations/resources#11-View-All-Nodes'>Resources page</a> or the <a href='/docs/v2/cloud-platform/projects/ide#07-Manage-Nodes'>Resources panel in the IDE</a>. The Resources panel lets you select a specific research node to use.</p>
