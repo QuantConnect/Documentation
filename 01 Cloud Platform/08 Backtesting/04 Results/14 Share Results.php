c38effd42e68882e42fc541508703d95a1088497
Jared <jaredbroad@gmail.com>
2023-01-27 20:23:32 -0800

    Update 14 Share Results.php

--- 01 Cloud Platform/08 Backtesting/04 Results/14 Share Results.php
+++ 01 Cloud Platform/08 Backtesting/04 Results/14 Share Results.php
@@ -6,31 +6,6 @@
 
 <div class='csharp'><div class="qc-embed-frame csharp" style="display: inline-block; position: relative; width: 100%; min-height: 100px; min-width: 300px;"><div class="qc-embed-dummy" style="padding-top: 56.25%;"></div><div class="qc-embed-element" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"><iframe class="qc-embed-backtest" src="https://www.quantconnect.com/terminal/processCache?request=embedded_backtest_f13408735fdd403e5d772ec0cca92743.html" style="max-width: calc(100vw - 30px); max-height: 100vw; overflow: hidden;" scrolling="no" width="100%" height="100%"></iframe></div></div></div>
 
-<p>The following table describes the tabs of the embedded backtest result:</p>
-
-<table class="qc-table table" id="order-quotas-table">
-   <thead>
-      <tr>
-         <th style="width: 25%;">Tab</th>
-         <th style="width: 75%;">Content</th>
-      </tr>
-   </thead>
-   <tbody>
-      <tr>
-         <td><span class="tab-name">Charts</span></td>
-         <td>Some of the <a href="/docs/v2/cloud-platform/backtesting/results#04-Built-in-Charts">built-in</a> and <a href="/docs/v2/cloud-platform/backtesting/results#05-Custom-Charts">custom charts</a> that plot the backtest performance</td>
-      </tr>
-      <tr>
-         <td><span class="tab-name">Statistics</span></td>
-         <td>The <a href="/docs/v2/cloud-platform/backtesting/results#08-Key-Statistics">overall statistics</a> of the backtest</td>
-      </tr>
-      <tr>
-         <td><span class="tab-name">Code</span></td>
-         <td>The project files that produced the backtest</td>
-      </tr>
-   </tbody>
-</table>
-
 <p>To attach the embedded backtest result to a forum discussion, see <a href="/docs/v2/cloud-platform/community/forum#07-Create-Discussions">Create Discussions</a> or <a href="/docs/v2/cloud-platform/community/forum#08-Post-Comments">Post Comments</a>.</p>
 
 <p>After you've made your backtest results public, the results are always stored, anyone with the link can access the results, and the results can't be made private again because someone may have already cloned your project.</p>
