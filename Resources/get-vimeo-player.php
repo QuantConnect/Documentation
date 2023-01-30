ca1e277d8598d74b08af4df00e3caa43b54a8d99
Jared <jaredbroad@gmail.com>
2023-01-30 15:20:27 -0800

    Update get-vimeo-player.php

--- Resources/get-vimeo-player.php
+++ Resources/get-vimeo-player.php
@@ -1,9 +1,18 @@
 <div class='sub-heading-content' data-bind='html: subheading.content'>
-<style>
+    <style>
     .videoWrapper {
-        position: relative;
+      position: relative;
+      padding-bottom: 56.25%; /* 16:9 */
+      height: 0;
     }
-</style>
+    .videoWrapper iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    </style>
     <div class='videoWrapper'>
         <iframe src='https://player.vimeo.com/video/<?=$vimeoCode?>' width='640' height='360' frameborder='0' allow='autoplay; fullscreen' allowfullscreen=''></iframe>
     </div>
