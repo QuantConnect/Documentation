9f858d309ef0b9a9f458e32e3b2ff224d33a3004
Web Editor <web.editor@quantconnect.com>
2023-01-30 18:22:32 -0500

    Automated push from server made by Jared Broad

--- 03 Writing Algorithms/03 Securities/04 Requesting Data/06 Extended Market Hours.php
+++ 03 Writing Algorithms/03 Securities/04 Requesting Data/06 Extended Market Hours.php
@@ -1,7 +1,6 @@
 <?php 
-include(DOCS_RESOURCES."/securities/extended-market-hours.php"); 
 $cCode = "AddEquity(\"SPY\", extendedMarketHours: true);";
 $pyCode = "self.AddEquity(\"SPY\", extendedMarketHours=True)";
 $supportedIntradayData = true;
-$getExtMarketHoursText($cCode, $pyCode, $supportedIntradayData);
+include(DOCS_RESOURCES."/securities/extended-market-hours.php"); 
 ?>
