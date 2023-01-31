fd6fb523e3081a1314045ce4149813d6e0086532
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 22:43:26 +0800

    refactor cli install introduction php (#982)
    * refactor cli install introduction php
    
    * refactor
    
    * Update and rename 01 Introduction.php to 01 Introduction.html
    
    * Delete introduction.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 05 Lean CLI/02 Installation/03 Installing Lean CLI/01 Introduction.php
+++
@@ -1,6 +0,0 @@
-<?php
-include(DOCS_RESOURCES."/cli/install/cli/introduction.php");
-$pipLink = "/docs/v2/lean-cli/installation/installing-pip";
-$dockerLink = "/docs/v2/lean-cli/installation/installing-docker";
-$getIntroText($pipLink, $dockerLink);
-?>
