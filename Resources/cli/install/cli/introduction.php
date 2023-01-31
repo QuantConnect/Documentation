094036cc930379131ae8949dc51db4dfe92fb2a3
LouisSzeto <hke0073@icloud.com>
2023-01-31 20:04:07 +0800

    refactor cli install introduction php

--- Resources/cli/install/cli/introduction.php
+++ Resources/cli/install/cli/introduction.php
@@ -1,15 +1,7 @@
-<?php
-$getIntroText = function($pipLink, $dockerLink)
-{
-	echo "
 <p>
-    The Lean CLI is distributed as a Python package, so it requires <code>pip</code> to be installed. To learn how to install <code>pip</code> on your operating system, see <a href='{$pipLink}'>Installing pip</a>.
+    The Lean CLI is distributed as a Python package, so it requires <code>pip</code> to be installed. To learn how to install <code>pip</code> on your operating system, see <a href='<?=$pipLink?>'>Installing pip</a>.
 </p>
 
 <p>
-    The commands which run the LEAN engine locally also depend on <a href='https://www.docker.com/'' target='_blank'>Docker</a> being installed and running. See <a href='{$dockerLink}'>Installing Docker</a> to learn how to install Docker on your operating system.
+    The commands which run the LEAN engine locally also depend on <a href='https://www.docker.com/' target='_blank'>Docker</a> being installed and running. See <a href='<?=$dockerLink?>'>Installing Docker</a> to learn how to install Docker on your operating system.
 </p>
-	";
-}
-?>
-
