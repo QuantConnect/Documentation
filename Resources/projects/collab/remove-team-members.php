4c1145769733daf41820460ff8eb60df3e0f38cb
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 19:06:21 +0000

    Refactors Projects remove-team-members (#1023)
    * Refactors Projects remove-team-members
    
    * Update remove-team-members.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- Resources/projects/collab/remove-team-members.php
+++ Resources/projects/collab/remove-team-members.php
@@ -1,27 +1,12 @@
-<?php
-$getRemoveMemberText = function($openProjectLink, $deleteProjectLink, $isLocal)
-{
-
-    echo "
 <p>Follow these steps to remove a team member from a project you own:</p>
 <ol>
-    <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    ";
+    <li><a href='<?=$localPlatform ? "/docs/v2/local-platform/projects/getting-started#04-Open-Projects" : "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects" ?>'>Open the project</a>.</li>
+
+    <? if ($localPlatform) { ?> <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
     
-    if ($isLocal)
-    {
-        echo "
-    <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li>
-        ";
-    }
-    
-    echo "
     <li>In the Collaborate section of the Project panel, click the profile image of the team member.</li>
     <li>Click <span class='button-name'>Remove User</span>.</li>
 </ol>
+</p>
 
-</p>To remove yourself as a collaborator from a project you don't own, <a href='{$deleteProjectLink}'>delete the project</a>.</p>
-
-    ";
-}
-?>
+<p>To remove yourself as a collaborator from a project you don't own, <a href='/docs/v2<?=$localPlatform ? "/local-platform/projects/getting-started#12-Delete-Projects" : "/cloud-platform/projects/getting-started#11-Delete-Projects" ?>'>delete the project</a>.</p>
