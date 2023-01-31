53fcb0fa6386fcb4245a80862184e653d7c4d2f0
derekmelchin <melchin.derek@gmail.com>
2023-01-31 11:19:40 -0700

    Fix global variable

--- Resources/projects/lean-engine-branches/change-branches.php
+++ Resources/projects/lean-engine-branches/change-branches.php
@@ -1,7 +1,7 @@
 <p>Follow these steps to change the LEAN engine branch that runs your backtests and live trading algorithms:</p>
 <ol>
     <li><a href='<?=$openProjectLink?>'>Open a project</a>.</li>
-    <?php if (!$isCloud) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><?php } ?>
+    <?php if (!$cloudPlatform) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><?php } ?>
     <li>In the Project panel, click the <span class='field-name'>LEAN Engine</span> field and then click a branch from the drop-down menu.</li>
     <li><span class='qualifier'>(Optional)</span> Click <span class='button-name'>About Version</span> to display the branch description.</li>
     <li>If you want to always use the master branch, select the <span class='button-name'>Always use Master Branch</span> check box.</li>
