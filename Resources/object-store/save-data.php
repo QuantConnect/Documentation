6404155d130c962aecad6922c2c420158e21b1fb
Jared <jaredbroad@gmail.com>
2023-01-31 09:13:16 -0800

    Update save-data.php

--- Resources/object-store/save-data.php
+++ Resources/object-store/save-data.php
@@ -1,17 +1,15 @@
 <?
-$keyText = $writingAlgorithms ? "To store data, you need to provide a key. If you provide a key that is already in the Object Store, it will overwrite the data at that location. To avoid overwriting objects from other projects in your organization, prefix the key with your project ID. You can find the project ID in the URL of your browser when you open a project. For example, the ID of the project at <span class='public-file-name'>quantconnect.com/project/12345</span> is 12345." : "";
 $cSharpPrefix = $research ? "qb." : "";
 $pythonPrefix = $research ? "qb." : "self.";
 ?>
 
-<p>The Object Store saves objects under a key-value system. If you save objects in backtests, you can access them from the Research Environment. <?=$writingAlgorithmsText?></p>
+<p>The Object Store saves objects under a key-value system. If you save objects in backtests, you can access them from the Research Environment. <?=$writingAlgorithms ? "To avoid slowing down your backtests, save data once in the <code>OnEndOfAlgorithm</code> event handler. In live trading, you can save data more frequently like at the end of a <code>Train</code> method or after universe selection." : "" ?></p>
 
 <p>If you run algorithms in QuantConnect Cloud, you need <a href='/docs/v2/cloud-platform/organizations/members#08-Permissions'>storage create permissions</a> to save data in the Object Store.</p>
 
 <p>If you don't have data to store, <a href='<?=$writingAlgorithms ? "/docs/v2/writing-algorithms/object-store#03-Create-Sample-Data" : "/docs/v2/research-environment/object-store#03-Create-Sample-Data" ?>'>create some sample data</a>.</p>
 
 <p class='csharp'>You can save the following types of objects in the Object Store:</p>
-
 <ul class='csharp'>
     <li><code>Bytes</code> objects</li>
     <li><code>string</code> objects</li>
@@ -19,9 +17,10 @@ $pythonPrefix = $research ? "qb." : "self.";
     <li>XML-formatted objects</li>
 </ul>
 
-<p class='csharp'><?=$keyText?></p>
-
-<p class='python'>You can save <code>Bytes</code> and <code>string</code> objects in the Object Store. <?=$keyText?></p>
+<p>
+<span class='python'>You can save <code>Bytes</code> and <code>string</code> objects in the Object Store.</span>
+<?=$writingAlgorithms ? "To store data, you need to provide a key. If you provide a key that is already in the Object Store, it will overwrite the data at that location. To avoid overwriting objects from other projects in your organization, prefix the key with your project ID. You can find the project ID in the URL of your browser when you open a project. For example, the ID of the project at <span class='public-file-name'>quantconnect.com/project/12345</span> is 12345." : ""?>
+</p>
 
 <h4>Bytes</h4>
 <p>To save a <code>Bytes</code> object, call the <code>SaveBytes</code> method.</p>
