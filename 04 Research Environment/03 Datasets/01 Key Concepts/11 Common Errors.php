29bb68aa6c84d263249f8f1327f923568515cacb
Jared Broad <jaredbroad@gmail.com>
2023-01-31 01:31:07 -0500

    Common errors

--- 04 Research Environment/03 Datasets/01 Key Concepts/11 Common Errors.php
+++ 04 Research Environment/03 Datasets/01 Key Concepts/11 Common Errors.php
@@ -1,5 +1,3 @@
-<?php
+<?
 include(DOCS_RESOURCES."/datasets/common-errors.php");
-$isWritingAlgorithms = false;
-$getCommonErrorsText($isWritingAlgorithms);
 ?>
--- Resources/datasets/common-errors.php
+++ Resources/datasets/common-errors.php
@@ -1,44 +1,26 @@
-<?php
-
-$getCommonErrorsText = function($isWritingAlgorithms)
-{
-    $pyVarName = $isWritingAlgorithms ? "self": "qb";
-    $pyFuncVariables = $isWritingAlgorithms ? "self, " : "";
-    $pyPrintFunction = $isWritingAlgorithms ? "self.Log" : "print";
-    $envName = $isWritingAlgorithms ? "algorithms" : "the Research Environment";
-    
-    if ($isWritingAlgorithms)
-    {
-        echo "
+<? if ($writingAlgorithms) { ?>
 <p>Errors can occur when you request historical data.</p>    
 <h4>Empty Data Errors</h4>      
-";
-    }
-    
-    echo "
+<? } ?> 
+
 <p class='python'>If the history request returns an empty DataFrame and you try to slice it, it throws an exception. To avoid issues, check if the DataFrame contains data before slicing it.</p>
 <div class='python section-example-container'>
-<pre class='python'>df = {$pyVarName}.History(symbol, 10).close    # raises exception if the request is empty
+<pre class='python'>df = <?=$writingAlgorithms ? "self": "qb"?>.History(symbol, 10).close    # raises exception if the request is empty
 
-def GetSafeHistoryCloses({$pyFuncVariables}symbols):
+def GetSafeHistoryCloses(<?=$writingAlgorithms ? "self, " : "" ?>symbols):
     if not symbols:
-        {$pyPrintFunction}(f'No symbols')
+        <?=$writingAlgorithms ? "self.Log" : "print"?>(f'No symbols')
         return  False, None
-    df = {$pyVarName}.History(symbols, 100, Resolution.Daily)
+    df = <?=$writingAlgorithms ? "self": "qb"?>.History(symbols, 100, Resolution.Daily)
     if df.empty:
-        {$pyPrintFunction}(f'Empy history for {symbols}')
+        <?=$writingAlgorithms ? "self.Log" : "print"?>(f'Empy history for {symbols}')
         return  False, None
      return True, df.close.unstack(0)</pre>
 </div>
 
-<p>If you run {$envName} on your local machine and history requests return no data, check if your <span class='public-file-name'>data</span> directory contains the data you request. To download datasets, see <a href='/docs/v2/cloud-platform/datasets/licensing#04-Download'>Download</a>.</p>
-";
-    
-    if ($isWritingAlgorithms)
-    {
-        echo "<h4>Numerical Precision Errors</h4>";
-        echo file_get_contents(DOCS_RESOURCES."/datasets/equity-common-errors.html");
-    }
-}
+<p>If you run <?=$writingAlgorithms ? "algorithms" : "the Research Environment" ?> on your local machine and history requests return no data, check if your <span class='public-file-name'>data</span> directory contains the data you request. To download datasets, see <a href='/docs/v2/cloud-platform/datasets/licensing#04-Download'>Download</a>.</p>
 
-?>
+<? if ($writingAlgorithms) { ?>
+<h4>Numerical Precision Errors</h4>
+<? file_get_contents(DOCS_RESOURCES."/datasets/equity-common-errors.html"); ?>
+<? } ?>
