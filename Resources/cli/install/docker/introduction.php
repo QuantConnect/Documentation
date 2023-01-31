7edac1a254ef1acfcaaff229bfa9d3c0f0604d34
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 22:47:01 +0800

    refactor cli docker introduction php (#984)
    * refactor cli docker introduction php
    
    * Update introduction.php
    
    * Update 01 Introduction.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

---
+++ Resources/cli/install/docker/introduction.php
@@ -0,0 +1,10 @@
+<p>
+    If you run the LEAN engine locally with <?=$leanCli ? "the CLI" : "QuantConnect Local Platform"?>, LEAN executes in a Docker container.
+    These Docker containers contain a minimal Linux-based operating system, the LEAN engine, and all the packages available to you on QuantConnect.com.
+    It is therefore required to install Docker if you plan on using <?=$leanCli ? "the CLI" : "QuantConnect Local Platform"?> to run the LEAN engine locally.
+</p>
+
+<p>
+    The installation instructions on this page should be sufficient for most users.
+    For more comprehensive instructions, see <a href="https://docs.docker.com/get-docker/" target="_blank">Get Docker</a> in the Docker documentation.
+</p>
