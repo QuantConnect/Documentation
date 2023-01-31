7985f7b031975b4b63c65cdb2fd1ae381330be66
Jared Broad <jaredbroad@gmail.com>
2023-01-30 22:38:11 -0500

    Inspect variables

--- 01 Cloud Platform/08 Backtesting/05 Debugging/05 Inspect Variables.php
+++ 01 Cloud Platform/08 Backtesting/05 Debugging/05 Inspect Variables.php
@@ -1,4 +1,3 @@
-<?php
+<?
 include(DOCS_RESOURCES."/debugging/inspect-variables.php");
-$getInspectVariablesText(true);
 ?>
--- Resources/debugging/inspect-variables.php
+++ Resources/debugging/inspect-variables.php
@@ -1,31 +1,23 @@
-<?php
-$getInspectVariablesText = function($isBacktest)
-{
+<?
+    $isBacktest = DOCS_URL(0) == "backtesting";
     $location = $isBacktest ? "algorithm" : "notebook" ;
-    echo "
-<p>After you launch the debugger, you can inspect the state of your {$location} as it executes each line of code. You can inspect local variables or custom expressions. <span class='python'>The values of variables in your {$location} are formatted in the IDE to improve readability. For example, if you inspect a variable that references a DataFrame, the debugger represents the variable value as the following:</span></p>
+?>
+
+<p>After you launch the debugger, you can inspect the state of your <?=$location?> as it executes each line of code. You can inspect local variables or custom expressions. <span class='python'>The values of variables in your <?=$location?> are formatted in the IDE to improve readability. For example, if you inspect a variable that references a DataFrame, the debugger represents the variable value as the following:</span></p>
 
 <img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/debugger-dataframe.png'>
 
 <h4>Local Variables</h4>
-<p>The <span class='page-section-name'>Variables</span> section of the Run and Debug panel shows the local variables at the current breakpoint. If a variable in the panel is an object, click it to see its members. The panel updates as the {$location} runs.</p>
-    ";
+<p>The <span class='page-section-name'>Variables</span> section of the Run and Debug panel shows the local variables at the current breakpoint. If a variable in the panel is an object, click it to see its members. The panel updates as the <?=$location?> runs.</p>
+
     
-    if ($isBacktest)
-    {
-        echo "
+<? if ($isBacktest) { ?>
 <img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/inspect-local-variables.gif'>
 <img class='csharp docs-image' src='https://cdn.quantconnect.com/i/tu/navigate-variables-debugger-c-sharp.gif'>
-        ";        
-    }
-    else
-    {
-        echo "
-<img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/research-environment-local-variables-py.gif   '>
-        ";
-    }
+<? } else { ?>
+<img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/research-environment-local-variables-py.gif'>
+<? } ?>
 
-    echo "
 <p>Follow these steps to update the value of a variable:</p>
 <ol>
     <li>In the Run and Debug panel, right-click a variable and then click <span class='menu-name'>Set Value</span>.</li>
@@ -33,31 +25,18 @@ $getInspectVariablesText = function($isBacktest)
 </ol>
 
 <h4>Custom Expressions</h4>
-    ";
     
-    if ($isBacktest)
-    {
-        echo "
+<? if ($isBacktest) { ?>
 <p>The <span class='page-section-name'>Watch</span> section of the Run and Debug panel shows any custom expressions you add. For example, you can add an expression to show the current date in the backtest.</p>
 <img class='csharp docs-image' src='https://cdn.quantconnect.com/i/tu/watch-variables-c-sharp.png'>
 <img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/watch-variables.png'>
-        ";        
-    }
-    else
-    {
-        echo "
+<? } else { ?>
 <p>The <span class='page-section-name'>Watch</span> section of the Run and Debug panel shows any custom expressions you add. For example, you can add an expression to show a <code>datetime</code> object.</p>
 <img class='docs-image' src='https://cdn.quantconnect.com/i/tu/research-env-custom-expressions-debug.jpg'>
-        ";
-    }
-    
-    echo "
-    
+<? } ?>
+
 <p>Follow these steps to add a custom expression:</p>
 <ol>
     <li>Hover over the <span class='page-section-name'>Watch</span> section and then click the <span class='icon-name'>plus</span> icon that appears.</li>
     <li>Enter an expression and then press <span class='key-combinations'>Enter</span>.</li>
-</ol>    
-    ";
-}
-?>
+</ol>
