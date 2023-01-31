c3ce36f5fa0a3bc73f7061647f662dd30f78349e
Jared <jaredbroad@gmail.com>
2023-01-31 11:05:58 -0800

    Update remove-team-members.php

--- Resources/projects/collab/remove-team-members.php
+++ Resources/projects/collab/remove-team-members.php
@@ -1,11 +1,12 @@
 <p>Follow these steps to remove a team member from a project you own:</p>
 <ol>
-    <li><a href='/docs/v2<?=$localPlatform ? "/local-platform/projects/getting-started#04-Open-Projects" : "/cloud-platform/projects/getting-started#02-View-All-Projects" ?>'>Open the project</a>.</li>
+    <li><a href='<?=$localPlatform ? "/docs/v2/local-platform/projects/getting-started#04-Open-Projects" : "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects" ?>'>Open the project</a>.</li>
 
     <? if ($localPlatform) { ?> <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
     
     <li>In the Collaborate section of the Project panel, click the profile image of the team member.</li>
     <li>Click <span class='button-name'>Remove User</span>.</li>
 </ol>
+</p>
 
-</p>To remove yourself as a collaborator from a project you don't own, <a href='/docs/v2<?=$localPlatform ? "/local-platform/projects/getting-started#12-Delete-Projects" : "/cloud-platform/projects/getting-started#11-Delete-Projects" ?>'>delete the project</a>.</p>
+<p>To remove yourself as a collaborator from a project you don't own, <a href='/docs/v2<?=$localPlatform ? "/local-platform/projects/getting-started#12-Delete-Projects" : "/cloud-platform/projects/getting-started#11-Delete-Projects" ?>'>delete the project</a>.</p>
