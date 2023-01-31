2af162c01bb548630ce563ac1654efc0853212e7
Jared <jaredbroad@gmail.com>
2023-01-31 12:12:47 -0800

    Update introduction.php

--- Resources/projects/ide/introduction.php
+++ Resources/projects/ide/introduction.php
@@ -1,2 +1,4 @@
-<p>The <?=$cloudPlatform ? "web" : "VS Code"?> Integrated Development Environment (IDE) lets you work on research notebooks and develop algorithms for backtesting and live trading. When you <a href='<?=$openProjectLink?>'>open a project</a>, the IDE automatically displays. <?php if (!$cloudPlatform) {?>The web IDE enables you to access your trading algorithms from anywhere in the world with just an internet connection and a browser. <?php } ?>If you prefer to use a different IDE, the <a href='/docs/v2/lean-cli'>CLI</a> allows you to develop locally in your preferred IDE.</p>
+<p>
+  The <?=$cloudPlatform ? "web" : "VS Code"?> Integrated Development Environment (IDE) lets you work on research notebooks and develop algorithms for backtesting and live trading. When you <a href='<?=$openProjectLink?>'>open a project</a>, the IDE automatically displays.  You can access your trading algorithms from anywhere in the world with just an internet connection and a browser. <?=$localPlatformOrCli ? "" : "If you prefer to use a different IDE, the <a href='/docs/v2/lean-cli'>CLI</a> allows you to develop locally in your preferred IDE." ?>
+</p>
 
