9db6e6defa0d293aefd682febe664a601467cc59
LouisSzeto <hke0073@icloud.com>
2023-01-31 20:14:57 +0800

    refactor cli docker introduction php

--- 05 Lean CLI/02 Installation/02 Installing Docker/01 Introduction.php
+++ 05 Lean CLI/02 Installation/02 Installing Docker/01 Introduction.php
@@ -1,7 +1,4 @@
-<?php
-include(DOCS_RESOURCES."/cli/install/docker/introduction-1.php");
+<?
 $isCLIDocs = true;
-$getIntroText($isCLIDocs);
-
-echo file_get_contents(DOCS_RESOURCES."/cli/install/docker/introduction-2.html");
+include(DOCS_RESOURCES."/cli/install/docker/introduction.php");
 ?>
