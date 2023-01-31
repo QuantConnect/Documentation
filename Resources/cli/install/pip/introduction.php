2b6c06b723cd8760f0fccd036980fc36891e1298
Jared <jaredbroad@gmail.com>
2023-01-31 07:00:17 -0800

    Update introduction.php

--- Resources/cli/install/pip/introduction.php
+++ Resources/cli/install/pip/introduction.php
@@ -1,20 +1,17 @@
-<? if ($isCLIDocs) { ?>
-    <p>
-        The Lean CLI is distributed as a Python package, so it requires <code>pip</code> to be installed.
-        Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the CLI.
-    </p>
+<? if ($leanCli) { ?>
+<p> 
+    The Lean CLI is distributed as a Python package, so it requires <code>pip</code> to be installed. Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the CLI.
+</p>
 <? } else { ?>
-    <p>
-        QuantConnect Local requires <code>pip</code> to be installed.
-        Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the QuantConnect Local extension.
-    </p>
+<p> 
+    QuantConnect Local Platform is better with <code>pip</code> installed. Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the QuantConnect Local Platform. The pip package manager is used to install the quantconnect-stubs package, which provides local autocomplete on the QuantConnect QCAlgorithm API.
+</p>
 <? } ?>
 
 <p>
-    This page contains installation instructions for <a href="https://docs.anaconda.com/anaconda/" target="_blank">Anaconda</a>, which is a Python distribution containing a lot of packages that are also available when running the LEAN engine.
-    Having these packages installed locally makes it possible for your editor to provide autocomplete for them.
+    This page contains installation instructions for <a href="https://docs.anaconda.com/anaconda/" target="_blank">Anaconda</a>, which is a Python distribution containing a lot of packages that are also available when running the LEAN engine. Having these packages installed locally makes it possible for your editor to provide autocomplete for them.
 </p>
 
 <p>
-    Note that the Python distribution from the Microsoft Store is not supported. <a href='https://github.com/QuantConnect/lean-cli/issues/54#issuecomment-1104148972' rel='nofollow' target='nofollow'>Docker doesn't support Python 3.10</a>, so you must have 3.9.x or lower. 
+    The Python distribution from the Microsoft Store is not supported. <a href='https://github.com/QuantConnect/lean-cli/issues/54#issuecomment-1104148972' rel='nofollow' target='nofollow'>Docker doesn't support Python 3.10</a>, so you must have 3.9.x or lower. 
 </p>
