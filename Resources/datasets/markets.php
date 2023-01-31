93be620fcd87e95e43d77da25eb8af250fe7ae35
Jared Broad <jaredbroad@gmail.com>
2023-01-30 23:54:29 -0500

    Escape strings

--- Resources/datasets/markets.php
+++ Resources/datasets/markets.php
@@ -4,6 +4,6 @@
 <p>LEAN can usually determine the correct market based on the ticker you provide when you create the security subscription. To manually set the market for a security, pass a <code>market</code> argument when you create the security subscription.</p>
 
 <div class='section-example-container'>
-    <pre class='csharp'><?=$writingAlgorithms ? "" : "qb." ?>AddEquity(\"SPY\", market: Market.USA);</pre>
-    <pre class='python'><?=$writingAlgorithms ? "self" : "qb" ?>.AddEquity(\"SPY\", market=Market.USA)</pre>
+    <pre class='csharp'><?=$writingAlgorithms ? "" : "qb." ?>AddEquity("SPY", market: Market.USA);</pre>
+    <pre class='python'><?=$writingAlgorithms ? "self" : "qb" ?>.AddEquity("SPY", market=Market.USA)</pre>
 </div>
