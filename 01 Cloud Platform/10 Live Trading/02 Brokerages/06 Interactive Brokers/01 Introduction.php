31373a75fe6ab72b550ef56ec316ef6824df3e94
oussamanahdi <79997186+oussamanahdi@users.noreply.github.com>
2023-01-31 18:48:44 +0100

    converted interactive-brokers

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
@@ -1,4 +1,4 @@
-<?php 
+<?
 include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); 
 echo file_get_contents(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.php");
 ?>
