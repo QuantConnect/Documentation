18e066f7e4edd3907ae104324db2a652d3e4716b
oussamanahdi <79997186+oussamanahdi@users.noreply.github.com>
2023-01-31 21:26:53 +0100

    changed to hmtl and updated references

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/01 Introduction.php
@@ -1,6 +1,6 @@
 <?
 include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); 
-include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.php");
+include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.html");
 ?>
 
 <p>To view the implementation of the IB brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.InteractiveBrokers' rel='nofollow' target="_blank">Lean.Brokerages.InteractiveBrokers repository</a>.</p>
