fba345bf7950c5cbe742c279b1611b9aeea0d2a5
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 17:14:08 +0000

    Refactors ObjectStore save-data (#1002)
    * Refactors ObjectStore save-data
    
    * Use Global `$research` and `$writingAlgorithms`
    
    * Update save-data.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 03 Writing Algorithms/29 Object Store/05 Save Data.php
+++ 03 Writing Algorithms/29 Object Store/05 Save Data.php
@@ -1,7 +1,1 @@
-<?php
-include(DOCS_RESOURCES."/object-store/save-data.php");
-$cSharpPrefix = "";
-$pythonPrefix = "self.";
-$sampleDataLink = "/docs/v2/writing-algorithms/object-store#03-Create-Sample-Data";
-$getSaveDataText($cSharpPrefix, $pythonPrefix, $sampleDataLink);
-?>
+<? include(DOCS_RESOURCES."/object-store/save-data.php"); ?>
--- 04 Research Environment/07 Object Store/05 Save Data.php
+++ 04 Research Environment/07 Object Store/05 Save Data.php
@@ -1,8 +1,1 @@
-<?php
-include(DOCS_RESOURCES."/object-store/save-data.php");
-$cSharpPrefix = "qb.";
-$pythonPrefix = "qb.";
-$sampleDataLink = "/docs/v2/research-environment/object-store#03-Create-Sample-Data";
-$writingAlgorithmsText = "";
-$getSaveDataText($cSharpPrefix, $pythonPrefix, $sampleDataLink, $writingAlgorithmsText);
-?>
+<? include(DOCS_RESOURCES."/object-store/save-data.php"); ?>
