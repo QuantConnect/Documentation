945711929b2900ebd7526b83d385de2d6cb0628b
Jared <jaredbroad@gmail.com>
2023-01-31 06:39:47 -0800

    Update and rename 02 Install.php to 02 Install.html

--- 05 Lean CLI/02 Installation/03 Installing Lean CLI/02 Install.php
+++
@@ -1,5 +0,0 @@
-<?
-$pipLink = "/docs/v2/lean-cli/installation/installing-pip";
-$dockerLink = "/docs/v2/lean-cli/installation/installing-docker";
-include(DOCS_RESOURCES."/cli/install/cli/install.php");
-?>
