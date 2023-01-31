5453b3f8708dc1c6439d9ec5fedf894e9362d8e6
Jared <jaredbroad@gmail.com>
2023-01-31 11:33:47 -0800

    Update stop-algorithm.php

--- Resources/trading-and-orders/stop-algorithm.php
+++ Resources/trading-and-orders/stop-algorithm.php
@@ -1,19 +1,26 @@
-<p><? if ($cloudPlatform) { ?>The live trading results page has a <span class='button-name'>Stop</span> button to immediately stop your algorithm from executing. <? } ?><? if ($leanCli) { ?>The <code>lean live stop</code> command immediately stops your algorithm from executing. <? } ?><? if($leanCliCloud) { ?>The <code>lean cloud live stop</code> command immediately stops your algorithm from executing. <? } ?>When you stop a live algorithm, your portfolio holdings are retained. Stop your algorithm if you want to perform any of the following actions:</p>
+<p>
+<? if ($cloudPlatform) { ?>The live trading results page has a <span class='button-name'>Stop</span> button to immediately stop your algorithm from executing. <? } ?>
+<? if ($leanCliCloud) { ?>The <code>lean cloud live stop</code> command immediately stops your algorithm from executing. 
+<? } elseif ($leanCli) { ?>The <code>lean live stop</code> command immediately stops your algorithm from executing. <? } ?>
+When you stop a live algorithm, your portfolio holdings are retained. Stop your algorithm if you want to perform any of the following actions:
+</p>
 
-<ul><? if ($cloudPlatform) { ?>
+<ul>
+<? if ($cloudPlatform) { ?>
         <li>Update your project's code files</li>
         <li>Upgrade the <a href='/docs/v2/cloud-platform/organizations/resources#04-Live-Trading-Nodes'>live trading node</a></li>
         <li>Update the settings you entered into the deployment wizard</li>
-        <li>Place manual orders through your brokerage account instead of the web IDE</li><? } ?>
-    <? if ($leanCli) { ?>
+        <li>Place manual orders through your brokerage account instead of the web IDE</li>
+<? } ?>
+
+<? if ($leanCliCloud) { ?>
         <li>Update your project's code files</li>
         <li>Upgrade the <a href='/docs/v2/cloud-platform/organizations/resources#04-Live-Trading-Nodes'>live trading node</a></li>
         <li>Update the settings you entered into the deployment command</li>
-    <? } ?>
-    <? if ($leanCliCloud) { ?>
+<? } elseif ($leanCli) { ?>
         <li>Update your project's code files</li>
         <li>Update the settings you entered into the deployment command</li>
-    <? } ?>
+<? } ?>
 </ul>
 
 <p>Furthermore, if you receive new securities in your portfolio because of a reverse merger, you also need to stop and redeploy the algorithm.</p>    
