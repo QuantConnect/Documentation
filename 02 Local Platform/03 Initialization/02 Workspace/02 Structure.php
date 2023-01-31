7f31b2fc7e5c71344979b0fec8069967e4f5a4f3
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 22:32:16 +0800

    refactor php for cli init structure (#980)
    * cli init
    
    * Update structure.php
    
    * Update 03 Directory Structure.php
    
    * Update 02 Structure.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 02 Local Platform/03 Initialization/02 Workspace/02 Structure.php
+++ 02 Local Platform/03 Initialization/02 Workspace/02 Structure.php
@@ -2,10 +2,6 @@
     The workspace directory initially has following structure:
 </p>
 
-<?php
-include(DOCS_RESOURCES."/cli/init/structure.php"); 
-$isCLIDocs = false;
-$getStructureText($isCLIDocs);
-?>
+<? include(DOCS_RESOURCES."/cli/init/structure.php"); ?>
 
 <p>As you add <a href='/docs/v2/local-platform/projects/getting-started'>projects</a>, the project files are added to your workspace directory. If you create and use <a href='/docs/v2/local-platform/projects/shared-libraries'>shared libraries</a> in your projects, the library files are added to a <span class='public-directory-name'>Library</span> directory in your workspace.</p> 
--- 05 Lean CLI/03 Initialization/01 Workspace/03 Directory Structure.php
+++ 05 Lean CLI/03 Initialization/01 Workspace/03 Directory Structure.php
@@ -2,10 +2,6 @@
     The <code>lean init</code> commands creates the following structure:
 </p>
 
-<?php
-include(DOCS_RESOURCES."/cli/init/structure.php"); 
-$isCLIDocs = true;
-$getStructureText($isCLIDocs);
-?>
+<? include(DOCS_RESOURCES."/cli/init/structure.php"); ?>
 
 <p>When you create new projects or pull existing projects from the cloud, your workspace stores the project files.</p>
