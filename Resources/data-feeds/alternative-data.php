2d29979c48715525264dc9787e189accbec8c196
Jared <jaredbroad@gmail.com>
2023-01-31 07:04:25 -0800

    Update alternative-data.php

--- Resources/data-feeds/alternative-data.php
+++ Resources/data-feeds/alternative-data.php
@@ -1,4 +1,1 @@
-<p>Brokerage data feeds support most alternative data feeds, except feeds that stream real-time intraday data. Streaming data feeds, like the <a href='/datasets/tiingo-news-feed'>Tiingo News Feed</a> and <a href='/datasets/benzinga-news-feed'>Benzinga News Feed</a>, require the QuantConnect data feed. 
-<? if ($hybridSupported) { ?> 
-    The hybrid QuantConnect-<?=$brokerageName?> data feed supports streaming data feeds.
-<? } ?></p>
+<p>Brokerage data feeds support most alternative data feeds, except feeds that stream real-time intraday data. Streaming data feeds, like the <a href='/datasets/tiingo-news-feed'>Tiingo News Feed</a> and <a href='/datasets/benzinga-news-feed'>Benzinga News Feed</a>, require the QuantConnect data feed. <?=$hybridSupported ? "The hybrid QuantConnect-{$brokerageName} data feed supports streaming data feeds." : "" ?></p>
