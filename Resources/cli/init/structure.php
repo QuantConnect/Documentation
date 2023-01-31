08d281564ab86ab4fbc270d554e276f64229918d
Jared <jaredbroad@gmail.com>
2023-01-31 06:27:42 -0800

    Update structure.php

--- Resources/cli/init/structure.php
+++ Resources/cli/init/structure.php
@@ -31,12 +31,12 @@
     <tbody>
         <tr>
             <td><span class='public-directory-name'>data /</span></td>
-            <td>This directory contains the <?=$isCLIDocs ? "<a href='/docs/v2/lean-cli/datasets/local-data'>local data</a>" : "local data"?> that is used when running the LEAN engine locally. This directory is filled with <a rel='nofollow' href='https://github.com/QuantConnect/Lean/tree/master/Data' target='_blank'>sample data from the QuantConnect/Lean repository</a>.</td>
+            <td>This directory contains the <?=$leanCli ? "<a href='/docs/v2/lean-cli/datasets/local-data'>local data</a>" : "local data"?> that is used when running the LEAN engine locally. This directory is filled with <a rel='nofollow' href='https://github.com/QuantConnect/Lean/tree/master/Data' target='_blank'>sample data from the QuantConnect/Lean repository</a>.</td>
         </tr>
     
         <tr>
             <td><span class='public-file-name'>lean.json</span></td>
-            <td>This file contains the <?=$isCLIDocs ? "<a href='/docs/v2/lean-cli/initialization/configuration#03-Lean-Configuration'>Lean configuration</a>" : "Lean configuration"?> that is used when running the LEAN engine locally. <?=$isCLIDocs ? "" : "The configuration is stored as JSON with support for both single-line and multiline comments. The Lean configuration file is based on the <a href='https://github.com/QuantConnect/Lean/blob/master/Launcher/config.json' rel='nofollow' target='_blank'>Launcher/config.json</a> file from the Lean repository. When you create a new workspace, the latest version of this file is downloaded and stored on your local drive."?></td>
+            <td>This file contains the <?=$leanCli ? "<a href='/docs/v2/lean-cli/initialization/configuration#03-Lean-Configuration'>Lean configuration</a>" : "Lean configuration"?> that is used when running the LEAN engine locally. <?=$leanCli ? "" : "The configuration is stored as JSON with support for both single-line and multiline comments. The Lean configuration file is based on the <a href='https://github.com/QuantConnect/Lean/blob/master/Launcher/config.json' rel='nofollow' target='_blank'>Launcher/config.json</a> file from the Lean repository. When you create a new workspace, the latest version of this file is downloaded and stored on your local drive."?></td>
         </tr>
     </tbody>
 </table>
