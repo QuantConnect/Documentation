f5876b1916c09c980fa4a306963e019fa95583f1
LouisSzeto <hke0073@icloud.com>
2023-01-31 20:22:36 +0800

    refactor cli docker install on window php

--- 05 Lean CLI/02 Installation/02 Installing Docker/02 Install on Windows.php
+++ 05 Lean CLI/02 Installation/02 Installing Docker/02 Install on Windows.php
@@ -1,7 +1,4 @@
-<?php
-echo file_get_contents(DOCS_RESOURCES."/cli/install/docker/windows-1.html");
-
-include(DOCS_RESOURCES."/cli/install/docker/windows-2.php");
+<?
 $isCLIDocs = true;
-$getInstallWindowsText($isCLIDocs);
+include(DOCS_RESOURCES."/cli/install/docker/windows.php");
 ?>
