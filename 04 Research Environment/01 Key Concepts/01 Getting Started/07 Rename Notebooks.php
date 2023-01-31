e8b338030cfc41f88e087d02429a0043ef83cff5
Jared Broad <jaredbroad@gmail.com>
2023-01-31 00:47:36 -0500

    Rename files

--- 04 Research Environment/01 Key Concepts/01 Getting Started/07 Rename Notebooks.php
+++ 04 Research Environment/01 Key Concepts/01 Getting Started/07 Rename Notebooks.php
@@ -1,7 +1,5 @@
-<?php
-include(DOCS_RESOURCES."/getting-started/projects/rename-files.php");
+<?
 $fileType = "notebook";
 $openProjectLink = "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects";
-$isDesktopDocs = false;
-$getRenameFileText($fileType, $openProjectLink, $isDesktopDocs);
+include(DOCS_RESOURCES."/getting-started/projects/rename-files.php");
 ?>
--- Resources/getting-started/projects/rename-files.php
+++ Resources/getting-started/projects/rename-files.php
@@ -1,24 +1,12 @@
-<?php
-$getRenameFileText = function($fileType, $openProjectLink, $isDesktopDocs=false, $videoLink=null)
-{
-    $navSide = $isDesktopDocs ? "left" : "right";
-    $sectionName = $isDesktopDocs ? "<span class='placeholder-text'>projectName</span>" : "Project (Workspace)";
-    echo "
 <p>Follow these steps to rename a {$fileType} in a project:</p>
 
 <ol>
     <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    <li>In the {$navSide} navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
+    <li>In the <?=$localPlatform ? "left" : "right" ?> navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
     <li>In the Explorer panel, right-click the {$fileType} you want to rename and then click <span class='menu-name'>Rename</span>.</li>
     <li>Enter the new name and then press <span class='button-name'>Enter</span>.</li>
 </ol>
-	";
-	
-    if (!is_null($videoLink))
-    {
-        echo "
+
+<? if (!is_null($videoLink)) { ?>
 <img class='docs-image' src='{$videoLink}'>
-        ";
-    }
-}
-?>
+<? } ?>
