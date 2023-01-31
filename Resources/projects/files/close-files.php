2f7ca6af84049ab9d36a54f95859d8bc25a172ce
Jared <jaredbroad@gmail.com>
2023-01-31 11:25:17 -0800

    Update close-files.php

--- Resources/projects/files/close-files.php
+++ Resources/projects/files/close-files.php
@@ -1,7 +1,3 @@
-<?php
-$env = $cloudPlatform ? "the IDE" : "VS Code" ;
-?>
-
-<p>To close a file, at the top of <?=$env?>, click the <span class='button-name'>x</span> button on the file tab you want to close.</p>
-<p>To close all of the files in a project, at the top of <?=$env?>, right-click one of the file tabs and then click <span class='button-name'>Close All</span>.</p>
+<p>To close a file, at the top of <?=$cloudPlatform ? "the IDE" : "VS Code"?>, click the <span class='button-name'>x</span> button on the file tab you want to close.</p>
+<p>To close all of the files in a project, at the top of <?=$cloudPlatform ? "the IDE" : "VS Code"?>, right-click one of the file tabs and then click <span class='button-name'>Close All</span>.</p>
 
