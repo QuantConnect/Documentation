4a37303d7f7f92d461a0a68d46bc2ffebeae5c59
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 14:02:03 +0000

    Refactors telegram-setup

--- 03 Writing Algorithms/40 Live Trading/40 Notifications/05 Telegram.php
+++ 03 Writing Algorithms/40 Live Trading/40 Notifications/05 Telegram.php
@@ -1,2 +1,2 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-intro.html"); ?>
-<?php include(DOCS_RESOURCES."/notifications/telegram-setup.php"); ?>
+<? include(DOCS_RESOURCES."/notifications/telegram-intro.html"); ?>
+<? include(DOCS_RESOURCES."/notifications/telegram-setup.php"); ?>
--- Resources/notifications/telegram-setup.php
+++ Resources/notifications/telegram-setup.php
@@ -4,6 +4,6 @@
     <pre class="python">self.Notify.Telegram(id, message, token)</pre>
 </div>
 
-<p>The <code>id</code> argument is your Telegram user Id or group Id. <?php echo file_get_contents(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
+<p>The <code>id</code> argument is your Telegram user Id or group Id. <? include(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
 
 <p>To add emoticons to your <code>message</code>, convert the emoticon character to UTF-32 with the <a href='https://www.branah.com/unicode-converter' rel='nofollow' target="_blank">Unicode Converter</a> on the Branah website. For example, the rocket emoji is <code>0001f680</code> in UTF-32 format, so use <code>\U001f680</code> in your message.</p>
