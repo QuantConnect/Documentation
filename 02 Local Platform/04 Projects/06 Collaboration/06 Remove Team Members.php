a2e5fec8cc6ede28f50f65f9f0ad155325436138
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 17:59:31 +0000

    Refactors Projects remove-team-members

--- 02 Local Platform/04 Projects/06 Collaboration/06 Remove Team Members.php
+++ 02 Local Platform/04 Projects/06 Collaboration/06 Remove Team Members.php
@@ -1,7 +1,1 @@
-<?php
-include(DOCS_RESOURCES."/projects/collab/remove-team-members.php");
-$openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
-$deleteProjectLink = "/docs/v2/local-platform/projects/getting-started#12-Delete-Projects";
-$isLocal = true;
-$getRemoveMemberText($openProjectLink, $deleteProjectLink, $isLocal);
-?>
+<? include(DOCS_RESOURCES."/projects/collab/remove-team-members.php"); ?>
