bb7f211834ad5c2e85b27359a816916da63ec8ff
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 18:06:53 +0000

    Refactors Projects toggle-live-control

--- 01 Cloud Platform/06 Projects/07 Collaboration/05 Toggle Live Control.php
+++ 01 Cloud Platform/06 Projects/07 Collaboration/05 Toggle Live Control.php
@@ -1,6 +1,1 @@
-<?php
-include(DOCS_RESOURCES."/projects/collab/toggle-live-control.php");
-$openProjectLink = "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects";
-$isLocal = false;
-$getToggleControlText($openProjectLink, $isLocal);
-?>
+<? include(DOCS_RESOURCES."/projects/collab/toggle-live-control.php"); ?>
--- Resources/projects/collab/toggle-live-control.php
+++ Resources/projects/collab/toggle-live-control.php
@@ -1,28 +1,11 @@
-<?php
-$getToggleControlText = function($openProjectLink, $isLocal)
-{
-
-    echo "
 <p>You need to have added a member to the project to toggle their live control of the project.</p>
 
 <p>Follow these steps to enable and disable live control for a team member:</p>
 <ol>
-    <li><a href='{$openProjectLink}'>Open the project</a>.</li>
-    ";
-    
-    if ($isLocal)
-    {
-        echo "
-    <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li>
-        ";
-    }
-    
-    echo "
+    <li><a href='/docs/v2<?=$localPlatform ? "/local-platform/projects/getting-started#04-Open-Projects" : "/cloud-platform/projects/getting-started#02-View-All-Projects" ?>'>Open the project</a>.</li>
+ 
+    <? if ($localPlatform) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'><span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
     <li>In the Collaborate section of the Project panel, click the profile image of the team member.</li>
     <li>Click the <span class='box-name'>Live Control</span> check box.</li>
     <li>Click <span class='button-name'>Save Changes</span>.</li>
 </ol>
-
-    ";
-}
-?>
