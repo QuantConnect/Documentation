7c13f0e214b942e32327af6151ac1bde967225de
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 22:53:31 +0800

    refactor php for cli install stay up to date (#983)
    * refactor php for cli install stay up to date
    
    * Update 03 Stay Up To Date.php
    
    * Update stay-up-to-date.php
    
    * Delete stay-up-to-date.php
    
    * Update 03 Stay Up To Date.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 05 Lean CLI/02 Installation/03 Installing Lean CLI/03 Stay Up To Date.php
+++ 05 Lean CLI/02 Installation/03 Installing Lean CLI/03 Stay Up To Date.php
@@ -1,13 +1,13 @@
-<?php
-include(DOCS_RESOURCES."/cli/install/cli/stay-up-to-date.php");
-$isCLIDocs = true;
-$getUpdateText($isCLIDocs);
-?>
+<p>
+    We regularly update the CLI to add new features and to fix issues. Therefore, it's important to keep both the CLI and the Docker images that the CLI uses up-to-date.
+</p>
+
+<?=$leanCli ? "<h4>Keep the CLI Up-To-Date</h4>" : "" ?>
+<p>
+    To update the CLI to the latest version, run <code>pip install --upgrade lean</code>. The CLI automatically performs a version check once a day and warns you in case you are running an outdated version.
+</p>
 
 <h4>Keep the Docker Images Up-To-Date</h4>
 <p>
-    Various commands like <code>lean backtest</code>, <code>lean optimize</code> and <code>lean research</code> run the LEAN engine in a Docker container to ensure all the required dependencies are available.
-    When you run these commands for the first time the Docker image containing LEAN and its dependencies is downloaded and stored.
-    Run these commands with the <code>--update</code> flag to update the images they use.
-    Additionally, these commands automatically perform a version check once a week and update the image they use when you are using an outdated Docker image.
+    Various commands like <code>lean backtest</code>, <code>lean optimize</code> and <code>lean research</code> run the LEAN engine in a Docker container to ensure all the required dependencies are available. When you run these commands for the first time the Docker image containing LEAN and its dependencies is downloaded and stored. Run these commands with the <code>--update</code> flag to update the images they use. Additionally, these commands automatically perform a version check once a week and update the image they use when you are using an outdated Docker image.
 </p>
