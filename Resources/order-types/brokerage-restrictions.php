a8ffb0bc001deec020059dc37cff0cada588befa
Jared <jaredbroad@gmail.com>
2023-01-31 09:02:44 -0800

    Update brokerage-restrictions.php

--- Resources/order-types/brokerage-restrictions.php
+++ Resources/order-types/brokerage-restrictions.php
@@ -1,8 +1,8 @@
-<p>Each brokerage has a set of assets and order types they support. To avoid issues with <?=$brokerageName?>, <a href='/docs/v2/writing-algorithms/initialization#05-Set-Brokerage-and-Cash-Model'>set the brokerage model</a> to a brokerage that supports them.</p>
+<p>Each brokerage has a set of assets and order types they support. To avoid issues with <?=$orderType ?>, <a href='/docs/v2/writing-algorithms/initialization#05-Set-Brokerage-and-Cash-Model'>set the brokerage model</a> to a brokerage that supports them.</p>
 
 <div class='section-example-container'>
     <pre class='csharp'>SetBrokerageModel(BrokerageName.<?=$brokerageName?>);</pre>
     <pre class='python'>self.SetBrokerageModel(BrokerageName.<?=$brokerageName?>)</pre>
 </div>
 
-<p>To check if your brokerage has any special requirements for <?=$brokerageName?>, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/cloud-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>
+<p>To check if your brokerage has any special requirements for <?=$orderType ?>, see the <span class='page-section-name'>Orders</span> section of the <a href='/docs/v2/cloud-platform/live-trading/brokerages'>brokerage integration documentation</a>.</p>
