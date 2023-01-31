d30fd58c04785bbacc7f0ed2da2ebd503a71cb07
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 18:50:37 +0000

    Replaces `file_get_contents` with `include`

--- 03 Writing Algorithms/01 Key Concepts/07 Research Guide/06 Overfitting.php
+++ 03 Writing Algorithms/01 Key Concepts/07 Research Guide/06 Overfitting.php
@@ -1,4 +1,4 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/research-guide/overfitting.php"); ?>
+<? include(DOCS_RESOURCES."/research-guide/overfitting.php"); ?>
 
 <p>If you have a collection of factors, you can backtest over a period of time to find the best-performing factors for the time period. If you then narrow the collection of factors to just the best-performing ones and backtest over the same period, the backtest will show great results. However, if you take the same best-performing factors and backtest them on an out-of-sample dataset, the performance will almost always underperform the in-sample period. To avoid issues with overfitting, follow these guidelines:</p>
 
