ea0665d97344e0dac204580dee35d9a1a4ad6220
Jared Broad <jaredbroad@gmail.com>
2023-01-31 01:03:45 -0500

    ide/Set descriptions

--- 02 Local Platform/04 Projects/01 Getting Started/10 Set Descriptions.php
+++ 02 Local Platform/04 Projects/01 Getting Started/10 Set Descriptions.php
@@ -1,6 +1,4 @@
-<?php
-include(DOCS_RESOURCES."/ide/set-descriptions.php");
+<?
 $openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
-$isDesktopDocs = true;
-$getSetDescriptionText($openProjectLink, $isDesktopDocs);
+include(DOCS_RESOURCES."/ide/set-descriptions.php");
 ?>
