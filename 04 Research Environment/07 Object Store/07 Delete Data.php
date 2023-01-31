8067a68b81a7fa5a55560a2dd980ffca6f3f7211
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 14:26:54 +0000

    Refactors ObjectStore delete-data

--- 04 Research Environment/07 Object Store/07 Delete Data.php
+++ 04 Research Environment/07 Object Store/07 Delete Data.php
@@ -1,7 +1,6 @@
-<?php
-include(DOCS_RESOURCES."/object-store/delete-data.php");
+<?
 $cSharpPrefix = "qb.";
 $pythonPrefix = "qb.";
 $context = "use the Research Environment";
-$getDeleteDataText($cSharpPrefix, $pythonPrefix, $context);
+include(DOCS_RESOURCES."/object-store/delete-data.php");
 ?>
