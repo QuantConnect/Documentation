92915f25155da733943ac0d8af9d51efaf182757
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 15:54:36 +0000

    Refactors ObjectStore cache-data (#996)

--- 04 Research Environment/07 Object Store/08 Cache Data.php
+++ 04 Research Environment/07 Object Store/08 Cache Data.php
@@ -1,7 +1,6 @@
-<?php
-include(DOCS_RESOURCES."/object-store/cache-data.php");
+<?
 $isAlgorithm = false;
 $cSharpPrefix = "qb.";
 $pythonPrefix = "qb.";
-$getCacheDataText($isAlgorithm, $cSharpPrefix, $pythonPrefix);
+include(DOCS_RESOURCES."/object-store/cache-data.php");
 ?>
