69072bc6bd7f4ae19cc54daecf79e25b0707a8ea
Jared <jaredbroad@gmail.com>
2023-01-31 06:53:05 -0800

    Update 03 Stay Up To Date.php

--- 05 Lean CLI/02 Installation/03 Installing Lean CLI/03 Stay Up To Date.php
+++ 05 Lean CLI/02 Installation/03 Installing Lean CLI/03 Stay Up To Date.php
@@ -1,4 +1,11 @@
-<? include(DOCS_RESOURCES."/cli/install/cli/stay-up-to-date.php"); ?>
+<p>
+    We regularly update the CLI to add new features and to fix issues. Therefore, it's important to keep both the CLI and the Docker images that the CLI uses up-to-date.
+</p>
+
+<?=$leanCli ? "<h4>Keep the CLI Up-To-Date</h4>" : "" ?>
+<p>
+    To update the CLI to the latest version, run <code>pip install --upgrade lean</code>. The CLI automatically performs a version check once a day and warns you in case you are running an outdated version.
+</p>
 
 <h4>Keep the Docker Images Up-To-Date</h4>
 <p>
