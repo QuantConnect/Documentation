e4cba0ae3e0325e610e9094229b7b3b2aeb5a415
Jared <jaredbroad@gmail.com>
2023-01-31 06:44:53 -0800

    Update introduction.php

--- Resources/cli/install/docker/introduction.php
+++ Resources/cli/install/docker/introduction.php
@@ -1,7 +1,7 @@
 <p>
-    If you run the LEAN engine locally with <?=$isCLIDocs ? "the CLI" : "QuantConnect Local"?>, LEAN executes in a Docker container.
+    If you run the LEAN engine locally with <?=$leanCli ? "the CLI" : "QuantConnect Local Platform"?>, LEAN executes in a Docker container.
     These Docker containers contain a minimal Linux-based operating system, the LEAN engine, and all the packages available to you on QuantConnect.com.
-    It is therefore required to install Docker if you plan on using <?=$isCLIDocs ? "the CLI" : "QuantConnect Local"?> to run the LEAN engine locally.
+    It is therefore required to install Docker if you plan on using <?=$leanCli ? "the CLI" : "QuantConnect Local Platform"?> to run the LEAN engine locally.
 </p>
 
 <p>
