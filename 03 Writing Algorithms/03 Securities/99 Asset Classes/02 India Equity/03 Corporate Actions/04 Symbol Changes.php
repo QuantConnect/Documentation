0595b64198f9383486ea408266a15ab76d756b2d
oussamanahdi <79997186+oussamanahdi@users.noreply.github.com>
2023-01-31 20:22:22 +0100

    converted symbol-changes

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/03 Corporate Actions/04 Symbol Changes.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/03 Corporate Actions/04 Symbol Changes.php
@@ -1,5 +1,4 @@
 <?php 
-include(DOCS_RESOURCES."/securities/corporate-actions/symbol-changes.php"); 
 $mapFilesLink = "https://github.com/QuantConnect/Lean/tree/master/Data/equity/india/map_files";
-$getSymbolChangesText($mapFilesLink);
+include(DOCS_RESOURCES."/securities/corporate-actions/symbol-changes.php"); 
 ?>
