a59fa907a498324e274b82842f366b3cfe5b7c48
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 22:56:18 +0800

    refactor cli docker install on window php (#985)
    * refactor cli docker install on window php
    
    * Update windows.php
    
    * Update 02 Install on Windows.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 05 Lean CLI/02 Installation/02 Installing Docker/02 Install on Windows.php
+++ 05 Lean CLI/02 Installation/02 Installing Docker/02 Install on Windows.php
@@ -1,7 +1,1 @@
-<?php
-echo file_get_contents(DOCS_RESOURCES."/cli/install/docker/windows-1.html");
-
-include(DOCS_RESOURCES."/cli/install/docker/windows-2.php");
-$isCLIDocs = true;
-$getInstallWindowsText($isCLIDocs);
-?>
+<? include(DOCS_RESOURCES."/cli/install/docker/windows.php"); ?>
--- Resources/cli/install/docker/windows.php
+++ Resources/cli/install/docker/windows.php
@@ -23,3 +23,9 @@
 <p>
     After you install Docker and restart your computer, if Docker prompts you that the WSL 2 installation is incomplete, follow the instructions in the dialog shown by Docker to finish the WSL 2 installation.
 </p>
+
+<p>
+    By default, Docker doesn't automatically start when your computer starts.
+    So, when you run the LEAN engine with <?=$leanCli ? "the CLI" : "QuantConnect Local Platform"?> for the first time after starting your computer, you must manually start Docker.
+    To automatically start Docker, open the Docker Desktop application, click <span class='menu-name'>Settings &gt; General</span>, and  then enable the <span class='box-name'>Start Docker Desktop when you log in</span> check box.
+</p>
