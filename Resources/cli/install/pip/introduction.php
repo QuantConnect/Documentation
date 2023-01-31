8dad74448ef0454aac5df624783f73575f4a337e
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-01-31 23:01:08 +0800

    refactor cli pip introduction php (#986)
    * refactor cli pip introduction php
    
    * Update introduction.php
    
    * Update 01 Introduction.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

---
+++ Resources/cli/install/pip/introduction.php
@@ -0,0 +1,17 @@
+<? if ($leanCli) { ?>
+<p> 
+    The Lean CLI is distributed as a Python package, so it requires <code>pip</code> to be installed. Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the CLI.
+</p>
+<? } else { ?>
+<p> 
+    QuantConnect Local Platform is better with <code>pip</code> installed. Because <code>pip</code> is distributed as a part of Python, you must install Python before you can install the QuantConnect Local Platform. The pip package manager is used to install the quantconnect-stubs package, which provides local autocomplete on the QuantConnect QCAlgorithm API.
+</p>
+<? } ?>
+
+<p>
+    This page contains installation instructions for <a href="https://docs.anaconda.com/anaconda/" target="_blank">Anaconda</a>, which is a Python distribution containing a lot of packages that are also available when running the LEAN engine. Having these packages installed locally makes it possible for your editor to provide autocomplete for them.
+</p>
+
+<p>
+    The Python distribution from the Microsoft Store is not supported. <a href='https://github.com/QuantConnect/lean-cli/issues/54#issuecomment-1104148972' rel='nofollow' target='nofollow'>Docker doesn't support Python 3.10</a>, so you must have 3.9.x or lower. 
+</p>
