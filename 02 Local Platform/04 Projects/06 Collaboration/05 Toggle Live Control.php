45726eaa83b10d901d9f471f3027485504f813dd
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-01-31 19:08:37 +0000

    Refactors Projects toggle-live-control (#1025)
    * Refactors Projects toggle-live-control
    
    * Update toggle-live-control.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 02 Local Platform/04 Projects/06 Collaboration/05 Toggle Live Control.php
+++ 02 Local Platform/04 Projects/06 Collaboration/05 Toggle Live Control.php
@@ -1,6 +1,1 @@
-<?php
-include(DOCS_RESOURCES."/projects/collab/toggle-live-control.php");
-$openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
-$isLocal = true;
-$getToggleControlText($openProjectLink, $isLocal);
-?>
+<? include(DOCS_RESOURCES."/projects/collab/toggle-live-control.php"); ?>
