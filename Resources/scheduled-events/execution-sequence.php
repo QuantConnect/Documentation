cd5aa33b96fb516fcce4c8d4bebc608a6fa7a3e8
Jared <jaredbroad@gmail.com>
2023-01-31 12:26:14 -0800

    Update execution-sequence.php

--- Resources/scheduled-events/execution-sequence.php
+++ Resources/scheduled-events/execution-sequence.php
@@ -1,8 +1,12 @@
 <p>The algorithm manager calls events in the following order:</p>
 <ol>
-    <? if ($linkScheduledEvents) { ?><li><a href='/docs/v2/writing-algorithms/scheduled-events'>Scheduled Events</a></li>
-<li>Consolidation event handlers</li><? } else { ?><li>Scheduled Events</li>
-<li><a href='/docs/v2/writing-algorithms/consolidating-data/getting-started'>Consolidation event handlers</a></li><? } ?>
+    <? if ($linkScheduledEvents) { ?>
+    <li><a href='/docs/v2/writing-algorithms/scheduled-events'>Scheduled Events</a></li>
+    <li>Consolidation event handlers</li>
+    <? } else { ?>
+    <li>Scheduled Events</li>
+    <li><a href='/docs/v2/writing-algorithms/consolidating-data/getting-started'>Consolidation event handlers</a></li>
+    <? } ?>
     <li><code>OnData</code> event handler</li>
 </ol>
-<p>This event flow is important to note. For instance, if your consolidation handlers or <code>OnData</code> event handler appends data to a <code>RollingWindow</code> and you use that <code>RollingWindow</code> in your Scheduled Event, when the Scheduled Event executes, the <code>RollingWindow</code> won't contain the most recent data.</p>"
+<p>This event flow is important to note. For instance, if your consolidation handlers or <code>OnData</code> event handler appends data to a <code>RollingWindow</code> and you use that <code>RollingWindow</code> in your Scheduled Event, when the Scheduled Event executes, the <code>RollingWindow</code> won't contain the most recent data.</p>
