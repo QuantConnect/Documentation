2b45517d9e09ae07bfffa9a3d2f4bb3d60bf0d1e
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-02-02 03:51:36 +0800

    Fix bugs on Python Typing in examples (#1049)

--- code-generators/_code_generation_helpers.py
+++ code-generators/_code_generation_helpers.py
@@ -8,11 +8,11 @@ API_REFERENCE = f'{WRITING_ALGORITHMS}/98 API Reference/'
 def get_text_content(url: str) -> str:
     return urlopen(url).read().decode('utf-8')
 
-def get_json_content(url: str) -> list:
+def get_json_content(url: str) -> List:
     content = get_text_content(url) \
         .replace("null", "None").replace("true", "True").replace("false", "False")
     return eval(content)
 
-def get_type(_type: str) -> list:
+def get_type(_type: str) -> List:
     url = f'https://www.quantconnect.com/services/inspector?type=T:{_type}'
     return get_json_content(url)
