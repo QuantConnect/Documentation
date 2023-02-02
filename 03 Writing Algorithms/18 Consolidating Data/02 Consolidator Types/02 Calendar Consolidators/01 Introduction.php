d949d2e58235742422aef87b2adca77eb8c7ce72
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-02 10:59:29 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/01 Introduction.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php 
 echo file_get_contents(DOCS_RESOURCES."/consolidators/consolidator-info/calendar/intro.html"); 
-include(DOCS_RESOURCE."/consolidators/consolidator-info/calendar/class-definition.php");
+include(DOCS_RESOURCES."/consolidators/consolidator-info/calendar/class-definition.php");
 ?>
