13e3756cab50612b3b5130c51cb828bfcb990a47
LouisSzeto <hke0073@icloud.com>
2023-02-02 23:53:10 +0800

    Address peer review

--- 01 Cloud Platform/11 Optimization/01 Getting Started/02 Launch Optimization Jobs.php
+++ 01 Cloud Platform/11 Optimization/01 Getting Started/02 Launch Optimization Jobs.php
@@ -1,5 +1,5 @@
 <p>The following video demonstrates how to launch an optimization job:</p>
-<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/optimization-gif.gif' alt="launch an optimization job">
+<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/optimization-gif.gif' alt="Launch an optimization job">
 
 <?php
 	echo file_get_contents(DOCS_RESOURCES."/getting-started/optimization/launch-optimization-jobs.html");
