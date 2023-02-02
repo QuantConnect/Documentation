ae1460a14698ae869ed9d7c1b05abd329b532c5f
derekmelchin <melchin.derek@gmail.com>
2023-02-02 12:55:44 -0700

    Add Crypto futures tutorial

---
+++ 04 Research Environment/03 Datasets/11 Crypto Futures/09 Wrangle Data.php
@@ -0,0 +1,27 @@
+<?
+$assetClass = 'Crypto Future';
+$singularAssetClass = 'Crypto Futures contract';
+$pluralAssetClass = 'Crypto Futures contracts';
+$historicalDataLink = "/docs/v2/research-environment/datasets/crypto-futures#04-Get-Historical-Data";
+$primarySymbolPy = 'btcusd';
+$primarySymbolC = 'btcusd';
+$primaryTicker = 'BTCUSD';
+$secondarySymbol = 'ethusd';
+$dataFrameImages = array(
+    'https://cdn.quantconnect.com/i/tu/crypto-future-research-data-1.jpg',
+    'https://cdn.quantconnect.com/i/tu/crypto-future-research-data-2.jpg',
+    'https://cdn.quantconnect.com/i/tu/crypto-future-research-data-3.jpg',
+    'https://cdn.quantconnect.com/i/tu/crypto-future-research-data-4.jpg'
+);
+$dataFrameColumnName = 'close';
+$columnNameEnglish = 'close';
+$supportsTrades = true;
+$supportsQuotes = true;
+$supportsTicks = true;
+$supportsAltData = false;
+$supportsOpenInterest = false;
+$supportsOptionHistory = false;
+$supportsFutureHistory = false;
+$contractExpiryDate = '';
+include(DOCS_RESOURCES."/datasets/wrangle-data.php");
+?>
