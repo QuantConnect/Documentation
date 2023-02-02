e005ef0f3e9f7e614199daa2522af4150b7a69b2
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-02-03 00:17:40 +0800

    Add `alt` tag for image (#1062)
    * Add `alt` tag for image in `Resources/forum-rewards` to `Resources/reports`
    
    * Address peer review
    
    * typo

--- Resources/algorithm-results/report/exposure.php
+++ Resources/algorithm-results/report/exposure.php
@@ -1,9 +1,9 @@
 <p>The backtest report displays time series for your portfolio's overall leverage and your portfolio's long-short exposure by asset class.<br></p>
 
 <h4>Leverage<br></h4>
-<img src="https://cdn.quantconnect.com/i/tu/report-leverage.jpg" class='img-responsive'>
+<img src="https://cdn.quantconnect.com/i/tu/report-leverage.jpg" class='img-responsive' alt="Leverage level chart over algorithm running period">
 <p>This chart displays your algorithm's utilization of leverage over time.</p>
 
 <h4>Long-Short Exposure By Asset Class</h4>
-<img src="https://cdn.quantconnect.com/i/tu/report-long-short-exposure.jpg" class='img-responsive'>
+<img src="https://cdn.quantconnect.com/i/tu/report-long-short-exposure.jpg" class='img-responsive' alt="Long-short exposure level chart over algorithm running period">
 <p>This chart displays your algorithm's long-short exposure by asset class over time.</p>
--- Resources/algorithm-results/report/rolling-statistics.php
+++ Resources/algorithm-results/report/rolling-statistics.php
@@ -1,14 +1,14 @@
 <p>The backtest report displays time series for your portfolio's rolling <a href='/docs/v2/writing-algorithms/key-concepts/glossary#07-beta'>beta</a> and <a href='/docs/v2/writing-algorithms/key-concepts/glossary#24-Sharpe-ratio'>Sharpe ratio</a>.<br></p>
 
 <h4>Rolling Portfolio Beta<br></h4>
-<img src="https://cdn.quantconnect.com/i/tu/report-rolling-beta.jpg" class='img-responsive'>
+<img src="https://cdn.quantconnect.com/i/tu/report-rolling-beta.jpg" alt="Rolling beta line chart" class='img-responsive'>
 <p>
     This chart displays the rolling portfolio beta over trailing 6 and 12 month periods.
     The light blue line represents the 6 month period and the dark blue line represents the 12 month period.
 </p>
 
 <h4>Rolling Sharpe Ratio</h4>
-<img src="https://cdn.quantconnect.com/i/tu/report-rolling-sharpe.jpg" class='img-responsive'>
+<img src="https://cdn.quantconnect.com/i/tu/report-rolling-sharpe.jpg" alt="Rolling Sharpe ratio line chart" class='img-responsive'>
 <p>
     This chart displays the rolling portfolio Sharpe ratio over trailing 6 and 12 month periods.
     The light blue line represents the 6 month period and the dark blue line represents the 12 month period.<br></p>
