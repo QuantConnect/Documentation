af15fc1b2072240365bc59c4a6fc66df82894c4e
Jared <jaredbroad@gmail.com>
2023-02-02 10:00:36 -0800

    Update 01 Introduction.php

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/01 Time Period Consolidators/01 Introduction.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/01 Time Period Consolidators/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php 
-echo file_get_contents(DOCS_RESOURCES."/consolidators/consolidator-info/time-period/introduction.html"); 
+include(DOCS_RESOURCES."/consolidators/consolidator-info/time-period/introduction.html"); 
 include(DOCS_RESOURCES."/consolidators/consolidator-info/time-period/class-definition.php");
 ?>
