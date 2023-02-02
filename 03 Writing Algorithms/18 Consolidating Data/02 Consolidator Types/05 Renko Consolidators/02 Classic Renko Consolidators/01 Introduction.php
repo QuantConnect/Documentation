a46162a3a887f3bdb03f738177048268ed663bdf
Web Editor <web.editor@quantconnect.com>
2023-02-02 13:05:34 -0500

    Automated push from server made by Jared Broad

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/05 Renko Consolidators/02 Classic Renko Consolidators/01 Introduction.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/05 Renko Consolidators/02 Classic Renko Consolidators/01 Introduction.php
@@ -1,4 +1,4 @@
 <?php 
-echo file_get_contents(DOCS_RESOURCES."/consolidators/consolidator-info/classic-renko/introduction.html"); 
-include(DOCS_RESOURCE."/consolidators/consolidator-info/classic-renko/class-definition.php");
+include(DOCS_RESOURCES."/consolidators/consolidator-info/classic-renko/introduction.html"); 
+include(DOCS_RESOURCES."/consolidators/consolidator-info/classic-renko/class-definition.php");
 ?>
