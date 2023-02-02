755667c57e3063b18a931e8582f1614f5b8fbb6a
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-02-02 21:06:43 +0000

    Adds Articles Section to Learning Center (#1073)
    * Adds Articles Section to Learning Center
    
    * Update strategy-library.html
    
    * Update strategy-library.html
    
    * Update 02 Articles.php
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

---
+++ 01 Cloud Platform/05 Learning Center/01 Training/02 Articles.php
@@ -0,0 +1,13 @@
+<p>QuantConnect maintains collections of related tutorials we call a <a href="/learning/articles/">Learn Series</a>. We have tutorial series covering the topics below - each with a set of articles or tutorials:</p>
+
+<a href="/learning/articles/investment-strategy-library"><h4>Investment Strategy Library<h4></a>
+<? include(DOCS_RESOURCES."/learning-center-training/strategy-library.html"); ?>
+
+<a href="/learning/articles/introduction-to-financial-python/"><h4>Introduction to Financial Python<h4></a>
+<p>Introduces basic Python functionality in the context of quantitative finance.</p>
+
+<a href="/learning/articles/introduction-to-options/"><h4>Introduction to Options<h4></a>
+<p>Introduces Options to those who are Option novices and have basic knowledge of applied mathematics, statistics, and financial markets.</p>
+
+<a href="/learning/articles/applied-options/"><h4>Applied Options<h4></a>
+<p>Simple options trading algorithms on QuantConnect to those who already have basic knowledge of options markets.</p>
