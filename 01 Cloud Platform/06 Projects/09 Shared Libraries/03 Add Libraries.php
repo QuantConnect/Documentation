86c294362e3d1aabec7e1de9b1679171cfeb43df
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-02-02 22:21:31 +0000

    Improves Create and Add Libraries (#1074)

--- 01 Cloud Platform/06 Projects/09 Shared Libraries/03 Add Libraries.php
+++ 01 Cloud Platform/06 Projects/09 Shared Libraries/03 Add Libraries.php
@@ -1,4 +1,4 @@
-<?php
+<?
 $openProjectLink = "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects";
 include(DOCS_RESOURCES."/projects/shared-libraries/add.php");
 ?>
--- Resources/projects/shared-libraries/add.php
+++ Resources/projects/shared-libraries/add.php
@@ -4,11 +4,20 @@
     <? if (!$cloudPlatform) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
     <li>In the Project panel, click <span class='button-name'>Add Library</span>.</li>
     <li>Click the <span class='field-name'>Choose a library...</span> field and then click a library from the drop-down menu.</li>
-    <li>Click <span class='button-name'>Add Library</span>.</li>
+    <li>Click <span class='button-name'>Add Library</span> (e.g. <span class='public-directory-name'>Calculators</span>).</li>
     <p>The library files are added to your project. To view the files, in the right navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</p>
-    <li class='python'>Import the library into your project to use the library.</li>
-    <div class='python section-example-container'>
-    <pre class='python'>from library import Library</pre>
+    <li>Import the library into your project to use the library.</li>
+    <div class='section-example-container'>
+    <pre class='csharp'>using Calculators;
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class AddLibraryAlgorithm : QCAlgorithm
+    {
+        private TaxesCalculator _taxesCalculator = new();
+    }
+}</pre>
+    <pre class='python'>from Calculators import TaxesCalculator
+class AddLibraryAlgorithm(QCAlgorithm):
+    taxes_calculator = TaxesCalculator()</pre>
     </div>
 </ol>
-
