2ac795e3818eabc24c37445d5c0f6c6c595b90f6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-06 10:53:31 -0700

    Update and rename 04 Part 2%2F Create Data Models.html to 04 Part 2%2F Create Data Models.php

--- 06 LEAN Engine/02 Contributing Datasets/02 Defining Data Models/04 Part 2%2F Create Data Models.php
+++ 06 LEAN Engine/02 Contributing Datasets/02 Defining Data Models/04 Part 2%2F Create Data Models.php
@@ -1,4 +1,4 @@
-<p>The input data to your model should be in a CSV file that's in chronological order.</p>
+<p>The input data to your model should be in a <span class="public-file-name">CSV</span> file that's in chronological order.</p>
 
 <div class="section-example-container"><pre>
 1997-01-01,905.2,941.4,905.2,939.55,38948210,978.21
