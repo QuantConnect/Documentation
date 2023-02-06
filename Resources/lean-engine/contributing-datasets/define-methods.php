f2ab1171a4ff1f8b2c0b0a14c83d8d1e3c775eb7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-06 10:56:38 -0700

    Update define-methods.php

--- Resources/lean-engine/contributing-datasets/define-methods.php
+++ Resources/lean-engine/contributing-datasets/define-methods.php
@@ -1,4 +1,4 @@
-<li>Define the methods in your dataset class that define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#06-Resolutions'>dataset frequency</a>.</li>
+<li>Define the methods in your dataset class that define the dataset frequency.</li>
 <div class="section-example-container">
     <pre>public class VendorNameDatasetName<?=$classNameEnding?> : BaseData
 {
@@ -19,7 +19,7 @@
 }</pre>
 </div>
 
-<li>Define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#07-Time-Zones'>DataTimeZone</a> method.</li>
+<li>Define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#06-TimeZones'>DataTimeZone</a> method.</li>
 <div class="section-example-container">
     <pre>public class VendorNameDatasetName<?=$classNameEnding?> : BaseData
 {
@@ -30,7 +30,7 @@
 }</pre>
 </div>
 
-<li>Define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#08-Linked-Datasets'>RequiresMapping</a> method.</li>
+<li>Define the <code>RequiresMapping</code> method.</li>
 <div class="section-example-container">
     <pre>public class VendorNameDatasetName<?=$classNameEnding?> : BaseData
 {
@@ -41,7 +41,7 @@
 }</pre>
 </div>
 
-<li>Define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#09-String-Representations'>ToString</a> method.</li>
+<li>Define the <code>ToString</code> method.</li>
 <div class="section-example-container">
     <pre>public class VendorNameDatasetName<?=$classNameEnding?> : BaseData
 {
@@ -52,7 +52,7 @@
 }</pre>
 </div>
 
-<li>Define the <a href='/docs/v2/lean-engine/contributing-datasets/key-concepts#10-Clone-Objects'>Clone</a> method.</li>
+<li>Define the <code>Clone</code> method.</li>
 <div class="section-example-container">
     <pre>public class VendorNameDatasetName<?=$classNameEnding?> : BaseData
 {
