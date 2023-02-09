1555136ab688933fae73ff3851d53a880dbe4c5b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-08 18:17:58 -0700

    Update 05 Orders.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Binance/05 Orders.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Binance/05 Orders.php
@@ -1,3 +1,3 @@
 <p>We model the Binance and Binance US APIs by supporting several order types, supporting order properties, and not supporting order updates. When you deploy live algorithms, you can <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#03-Place-Manual-Trades'>place manual orders</a> through the IDE.</p>
 
-<?php include(DOCS_RESOURCE."/brokerages/binance/orders.php"); ?>
+<?php include(DOCS_RESOURCES."/brokerages/binance/orders.php"); ?>
