d3e686c17e8107088e11c7f39d58390640982fb0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-08 18:30:37 -0700

    Update 03 Orders.php

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/13 Wolverine/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/13 Wolverine/03 Orders.php
@@ -1,4 +1,4 @@
-<p>The <code>WolverineBrokerageModel</code> supports several order types, order properties, and order updates.</p>
+<p>The <code>WolverineBrokerageModel</code> supports one order type, several order properties, and order updates.</p>
 
 <?php include(DOCS_RESOURCES."/brokerages/wolverine/orders.php"); ?>
 
