78e75a1ba4b8088c384c8481efd00dbabc7f0cb5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-08 18:31:29 -0700

    Update 03 Orders.php

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/13 Wolverine/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/13 Wolverine/03 Orders.php
@@ -1,4 +1,4 @@
-<p>The <code>WolverineBrokerageModel</code> supports one order type, order updates, but doesn't support extended market hours trading.</p>
+<p>The <code>WolverineBrokerageModel</code> supports one order type, but doesn't support order updates or extended market hours trading.</p>
 
 <?php include(DOCS_RESOURCES."/brokerages/wolverine/orders.php"); ?>
 
