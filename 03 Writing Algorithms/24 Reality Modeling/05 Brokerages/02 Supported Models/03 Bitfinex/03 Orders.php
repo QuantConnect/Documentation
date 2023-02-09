b9eaae73ffd2ef7d0e309412a5bda3dd00a52f3d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-08 18:25:54 -0700

    Update 03 Orders.php

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/03 Bitfinex/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/03 Bitfinex/03 Orders.php
@@ -1,3 +1,3 @@
 <p>The <code>BitfinexBrokerageModel</code> supports several order types, order properties, and order updates.</p>
 
-<?php include(DOCS_RESOURCE."/brokerages/bitfinex/orders.php"); ?>
+<?php include(DOCS_RESOURCES."/brokerages/bitfinex/orders.php"); ?>
