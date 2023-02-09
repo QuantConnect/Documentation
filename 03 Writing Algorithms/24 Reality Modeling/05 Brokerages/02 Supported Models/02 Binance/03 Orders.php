30f68bab6e54993bd98835e172e0aa29e4c14ca6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-08 18:25:27 -0700

    Update 03 Orders.php

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/02 Binance/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/02 Binance/03 Orders.php
@@ -1,3 +1,3 @@
 <p>The Binance and Binance US brokerage models support several order types and order properties, but don't support order updates.</p>
 
-<?php include(DOCS_RESOURCE."/brokerages/binance/orders.php"); ?>
+<?php include(DOCS_RESOURCES."/brokerages/binance/orders.php"); ?>
