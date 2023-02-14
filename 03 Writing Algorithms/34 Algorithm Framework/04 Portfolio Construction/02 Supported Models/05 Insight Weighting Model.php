fa153d2fab5cdb714da007b9c429478f6930ef81
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-14 09:50:19 -0700

    Update 05 Insight Weighting Model.php

--- 03 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/05 Insight Weighting Model.php
+++ 03 Writing Algorithms/34 Algorithm Framework/04 Portfolio Construction/02 Supported Models/05 Insight Weighting Model.php
@@ -33,7 +33,7 @@
     </tbody>
 </table>
 
-<p>This model supports other data types for the rebalancing frequency argument. For more information about the supported types, see <a href='/docs/v2/writing-algorithms/algorithm-framework/portfolio-construction/key-concepts#06-Rebalance-Frequency'>Rebalance Frequency</a>.</p>
+<p>This model supports other data types for the rebalancing frequency argument. For more information about the supported types, see <a href='/docs/v2/writing-algorithms/algorithm-framework/portfolio-construction/key-concepts#07-Rebalance-Frequency'>Rebalance Frequency</a>.</p>
 
 <p>To view the implementation of this model, see the <span class="csharp"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/InsightWeightingPortfolioConstructionModel.cs">LEAN GitHub repository</a></span><span class="python"><a target="_blank" rel="nofollow" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Framework/Portfolio/InsightWeightingPortfolioConstructionModel.py">LEAN GitHub repository</a></span>.</p>
 
