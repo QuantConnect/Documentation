5b7406cd237d5aaeebc223cb71ff886c83b05bbe
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-14 16:21:23 -0700

    Update 04 Get Historical Data.php

--- 04 Research Environment/04 Charting/05 Plotly.NET/04 Get Historical Data.php
+++ 04 Research Environment/04 Charting/05 Plotly.NET/04 Get Historical Data.php
@@ -1,1 +1,15 @@
-<?php include(DOCS_RESOURCES."/charting/get-historical-data.html"); ?>
+<p><a href="/docs/v2/research-environment/datasets/us-equity#04-Get-Historical-Data">Get some historical market data</a> to produce the plots. For example, to get data for a bank sector ETF and some banking companies over 2021, run:</p>
+
+<div class="section-example-container">
+    <pre class="csharp">var qb = new QuantBook();
+var tickers = new[] 
+{
+    "XLF",  // Financial Select Sector SPDR Fund
+    "COF",  // Capital One Financial Corporation
+    "GS",   // Goldman Sachs Group, Inc.
+    "JPM",  // J P Morgan Chase &amp; Co
+    "WFC"   // Wells Fargo &amp; Company   
+}
+var symbols = tickers.Select(ticker => qb.AddEquity(ticker, Resolution.Daily).Symbol);
+var history = qb.History(symbols, new DateTime(2021, 1, 1), new DateTime(2022, 1, 1));</pre>
+</div>
