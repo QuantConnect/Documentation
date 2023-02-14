c18f63d992415a01921f9a6175ea971996d69ac3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-14 10:05:55 -0700

    Update market-orders-requirements.php

--- Resources/order-types/market-orders-requirements.php
+++ Resources/order-types/market-orders-requirements.php
@@ -1,5 +1,5 @@
 <p><?=$orderType ?> must be submitted during market hours for all security types.</p>
-<p>If your algorithm place <?=strtolower($orderType) ?> at or after the last minute of regular market hours, they will be converted into <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders'>market-on-open orders</a> and will have to observe their <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders#07 Requirements.html'></a> for the following asset types:
+<p>If your algorithm place <?=strtolower($orderType) ?> at or after the last minute of regular market hours, they will be converted into <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders'>market-on-open orders</a> and will have to observe their <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders#07-Requirements'></a> for the following asset types:
 <ol>
     <li>Equities</li>
     <li>Equity Options</li>
