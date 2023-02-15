424599eb483e4953f4d9e2d1796253748d33ba9f
Web Editor <web.editor@quantconnect.com>
2023-02-14 19:57:33 -0500

    Automated push from server made by Derek Melchin

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/05 Renko Consolidators/03 Volume Renko Consolidators/01 Introduction.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/05 Renko Consolidators/03 Volume Renko Consolidators/01 Introduction.php
@@ -2,5 +2,3 @@
 include(DOCS_RESOURCES."/consolidators/consolidator-info/volume-renko/introduction.html"); 
 include(DOCS_RESOURCES."/consolidators/consolidator-info/volume-renko/class-definition.php");
 ?>
-
-<p>Note that <code>VolumeRenkoConsolidator</code> only accept <code>TradeBar</code> and <code>Tick</code> with <code>TickType.Trade</code> as input.</p>
