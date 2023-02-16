d19ae3b619de8971a5834106cb14e73dbb0778c1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-02-16 15:23:28 -0700

    Update orders.php

--- Resources/brokerages/interactive-brokers/orders.php
+++ Resources/brokerages/interactive-brokers/orders.php
@@ -270,4 +270,7 @@ def OnData(self, slice: Slice) -&gt; None:
 <h4>Financial Advisor Group Orders</h4>
 <p>To place FA group orders, see <a href='/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/interactive-brokers#18-Financial-Advisors'>Financial Advisors</a>.</p>
 
+<h4>Fractional Trading</h4>
+<p>The IB API and FIX/CTCI don't support <a rel="nofollow" target="_blank" href='https://www.interactivebrokers.com/en/trading/fractional-trading.php'>fractional trading</a>.</p>
+
 <?php include(DOCS_RESOURCES."/brokerages/handling-splits.html"); ?>
