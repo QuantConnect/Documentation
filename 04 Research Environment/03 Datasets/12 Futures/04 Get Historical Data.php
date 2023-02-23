3b9d1b41a7480be6df93aa95097e147b047a75b9
Jared <jaredbroad@gmail.com>
2023-02-23 15:07:10 -0800

    Update 04 Get Historical Data.php

--- 04 Research Environment/03 Datasets/12 Futures/04 Get Historical Data.php
+++ 04 Research Environment/03 Datasets/12 Futures/04 Get Historical Data.php
@@ -2,7 +2,11 @@
 $createSubscriptionsLink = "/docs/v2/research-environment/datasets/futures#03-Create-Subscriptions";
 $assetClass = "Futures";
 $underlyingSymbolVariable = "future.Symbol";
-$underlyingAssetClass = "";
+
+$contractVariableC = $underlyingSymbolVariable;
+$contractVariablePy = $underlyingSymbolVariable;
+
+$underlyingAssetClass = ""; 
 $supportsTicks = true;
 include(DOCS_RESOURCES."/datasets/research-environment/get-historical-data.php");
 ?>
