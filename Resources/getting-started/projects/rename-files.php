4dfe03f6ecd533c97d8913c56b883588666bd28a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-03-07 10:13:33 -0700

    Update rename-files.php

--- Resources/getting-started/projects/rename-files.php
+++ Resources/getting-started/projects/rename-files.php
@@ -1,12 +1,12 @@
-<p>Follow these steps to rename a {$fileType} in a project:</p>
+<p>Follow these steps to rename a <?=$fileType?> in a project:</p>
 
 <ol>
-    <li><a href='{$openProjectLink}'>Open the project</a>.</li>
+    <li><a href='<?=$openProjectLink?>'>Open the project</a>.</li>
     <li>In the <?=$localPlatform ? "left" : "right" ?> navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg'> <span class='icon-name'>Explorer</span> icon.</li>
-    <li>In the Explorer panel, right-click the {$fileType} you want to rename and then click <span class='menu-name'>Rename</span>.</li>
+    <li>In the Explorer panel, right-click the <?=$fileType?> you want to rename and then click <span class='menu-name'>Rename</span>.</li>
     <li>Enter the new name and then press <span class='button-name'>Enter</span>.</li>
 </ol>
 
 <? if (!is_null($videoLink)) { ?>
-<img class='docs-image' src='{$videoLink}' alt="Renaming a file">
+<img class='docs-image' src='<?=$videoLink?>' alt="Renaming a file">
 <? } ?>
