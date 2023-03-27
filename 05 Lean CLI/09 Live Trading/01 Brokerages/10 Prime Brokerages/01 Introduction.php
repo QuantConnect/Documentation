1d3e4cf9782c3681e621e4d150c71cd12f855e22
Web Editor <web.editor@quantconnect.com>
2023-03-27 11:53:21 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/01 Brokerages/10 Prime Brokerages/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/10 Prime Brokerages/01 Introduction.php
@@ -2,5 +2,3 @@
 $brokerageName="Terminal Link";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php"); 
 ?>
-
-<p>To view the implementation of Terminal Link, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.TerminalLink' rel='nofollow' target="_blank">Lean.Brokerages.TerminalLink repository</a>. </p>
