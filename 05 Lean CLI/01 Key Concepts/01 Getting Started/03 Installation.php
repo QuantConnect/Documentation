3b42dd73ab213bf2250012e70b4e0c04a6fe9af0
Nelson Vega <nelson@kleiotechnology.com>
2023-03-29 10:06:19 -0400

    Adding authentication step before lean init

--- 05 Lean CLI/01 Key Concepts/01 Getting Started/03 Installation.php
+++ 05 Lean CLI/01 Key Concepts/01 Getting Started/03 Installation.php
@@ -1,13 +1,14 @@
 <p>
     Run <code>pip install lean</code> in a terminal to install the latest version of the CLI.
 </p>
-
 <p>
-    After installing the CLI, open a terminal in an empty directory and run <code>lean init</code> to create your first workspace.
+   Open a terminal and run <code> lean login</code>. Enter userId and token.<a href="https://www.quantconnect.com/docs/v2/cloud-platform/community/profile#09-Request-API-Token" target="_blank">Request a token></a>
+</p>
+<p>
+    After authentication, open a terminal in an empty directory and run <code>lean init</code> to create your first workspace.
     The command downloads the latest configuration file and sample data from the <a href="https://github.com/QuantConnect/Lean" target="_blank">QuantConnect/Lean</a> repository.
     We recommend running all Lean CLI commands in your workspace directory.
 </p>
-
 <div class="cli section-example-container">
 <pre>$ lean init
 Downloading latest sample data from the Lean repository...
