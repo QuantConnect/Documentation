135d9fc601fdf34074e7c52d0c1ed28a28d16ba9
LouisSzeto <hke0073@icloud.com>
2023-03-31 13:51:31 +0800

    address peer review

--- code-generators/_code_generation_helpers.py
+++ code-generators/_code_generation_helpers.py
@@ -9,6 +9,16 @@ API_REFERENCE = f'{WRITING_ALGORITHMS}/98 API Reference/'
 MHDB = "https://raw.githubusercontent.com/QuantConnect/Lean/master/Data/market-hours/market-hours-database.json"
 SPDB = "https://raw.githubusercontent.com/QuantConnect/Lean/master/Data/symbol-properties/symbol-properties-database.csv"
 
+class MARKET_HOUR:
+    INTRODUCTION = "introduction"
+    PRE_MARKET = "pre-market-hours"
+    REGULAR = "regular-trading-hours"
+    POST_MARKET = "post-market-hours"
+    HOLIDAY = "holidays"
+    EARLY_CLOSE = "early-closes"
+    LATE_OPEN = "late-opens"
+    TIME_ZONE = "time-zone"
+
 def get_text_content(url: str) -> str:
     return urlopen(url).read().decode('utf-8')
 
