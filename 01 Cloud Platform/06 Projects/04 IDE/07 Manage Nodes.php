4dc01cfb7f18c07709f280d038a71189521a1103
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-14 13:04:35 -0600

    Update 07 Manage Nodes.php

--- 01 Cloud Platform/06 Projects/04 IDE/07 Manage Nodes.php
+++ 01 Cloud Platform/06 Projects/04 IDE/07 Manage Nodes.php
@@ -1,5 +1,5 @@
 <?php 
 $projectLink = "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects";
-$imageLink = "https://cdn.quantconnect.com/i/tu/resources-panel.png";
+$imageLink = "https://cdn.quantconnect.com/i/tu/resource-panel.png";
 include(DOCS_RESOURCES."/projects/ide/manage-nodes.php");
 ?>
