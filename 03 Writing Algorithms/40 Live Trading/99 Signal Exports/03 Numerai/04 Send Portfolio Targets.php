00a0b2fefb197396413533906684694d79139dd5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-17 18:17:36 -0600

    Update 04 Send Portfolio Targets.php

--- 03 Writing Algorithms/40 Live Trading/99 Signal Exports/03 Numerai/04 Send Portfolio Targets.php
+++ 03 Writing Algorithms/40 Live Trading/99 Signal Exports/03 Numerai/04 Send Portfolio Targets.php
@@ -3,6 +3,6 @@
 <p>To send targets that aren't based on your current portfolio holdings, pass a list of <code>PortfolioTarget</code> objects to the <code>SetTargetPortfolio</code> method. In this situation, the number you pass to the <code>PortfolioTarget</code> constructor represents the portfolio weight. Don't use the <code>PortfolioTarget.Percent</code> method.</p>
 
 <div class="section-example-container">
-<pre class="csharp">SignalExport.SetTargetPortfolioFromPortfolio(targets);</pre>
-<pre class="python">self.SignalExport.SetTargetPortfolioFromPortfolio(targets)</pre>
+<pre class="csharp">SignalExport.SetTargetPortfolio(targets);</pre>
+<pre class="python">self.SignalExport.SetTargetPortfolio(targets)</pre>
 </div>
