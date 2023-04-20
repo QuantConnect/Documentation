603f770b8944ac662ba38287db0c2accfd133387
Web Editor <web.editor@quantconnect.com>
2023-04-20 16:20:28 -0400

    Automated push from server made by Derek Melchin

--- 03 Writing Algorithms/40 Live Trading/99 Signal Exports/01 Collective2/04 Send Portfolio Targets.php
+++ 03 Writing Algorithms/40 Live Trading/99 Signal Exports/01 Collective2/04 Send Portfolio Targets.php
@@ -1,8 +1,15 @@
-<p>To send targets, pass a <code>PortfolioTarget</code> object or a list of <code>PortfolioTarget</code> objects to the <code>SetTargetPortfolio</code> method. In this situation, the number you pass to the <code>PortfolioTarget</code> constructor represents the portfolio weight. Don't use the <code>PortfolioTarget.Percent</code> method.</p>
+<p>To send targets to Collective2, pass a <code>PortfolioTarget</code> object or a list of <code>PortfolioTarget</code> objects to the <code>SetTargetPortfolio</code> method. In this situation, the number you pass to the <code>PortfolioTarget</code> constructor represents the portfolio weight. Don't use the <code>PortfolioTarget.Percent</code> method.</p>
 
 <div class="section-example-container">
 <pre class="csharp">var target = new PortfolioTarget(_symbol, weight);
 SignalExport.SetTargetPortfolio(target);</pre>
 <pre class="python">target = PortfolioTarget(self.symbol, weight)
 self.SignalExport.SetTargetPortfolio(target)</pre>
+</div>
+
+<p>If you use a margin account, you can send your current portfolio holdings by calling the <code>SetTargetPortfolioFromPortfolio</code> method.</p>
+
+<div class="section-example-container">
+<pre class="csharp">SignalExport.SetTargetPortfolioFromPortfolio();</pre>
+<pre class="python">self.SignalExport.SetTargetPortfolioFromPortfolio()</pre>
 </div>
