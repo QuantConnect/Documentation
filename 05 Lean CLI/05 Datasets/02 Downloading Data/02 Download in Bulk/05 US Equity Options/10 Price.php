96961f23e44795f3571856dcad170c56795bb666
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-21 14:53:14 -0600

    Update and rename 10 Price.html to 10 Price.php

--- 05 Lean CLI/05 Datasets/02 Downloading Data/02 Download in Bulk/05 US Equity Options/10 Price.php
+++ 05 Lean CLI/05 Datasets/02 Downloading Data/02 Download in Bulk/05 US Equity Options/10 Price.php
@@ -1,3 +1,7 @@
+<p>The following table shows the price of an annual subscription to the US Equity Security Master for each organization tier:</p>
+
+<?php include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
+
 <p>The following table shows the price of the US Equity Options dataset subscriptions:</p>
 
 <table class="qc-table table" id='trade-and-quote-price'>
