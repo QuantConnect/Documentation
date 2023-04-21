92af868cc05f522c5e10a40b9d6d8e333d4af937
derekmelchin <melchin.derek@gmail.com>
2023-04-21 10:00:30 -0600

    Add US Coarse Universe Bulk Download

---
+++ 05 Lean CLI/05 Datasets/02 Downloading Data/02 Download in Bulk/03 US Coarse Universe/10 Price.php
@@ -0,0 +1,46 @@
+<p>The following table shows the price of an annual subscription to the US Equity Security Master for each organization tier:</p>
+
+<?php include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
+
+<p>The following table shows the price of the US Equity dataset subscriptions:</p>
+
+<table class="qc-table table" id='trade-and-quote-price'>
+    <thead>
+        <tr>
+            <th>Resolution</th>
+            <th>Price of Historical Data ($)</th>
+            <th>Price of Daily Updates ($/Year)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Hour & Daily</td>
+            <td>3,480</td>
+            <td>960</td>
+        </tr>
+        <tr>
+            <td>Minute</td>
+            <td>TBD</td>
+            <td>TBD</td>
+        </tr>
+        <tr>
+            <td>Second</td>
+            <td>TBD</td>
+            <td>TBD</td>
+        </tr>
+        <tr>
+            <td>Tick</td>
+            <td>TBD</td>
+            <td>TBD</td>
+        </tr>
+    </tbody>
+</table>
+
+<style>
+#trade-and-quote-price td:nth-child(2), 
+#trade-and-quote-price th:nth-child(2),
+#trade-and-quote-price td:last-child, 
+#trade-and-quote-price th:last-child {
+    text-align: right;
+}
+</style>
