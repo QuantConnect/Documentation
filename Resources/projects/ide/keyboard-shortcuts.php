28b05c9cf176b79cdeabdebcb15d9b6b115f1080
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-24 14:02:01 -0600

    Update keyboard-shortcuts.php

--- Resources/projects/ide/keyboard-shortcuts.php
+++ Resources/projects/ide/keyboard-shortcuts.php
@@ -5,7 +5,7 @@
 <ol>
     <li><a href='<?=$projectLink?>'>Open a project</a>.</li>
     <li>Press <span class='key-combinations'>F1</span>.</li>
-    <li>Enter \"Preferences: Open Keyboard Shortcuts\".</li>
+    <li>Enter "Preferences: Open Keyboard Shortcuts".</li>
     <li>Click <span class='button-name'>Preferences: Open Keyboard Shortcuts</span>.</li>
 </ol>
 
