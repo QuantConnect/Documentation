be48f6b0e29ba08551585b96c1ab079f8b9e5934
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-24 11:45:57 -0600

    Update 05 Set Models.php

--- 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/05 Set Models.php
+++ 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/05 Set Models.php
@@ -9,8 +9,7 @@ security = self.AddEquity("SPY")
 security.SetFeeModel(SecurityMarginModel(3))</pre>
 </div>
 
-<p>You can also set the buying power model in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>
-. If your algorithm has a universe, use the security initializer technique. In order to initialize single security subscriptions with the security initializer, call <code>SetSecurityInitializer</code> before you create the subscriptions.</p>
+<p>You can also set the buying power model in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>. If your algorithm has a universe, use the security initializer technique. In order to initialize single security subscriptions with the security initializer, call <code>SetSecurityInitializer</code> before you create the subscriptions.</p>
 
 <?php
 $overwriteCodePy = "security.SetBuyingPowerModel(SecurityMarginModel(3))";
