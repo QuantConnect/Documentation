92441fd98300a6e0e7eed39a83db05642191505d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-24 13:11:30 -0600

    Update 04 Send Portfolio Targets.php

--- 03 Writing Algorithms/40 Live Trading/99 Signal Exports/02 CrunchDAO/04 Send Portfolio Targets.php
+++ 03 Writing Algorithms/40 Live Trading/99 Signal Exports/02 CrunchDAO/04 Send Portfolio Targets.php
@@ -1,4 +1,7 @@
-<?php include(DOCS_RESOURCES."/live-trading/signal-exports/send-portfolio-holdings.html"); ?>
+<?php 
+$providerName = "CrunchDAO";
+include(DOCS_RESOURCES."/live-trading/signal-exports/send-portfolio-holdings.php");
+?>
 
 <p>To send targets that aren't based on your current portfolio holdings, pass a <code>PortfolioTarget</code> object or a list of <code>PortfolioTarget</code> objects to the <code>SetTargetPortfolio</code> method. In this situation, the number you pass to the <code>PortfolioTarget</code> constructor represents the portfolio weight. Don't use the <code>PortfolioTarget.Percent</code> method.</p>
 
