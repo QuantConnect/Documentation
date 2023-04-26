74fa0670511e48d3ffdef030aa56ab9d9298d8f1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-04-26 11:22:23 -0600

    Update 04 Troubleshooting.php

--- 02 Local Platform/02 Set Up Environment/01 Installing Local Platform/04 Troubleshooting.php
+++ 02 Local Platform/02 Set Up Environment/01 Installing Local Platform/04 Troubleshooting.php
@@ -1,5 +1,7 @@
+<p>The following sections explain how to solve some issues you may encounter while installing the local platform.</p>
+
 <h4>Docker with M1/M2/Colima</h4>
-<?php include(DOCS_RESOURCES."/cli/install/docker/mac.html"); ?>
+<?php include(DOCS_RESOURCES."/cli/install/docker/mac.php"); ?>
 
 <h4>Docker with WSL 2 Features</h4>
 <p>When you download Docker Desktop, you need to select the <span class="box-name">Enable WSL 2 Features</span> check box. After you install Docker and restart your computer, if Docker prompts you that the WSL 2 installation is incomplete, follow the instructions in the dialog shown by Docker to finish the WSL 2 installation.</p>
