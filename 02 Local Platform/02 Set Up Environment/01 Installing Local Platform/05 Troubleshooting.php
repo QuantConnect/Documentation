e9dbf06e528bcf4075f835a306b1713f37790fd9
Web Editor <web.editor@quantconnect.com>
2023-04-27 19:16:53 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/02 Set Up Environment/01 Installing Local Platform/05 Troubleshooting.php
+++ 02 Local Platform/02 Set Up Environment/01 Installing Local Platform/05 Troubleshooting.php
@@ -6,5 +6,11 @@
 <h4>Docker with WSL 2 Features</h4>
 <p>When you download Docker Desktop, you need to select the <span class="box-name">Enable WSL 2 Features</span> check box. After you install Docker and restart your computer, if Docker prompts you that the WSL 2 installation is incomplete, follow the instructions in the dialog shown by Docker to finish the WSL 2 installation.</p>
 
+<h4>Docker Not Found</h4>
+<p>If you have Docker installed but the local platform can't detect it, update your <a href=''><span class='button-name'>Executable Path: Docker</span> setting</a> to be the path to your Docker executable.</p>
+
+<h4>LEAN CLI Conflicts</h4>
+## TODO
+
 <h4>Further Support</h4>
 <p>For further support with installing the local platform, <a href='/contact'>contact us</a>.</p>
