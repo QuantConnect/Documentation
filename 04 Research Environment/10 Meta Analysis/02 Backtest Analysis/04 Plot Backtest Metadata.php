66b6c731e49ec9308cb4fe90637898804cd56a7b
LouisSzeto <hke0073@icloud.com>
2023-04-28 11:36:15 +0800

    Improves meta analysis visualization pages

--- 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/04 Plot Backtest Metadata.php
+++ 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/04 Plot Backtest Metadata.php
@@ -1,4 +1,4 @@
-<p>Follow these steps to plot the equity curve of a backtest:</p>
+<p>Follow these steps to plot the equity curve, benchmark, and drawdown of a backtest:</p>
 
 <ol>
     <li>Get the backtest instance.</li>
@@ -10,6 +10,10 @@
 	
     <?
     $isLive = false;
-    include(DOCS_RESOURCES."/qc-api/plot-equity-curve.php");
+    include(DOCS_RESOURCES."/qc-api/plot-metadata.php");
     ?>
 </ol>
+
+<p>Apart from the attributes of the above example, the below shows the complete list of items that is available to use the above method to visualize:</p>
+
+<?include(DOCS_RESOURCES."/qc-api/metadata-table.html");?>
---
+++ Resources/qc-api/plot-metadata.php
@@ -0,0 +1,45 @@
+<li>Get the "Strategy Equity", "Drawdown", and "Benchmark" <code>Chart</code> instance.</li>
+<div class="section-example-container">
+    <pre class="python">equity_chart = <?=$isLive ? "results" : "backtest"?>.Charts["Strategy Equity"]
+drawdown_chart = <?=$isLive ? "results" : "backtest"?>.Charts["Drawdown"]
+benchmark_chart = <?=$isLive ? "results" : "backtest"?>.Charts["Benchmark"]</pre>
+</div>
+
+<li>Get values from the "Equity", "Equity Drawdown", and "Benchmark" <code>Series</code> from the corresponding charts.</li>
+<div class="section-example-container">
+    <pre class="python">equity = equity_chart.Series["Equity"].Values
+drawdown = drawdown_chart.Series["Equity Drawdown"].Values
+benchmark = benchmark_chart.Series["Benchmark"].Values</pre>
+</div>
+
+<li>Create a <code>pandas.DataFrame</code> from the series values, use <code>ffill</code> method to smooth the series.</li>
+<div class="section-example-container">
+    <pre class="python">df = pd.DataFrame({
+    "Equity": pd.Series({datetime.fromtimestamp(value.x): value.y for value in equity}),
+    "Drawdown": pd.Series({datetime.fromtimestamp(value.x): value.y for value in drawdown}),
+    "Benchmark": pd.Series({datetime.fromtimestamp(value.x): value.y for value in benchmark})
+}).ffill()</pre>
+</div>
+
+<li>Plot the performance chart.</li>
+<div class="section-example-container">
+    <pre class="python"># Create subplots to plot series on same/different plots
+fig, ax = plt.subplots(2, 1, figsize=(12, 12), sharex=True, gridspec_kw={'height_ratios': [2, 1]})
+
+# Plot the equity curve
+ax[0].plot(df.index, df["Equity"])
+ax[0].set_title("Strategy Equity Curve")
+ax[0].set_ylabel("Portfolio Value ($)")
+
+# Plot the benchmark on the same plot, scale by using another y-axis
+ax2 = ax[0].twinx()
+ax2.plot(df.index, df["Benchmark"], color="grey")
+ax2.set_ylabel("Benchmark Price ($)", color="grey")
+
+# Plot the drawdown on another plot
+ax[1].plot(df.index, df["Drawdown"], color="red")
+ax[1].set_title("Drawdown")
+ax[1].set_xlabel("Time")
+ax[1].set_ylabel("%")</pre>
+</div>
+<img class='docs-image' src="https://cdn.quantconnect.com/i/tu/api-result-plot.png" alt="api-equity-curve">
