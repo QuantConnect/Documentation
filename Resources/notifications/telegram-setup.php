95e1f936fd7156bde77ac01aa3f3bdeead9cb50d
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-04-28 19:18:09 +0100

    Updates Telegram Setup Instructions

--- Resources/notifications/telegram-setup.php
+++ Resources/notifications/telegram-setup.php
@@ -4,6 +4,8 @@
     <pre class="python">self.Notify.Telegram(id, message, token)</pre>
 </div>
 
-<p>The <code>id</code> argument is your Telegram user Id or group Id. <? include(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
+<p>The <code>id</code> argument is your Telegram group Id. <? include(DOCS_RESOURCES."/notifications/telegram-group-id.html"); ?></p>
+
+<p>The <code>token</code> optional argument is token of a bot you must add to your Telegram group. To create a bot, please chat with @BotFather, and follow his instructions. If you want to use our bot, the username is @quantconnect_notifications_bot.</p>
 
 <p>To add emoticons to your <code>message</code>, convert the emoticon character to UTF-32 with the <a href='https://www.branah.com/unicode-converter' rel='nofollow' target="_blank">Unicode Converter</a> on the Branah website. For example, the rocket emoji is <code>0001f680</code> in UTF-32 format, so use <code>\U001f680</code> in your message.</p>
