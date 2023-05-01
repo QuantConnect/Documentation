382bc06b5c981544ff39df36dc7756d05d66e3a0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-01 16:34:58 -0600

    Update 02 Set Parameters.php

--- 03 Writing Algorithms/30 Parameters/02 Set Parameters.php
+++ 03 Writing Algorithms/30 Parameters/02 Set Parameters.php
@@ -1,4 +1,7 @@
-<?
-$openProjectLink = "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects";
-include(DOCS_RESOURCES."/ide/edit-parameters.php");
-?>
+<p>The process to add and edit parameters depends on the environment you use to write algorithms. See the tutorial in one of the following environments:</p>
+
+<ul>
+    <li><a href='/docs/v2/cloud-platform/projects/getting-started#10-Edit-Parameters'>Cloud Platform</a></li>
+    <li><a href='/docs/v2/local-platform/projects/getting-started#11-Edit-Parameters'>Local Platform</a></li>
+    <li><a href='/docs/v2/lean-cli/optimization/parameters'>LEAN CLI</a></li>
+</ul>
