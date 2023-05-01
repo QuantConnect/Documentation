9714b42ae096a785d8156ae8f784c7677a8ba080
Web Editor <web.editor@quantconnect.com>
2023-05-01 12:52:57 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/04 Initialization/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
+++ 02 Local Platform/04 Initialization/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
@@ -1,4 +1,4 @@
-<p>Follow these steps to pull one of your <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>cloud workspaces</a> and set it as your local workspace:</p>
+<p>Follow these steps to pull one of your <a href='/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>cloud workspaces</a> and set it as your local workspace:</p>
 <ol>
     <li><a href="/docs/v2/local-platform/set-up-environment/accounts#02-Log-In">Log in to Local Platform</a>.</li>
     <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg" alt="VS Code QuantConnect icon"> <span class="icon-name">QuantConnect</span> icon.</li>
@@ -11,6 +11,6 @@
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/pull-cloud-workspaces.png" alt="Pull cloud workspace">
 </ol>
 
-<p>It takes a few minutes to create a new workspace directory and populate it with the <a href='/docs/v2/local-platform/set-up-environment/organization-workspaces#05-Directory-Structure'>the initial file structure</a>. After the workspace directory is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
+<p>It takes a few minutes to create a new workspace directory and populate it with the <a href='/docs/v2/local-platform/initialization/organization-workspaces#07-Directory-Structure'>the initial file structure</a>. After the workspace directory is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
 
 <? include(DOCS_RESOURCES."/cli/init/wsl.html"); ?>
