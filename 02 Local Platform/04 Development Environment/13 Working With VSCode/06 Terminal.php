ec10beabcc0a7ea1a5443c63368c86c5aa6dc6a9
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-04 16:29:04 -0600

    Update 06 Terminal.php

--- 02 Local Platform/04 Development Environment/13 Working With VSCode/06 Terminal.php
+++ 02 Local Platform/04 Development Environment/13 Working With VSCode/06 Terminal.php
@@ -1,6 +1,6 @@
 <?php 
 $cloudTermianlImage = "https://cdn.quantconnect.com/i/tu/local-platform-terminal.png";
-$problemsImagePy = "https://cdn.quantconnect.com/i/tu/qc-desktop-cloud-terminal-problems.jpg";
-$problemsImageC = "https://cdn.quantconnect.com/i/tu/qc-desktop-cloud-terminal-problems-c.jpg";
+$problemsImagePy = "https://cdn.quantconnect.com/i/tu/local-platform-terminal-problems.png";
+$problemsImageC = "https://cdn.quantconnect.com/i/tu/local-platform-terminal-problems-c.png";
 include(DOCS_RESOURCES."/projects/ide/cloud-terminal.php");
 ?>
