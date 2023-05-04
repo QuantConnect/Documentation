576f6aead53c81cc5737b0e03f6ef1344b08d299
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-04 16:02:42 -0600

    Update toggle-live-control.php

--- Resources/projects/collab/toggle-live-control.php
+++ Resources/projects/collab/toggle-live-control.php
@@ -3,7 +3,7 @@
 <p>Follow these steps to enable and disable live control for a team member:</p>
 <ol>
     <li><a href='<?=$localPlatform ? "/docs/v2/local-platform/projects/getting-started#04-Open-Projects" : "/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects" ?>'>Open the project</a>.</li>
-    <? if ($localPlatform) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'><span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
+    <? if ($localPlatform) { ?><li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'><span class='icon-name'> QuantConnect</span> icon.</li><? } ?>
     <li>In the Collaborate section of the Project panel, click the profile image of the team member.</li>
     <li>Click the <span class='box-name'>Live Control</span> check box.</li>
     <li>Click <span class='button-name'>Save Changes</span>.</li>
