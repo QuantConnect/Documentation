68f0ee2fd55943b1597afe83888af6e6cfd6b99f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-04 16:24:59 -0600

    Update cloud-terminal.php

--- Resources/projects/ide/cloud-terminal.php
+++ Resources/projects/ide/cloud-terminal.php
@@ -1,5 +1,5 @@
-<p>The Cloud Terminal panel at the bottom of the IDE shows API messages, errors, and the logs from your algorithms.</p>
-<img class='docs-image' src='<?=$cloudTermianlImage?>' alt="Cloud IDE interface">
+<p>The terminal panel at the bottom of the IDE shows API messages, errors, and the logs from your algorithms.</p>
+<img class='docs-image' src='<?=$cloudTermianlImage?>' alt="Termianl interface">
 
 <p>The <span class='tab-name'>Problems</span> tab of the panel highlights the coding errors in your algorithms.</p>
 <img class='python docs-image' src='<?=$problemsImagePy?>' alt="Compilation error in code snippets">
