27fbdd0b887e4564388adcd43d37209cd5b66670
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-04 16:04:33 -0600

    Update and rename 03 Request New Branches.php to 03 Custom Branches.php

---
+++ 02 Local Platform/04 Development Environment/08 LEAN Engine Versions/03 Custom Branches.php
@@ -0,0 +1,1 @@
+<p>To create a custom version of LEAN, see <a href='/docs/v2/lean-cli/projects/custom-docker-images'>Custom Docker Images</a>.</p>
--- 02 Local Platform/04 Development Environment/08 LEAN Engine Versions/03 Request New Branches.php
+++
@@ -1,1 +0,0 @@
-<?php echo file_get_contents(DOCS_RESOURCES."/projects/lean-engine-branches/request.html"); ?>
