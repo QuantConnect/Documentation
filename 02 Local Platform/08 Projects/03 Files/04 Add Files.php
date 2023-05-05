2102bb1d75cbe8a2a4441f3fbdefc9059286ff94
Web Editor <web.editor@quantconnect.com>
2023-05-05 18:33:31 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/08 Projects/03 Files/04 Add Files.php
+++ 02 Local Platform/08 Projects/03 Files/04 Add Files.php
@@ -1,5 +1,5 @@
 <?php
-$videoLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-add-file.gif";
+$videoLink = "https://cdn.quantconnect.com/i/tu/local-platform-add-file-py.gif";
 $openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
 include(DOCS_RESOURCES."/projects/files/add-files.php");
 ?>
