cd7c36ead33083695b761e46536ea80ff70defc7
Web Editor <web.editor@quantconnect.com>
2023-05-05 17:21:53 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/04 Development Environment/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
+++ 02 Local Platform/04 Development Environment/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
@@ -1,4 +1,4 @@
-<p>Follow these steps to pull one of your <a href='/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>cloud workspaces</a> and set it as your local workspace:</p>
+<p>Follow these steps to pull one of your <a href='/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>cloud organization workspaces</a> and set it as your local organization workspace:</p>
 <ol>
     <li><a href="/docs/v2/local-platform/initialization/accounts#02-Log-In">Log in to Local Platform</a>.</li>
     <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg" alt="VS Code QuantConnect icon"> <span class="icon-name">QuantConnect</span> icon.</li>
@@ -7,10 +7,10 @@
     <li>In the Pull QuantConnect Organization Workspace window, click the cloud workspace (<a href='https://www.quantconnect.com/docs/v2/cloud-platform/organizations'>organization</a>) that you want to pull.</li>
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/pull-cloud-workspaces-1.png" alt="Pull cloud organization">
     
-    <li>In the Pull QuantConnect Organization Workspace window, create a directory to serve as the workspace and then click <span class="button-name">Select</span>.</li>
+    <li>In the Pull QuantConnect Organization Workspace window, create a directory to serve as the organization workspace and then click <span class="button-name">Select</span>.</li>
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/pull-cloud-workspaces-2.png" alt="Pull cloud workspace">
 </ol>
 
-<p>It takes a few minutes to create a new workspace directory and populate it with the <a href='/docs/v2/local-platform/initialization/organization-workspaces#07-Directory-Structure'>the initial file structure</a>. After the workspace directory is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
+<p>It takes a few minutes to create a new organization workspace directory and populate it with the <a href='/docs/v2/local-platform/initialization/organization-workspaces#07-Directory-Structure'>the initial file structure</a>. After the organization workspace is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
 
 <? include(DOCS_RESOURCES."/cli/init/wsl.html"); ?>
