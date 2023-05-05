39fbe71c35bf4de5ec12c8e9fc830d8f73a40b96
Web Editor <web.editor@quantconnect.com>
2023-05-05 18:50:45 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/08 Projects/03 Files/08 Rename Files and Directories.php
+++ 02 Local Platform/08 Projects/03 Files/08 Rename Files and Directories.php
@@ -1,6 +1,6 @@
 <?
 $fileType = "file or directory";
 $openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
-$videoLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-rename-files.gif";
+$videoLink = "https://cdn.quantconnect.com/i/tu/local-platform-rename-file-py.gif";
 include(DOCS_RESOURCES."/getting-started/projects/rename-files.php");
 ?>
