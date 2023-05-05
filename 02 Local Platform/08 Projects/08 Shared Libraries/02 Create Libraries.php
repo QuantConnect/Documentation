74be453e41bf4b7c1c7baa66e55cd90b09110897
Web Editor <web.editor@quantconnect.com>
2023-05-05 17:25:53 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/08 Projects/08 Shared Libraries/02 Create Libraries.php
+++ 02 Local Platform/08 Projects/08 Shared Libraries/02 Create Libraries.php
@@ -7,7 +7,7 @@
     <li>Click <span class="button-name">Create New</span>.</li>
     <li>In the <span class="field-name">Input Library Name</span> field, enter a name for the library.</li>
     <li>Click <span class="button-name">Create Library</span>.</li>
-    <p>The template library files are added to a new project in the <span class='public-file-name'>Library</span> directory in your <a href='/docs/v2/local-platform/initialization/workspace'>workspace</a>.</p>
+    <p>The template library files are added to a new project in the <span class='public-file-name'>Library</span> directory in your <a href='/docs/v2/local-platform/development-environment/organization-workspaces'>organization workspace</a>.</p>
     <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/local-lab-explorer-icon.jpg" alt="Local Platform explorer icon"> <span class="icon-name">Explorer</span> icon.</li>
     <li>In Explorer panel, open the <span class="public-file-name python">Library.py</span><span class="public-file-name csharp">Library.cs</span> file and implement your library.</li>
 </ol>
