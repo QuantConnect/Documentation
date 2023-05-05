f9258671757a8891925dbd7cc0cb7c5f13302f49
Web Editor <web.editor@quantconnect.com>
2023-05-05 18:33:59 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/08 Projects/03 Files/05 Add Directories.php
+++ 02 Local Platform/08 Projects/03 Files/05 Add Directories.php
@@ -1,7 +1,7 @@
 <p>You can organize the code and notebook files in your project into directories to make navigating them easier. For example, if you have multiple <a href='/docs/v2/writing-algorithms/algorithm-framework/alpha/key-concepts'>Alpha models</a> in your strategy, you can create an <span class='public-directory-name'>alphas</span> directory in your project to hold a file for each Alpha model.</p>
 
 <?php
-$videoLink = "https://cdn.quantconnect.com/i/tu/qc-desktop-add-directory.gif";
+$videoLink = "https://cdn.quantconnect.com/i/tu/local-platform-add-directory-py.gif";
 $openProjectLink = "/docs/v2/local-platform/projects/getting-started#04-Open-Projects";
 include(DOCS_RESOURCES."/projects/files/add-directories.php");
 ?>
