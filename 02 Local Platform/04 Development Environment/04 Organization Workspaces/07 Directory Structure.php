eb610b46f587bd27c4cd72e3fad49dbb8a163b17
Web Editor <web.editor@quantconnect.com>
2023-05-05 17:23:21 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/04 Development Environment/04 Organization Workspaces/07 Directory Structure.php
+++ 02 Local Platform/04 Development Environment/04 Organization Workspaces/07 Directory Structure.php
@@ -1,7 +1,7 @@
 <p>
-    The workspace directory initially has following structure:
+    The organization workspace directory initially has following structure:
 </p>
 
 <? include(DOCS_RESOURCES."/cli/init/structure.php"); ?>
 
-<p>As you add <a href='/docs/v2/local-platform/projects/getting-started'>projects</a>, the project files are added to your workspace directory. If you create and use <a href='/docs/v2/local-platform/projects/shared-libraries'>shared libraries</a> in your projects, the library files are added to a <span class='public-directory-name'>Library</span> directory in your workspace.</p> 
+<p>As you add <a href='/docs/v2/local-platform/projects/getting-started'>projects</a>, the project files are added to your organization workspace directory. If you create and use <a href='/docs/v2/local-platform/projects/shared-libraries'>shared libraries</a> in your projects, the library files are added to a <span class='public-directory-name'>Library</span> directory in your organization workspace.</p> 
