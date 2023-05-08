833ed9cee111163c6b63582271c8fbbca48b4de6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-08 17:42:03 -0600

    Update 03 Launch Debugger.php

--- 01 Cloud Platform/08 Backtesting/05 Debugging/03 Launch Debugger.php
+++ 01 Cloud Platform/08 Backtesting/05 Debugging/03 Launch Debugger.php
@@ -1,8 +1,1 @@
-<p>Follow these steps to launch the debugger:</p>
-<ol>
-    <li><a href="/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects">Open the project</a> you want to debug.</li>
-    <li>In your project's code files, add at least one breakpoint.</li>
-    <li>Click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/debug-icon.png' alt="Debug icon"> <span class='icon-name'>Debug</span> icon.</li>
-</ol>
-
-<p>If the Run and Debug panel is not open, it opens when the first breakpoint is hit.</p>
+<? include(DOCS_RESOURCES."/debugging/launch-backtest-debugger.php"); ?>
