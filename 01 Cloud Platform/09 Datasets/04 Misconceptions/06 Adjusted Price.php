a8028619d6bede9706118303465827984ad5fc2c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-08 11:02:22 -0600

    Update and rename 06 Adjusted Price.php to 06 Adjusted Prices.php

--- 01 Cloud Platform/09 Datasets/04 Misconceptions/06 Adjusted Price.php
+++
@@ -1,5 +0,0 @@
-<p>By default, LEAN adjusts US Equity data for splits and dividends to produce a smooth price curve.</p>
-
-<? include(DOCS_RESOURCES."/datasets/data-normalization.php"); ?>
-
-<p>Backtest differences occur when you run backtests before and after splits and dividends since the adjusted prices will be different. The difference can be significant in large universes because of multiple corporate actions and the cummulative effect of orders with a small difference.</p>
