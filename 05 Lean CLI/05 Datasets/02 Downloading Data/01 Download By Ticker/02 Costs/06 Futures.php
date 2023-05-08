163402654d4d9ffc1891841af9d9b3cf526a8271
derekmelchin <melchin.derek@gmail.com>
2023-05-08 14:34:08 -0600

    Add Futures and Index Options

---
+++ 05 Lean CLI/05 Datasets/02 Downloading Data/01 Download By Ticker/02 Costs/06 Futures.php
@@ -0,0 +1,114 @@
+<p>Futures algorithms require some data from the <a href='https://www.quantconnect.com/datasets/algoseek-us-futures'>US Futures</a> dataset. The US Futures dataset is available in several resolutions. The resolution you need depends on the US Future subscriptions you create in your algorithm and the resolution of data you get in <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/historical-data/history-requests'>history requests</a>. The following table describes the file format and costs of each resolution:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Resolution</th>
+            <th>File Format</th>
+            <th>Cost per file</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Tick</td>
+            <td>One file per ticker per trading day</td>
+            <td>6 QCC = $0.06 USD</td>
+        </tr>
+        <tr>
+            <td>Second</td>
+            <td>One file per ticker per trading day</td>
+            <td>5 QCC = $0.05 USD</td>
+        </tr>
+        <tr>
+            <td>Minute</td>
+            <td>One file per ticker per trading day</td>
+            <td>5 QCC = $0.05 USD</td>
+        </tr>
+        <tr>
+            <td>Hour</td>
+            <td>One file per ticker</td>
+            <td>300 QCC = $3 USD</td>
+        </tr>
+        <tr>
+            <td>Daily</td>
+            <td>One file per ticker</td>
+            <td>100 QCC = $1 USD</td>
+        </tr>
+    </tbody>
+</table>
+
+<p>If you want <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/universes/futures#12-Continous-Contracts'>continuous contracts</a> in your algorithm, you also need the <a href='https://www.quantconnect.com/datasets/quantconnect-us-futures-security-master'>US Futures Security Master</a> dataset. The following table shows the cost of an annual subscription to the US Futures Security Master for each organization tier:</p>
+
+<?php include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
+
+<p>For example, the following algorithm subscribes to minute resolution data for a universe of ES Futures contracts and creates a continuous contract:</p>
+
+<div class="section-example-container">
+<pre class="csharp">namespace QuantConnect.Algorithm.CSharp
+{
+    public class USFuturesDataAlgorithm : QCAlgorithm
+    {
+        public override void Initialize()
+        {
+            SetStartDate(2020, 1, 1);
+            SetEndDate(2021, 1, 1);
+            var future = AddFuture(Futures.Indices.SP500EMini,
+                dataNormalizationMode: DataNormalizationMode.BackwardsRatio,
+                dataMappingMode: DataMappingMode.OpenInterest,
+                contractDepthOffset: 0
+            );
+            future.SetFilter(0, 90);
+        }
+    }
+}
+</pre>
+<pre class="python">class USFuturesDataAlgorithm(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)
+        self.SetEndDate(2021, 1, 1)
+        future = self.AddFuture(Futures.Indices.SP500EMini,
+                                dataNormalizationMode = DataNormalizationMode.BackwardsRatio,
+                                dataMappingMode = DataMappingMode.OpenInterest,
+                                contractDepthOffset = 0)
+        future.SetFilter(0, 90)</pre>
+</div>
+
+<p>The following table shows the data cost of the preceding algorithm on the Quant Researcher tier:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Dataset</th>
+            <th>Package</th>
+            <th>Initial Cost</th>
+            <th>Ongoing Cost</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>US Futures Security Master</td>
+            <td>Download On Premise</td>
+            <td>$600 USD</td>
+            <td>$0 USD/year</td>
+        </tr>
+        <tr>
+            <td>US Futures</td>
+            <td>Minute Download</td>
+            <td>1 ticker over 252 trading days
+                <br>=&gt; 252 files
+                <br>
+                <br>252 files @ 5 QCC/file
+                <br>=&gt; 252 * 5 QCC
+                <br>= 1,260 QCC 
+                <br>= $12.60 USD
+            </td>
+            <td>1 ticker
+                <br>=&gt; 1 file/day
+                <br>
+                <br>1 file/day @ 5 QCC/file
+                <br>=&gt; 5 QCC/day
+                <br>= $0.05 USD/day
+            </td>
+        </tr>
+    </tbody>
+</table>
