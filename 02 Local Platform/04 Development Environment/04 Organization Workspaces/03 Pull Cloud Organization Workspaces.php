2ab392e91c0f8b5a689617f467bc2c361796f35d
derekmelchin <melchin.derek@gmail.com>
2023-05-09 10:16:43 -0600

    Fix broken links

--- 02 Local Platform/04 Development Environment/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
+++ 02 Local Platform/04 Development Environment/04 Organization Workspaces/03 Pull Cloud Organization Workspaces.php
@@ -1,6 +1,6 @@
 <p>Follow these steps to pull one of your <a href='/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>cloud organization workspaces</a> and set it as your local organization workspace:</p>
 <ol>
-    <li><a href="/docs/v2/local-platform/initialization/accounts#02-Log-In">Log in to Local Platform</a>.</li>
+    <li><a href="/docs/v2/local-platform/development-environment/authentication#02-Log-In">Log in to Local Platform</a>.</li>
     <li>In the left navigation menu, click the <img class="inline-icon" src="https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg" alt="VS Code QuantConnect icon"> <span class="icon-name">QuantConnect</span> icon.</li>
     <li>In the Select Workspace panel, click <span class="button-name">Pull Organization Workspace</span>.</li>
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/select-workspace.png" alt="Select workspace">
@@ -11,6 +11,6 @@
     <img class="docs-image" src="https://cdn.quantconnect.com/i/tu/pull-cloud-workspaces-2.png" alt="Pull cloud workspace">
 </ol>
 
-<p>It takes a few minutes to create a new organization workspace directory and populate it with the <a href='/docs/v2/local-platform/initialization/organization-workspaces#07-Directory-Structure'>the initial file structure</a>. After the organization workspace is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
+<p>It takes a few minutes to create a new organization workspace directory and populate it with the <a href='/docs/v2/local-platform/development-environment/organization-workspaces#07-Directory-Structure'>the initial file structure</a>. After the organization workspace is populated with the initial file structure, it pulls <a href='https://www.quantconnect.com/docs/v2/cloud-platform/projects/getting-started#02-View-All-Projects'>your cloud project files</a>.</p>
 
 <? include(DOCS_RESOURCES."/cli/init/wsl.html"); ?>
