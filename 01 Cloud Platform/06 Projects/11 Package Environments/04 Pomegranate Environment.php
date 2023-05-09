572b8e10f6b3f2695c35f4302a60701ad4d06f39
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-09 12:40:44 -0600

    Update 04 Pomegranate Environment.php

--- 01 Cloud Platform/06 Projects/11 Package Environments/04 Pomegranate Environment.php
+++ 01 Cloud Platform/06 Projects/11 Package Environments/04 Pomegranate Environment.php
@@ -1,4 +1,4 @@
-<p class="csharp">The Pomegranate environment cannot be selected on C# projects.</p>
+<p class="csharp">If you use C#, this environment isn't available.</p>
 <p class="python">The Pomegranate environment supports the following libraries:</p>
 
 <div class="python">
