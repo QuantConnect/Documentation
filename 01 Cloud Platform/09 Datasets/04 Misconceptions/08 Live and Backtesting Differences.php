1f52fd2eed4f832ed54021a29a888d7e6e8b4f65
Web Editor <web.editor@quantconnect.com>
2023-05-10 11:45:59 -0400

    Automated push from server made by Derek Melchin

--- 01 Cloud Platform/09 Datasets/04 Misconceptions/08 Live and Backtesting Differences.php
+++ 01 Cloud Platform/09 Datasets/04 Misconceptions/08 Live and Backtesting Differences.php
@@ -1,3 +1,3 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/datasets/live-and-backtest-differences.html"); ?>
 
-<p>There will be delay for data available to be backtested. Live feed data is available for backtesting after 24-48 hours normally. Consider using <a href="https://www.quantconnect.com/docs/v2/cloud-platform/live-trading/brokerages/quantconnect-paper-trading">Paper live trading</a> if closely monitoring is needed.</p>
+<p>There is a delay in when new live data is available for backtesting. It's normally available after 24-48 hours. If you need to closely monitor new data, use <a href="/docs/v2/cloud-platform/live-trading/brokerages/quantconnect-paper-trading">live paper trading</a>.</p>
