d62c6d441559e23da6138aae9d95b9886fdd3f35
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-10 17:35:06 -0600

    Update 04 Rename Libraries.php

--- 05 Lean CLI/06 Projects/08 Libraries/02 Project Libraries/04 Rename Libraries.php
+++ 05 Lean CLI/06 Projects/08 Libraries/02 Project Libraries/04 Rename Libraries.php
@@ -5,7 +5,7 @@
     <li>In the <span class='public-directory-name'>Library</span> directory, rename the library project.</li>
     <? include(DOCS_RESOURCES."/cli/python-library-naming-rules.html"); ?>
     <li>If you have a copy of the library in QuantConnect Cloud, open a terminal in your organization workspace and push the library project.</li>
-    <div class="python cli section-example-container">
+    <div class="cli section-example-container">
         <pre>$ lean cloud push --project "Library/MySpecialLibrary"
 [1/1] Pushing 'Library\MySpecialLibrary'
 Renaming project in cloud from 'Library/MyLibrary' to 'Library/MySpecialLibrary'
