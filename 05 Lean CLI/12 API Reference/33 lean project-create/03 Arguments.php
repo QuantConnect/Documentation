f0630d928516c680e5d7371a518f50a374af18cb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-05-10 16:48:13 -0600

    Update 03 Arguments.php

--- 05 Lean CLI/12 API Reference/33 lean project-create/03 Arguments.php
+++ 05 Lean CLI/12 API Reference/33 lean project-create/03 Arguments.php
@@ -15,6 +15,7 @@
             <td>
                 The name of the project to create. This name may contain slashes to create a project in a subdirectory.
                 <?php echo file_get_contents(DOCS_RESOURCES."/cli/project-name-rules.html");?>
+                If the project is a Python library, the library name can only contain letters (a-z), numbers (0-9), and underscores (_). Python library names can't contain spaces or start with a number.
             </td>
         </tr>
     </tbody>
