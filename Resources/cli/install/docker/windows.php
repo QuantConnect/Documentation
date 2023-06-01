2fd7dd5c798ade7ff077c07d99be79b2aa57e112
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-01 16:52:18 -0600

    Update windows.php

--- Resources/cli/install/docker/windows.php
+++ Resources/cli/install/docker/windows.php
@@ -8,8 +8,9 @@
 <p>Follow these steps to install Docker:</p>
 <ol>
     <li>Follow the <a href='https://docs.docker.com/desktop/install/windows-install/' rel='nofollow' target='_blank'>Install Docker Desktop on Windows</a> tutorial in the Docker documentation.</li>
-    <p>As you install docker, enable WSL 2 features. After you install Docker and restart your computer, if Docker prompts you that the WSL 2 installation is incomplete, follow the instructions in the dialog shown by Docker to finish the WSL 2 installation.</p>
+    <p>As you install docker, enable WSL 2 features.</p>
     <li>Restart your computer.</li>
+    <li>If Docker prompts you that the WSL 2 installation is incomplete, follow the instructions in the dialog shown by Docker to finish the WSL 2 installation.</li>
     <li>Open PowerShell with adminstrator privledges and run:</li>
     <div class="cli section-example-container">
         <pre>$ wsl --update</pre>
