3744a3d714e6e414d5df8d072e99ab635e38da0b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-01 16:42:41 -0600

    Update 03 Install Docker.php

--- 02 Local Platform/02 Installation/03 Install on Linux/03 Install Docker.php
+++ 02 Local Platform/02 Installation/03 Install on Linux/03 Install Docker.php
@@ -1,4 +1,4 @@
 <? 
 include(DOCS_RESOURCES."/cli/install/docker/introduction.php");
-include(DOCS_RESOURCES."/cli/install/docker/linux.php"); 
+include(DOCS_RESOURCES."/cli/install/docker/linux.html"); 
 ?>
