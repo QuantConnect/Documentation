6bef5cabe93dafa3cfc56c07e5db3ee9e59c474d
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-06-01 23:42:58 +0100

    Trader Order Limitation Handling
    Closes #1316

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/11 Tradier/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/11 Tradier/03 Orders.php
@@ -1,4 +1,11 @@
 <p>The <code>TradierBrokerageModel</code> supports several order types, order properties, and most order updates.</p>
 
-<?php include(DOCS_RESOURCES."/brokerages/tradier/orders.php"); ?>
+<? include(DOCS_RESOURCES."/brokerages/tradier/orders.php"); ?>
 
+<p>The <code>TradierBrokerageModel</code> validates orders before submission for the following cases.</p>
+
+<ol>
+    <li><code>ShortOrderIsGtc</code> - You cannot place short stock orders with <code>GoodTilCanceled</code></li>
+    <li><code>SellShortOrderLastPriceBelow5</code> - Sell Short order cannot be placed for stock priced below $5</li>
+    <li><code>IncorrectOrderQuantity</code> - Quantity should be between 1 and 10,000,000 shares</li>
+</ol>
