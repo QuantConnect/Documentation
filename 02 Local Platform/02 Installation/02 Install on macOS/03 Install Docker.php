db5c7e460a7a989d4d8a9dcada5d493abd883c91
Web Editor <web.editor@quantconnect.com>
2023-06-01 18:27:48 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/02 Installation/02 Install on macOS/03 Install Docker.php
+++ 02 Local Platform/02 Installation/02 Install on macOS/03 Install Docker.php
@@ -1,4 +1,5 @@
 <? 
 include(DOCS_RESOURCES."/cli/install/docker/introduction.php");
-include(DOCS_RESOURCES."/cli/install/docker/mac.php"); 
 ?>
+
+<p>To install Docker, see <a href='https://docs.docker.com/desktop/install/mac-install/' rel='nofollow' target='_blank'>Install Docker Desktop on Mac</a> in the Docker documentation.</p>
