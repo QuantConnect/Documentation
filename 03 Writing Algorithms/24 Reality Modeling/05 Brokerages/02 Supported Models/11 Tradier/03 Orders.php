8dcf269e4ea1da40736e25cdcdac8b21e41f377d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-02 08:42:12 -0600

    Update 03 Orders.php

--- 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/11 Tradier/03 Orders.php
+++ 03 Writing Algorithms/24 Reality Modeling/05 Brokerages/02 Supported Models/11 Tradier/03 Orders.php
@@ -2,10 +2,27 @@
 
 <? include(DOCS_RESOURCES."/brokerages/tradier/orders.php"); ?>
 
-<p>The <code>TradierBrokerageModel</code> validates orders before submission for the following cases.</p>
+<p>The <code>TradierBrokerageModel</code> validates your orders  before submission for the following cases.</p>
 
-<ol>
-    <li><code>ShortOrderIsGtc</code> - You cannot place short stock orders with <code>GoodTilCanceled</code></li>
-    <li><code>SellShortOrderLastPriceBelow5</code> - Sell Short order cannot be placed for stock priced below $5</li>
-    <li><code>IncorrectOrderQuantity</code> - Quantity should be between 1 and 10,000,000 shares</li>
-</ol>
+<table class="qc-table table" id='backtesting-nodes-table'>
+    <thead>
+        <tr>
+            <th>Error</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>ShortOrderIsGtc</code></td>
+            <td>You can't short sell with the <code>GoodTilCanceled</code> <a href='/docs/v2/writing-algorithms/trading-and-orders/order-properties#03-Time-In-Force'>time in force</a></td>
+        </tr>
+        <tr>
+            <td><code>SellShortOrderLastPriceBelow5</code></td>
+            <td>You can't short sell stock priced below $5</td>
+        </tr>
+        <tr>
+            <td><code>IncorrectOrderQuantity</code></td>
+            <td>The order quantity must be between 1 and 10,000,000 shares</td>
+        </tr>
+    </tbody>
+</table>
