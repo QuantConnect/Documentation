90a2e7dd7ec741009846dde91054e475f9058b5d
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-06-07 23:08:11 +0100

    Document Position Group Buying Power Model
    Closes #1339

--- 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/08 Default Behavior.php
+++ 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/08 Default Behavior.php
@@ -1,3 +1,5 @@
 <p>The brokerage model of your algorithm automatically sets the buying power model of each security. The default brokerage model is the <code>DefaultBrokerageModel</code>, which sets the buying power model based on the asset class of the security. The following table shows the default buying power model of each asset class:</p>
 
 <?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-models.html"); ?>
+
+<p>You cannot change the position group buying power models.</p>
--- Resources/reality-modeling/disable-buying-power.php
+++ Resources/reality-modeling/disable-buying-power.php
@@ -1,11 +1,15 @@
-<p>In live trading, orders can pass the validations associated with buying power in LEAN and let your brokerage decide to accept or reject them. To disable the validations of the <a href='/docs/v2/writing-algorithms/reality-modeling/buying-power#07-Default-Behavior'>default buying power model</a>, use the <code>NullBuyingPowerModel</code>.</p>
+<p>In live trading, orders can pass the validations associated with buying power in LEAN and let your brokerage decide to accept or reject them. To disable the validations of the <a href='/docs/v2/writing-algorithms/reality-modeling/buying-power#08-Default-Behavior'>default buying power model</a>, use the <code>NullBuyingPowerModel</code>.</p>
 
-<p>To set the <code>NullBuyingPowerModel</code> for a security subscription, call the <code>SetBuyingPowerModel</code> method with the <code>BuyingPowerModel.Null</code> argument.</p>
+<p>The <a href='/docs/v2/writing-algorithms/reality-modeling/buying-power#08-Default-Behavior'>default position group buying power models</a> are helpful for option trading strategies. However, it can be counterproductive if the option strategy is not supported. To disable the validations of the <a href='/docs/v2/writing-algorithms/reality-modeling/buying-power#08-Default-Behavior'>default position group buying power model</a>, use the <code>NullSecurityPositionGroupModel</code>.
+
+<p>To set the <code>NullBuyingPowerModel</code> for a security subscription, call the <code>SetBuyingPowerModel</code> method with the <code>BuyingPowerModel.Null</code> argument. To set the <code>NullSecurityPositionGroupModel</code> for the portfolio, call the <code>SetPositions</code> with the <code>SecurityPositionGroupModel.Null</code> argument.</p>
 <div class="section-example-container">
 <pre class="csharp">// In Initialize
+Portfolio.SetPositions(SecurityPositionGroupModel.Null);
 var equity = AddEquity("SPY");
 equity.SetBuyingPowerModel(BuyingPowerModel.Null);</pre>
 <pre class="python"># In Initialize
+self.Portfolio.SetPositions(SecurityPositionGroupModel.Null)
 equity = self.AddEquity("SPY")
 equity.SetBuyingPowerModel(BuyingPowerModel.Null)</pre>
 </div>
@@ -13,7 +17,7 @@ equity.SetBuyingPowerModel(BuyingPowerModel.Null)</pre>
 
 <p>You can also set the asset <code>NullBuyingPowerModel</code> in a security initializer. In order to set the buying power of securities in the security initializer, call <code>SetSecurityInitializer</code> before you create security subscriptions and after you call <code>SetBrokerageModel</code>.<br></p>
 
-<?php
+<?
 $overwriteCodePy = "security.SetBuyingPowerModel(BuyingPowerModel.Null)";
 $overwriteCodeC = "security.SetBuyingPowerModel(BuyingPowerModel.Null);";
 $comment = "the security buying power";
