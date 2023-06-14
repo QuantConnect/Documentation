c954e3f79c4fe55240d64a708c659688d6c06ab5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-14 14:22:22 -0600

    Update 13 Project Files.php

--- 02 Local Platform/10 Backtesting/04 Results/13 Project Files.php
+++ 02 Local Platform/10 Backtesting/04 Results/13 Project Files.php
@@ -2,3 +2,9 @@
 $pageName = "backtest";
 include(DOCS_RESOURCES."/algorithm-results/project-files.php"); 
 ?>
+
+<p>If you ran a local backtest, the project files are also available in the following locations:</p>
+<ul>
+    <li>The <span class='public-file-name'>Project / backtests / &lt;unixTimestamp&gt; / code</span> directory from your <a href='/docs/v2/local-platform/backtesting/results#15-View-Local-Result-Files'>local result files</a> in Local Platform</li>
+    <li>The <span class='public-file-name'>&lt;projectName&gt; / backtests / &lt;unixTimestamp&gt; / code</span> directory in your <a href='/docs/v2/local-platform/development-environment/organization-workspaces'>organization workspace</a></li>
+</ul>
