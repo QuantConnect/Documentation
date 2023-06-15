f341ae24a5dbcca49012bce4738d9b74ad332353
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-15 15:14:55 -0600

    Update 02 Read Optimization Results.php

--- 04 Research Environment/10 Meta Analysis/03 Optimization Analysis/02 Read Optimization Results.php
+++ 04 Research Environment/10 Meta Analysis/03 Optimization Analysis/02 Read Optimization Results.php
@@ -5,7 +5,7 @@
     <pre class="python">optimization = api.ReadOptimization(optimization_id)</pre>
 </div>
 
-<p>To get the optimization Id, check the <a href='/docs/v2/cloud-platform/projects/ide#06-Cloud-Terminal'>Cloud Terminal</a> when you <a href='/docs/v2/cloud-platform/optimization/getting-started#04-View-All-Optimizations'>run an optimization in the Algorithm Lab</a>. An example optimization Id is O-696d861d6dbbed45a8442659bd24e59f.</p>
+<p>To get the optimization Id, check the <a href='/docs/v2/cloud-platform/optimization/results#07-Individual-Backtest-Results'>individual backtest results table on the optimization results page</a>. An example optimization Id is O-696d861d6dbbed45a8442659bd24e59f.</p>
 
 <p>The <code>ReadOptimization</code> method returns an <code>Optimization</code> object, which have the following attributes:</p>
 <div data-tree='QuantConnect.Api.Optimization'></div>
