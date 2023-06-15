34f2b98de82fe61af300a093583d3f3f98062c87
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-15 15:08:40 -0600

    Update deploy-steps.php

--- Resources/brokerages/interactive-brokers/deploy-steps.php
+++ Resources/brokerages/interactive-brokers/deploy-steps.php
@@ -1,8 +1,10 @@
     <li>Enter your IB user name, ID, and password.</li>
+    <? if ($localPlatform) { include(DOCS_RESOURCES."/brokerages/interactive-brokers/paper-trading-data-feeds.html"); } ?>
     <p>Your account details are not saved on QuantConnect.</p>
     <li>In the <span class="field-name">Weekly Restart UTC</span> field, enter the Coordinated Universal Time (UTC) time of when you want to receive notifications on Sundays to re-authenticate your account connection.</li>
     <p>For example, 4 PM UTC is equivalent to 11 AM Eastern Standard Time, 12 PM Eastern Daylight Time, 8 AM Pacific Standard Time, and 9 AM Pacific Daylight Time. To convert from UTC to a different time zone, see the <a rel='nofollow' target='_blank' href='https://www.utctime.net/utc-time-zone-converter'>UTC Time Zone Converter</a> on the UTC Time website.</p>
     <p>If your IB account has 2FA enabled, you receive a notification on your IB Key device every Sunday to re-authenticate the connection between IB and your live algorithm. If you don't re-authenticate before the timeout period, your algorithm quits executing.</p>
+    <? if ($cloudPlatform) { ?>
     <li>Click the <span class="field-name">Data Provider</span> field and then click one of the data feeds from the drop-down menu.</li>
     <p>The following table describes the available data feeds:</p>
     <table class="qc-table table">
@@ -28,4 +30,7 @@
         </tbody>
     </table>
 
-    <p>If you use IB data feeds and trade with a paper trading account, you need to share the data feed with your paper trading account. For instructions on sharing data feeds, see <a href="/docs/v2/cloud-platform/live-trading/brokerages/interactive-brokers#02-Account-Types">Account Types</a>.</p>
+    <? 
+        include(DOCS_RESOURCES."/brokerages/interactive-brokers/paper-trading-data-feeds.html"); 
+    } 
+    ?>
