6d9be9fb0d13e35156ad5cc32c2537c1e95948a6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-16 09:40:27 -0600

    Update 08 Default Behavior.php

--- 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/08 Default Behavior.php
+++ 03 Writing Algorithms/24 Reality Modeling/06 Buying Power/08 Default Behavior.php
@@ -1,5 +1,3 @@
 <p>The brokerage model of your algorithm automatically sets the buying power model of each security. The default brokerage model is the <code>DefaultBrokerageModel</code>, which sets the buying power model based on the asset class of the security. The following table shows the default buying power model of each asset class:</p>
 
-<?php echo file_get_contents(DOCS_RESOURCES."/brokerages/default-buying-power-models.html"); ?>
-
-<p>You cannot change the position group buying power models.</p>
+<? include(DOCS_RESOURCES."/brokerages/default-buying-power-models.html"); ?>
