658f678f474f00bc03f8da318db2d938a3c730a8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-20 12:22:53 -0600

    Create 04 Runtime Statistics.php

---
+++ 03 Writing Algorithms/40 Live Trading/41 Charting and Logging/04 Runtime Statistics.php
@@ -0,0 +1,7 @@
+<p>Runtime statistics show the performace of your algorithm at a single moment in time.</p>
+
+<? 
+$pageName = "live trading";
+include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
+include(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.html"); 
+?>
