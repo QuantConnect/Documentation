1dc251e0b9cafc7a87627130bb9a39cfccef341b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 11:19:28 -0600

    Update 03 Runtime Statistics.php

--- 02 Local Platform/10 Backtesting/04 Results/03 Runtime Statistics.php
+++ 02 Local Platform/10 Backtesting/04 Results/03 Runtime Statistics.php
@@ -7,6 +7,6 @@ $pageName = "backtest";
 include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
 ?>
 
-<p>To view the runtime statistics data in JSON format, see <a href='/docs/v2/local-platform/backtesting/results#15-View-Local-Result-Files'>View Local Result Files</a> or <a href='/docs/v2/local-platform/backtesting/results#16-Download-Cloud-Result-Files'>Download Cloud Result Files</a>.</p>
+<p>To view the runtime statistics data in JSON format, see <a href='/docs/v2/local-platform/backtesting/results#15-View-Result-Files'>View Result Files</a>.</p>
 
 <p>To add a custom runtime statistic, see <a href='/docs/v2/writing-algorithms/logging#06-Runtime-Statistics'>Runtime Statistics</a>.</p>
