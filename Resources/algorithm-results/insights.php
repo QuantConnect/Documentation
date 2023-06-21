cf936ff09d17f89eb69151ee21034152e2ffc408
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 11:30:56 -0600

    Update insights.php

--- Resources/algorithm-results/insights.php
+++ Resources/algorithm-results/insights.php
@@ -7,10 +7,5 @@
 <p>To view the insights in JSON format, open the <?=$pageName?> result page, click the <span class='tab-name'>Insights</span> tab, and then click <span class='button-name'>Download Insights</span>. The timestamps in the CSV file are based in Coordinated Universal Time (UTC).</p>
 
 <? if ($localPlatform) { ?>
-<p>If you run a local backtest, the JSON file is also available in the following locations:</p>
-
-<ul>
-    <li>The <span class='public-file-name'>Project / backtests / &lt;unixTimestamp&gt; / &lt;algorithmId&gt;-alpha-insights.json</span> file from your <a href='/docs/v2/local-platform/backtesting/results#15-View-Local-Result-Files'>local result files</a> in Local Platform</li>
-    <li>The <span class='public-file-name'>&lt;projectName&gt; / backtests / &lt;unixTimestamp&gt; / &lt;algorithmId&gt;-alpha-insights.txt</span> file from your <a href='/docs/v2/local-platform/development-environment/organization-workspaces'>organization workspace</a></li>
-</ul>
+<p>If you run a local backtest, the JSON file is also available in the <span class='public-file-name'>Project / backtests / &lt;unixTimestamp&gt; / &lt;algorithmId&gt;-alpha-insights.json</span> file from your <a href='/docs/v2/local-platform/backtesting/results#15-View-Result-Files'>local result files</a>.</p>
 <? } ?>
