339058dfb398b938e1aa3cc0463881181406beea
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 16:04:12 -0600

    Update orders.php

--- Resources/algorithm-results/orders.php
+++ Resources/algorithm-results/orders.php
@@ -15,7 +15,7 @@
 <p>The timestamps in the Order Summary table are based in Eastern Time (ET).</p>
 
 <h4>Access the Order Summary CSV</h4>
-<p>To view the orders data in CSV format, open the <?=$pageName?> results page, click the <span class='tab-name'>Orders</span> tab, and then click <span class='button-name'>Download Orders</span>. The content of the CSV file is the content displayed in the Orders Summary table when the table rows are collapsed. The timestamps in the CSV file are based in Coordinated Universal Time (UTC). <? if ($localPlatform) { ?>If you download the order summary CSV for a local backtest, the file is stored in <span class='public-file-name'>Project / backtests / &lt;unixTimestamp&gt; / orders.csv</span>.<? } ?></p>
+<p>To view the orders data in CSV format, open the <?=$pageName?> results page, click the <span class='tab-name'>Orders</span> tab, and then click <span class='button-name'>Download Orders</span>. The content of the CSV file is the content displayed in the Orders Summary table when the table rows are collapsed. The timestamps in the CSV file are based in Coordinated Universal Time (UTC). <? if ($localPlatform) { ?>If you download the order summary CSV for a local backtest, the file is stored in <span class='public-file-name'><a href='/docs/v2/local-platform/development-environment/organization-workspaces'>&lt;organizationWorkspace&gt;</a> / &lt;projectName&gt; / backtests / &lt;unixTimestamp&gt; / orders.csv</span>.<? } ?></p>
 
 <? if ($pageName == "backtest") { ?>
 <h4>Access the Orders JSON</h4>
