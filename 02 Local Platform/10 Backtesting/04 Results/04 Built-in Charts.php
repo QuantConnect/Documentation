440a379e2edc5bbc2936a259d77030bfb0d46cbc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 16:22:21 -0600

    Update 04 Built-in Charts.php

--- 02 Local Platform/10 Backtesting/04 Results/04 Built-in Charts.php
+++ 02 Local Platform/10 Backtesting/04 Results/04 Built-in Charts.php
@@ -1,3 +1,3 @@
 <? include(DOCS_RESOURCES."/backtesting/results/built-in-charts.php"); ?>
 
-<p>To view the chart data in JSON format, see <a href='/docs/v2/local-platform/backtesting/results#15-View-Result-Files'>View Result Files</a>.</p>
+<p>To view the chart data in JSON format, open the <span class='public-file-name'><a href='/docs/v2/local-platform/development-environment/organization-workspaces'>&lt;organizationWorkspace&gt;</a> / &lt;projectName&gt; / backtests / &lt;unixTimestamp&gt; / &lt;algorithmId&gt;.json</span> file and search for the <code>Charts</code> key.</p>
