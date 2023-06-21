d9078817a7385ffa6c917fffa00fce355d188b4d
Web Editor <web.editor@quantconnect.com>
2023-06-21 16:42:46 -0400

    Automated push from server made by Derek Melchin

--- 02 Local Platform/11 Research/01 Getting Started/01 Introduction.php
+++ 02 Local Platform/11 Research/01 Getting Started/01 Introduction.php
@@ -1,5 +1,5 @@
 <?php include(DOCS_RESOURCES."/landing-page-introductions/research-environment-2.php"); ?>
 
 <div class="highlight">
-    <p>Note: This chapter is an introduction to the Research Environment for the Algorithm Lab. For more comprehensive information on using research notebooks, see our dedicated <a href="/docs/v2/research-environment">Research Environment</a> documentation.</p>
+    <p>Note: This chapter is an introduction to the Research Environment for Local Platform. For more comprehensive information on using research notebooks, see our dedicated <a href="/docs/v2/research-environment">Research Environment</a> documentation.</p>
 </div>
