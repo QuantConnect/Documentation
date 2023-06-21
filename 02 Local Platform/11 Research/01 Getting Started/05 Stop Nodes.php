a9c6ab5004a1356c00b85eef09cd1f0054091449
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 14:45:04 -0600

    Update 05 Stop Nodes.php

--- 02 Local Platform/11 Research/01 Getting Started/05 Stop Nodes.php
+++ 02 Local Platform/11 Research/01 Getting Started/05 Stop Nodes.php
@@ -1,8 +1,7 @@
 <p>Follow these steps to stop a research node:</p>
 
 <ol>
-	<li>If you opened the research notebook on Local Platform, <a href='/docs/v2/local-platform/projects/files#07-Close-Files'>close the notebook file</a>.</li>
+    <li>If you opened the research notebook on Local Platform, <a href='/docs/v2/local-platform/projects/files#07-Close-Files'>close the notebook file</a>.</li>
     <li>In the <a href='/docs/v2/local-platform/development-environment/resource-management'>Resources panel</a>, click the <span class='button-name'>stop</span> button next to the research node you want to stop.</li>
-    <p>If you are using the <a href='/docs/v2/local-platform/key-concepts/deployment-targets#03-Cloud'>Cloud Platform deployment target</a>, you need <a href='/docs/v2/cloud-platform/organizations/members#08-Permissions'>stop node permissions</a> to stop research nodes.</p>
     <li>In the Visual Studio Code window, click <span class='button-name'>Yes</span>.</li>
 </ol>
