4bd0bec8b77d6f4f0aa0d4bdf3b3375ed06fe6a8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 12:16:23 -0600

    Update custom-charts.php

--- Resources/algorithm-results/custom-charts.php
+++ Resources/algorithm-results/custom-charts.php
@@ -18,8 +18,11 @@
 
 
 <? if ($backtest) { ?>
-<p>Custom charts are limited to 4,000 data points. Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. If you exceed the quota, the Cloud Terminal displays the following message:</p>
+<p>You can chart up to 4,000 data points. <? if ($cloudPlatform) { ?>Intensive charting requires hundreds of megabytes of data, which is too much to stream online or display in a web browser. <? } ?>If you exceed the quota, the terminal displays the following message:</p>
 <p><span class='error-messages'>Exceeded maximum points per chart, data skipped</span></p>
+<?     if ($localPlatform) { ?>
+    <p>To adjust the data point quota, open your <a href='/docs/v2/local-platform/development-environment/configuration#03-LEAN-Settings'>LEAN configuration file</a> and adjust the value of the <code>maximum-data-points-per-chart-series</code> key.</p>
+<?     } ?>
 <? } ?>
 
 <p>You can create up to 10 custom chart series per algorithm. If you exceed the quota, your algorithm stops executing and the Cloud Terminal displays the following message:</p>
