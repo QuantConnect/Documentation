d76c249636213519981eb32b6bff680dec25bb49
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-21 16:27:18 -0600

    Update 09 Reports.php

--- 02 Local Platform/10 Backtesting/04 Results/09 Reports.php
+++ 02 Local Platform/10 Backtesting/04 Results/09 Reports.php
@@ -1,3 +1,3 @@
 <? include(DOCS_RESOURCES."/backtesting/results/create-report.html"); ?>
 
-<p>If you create a report for a local backtest, the report is stored in the <span class='public-file-name'>Project / backtests / &lt;unixTimestamp&gt;</span> directory as <span class='public-file-name'>report.html</span> and <span class='public-file-name'>report.pdf</span>.</p>
+<p>If you create a report for a local backtest, the report is stored in the <span class='public-file-name'><a href='/docs/v2/local-platform/development-environment/organization-workspaces'>&lt;organizationWorkspace&gt;</a> / &lt;projectName&gt; / backtests / &lt;unixTimestamp&gt;</span> directory as <span class='public-file-name'>report.html</span> and <span class='public-file-name'>report.pdf</span>.</p>
