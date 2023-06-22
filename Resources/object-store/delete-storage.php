abf5cea879bb37f4011fdffbe64ac5a5f399f7c7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-22 14:49:42 -0600

    Update delete-storage.php

--- Resources/object-store/delete-storage.php
+++ Resources/object-store/delete-storage.php
@@ -4,4 +4,4 @@
     <pre class="python">self.ObjectStore.Delete(key)</pre>
 </div>
 
-<? if ($localPlatform) { ?> <p>You can also delete the files in the project's storage directory.</p> <? } ?>
+<? if ($localPlatform) { ?> <p>Alternative, delete the files in <span class="public-file-name"><a href="/docs/v2/local-platform/development-environment/organization-workspaces">&lt;organizationWorkspace&gt;</a> / &lt;projectName&gt; / storage</span> directory directory.</p> <? } ?>
