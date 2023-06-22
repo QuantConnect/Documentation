bd548b25320a333b70b93a1c10bc2736c63d6dcb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-22 16:05:47 -0600

    Update 08 Delete Storage.php

--- 02 Local Platform/14 Data Storage/08 Delete Storage.php
+++ 02 Local Platform/14 Data Storage/08 Delete Storage.php
@@ -1,1 +1,3 @@
 <? include(DOCS_RESOURCES."/object-store/delete-storage.php"); ?>
+
+<p>Alternatively, delete the files in <span class="public-file-name"><a href="/docs/v2/local-platform/development-environment/organization-workspaces">&lt;organizationWorkspace&gt;</a> / &lt;projectName&gt; / storage</span> directory.</p>
