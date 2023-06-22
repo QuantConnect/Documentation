4394778f9837431acf436134e9e3100c6274ccbf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-06-22 16:13:32 -0600

    Update 16 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Interactive Brokers/16 Deploy Live Algorithms.php
@@ -14,6 +14,7 @@
     <li>Configure the <span class="box-name">Automatically restart algorithm</span> setting.</li>
     <p>By enabling automatic restarts, the algorithm will use best efforts to restart the algorithm if it fails due to a runtime error. This can help improve the algorithm's resilience to temporary outages such as a brokerage API disconnection.</p>
     <li>Click <span class="button-name">Deploy</span>.</li>
+    <li>If your IB account has 2FA enabled, tap the notification on your IB Key device and then enter your pin.</li>
 </ol>
 
 <p>The deployment process can take up to 5 minutes. When the algorithm deploys, the <a href="/docs/v2/cloud-platform/live-trading/results">live results page</a> displays. If you know your brokerage positions before you deployed, you can verify they have been loaded properly by checking your equity value in the runtime statistics, your cashbook holdings, and your position holdings.</p>
