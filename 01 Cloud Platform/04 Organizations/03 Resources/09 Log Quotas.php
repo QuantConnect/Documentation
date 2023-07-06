f706fffa84b0d6c9109439899439c86348a00517
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-07-06 18:07:50 +0100

    Adds Information About Tick Quota Renewal

--- 01 Cloud Platform/04 Organizations/03 Resources/09 Log Quotas.php
+++ 01 Cloud Platform/04 Organizations/03 Resources/09 Log Quotas.php
@@ -1,7 +1,7 @@
 <p>Per our <a href='/terms'><span class='document-title'>Terms and Conditions</span></a>, you may not use the logs to export dataset information. The following table shows the amount of logs each organization tier can produce:</p>
 <?php echo file_get_contents(DOCS_RESOURCES."/quotas/logs.html"); ?>
 
-<p>If you delete a backtest or project that produced logs, your quotas aren't restored. Additionally, daily log quotas aren't fully restored at midnight. They are restored according to a 24-hour following window.</p>
+<p>If you delete a backtest or project that produced logs, your quotas aren't restored. Additionally, daily log quotas aren't fully restored at midnight. They are restored according to a 24-hour rolling window.</p>
 
 <p>The log files of each live trading project can store up to 100,000 lines for up to one year. If you log more than 100,000 lines or some lines become older than one year, we remove the oldest lines in the files so your project stays within the quota.</p>
 
