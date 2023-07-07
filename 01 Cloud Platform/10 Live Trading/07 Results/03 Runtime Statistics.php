699dfdb5ce4e1ae489372b7a8b1a0294d3c82a4f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-07-07 16:02:03 -0600

    Update 03 Runtime Statistics.php

--- 01 Cloud Platform/10 Live Trading/07 Results/03 Runtime Statistics.php
+++ 01 Cloud Platform/10 Live Trading/07 Results/03 Runtime Statistics.php
@@ -5,8 +5,7 @@
 <?php 
 $pageName = "live";
 include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
-
-echo file_get_contents(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.html"); 
+include(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.php"); 
 ?>
 
 <p>If you <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#05-Stop-the-Algorithm'>stop</a> and redeploy a live algorithm, the runtime statistics are reset.</p>
