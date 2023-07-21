226a2aef72930d2ee00a8b823a13964f9af6592a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-07-21 16:51:00 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
@@ -1,4 +1,9 @@
 <?
+ob_start();
+$extraText = "Enter a time on Sunday to receive the notification.";
+include(DOCS_RESOURCES."/brokerages/interactive-brokers/weekly-restarts.php");
+$weeklyRestartText = ob_get_clean();
+
 $brokerageDetails = "
 <li>Set up IB Key Security via IBKR Mobile. For instructions, see <a href='https://guides.interactivebrokers.com/iphone/log_in/ibkey.htm?tocpath=IB%20Key%20Security%20Protocol%7C_____0' target='_blank' rel='nofollow'>IB Key Security via IBKR Mobile</a> on the IB website.</li>
 
@@ -9,6 +14,14 @@ Username: trader777
 Account id: DU1234567
 Account password: ****************</pre>
 </div>
+</li>
+
+<li>Enter a weekly restart time that's convenient for you.</li>
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Weekly restart UTC time (hh:mm:ss) [21:00:00]: </pre>
+</div>
+{$weeklyRestartText}
 </li>
 
 <li>Enter whether you want to use the <a href='https://www.quantconnect.com/docs/v2/cloud-platform/live-trading/data-feeds/brokerage-data-feeds/interactive-brokers'>price data feed from Interactive Brokers</a> instead of the one from QuantConnect. Enabling this feature requires you to have active Interactive Brokers market data subscriptions for all data required by your algorithm.
