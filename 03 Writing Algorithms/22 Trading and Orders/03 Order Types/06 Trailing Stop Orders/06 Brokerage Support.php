9d7529638bb6a9569bd14b564def918ec2c7b519
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-08-01 23:19:05 +0100

    Suggest Buy and Hold with a Trailing Stop Bootcamp
    Following https://github.com/QuantConnect/Documentation/commit/d7eff3f5ee4d2240f8eca4ff23467716c2512229,
    we need to direct members to a workaround.

--- 03 Writing Algorithms/22 Trading and Orders/03 Order Types/06 Trailing Stop Orders/06 Brokerage Support.php
+++ 03 Writing Algorithms/22 Trading and Orders/03 Order Types/06 Trailing Stop Orders/06 Brokerage Support.php
@@ -3,3 +3,5 @@ $orderType = "trailing stop orders";
 $brokerageName = "QuantConnectBrokerage";
 include(DOCS_RESOURCES."/order-types/brokerage-restrictions.php");
 ?>
+
+<p>If your brokerage doesn't support them, you can implement a workaround using just stop market orders. To set a trailing stop loss order, see the <a href="/learning/task/87/introduction-video">Buy and Hold with a Trailing Stop</a> Bootcamp lesson.</p>
