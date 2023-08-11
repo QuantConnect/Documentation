c07e1c162adc8de9fc1ee423e9bf7d7fd8d402e7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-11 16:18:35 -0600

    Update data-normalization.php

--- Resources/securities/data-normalization.php
+++ Resources/securities/data-normalization.php
@@ -1,6 +1,6 @@
 <p>The data normalization mode defines how historical data is adjusted for <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-actions'>corporate actions</a>.<? if ($isWritingAlgorithms) { ?> The data normalization mode affects the data that LEAN passes to <code>OnData</code> and the data from <a href='/docs/v2/writing-algorithms/historical-data/history-requests'>history requests</a>. <? } ?> By default, LEAN adjusts US Equity data for splits and dividends to produce a smooth price curve, but the following data normalization modes are available:</p>
     
-<div data-tree='QuantConnect.DataNormalizationMode' data-fields='Raw,Adjusted,SplitAdjusted,TotalReturn'></div>
+<div data-tree='QuantConnect.DataNormalizationMode' data-fields='Raw,Adjusted,SplitAdjusted,TotalReturn<?=$research ? ",ScaledRaw" : "";?>'></div>
 
 <? include(DOCS_RESOURCES."/datasets/data-normalization.php"); ?>
 
