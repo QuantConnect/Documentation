8ccf7daba3bcdecd3db07af9646d3abcb4b943c8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-11 16:11:54 -0600

    Update 09 Data Normalization.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/09 Data Normalization.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/09 Data Normalization.php
@@ -1,4 +1,4 @@
 <? $additionalInformation = " By default, LEAN doesn't adjust Equity Options data for splits and dividends of their underlying."; 
 include(DOCS_RESOURCES."/securities/data-normalization-raw-only.php"); ?>
 
-<p>LEAN closes the open Options positions when the corporate actions of the underlying occur.</p>
+<p>If you hold an Option contract when a corporate action occurs for the underlying Equity, LEAN automatically closes your position.</p>
