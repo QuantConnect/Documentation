69ab3c2642abf10005180f67e1fb96d34fcf6cae
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-11 10:03:28 -0600

    Update 06 Brokerage Support.php

--- 03 Writing Algorithms/22 Trading and Orders/03 Order Types/06 Trailing Stop Orders/06 Brokerage Support.php
+++ 03 Writing Algorithms/22 Trading and Orders/03 Order Types/06 Trailing Stop Orders/06 Brokerage Support.php
@@ -4,4 +4,4 @@ $brokerageName = "QuantConnectBrokerage";
 include(DOCS_RESOURCES."/order-types/brokerage-restrictions.php");
 ?>
 
-<p>If your brokerage doesn't support them, you can implement a workaround using just stop market orders. To set a trailing stop loss order, see the <a href="/learning/task/87/introduction-video">Buy and Hold with a Trailing Stop</a> Bootcamp lesson.</p>
+<p>If your brokerage doesn't support them, you can implement a workaround with <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/stop-market-orders'>stop market orders</a>. For an example, see the <a href="/learning/task/87/introduction-video">Buy and Hold with a Trailing Stop</a> Bootcamp lesson.</p>
