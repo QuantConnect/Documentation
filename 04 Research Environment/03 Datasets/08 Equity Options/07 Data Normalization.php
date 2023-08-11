bb4e134574a210a44d599600e2ad3fc970416521
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-11 16:16:42 -0600

    Update 07 Data Normalization.php

--- 04 Research Environment/03 Datasets/08 Equity Options/07 Data Normalization.php
+++ 04 Research Environment/03 Datasets/08 Equity Options/07 Data Normalization.php
@@ -1,4 +1,2 @@
-<? $additionalInformation = " By default, LEAN does not adjusts Equity Options data for splits and dividends of their underlying."; 
+<? $additionalInformation = " By default, LEAN doesn't adjust Equity Options data for splits and dividends of their underlying."; 
 include(DOCS_RESOURCES."/securities/data-normalization-raw-only.php"); ?>
-
-<p>LEAN closes the open Options positions when the corporate actions of the underlying occur.</p>
