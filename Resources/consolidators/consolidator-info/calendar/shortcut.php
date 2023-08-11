b5f31c7dc417019001ed1ad6e65b5171f52644cf
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-11 13:59:38 -0600

    Update shortcut.php

--- Resources/consolidators/consolidator-info/calendar/shortcut.php
+++ Resources/consolidators/consolidator-info/calendar/shortcut.php
@@ -1,10 +1,8 @@
 <p>You can also use the <code>Consolidate</code> helper method to create calendar consolidators and register them for automatic updates.</p>
 
-<ul>
-    <div class='section-example-container'>
-		<pre class='csharp'>_consolidator = Consolidate<?= $consolidationHandlerType == "QuoteBar" ? "&lt;QuoteBar&gt;" : "" ?>(_symbol, Calendar.Weekly, <?=$this->shortCutTickTypeArg?>ConsolidationHandler);</pre>
-		<pre class='python'>self.consolidator = self.Consolidate(self.symbol, Calendar.Weekly, <?=$this->shortCutTickTypeArg?>self.consolidation_handler)</pre>
-	</div>
-</ul>
+<div class='section-example-container'>
+    <pre class='csharp'>_consolidator = Consolidate<?= $consolidationHandlerType == "QuoteBar" ? "&lt;QuoteBar&gt;" : "" ?>(_symbol, Calendar.Weekly, <?=$this->shortCutTickTypeArg?>ConsolidationHandler);</pre>
+    <pre class='python'>self.consolidator = self.Consolidate(self.symbol, Calendar.Weekly, <?=$this->shortCutTickTypeArg?>self.consolidation_handler)</pre>
+</div>
 
 <?php include(DOCS_RESOURCES."/consolidators/get-shortcut-text.php"); ?>
