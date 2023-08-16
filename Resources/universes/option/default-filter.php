0666cb0681ce7ee7508588a2a332289326748724
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-08-16 23:44:13 +0100

    Custom Annotation to Default Filter
    For Future Options, the annotiation informs that
    weeklies and non-standard contracts are not supported.

--- Resources/universes/option/default-filter.php
+++ Resources/universes/option/default-filter.php
@@ -1,5 +1,5 @@
 <ul>
-	<li>Standard type (exclude weeklys)</li>
+	<li>Standard type (<?=$annotation?>)</li>
 	<li>Within 1 strike price of the underlying asset price</li>
 	<li>Expire within 31 days</li>
 </ul>
