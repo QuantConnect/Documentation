2cf60c7c21364efc845e879df5c2293702240012
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 11:31:23 -0600

    Update cloud-terminal.php

--- Resources/projects/ide/cloud-terminal.php
+++ Resources/projects/ide/cloud-terminal.php
@@ -1,5 +1,11 @@
-<p>The terminal panel at the bottom of the IDE shows API messages, errors, and the logs from your algorithms.</p>
+<p>The console panel at the bottom of the IDE shows API messages, errors, and the logs from your algorithms.</p>
 <img class='docs-image' src='<?=$cloudTermianlImage?>' alt="Termianl interface">
+
+<? if ($cloudPlatform)  { ?>
+<p>The <span class='tab-name'>Ask Mia</span> tab of the panel is where you can interact with our AI assistant, Mia.</p>
+<img src="https://cdn.quantconnect.com/i/tu/ask-mia.png" class="docs-image"/>
+<p>Mia provides contextual assistance to most issues you may encounter when developing a strategy, including build errors, API methods, and best coding practices. It has been trained on hundreds of algorithms and thousands of documentation pages.</p>
+<? } ?>
 
 <p>The <span class='tab-name'>Problems</span> tab of the panel highlights the coding errors in your algorithms.</p>
 <img class='python docs-image' src='<?=$problemsImagePy?>' alt="Compilation error in code snippets">
