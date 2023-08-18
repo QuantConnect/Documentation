3fbcc25d7788c83fe0c2a6e1d9507ad5b1486796
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 11:52:17 -0600

    Update 06 Console.php

--- 01 Cloud Platform/06 Projects/04 IDE/06 Console.php
+++ 01 Cloud Platform/06 Projects/04 IDE/06 Console.php
@@ -1,5 +1,5 @@
 <?php 
-$cloudTermianlImage = "https://cdn.quantconnect.com/i/tu/cloud-terminal.jpg";
+$cloudTermianlImage = "https://cdn.quantconnect.com/i/tu/cloud-terminal-qc-cloud.png";
 $problemsImagePy = "https://cdn.quantconnect.com/i/tu/cloud-terminal-problems-1.jpg";
 $problemsImageC = "https://cdn.quantconnect.com/i/tu/cloud-terminal-problems-c.jpg";
 include(DOCS_RESOURCES."/projects/ide/cloud-terminal.php");
