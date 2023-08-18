2aab1df107c72cc58c0ab984c54218bcf72c539c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 11:30:11 -0600

    Update 11 AI Support.php

--- 01 Cloud Platform/04 Organizations/05 Support/11 AI Support.php
+++ 01 Cloud Platform/04 Organizations/05 Support/11 AI Support.php
@@ -1,5 +1,5 @@
 <p>Mia is an AI assistant in the <a href='/docs/v2/cloud-platform/projects/ide#06-Console'>Algorithm Lab console</a>. Mia provides contextual assistance to most issues you may encounter when developing a strategy. It has been trained on hundreds of algorithms and thousands of documentation pages.</p>
-<img src="https://cdn.quantconnect.com/i/tu/ask-mia.png" class="img-responsive"/>
+<img src="https://cdn.quantconnect.com/i/tu/ask-mia.png" class="docs-image"/>
 <p>The following table shows the number of AI tickets each support seat tier can open during a 1-month rolling period:</p>
 
 <? include(DOCS_RESOURCES."/quotas/ask-mia.html"); ?>
