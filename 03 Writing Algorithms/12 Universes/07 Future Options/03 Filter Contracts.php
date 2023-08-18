6a3b8cc7e1203618b9b42692e91db9795b12584c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 13:07:44 -0600

    Update 03 Filter Contracts.php

--- 03 Writing Algorithms/12 Universes/07 Future Options/03 Filter Contracts.php
+++ 03 Writing Algorithms/12 Universes/07 Future Options/03 Filter Contracts.php
@@ -1,6 +1,6 @@
 <p>By default, LEAN subscribes to the Option contracts that have the following characteristics:</p>
 
-<? $annotation = "weeklies and non-standard contracts are not supported"; include(DOCS_RESOURCES."/universes/option/default-filter.php"); ?>
+<? $annotation = "weeklies and non-standard contracts are not available"; include(DOCS_RESOURCES."/universes/option/default-filter.php"); ?>
 
 <p>To adjust the universe of contracts, set a filter. The filter usually runs at every <a href="/docs/v2/writing-algorithms/key-concepts/time-modeling/timeslices">time step</a> in your algorithm. When the filter selects a contract that isn't currently in your universe, LEAN adds the new contract data to the next <code>Slice</code> that it passes to the <code>OnData</code> method.</p>
 
