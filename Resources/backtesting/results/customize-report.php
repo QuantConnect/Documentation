1ff48d20ad255e9853d351208a75869e550c6cc5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 16:27:15 -0600

    Update customize-report.php

--- Resources/backtesting/results/customize-report.php
+++ Resources/backtesting/results/customize-report.php
@@ -1,6 +1,6 @@
 <h4>Customize the Report HTML</h4>
 
-<p>The <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/template.html'><span class='public-file-name'>Report / template.html</span></a> in the LEAN GitHub repository defines the stucture of the reports you generate. To override the HTML file, <?=$addHTMLFileInstructions?>. To include some of the information and charts that are in the default report, use the report keys in the <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/ReportKey.cs'><span class='public-file-name'>Report / ReportKey.cs</span></a> file in the LEAN GitHub repository. For example, to add the <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/glossary#27-Sharpe-ratio'>Sharpe ratio</a> of your backtest to the custom HTML file, use <code>{{$KPI-SHARPE}}</code>.</p>
+<p>The <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/template.html'><span class='public-file-name'>Report / template.html</span></a> file in the LEAN GitHub repository defines the stucture of the reports you generate. To override the HTML file, <?=$addHTMLFileInstructions?>. To include some of the information and charts that are in the default report, use the report keys in the <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/ReportKey.cs'><span class='public-file-name'>Report / ReportKey.cs</span></a> file in the LEAN GitHub repository. For example, to add the <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/glossary#27-Sharpe-ratio'>Sharpe ratio</a> of your backtest to the custom HTML file, use <code>{{$KPI-SHARPE}}</code>.</p>
 
 <p>To include the <a href='<?=$crisisEventLink?>'>crisis event plots</a> in your report, add the <code>{{$HTML-CRISIS-PLOTS}}</code> key and then define the structure of the individual plots inside of <code>&lt;!--crisis</code> and <code>crisis--&gt;</code>. Inside of this comment, you can utilize the <code>{{$TEXT-CRISIS-TITLE}}</code> and <code>{{$PLOT-CRISIS-CONTENT}}</code> keys. For example, the following HTML is the default format for each crisis plot:</p>
 
@@ -44,7 +44,7 @@ parameters--&gt</pre>
 
 <h4>Customize the Report CSS</h4>
 
-<p>The <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/css/report.css'><span class='public-file-name'>Report / css / report.css</span></a> in the LEAN GitHub repository defines the style of the reports you generate. To override the stylesheet, <?=$addCSSFileInstructions?>.</p>
+<p>The <a rel='nofollow' target='_blank' href='https://github.com/QuantConnect/Lean/blob/master/Report/css/report.css'><span class='public-file-name'>Report / css / report.css</span></a> file in the LEAN GitHub repository defines the style of the reports you generate. To override the stylesheet, <?=$addCSSFileInstructions?>.</p>
 
 <? if ($leanCLI) { ?>
 <p>To generate the report with your custom CSS file, run <code>lean report --css &lt;pathToCustomCSSFile&gt;</code>.</p>
