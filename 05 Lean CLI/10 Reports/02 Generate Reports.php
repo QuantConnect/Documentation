88eb17e18ca59442d547b5053cc576e36740d7fc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 16:24:01 -0600

    Update 02 Generate Reports.php

--- 05 Lean CLI/10 Reports/02 Generate Reports.php
+++ 05 Lean CLI/10 Reports/02 Generate Reports.php
@@ -53,5 +53,6 @@ Successfully generated report to './report.html'
 <? 
 $addHTMLFileInstructions = "add a new HTML file to your local machine. If you add it to your <a href='/docs/v2/lean-cli/initialization/organization-workspaces'>organization workspace</a>, don't name it <span class='public-file-name'>report.html</span> because that's the default name and location of the reports you generate";
 $addCSSFileInstructions = "add a new CSS file to your local machine";
+$crisisEventLink = "/docs/v2/lean-cli/reports#09-Crisis-Events";
 include(DOCS_RESOURCES."/backtesting/results/customize-report.php"); 
 ?>
