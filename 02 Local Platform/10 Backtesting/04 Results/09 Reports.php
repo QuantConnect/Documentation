9800425ae0158c42bdeb164cf63b0debbdc7ae1c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-08-18 16:25:44 -0600

    Update 09 Reports.php

--- 02 Local Platform/10 Backtesting/04 Results/09 Reports.php
+++ 02 Local Platform/10 Backtesting/04 Results/09 Reports.php
@@ -5,7 +5,6 @@
 <? 
 $addHTMLFileInstructions = "<a href='/docs/v2/local-platform/projects/files#04-Add-Files'>add a <span class='public-file-name'>report.html</span> file to your project</a>";
 $addCSSFileInstructions = "<a href='/docs/v2/local-platform/projects/files#04-Add-Files'>add a <span class='public-file-name'>report.css</span> file to your project</a>";
-include(DOCS_RESOURCES."/backtesting/results/customize-report.php"); 
-
+$crisisEventLink = "/docs/v2/cloud-platform/backtesting/report#08-Crisis-Events";
 include(DOCS_RESOURCES."/backtesting/results/customize-report.php"); 
 ?>
