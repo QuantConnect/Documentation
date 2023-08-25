20f34db5385c91b151f65ab1e45d95e2c675cbdf
Web Editor <web.editor@quantconnect.com>
2023-08-25 17:52:21 -0400

    Automated push from server made by Derek Melchin

--- 01 Cloud Platform/07 Research/01 Getting Started/04 Run Notebook Cells.php
+++ 01 Cloud Platform/07 Research/01 Getting Started/04 Run Notebook Cells.php
@@ -1,1 +1,3 @@
 <? include(DOCS_RESOURCES."/getting-started/research-environment/run-notebook-cells.html"); ?>
+
+<p>There is a 15 minute timeout period before the cells becomes unresponsive. If this occurs, restart the notebook to be able to run cells again.</p>
