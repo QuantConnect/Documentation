8f28fb4acf5c95114980e5d051b611d23b80850c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-01 16:31:33 -0600

    Update view-cloud-storage.php

--- Resources/object-store/view-cloud-storage.php
+++ Resources/object-store/view-cloud-storage.php
@@ -1,3 +1,7 @@
 <p>The Object Store page shows all the data your organization has in the Object Store. To view the page, log in to the Algorithm Lab and then, in the left navigation bar, click <span class='menu-name'>Organization > Object Store</span>.</p>
 
 <img class='docs-image' src='https://cdn.quantconnect.com/i/tu/view-cloud-platform-object-store.png' alt='Table of files and their sizes'> 
+
+<p>To view metadata of a file (including it's path, size, and a content preview), click one of the files in the table.</p>
+
+<img class='docs-image' src='https://cdn.quantconnect.com/i/tu/view-cloud-platform-object-store-metadata.png' alt='Panel of metadata'>
