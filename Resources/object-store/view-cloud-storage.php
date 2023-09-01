d2f5c14b6543c5db09fb523d53f43b4e4efdc532
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-01 16:33:16 -0600

    Update view-cloud-storage.php

--- Resources/object-store/view-cloud-storage.php
+++ Resources/object-store/view-cloud-storage.php
@@ -2,6 +2,6 @@
 
 <img class='docs-image' src='https://cdn.quantconnect.com/i/tu/view-cloud-platform-object-store.png' alt='Table of files and their sizes'> 
 
-<p>To view metadata of a file (including it's path, size, and a content preview), click one of the files in the table.</p>
+<p>To view the metadata of a file (including it's path, size, and a content preview), click one of the files in the table.</p>
 
 <img class='docs-image' src='https://cdn.quantconnect.com/i/tu/view-cloud-platform-object-store-metadata.png' alt='Panel of metadata'>
