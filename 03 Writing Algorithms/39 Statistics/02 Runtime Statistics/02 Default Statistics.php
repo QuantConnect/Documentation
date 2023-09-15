6426243119a62189d154d84062202b5cc68711d2
Derek Melchin <melchin.derek@gmail.com>
2023-09-15 16:55:50 -0600

    Move Runtime Statistics to its own page

---
+++ 03 Writing Algorithms/39 Statistics/02 Runtime Statistics/02 Default Statistics.php
@@ -0,0 +1,1 @@
+<? include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php"); ?>
--- 03 Writing Algorithms/40 Live Trading/41 Charting and Logging/04 Runtime Statistics.php
+++ 03 Writing Algorithms/40 Live Trading/41 Charting and Logging/04 Runtime Statistics.php
@@ -1,7 +1,5 @@
 <p>Runtime statistics show the performace of your algorithm at a single moment in time.</p>
 
-<? 
-$pageName = "live trading";
-include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
-include(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.php"); 
-?>
+<? include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php"); ?>
+
+<p>For more information about runtime statistics, see <a href='/docs/v2/writing-algorithms/statistics/runtime-statistics'></a>
