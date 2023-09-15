71a5b8dee6b904cc8e8f9fbfc2ba44d22a89c01a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-15 14:28:36 -0600

    Update 03 Runtime Statistics.php

--- 01 Cloud Platform/08 Backtesting/04 Results/03 Runtime Statistics.php
+++ 01 Cloud Platform/08 Backtesting/04 Results/03 Runtime Statistics.php
@@ -2,10 +2,7 @@
 
 <img class='docs-image' src="https://cdn.quantconnect.com/i/tu/runtime-statistics-live-1.png" alt="Backtest runtime statistics">
 
-<?php 
-$pageName = "backtest";
-include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
-?>
+<? include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php"); ?>
 
 <p>To download the runtime statistics data, see <a href='/docs/v2/cloud-platform/backtesting/results#15-Download-Results'>Download Results</a>.</p>
 
