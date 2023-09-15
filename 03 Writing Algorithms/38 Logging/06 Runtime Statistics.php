7224f1a9bfdb0fe640b556aa53e00ab00a007530
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-15 14:30:14 -0600

    Update 06 Runtime Statistics.php

--- 03 Writing Algorithms/38 Logging/06 Runtime Statistics.php
+++ 03 Writing Algorithms/38 Logging/06 Runtime Statistics.php
@@ -1,12 +1,6 @@
 <p>Runtime statistics show the performace of your algorithm at a single moment in time.</p>
 
-<?php 
-$pageName = "backtest";
+<? 
 include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php");
-?>
-
-<p>The capacity statistic is only available for backtests.</p>
-
-<?php 
 include(DOCS_RESOURCES."/algorithm-results/create-custom-runtime-statistic.php"); 
 ?>
