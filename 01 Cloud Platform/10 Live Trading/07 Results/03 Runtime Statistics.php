f4910a1a9c57144cafe608fe599dd26211bb7607
Derek Melchin <melchin.derek@gmail.com>
2023-09-15 17:00:29 -0600

    Fix links

--- 01 Cloud Platform/10 Live Trading/07 Results/03 Runtime Statistics.php
+++ 01 Cloud Platform/10 Live Trading/07 Results/03 Runtime Statistics.php
@@ -4,6 +4,6 @@
 
 <? include(DOCS_RESOURCES."/algorithm-results/runtime-statistics-table.php"); ?>
 
-<p>To add a custom runtime statistic, see <a href='/docs/v2/writing-algorithms/logging#06-Runtime-Statistics'>Runtime Statistics</a>.</p>
+<p>To add a custom runtime statistic, see <a href='/docs/v2/writing-algorithms/statistics/runtime-statistics#03-Add-Statistics'>Add Statistics</a>.</p>
 
 <p>If you <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#05-Stop-the-Algorithm'>stop</a> and redeploy a live algorithm, the runtime statistics are reset.</p>
