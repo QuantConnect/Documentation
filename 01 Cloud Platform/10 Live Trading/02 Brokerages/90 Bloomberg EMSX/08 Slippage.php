1a931da880dd738e33fc616f2d737ead1a685dbe
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 16:53:01 -0600

    Delete 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/08 Slippage.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/08 Slippage.php
+++
@@ -1,7 +0,0 @@
-<?php
-$brokerageName = "Terminal Link";
-$paperTradingSupported = false;
-include(DOCS_RESOURCES."/brokerages/slippage.php");
-?>
-
-<p>To view how we model Bloomberg™ Execution Management System slippage, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/quantconnect-paper-trading#05-Slippage">Slippage</a>.</p>
