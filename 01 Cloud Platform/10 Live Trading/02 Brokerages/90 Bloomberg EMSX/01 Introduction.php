4088feb52d017846912aa6c031870ae3d4522f81
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 17:11:42 -0600

    Update 01 Introduction.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
@@ -2,6 +2,6 @@
 
 <img class='docs-image' style='width: 50%' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg' alt="Terminal link icon">
 
-<p>QuantConnect can integrate with the Bloomberg™ Server API (SAPI) or Desktop API (DAPI) in different cloud environments. This integration allows research, backtesting, opitimization, and live trading through the Bloomberg APIs. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
+<p>QuantConnect can integrate with the Bloomberg™ Server API (SAPI) or Desktop API (DAPI) in different cloud environments. This integration allows research, backtesting, opitimization, and live trading through the Bloomberg APIs. Terminal Link is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
 
 <p>QuantConnect Cloud only supports routing trades to the Bloomberg™ Server API. In this environment, you can route orders to any of the prime brokerages supported by Bloomberg, but you still get to leverage the data feed, server management, and data management from QuantConnect, giving you the best of both worlds. To use Terminal Link, you need to be a member of an organization on the Trading Firm or Institution <a href='https://www.quantconnect.com/docs/v2/cloud-platform/organizations/tier-features'>tier</a>.</p>
