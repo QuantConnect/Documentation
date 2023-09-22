37ba9ddfedfa5196f864eda9a1ffffd9137b6029
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 17:15:50 -0600

    Update 09 Fills.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/09 Fills.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/09 Fills.php
@@ -1,3 +1,3 @@
-<p>In live trading, LEAN routes your orders to the exchange or prime brokerage you select. The order fills and is routed back to you.</p>
+<p>In live trading, LEAN routes your orders to the exchange or prime brokerage you select. The order fills and then routes back to you.</p>
 
 <p>To view how we model Bloomberg™ Execution Management System order fills, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/quantconnect-paper-trading#04-Fills">Fills</a>.</p>
