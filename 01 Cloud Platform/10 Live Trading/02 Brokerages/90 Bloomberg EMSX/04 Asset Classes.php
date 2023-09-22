fbef8e733d77318c235600c3b5d5f441660d2880
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 16:45:33 -0600

    Update 04 Asset Classes.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/04 Asset Classes.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/04 Asset Classes.php
@@ -3,10 +3,6 @@
 <ul>
     <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity'>Equities</a></li>
     <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options'>Equity Options</a></li>
+    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/index-options'>Index Options</a>
     <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/futures'>Futures</a></li>
 </ul>
-
-<?
-$brokerageName = "Terminal Link";
-include(DOCS_RESOURCES."/brokerages/us-etfs.php");
-?>
