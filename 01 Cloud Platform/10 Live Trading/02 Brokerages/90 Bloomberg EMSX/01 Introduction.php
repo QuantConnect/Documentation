2ab9c66a527eea8f29e1e66e7de2160a48e97641
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 16:27:29 -0600

    Update 01 Introduction.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
@@ -1,6 +1,6 @@
 <? include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 
-<img class='docs-image' style='width: 100%;' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg' alt="Terminal link icon">
+<img class='docs-image' style='width: 50%' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg' alt="Terminal link icon">
 
 <p>On the QuantConnect Local Platform, LEAN can connect to the Bloomberg™ Desktop API (DAPI) through our Terminal Link plug-in. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
 
