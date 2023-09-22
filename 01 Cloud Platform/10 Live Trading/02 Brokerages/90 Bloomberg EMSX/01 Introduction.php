e240fe9a3394209165b59e3b88d0d5f8320e8298
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-22 16:34:59 -0600

    Update 01 Introduction.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/90 Bloomberg EMSX/01 Introduction.php
@@ -2,6 +2,6 @@
 
 <img class='docs-image' style='width: 50%' src='https://cdn.quantconnect.com/i/tu/terminal-link-logo.svg' alt="Terminal link icon">
 
-<p>On the QuantConnect Local Platform, LEAN can connect to the Bloomberg™ Desktop API (DAPI) through our Terminal Link plug-in. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
+<p>QuantConnect can integrate with the Bloomberg™ Server API (SAPI) or Desktop API (DAPI) in different cloud environments. This allows research, backtesting, opitimization, and live trading through the Bloomberg APIs. This product is in no way affiliated with or endorsed by Bloomberg™; it is simply an add-on. Add Terminal link to your organization to access the 1,300+ prime brokerages in the Bloomberg Execution Management System network.</p>
 
-<p>For more infomation about Terminal Link, refer to the <a href='/docs/v2/lean-cli/live-trading/brokerages/prime-brokerages'>CLI documentation</a>.</p>
+<p>QuantConnect Cloud only supports routing trades to the Bloomberg™ Server API. In this environment, you can route orders to any of the prime brokerages supported by Bloomberg, but you still get to leverage the data feed, server management, and data management from QuantConnect, giving you the best of both worlds.</p>
