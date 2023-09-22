1272ae7be4f5150bcb48753b2dc7fe5d96311306
Derek Melchin <melchin.derek@gmail.com>
2023-09-22 15:11:29 -0600

    Add Terminal Link SAPI

---
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/09 Bloomberg EMSX/09 Fills.php
@@ -0,0 +1,3 @@
+<? include(DOCS_RESOURCES."/brokerages/fills.html"); ?>
+
+<p>To view how we model Bloomberg™ Execution Management System order fills, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/quantconnect-paper-trading#04-Fills">Fills</a>.</p>
---
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/10 Bloomberg EMSX/02 Asset Classes.php
@@ -0,0 +1,12 @@
+<p>Terminal Link supports trading the following asset classes:</p>
+
+<ul>
+    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity'>Equities</a></li>
+    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options'>Equity Options</a></li>
+    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/futures'>Futures</a></li>
+</ul>
+
+<?
+$brokerageName = "Terminal Link";
+include(DOCS_RESOURCES."/brokerages/us-etfs.php");
+?>
