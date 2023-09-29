348d5a42e6c9631c100b6431ff7634eda24e9004
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:35:05 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/03 Binance/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/03 Binance/03 Deploy Cloud Algorithms.php
@@ -1,27 +1,24 @@
 <?
 $brokerageDetails = "
-<li>Enter the exchange to use.
+<li>Enter the exchange to use.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Binance Exchange (Binance, BinanceUS, Binance-USDM-Futures, Binance-COIN-Futures): BinanceUS</pre>
 </div>
-</li>
 
-<li>Enter your Binance API key and secret.
+<li>Enter your Binance API key and secret.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 API key: wL1waeOC7VD447skCFeiat9pP3r1uKXfYomGg43uyCOgzl8xsI9SZsX97AXP4zWv
 API secret: ****************************************************************</pre>
 </div>
 To create new credentials, see <a rel='nofollow' href='https://www.binance.com/en/support/faq/how-to-create-api-keys-on-binance-360002502072' target='_blank'>How to Create API Keys on Binance</a>.
-</li>
 
-<li>Enter the environment to use.
+<li>Enter the environment to use.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Use the testnet? (live, paper): </pre>
 </div>
-</li>
 ";
 $brokerageName="Binance";
 $isSupported=true;
