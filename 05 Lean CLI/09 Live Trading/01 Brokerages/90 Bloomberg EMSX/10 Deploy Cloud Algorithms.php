844afc718cf922a82540db6e94feaa3300cf5fa9
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:56:33 -0600

    Update 10 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/10 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/10 Deploy Cloud Algorithms.php
@@ -1,6 +1,48 @@
 <?php
+$brokerageDetails = "
+<li>Enter your unique user identifier (UUID).</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Configure credentials for Terminal Link
+Using 'SAPI' Connection Type
+Server Auth ID: </pre>
+</div>
+<p>The UUID is a unique integer identifier that's assigned to each Bloomberg Anywhere user. If you don't know your UUID, contact Bloomberg.</p>
+
+<li>Enter the environment to use.</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Environment (Production, Beta): </pre>
+</div>
+
+<li>Enter the SAPI host and port.</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Server host: 
+Server port:  </pre>
+</div>
+<p>The default port is 8194.</p>
+
+<li>Enter the EMSX broker to use.</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+EMSX broker: </pre>
+</div>
+
+<li>Enter the account to which LEAN should route orders.</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+EMSX account []: </pre>
+</div>
+
+<li>Enter your OpenFIGI API key.</li>
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Open FIGI API key: </pre>
+</div>
+";
 $brokerageName="Terminal Link";
-$isSupported=false;
+$isSupported=true;
 $supportsCashHoldings=false;
 $supportsPositionHoldings=false;
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
