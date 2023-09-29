42bab0d922e7c7bbd1e17d7c6feeb220a0c7632d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 15:10:12 -0600

    Delete 01 Cloud Platform/04 Organizations/05 Support/11 AI Support.php

--- 01 Cloud Platform/04 Organizations/05 Support/11 AI Support.php
+++
@@ -1,7 +0,0 @@
-<p>Mia is an AI assistant in the <a href='/docs/v2/cloud-platform/projects/ide#06-Console'>Algorithm Lab console</a>. Mia provides contextual assistance to most issues you may encounter when developing a strategy. It has been trained on hundreds of algorithms and thousands of documentation pages.</p>
-<img src="https://cdn.quantconnect.com/i/tu/ask-mia.png" class="docs-image"/>
-<p>The following table shows the number of AI tickets each support seat tier can open during a 1-month rolling period:</p>
-
-<? include(DOCS_RESOURCES."/quotas/ask-mia.html"); ?>
-
-<p>The quotas don't restore on the first day of the month or the billing date. They restore according to a 1-month rolling window.</p>
