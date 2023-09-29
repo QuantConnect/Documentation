0054ac3d1c7307948483ee25dad4719069aa246d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 09:51:24 -0600

    Update 02 Asset Classes.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/02 Asset Classes.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/02 Asset Classes.php
@@ -1,7 +1,8 @@
 <p>Terminal Link supports trading the following asset classes:</p>
 
 <ul>
-    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity'>Equities</a></li>
-    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options'>Equity Options</a></li>
-    <li><a href='/docs/v2/writing-algorithms/securities/asset-classes/futures'>Futures</a></li>
+    <li><a href='https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/us-equity'>Equities</a></li>
+    <li><a href='https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/equity-options'>Equity Options</a></li>
+    <li><a href='https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/futures'>Futures</a></li>
+    <li><a href='https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/index-options'>Index Options</a></li>
 </ul>
