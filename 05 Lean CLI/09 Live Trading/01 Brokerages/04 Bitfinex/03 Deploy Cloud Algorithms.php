63aa2081c2ca7c8322341f9eb2497acbe273bddd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:35:23 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/04 Bitfinex/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/04 Bitfinex/03 Deploy Cloud Algorithms.php
@@ -1,12 +1,11 @@
 <?
 $brokerageDetails = "
-<li>Enter your API key Id and secret.
+<li>Enter your API key Id and secret.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 API key: bbbMsqbxjytVM9cGvnLpKguz9rZf2T5qACxaVx7E8Mm
 Secret key: *******************************************</div>
-To create new API credentials, see the <a rel='nofollow' href='https://www.bitfinex.com/api' target='_blank'>API Management page</a> on the Bitfinex website.
-</li>
+<p>To create new API credentials, see the <a rel='nofollow' href='https://www.bitfinex.com/api' target='_blank'>API Management page</a> on the Bitfinex website.</p>
 ";
 $brokerageName="Bitfinex";
 $isSupported=true;
