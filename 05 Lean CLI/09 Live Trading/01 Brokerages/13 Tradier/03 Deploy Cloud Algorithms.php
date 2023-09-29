b9051c3a90ef1751c666cf460e7f6f8326c3a287
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:37:53 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/13 Tradier/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/13 Tradier/03 Deploy Cloud Algorithms.php
@@ -1,20 +1,18 @@
 <?
 $brokerageDetails = "
-<li>Enter your Tradier account Id and access token.
+<li>Enter your Tradier account Id and access token.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Account id: VA000001
 Access token: ****************</pre>
 </div>
-To get these credentials, see your <a href='https://dash.tradier.com/settings/api' target='_blank' rel='nofollow'>Settings/API Access page</a> on the Tradier website.
-</li>
+<p>To get these credentials, see your <a href='https://dash.tradier.com/settings/api' target='_blank' rel='nofollow'>Settings/API Access page</a> on the Tradier website.</p>
 
-<li>Enter whether the developer sandbox should be used.
+<li>Enter whether the developer sandbox should be used.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Use the developer sandbox? (live, paper): </pre>
 </div>
-</li>
 ";
 $brokerageName="Tradier";
 $isSupported=true;
