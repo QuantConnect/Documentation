b0a7bbf7eacbdafdb975105f7384725313272218
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:36:18 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/07 Interactive Brokers/03 Deploy Cloud Algorithms.php
@@ -7,29 +7,26 @@ $weeklyRestartText = ob_get_clean();
 $brokerageDetails = "
 <li>Set up IB Key Security via IBKR Mobile. For instructions, see <a href='https://guides.interactivebrokers.com/iphone/log_in/ibkey.htm?tocpath=IB%20Key%20Security%20Protocol%7C_____0' target='_blank' rel='nofollow'>IB Key Security via IBKR Mobile</a> on the IB website.</li>
 
-<li>Go back to the terminal and enter your Interactive Brokers username, account id, and password.
+<li>Go back to the terminal and enter your Interactive Brokers username, account id, and password.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Username: trader777
 Account id: DU1234567
 Account password: ****************</pre>
 </div>
-</li>
 
-<li>Enter a weekly restart time that's convenient for you.
+<li>Enter a weekly restart time that's convenient for you.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Weekly restart UTC time (hh:mm:ss) [21:00:00]: </pre>
 </div>
 {$weeklyRestartText}
-</li>
 
-<li>Enter whether you want to use the <a href='https://www.quantconnect.com/docs/v2/cloud-platform/live-trading/data-feeds/brokerage-data-feeds/interactive-brokers'>price data feed from Interactive Brokers</a> instead of the one from QuantConnect. Enabling this feature requires you to have active Interactive Brokers market data subscriptions for all data required by your algorithm.
+<li>Enter whether you want to use the <a href='https://www.quantconnect.com/docs/v2/cloud-platform/live-trading/data-feeds/brokerage-data-feeds/interactive-brokers'>price data feed from Interactive Brokers</a> instead of the one from QuantConnect. Enabling this feature requires you to have active Interactive Brokers market data subscriptions for all data required by your algorithm.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Do you want to use the Interactive Brokers price data feed instead of the QuantConnect price data feed? (yes/no): y</pre>
 </div>
-</li>
 ";
 $brokerageName="Interactive Brokers";
 $isSupported=true;
