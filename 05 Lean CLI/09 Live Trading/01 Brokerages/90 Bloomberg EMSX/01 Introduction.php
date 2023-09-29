2ed60aec682e579b2d88d2f09c23ae8945294e25
Web Editor <web.editor@quantconnect.com>
2023-09-29 11:37:41 -0400

    Automated push from server made by Derek Melchin

--- 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/90 Bloomberg EMSX/01 Introduction.php
@@ -2,3 +2,5 @@
 $brokerageName="Terminal Link";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php"); 
 ?>
+
+<p>The LEAN CLI integrates with the Bloomberg Desktop API (DAPI) to get data for research, backtests, optimization, and live trading or through your existing Bloomberg data subscriptions. In addition, you can route live trading strategies and and UAT paper trading through Bloomberg order routing via the EMSX network.</p>
