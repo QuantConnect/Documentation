db1385be1fc95807c84a7320e1b260402fed78dc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 16:32:21 -0600

    Update 04 Live Trading Nodes.php

--- 01 Cloud Platform/04 Organizations/03 Resources/04 Live Trading Nodes.php
+++ 01 Cloud Platform/04 Organizations/03 Resources/04 Live Trading Nodes.php
@@ -5,3 +5,5 @@
 <p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each live trading node model.</p>
 
 <p>GPU nodes perform best on repetitive and highly-parallel tasks like training machine learning models. It takes time to transfer the data to the GPU for computation, so if your algorithm doesn't train machine learning models, the extra time it takes to transfer the data can make it appear that GPU nodes run slower than CPU nodes.</p>
+
+<p>The CPU nodes are available on a fair usage basis. The GPU nodes can be shared with a maximum of two members. Depending on the server load, you may use the all of the GPU's processing power.</p>
