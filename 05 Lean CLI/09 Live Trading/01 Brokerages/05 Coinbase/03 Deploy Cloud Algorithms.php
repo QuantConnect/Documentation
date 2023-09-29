1e8bb7f4d55bef4b03db08dc8ee344534a5fc6c1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-09-29 11:35:44 -0600

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/05 Coinbase/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/05 Coinbase/03 Deploy Cloud Algorithms.php
@@ -1,21 +1,19 @@
 <?
 $brokerageDetails = "
-<li>Enter whether the sandbox should be used.
+<li>Enter whether the sandbox should be used.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 Use sandbox? (live, paper): live</pre>
 </div>
-</li>
 
-<li>Enter your API key, API secret, and passphrase.
+<li>Enter your API key, API secret, and passphrase.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 API key: 6d3ef5ca2d2fa52e4ee55624b0471261
 API secret: ****************************************************************************************
 Passphrase: ****************
 </div>
-To create new API credentials, see the <a href='https://pro.coinbase.com/profile/api' target='_blank' rel='nofollow'>API settings page</a> on the Coinbase website.
-</li>
+<p>To create new API credentials, see the <a href='https://pro.coinbase.com/profile/api' target='_blank' rel='nofollow'>API settings page</a> on the Coinbase website.</p>
 ";
 $brokerageName="Coinbase";
 $isSupported=true;
