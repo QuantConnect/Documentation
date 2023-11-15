995fa9db696621a2f09557f54a3e82224e0c7d51
Web Editor <web.editor@quantconnect.com>
2023-11-15 17:02:58 -0500

    Automated push from server made by Derek Melchin

--- 01 Cloud Platform/13 Community/02 Forum/08 Post Comments.php
+++ 01 Cloud Platform/13 Community/02 Forum/08 Post Comments.php
@@ -6,8 +6,9 @@
     <li>Log in to your account.</li>
     <li>Open the <a href="/forum">forum homepage</a>.</li>
     <li>Click the discussion on which you want to comment.</li>
-    <li>At the bottom of the discussion page, enter your comment.</li>
-    <li><span class='qualifier'>(Optional)</span> Follow these steps to attach a backtest:</li>
+    <li>If you want to reply to a comment in the discussion, click <span class='button-name'>Reply</span> at the bottom of the comment.</li>
+    <li>If you want to comment on the top-level discussion, scroll to the bottom of the discussion page and enter your comment.</li>
+    <li><span class='qualifier'>(Optional)</span> If you comment on the top-level discussion, follow these steps to attach a backtest:</li>
     <ol>
         <li>Click <span class='button-name'>Attach Backtest</span>.</li>
         <li>Click the <span class='field-name'>Project</span> field and then select the project that contains the backtest you want to attach from the drop-down menu.</li>
@@ -15,6 +16,6 @@
         <li>Click the <span class='field-name'>Backtest</span> field and then select a backtest from the drop-down menu.</li>
     </ol>
     <p>When you attach a backtest, you're sharing all of the project files that were used to generate the backtest results.</p>
-    <li>Click <span class='button-name'>Comment</span>.</li>
+    <li>Click <span class='button-name'>Reply</span>.</li>
     <p>Your comment is appended to the discussion.</p>
 </ol>
