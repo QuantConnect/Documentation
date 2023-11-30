c3aac5f8ee7065002e92aeecc79c79c6d945b376
Web Editor <web.editor@quantconnect.com>
2023-11-30 14:05:45 -0500

    Automated push from server made by Jared Broad

--- 02 Local Platform/02 Installation/03 Install on Linux/02 Requirements.php
+++ 02 Local Platform/02 Installation/03 Install on Linux/02 Requirements.php
@@ -2,7 +2,7 @@
 
 <ul>
     <li>4 GB RAM or more</li>
-    <li>20 GB hard drive or more</li>
+    <li>60 GB hard drive or more</li>
 </ul>
 
 <? include(DOCS_RESOURCES."/local-platform/installation/internet-requirement.html");   ?>
