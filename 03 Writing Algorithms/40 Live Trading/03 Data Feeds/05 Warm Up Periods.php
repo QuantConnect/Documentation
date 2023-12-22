b91a1b5d0bab9979889db22cb0a726d9c3a0d825
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2023-12-22 14:55:46 -0700

    Update 05 Warm Up Periods.php

--- 03 Writing Algorithms/40 Live Trading/03 Data Feeds/05 Warm Up Periods.php
+++ 03 Writing Algorithms/40 Live Trading/03 Data Feeds/05 Warm Up Periods.php
@@ -1,3 +1,1 @@
 <?php echo file_get_contents(DOCS_RESOURCES."/datasets/warm-up-introduction.html"); ?>
-
-<p><a href="/docs/v2/writing-algorithms/datasets/morningstar/us-fundamental-data">US Fundamental Data by Morningstar</a> is limited to the last 30 days.<p>
