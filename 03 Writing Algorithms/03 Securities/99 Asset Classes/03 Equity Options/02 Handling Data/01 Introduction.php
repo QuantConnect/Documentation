225d72e3e4b41c639079a5a7a164cc4ab4772678
Jared <jaredbroad@gmail.com>
2024-01-02 15:17:20 -0500

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/02 Handling Data/01 Introduction.php
@@ -3,3 +3,6 @@ $dataFormat = "TradeBar";
 $securityName = "contract";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+<p>
+Equity Options supports Minute, Hour, and Daily resolution bars. 
+</p>
