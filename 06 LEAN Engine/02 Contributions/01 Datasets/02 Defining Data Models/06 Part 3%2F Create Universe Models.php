c3243fb4e816f3c405b0ce91bde6dfe8fa0c89c5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-03 16:56:56 -0700

    Update 06 Part 3%2F Create Universe Models.php

--- 06 LEAN Engine/02 Contributions/01 Datasets/02 Defining Data Models/06 Part 3%2F Create Universe Models.php
+++ 06 LEAN Engine/02 Contributions/01 Datasets/02 Defining Data Models/06 Part 3%2F Create Universe Models.php
@@ -7,7 +7,7 @@
     <li>Skip the rest of this page.</li>
 </ol>
 
-<p>The input to your model should be a <span class="public-file-name">CSV</span> file where the first column is the <a href='http://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/security-identifiers'>security identifier</a> and the second column is the point-in-time ticker.</p>
+<p>The input to your model should be many <span class="public-file-name">CSV</span> files where the first column is the <a href='http://www.quantconnect.com/docs/v2/writing-algorithms/key-concepts/security-identifiers'>security identifier</a> and the second column is the point-in-time ticker.</p>
 
 <div class="section-example-container"><pre>
 A R735QTJ8XC9X,A,17.19,109700,1885743,False,0.9904858,1
@@ -30,7 +30,7 @@ ZVX R735QTJ8XC9X,ZVX,10,15600,156000,False,1,0.666667
         <li>Replace the “Some custom data property” comments with a description of each property in your dataset.</li>
     </ol>
     <li>Define the <a href="/docs/v2/lean-engine/contributions/datasets/key-concepts#04-Data-Sources">GetSource</a> method to point to the path of your dataset file(s).</li>
-    <p>Use the <code>date</code> parameter as the file name to get the date of data being requested. An example output file path is <span class="public-file-name">/ output / alternative / xyzairline / ticketsales / universe / 20200320.csv</span>.<br></p>
+    <p>Use the <code>date</code> parameter as the file name to get the <code>DateTime</code> of data being requested. Example output file paths are <span class="public-file-name">/ output / alternative / xyzairline / ticketsales / universe / 20200320.csv</span> for daily data and <span class="public-file-name">/ output / alternative / xyzairline / ticketsales / universe / 2020032000.csv</span> for hourly data.<br></p>
     <li>Define the <code>Reader</code> method to return instances of your universe class.</li>
     <p>The first column in your data file must be the security identifier and the second column must be the point-in-time ticker. With this configuration, use <code>new Symbol(SecurityIdentifier.Parse(csv[0]), csv[1])</code> to create the security <code>Symbol</code>.</p>
     <p>The date in your data file must be the date that the data point is available for consumption. With this configuration, set the <code>Time</code> to <code>date - Period</code>.</p>
