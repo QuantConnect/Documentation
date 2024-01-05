3373917e1994d68ea6d4a68d8eeae2f9846e6ac7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:11:26 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "Tick";
 $securityName = "Index";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for Index data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/index/requesting-data#03-Resolutions">Resolutions</a>.</p>
