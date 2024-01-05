71b5a2c415df377e878fd03b6c36d082cbbb2827
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:09:48 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "QuoteBar";
 $securityName = "Forex pair";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for Forex data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/forex/requesting-data#04-Resolutions">Resolutions</a>.</p>
