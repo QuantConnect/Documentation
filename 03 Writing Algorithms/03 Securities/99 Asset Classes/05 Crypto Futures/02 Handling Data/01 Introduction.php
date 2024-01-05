6903c316633246f03a3abe6b01d66bbe3d8a0dca
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:09:07 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/05 Crypto Futures/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/05 Crypto Futures/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "TradeBar";
 $securityName = "security";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for Crypto Futures data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/crypto-futures/requesting-data#04-Resolutions">Resolutions</a>.</p>
