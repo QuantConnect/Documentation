96ecd45d308287a84c752d575c3126cd3dbfc4dc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 13:40:13 -0700

    Update example_plotting.php

--- Resources/object-store/example_plotting.php
+++ Resources/object-store/example_plotting.php
@@ -1,7 +1,7 @@
-<p>You can use the Object Store to plot data from your backtests and live algorithm in the Research Environment. In the following example, you will learn how to plot the <a href="/docs/v2/writing-algorithms/indicators/supported-indicators/simple-moving-average">Simple Moving Average</a> indicator generated in a backtest.</p>
+<p>You can use the Object Store to plot data from your backtests and live algorithm in the Research Environment. The following example demonstrates how to plot a <a href="/docs/v2/writing-algorithms/indicators/supported-indicators/simple-moving-average">Simple Moving Average</a> indicator that's generated during a backtest.</p>
 
 <ol>
-    <li>Create a algorithm, add a data subscription and a <a href="/docs/v2/writing-algorithms/indicators/supported-indicators/simple-moving-average">Simple Moving Average</a> indicator.</li>
+    <li>Create a algorithm, add a data subscription, and add a simple moving average indicator.</li>
     <div class='section-example-container'>
     <pre class='csharp'>public class ObjectStoreChartingAlgorithm : QCAlgorithm
 {
@@ -24,7 +24,7 @@
 
     <p>The algorithm will save <code class='csharp'>_content</code><code class='python'>self.content</code> to the Object Store.</p>
 
-    <li>Save indicator data as <code>string</code> in <code class='csharp'>_content</code><code class='python'>self.content</code>.</li>
+    <li>Save the indicator data as <code>string</code> in <code class='csharp'>_content</code><code class='python'>self.content</code>.</li>
     <div class='section-example-container'>
     <pre class='csharp'>public override void OnData(Slice data)
 {
@@ -45,7 +45,7 @@
     self.ObjectStore.Save('sma_values_python', self.content)</pre>
     </div>
     
-    <li>Open the Research Environment, and create a <code>QuantBook</code>.</li>
+    <li><a href='/docs/v2/research-environment/key-concepts/getting-started#03-Open-Notebooks'>Open the Research Environment</a> and create a <code>QuantBook</code>.</li>
 
     <div class='section-example-container'>
     <pre class='csharp'>// Execute the following command in first
@@ -60,14 +60,16 @@ var qb = new QuantBook();</pre>
     <pre class='python'>qb = QuantBook()</pre>
     </div>
 
-    <li>To read data from the Object Store, call the <code>Read</code> method. You need to provide the key you used to store the object.</li>
+    <li>Read the indicator data from the Object Store.</li>
 
     <div class='section-example-container'>
     <pre class='csharp'>var content = qb.ObjectStore.Read("sma_values_csharp");</pre>
     <pre class='python'>content = qb.ObjectStore.Read("sma_values_python")</pre>
     </div>
 
-    <li class='python'>Convert the data to a pandas object, and create a chart.</li>
+    <p>The key you provide must be the same key you used to save the object.</p>
+
+    <li class='python'>Convert the data to a pandas object and create a chart.</li>
     <div class="python section-example-container">
     <pre class="python">data = {}
 for line in content.split('\n'):
@@ -86,7 +88,7 @@ using Plotly.NET;
 using Plotly.NET.LayoutObjects;</pre>
     </div>
 
-    <li class='csharp'>Create the <code>Layout</code> object, and set the <code>title</code>, <code>xaxis</code>, and <code>yaxis</code> properties.</li>
+    <li class='csharp'>Create the <code>Layout</code> object and set the <code>title</code>, <code>xaxis</code>, and <code>yaxis</code> properties.</li>
     <div class="csharp section-example-container">
     <pre class="csharp">var layout = new Layout();
 layout.SetValue("title", Title.init("SMA"));
@@ -100,7 +102,7 @@ yAxis.SetValue("title", "SMA");
 layout.SetValue("yaxis", yAxis);</pre>
     </div>
 
-    <li class='csharp'>Convert the data to a list of <code>DateTime</code> objects for the chart x-axis and a list of <code>decimal</code> objects for the chart y-axis. Create a <code>Chart2D.Chart.Line</code> object with the data.</li>
+    <li class='csharp'>Convert the data to a list of <code>DateTime</code> objects for the chart x-axis and a list of <code>decimal</code> objects for the chart y-axis, then create a <code>Chart2D.Chart.Line</code> object with the data.</li>
     <div class="csharp section-example-container">
     <pre class="csharp">var index = new List&lt;DateTimee&gt;();
 var values = new List&lt;decimal&gt;();
@@ -118,7 +120,7 @@ foreach (var line in content.Split('\n'))
 var chart = Chart2D.Chart.Linee&lt;DateTime, decimal, stringe&gt;(index, values);</pre>
     </div>
 
-    <li class='csharp'>Apply the layout to the <code>Line</code> object, and create the <code>HTML</code> object.</li>
+    <li class='csharp'>Apply the layout to the <code>Line</code> object and create the <code>HTML</code> object.</li>
     <div class="csharp section-example-container">
     <pre class="csharp">chart.WithLayout(layout);
 var result = HTML(GenericChart.toChartHTML(chart));</pre>
