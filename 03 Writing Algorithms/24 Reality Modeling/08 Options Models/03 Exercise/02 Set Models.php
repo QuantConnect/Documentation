4a45f41c42b6e40bc78b9ca32d09fb8c4b841549
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 11:52:31 -0700

    Update 02 Set Models.php

--- 03 Writing Algorithms/24 Reality Modeling/08 Options Models/03 Exercise/02 Set Models.php
+++ 03 Writing Algorithms/24 Reality Modeling/08 Options Models/03 Exercise/02 Set Models.php
@@ -1,24 +1,12 @@
-<p>To set the exercise model of an Option, call the <code>SetOptionExerciseModel</code> method of the <code>Option</code> object.</p>
-
-<p>If you have access to the <code>Option</code> object when you subscribe to the Option universe or contract, you can set the exercise model immediately after you create the subscription.</p>
-<div class="section-example-container">
-    <pre class="csharp">// In Initialize
-var option = AddOption("SPY");
-option.SetOptionExerciseModel(new DefaultExerciseModel());</pre>
-    <pre class="python"># In Initialize
-option = AddOption("SPY")
-option.SetOptionExerciseModel(DefaultExerciseModel())</pre>
-</div>
-
-<p>Otherwise, set the assignment model in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>.</p>
+<p>To set the exercise model of an Option, call the <code>SetOptionExerciseModel</code> method of the <code>Option</code> object in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>.</p>
 
 <?php
 $overwriteCodePy = "if security.Type == SecurityType.Option: # Option type
-            option.SetOptionExerciseModel(DefaultExerciseModel())";
+            security.SetOptionExerciseModel(DefaultExerciseModel())";
 $overwriteCodeC = "if (security.Type == SecurityType.Option) // Option type
         {
-            option.SetOptionExerciseModel(new DefaultExerciseModel());
+            (security as Option).SetOptionExerciseModel(new DefaultExerciseModel());
         }";
-$comment = "the exercise model";
+$comment = "the Option exercise model";
 include(DOCS_RESOURCES."/reality-modeling/brokerage-model-security-init.php");
 ?>
