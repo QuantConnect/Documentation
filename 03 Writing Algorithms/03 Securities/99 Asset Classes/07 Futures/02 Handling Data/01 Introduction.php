30afd175da09762bd50020ae269681d34e29cb27
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:10:22 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Futures/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "TradeBar";
 $securityName = "contract";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for Futures data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/futures/requesting-data#03-Resolutions">Resolutions</a>.</p>
