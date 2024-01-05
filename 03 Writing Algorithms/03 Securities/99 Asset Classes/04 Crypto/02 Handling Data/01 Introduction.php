38350c9caffc197511d88ad8100dfcd8be1c7323
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:08:33 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "TradeBar";
 $securityName = "security";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for Crypto data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/crypto/requesting-data#04-Resolutions">Resolutions</a>.</p>
