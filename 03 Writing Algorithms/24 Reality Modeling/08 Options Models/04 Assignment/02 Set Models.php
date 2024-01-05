41cc3460faebd793a9c5bd3a10d9ac889bf1cfc6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 11:36:42 -0700

    Update 02 Set Models.php

--- 03 Writing Algorithms/24 Reality Modeling/08 Options Models/04 Assignment/02 Set Models.php
+++ 03 Writing Algorithms/24 Reality Modeling/08 Options Models/04 Assignment/02 Set Models.php
@@ -1,17 +1,4 @@
-<p>To set the assignment model of an Option, call the <code>SetOptionAssignmentModel</code> method of the <code>Option</code> object.</p>
-
-<p>If you have access to the <code>Option</code> object when you subscribe to the Option universe or contract, you can set the assignment model immediately after you create the subscription.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">// In Initialize
-var option = AddOption("SPY");
-option.SetOptionAssignmentModel(new DefaultOptionAssignmentModel());</pre>
-    <pre class="python"># In Initialize
-option = AddOption("SPY")
-option.SetOptionAssignmentModel(DefaultOptionAssignmentModel())</pre>
-</div>
-
-<p>Otherwise, set the assignment model in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>.</p>
+<p>To set the assignment model of an Option, in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>, call the <code>SetOptionAssignmentModel</code> method of the <code>Option</code> object.</p>
 
 <?php
 $overwriteCodePy = "if security.Type == SecurityType.Option: # Option type
