a0e467593f07e6d32b4607c342065ce4dd42a323
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:12:26 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "QuoteBar";
 $securityName = "CFD";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for CFD data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/cfd/requesting-data#04-Resolutions">Resolutions</a>.</p>
