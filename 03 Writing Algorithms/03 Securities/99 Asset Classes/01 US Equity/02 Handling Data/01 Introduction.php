793bc9039f3e12ef27dd4b2743dc33bf1efbd9e6
Web Editor <web.editor@quantconnect.com>
2024-01-05 18:04:55 -0500

    Automated push from server made by Derek Melchin

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/01 US Equity/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "TradeBar";
 $securityName = "security";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for US Equity data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#03-Resolutions">Resolutions</a>.</p>
