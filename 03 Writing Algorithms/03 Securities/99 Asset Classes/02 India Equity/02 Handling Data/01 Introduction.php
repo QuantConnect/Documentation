1508386b2828503ab849d6b3e525b5d06969c378
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 16:06:13 -0700

    Update 01 Introduction.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/02 Handling Data/01 Introduction.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 India Equity/02 Handling Data/01 Introduction.php
@@ -3,3 +3,5 @@ $dataFormat = "TradeBar";
 $securityName = "security";
 include(DOCS_RESOURCES."/securities/handling-data-intro.php"); 
 ?>
+
+<p>To view the resolutions that are available for India Equity data, see <a href="/docs/v2/writing-algorithms/securities/asset-classes/india-equity/requesting-data#03-Resolutions">Resolutions</a>.</p>
