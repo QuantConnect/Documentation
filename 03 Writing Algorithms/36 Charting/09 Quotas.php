a8d30862bab6002b4562b30c0cb6789c20d69c7b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-05 17:26:27 -0700

    Update 09 Quotas.php

--- 03 Writing Algorithms/36 Charting/09 Quotas.php
+++ 03 Writing Algorithms/36 Charting/09 Quotas.php
@@ -1,1 +1,7 @@
-<? include(DOCS_RESOURCES."/plotting/quotas.php"); ?>
+<p>When you run backtests, you must stay within the plotting quotas to avoid errors.</p>
+
+<h4>Cloud Quotas</h4>
+<p>If you execute backtests in QuantConnect Cloud, see <a href='/docs/v2/cloud-platform/backtesting/results#05-Custom-Charts'>Custom Charts</a> for more information about the charting quotas.</p>
+
+<h4>Local Quotas</h4>
+<p>If you execute local backtests, the charting quotas are set by the <code>maximum-chart-series</code> and <code>maximum-data-points-per-chart-series</code> <a href='/docs/v2/local-platform/development-environment/configuration#03-LEAN-Settings'>configuration settings</a>.</p>
