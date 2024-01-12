c1fe593256a496612f14868316de609ea675f21f
Web Editor <web.editor@quantconnect.com>
2024-01-12 13:29:28 -0500

    Automated push from server made by Derek Melchin

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/16 Deploy Live Algorithms.php
@@ -3,29 +3,8 @@ $brokerageName = "Coinbase";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Enter your Coinbase API key, API secret, and passphrase.</li>" .
-file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/coinbase.html") .
-"<li>Click on the <span class='field-name'>Environment</span> field and then click one of the environments.</li>
-<p>The following table shows the supported environments:</p>
-
-<table class='table qc-table'>
-    <thead>
-        <tr>
-            <th>Environment</th>
-            <th>Description</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Live</td>
-            <td>Trade with real money</td>
-        </tr>
-        <tr>
-            <td>Paper</td>
-            <td>Trade with paper money</td>
-        </tr>
-    </tbody>
-</table>";
+$authentication = "<li>Enter your Coinbase API key and API secret.</li>" .
+file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/coinbase.html");
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
