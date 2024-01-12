a4d06edfea1a53c7a4c775514aeaf9ffd3c6ac6b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-12 11:25:56 -0700

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/06 Coinbase/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/06 Coinbase/03 Deploy Cloud Algorithms.php
@@ -1,11 +1,10 @@
 <?
 $brokerageDetails = "
-<li>Enter your API key, API secret, and passphrase.</li>
+<li>Enter your API key and API secret.</li>
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 API key: 6d3ef5ca2d2fa52e4ee55624b0471261
-API secret: ****************************************************************************************
-Passphrase: ****************
+API secret: ****************************************************************************************</pre>
 </div>
 <p>To create new API credentials, see the <a href='https://www.coinbase.com/settings/api' target='_blank' rel='nofollow'>API settings page</a> on the Coinbase website.</p>
 ";
