5e09c05afb7aacc49e5fd2ca3f7ea7acef0166f7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-12 11:30:04 -0700

    Update 16 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/16 Deploy Live Algorithms.php
@@ -3,8 +3,7 @@ $brokerageName = "Coinbase";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Enter your Coinbase API key and API secret.</li>" .
-file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/coinbase.html");
+$authentication = "<li>Enter your Coinbase API key and API secret.</li>" . file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/coinbase.html");
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
