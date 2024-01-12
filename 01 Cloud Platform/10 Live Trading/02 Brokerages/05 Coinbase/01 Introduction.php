396e40e82e74a4b3883e7c1d3e4879311e04da74
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-12 11:01:35 -0700

    Update 01 Introduction.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/01 Introduction.php
@@ -2,4 +2,4 @@
 
 <p>Coinbase was founded by Brian Armstrong and Fred Ehrsam in 2012 with the goal to "increase economic freedom in the world". Coinbase provides access to trading Crypto for clients in over <a rel="nofollow" target="_blank" href="https://www.coinbase.com/places">100 countries</a> with no minimum deposit. Coinbase also provides a self-hosted Crypto wallet, a Visa debit rewards card, and Bitcoin collateral-backed lines of credit.</p>
 
-<p>To view the implementation of the Coinbase brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.CoinbasePro' rel='nofollow' target="_blank">Lean.Brokerages.CoinbasePro repository</a>.</p>
+<p>To view the implementation of the Coinbase brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Coinbase' rel='nofollow' target="_blank">Lean.Brokerages.Coinbase repository</a>.</p>
