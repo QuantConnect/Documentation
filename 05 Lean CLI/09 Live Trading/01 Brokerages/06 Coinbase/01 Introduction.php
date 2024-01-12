d5ef33a30c77998d360b845a64192f71bfcb363b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-12 11:00:43 -0700

    Update 01 Introduction.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/06 Coinbase/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/06 Coinbase/01 Introduction.php
@@ -3,4 +3,4 @@ $brokerageName="Coinbase";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 ?>
 
-<p>To view the implementation of the Coinbase brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.CoinbasePro' rel='nofollow' target="_blank">Lean.Brokerages.CoinbasePro repository</a>.</p>
+<p>To view the implementation of the Coinbase brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Coinbase' rel='nofollow' target="_blank">Lean.Brokerages.Coinbase repository</a>.</p>
