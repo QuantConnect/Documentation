f7f407e67ec305948c31b32e05428960ddaa0d1f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-19 12:09:01 -0700

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/03 Binance/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/03 Binance/03 Deploy Cloud Algorithms.php
@@ -12,7 +12,7 @@ Binance Exchange (Binance, BinanceUS, Binance-USDM-Futures, Binance-COIN-Futures
 API key: wL1waeOC7VD447skCFeiat9pP3r1uKXfYomGg43uyCOgzl8xsI9SZsX97AXP4zWv
 API secret: ****************************************************************</pre>
 </div>
-To create new credentials, see <a rel='nofollow' href='https://www.binance.com/en/support/faq/how-to-create-api-keys-on-binance-360002502072' target='_blank'>How to Create API Keys on Binance</a>.
+<p>To create new credentials, see <a rel='nofollow' href='https://www.binance.com/en/support/faq/how-to-create-api-keys-on-binance-360002502072' target='_blank'>How to Create API Keys on Binance</a>.</p>
 
 <li>Enter the environment to use.</li>
 <div class='cli section-example-container'>
