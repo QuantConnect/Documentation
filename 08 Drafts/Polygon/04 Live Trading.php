823fbf058f6a6b52e711ea3f7d14d6fff90c5353
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-26 12:10:26 -0700

    Create 04 Live Trading.php

---
+++ 08 Drafts/Polygon/04 Live Trading.php
@@ -0,0 +1,21 @@
+<?
+$brokerageName = "QuantConnect Paper Trading";
+$dataFeedName = "Polygon";
+$isBrokerage = false;
+$brokerageDetails = "";
+$dataFeedDetails = "
+    <li>Enter your Polygon API key.
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+Configure credentials for Polygon Data Feed
+
+Your Polygon data feed API Key:</pre>
+</div>
+
+<p>To get your API key, see the <a rel='nofollow' target='_blank' href='https://polygon.io/dashboard/api-keys'>API Keys page</a> on the Polygon website.</p>
+    </li>
+";
+$supportsIQFeed = false;
+$requiresSubscription = false;
+include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php");
+?>
