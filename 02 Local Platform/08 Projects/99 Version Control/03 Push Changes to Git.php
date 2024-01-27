215b370aec41ec2d2355161e2d98b3bdc64c02da
Gustavo Aviles <gaviles@users.noreply.github.com>
2024-01-26 16:56:42 -0800

    Update 03 Push Changes to Git.php
    Fix issue with undefined variable.

--- 02 Local Platform/08 Projects/99 Version Control/03 Push Changes to Git.php
+++ 02 Local Platform/08 Projects/99 Version Control/03 Push Changes to Git.php
@@ -1,1 +1,9 @@
-<? include(DOCS_RESOURCES."/cli/version-control/push-changes-to-git.php"); ?>
+<?php
+
+if(!isset($leanCLI)){
+  $leanCLI = fasle;
+}
+ 
+include(DOCS_RESOURCES."/cli/version-control/push-changes-to-git.php"); 
+
+?>
