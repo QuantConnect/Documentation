9ff19e0acf50ea044986ac80009bd248d7f5cca9
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-01-29 17:25:18 -0700

    Update orders.php

--- Resources/brokerages/interactive-brokers/orders.php
+++ Resources/brokerages/interactive-brokers/orders.php
@@ -143,7 +143,7 @@
       <tr>
         <td><a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-close-orders'>MarketOnCloseOrder</a></td>
         <td><img src="https://cdn.quantconnect.com/i/tu/check.png" alt="green check" width="15px;"></td>
-        <td><img src="https://cdn.quantconnect.com/i/tu/check.png" alt="green check" width="15px;"></td>
+        <td></td>
         <td><img src="https://cdn.quantconnect.com/i/tu/check.png" alt="green check" width="15px;"></td>
         <td><img src="https://cdn.quantconnect.com/i/tu/check.png" alt="green check" width="15px;"></td>
         <td><img src="https://cdn.quantconnect.com/i/tu/check.png" alt="green check" width="15px;"></td>
