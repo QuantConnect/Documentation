1c66a67446379af29e1b7f28fe92f659874acbfe
Jared <jaredbroad@gmail.com>
2024-01-31 06:48:13 -0500

    Update 09 Custom LEAN Images.php

--- 02 Local Platform/01 Key Concepts/03 Features/09 Custom LEAN Images.php
+++ 02 Local Platform/01 Key Concepts/03 Features/09 Custom LEAN Images.php
@@ -1,2 +1,3 @@
-With the Local Platform you can build custom images of the LEAN Algorithmic Trading Engine to add extensions at your convenience.
-
+<p>
+  With the Local Platform, you can use custom images of the LEAN to add extensions or fixes at your convenience, and then run these images with the Local Platform interface.
+</p>
