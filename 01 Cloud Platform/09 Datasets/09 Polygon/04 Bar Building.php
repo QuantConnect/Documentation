6feb43cec48b4c34ad725c4b1a7dab10c4ceaf5e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-02-23 16:56:40 -0700

    Update 04 Bar Building.php

--- 01 Cloud Platform/09 Datasets/09 Polygon/04 Bar Building.php
+++ 01 Cloud Platform/09 Datasets/09 Polygon/04 Bar Building.php
@@ -1,1 +1,5 @@
-<p>The data feed is a stream of asset prices collected by WebSockets and distributed to algorithms on the platform.</p>
+<h4>Backtesting</h4>
+<p>QuantConnect Cloud doesn't currently support backtesting with the Polygon data provider.</p>
+
+<h4>Live Trading</h4>
+<p>In live trading, the data feed is a stream of asset prices, built into bars and consumed by the algorithm.</p>
