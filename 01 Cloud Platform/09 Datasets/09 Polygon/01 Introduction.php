ba5fe8bae61ed9cc53e8410697154ff9915639b2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-02-23 17:07:58 -0700

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/09 Polygon/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/09 Polygon/01 Introduction.php
@@ -1,4 +1,13 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/polygon.html"); ?>
 
-<p>The Polygon data provider streams live asset prices from Polygon. If you use this data provider and request historical data, the historical data comes from Polygon.</p>
-<p>Instead of using the data from QuantConnect or your brokerage, you can use data from <a rel='nofollow' target='_blank' href='https://polygon.io/'>Polygon</a> if you're deploying a live project and have an API key. To get an API key, see the <a href='https://polygon.io/dashboard/api-keys' rel='nofollow' target='_blank'>API Keys</a> page on the Polygon website</p>
+<p>
+  The Polygon data provider streams live asset prices from Polygon. 
+  Instead of using the data from QuantConnect or your brokerage, you can use data from <a rel='nofollow' target='_blank' href='https://polygon.io/'>Polygon</a> if you're deploying a live project and have an API key. 
+  To get an API key, see the <a href='https://polygon.io/dashboard/api-keys' rel='nofollow' target='_blank'>API Keys</a> page on the Polygon website
+  If you use this data provider and request historical data, the historical data comes from Polygon.
+</p>
+
+<p>
+  QuantConnect Cloud currently only supported streaming Polygon data for live algorithms. 
+  To download data from Polygon to use for backtesting, research, and optimizations, use the <a href='/docs/v2/lean-cli/datasets/polygon'>CLI</a>.
+</p>
