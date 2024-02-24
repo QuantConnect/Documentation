0d3a90edd4cc5a00e0bfddde1a8aa3adfcf62594
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-02-23 17:01:22 -0700

    Update 04 Bar Building.php

--- 01 Cloud Platform/09 Datasets/09 Polygon/04 Bar Building.php
+++ 01 Cloud Platform/09 Datasets/09 Polygon/04 Bar Building.php
@@ -1,5 +1,5 @@
 <h4>Backtesting</h4>
-<p>QuantConnect Cloud doesn't currently support backtesting with the Polygon data provider.</p>
+<p>The Polygon data provider is not currently supported for backtesting in QuantConnect Cloud.</p>
 
 <h4>Live Trading</h4>
 <p>In live trading, the data feed is a stream of asset prices, built into bars and consumed by the algorithm.</p>
