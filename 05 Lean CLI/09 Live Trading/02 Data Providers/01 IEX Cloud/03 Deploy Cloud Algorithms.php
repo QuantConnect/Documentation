f555030badd1dbb7ed11edd9b02df0297f3614f8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-01 14:24:21 -0700

    Update 03 Deploy Cloud Algorithms.php

--- 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/03 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/03 Deploy Cloud Algorithms.php
@@ -4,5 +4,22 @@ $isSupported=true;
 $brokerageDetails="";
 $supportsCashHoldings=true;
 $supportsPositionHoldings=true;
+$dataFeedName = "IEX Cloud";
+$dataFeedDetails = "
+    <li>Enter your IEX Cloud API Key. 
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+Configure credentials for IEX
+Your iexcloud.io API token publishable key: </pre>
+</div>
+    </li>
+
+    <li>Enter your price plan.
+<div class='cli section-example-container'>
+<pre>$ lean cloud live \"My Project\" --push --open
+IEX Cloud Price plan (Launch, Grow, Enterprise): </pre>
+</div>
+    </li>
+";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
 ?>
