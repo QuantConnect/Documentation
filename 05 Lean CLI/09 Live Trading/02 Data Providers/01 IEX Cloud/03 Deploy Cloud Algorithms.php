f57718382a771d66b06713958814e3948c289192
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-01 14:18:31 -0700

    Update and rename 03 Deploy Cloud Algorithms.html to 03 Deploy Cloud Algorithms.php

---
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/03 Deploy Cloud Algorithms.php
@@ -0,0 +1,8 @@
+<?
+$brokerageName="QuantConnect Paper Trading";
+$isSupported=true;
+$brokerageDetails="";
+$supportsCashHoldings=true;
+$supportsPositionHoldings=true;
+include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-cloud-algorithms.php");
+?>
