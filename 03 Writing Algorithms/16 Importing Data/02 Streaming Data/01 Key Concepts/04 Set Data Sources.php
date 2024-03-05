59c7d2dc0d45c6f0d03510bdc19a4e208de88ae4
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-03-05 19:34:48 +0000

    Restructure Set Data Sources
    Replace Data Tree for Table to add a column for links to examples

--- 03 Writing Algorithms/16 Importing Data/02 Streaming Data/01 Key Concepts/04 Set Data Sources.php
+++ 03 Writing Algorithms/16 Importing Data/02 Streaming Data/01 Key Concepts/04 Set Data Sources.php
@@ -1,6 +1,65 @@
-<p>The <code>GetSource</code> method in your custom data class instructs LEAN where to find the data. This method must return a <code>SubscriptionDataSource</code> object, which contains the data location and format.</p>
+<p>The <code>GetSource</code> method in your custom data class instructs LEAN where to find the data.</p>
+<div class="section-example-container">
+    <pre class="csharp">public class MyCustomDataType : BaseData
+{
+    public override SubscriptionDataSource GetSource(
+        SubscriptionDataConfig config,
+        DateTime date,
+        bool isLiveMode)
+    {
+        if (isLiveMode)
+        {
+            return new SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.Rest);
+        }
 
-<p>The following table describes the arguments the <code>SubscriptionDataSource</code> accepts:</p>
+        var source = $"http://my-ftp-server.com/{config.Symbol.Value}/{date:yyyyMMdd}.csv";
+        return new SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile);
+    }
+}</pre>
+    <pre class="python">class MyCustomDataType(PythonData):
+    def GetSource(self,
+         config: SubscriptionDataConfig,
+         date: datetime,
+         isLiveMode: bool) -&gt; SubscriptionDataSource:
+        
+         if isLiveMode:
+            return SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.Rest)
+
+        source = f"http://my-ftp-server.com/{config.Symbol.Value}/{date:%Y%M%d}.csv"
+        return SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile)
+</pre>
+</div>
+
+<p>The following table describes the arguments the <code>GetSource</code> method accepts:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>Data Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>config</code></td>
+            <td><code>SubscriptionDataConfig</code></td>
+            <td>The subscription configuration</td>
+        </tr>
+        <tr>
+            <td><code>date</code></td>
+            <td><code class="csharp">DateTime</code><code class="python">datetime</code></td>
+            <td>Date of this source file</td>
+        </tr>
+        <tr>
+            <td><code>isLiveMode</code></td>
+            <td><code>bool</code></td>
+            <td><code class="csharp">true</code><code class="python">True</code> if algorithm is running in live mode</td>
+        </tr>
+    </tbody>
+</table>
+
+<p>You can use these arguments to create <code>SubscriptionDataSource</code> objects representing different locations and formats. The following table describes the arguments the <code>SubscriptionDataSource</code> accepts:</p>
 
 <table class="qc-table table">
     <thead>
@@ -39,72 +98,40 @@
     </tbody>
 </table>
 
-<p>The <code>SubscriptionTransportMedium</code> enumeration has the following members:</p>
-
-<div data-tree="QuantConnect.SubscriptionTransportMedium"></div>
-
 <p>The <code>FileFormat</code> enumeration has the following members:</p>
 
 <div data-tree="QuantConnect.Data.FileFormat"></div>
 
-<p>The following table describes the arguments the <code>GetSource</code> method accepts:</p>
+<p>The <code>SubscriptionTransportMedium</code> enumeration has the following members:</p>
 
 <table class="qc-table table">
     <thead>
         <tr>
-            <th>Argument</th>
-            <th>Data Type</th>
+            <th>Member</th>
             <th>Description</th>
+            <th>Example</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td><code>config</code></td>
-            <td><code>SubscriptionDataConfig</code></td>
-            <td>The subscription configuration</td>
+            <td><code>LocalFile</code></td>
+            <td>The data comes from disk</td>
+            <td><a href='https://github.com/QuantConnect/Lean.DataSource.CBOE/blob/master/CBOE.cs#L60'>Lean.DataSource.CBOE</a></td>
         </tr>
         <tr>
-            <td><code>date</code></td>
-            <td><code class="csharp">DateTime</code><code class="python">datetime</code></td>
-            <td>Date of this source file</td>
+            <td><code>RemoteFile</code></td>
+            <td>The data is downloaded from a remote source</td>
+            <td><code><a href='/docs/v2/writing-algorithms/importing-data/streaming-data/custom-securities/csv-format-example#99-Demonstration-Algorithms'>Custom Securities Examples</a></td>
         </tr>
         <tr>
-            <td><code>isLiveMode</code></td>
-            <td><code>bool</code></td>
-            <td><code class="csharp">true</code><code class="python">True</code> if algorithm is running in live mode</td>
+            <td><code>Rest</code></td>
+            <td>The data comes from a rest call that is polled and returns a single line/data point of information</td>
+            <td><code>LiveMode</code> case of <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts#99-Demonstration-Algorithm'>Demonstration Algorithm</a></td>
+        </tr>
+        <tr>
+            <td><code>ObjectStore</code></td>
+            <td>The data comes from the object store</td>
+            <td><a href='/docs/v2/writing-algorithms/object-store#13-Example-of-Custom-Data'>Example of Custom Data</a></td>
         </tr>
     </tbody>
 </table>
-
-<p>You can use these arguments to create <code>SubscriptionDataSource</code> objects representing different locations and formats.</p>
-
-<div class="section-example-container">
-    <pre class="csharp">public class MyCustomDataType : BaseData
-{
-    public override SubscriptionDataSource GetSource(
-        SubscriptionDataConfig config,
-        DateTime date,
-        bool isLiveMode)
-    {
-        if (isLiveMode)
-        {
-            return new SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.Rest);
-        }
-
-        var source = $"http://my-ftp-server.com/{config.Symbol.Value}/{date:yyyyMMdd}.csv";
-        return new SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile);
-    }
-}</pre>
-    <pre class="python">class MyCustomDataType(PythonData):
-    def GetSource(self,
-         config: SubscriptionDataConfig,
-         date: datetime,
-         isLiveMode: bool) -&gt; SubscriptionDataSource:
-        
-         if isLiveMode:
-            return SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.Rest)
-
-        source = f"http://my-ftp-server.com/{config.Symbol.Value}/{date:%Y%M%d}.csv"
-        return SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile)
-</pre>
-</div>
