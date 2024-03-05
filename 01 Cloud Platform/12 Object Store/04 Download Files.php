7001efef75dfd2d234d5c981b44e5f3937100616
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-03-05 21:54:16 +0000

    Adds Download Files from Object Store

--- 01 Cloud Platform/12 Object Store/04 Download Files.php
+++ 01 Cloud Platform/12 Object Store/04 Download Files.php
@@ -1,1 +1,1 @@
-
+<? include(DOCS_RESOURCES."/object-store/download-files-from-cloud.php"); ?>
---
+++ Resources/object-store/download-files-from-cloud.php
@@ -0,0 +1,12 @@
+<p>Permissioned <a href="/docs/v2/cloud-platform/organizations/tier-features#06-Institution-Tier">Institutional</a> clients can build derivative data such as machine learning models and download it from the Object Store. <a href="mailto:support@quantconnect.com?cc=jared@quantconnect.com&amp;?subject=Download%20Files%20From%20Object%20Store">Contact us</a> to unlock this feature for your account.</p>
+
+<p>Follow these steps to download files and directories from the Object Store:</p>
+
+<ol>
+	<li>Open the <a href='/docs/v2/cloud-platform/object-store#02-View-Storage'>Object Store</a> page.</li>
+	<li>Navigate to the directory in the Object Store where you want to download files and directories from.</li>
+	<li>Select the file(s) and directory(ies) to download</li>
+	<li>Click <span class='button-name'>Download</span>.</li>
+	<li>Wait while QuantConnect processes the request</li>
+	<li>Click <a>Download</a>.</li>
+</ol>
