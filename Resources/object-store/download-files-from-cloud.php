feb783171b1d2f73630c08cfd5833ae8c6813477
Jared <jaredbroad@gmail.com>
2024-03-05 17:02:23 -0500

    Update download-files-from-cloud.php

--- Resources/object-store/download-files-from-cloud.php
+++ Resources/object-store/download-files-from-cloud.php
@@ -4,9 +4,9 @@
 
 <ol>
 	<li>Open the <a href='/docs/v2/cloud-platform/object-store#02-View-Storage'>Object Store</a> page.</li>
-	<li>Navigate to the directory in the Object Store where you want to download files and directories from.</li>
+	<li>Navigate to the directory in the Object Store where you want to download files and directories.</li>
 	<li>Select the file(s) and directory(ies) to download</li>
 	<li>Click <span class='button-name'>Download</span>.</li>
 	<li>Wait while QuantConnect processes the request</li>
-	<li>Click <a>Download</a>.</li>
+	<li>Click the <a>Download</a> link that appears.</li>
 </ol>
