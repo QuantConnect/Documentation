cac26db4d992045ee15f22ceb25b245cd4f5a779
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-03-05 23:01:35 +0000

    Adds Info About the Selector in the Resources
    Move the information to easy the access.

--- 01 Cloud Platform/04 Organizations/03 Resources/03 Research Nodes.php
+++ 01 Cloud Platform/04 Organizations/03 Resources/03 Research Nodes.php
@@ -4,4 +4,6 @@
 
 <p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each research node model. You get one free R1-4 research node in your first organization, but the node is replaced when you subscribe to a new research node in the organization.</p>
 
+<p>By default, the best-performing resource is selected when you launch a research notebook, but you can <a href='/docs/v2/cloud-platform/projects/ide#07-Manage-Nodes'>select a specific resource to use</a>.</p>
+
 <p>The CPU nodes are available on a fair usage basis. The GPU nodes can be shared with a maximum of three members. Depending on the server load, you may use all of the GPU's processing power.</p>
--- 01 Cloud Platform/04 Organizations/03 Resources/04 Live Trading Nodes.php
+++ 01 Cloud Platform/04 Organizations/03 Resources/04 Live Trading Nodes.php
@@ -4,6 +4,8 @@
 
 <p>Refer to the <a href="/pricing">Pricing</a> page to see the price of each live trading node model.</p>
 
+<p>By default, the best-performing resource is selected when you deply a live algorithm, but you can <a href='/docs/v2/cloud-platform/projects/ide#07-Manage-Nodes'>select a specific resource to use</a>.</p>
+
 <p>GPU nodes perform best on repetitive and highly-parallel tasks like training machine learning models. It takes time to transfer the data to the GPU for computation, so if your algorithm doesn't train machine learning models, the extra time it takes to transfer the data can make it appear that GPU nodes run slower than CPU nodes.</p>
 
 <p>The CPU nodes are available on a fair usage basis. The GPU nodes can be shared with a maximum of two members. Depending on the server load, you may use the all of the GPU's processing power.</p>
