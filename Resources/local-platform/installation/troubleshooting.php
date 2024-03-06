6b8aa9ec7e3a11eaf10e893bbcb1589f0a857a6b
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-03-06 22:15:29 +0000

    Adds Troubleshooting for Windows Security

--- Resources/local-platform/installation/troubleshooting.php
+++ Resources/local-platform/installation/troubleshooting.php
@@ -3,6 +3,18 @@
 <? if ($os == "windows") { ?>
 <h4>Docker with WSL 2 Features</h4>
 <? include(DOCS_RESOURCES."/cli/install/docker/wsl2.html"); ?>
+
+<h4>Windows Security</h4>
+<p>If you cannot synchonize your workpace, configure controlled folder access on your computer.</p>
+<ol>
+    <li>Press the Windows key to open the Start Menu.</li>
+    <li>Type Windows Security in the search bar and click the Open option in the right pane.</li>
+    <li>On the Windows Security homepage, click the Virus & threat protection option in the left sidebar.</li>
+    <li>Click Manage settings under the 'Virus & threat protection settings' section.</li>
+    <li>Scroll down to the 'Ransomware protection' section and click Manage ransomware protection.</li>
+    <li>Click on Allow an app through Controlled folder access under "Controlled folder access"</li>
+    <li>Click on Add an allowed app, select Recently blocked apps, and allow lean.exe</li>
+</ol>
 <? } ?>
 
 <h4>Docker Not Found</h4>
