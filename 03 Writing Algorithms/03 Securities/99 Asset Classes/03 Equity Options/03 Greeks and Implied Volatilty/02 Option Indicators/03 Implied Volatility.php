2cdd39f3d5d8b95189f194e729105cd188f1ebd8
LouisSzeto <hke0073@icloud.com>
2024-03-08 21:46:32 +0800

    additional info

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatilty/02 Option Indicators/03 Implied Volatility.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatilty/02 Option Indicators/03 Implied Volatility.php
@@ -36,7 +36,31 @@ include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
 ?>
 
 <h4>Volatility Smoothing</h4>
-<p>To perform <a href="/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/key-concepts#05-Volatility-Smoothing">IV smoothing</a>, you can call the <code>SetSmoothingFunction</code> method of the <code>ImpliedVolatility</code> object. Note that you must use the mirror-contract constructor.</p>
+<p>To perform <a href="/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/key-concepts#05-Volatility-Smoothing">IV smoothing</a>, you can call the <code>SetSmoothingFunction</code> method of the <code>ImpliedVolatility</code> object. Note that you must use the mirror-contract constructor. Belows shows the arguments you should have for the custom function</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>Data Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>iv</code></td>
+            <td><code class="csharp">decimal</code><code class="python">float</code></td>
+            <td>The calculated IV of the option contract.</td>
+        </tr>
+        <tr>
+            <td><code>mirrorIv</code></td>
+            <td><code class="csharp">decimal</code><code class="python">float</code></td>
+            <td>The calculated IV of the mirror option contract.</td>
+        </tr>
+    </tbody>
+</table>
+<p>The method should return a <code class="csharp">decimal</code><code class="python">float</code> as the smoothened IV.</p>
+
 <div class="section-example-container">
     <pre class="csharp">private ImpliedVolatility _iv;
 
