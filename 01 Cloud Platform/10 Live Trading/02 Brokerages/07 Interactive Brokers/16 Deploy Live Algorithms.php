d0293da3673e6a49cdfa21b2048e5e2ed411cf57
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 15:35:15 -0700

    Update 16 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Interactive Brokers/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Interactive Brokers/16 Deploy Live Algorithms.php
@@ -4,7 +4,7 @@ $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
 $authentication = file_get_contents(DOCS_RESOURCES."/brokerages/interactive-brokers/deploy-steps.php");
-$dataProviderDetails = file_get_contents(DOCS_RESOURCES."/brokerages/interactive-brokers/paper-trading-data-feeds.html");
+$dataProviderDetails =  "<p>In most cases, we suggest using <a href='/docs/v2/cloud-platform/datasets/interactive-brokers#07-Hybrid-QuantConnect-Data-Provider'>both the QC and IB data providers</a>.</p>" . file_get_contents(DOCS_RESOURCES."/brokerages/interactive-brokers/paper-trading-data-feeds.html");
 $postDeploy = "<li>If your IB account has 2FA enabled, tap the notification on your IB Key device and then enter your pin.</li>";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
