49c266f438db182bc9e811135274cb9a5c67e97a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 16:03:18 -0700

    Update 15 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/16 Zerodha/15 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/16 Zerodha/15 Deploy Live Algorithms.php
@@ -50,6 +50,8 @@ $authentication = "<li>Enter your Kite Connect access token and key.</li>
 </table>
 <li>Click the <span class='field-name'>History Subscription</span> field and then click <span class='button-name'>Yes</span> or <span class='button-name'>No</span> from the drop-down menu.</li>
 <p>Use this field to declare whether you have a history API subscription on your Kite Connect account.</p>";
+$omitTemplateDataProviderText = True;
+$dataProviderDetails = "<li>In the <span class='page-section-name'>Data Provider</span> section, click <span class='button-name'>Show</span> and then change the QuantConnect data provider to the Samco data provider.</li>";
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
