0f4bec115e757fd989656fb710c72cd087813801
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 15:48:33 -0700

    Update 15 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/13 Tradier/15 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/13 Tradier/15 Deploy Live Algorithms.php
@@ -26,28 +26,8 @@ $authentication .= "    <li>Click the <span class='field-name'>Environment</span
             <td>Trade with paper money</td>
         </tr>
     </tbody>
-</table>
-
-<li>Click the <span class='field-name'>Data Provider</span> field and then click one of the data providers from the drop-down menu.</li>
-<p>The following table describes the available data providers:</p>
-<table class='qc-table table'>
-    <thead>
-        <tr>
-            <th style='width: 25%'>Data Provider</th>
-            <th>Description</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>QuantConnect</td>
-            <td>Use data collected across all of the exchanges. For more details about this data provider, see <a href='/docs/v2/cloud-platform/datasets'>Datasets</a>.</td>
-        </tr>
-        <tr>
-            <td>Tradier</td>
-            <td>Use data sourced directly from Tradier. This data provider isn't available for the demo environment. For more details about this data provider, see the <a href='/docs/v2/cloud-platform/datasets/tradier'>Tradier data provider</a>.</td>
-        </tr>
-    </tbody>
 </table>";
-$postDeploy = "";
+$dataProviderDetails = "<p>If you use the Tradier data provider, it sources data directly from Tradier";
+$postDeploy = "In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/tradier'>Tradier data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean. ";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
