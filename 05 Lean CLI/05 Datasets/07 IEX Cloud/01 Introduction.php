45c96034a5566cdf10d9878280dbce446bebd39d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 09:00:37 -0700

    Update and rename 01 Introduction.html to 01 Introduction.php

---
+++ 05 Lean CLI/05 Datasets/07 IEX Cloud/01 Introduction.php
@@ -0,0 +1,4 @@
+<? 
+$startingSentence = "Instead of using the data from QuantConnect or your brokerage, you can use IEX Cloud if you're deploying a local project.";
+include(DOCS_RESOURCES."/data-providers/introduction/iex_cloud.php");
+?>
