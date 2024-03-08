d3e9030931fc359648b02a2b5853f57111b2e492
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 09:01:16 -0700

    Update and rename 01 Introduction.html to 01 Introduction.php

---
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/01 Introduction.php
@@ -0,0 +1,4 @@
+<? 
+$startingSentence = "Instead of using the live data from your brokerage, you can also use IEX Cloud.";
+include(DOCS_RESOURCES."/data-providers/introduction/iex_cloud.php");
+?>
