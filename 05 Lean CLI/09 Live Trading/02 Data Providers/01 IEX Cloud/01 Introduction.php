de186a6a362e1254d2fa69d38d38362848bde4c8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 09:09:40 -0700

    Update 01 Introduction.php

--- 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/01 IEX Cloud/01 Introduction.php
@@ -1,4 +1,4 @@
 <? 
-$startingSentence = "Instead of using the live data from your brokerage, you can also use IEX Cloud. This tutorial demonstrates how to set up the IEX Cloud data provider with the QuantConnect Paper Trading brokerage.";
+$startingSentence = "Instead of using the live data from your brokerage, you can also use IEX Cloud. This tutorial demonstrates how to set up the IEX Cloud data provider with the QuantConnect Paper Trading brokerage.</p><p>";
 include(DOCS_RESOURCES."/data-providers/introduction/iex_cloud.php");
 ?>
