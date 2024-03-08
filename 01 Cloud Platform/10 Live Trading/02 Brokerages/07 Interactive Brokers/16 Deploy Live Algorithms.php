4c3747344c4eee139ac73e01edb518c90c8a13e4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-08 15:30:20 -0700

    Update 16 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Interactive Brokers/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Interactive Brokers/16 Deploy Live Algorithms.php
@@ -4,6 +4,7 @@ $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
 $authentication = file_get_contents(DOCS_RESOURCES."/brokerages/interactive-brokers/deploy-steps.php");
+$dataProviderDetails = file_get_contents(DOCS_RESOURCES."/brokerages/interactive-brokers/paper-trading-data-feeds.html");
 $postDeploy = "<li>If your IB account has 2FA enabled, tap the notification on your IB Key device and then enter your pin.</li>";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
