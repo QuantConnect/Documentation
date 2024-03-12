07fbfb3fb574bc4aa33a448be7b5e54fa66943e2
LouisSzeto <hke0073@icloud.com>
2024-03-12 13:57:21 +0800

    IV smoothing on IV property of greeks

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatilty/02 Option Indicators/04 Delta.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatilty/02 Option Indicators/04 Delta.php
@@ -34,3 +34,9 @@ $typeName = "Delta";
 $indicatorPage = "delta";
 include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
 ?>
+
+<h4>Volatility Smoothing</h4>
+<?
+$typeName = "delta";
+include(DOCS_RESOURCES."/option-indicators/iv-smoothing.php");
+?>
