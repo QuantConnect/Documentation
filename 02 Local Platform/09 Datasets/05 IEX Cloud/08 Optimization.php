c926a947ef61855b5b65cb88e7ab43acc75fa64e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-15 14:02:07 -0600

    Update 08 Optimization.php

--- 02 Local Platform/09 Datasets/05 IEX Cloud/08 Optimization.php
+++ 02 Local Platform/09 Datasets/05 IEX Cloud/08 Optimization.php
@@ -1,4 +1,5 @@
 <? 
 $dataProviderName = "IEX Cloud";
+$backtestLink = "/docs/v2/lean-cli/datasets/iex-cloud#02-Backtesting";
 include(DOCS_RESOURCES."/data-providers/local-platform-optimization.php"); 
 ?>
