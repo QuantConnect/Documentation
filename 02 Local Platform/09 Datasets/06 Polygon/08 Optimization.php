d0afafe1fa49725e6fbba7e3ac3c22abfbaa2796
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-15 14:01:46 -0600

    Update 08 Optimization.php

--- 02 Local Platform/09 Datasets/06 Polygon/08 Optimization.php
+++ 02 Local Platform/09 Datasets/06 Polygon/08 Optimization.php
@@ -1,4 +1,5 @@
 <? 
 $dataProviderName = "Polygon";
+$backtestLink = "/docs/v2/lean-cli/datasets/polygon#02-Backtesting";
 include(DOCS_RESOURCES."/data-providers/local-platform-optimization.php"); 
 ?>
