a92ca5354526cd261489c20b1b9efad35baa5159
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-15 13:50:25 -0600

    Update 06 Research.php

--- 02 Local Platform/09 Datasets/05 IEX Cloud/06 Research.php
+++ 02 Local Platform/09 Datasets/05 IEX Cloud/06 Research.php
@@ -1,4 +1,5 @@
 <? 
 $dataProviderName = "IEX Cloud";
+$researchLink = "/docs/v2/lean-cli/datasets/iex-cloud#03-Research";
 include(DOCS_RESOURCES."/data-providers/local-platform-research.php"); 
 ?>
