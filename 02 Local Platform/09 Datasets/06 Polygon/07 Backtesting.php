a3470320f08352577c1f6d1c3aa643c525c234ee
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-15 13:48:38 -0600

    Update 07 Backtesting.php

--- 02 Local Platform/09 Datasets/06 Polygon/07 Backtesting.php
+++ 02 Local Platform/09 Datasets/06 Polygon/07 Backtesting.php
@@ -1,4 +1,5 @@
 <? 
+$dataProviderName = "Polygon";
 $backtestingLink = "/docs/v2/lean-cli/datasets/polygon#02-Backtesting";
 include(DOCS_RESOURCES."/data-providers/local-platform-backtesting.php"); 
 ?>
