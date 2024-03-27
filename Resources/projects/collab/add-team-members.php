32707441e4b0da083117a2359a8a2937e0c474b0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-03-27 09:47:22 -0600

    Update add-team-members.php

--- Resources/projects/collab/add-team-members.php
+++ Resources/projects/collab/add-team-members.php
@@ -8,7 +8,7 @@
     <? if ($localPlatform) { ?> <li>In the left navigation menu, click the <img class='inline-icon' src='https://cdn.quantconnect.com/i/tu/vscode-qc-icon.jpg'> <span class='icon-name'>QuantConnect</span> icon.</li><? } ?>
     <li>In the Collaborate section of the Project panel, click <span class='button-name'>Add Collaborator</span>.</li>
     <li>Click the <span class='field-name'>Select User...</span> field and then click a member from the drop-down menu.</li>
-    <li>If you want to give the member live control of the project, select the <span class='box-name'>Live Control</span> check box.</li>
+    <li>If you want to give the member <a href='/docs/v2/cloud-platform/live-trading/algorithm-control'>control of the project's live deployments</a>, select the <span class='box-name'>Live Control</span> check box.</li>
     <li>Click <span class='button-name'>Add User</span>.</li>
     <p>The member you add receives an email with a link to the project.</p>
 </ol>
