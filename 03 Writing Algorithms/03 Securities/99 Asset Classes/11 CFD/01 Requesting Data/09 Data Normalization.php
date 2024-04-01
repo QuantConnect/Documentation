116e440a3a252f8491f0d23ae10fe63f9e9e34ef
Web Editor <web.editor@quantconnect.com>
2024-04-01 15:30:34 -0400

    Automated push from server made by Jared Broad

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/09 Data Normalization.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/09 Data Normalization.php
@@ -1,1 +1,4 @@
 <? $specificAsset = ""; $additionalInformation = ""; include(DOCS_RESOURCES."/securities/data-normalization-raw-only.php"); ?>
+<p>
+Interactive Brokers Stock CFDs apply corporate actions to the price of the asset, including paying dividends in cash to your account. This behavior is not modelled today and splits will result in price discontinuities. 
+</p>
