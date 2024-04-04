4840d450ce964e672effc1fa50d635667182d6d2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-04 11:51:18 -0600

    Update 04 Autokeras Environment.php

--- 01 Cloud Platform/06 Projects/11 Package Environments/04 Autokeras Environment.php
+++ 01 Cloud Platform/06 Projects/11 Package Environments/04 Autokeras Environment.php
@@ -1,4 +1,4 @@
-<p class="csharp">If you use C#, this environment isn't available.</p>
+<p class="csharp">This environment is only available for Python.</p>
 <p class="python">The Autokeras environment provides a backwards compatability environment for compatibility with <code>Keras < 3</code>, <code>Tensorflow 2.14.1</code>, and <code>Pydantic</code>:</p>
 <div class="python">
     <? include(DOCS_RESOURCES."/libraries/supported-libraries-foundation-autokeras.html"); ?>
