64f9f8c1f94b1674e8340e16109a21604d50a46d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-04 12:05:52 -0600

    Update 03 Autokeras Environment Libraries.php

--- 03 Writing Algorithms/01 Key Concepts/12 Libraries/03 Autokeras Environment Libraries.php
+++ 03 Writing Algorithms/01 Key Concepts/12 Libraries/03 Autokeras Environment Libraries.php
@@ -1,4 +1,4 @@
-<p class="csharp">The Autokeras environment cannot be selected on C# projects.</p>
+<p class="csharp">The Autokeras environment is only available for Python projects.</p>
 <p class="python">The Autokeras environment provides a backwards compatability environment for compatibility with <code>Keras < 3</code>, <code>Tensorflow 2.14.1</code>, and <code>Pydantic</code>:</p>
 
 <div class="python">
