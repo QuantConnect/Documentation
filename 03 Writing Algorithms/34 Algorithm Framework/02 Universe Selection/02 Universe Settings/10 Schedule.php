98ce2c54e9de795ab318f3caf9b4afa39991ffe4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-05 09:44:51 -0600

    Add scheduled universes

---
+++ 03 Writing Algorithms/34 Algorithm Framework/02 Universe Selection/02 Universe Settings/10 Schedule.php
@@ -0,0 +1,16 @@
+<p>
+The <code>Schedule</code> setting defines the selection schedule of the universe. 
+Most universes run on a daily schedule.
+To change the selection schedule, call the <code>UniverseSettings.Schedule.On</code> method with an <code>IDateRule</code> object before you create the Universe Selection model.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">UniverseSettings.Schedule.On(DateRules.MonthStart());
+AddUniverseSelection(new ETFConstituentsUniverseSelectionModel("QQQ"));</pre>
+    <pre class="python">self.UniverseSettings.Schedule.On(self.DateRules.MonthStart())
+self.AddUniverseSelection(ETFConstituentsUniverseSelectionModel("QQQ"))</pre>
+</div>
+
+<? include(DOCS_RESOURCES."/scheduled-events/date-rules.html"); ?>
+
+<p>In live trading, scheduled universes run at roughly 8 AM Eastern Time to ensure there is enough time for the data to process.</p>
