41456f7427d0a126db1ba04d0191bb3dff5a0632
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-05 16:56:31 -0600

    Update 01 Introduction.php

--- 08 Drafts/06 Range Consolidator/01 Introduction.php
+++ 08 Drafts/06 Range Consolidator/01 Introduction.php
@@ -1,4 +1,4 @@
-<?php 
-include(DOCS_RESOURCES."/consolidators/consolidator-info/renko/introduction.html"); 
-include(DOCS_RESOURCES."/consolidators/consolidator-info/renko/class-definition.php");
+<? 
+include(DOCS_RESOURCES."/consolidators/consolidator-info/range/introduction.html"); 
+include(DOCS_RESOURCES."/consolidators/consolidator-info/range/class-definition.php");
 ?>
