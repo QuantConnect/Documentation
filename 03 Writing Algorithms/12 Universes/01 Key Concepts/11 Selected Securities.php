6b5bf7519d8d9df5116d033391a90f8bee5f2b20
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-05 16:13:25 -0600

    Create 11 Selected Securities.php

---
+++ 03 Writing Algorithms/12 Universes/01 Key Concepts/11 Selected Securities.php
@@ -0,0 +1,42 @@
+<p>
+  The <code>Selected</code> property of your <code>Universe</code> contains references to all the assets that are currently in the universe.
+  The <code>Universe.Selected</code> property differs from the <code>QCAlgorithm.ActiveSecurities</code> property because <code>QCAlgorithm.ActiveSecurities</code> can contain more assets than <code>Universe.Selected</code>.
+  To access the <code>Universe</code> object, save a reference to the result of the <code>AddUniverse</code> method.
+  The following algorithm demonstrates how to use the <code>Universe.Selected</code> property to create simple rebalancing strategies:
+</p>
+
+ <div class="section-example-container">
+<pre class="csharp">public class SimpleRebalancingAlgorithm : QCAlgorithm
+{
+    public override void Initialize()
+    {
+        var symbol = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
+        var dateRule = DateRules.WeekStart(symbol);
+        UniverseSettings.Schedule.On(dateRule);
+        var universe = AddUniverse(Universe.DollarVolume.Top(5));
+        Schedule.On(
+            dateRule,
+            TimeRules.AfterMarketOpen(symbol, 30),
+            () => SetHoldings(
+                universe.Selected.Select(symbol => new PortfolioTarget(symbol, 1.0m/universe.Selected.Count)).ToList(),
+                true
+            )
+        );
+    }
+}</pre>
+<pre class="python">class SimpleRebalancingAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        symbol = Symbol.Create("SPY", SecurityType.Equity, Market.USA)
+        date_rule = self.DateRules.WeekStart(symbol)
+        self.UniverseSettings.Schedule.On(date_rule)
+        universe = self.AddUniverse(self.Universe.DollarVolume.Top(5))
+        self.Schedule.On(
+            date_rule,
+            self.TimeRules.AfterMarketOpen(symbol, 30),
+            lambda: self.SetHoldings(
+                [PortfolioTarget(symbol, 1/len(universe.Selected)) for symbol in universe.Selected], 
+                True
+            )
+        )</pre>
+</div>
