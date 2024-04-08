8f7f8a1f7b6cbc7912925c4dae8062a327ad3b9d
LouisSzeto <hke0073@icloud.com>
2024-04-08 11:04:06 +0800

    Add image for range consolidator

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/06 Range Consolidators/02 Classic Range Consolidators/01 Introduction.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/06 Range Consolidators/02 Classic Range Consolidators/01 Introduction.php
@@ -1,4 +1,7 @@
 <?php 
 include(DOCS_RESOURCES."/consolidators/consolidator-info/classic-renko/introduction.html"); 
+?>
+<img src="https://cdn.quantconnect.com/i/tu/range-bar.png" alt="RangeBar sample">
+<?
 include(DOCS_RESOURCES."/consolidators/consolidator-info/classic-renko/class-definition.php");
 ?>
