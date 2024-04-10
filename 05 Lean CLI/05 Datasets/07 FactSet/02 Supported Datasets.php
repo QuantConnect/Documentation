85b027b354a4b8788e9e58792d681e40c7f1c1f7
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-04-10 19:42:00 +0100

    Adds FactSett Dataset Page

---
+++ 05 Lean CLI/05 Datasets/07 FactSet/02 Supported Datasets.php
@@ -0,0 +1,2 @@
+<p>Our FactSet integration supports securities from the following asset classes:</p>
+<? include(DOCS_RESOURCES."/data-providers/supported-assets/factset.html"); ?>
---
+++ 05 Lean CLI/05 Datasets/07 FactSet/03 Universe Selection.php
@@ -0,0 +1,4 @@
+<? 
+$dataProviderName = "FactSet";
+include(DOCS_RESOURCES."/data-providers/local-platform-universe-selection.php"); 
+?>
