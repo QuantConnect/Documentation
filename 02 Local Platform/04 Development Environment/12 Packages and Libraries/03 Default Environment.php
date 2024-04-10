f8810eac47e82dc80d1da797c7ed99930a2d9782
LouisSzeto <hke0073@icloud.com>
2024-04-10 20:32:46 +0800

    add cloud platform tag

--- 02 Local Platform/04 Development Environment/12 Packages and Libraries/03 Default Environment.php
+++ 02 Local Platform/04 Development Environment/12 Packages and Libraries/03 Default Environment.php
@@ -1,2 +1,5 @@
 <p>The default environment supports the following libraries:</p>
-<? include(DOCS_RESOURCES."/libraries/supported-libraries.php"); ?>
+<? 
+$cloudPlatform = false;
+include(DOCS_RESOURCES."/libraries/supported-libraries.php"); 
+?>
