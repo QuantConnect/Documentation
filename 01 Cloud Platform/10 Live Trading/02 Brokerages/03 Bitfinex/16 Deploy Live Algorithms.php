90ed5abd1f4c02442416046c2f3fb208fc803fbd
LouisSzeto <hke0073@icloud.com>
2024-04-11 20:51:36 +0800

    pep8 python code change

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/03 Bitfinex/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/03 Bitfinex/16 Deploy Live Algorithms.php
@@ -4,9 +4,9 @@ $cashState = false;
 $holdingsState = false;
 $secondBullet = "<p>If you are deploying a paper trading algorithm without the QuantConnect Paper Trading brokerage, include the following lines of code in the <code>Initialize</code> method of your algorithm:</p>
 <div class='section-example-container'>
-    <pre class='python'>self.SetAccountCurrency('TESTUSD') # or 'TESTUSDT'
-self.SetBrokerageModel(BrokerageName.Bitfinex, AccountType.Cash)
-self.SetBenchmark(lambda x: 0) # or the Symbol of the TESTBTCTESTUSD/TESTBTCTESTUSDT securities</pre>
+    <pre class='python'>self.set_account_currency('TESTUSD') # or 'TESTUSDT'
+self.set_brokerage_model(BrokerageName.BITFINEX, AccountType.cash)
+self.set_benchmark(lambda x: 0) # or the Symbol of the TESTBTCTESTUSD/TESTBTCTESTUSDT securities</pre>
     <pre class='csharp'>SetAccountCurrency(\"TESTUSD\"); // or \"TESTUSDT\"
 SetBrokerageModel(BrokerageName.Bitfinex, AccountType.Cash);
 SetBenchmark(x =&gt; 0); // or the Symbol of the TESTBTCTESTUSD/TESTBTCTESTUSDT securities</pre>
