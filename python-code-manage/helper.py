3f2c4db4f62e329ae85f257c29325e10e2928a2b
LouisSzeto <hke0073@icloud.com>
2024-04-12 21:00:33 +0800

    pep8 conversion script

---
+++ python-code-manage/helper.py
@@ -0,0 +1,59 @@
+import re
+from exceptions import ENUMS, SWAPS, EXCEPTIONS
+
+def get_code_snippet(pre_content):
+    content = str(pre_content).split('>', maxsplit=1)[1].rsplit("</pre>", 1)[0]
+    return content
+
+def conversion(content):
+    # do not check for the string in EXCEPTIONS list
+    content_to_check = content
+    for e in EXCEPTIONS:
+        content_to_check = content_to_check.replace(e, "")
+
+    methods = re.findall(r"def\s+(\w+)\s*\(", content_to_check)
+
+    for method in sorted(set(methods), key=len, reverse=True):
+        snake_case_method = _title_to_snake_case(method)
+        content = content.replace(f"def {method}(", f"def {snake_case_method}(")
+    
+    methods = re.findall(r"\.(\w+)", content_to_check)
+
+    for method in sorted(set(methods), key=len, reverse=True):
+        snake_case_method = _title_to_snake_case(method)
+        content = content.replace(f".{method}", f".{snake_case_method}")
+    
+    methods = re.findall(r"<\?=\$research \? \"qb\.\" : \"self\.\"\?>(\w+)", content_to_check)
+
+    for method in sorted(set(methods), key=len, reverse=True):
+        snake_case_method = _title_to_snake_case(method)
+        content = content.replace(f"<?=$research ? \"qb.\" : \"self.\"?>{method}", f"<?=$research ? \"qb.\" : \"self.\"?>{snake_case_method}")
+    
+    methods = re.findall(r"<\?=\$pythonPrefix\?>(\w+)", content_to_check)
+
+    for method in sorted(set(methods), key=len, reverse=True):
+        snake_case_method = _title_to_snake_case(method)
+        content = content.replace(f"<?=$pythonPrefix?>{method}", f"<?=$pythonPrefix?>{snake_case_method}")
+        
+    for enum in sorted(ENUMS, key=len, reverse=True):
+        wanted_pattern = fr"(?![\.]){enum}\.(\w+)(?!\(|\.)"
+        unwanted_pattern = fr"(?![\.]){enum}\.(\w+)[\(\.]"
+        wanted_methods = re.findall(wanted_pattern, f"{content}<")
+        unwanted_methods = [x[:-1] for x in re.findall(unwanted_pattern, f"{content}<")]
+        
+        for method in sorted(set(wanted_methods).difference(set(unwanted_methods)), key=len, reverse=True):
+            content = content.replace(f"{enum}.{method}", f"{enum}.{method.upper()}")
+    
+    return content
+
+def _title_to_snake_case(title):
+    for original, new in SWAPS.items():
+        if original in title:
+            title = title.replace(original, new)
+        
+    # for whole method is upper case: indicators, ID, ...
+    if title.isupper():
+        return title
+    
+    snake_case = re.sub(r'(?<!^)(?=[A-Z])', '_', title).lower()
+    return snake_case
---
+++ python-code-manage/python-code-modifier.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from pathlib import Path
+from helper import *
+from exceptions import SUFFIXES
+
+file_count = 0
+code_snippet_count = 0
+
+for path in [p for suffix in SUFFIXES for p in Path().resolve().rglob(suffix)]:
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+        soup = BeautifulSoup(content, 'html.parser')
+
+        for code_section in soup.find_all("div", class_="section-example-container"):
+            for code_snippet in code_section.find_all('pre', {'class' : 'python'}):
+                clean_snippet = get_code_snippet(code_snippet)
+                converted_snippet = conversion(clean_snippet)
+                content = content.replace(clean_snippet, converted_snippet)
+                
+                code_snippet_count += 1
+                
+        for code_section in soup.find_all("div", class_="python section-example-container"):
+            for code_snippet in code_section.find_all('pre'):
+                clean_snippet = get_code_snippet(code_snippet)
+                converted_snippet = conversion(clean_snippet)
+                content = content.replace(clean_snippet, converted_snippet)
+                
+                code_snippet_count += 1
+
+        for code_section in soup.find_all("code", class_="python"):
+            clean_snippet = get_code_snippet(code_section)
+            converted_snippet = conversion(clean_snippet)
+            content = content.replace(clean_snippet, converted_snippet)
+            
+            code_snippet_count += 1
+                
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(content)
+            
+    file_count += 1
+            
+print(f"Found {code_snippet_count} pieces of code snippets from {file_count} files.")
