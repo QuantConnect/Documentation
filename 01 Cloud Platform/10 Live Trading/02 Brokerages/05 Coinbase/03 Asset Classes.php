4ebc0cf0ffe0eb7e83c7cdfda52bb8b86ea59d51
LouisSzeto <hke0073@icloud.com>
2024-04-12 12:51:23 +0800

    fix enums

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/03 Asset Classes.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/05 Coinbase/03 Asset Classes.php
@@ -2,7 +2,7 @@
 
 <div class="section-example-container">
     <pre class="csharp">AddCrypto("BTCUSD", Resolution.Minute, Market.Coinbase);</pre>
-    <pre class="python">self.add_crypto("BTCUSD", Resolution.MINUTE, Market.coinbase)</pre>
+    <pre class="python">self.add_crypto("BTCUSD", Resolution.MINUTE, Market.COINBASE)</pre>
 </div>
 
 <p>If you call the <code>SetBrokerageModel</code> method with the correct <code>BrokerageName</code>, then you don't need to pass a <code>Market</code> argument to the <code>AddCrypto</code> method because the brokerage model has a <a href='/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/coinbase#17-Default-Markets'>default market</a>.</p>
