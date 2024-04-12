f44df27081a82c3b978e10256a146346682dec5a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-12 11:35:56 -0600

    Update 06 Vega.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatility/02 Option Indicators/06 Vega.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatility/02 Option Indicators/06 Vega.php
@@ -1,4 +1,7 @@
-<p>The Vega, <script type="math/tex">\nu</script>, is the rate of change in the value of the option with respect to the volatility of the underlying asset.</p>
+<p>
+    Vega, <script type="math/tex">\nu</script>, is the rate of change in the value of the Option with respect to the volatility of the underlying asset.
+    For more information about vega, see <a href='/learning/articles/introduction-to-options/the-greek-letters#vega'>Vega</a>.
+</p>
 
 <h4>Automatic Indicator</h4>
 <?
@@ -7,7 +10,8 @@ $helperMethod = "V";
 include(DOCS_RESOURCES."/option-indicators/automatic-indicator.php"); 
 ?>
 
-<p>Note that the <code>V</code> method has an extra argument.</p>
+<p>The follow table describes the arguments that the <code>V</code> method accepts in addition to the <a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/option-indicators#02-Parameters'>standard parameters</a>:</p>
+
 
 <table class="qc-table table">
     <thead>
@@ -22,14 +26,21 @@ include(DOCS_RESOURCES."/option-indicators/automatic-indicator.php");
         <tr>
             <td><code>ivModel</code></td>
             <td><code>OptionPricingModelType</code></td>
-            <td>The option pricing model used to estimate the IV for Greek calculation. Will use the <code>optionModel</code> if not specified.</td>
+            <td>
+                The Option pricing model to use to estimate the IV when calculating Vega. 
+                If you don't provide a value, the default value is to match the <code>optionModel</code> parameter.
+            </td>
             <td><code class="csharp">null</code><code class="python">None</code></td>
         </tr>
     </tbody>
 </table>
 
+
+<p>For more information about the <code>D</code> method, see <a href='/docs/v2/writing-algorithms/indicators/supported-indicators/vega#02-Using-V-Indicator'>Using V Indicator</a>.</p>
+
 <h4>Manual Indicator</h4>
 <?
+$name = "vega";
 $typeName = "Vega";
 $indicatorPage = "vega";
 include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
@@ -37,6 +48,7 @@ include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
 
 <h4>Volatility Smoothing</h4>
 <?
+$name = "vega";
 $typeName = "vega";
 include(DOCS_RESOURCES."/option-indicators/iv-smoothing.php");
 ?>
