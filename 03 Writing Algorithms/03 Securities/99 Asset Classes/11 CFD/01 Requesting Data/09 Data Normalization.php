fb9c03200728546691a034668b4cd8119b1668ff
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-12 16:04:48 -0600

    Update 09 Data Normalization.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/09 Data Normalization.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/09 Data Normalization.php
@@ -1,4 +1,4 @@
 <? $specificAsset = ""; $additionalInformation = ""; include(DOCS_RESOURCES."/securities/data-normalization-raw-only.php"); ?>
 <p>
-Interactive Brokers Stock CFDs apply corporate actions to the price of the asset, including paying dividends in cash to your account. This behavior is not modeled today and splits in live trading will result in price discontinuities. Algorithms should monitor the underlying equity contracts for events and use the underlying Equity contracts for indicators. 
+Interactive Brokers Stock CFDs apply corporate actions to the price of the asset, including paying dividends in cash to your account. This behavior is not modeled today and splits in live trading will result in price discontinuities. You should <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-actions'>monitor the underlying Equity</a> for events and use the underlying Equity for indicators. 
 </p>
