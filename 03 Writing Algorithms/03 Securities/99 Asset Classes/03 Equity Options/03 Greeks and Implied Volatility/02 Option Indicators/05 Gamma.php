effc938abb5a7b756e9e70912d9bc00ff5a68725
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-04-12 11:30:26 -0600

    Update 05 Gamma.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatility/02 Option Indicators/05 Gamma.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/03 Greeks and Implied Volatility/02 Option Indicators/05 Gamma.php
@@ -1,13 +1,19 @@
-<p>Gamma, <script type="math/tex">\Gamma</script>, is the rate of change of the portfolio's delta with respect to the underlying asset's price. It represents the second-order sensitivity of the option to a movement in the underlying asset's price.</p>
+<p>
+    Gamma, <script type="math/tex">\Gamma</script>, is the rate of change of the portfolio's delta with respect to the underlying asset's price. 
+    It represents the second-order sensitivity of the Option to a movement in the underlying asset's price.
+    For more information about Gamma, see <a href='/learning/articles/introduction-to-options/the-greek-letters#gamma'>Gamma</a>.
+</p>
 
 <h4>Automatic Indicator</h4>
 <?
+$name = "gamma";
 $typeName = "Gamma";
 $helperMethod = "G";
 include(DOCS_RESOURCES."/option-indicators/automatic-indicator.php"); 
 ?>
 
-<p>Note that the <code>G</code> method has an extra argument.</p>
+<p>The follow table describes the arguments that the <code>G</code> method accepts in addition to the <a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/option-indicators#02-Parameters'>standard parameters</a>:</p>
+
 
 <table class="qc-table table">
     <thead>
@@ -28,8 +34,11 @@ include(DOCS_RESOURCES."/option-indicators/automatic-indicator.php");
     </tbody>
 </table>
 
+<p>For more information about the <code>D</code> method, see <a href='/docs/v2/writing-algorithms/indicators/supported-indicators/gamma#02-Using-G-Indicator'>Using G Indicator</a>.</p>
+
 <h4>Manual Indicator</h4>
 <?
+$name = "gamma";
 $typeName = "Gamma";
 $indicatorPage = "gamma";
 include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
