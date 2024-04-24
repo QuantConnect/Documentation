31e47a8caedfb19c33c69378f04a63fb720cd521
LouisSzeto <hke0073@icloud.com>
2024-04-24 15:18:59 +0800

    Add jump (id) link, extra h3 content, recursive generation level, "field" object; clean codes up; fix bugs

--- code-generators/writing-algorithm-api-reference-sphinx-code-generator.py
+++ code-generators/writing-algorithm-api-reference-sphinx-code-generator.py
@@ -1,3 +1,4 @@
+import copy
 import json
 from pathlib import Path
 import re
@@ -5,68 +6,139 @@ from urllib.request import urlopen
 
 LEAN = "https://github.com/QuantConnect/Lean/blob/master"
 RAW_LEAN = "https://raw.githubusercontent.com/QuantConnect/Lean/master"
-TYPE_JSON = "https://www.quantconnect.com/services/inspector?type=T:QuantConnect.Algorithm.QCAlgorithm&language=python"
+LEAN_SERVICE = "https://www.quantconnect.com/services/inspector?language=python&type=T:%s"
 SOURCE_FILE = "Algorithm/QCAlgorithm.cs"
 
 WRITE_PATH = Path("08 Drafts/98 API Reference")   #"03 Writing Algorithms/98 API Reference/"
 WRITE_PATH.mkdir(parents=True, exist_ok=True)
-WRITE_FILE = WRITE_PATH / "01 QCAlgorithm API.html"
 
+DOCS_SECTION = {
+    "QCAlgorithm API": "QuantConnect.Algorithm.QCAlgorithm",
+    "Universe": "QuantConnect.Data.UniverseSelection.Universe",
+    "Security": "QuantConnect.Securities.Security",
+    "OrderTicket": "QuantConnect.Orders.OrderTicket",
+    "OrderEvent": "QuantConnect.Orders.OrderEvent"
+}
+MAX_RECURSION = 1
+
+EXTRAS = {}
+DONE = []
 SOURCE_CODES = {}
 XML_REGEX_PATTERNS = [r'<see cref="(.*?)"(?:\s*/)?>', r'typeparam name="(.*?)"(?:\s*/)?']
 
-def render_qcalgorithm_api():
-    content = json.loads(urlopen(TYPE_JSON).read())
+STYLE = '''
+<style>
+.code-source        { color: grey; float: right }
+.object-type        { font-style: italic; }
+.arg-name           { font-weight: bold; }
+.subsection-header  { font-weight: bold; background-color: lightgray; }
+.subsection-content { margin-left: 10px; }
+</style>
+'''
+
+def render_docs():
+    for i, (h3, type_json_url) in enumerate(DOCS_SECTION.items()):
+        _render_section_docs(i, h3, type_json_url)
+        DONE.append(h3.strip().lower())
+        
+    for x in list(EXTRAS.keys()):
+        if x.strip().lower() in DONE:
+            del EXTRAS[x]
     
-    type_heading_html = _render_type_heading(f'{content["type-name"]} API', content["description"], content["full-type-name"])
+    iters = 0
+    while len(EXTRAS) > 0 and iters < MAX_RECURSION:
+        extra_copy = copy.deepcopy(EXTRAS)
+        EXTRAS.clear()
+        
+        for j, (h3, type_json_url) in enumerate(sorted(extra_copy.items(), key=lambda x: x[0])):
+            _render_section_docs(i+j+1, h3, type_json_url)
+            DONE.append(h3.strip().lower())
+        
+        for x in list(EXTRAS.keys()):
+            if x.strip().lower() in DONE:
+                del EXTRAS[x]
+                
+        i = i+j+1
+        iters += 1
+
+def _render_section_docs(i, write_location, type_json_url):
+    content = json.loads(urlopen(LEAN_SERVICE % type_json_url).read())
+    
+    # Not a type, do not render
+    if "type-name" not in content:
+        return
+    
+    type_heading_html = _render_type_heading(content["type-name"], content["description"], content["full-type-name"])
     methods = _render_methods(content["methods"])
     properties = _render_properties(content["properties"])
+    fields = _render_fields(content["fields"])
     
-    return f'''{type_heading_html}
+    html = f'''{type_heading_html}
 <div class=\"subsection-content\">
 {methods}
 </div>
 <div class=\"subsection-content\">
 {properties}
+</div>
+<div class=\"subsection-content\">
+{fields}
 </div>'''
 
-def _render_type_heading(type_name, type_description, type_full_name, type_extend_description=""):
-    return f'''<h3 class=\"type-name\">{type_name}</h3>
-<p>{extract_xml_content(type_description)}</p>
-<div class=\"code-snippet\"><pre><span class=\"object-type\">class</span> {type_full_name}()<a class=\"code-source\" href=\"{LEAN}/{SOURCE_FILE}\">[source]</a>
+    with open(WRITE_PATH / f"{i+1:02} {write_location}.html", 'w', encoding="utf-8") as file:
+        file.write(html)
+        file.write(STYLE)
+
+def _render_type_heading(type_name, type_description, type_full_name):
+    return f'''<div class=\"code-snippet\"><pre id=\"{type_name}\"><span class=\"object-type\">class</span> {type_full_name}()<a class=\"code-source\" href=\"{LEAN}/{SOURCE_FILE}\">[source]</a>
 </pre></div>
-{type_extend_description}'''
+<p>{extract_xml_content(type_description)}</p>
+'''
+
+def _render_types(type_, type_list):
+    types = []
+    for type in sorted(type_list, key=lambda x: x[f"{type_}-name"]):
+        type_html = eval(f"_render_{type_}(type)")
+        types.append(type_html)
+    return '\n'.join(types)
+
+def _render_type(type_, type_dict, type_ret=None, line_arg="", params=""):
+    doc_attr = type_dict["documentation-attributes"][0] if "documentation-attributes" in type_dict and len(type_dict["documentation-attributes"]) > 0 else None
+    line = doc_attr["line"] if doc_attr else None
+    source_url = f'{doc_attr["fileName"]}#L{line}' if doc_attr else None
+    
+    if not type_ret:
+        type_ret = f"{type_}-full-type-name"
+    type_name = type_dict[type_ret] \
+        if type_dict[type_ret] and type_dict[type_ret].split('.')[0] == "QuantConnect" \
+        else type_dict[type_ret.replace("full", "short")]
+    type_return = eval(f'_get_{type_}_return(type_name, type_dict["{type_}-description"], source_url, line)')
+    type_description = extract_xml_content(type_dict[f"{type_}-description"])
+    source_html = f"<a class=\"code-source\" href=\"{LEAN}/{source_url}\">[source]</a>" if source_url else ""
+    
+    return f'''<div class=\"code-snippet\"><pre>{"" if type_ == "method" else '<span class="object-type">' + type_ + "</span> "}{type_dict[f"{type_}-name"]}{line_arg}{source_html}</pre></div>
+<div class=\"subsection-content\">
+<p>{type_description}</p>
+{params}
+{type_return}
+</div>
+'''
 
 def _render_methods(method_list):
-    methods = []
-    for method in sorted(method_list, key=lambda x: x["method-name"]):
-        method_html = _render_method(method)
-        methods.append(method_html)
-    return '\n'.join(methods)
+    return _render_types("method", method_list)
 
 def _render_method(method_dict):
     line_arguments = ""
     if "method-arguments" in method_dict:
         line_arguments = ", ".join(f'{arg["argument-name"]}={arg["argument-default"]}' if arg["argument-optional"] else arg["argument-name"]
                                for arg in method_dict["method-arguments"])
+        line_arguments = f"<span class=\"object-type\">{line_arguments}</span>"
 
-    # Do not render the ones without documentation attributes
-    if len(method_dict["documentation-attributes"]) == 0:
-        return ""
-
-    doc_attr = method_dict["documentation-attributes"][0]
-    source_url = f'{doc_attr["fileName"]}#L{doc_attr["line"]}'
-    method_params = _get_params(method_dict["method-arguments"], source_url, doc_attr["line"])
-    method_return = _get_return(method_dict["method-return-type-short-name"], source_url, doc_attr["line"])
-    method_description = extract_xml_content(method_dict["method-description"])
-
-    return f'''<div class=\"code-snippet\"><pre>{method_dict["method-name"]}(<span class=\"object-type\">{line_arguments}</span>)<a class=\"code-source\" href=\"{LEAN}/{source_url}\">[source]</a></pre></div>
-<div class=\"subsection-content\">
-<p>{method_description}</p>
-{method_params}
-{method_return}
-</div>
-'''
+    doc_attr = method_dict["documentation-attributes"][0] if "documentation-attributes" in method_dict and len(method_dict["documentation-attributes"]) > 0 else None
+    line = doc_attr["line"] if doc_attr else None
+    source_url = f'{doc_attr["fileName"]}#L{line}' if doc_attr else None
+    method_params = _get_params(method_dict["method-arguments"], source_url, line)
+    
+    return _render_type("method", method_dict, "method-return-type-full-name", f"({line_arguments})", method_params)
 
 def _get_params(args, source_url, line_num):
     if len(args) == 0:
@@ -74,8 +146,10 @@ def _get_params(args, source_url, line_num):
 
     arg_htmls = []
     for i, arg in enumerate(args):
-        arg_type = _type_conversion(arg["argument-type-short-name"])
-        argument_description = _get_param_description(source_url, line_num, i)
+        arg_type_name_selected = arg["argument-type-full-name"] \
+            if arg["argument-type-full-name"] and arg["argument-type-full-name"].split('.')[0] == "QuantConnect" else arg["argument-type-short-name"]
+        arg_type = _get_hyperlinked_type(arg_type_name_selected)
+        argument_description = _get_param_description(source_url, line_num, i) if source_url else None
         arg_html = f'''    <li>
     <span class=\"arg-name\">{arg["argument-name"]}</span> (<span class=\"object-type\">{arg_type}{", optional" if arg["argument-optional"] else ""}</span>){" &mdash; " + argument_description if argument_description else ""}
     </li>'''
@@ -109,22 +183,27 @@ def _get_param_description(source_url, line_num, arg_num):
     description = ' '.join(x.replace('/', '').strip() for x in substr[description_char+6:].split("</param>")[0].split("\">")[-1].split('\n'))
     return extract_xml_content(description)
 
-def _get_return(return_type_name, source_url, line_num):
-    if return_type_name == "void":
-        return ""
-    
-    return_type = _type_conversion(return_type_name)
+def _get_return(type_, return_type_name, description, source_url, line_num):
+    return_description = _get_method_return_description(source_url, line_num) \
+        if source_url and type_ == "method" else extract_xml_content(description) \
+        if type_ != "method" else ""
     description_html = ""
-    return_description = _get_return_description(source_url, line_num)
     if return_description:
         description_html = f'''<div class=\"subsection-header\">Returns:</div>
 <p class=\"subsection-content\">{return_description}</p>'''
 
+    return_type = _get_hyperlinked_type(return_type_name)
+    
     return f'''{description_html}
 <div class=\"subsection-header\">Return type:</div>
 <p class=\"subsection-content\">{return_type}</p>'''
 
-def _get_return_description(source_url, line_num):
+def _get_method_return(return_type_name, description, source_url, line_num):
+    if return_type_name == "void":
+        return ""
+    return _get_return("method", return_type_name, description, source_url, line_num)
+
+def _get_method_return_description(source_url, line_num):
     key = source_url.split('#')[0]
     if key not in SOURCE_CODES:
         raw = urlopen(f"{RAW_LEAN}/{source_url}").readlines()
@@ -139,6 +218,43 @@ def _get_return_description(source_url, line_num):
     
     description = ' '.join(x.replace('/', '').strip() for x in substr[description_char+9:].split("</returns>")[0].split('\n'))
     return extract_xml_content(description)
+
+def _get_hyperlinked_type(type_name):
+    _type = _type_conversion(type_name)
+    split_type_name = _type.split('.')
+    short_name = split_type_name[-1].replace("[]", "")
+    
+    if split_type_name[0] == "QuantConnect":
+        if short_name[0] == "I" and short_name[1].isupper():
+            return _type
+        
+        if split_type_name[1] == "Indicators":
+            return f"<a href=\"/docs/v2/writing-algorithms/indicators/supported-indicators/{title_to_dash_linked_lower_case(short_name)}\">{_type}</a>"
+        elif short_name == "CandlestickPatterns":
+            return f"<a href=\"/docs/v2/writing-algorithms/indicators/supported-indicators/candlestick-patterns\">{_type}</a>"
+        
+        EXTRAS[short_name] = type_name
+        return f"<a href=\"#{short_name}\">{_type}</a>"
+    
+    return _type
+                
+def _render_properties(property_list):
+    return _render_types("property", property_list)
+
+def _render_property(property_dict):
+    return _render_type("property", property_dict)
+
+def _get_property_return(return_type_name, description, source_url, line_num):
+    return _get_return("property", return_type_name, description, source_url, line_num)
+                
+def _render_fields(field_list):
+    return _render_types("field", field_list)
+
+def _render_field(field_dict):
+    return _render_type("field", field_dict)
+
+def _get_field_return(return_type_name, description, source_url, line_num):
+    return _get_return("field", return_type_name, description, source_url, line_num)
 
 def _type_conversion(type):
     type_replacement = {
@@ -176,49 +292,6 @@ def _type_conversion(type):
                 type = type[:-1]
     
     return type
-                
-def _render_properties(property_list):
-    properties = []
-    for property in sorted(property_list, key=lambda x: x["property-name"]):
-        property_html = _render_property(property)
-        properties.append(property_html)
-    return '\n'.join(properties)
-
-def _render_property(property_dict):
-    # Do not render the ones without documentation attributes
-    if len(property_dict["documentation-attributes"]) == 0:
-        return ""
-
-    doc_attr = property_dict["documentation-attributes"][0]
-    source_url = f'{doc_attr["fileName"]}#L{doc_attr["line"]}'
-    property_return = _get_property_return(property_dict["property-short-type-name"], source_url, doc_attr["line"])
-    property_description = extract_xml_content(property_dict["property-description"])
-
-    return f'''<div class=\"code-snippet\"><pre><span class=\"object-type\">property</span> {property_dict["property-name"]}<a class=\"code-source\" href=\"{LEAN}/{source_url}\">[source]</a></pre></div>
-<p>{property_description}</p>
-{property_return}
-'''
-
-def _get_property_return(return_type_name, source_url, line_num):
-    return_type = _type_conversion(return_type_name)
-    return_description = _get_property_return_description(source_url, line_num)
-    
-    return f'''<div class=\"subsection-header\">Returns:</div>
-<p class=\"subsection-content\">{return_description}</p>
-<div class=\"subsection-header\">Return type:</div>
-<p class=\"subsection-content\">{return_type}</p>'''
-
-def _get_property_return_description(source_url, line_num):
-    key = source_url.split('#')[0]
-    if key not in SOURCE_CODES:
-        raw = urlopen(f"{RAW_LEAN}/{source_url}").readlines()
-        SOURCE_CODES[key] = raw
-    else:
-        raw = SOURCE_CODES[key]
-
-    substr = '\n'.join(x.decode('utf-8') for x in raw[:line_num]).split("<summary>")[-1]
-    description = ' '.join(x.replace('/', '').strip() for x in substr.split("</summary>")[0].split('\n'))
-    return extract_xml_content(description)
 
 def extract_xml_content(xml_string):
     output = xml_string
@@ -230,20 +303,14 @@ def extract_xml_content(xml_string):
     
     return output.replace('<', '&lt;').replace('>', '&gt;')
 
+def title_to_dash_linked_lower_case(title):
+    lower_case = ""
+    for i, char in enumerate(title):
+        if i > 0 and char.isupper():
+            lower_case += "-"
+        lower_case += char.lower()
+    return lower_case
+
     
 if __name__ == '__main__':
-    qcalgorithm_api_html = render_qcalgorithm_api()
-
-    style = '''
-<style>
-.code-source        { color: grey; float: right }
-.object-type        { font-style: italic; }
-.arg-name           { font-weight: bold; }
-.subsection-header  { font-weight: bold; background-color: lightgray; }
-.subsection-content { margin-left: 10px; }
-</style>
-'''
-    
-    with open(WRITE_FILE, 'w', encoding="utf-8") as file:
-        file.write(qcalgorithm_api_html)
-        file.write(style)
+    render_docs()
