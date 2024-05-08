3e2211a55f0279e27d6d2ac6231e48315c250517
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:40:17 -0600

    Update 10 Remove Subscriptions.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/10 Remove Subscriptions.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/10 Remove Subscriptions.php
@@ -1,5 +1,5 @@
 <?php 
-$pythonVariableName = "self.option_contract_symbol";
+$pythonVariableName = "self._option_contract_symbol";
 $cSharpVariableName = "_optionContractSymbol";
 $addContractMethodName = "AddFutureOptionContract";
 include(DOCS_RESOURCES."/securities/remove-option-contract.php"); 
