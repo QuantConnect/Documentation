f7e80f6171b20e2d3dd871f702d8d50c7b591495
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 15:59:47 -0600

    Update shortcut.php

--- Resources/consolidators/consolidator-info/calendar/shortcut.php
+++ Resources/consolidators/consolidator-info/calendar/shortcut.php
@@ -1,4 +1,4 @@
-<p>You can also use the <code>Consolidate</code> helper method to create calendar consolidators and register them for automatic updates.</p>
+<p>You can also use the <code class='csharp'>Consolidate</code><code class='python'>consolidate</code> helper method to create calendar consolidators and register them for automatic updates.</p>
 
 <div class='section-example-container'>
     <pre class='csharp'>_consolidator = Consolidate<?= $consolidationHandlerType == "QuoteBar" ? "&lt;QuoteBar&gt;" : "" ?>(_symbol, Calendar.Weekly, <?=$this->shortCutTickTypeArg?>ConsolidationHandler);</pre>
