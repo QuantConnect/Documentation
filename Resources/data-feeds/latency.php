76a41a04987a17bbb3e19930a40d4e2903d9bf99
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 09:51:19 -0600

    Update latency.php

--- Resources/data-feeds/latency.php
+++ Resources/data-feeds/latency.php
@@ -1,6 +1,6 @@
 <p>
   Live data takes time to travel from the source to your algorithm. 
-  The QuantConnect latencies vary depending on the data provider but for US Equities we have a latency of 5-40 milliseconds. 
-  A much more significant source of latency is the round trip order times from brokers which can vary from 100ms to 5 seconds. 
-  QuantConnect is not intended for high-frequency trading, but we have integrations to high-speed brokers if required.  
+  The QuantConnect latencies vary depending on the data provider, but for US Equities, we have a latency of 5-40 milliseconds. 
+  A much more significant source of latency is the round trip order times from brokers, which can vary from 100ms to 5 seconds. 
+  QuantConnect is not intended for high-frequency trading, but we have integrations to high-speed brokers if you need.
 </p>
