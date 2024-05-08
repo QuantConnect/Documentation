3b23412cbed46a9a44cc67a09a564249244d24b5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:40:02 -0600

    Update 07 Extended Market Hours.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/07 Extended Market Hours.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/01 Requesting Data/07 Extended Market Hours.php
@@ -1,6 +1,6 @@
 <?
 $cCode = "AddFutureOptionContract(_optionContractSymbol, extendedMarketHours: true);";
-$pyCode = "self.add_future_option_contract(self.option_contract_symbol, extended_market_hours=True)";
+$pyCode = "self.add_future_option_contract(self._option_contract_symbol, extended_market_hours=True)";
 $supportedIntradayData = true;
 $marketHoursLink = "/docs/v2/writing-algorithms/securities/asset-classes/future-options/market-hours";
 include(DOCS_RESOURCES."/securities/extended-market-hours.php"); 
