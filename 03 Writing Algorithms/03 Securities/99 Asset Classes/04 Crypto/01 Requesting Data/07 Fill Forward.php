fe7e35fe843452c70be2e209d8c9cb006dd98d82
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:17:22 -0600

    Update 07 Fill Forward.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/07 Fill Forward.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/04 Crypto/01 Requesting Data/07 Fill Forward.php
@@ -1,5 +1,5 @@
 <?php
 $cCode = "_symbol = AddCrypto(\"BTCUSD\", fillForward: false).Symbol;";
-$pyCode = "self._symbol = self.add_crypto(\"BTCUSD\", fill_forward=False).Symbol";
+$pyCode = "self._symbol = self.add_crypto(\"BTCUSD\", fill_forward=False).symbol";
 include(DOCS_RESOURCES."/securities/fill-forward.php");
 ?>
