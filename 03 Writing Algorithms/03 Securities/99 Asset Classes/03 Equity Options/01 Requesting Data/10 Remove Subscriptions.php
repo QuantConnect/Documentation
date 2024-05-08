50b807da130e14dc1019ef86c2318d8cff45fa9d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 11:54:57 -0600

    Update 10 Remove Subscriptions.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/10 Remove Subscriptions.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/10 Remove Subscriptions.php
@@ -1,5 +1,5 @@
 <?php 
-$pythonVariableName = "self.contract_symbol";
+$pythonVariableName = "self._contract_symbol";
 $cSharpVariableName = "_contractSymbol";
 $addContractMethodName = "AddOptionContract";
 include(DOCS_RESOURCES."/securities/remove-option-contract.php"); 
