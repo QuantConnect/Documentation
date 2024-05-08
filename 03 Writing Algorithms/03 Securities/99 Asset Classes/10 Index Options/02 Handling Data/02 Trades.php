c447d956de9b9dbfb48c52fcdd36d68b927e5c83
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:52:25 -0600

    Update 02 Trades.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/10 Index Options/02 Handling Data/02 Trades.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/10 Index Options/02 Handling Data/02 Trades.php
@@ -1,6 +1,6 @@
 <?
 $securityName = "contract";
-$pythonVariable = "self.contract_symbol";
+$pythonVariable = "self._contract_symbol";
 $cSharpVariable = "_contractSymbol";
 include(DOCS_RESOURCES."/securities/tradebar.php"); 
 ?>
