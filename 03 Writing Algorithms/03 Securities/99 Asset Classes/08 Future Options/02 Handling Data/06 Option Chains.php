f2709a868bd4fa86236268c9b52b697b43a76d58
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:41:40 -0600

    Update 06 Option Chains.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/06 Option Chains.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Future Options/02 Handling Data/06 Option Chains.php
@@ -1,7 +1,7 @@
 <?php 
 $path = "regular";
 $cSharpMemberName = "_optionContractSymbol.Canonical";
-$pythonMemberName = "self.option_contract_symbol.Canonical";
+$pythonMemberName = "self._option_contract_symbol.Canonical";
 $cSharpVariableName = "canonicalFOPSymbol";
 $pythonVariableName = "canonical_fop_symbol";
 include(DOCS_RESOURCES."/securities/option-chains.php"); 
