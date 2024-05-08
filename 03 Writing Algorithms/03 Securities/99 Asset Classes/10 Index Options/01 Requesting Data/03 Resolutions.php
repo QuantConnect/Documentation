113c15fa70305efa566a52e3838dd7a2b08862fb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:44:56 -0600

    Update 03 Resolutions.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/10 Index Options/01 Requesting Data/03 Resolutions.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/10 Index Options/01 Requesting Data/03 Resolutions.php
@@ -6,7 +6,7 @@
 
 <div class="section-example-container">
     <pre class="csharp">AddIndexOptionContract(_contractSymbol, Resolution.Hour);</pre>
-    <pre class="python">self.add_index_option_contract(self.contract_symbol, Resolution.HOUR)</pre>
+    <pre class="python">self.add_index_option_contract(self._contract_symbol, Resolution.HOUR)</pre>
 </div>
 
 <p>To create custom resolution periods, see <a href="/docs/v2/writing-algorithms/consolidating-data/getting-started">Consolidating Data</a>.</p>
