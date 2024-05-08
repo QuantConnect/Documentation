b0c7fc5394ac69c957fc338a4cae5480ebc24a6f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 11:54:27 -0600

    Update 05 Fill Forward.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/05 Fill Forward.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/03 Equity Options/01 Requesting Data/05 Fill Forward.php
@@ -1,5 +1,5 @@
 <?php
 $cCode = "AddOptionContract(_contractSymbol, fillForward: false);";
-$pyCode = "self.add_option_contract(self.contract_symbol, fill_forward=False)";
+$pyCode = "self.add_option_contract(self._contract_symbol, fill_forward=False)";
 include(DOCS_RESOURCES."/securities/fill-forward.php");
 ?>
