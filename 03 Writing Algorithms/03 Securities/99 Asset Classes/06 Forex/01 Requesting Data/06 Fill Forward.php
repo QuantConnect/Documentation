cb66468292ffcfa0509889757ce43ac70cb1682b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 13:20:20 -0600

    Update 06 Fill Forward.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/01 Requesting Data/06 Fill Forward.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Forex/01 Requesting Data/06 Fill Forward.php
@@ -1,5 +1,5 @@
 <?php
 $cCode = "_symbol = AddForex(\"EURUSD\", fillForward: false).Symbol;";
-$pyCode = "self._symbol = self.add_forex(\"EURUSD\", fill_forward=False).Symbol";
+$pyCode = "self._symbol = self.add_forex(\"EURUSD\", fill_forward=False).symbol";
 include(DOCS_RESOURCES."/securities/fill-forward.php");
 ?>
