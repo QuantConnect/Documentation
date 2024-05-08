2545834d3d1a80757f923f726d0220cc041c3e8f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 16:04:45 -0600

    Update create-consolidator.php

--- Resources/consolidators/consolidator-info/mixed-mode/create-consolidator.php
+++ Resources/consolidators/consolidator-info/mixed-mode/create-consolidator.php
@@ -2,7 +2,7 @@
 
 <div class='section-example-container'>
 	<pre class='csharp'>_consolidator = new <?=$consolidatorClassName?>(<?=$this->numSamples?>, TimeSpan.<?=$this->timeSpanPeriod?>);</pre>
-	<pre class='python'>self.consolidator = <?=$consolidatorClassName?>(<?=$this->numSamples?>, timedelta(<?=$this->timeDeltaPeriod?>))</pre>
+	<pre class='python'>self._consolidator = <?=$consolidatorClassName?>(<?=$this->numSamples?>, timedelta(<?=$this->timeDeltaPeriod?>))</pre>
 </div>
 
 <?php 
