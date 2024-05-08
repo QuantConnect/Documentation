f0d9db96dc74f788c8c66c4e4f502c52e82941ea
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-08 09:58:44 -0600

    Update 03 Default Environment.php

--- 01 Cloud Platform/06 Projects/11 Package Environments/03 Default Environment.php
+++ 01 Cloud Platform/06 Projects/11 Package Environments/03 Default Environment.php
@@ -1,5 +1,2 @@
 <p>The default environment supports the following libraries:</p>
-<? 
-$cloudPlatform = true;
-include(DOCS_RESOURCES."/libraries/supported-libraries.php"); 
-?>
+<? include(DOCS_RESOURCES."/libraries/supported-libraries.php"); ?>
