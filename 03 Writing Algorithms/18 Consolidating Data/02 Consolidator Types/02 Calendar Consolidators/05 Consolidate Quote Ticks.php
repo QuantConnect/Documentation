2735d940f05045243bc1ff1612e3bf493e72f97f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-09 09:27:53 -0600

    Update 05 Consolidate Quote Ticks.php

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/05 Consolidate Quote Ticks.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/05 Consolidate Quote Ticks.php
@@ -2,11 +2,12 @@
 include(DOCS_RESOURCES."/consolidators/consolidator-format-info/tick-quotebar-definition.php");
 $dataFormatInfo = new TickQuoteBarConsolidatorFormatInfo();
 
-$createConsolidatorExtraArgs = ", TickType.Quote";
+$createConsolidatorExtraArgsC = ", TickType.Quote";
+$createConsolidatorExtraArgsPy = ", TickType.QUOTE";
 $shortCutTickTypeArgC = "TickType.Quote, ";
 $shortCutTickTypeArgPy = "TickType.QUOTE, ";
 
-$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgs, $shortCutTickTypeArgC, $shortCutTickTypeArgPy);
+$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgsC, $createConsolidatorExtraArgsPy, $shortCutTickTypeArgC, $shortCutTickTypeArgPy);
 
 include(DOCS_RESOURCES."/consolidators/manage-consolidators.php");
 ?>
