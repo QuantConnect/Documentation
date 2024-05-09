afce59a8d96d00d2ac604bc894cfe07d2396a4d3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-09 09:30:55 -0600

    Update 07 Consolidate Other Data.php

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/07 Consolidate Other Data.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/07 Consolidate Other Data.php
@@ -3,11 +3,12 @@ include(DOCS_RESOURCES."/consolidators/consolidator-format-info/dynamic-data-def
 
 $dataFormatInfo = new DynamicDataConsolidatorFormatInfo();
 
-$createConsolidatorExtraArgs = "";
+$createConsolidatorExtraArgsC = "";
+$createConsolidatorExtraArgsPy = "";
 $shortCutTickTypeArgC = "";
 $shortCutTickTypeArgPy = "";
 
-$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgs, $shortCutTickTypeArgC, $shortCutTickTypeArgPy);
+$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgsC, $createConsolidatorExtraArgsPy, $shortCutTickTypeArgC, $shortCutTickTypeArgPy);
 
 include(DOCS_RESOURCES."/consolidators/manage-consolidators.php");
 ?>
