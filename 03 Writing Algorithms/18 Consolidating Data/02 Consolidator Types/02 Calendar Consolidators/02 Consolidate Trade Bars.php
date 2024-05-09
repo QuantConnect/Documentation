1225ec6bdf98f019da5f9cfe526a193d1dacafe8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-09 09:23:12 -0600

    Update 02 Consolidate Trade Bars.php

--- 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/02 Consolidate Trade Bars.php
+++ 03 Writing Algorithms/18 Consolidating Data/02 Consolidator Types/02 Calendar Consolidators/02 Consolidate Trade Bars.php
@@ -4,9 +4,10 @@ include(DOCS_RESOURCES."/consolidators/consolidator-format-info/trade-bar-defini
 $dataFormatInfo = new TradeBarConsolidatorFormatInfo();
 
 $createConsolidatorExtraArgs = "";
-$shortCutTickTypeArg = "";
+$shortCutTickTypeArgC = "";
+$shortCutTickTypeArgPy = "";
 
-$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgs, $shortCutTickTypeArg);
+$consolidatorInfo = new CalendarConsolidatorInfo($createConsolidatorExtraArgs, $shortCutTickTypeArgC, $shortCutTickTypeArgPy);
 
 include(DOCS_RESOURCES."/consolidators/manage-consolidators.php");
 ?>
