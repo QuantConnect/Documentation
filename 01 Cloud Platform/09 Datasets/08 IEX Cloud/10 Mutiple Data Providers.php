b05dc36117180eebee024dba31c3b7c436fdfaf8
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-05-16 21:42:19 +0100

    Adds Multiple Data Provider for IEX, IQ Feed and Polygon

---
+++ 01 Cloud Platform/09 Datasets/08 IEX Cloud/10 Mutiple Data Providers.php
@@ -0,0 +1,1 @@
+<? include(DOCS_RESOURCES."/data-feeds/multiple-data-providers-iex.php"); ?>
---
+++ Resources/data-feeds/multiple-data-providers-iqfeed.php
@@ -0,0 +1,4 @@
+<p>
+    When you <a href='/docs/v2/lean-cli/live-trading/data-providers/iqfeed#02-Deploy-Local-Algorithms'>deploy a live algorithm</a>, you can add multiple data provider.
+    If you use multiple data providers, the order you select them in defines their order of precedence in Lean.
+</p>
