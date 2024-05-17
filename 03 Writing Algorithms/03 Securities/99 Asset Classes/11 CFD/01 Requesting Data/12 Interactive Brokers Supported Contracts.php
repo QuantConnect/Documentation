61b4bb88476602eacd6417d543289c8f3a96bf37
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-17 11:46:34 -0600

    Update 12 Interactive Brokers Supported Contracts.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/12 Interactive Brokers Supported Contracts.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/11 CFD/01 Requesting Data/12 Interactive Brokers Supported Contracts.php
@@ -1,1 +1,3 @@
-<?php include(DOCS_RESOURCES."/datasets/supported-securities/cfd/interactivebrokers.html"); ?>
+<p>The following table shows the CFD contracts that IB supports:</p>
+
+<? include(DOCS_RESOURCES."/datasets/supported-securities/cfd/interactivebrokers.html"); ?>
