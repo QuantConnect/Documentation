95a874f6be98b36928e08baf308a75585efe6ffe
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-05-17 15:37:48 -0600

    Rename 09 Automate Deployment.php to 09 Automate Deployments.php

--- 01 Cloud Platform/10 Live Trading/04 Deployment/09 Automate Deployment.php
+++
@@ -1,1 +0,0 @@
-<? echo file_get_contents(DOCS_RESOURCES."/live-trading/automate-deployment.html"); ?>
---
+++ 01 Cloud Platform/10 Live Trading/04 Deployment/09 Automate Deployments.php
@@ -0,0 +1,1 @@
+<? echo file_get_contents(DOCS_RESOURCES."/live-trading/automate-deployment.html"); ?>
