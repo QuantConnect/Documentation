36ea60643a1b9fd5a9cc52b42ce275e061f92a07
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-06-06 00:22:36 +0100

    Updates Environment Libraries

---
+++ 01 Cloud Platform/06 Projects/11 Package Environments/05 Autogluon Environment.php
@@ -0,0 +1,5 @@
+<p class="csharp">This environment is only available for Python.</p>
+<p class="python">The Autogluon environment provides the <code>Autogluon</code> library:</p>
+<div class="python">
+    <? include(DOCS_RESOURCES."/libraries/supported-libraries-foundation-autogluon.html"); ?>
+</div>
