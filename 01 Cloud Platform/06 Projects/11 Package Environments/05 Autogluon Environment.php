eec455fcaaacb2fdd8e7e208d365054507cc8dc1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-06-07 11:45:04 -0600

    Update 05 Autogluon Environment.php

--- 01 Cloud Platform/06 Projects/11 Package Environments/05 Autogluon Environment.php
+++ 01 Cloud Platform/06 Projects/11 Package Environments/05 Autogluon Environment.php
@@ -1,5 +1,5 @@
 <p class="csharp">This environment is only available for Python.</p>
-<p class="python">The Autogluon environment provides the <code>Autogluon</code> library:</p>
+<p class="python">The Autogluon environment provides the following libraries:</p>
 <div class="python">
     <? include(DOCS_RESOURCES."/libraries/supported-libraries-foundation-autogluon.html"); ?>
 </div>
