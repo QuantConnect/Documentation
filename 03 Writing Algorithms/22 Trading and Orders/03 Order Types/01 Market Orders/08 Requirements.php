dd2c09e9c1db841f3f37498adca9819a33ceb1df
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-06-07 16:00:27 -0600

    Update 08 Requirements.php

--- 03 Writing Algorithms/22 Trading and Orders/03 Order Types/01 Market Orders/08 Requirements.php
+++ 03 Writing Algorithms/22 Trading and Orders/03 Order Types/01 Market Orders/08 Requirements.php
@@ -1,4 +1,12 @@
-<?
-$orderType = "Market orders";
-include(DOCS_RESOURCES."/order-types/market-orders-requirements.php");
-?>
+<p>
+  You can submit market orders during regular trading hours for all asset classes. 
+  For Futures and Future Options, you can place market orders during regular and extended market hours. 
+  For all other asset classes, if you place market orders at/after the last minute of regular market hours, LEAN converts the market orders into <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders'>market-on-open orders</a> as long as <a href='/docs/v2/writing-algorithms/trading-and-orders/order-types/market-on-open-orders#07-Requirements'>the asset class supports them</a>. 
+  To view the trading hours of each asset class, follow these steps:
+</p>
+
+<ol>
+  <li>Open the <a href='/docs/v2/writing-algorithms/securities/asset-classes'>Asset Classes</a> documentation.</li>
+  <li>Click an asset class.</li>
+  <li>Click <span class=button-name'>Market Hours</span>.</li>
+</ol>
