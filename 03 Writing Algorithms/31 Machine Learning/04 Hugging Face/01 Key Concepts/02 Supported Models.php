7f1c9ef6dbaf343af45e9be79fec60a939fce53a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-06-13 09:33:00 -0600

    Update and rename 02 Supported Models.html to 02 Supported Models.php

---
+++ 03 Writing Algorithms/31 Machine Learning/04 Hugging Face/01 Key Concepts/02 Supported Models.php
@@ -0,0 +1,29 @@
+<p>
+  Hundreds of thousands of Hugging Face models are publicly available and ready to use. 
+  To view all of them, see the <a href='https://huggingface.co/models' rel='nofollow' target='_blank'>Model Hub</a> on the Hugging Face website. 
+  The <span class='tab-name'>Model card</span> tab of each model repository explains an overview of how the model works, its requirements, and a quick start guide.
+</p>
+<p>
+  QuantConnect Cloud caches some of the most popular models to speed up your development workflow. 
+  The following table shows the cached models:
+</p>
+
+<? include(DOCS_RESOURCES."/machine-learning/hugging-face-table.html"); ?>
+
+<p>To see the commit hash of the cached models, run the following algorithm and then <a href='/docs/v2/writing-algorithms/logging#07-Get-Logs'>view the logs</a>:</p>
+
+<div class="section-example-container">
+<pre class="python">from huggingface_hub import scan_cache_dir
+
+class HuggingFaceModelHashAlgorithm(QCAlgorithm):
+
+    def initialize(self):
+        cache_info = scan_cache_dir()
+        cached_models_log = ''
+        for entry in cache_info.repos:
+            revisions = [revision.commit_hash for revision in entry.revisions]
+            cached_models_log += f'\nRepo: {entry.repo_id}. Revisions {str(revisions)}'
+        self.quit(cached_models_log)</pre> 
+</div>
+
+
