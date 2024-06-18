f1bbeb1ecd7aab0c6bd2154637ad16d40b5cf13c
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-06-18 21:54:40 +0100

    Fixes FRED Categories Code Generator

--- code-generators/fred-categories-table-code-generator.py
+++ code-generators/fred-categories-table-code-generator.py
@@ -1,52 +1,24 @@
+from json import loads
 from urllib.request import urlopen
 
-base = "https://raw.githubusercontent.com/QuantConnect/Lean.DataSource.FRED/master/"
 destination = "Resources/datasets/data-point-attributes/fred/supported-indicators.html"
-subcategories = ["CBOE", "CentralBankInterventions", "CommercialPaper", "ICEBofAML", "LIBOR", "OECDRecessionIndicators", "TradeWeightedIndexes", "Wilshire"]
-summaries = {}
+LEAN_SERVICE = "https://www.quantconnect.com/services/inspector?language=%s&type=T:QuantConnect.DataSource.Fred.%s"
 
-for cat in subcategories:
-    summaries[cat] = {}
-    
-    lines = urlopen(f"{base}{cat}.cs").read().decode("utf-8").split('\n')
-    
-    for i in range(len(lines)):
-        if "public static string " in lines[i]:
-            symbol_line = lines[i].split("public static string ")[1].split(" = ")
-            key = symbol_line[0]
-            ticker = symbol_line[1].replace('"', "")[:-1]
-            
-            j = i
-            while "/// </summary>" not in lines[j]:
-                j -= 1
-            summary = lines[j-1].split("/// ")[1]
-        
-            key = f"Fred.{cat}.{key}"
-            summaries[cat][key] = (ticker, summary)
+def get_data(type: str) -> str:
+    data = {}
+    languages = {'csharp':'language-cs', 'python':'language-python'}
+    for language, css_class in languages.items():
+        fields = loads(urlopen(LEAN_SERVICE % (language, type)).read()).get('fields')
+        for field in fields:
+            ticker = field.pop('field-default-value')
+            data[ticker] = data.get(ticker, {})
+            data[ticker][language] = f"<b class='{css_class}'>Fred.{type}.{field.pop('field-name')}</b>"
+            data[ticker]['summary'] = field.pop('field-description')
 
-html = '''<table class="table qc-table table-reflow">
-<thead>
-<tr><th style="width: 10%;">Symbol</th><th style="width: 30%;">Accessor Code</th><th style="width: 60%;">Summary</th></tr>
-</thead>
-<tbody>
-'''
-
-for cat, key_dict in summaries.items():
-    html += f'''<tr><td colspan="3"><b>{cat}</b></td></tr>
-'''
-
-    for key, (ticker, summary) in key_dict.items():
-        html += f'''<tr><td>{ticker}</td><td><code>{key}</code></td><td>{summary}</td></tr>
-'''
-
-html += """</tbody>
-</table>"""
+    return f'<p>Category: <b>{type}</b></p>\n<ul>\n' + '\n'.join(
+        [f"<li>{info['csharp']}: {info['summary']} (<b>{ticker}</b>)</li>"
+         for ticker, info in data.items()]) + '\n</ul>'
 
 with open(destination, "w", encoding="utf-8") as text:
-    text.write("""<h4>Fred Attributes</h4>
-<div data-tree="QuantConnect.DataSource.Fred"></div>
-
-<h4>Reference Table</h4>
-""")
+    html = '\n'.join([get_data(cat) for cat in ["CBOE", "CentralBankInterventions", "CommercialPaper", "ICEBofAML", "LIBOR", "OECDRecessionIndicators", "TradeWeightedIndexes", "Wilshire"]])
     text.write(html)
-    
