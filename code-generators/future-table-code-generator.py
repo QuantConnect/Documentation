024736b19663992caa1b016eb03dedc67c470a3a
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-06-21 00:17:45 +0100

    Apply PEP8 to Futures Tables

--- code-generators/future-table-code-generator.py
+++ code-generators/future-table-code-generator.py
@@ -1,5 +1,9 @@
 from pathlib import Path
-from _code_generation_helpers import *
+from json import loads
+from _code_generation_helpers import urlopen
+
+LEAN_SERVICE = "https://www.quantconnect.com/services/inspector?language=%s&type=T:QuantConnect.Securities.Futures.%s"
+
 TICKERS_BY_MARKET = {
     "CBOT" : ["10Y","2YY","30Y","5YY","AW","BCF","BWF","EH","F1U","KE","MYM","TN","UB","YM","ZB","ZC","ZF","ZL","ZM","ZN","ZO","ZS","ZT","ZW"],
     "CFE" : ["VX"],
@@ -7,58 +11,49 @@ TICKERS_BY_MARKET = {
     "COMEX" : ["AUP","EDP","GC","HG","MGC","MGT","SI","SIL"],
     "ICE" : ["SB"],
     "NYMEX" : ["A0D","A0F","A1R","A3G","A7E","A7Q","A8K","A9N","AA6","AA8","AC0","AD0","ADB","AE5","AGA","AJL","AJS","AKL","APS","ARE","AYV","AYX","AZ1","B0","B7H","BK","BOO","BZ","CL","CRB","CSW","CSX","CU","D1N","DCB","EN","EPN","EVC","EWG","EWN","EXR","FO","FRC","FSS","GCU","HCL","HH","HO","HP","HRC","HTT","M1B","MAF","MCL","MEF","NG","PA","PAM","PL","R5O","RB","S5O","YO"]
-    }
+}
 
-class FutureProperties:
-    def __init__(self, ticker, market, name):
-        self.category, self.accessor = '',''
-        self.full_accessor = '/'
-        self.option_ticker = ''
-        self.ticker = ticker
-        self.market = market
-        self.name = name
+FUTURE_OPTIONS = {
+    "DC":"DC", "ES":"ES", "CL":"LO", "NQ":"NQ", "SI":"SO",
+    "RB":"OB", "GC":"OG", "HO":"OH", "NG":"ON", "HG":"HXE",
+    "ZB":"OZB", "ZC":"OZC", "ZS":"OZS", "ZT":"OZT", "ZW":"OZW"
+}
 
-    def SetAccessor(self, category, accessor):
-        self.category = category
-        self.accessor = accessor
-        self.full_accessor = f'Futures.{category}.{accessor}'
-    
-    def SetOptionTicker(self, option_ticker):
-        self.option_ticker = option_ticker
-
-    def __repr__(self) -> str:
-        return f'{self.market},{self.ticker},{self.full_accessor},{self.name}'
+def get_data() -> dict:
+    data = {}
+    for category in ['Currencies', 'Dairy', 'Energy', 'Financials', 'Forestry',
+                     'Grains', 'Indices', 'Meats', 'Metals', 'Softs']:
+        for language in ['csharp', 'python']:
+            fields = loads(urlopen(LEAN_SERVICE % (language, category)).read()).get('fields')
+            for field in fields:
+                ticker = field.pop('field-default-value')
+                data[ticker] = data.get(ticker, {})
+                data[ticker]['ticker'] = ticker
+                data[ticker]['summary'] = field.pop('field-description')
+                data[ticker][language] = f"Futures.{category}.{field.pop('field-name')}"
+    return data
 
 if __name__ == '__main__':
-    lean_master = 'https://raw.githubusercontent.com/QuantConnect/Lean/master'
 
-    properties = {}
-    spdb = get_text_content(SPDB)
-    for line in spdb.split('\n'):
-        csv = line.split(',')
-        if len(csv) > 2 and csv[2] == 'future':
-            market, ticker = csv[0].upper(), csv[1].upper()
-            if ticker not in TICKERS_BY_MARKET.get(market, []):
+    supported = {}
+    properties = get_data()
+
+    for market, tickers in TICKERS_BY_MARKET.items():
+        for ticker in tickers:
+            property = properties.pop(ticker, None)
+            if not property:
                 continue
-            properties[csv[1].upper()] = FutureProperties(ticker, market, csv[3])
+            property['market'] = market
+            property['option'] = FUTURE_OPTIONS.get(ticker)
+            supported[ticker] = property
 
-    category = ""
-
-    futures = get_text_content(f'{lean_master}/Common/Securities/Future/Futures.cs')
-    for x in futures.split('\n'):
-        if '=' in x and '"' in x:
-            tmp = x.split(" = ")
-            accessor =  f'{tmp[0].split(" ")[-1]}'
-            ticker = tmp[1][1:-2].upper()
-            property = properties.get(ticker)
-            if property:
-                property.SetAccessor(category, accessor) 
-
-        if 'public static class' in x:
-            category = x.split(" ")[-1]
+    for ticker, property in supported.items():
+        cs, py = property.get('csharp'), property.get('python')
+        property['accessor'] = f"<b>{cs}</b>" if cs == py else \
+                f"<b class='language-cs'>{cs}</b><b class='language-python'>{py}</b>"
 
     ###
-    ### Write Future  Table
+    ### Write Future Table
     ###
     path = Path('Resources/datasets/supported-securities/future/')
     path.mkdir(parents=True, exist_ok=True)
@@ -66,13 +61,10 @@ if __name__ == '__main__':
     with open(f'{path}/supported-contracts.html', 'w', encoding='utf-8') as fp:
         html = '''<ul>
     '''
-        count = 0
-        for x in sorted(properties.values(), key=lambda x: (x.category, x.ticker)):
-            if not x.category: continue
-            count +=1
-            html += f'''<li><b>{x.full_accessor}</b>: {x.name} ({x.market}: {x.ticker})</li>
+        for x in sorted(supported.values(), key=lambda x: x['python']):
+            html += f'''<li>{x['accessor']}: {x['summary']} ({x['market']}: {x['ticker']})</li>
     '''
-        fp.write(f"<p>The following list shows the available ({count}) Futures:</p>{html}</ul>")
+        fp.write(f"<p>The following list shows the available ({len(supported)}) Futures:</p>{html}</ul>")
 
     ###
     ### Write Future Option Table
@@ -80,29 +72,14 @@ if __name__ == '__main__':
     path = Path('Resources/datasets/supported-securities/futureoption/')
     path.mkdir(parents=True, exist_ok=True)
 
-    mapping = {
-        "DC":"DC", "ES":"ES", "LO":"CL", "NQ":"NQ", "SO":"SI",
-        "OB":"RB", "OG":"GC", "OH":"HO", "ON":"NG", "HXE":"HG",
-        "OZB":"ZB", "OZC":"ZC", "OZS":"ZS", "OZT":"ZT", "OZW":"ZW"    
-    }
-
-    tmp = {}
-    for option_ticker, ticker in mapping.items():
-        property = properties.pop(ticker)
-        if property:
-            property.SetOptionTicker(option_ticker)
-            tmp[ticker] = property
-    properties = tmp
-
     with open(f'{path}/supported-contracts.html', 'w', encoding='utf-8') as fp:
-
         html = '''<ul>
     '''
-        count = 0
-        for x in sorted(properties.values(), key=lambda x: (x.category, x.ticker)):
-            if not x.category: continue
-            count += 1
-            underlying = '' if x.option_ticker == x.ticker else f' | Underlying: {x.ticker}'
-            html += f'''<li><b>{x.full_accessor}</b>: {x.name} ({x.market}: {x.option_ticker}{underlying})</li>
+        for x in sorted(supported.values(), key=lambda x: x['python']):
+            option, ticker = x.get("option"), x.get("ticker")
+            if not option:
+                continue
+            underlying = '' if option == ticker else f' | Underlying: {ticker}'
+            html += f'''<li><b>{x['accessor']}</b>: {x['summary']} ({x['market']}: {option}{underlying})</li>
     '''
-        fp.write(f"<p>The following list shows the available ({count}) Futures Options:</p>{html}</ul>")
+        fp.write(f"<p>The following list shows the available ({len(FUTURE_OPTIONS)}) Futures Options:</p>{html}</ul>")
