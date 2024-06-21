1cc133084074521eeccca7195fd8cf31dfa0e7f2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-06-21 15:35:06 -0600

    Update orders.php

--- Resources/brokerages/binance/orders.php
+++ Resources/brokerages/binance/orders.php
@@ -43,13 +43,16 @@
 <table class="table qc-table">
    <thead>
       <tr>
-         <th style="width: 25%">Property</th>
-         <th style="width: 75%">Description</th>
+         <th>Property</th>
+         <th>Data Type</th>
+         <th>Description</th>
+         <th>Default Value</th>
       </tr>
    </thead>
    <tbody>
       <tr>
          <td><code class="csharp">TimeInForce</code><code class="python">time_in_force</code></td>
+         <td><code>TimeInForce</code></td>
          <td>
              A <a href='/docs/v2/writing-algorithms/trading-and-orders/order-properties#03-Time-In-Force'>TimeInForce</a> instruction to apply to the order. The following instructions are supported:
              <ul>
@@ -58,11 +61,14 @@
                  <li><code class="csharp">GoodTilDate</code><code class="python">GOOD_TIL_DATE</code></li>
              </ul>
          </td>
+         <td><code class='csharp'>TimeInForce.GoodTilCanceled</code><code class='csharp'>TimeInForce.GOOD_TIL_CANCELED</code></td>
       </tr>
       <tr>
          <td><code class="csharp">PostOnly</code><code class="python">post_only</code></td>
+         <td><code>bool</code></td>
          <td>A flag to signal that the order must only add liquidity to the order book and not take liquidity from the order book. If part of the order results in taking liquidity rather than providing liquidity, the order is rejected without any part of it being filled.</td>
       </tr>
+      <td></td>
    </tbody>
 </table>
 
