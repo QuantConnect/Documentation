df423d0f4a5c22d6003cc67a80f72425f7291901
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-07-05 16:50:21 -0600

    Update 04 Plot Metadata.php

--- 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/04 Plot Metadata.php
+++ 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/04 Plot Metadata.php
@@ -5,8 +5,34 @@
     <div class="section-example-container">
 	    <pre class="python">backtest = api.read_backtest(project_id, backtest_id)</pre>
 	</div>
-	<?php include(DOCS_RESOURCES."/qc-api/get-project-id-in-research.html"); ?>
-	<?php include(DOCS_RESOURCES."/qc-api/get-backtest-id-in-research.html"); ?>
+	<p>The following table provides links to documentation that explains how to get the project Id and backtest Id, depending on the platform you use:</p>
+
+	<table class="qc-table table">
+	    <thead>
+	        <tr>
+	            <th>Platform</th>
+	            <th>Project Id</th>
+	            <th>Backtest Id</th>
+	        </tr>
+	    </thead>
+	    <tbody>
+	        <tr>
+	            <td>Cloud Platform</td>
+	            <td><a href='/docs/v2/cloud-platform/projects/getting-started#13-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/cloud-platform/backtesting/getting-started#06-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	        <tr>
+	            <td>Local Platform</td>
+	            <td><a href='/docs/v2/local-platform/projects/getting-started#14-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/local-platform/backtesting/getting-started#07-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	        <tr>
+	            <td>CLI</td>
+	            <td><a href='/docs/v2/lean-cli/projects/project-management#07-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/lean-cli/backtesting/deployment#05-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	    </tbody>
+	</table>
 	
     <?
     $isLive = false;
