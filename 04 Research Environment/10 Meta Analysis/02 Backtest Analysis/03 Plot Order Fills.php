a67dafab322dcd6ef7b13a986443b2be91159838
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-07-05 16:49:44 -0600

    Update 03 Plot Order Fills.php

--- 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/03 Plot Order Fills.php
+++ 04 Research Environment/10 Meta Analysis/02 Backtest Analysis/03 Plot Order Fills.php
@@ -5,12 +5,39 @@
     <div class="section-example-container">
 	    <pre class="python">orders = api.read_backtest_orders(project_id, backtest_id)</pre>
 	</div>
-	<?php include(DOCS_RESOURCES."/qc-api/get-project-id-in-research.html"); ?>
-	<?php include(DOCS_RESOURCES."/qc-api/get-backtest-id-in-research.html"); ?>
+	<p>The following table provides links to documentation that explains how to get the project Id and backtest Id, depending on the platform you use:</p>
+
+	<table class="qc-table table">
+	    <thead>
+	        <tr>
+	            <th>Platform</th>
+	            <th>Project Id</th>
+	            <th>Backtest Id</th>
+	        </tr>
+	    </thead>
+	    <tbody>
+	        <tr>
+	            <td>Cloud Platform</td>
+	            <td><a href='/docs/v2/cloud-platform/projects/getting-started#13-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/cloud-platform/backtesting/getting-started#06-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	        <tr>
+	            <td>Local Platform</td>
+	            <td><a href='/docs/v2/local-platform/projects/getting-started#14-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/local-platform/backtesting/getting-started#07-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	        <tr>
+	            <td>CLI</td>
+	            <td><a href='/docs/v2/lean-cli/projects/project-management#07-Get-Project-Id'>Get Project Id</a></td>
+	            <td><a href='/docs/v2/lean-cli/backtesting/deployment#05-Get-Backtest-Id'>Get Backtest Id</a></td>
+	        </tr>
+	    </tbody>
+	</table>
+	
 	<p>The <code class="csharp">ReadBacktestOrders</code><code class="python">read_backtest_orders</code> method returns a list of <code>Order</code> objects, which have the following properties:</p>
 	<div data-tree='QuantConnect.Orders.Order'></div>
 
-	<?php include(DOCS_RESOURCES."/qc-api/plot-fills.php"); ?>
+	<? include(DOCS_RESOURCES."/qc-api/plot-fills.php"); ?>
 </ol>
 
 
