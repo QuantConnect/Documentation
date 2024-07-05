baa2a7deb978de98bfd2aa22075e6f76c788c856
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-07-05 17:07:18 -0600

    Update 02 Read Live Results.php

--- 04 Research Environment/10 Meta Analysis/04 Live Analysis/02 Read Live Results.php
+++ 04 Research Environment/10 Meta Analysis/04 Live Analysis/02 Read Live Results.php
@@ -11,8 +11,34 @@ var liveAlgorithm = api.ReadLiveAlgorithm(projectId, deployId);</pre>
     <pre class="python">live_algorithm = api.read_live_algorithm(project_id, deploy_id)</pre>
 </div>
 
-<?php include(DOCS_RESOURCES."/qc-api/get-project-id-in-research.html"); ?>
-<?php include(DOCS_RESOURCES."/qc-api/get-deployment-id-in-research.html"); ?>
+<p>The following table provides links to documentation that explains how to get the project Id and deployment Id, depending on the platform you use:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th>Platform</th>
+            <th>Project Id</th>
+            <th>Deployment Id</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Cloud Platform</td>
+            <td><a href='/docs/v2/cloud-platform/projects/getting-started#13-Get-Project-Id'>Get Project Id</a></td>
+            <td><a href='/docs/v2/cloud-platform/live-trading/getting-started#06-Get-Deployment-Id'>Get Deployment Id</a></td>
+        </tr>
+        <tr>
+            <td>Local Platform</td>
+            <td><a href='/docs/v2/local-platform/projects/getting-started#14-Get-Project-Id'>Get Project Id</a></td>
+            <td><a href='/docs/v2/local-platform/live-trading/getting-started#08-Get-Deployment-Id'>Get Deployment Id</a></td>
+        </tr>
+        <tr>
+            <td>CLI</td>
+            <td><a href='/docs/v2/lean-cli/projects/project-management#07-Get-Project-Id'>Get Project Id</a></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
 
 <p>The <code class="csharp">ReadLiveAlgorithm</code><code class="python">read_live_algorithm</code> method returns a <code>LiveAlgorithmResults</code> object, which have the following attributes:</p>
 <div data-tree='QuantConnect.Api.LiveAlgorithmResults'></div>
