4d4bee975eda1ea8e3a46c8e44e62c20eaf5fcfc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-07-05 17:09:23 -0600

    Update 04 Plot Order Fills.php

--- 04 Research Environment/10 Meta Analysis/04 Live Analysis/04 Plot Order Fills.php
+++ 04 Research Environment/10 Meta Analysis/04 Live Analysis/04 Plot Order Fills.php
@@ -5,7 +5,32 @@
 	<div class="section-example-container">
 	    <pre class="python">orders = api.read_live_orders(project_id)</pre>
 	</div>
-	<?php include(DOCS_RESOURCES."/qc-api/get-project-id-in-research.html"); ?>
+
+        <p>The following table provides links to documentation that explains how to get the project Id, depending on the platform you use:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th style='width: 50%'>Platform</th>
+            <th>Project Id</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Cloud Platform</td>
+            <td><a href='/docs/v2/cloud-platform/projects/getting-started#13-Get-Project-Id'>Get Project Id</a></td>
+        </tr>
+        <tr>
+            <td>Local Platform</td>
+            <td><a href='/docs/v2/local-platform/projects/getting-started#14-Get-Project-Id'>Get Project Id</a></td>
+        </tr>
+        <tr>
+            <td>CLI</td>
+            <td><a href='/docs/v2/lean-cli/projects/project-management#07-Get-Project-Id'>Get Project Id</a></td>
+        </tr>
+    </tbody>
+</table>
+	
 	<p>By default, the orders with an ID between 0 and 100. To get orders with an ID greater than 100, pass <code>start</code> and <code>end</code> arguments to the <code class="csharp">ReadLiveOrders</code><code class="python">read_live_orders</code> method. Note that <code>end</code> - <code>start</code> must be less than 100.</p>
 	<div class="section-example-container">
 	    <pre class="python">orders = api.read_live_orders(project_id, 100, 150)</pre>
