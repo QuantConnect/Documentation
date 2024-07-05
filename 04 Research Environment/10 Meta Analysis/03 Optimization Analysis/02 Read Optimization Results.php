093a5f3ca51012fa629239124a49419b25e07da4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-07-05 16:58:53 -0600

    Update 02 Read Optimization Results.php

--- 04 Research Environment/10 Meta Analysis/03 Optimization Analysis/02 Read Optimization Results.php
+++ 04 Research Environment/10 Meta Analysis/03 Optimization Analysis/02 Read Optimization Results.php
@@ -5,6 +5,31 @@
     <pre class="python">optimization = api.read_optimization(optimization_id)</pre>
 </div>
 
+<p>The following table provides links to documentation that explains how to get the optimization Id, depending on the platform you use:</p>
+
+<table class="qc-table table">
+    <thead>
+        <tr>
+            <th style='width: 50%'>Platform</th>
+            <th>Optimziation Id</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Cloud Platform</td>
+            <td><a href='/docs/v2/cloud-platform/optimization/getting-started#06-Get-Optimization-Id'>Get Optimization Id</a></td>
+        </tr>
+        <tr>
+            <td>Local Platform</td>
+            <td><a href='/docs/v2/local-platform/optimization/getting-started#07-Get-Optimization-Id'>Get Optimization Id</a></td>
+        </tr>
+        <tr>
+            <td>CLI</td>
+            <td> </td>
+        </tr>
+    </tbody>
+</table>
+
 <p>To get the optimization Id, check the <a href='/docs/v2/cloud-platform/optimization/results#07-Individual-Backtest-Results'>individual backtest results table on the optimization results page</a>. An example optimization Id is O-696d861d6dbbed45a8442659bd24e59f.</p>
 
 <p>The <code class="csharp">ReadOptimization</code><code class="python">read_optimization</code> method returns an <code>Optimization</code> object, which have the following attributes:</p>
