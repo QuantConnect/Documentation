4ec9339b6fbd347e49ad5f1472db8d64c4876739
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-07-22 21:07:37 +0100

    Fixes Alpaca and TradeStation Deploy Process
    Adds information on the Autheticate button.

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/15 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/15 Deploy Live Algorithms.php
@@ -3,8 +3,29 @@ $brokerageName = "Alpaca";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Enter your Alpaca account Id and token.</li>";
-$authentication .= file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/alpaca.html");
+$authentication = "<li>Click on the <span class='field-name'>Environment</span> field and then click one of the environments.</li>
+<p>The following table shows the supported environments:</p>
+
+<table class='table qc-table'>
+    <thead>
+        <tr>
+            <th>Environment</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Paper</td>
+            <td>Trade with paper money</td>
+        </tr>
+        <tr>
+            <td>Live</td>
+            <td>Trade with real money</td>
+        </tr>
+    </tbody>
+</table>
+<li>Check the <span class=\"box-name\">Authorization</span> box, and click on <span class=\"button-name\">Authenticate</span> button.</li>
+<p>Click on the <span class=\"button-name\">Allow</span> button to grant QuantConnect access to your account information and authorization.</p>";
 $dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/alpaca'>Alpaca data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>";
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
--- 01 Cloud Platform/10 Live Trading/02 Brokerages/13 TradeStation/15 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/13 TradeStation/15 Deploy Live Algorithms.php
@@ -3,8 +3,58 @@ $brokerageName = "TradeStation";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Enter your TradeStation cliend Id and secret.</li>";
-$authentication .= file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/tradestation.html");
+$authentication = "<li>Click on the <span class='field-name'>Account Type</span> field and then click one of the types.</li>
+<p>The following table shows the account types:</p>
+
+<table class='table qc-table'>
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Cash</td>
+            <td>Cash account type</td>
+        </tr>
+        <tr>
+            <td>Margin</td>
+            <td>Margin account type</td>
+        </tr>
+        <tr>
+            <td>Futures</td>
+            <td>Futures account type</td>
+        </tr>
+        <tr>
+            <td>DVP</td>
+            <td>Delivery versus Payment (DVP) account type.</td>
+        </tr>
+    </tbody>
+</table>
+<li>Click on the <span class='field-name'>Environment</span> field and then click one of the environments.</li>
+<p>The following table shows the supported environments:</p>
+
+<table class='table qc-table'>
+    <thead>
+        <tr>
+            <th>Environment</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Simulator</td>
+            <td>Trade with paper money</td>
+        </tr>
+        <tr>
+            <td>Live</td>
+            <td>Trade with real money</td>
+        </tr>
+    </tbody>
+</table>
+<li>Click on <span class=\"button-name\">Authenticate</span> button.</li>
+<p>Login to your TradeStation account to grant QuantConnect access to your account information and authorization.</p>";
 $dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/tradestation'>TradeStation data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>";
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
