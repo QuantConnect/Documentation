2cd325db4971c975f8a35f73b790eb7058b0e288
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-16 11:56:14 -0600

    Update 07 0DTE.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/01 Universe/07 0DTE.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/01 Universe/07 0DTE.php
@@ -1,4 +1,4 @@
-<p><span class='new-term'>0DTE Options</span> are Option contracts that expire on the same day you trade them. To create an universe with 0DTE Options, call the <code class="csharp">SetFilter</code><code class="python">set_filter</code> method with the following argument:</p>
+<p><span class='new-term'>0DTE Options</span> are Option contracts that expire on the same day you trade them. To create a universe with 0DTE Options, call the <code class="csharp">SetFilter</code><code class="python">set_filter</code> method with the following argument:</p>
 
 <div class="section-example-container">
     <pre class="csharp">option.SetFilter(u => u.IncludeWeeklys().Expiration(0, 0).Strikes(minStrike, maxStrike));</pre>
