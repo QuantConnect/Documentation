6917544f47a1de7760bd9fb943a7754453f859ff
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-16 17:28:32 -0600

    Update Index Options docs

---
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/01 Requesting Data/02 Individual Contracts/13 Exceptions and Edge Cases.php
@@ -0,0 +1,37 @@
+<p>The following sections explain exceptions and edge cases with subscribing to individual Option contracts.</p>
+
+<h4>Default Underlying Subscription Settings</h4>
+
+<p>If you subscribe to an Index Option contract but don't have a subscription to the underlying Index, LEAN automatically subscribes to the underlying Index and sets its <a href='/docs/v2/writing-algorithms/securities/asset-classes/index/requesting-data#05-Fill-Forward'>fill forward</a> property to match that of the Index Option contract. In this case, you still need the Index <code class="csharp">Symbol</code><code class="python">symbol</code> to subscribe to Index Option contracts. If you don't have access to it, create it.</p>
+<div class="section-example-container">
+    <pre class="csharp">_symbol = QuantConnect.Symbol.Create("SPX", SecurityType.Index, Market.USA);</pre>
+    <pre class="python">self._symbol = Symbol.create("SPX", SecurityType.INDEX, Market.USA)</pre>
+</div>
+
+<h4>Manually Creating Option Symbol Objects</h4>
+
+<p>
+	To subscribe to an Option contract, you need the contract <code>Symbol</code>. 
+	To get Index Option contract <code>Symbol</code> objects, call the <code class="csharp">CreateOption</code><code class="python">create_option</code> method or use the <code class="csharp">OptionChainProvider</code><code class="python">option_chain_provider</code>.
+	If you use the <code class="csharp">CreateOption</code><code class="python">create_option</code> method, you need to know the specific contract details.</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Standard contracts
+_contractSymbol = QuantConnect.Symbol.CreateOption(_symbol, Market.USA,
+    OptionStyle.European, OptionRight.Call, 3650, new DateTime(2022, 6, 17));
+
+// Weekly contracts
+_weeklyContractSymbol = QuantConnect.Symbol.CreateOption(_symbol, "SPXW", Market.USA,
+    OptionStyle.European, OptionRight.Call, 3650, new DateTime(2022, 6, 17));</pre>
+    <pre class="python"># Standard contracts
+self._contract_symbol = Symbol.create_option(self._symbol, Market.USA,
+    OptionStyle.EUROPEAN, OptionRight.CALL, 3650, datetime(2022, 6, 17))
+
+# Weekly contracts
+self._weekly_contract_symbol = Symbol.create_option(self._symbol, "SPXW", Market.USA,
+    OptionStyle.EUROPEAN, OptionRight.CALL, 3650, datetime(2022, 6, 17))</pre>
+</div>
+
+<h4>Overriding the Initial Implied Volatility Guess</h4>
+
+<? include(DOCS_RESOURCES."/reality-modeling/volatility-model.html"); ?>
