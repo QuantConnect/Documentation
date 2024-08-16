1e10175a88087f175ef8e66af9fef6460ffff095
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-16 14:26:12 -0600

    Update set-filter.php

--- Resources/universes/option/set-filter.php
+++ Resources/universes/option/set-filter.php
@@ -1,9 +1,3 @@
-<p>By default, LEAN subscribes to the Option contracts that have the following characteristics:</p>
-
-<? $annotation = "exclude weeklys"; include(DOCS_RESOURCES."/universes/option/default-filter.php"); ?>
-
-<p>To adjust the universe of contracts, set a filter. The filter usually runs at the first bar of every day. When the filter selects a contract that isn't currently in your universe, LEAN adds the new contract data to the next <code>Slice</code> that it passes to the <code class="csharp">OnData</code><code class="python">on_data</code> method.</p>
-
 <p>To set a contract filter, in the <code class="csharp">Initialize</code><code class="python">initialize</code> method, call the <code class="csharp">SetFilter</code><code class="python">set_filter</code> method of the <code>Option</code> object. The following table describes the available filter techniques:</p>
 
 <table class="table qc-table">
