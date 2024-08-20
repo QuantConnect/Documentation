b5b74b8807ac056dc374e55ed5d904f2647abf04
baobach <95854748+baobach@users.noreply.github.com>
2024-08-20 14:29:36 +0700

    Adding comment for snippets

--- 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
@@ -1,9 +1,11 @@
 <? include(DOCS_RESOURCES."/universes/settings/minimum-time-in-universe.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
+    <pre class="csharp">\\ Each security in the universe will remain in the securities list for 7 days before being removed.
+        UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python">self.universe_settings.minimum_time_in_universe = timedelta(7)
+    <pre class="python"># Each security in the universe will remain in the securities list for 7 days before being removed.
+        self.universe_settings.minimum_time_in_universe = timedelta(7)
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
 
