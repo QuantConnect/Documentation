bd4ca0826b0b8d75ca20e8da0ac000eecc279488
Robert Bach <95854748+baobach@users.noreply.github.com>
2024-08-20 20:16:05 +0700

    Adding comment for snippets (#1829)
    * Adding comment for snippets
    
    * Adding comment for snippets
    
    * Adding comment for snippets
    
    * Update 05 Deposits and Withdraws.html
    
    * Update 06 Minimum Time in Universe.php
    
    * Update 04 Historical Data.html
    
    * Update 12 Remove Subscriptions.html
    
    * Update 04 Check Open Positions.html
    
    * Update 02 Null Model.html
    
    ---------
    
    Co-authored-by: Jared <jaredbroad@gmail.com>

--- 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
@@ -1,9 +1,11 @@
 <? include(DOCS_RESOURCES."/universes/settings/minimum-time-in-universe.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
+    <pre class="csharp">// Keep each security in universe for a minimum of 7 days.
+        UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python">self.universe_settings.minimum_time_in_universe = timedelta(7)
+    <pre class="python"># Keep each security in universe for a minimum of 7 days.
+        self.universe_settings.minimum_time_in_universe = timedelta(7)
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
 
