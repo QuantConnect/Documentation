d380991bf7b32523840b1072170d0810f6f276dc
Jared <jaredbroad@gmail.com>
2024-08-20 08:42:56 -0400

    Update 04 Algorithm Time Zone.php

--- 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
+++ 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
@@ -5,19 +5,18 @@ include(DOCS_RESOURCES."/initialization/set-time-zone.php");
 <p>To get the time zone of your algorithm, use the<code class="csharp">TimeZone</code><code class="python">time_zone</code> property.</p>
 
 <div class="section-example-container">
-<pre class="python"># Assign the time zone of the algorithm
+<pre class="python"># The algorithm timezone property can assist with multi-asset trading
     time_zone = self.time_zone</pre>
-<pre class="csharp">// Assign the time zone of the algorithm
+<pre class="csharp">// The algorithm timezone property can assist with multi-asset trading
     var timeZone = TimeZone;</pre>
 </div> 
 
  <p>To get the algorithm time in Coordinated Universal Time (UTC), use the <code class="csharp">UtcTime</code><code class="python">utc_time</code> property.</p>
 
-
 <div class="section-example-container">
-<pre class="python"># Assign the current UTC time to the variable
+<pre class="python"># Access the current UTC time to coordinate multi-timezone events or improve logging.
     utc_time = self.utc_time</pre>
-<pre class="csharp">// Assign the current UTC time to the variable
+<pre class="csharp">// Access the current UTC time to coordinate multi-timezone events or improve logging.
     var utcTime = UtcTime;</pre>
 </div> 
 
