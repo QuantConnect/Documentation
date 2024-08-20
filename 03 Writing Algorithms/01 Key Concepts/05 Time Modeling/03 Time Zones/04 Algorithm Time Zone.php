55495a28d77383204f7351f59d38096481d2fb32
Ashutosh <ashutoshrana171@gmail.com>
2024-08-19 21:49:12 -0400

    Add comments to code blocks

--- 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
+++ 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
@@ -5,16 +5,20 @@ include(DOCS_RESOURCES."/initialization/set-time-zone.php");
 <p>To get the time zone of your algorithm, use the<code class="csharp">TimeZone</code><code class="python">time_zone</code> property.</p>
 
 <div class="section-example-container">
-<pre class="python">time_zone = self.time_zone</pre>
-<pre class="csharp">var timeZone = TimeZone;</pre>
+<pre class="python"># Assign the time zone of the algorithm
+    time_zone = self.time_zone</pre>
+<pre class="csharp">// Assign the time zone of the algorithm
+    var timeZone = TimeZone;</pre>
 </div> 
 
  <p>To get the algorithm time in Coordinated Universal Time (UTC), use the <code class="csharp">UtcTime</code><code class="python">utc_time</code> property.</p>
 
 
 <div class="section-example-container">
-<pre class="python">utc_time = self.utc_time</pre>
-<pre class="csharp">var utcTime = UtcTime;</pre>
+<pre class="python"># Assign the current UTC time to the variable
+    utc_time = self.utc_time</pre>
+<pre class="csharp">// Assign the current UTC time to the variable
+    var utcTime = UtcTime;</pre>
 </div> 
 
 <p>The <code class="csharp">Time</code><code class="python">time</code> and <code class="csharp">UtcTime</code><code class="python">utc_time</code> objects have no time zone. LEAN maintains their state to be consistent.</p>
