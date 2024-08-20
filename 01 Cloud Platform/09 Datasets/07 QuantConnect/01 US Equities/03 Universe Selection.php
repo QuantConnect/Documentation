01eb34be1a91b5a406b61706ef2b0dada53a9f3c
Jared <jaredbroad@gmail.com>
2024-08-20 08:29:20 -0400

    Update 03 Universe Selection.php

--- 01 Cloud Platform/09 Datasets/07 QuantConnect/01 US Equities/03 Universe Selection.php
+++ 01 Cloud Platform/09 Datasets/07 QuantConnect/01 US Equities/03 Universe Selection.php
@@ -3,10 +3,10 @@
 <h4>Fundamental Universe</h4>
 <? include(DOCS_RESOURCES."/data-feeds/us-equities/fundamental-data-availability.html"); ?>
 <div class="section-example-container">
-    <pre class="csharp">// Set universe to asynchronous and add fundamental universe.
+    <pre class="csharp">// Run universe selection asynchronously to speed up your algorithm.
 UniverseSettings.Asynchronous = true;
 AddUniverse(SelectFundamental);</pre>
-    <pre class="python"># Set universe to asynchronous and add fundamental universe.
+    <pre class="python">#  Run universe selection asynchronously to speed up your algorithm.
 self.universe_settings.asynchronous = True
 self.add_universe(self._select_fundamental)</pre>
 </div>
@@ -15,11 +15,11 @@ self.add_universe(self._select_fundamental)</pre>
 <p>The QuantConnect data provider enables you to create a universe of securities to match the constituents of an ETF. For more information about ETF universes, see <a href='/docs/v2/writing-algorithms/universes/equity/etf-constituents-universes'>ETF Constituents Selection</a>.</p>
 
 <div class="section-example-container">
-    <pre class="csharp">// Enable asynchronous universe, add SPY equity, and add ETF universe to match constituents of SPY
+    <pre class="csharp">// Request an async universe of stocks that match the ETF constituents of SPY
 UniverseSettings.Asynchronous = true;
 var spy = AddEquity("SPY").Symbol;
 AddUniverse(Universe.ETF(spy, UniverseSettings, ETFConstituentsFilter));</pre>
-    <pre class="python"># Enable asynchronous universe, add SPY equity, and add ETF universe to match constituents of SPY
+    <pre class="python"># Request an async universe of stocks that match the ETF constituents of SPY
 self.universe_settings.asynchronous = True
 spy = self.add_equity("SPY").symbol
 self.add_universe(self.universe.etf(spy, self.universe_settings, self._etf_constituents_filter))</pre>
