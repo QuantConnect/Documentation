6b14d9a5a869bc2170f5268edee4438f475949cf
Jared <jaredbroad@gmail.com>
2024-08-20 09:05:35 -0400

    Update 06 Minimum Time in Universe.php

--- 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
@@ -1,10 +1,10 @@
 <? include(DOCS_RESOURCES."/universes/settings/minimum-time-in-universe.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">\\ Each security in the universe will remain in the securities list for 7 days before being removed.
+    <pre class="csharp">// Keep each security in universe for a minimum of 7 days.
         UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"># Each security in the universe will remain in the securities list for 7 days before being removed.
+    <pre class="python"># Keep each security in universe for a minimum of 7 days.
         self.universe_settings.minimum_time_in_universe = timedelta(7)
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
