e565c14bf72dac9a4c5bf90550a9460a677884fe
Robert Bach <95854748+baobach@users.noreply.github.com>
2024-08-21 09:02:06 +0700

    Update 04 Algorithm Time Zone.php

--- 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
+++ 03 Writing Algorithms/01 Key Concepts/05 Time Modeling/03 Time Zones/04 Algorithm Time Zone.php
@@ -6,18 +6,18 @@ include(DOCS_RESOURCES."/initialization/set-time-zone.php");
 
 <div class="section-example-container">
 <pre class="python"># The algorithm timezone property can assist with multi-asset trading
-    time_zone = self.time_zone</pre>
+time_zone = self.time_zone</pre>
 <pre class="csharp">// The algorithm timezone property can assist with multi-asset trading
-    var timeZone = TimeZone;</pre>
+var timeZone = TimeZone;</pre>
 </div> 
 
  <p>To get the algorithm time in Coordinated Universal Time (UTC), use the <code class="csharp">UtcTime</code><code class="python">utc_time</code> property.</p>
 
 <div class="section-example-container">
 <pre class="python"># Access the current UTC time to coordinate multi-timezone events or improve logging.
-    utc_time = self.utc_time</pre>
+utc_time = self.utc_time</pre>
 <pre class="csharp">// Access the current UTC time to coordinate multi-timezone events or improve logging.
-    var utcTime = UtcTime;</pre>
+var utcTime = UtcTime;</pre>
 </div> 
 
 <p>The <code class="csharp">Time</code><code class="python">time</code> and <code class="csharp">UtcTime</code><code class="python">utc_time</code> objects have no time zone. LEAN maintains their state to be consistent.</p>
