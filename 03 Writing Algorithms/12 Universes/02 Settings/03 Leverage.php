d0703ebacf2bacd10cdd1f09ec98a360f25e8619
Lahiru Fernando <lahiru54@uw.edu>
2024-08-22 16:51:37 -0700

    Universes-Settings partial comment update

--- 03 Writing Algorithms/12 Universes/02 Settings/03 Leverage.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/03 Leverage.php
@@ -1,8 +1,12 @@
 <? include(DOCS_RESOURCES."/universes/settings/leverage.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">UniverseSettings.Leverage = 2.0m;
+    <pre class="csharp"> // Assign leverage to 2.0 for all securities in universe. 
+UniverseSettings.Leverage = 2.0m;
+// Adds securities to universe that have top 50 highest dollar trading volume.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python">self.universe_settings.leverage = 2.0
+    <pre class="python"> # Assign leverage to 2.0 for universe. 
+self.universe_settings.leverage = 2.0
+# Adds securities to universe that have top 50 highest dollar trading volume.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
--- 03 Writing Algorithms/12 Universes/02 Settings/05 Extended Market Hours.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/05 Extended Market Hours.php
@@ -3,15 +3,21 @@
 <p>To enable extended market hours in non-derivative universes, in the <a href='/docs/v2/writing-algorithms/initialization'>Initialize</a> method, adjust the algorithm's <code class="csharp">UniverseSettings</code><code class="python">universe_settings</code> before you add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">UniverseSettings.ExtendedMarketHours = true;
+    <pre class="csharp"> // Enables extended market hour data for non-derivative universes (only for intraday resolutions). 
+UniverseSettings.ExtendedMarketHours = true;
+// Adds securities to universe that have top 50 highest dollar trading volume.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python">self.universe_settings.extended_market_hours = True
+    <pre class="python"> # Enables extended market hour data for non-derivative universes (only for intraday resolutions). 
+self.universe_settings.extended_market_hours = True
+# Adds securities to universe that have top 50 highest dollar trading volume.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
 
 <p>To enable extended market hours in derivative universes, pass an <code class="csharp">extendedMarketHours</code><code class="python">extended_market_hours</code> argument to the universe creation method.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">AddFuture(Futures.Currencies.BTC, extendedMarketHours: true);</pre>
-    <pre class="python">self.add_future(Futures.Currencies.BTC, extended_market_hours=True)</pre>
+    <pre class="csharp"> // Enabling extended market hours data in derivative universes.
+AddFuture(Futures.Currencies.BTC, extendedMarketHours: true);</pre>
+    <pre class="python"> # Enabling extended market hours data in derivative universes.
+self.add_future(Futures.Currencies.BTC, extended_market_hours=True)</pre>
 </div>
