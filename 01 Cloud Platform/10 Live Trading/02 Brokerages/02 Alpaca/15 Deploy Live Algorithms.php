b000fa137774060299f1a660628389d537cd420b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-23 12:15:21 -0600

    Update 15 Deploy Live Algorithms.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/15 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/15 Deploy Live Algorithms.php
@@ -26,7 +26,7 @@ $authentication = "<li>Click on the <span class='field-name'>Environment</span> 
 </table>
 <li>Check the <span class=\"box-name\">Authorization</span> check box and then click <span class=\"button-name\">Authenticate</span>.</li>
 <li>On the Alpaca website, click <span class=\"button-name\">Allow</span> to grant QuantConnect access to your account information and authorization.</li>";
-$dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/alpaca'>Alpaca data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p><p>If you add Alpaca data provider, enter your API key and secret. To get your API key and secret, see the Create an Account section in the <a href='/docs/v2/cloud-platform/live-trading/brokerages/alpaca#02-Account-Types'>Account Types</a> documentation. Your account details are not saved on QuantConnect.</p>";
+$dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/alpaca'>Alpaca data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p><p>If you add the Alpaca data provider, enter your API key and secret. To get your API key and secret, see <a href='/docs/v2/cloud-platform/live-trading/brokerages/alpaca#02-Account-Types'>Account Types</a>. Your account details are not saved on QuantConnect.</p>";
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
