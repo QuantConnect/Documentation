abfbfcb889c1c5d6080124854750cdd508cb14de
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-23 15:57:36 -0600

    Update 10 Remove Subscriptions.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/01 Requesting Data/02 Individual Contracts/10 Remove Subscriptions.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/01 Requesting Data/02 Individual Contracts/10 Remove Subscriptions.php
@@ -1,6 +1,6 @@
 <?php 
 $pythonVariableName = "self._contract_symbol";
 $cSharpVariableName = "_contractSymbol";
-$addContractMethodName = "AddIndexOptionContract";
+$addContractMethodName = "<code class='csharp'>AddIndexOptionContract</code><code class='python'>add_index_option_contract</code>";
 include(DOCS_RESOURCES."/securities/remove-option-contract.php"); 
 ?>
