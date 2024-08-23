6667ec09d2e13eb61b625728c42b629c12ae300e
Jared <jaredbroad@gmail.com>
2024-08-23 09:04:42 -0400

    Update 05 Extended Market Hours.php

--- 03 Writing Algorithms/12 Universes/02 Settings/05 Extended Market Hours.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/05 Extended Market Hours.php
@@ -3,11 +3,11 @@
 <p>To enable extended market hours in non-derivative universes, in the <a href='/docs/v2/writing-algorithms/initialization'>Initialize</a> method, adjust the algorithm's <code class="csharp">UniverseSettings</code><code class="python">universe_settings</code> before you add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp"> // Enables extended market hour data for non-derivative universes (only for intraday resolutions). 
+    <pre class="csharp"> // Enables pre and post market hours data assets in the universe. 
 UniverseSettings.ExtendedMarketHours = true;
 // Adds securities to universe that have top 50 highest dollar trading volume.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"> # Enables extended market hour data for non-derivative universes (only for intraday resolutions). 
+    <pre class="python"> # Enables pre and post market hours data assets in the universe.
 self.universe_settings.extended_market_hours = True
 # Adds securities to universe that have top 50 highest dollar trading volume.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
@@ -16,8 +16,8 @@ self.add_universe(self.universe.dollar_volume.top(50))</pre>
 <p>To enable extended market hours in derivative universes, pass an <code class="csharp">extendedMarketHours</code><code class="python">extended_market_hours</code> argument to the universe creation method.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp"> // Enabling extended market hours data in derivative universes.
+    <pre class="csharp"> // Requesting pre and post market hours data for the specific asset.
 AddFuture(Futures.Currencies.BTC, extendedMarketHours: true);</pre>
-    <pre class="python"> # Enabling extended market hours data in derivative universes.
+    <pre class="python"> # Requesting pre and post market hours data for the specific asset.
 self.add_future(Futures.Currencies.BTC, extended_market_hours=True)</pre>
 </div>
