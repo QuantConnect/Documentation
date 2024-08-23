161e7690a3a7c39a9c2ad218d008be424ea43485
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-23 16:01:30 -0600

    Update 11 Properties.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/02 Individual Contracts/11 Properties.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/02 Individual Contracts/11 Properties.php
@@ -1,4 +1,4 @@
 <?
-$addOptionContractAlias = "AddOptionContract";
+$addOptionContractAlias = "<code class='csharp'>AddOptionContract</code><code class='python'>add_option_contract</code>";
 include(DOCS_RESOURCES."/securities/option-properties.php"); 
 ?>
