567309ebacb7d652bc9a70561f3efcda81c0237a
Jared <jaredbroad@gmail.com>
2024-08-23 09:24:18 -0400

    Update 09 Asynchronous Selection.php

--- 03 Writing Algorithms/12 Universes/02 Settings/09 Asynchronous Selection.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/09 Asynchronous Selection.php
@@ -1,10 +1,10 @@
 <? include(DOCS_RESOURCES."/universes/settings/async.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">// To enable universe selection asynchronously, set the Asynchronous property in the UniverseSettings object to true.
+    <pre class="csharp">// To speed up algorithm performance you can set universe selection to happen asynchronously. Only use this when the code is self-contained and stateless.
 UniverseSettings.Asynchronous = true;
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"># To enable universe selection asynchronously, set the asynchronous field in the universe_settings object to true.
+    <pre class="python"># To speed up algorithm performance you can set universe selection to happen asynchronously. Only use this when the code is self-contained and stateless.
 self.universe_settings.asynchronous = True
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
