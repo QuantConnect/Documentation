a18d1d1452be64bbecf281c291e5581f5a868178
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-23 12:41:54 -0600

    Update 06 Minimum Time in Universe.php

--- 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/06 Minimum Time in Universe.php
@@ -1,13 +1,13 @@
 <? include(DOCS_RESOURCES."/universes/settings/minimum-time-in-universe.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">// Keep each security in universe for a minimum of 7 days.
+    <pre class="csharp">// Keep each security in the universe for a minimum of 7 days.
 UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
-// Adds securities to universe that have top 50 highest dollar trading volume.
+// Add the top 50 most liquid Equities to the universe.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"># Keep each security in universe for a minimum of 7 days.
+    <pre class="python"># Keep each security in the universe for a minimum of 7 days.
 self.universe_settings.minimum_time_in_universe = timedelta(7)
-# Adds securities to universe that have top 50 highest dollar trading volume.
+# Add the top 50 most liquid Equities to the universe.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
 
