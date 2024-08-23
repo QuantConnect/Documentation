0853352b67658b66eec83718644f87884c5f19e6
Jared <jaredbroad@gmail.com>
2024-08-23 09:31:54 -0400

    Update security-initializers.php

--- Resources/algorithm-framework/security-initializers.php
+++ Resources/algorithm-framework/security-initializers.php
@@ -2,7 +2,7 @@
 Instead of configuring global universe settings, you can individually configure the settings of each security in the universe with a security initializer. Security initializers let you apply any <a href='/docs/v2/writing-algorithms/reality-modeling/key-concepts#02-Security-Level-Models'>security-level reality model</a> or special data requests on a per-security basis. To set the security initializer, in the <code class="csharp">Initialize</code><code class="python">initialize</code> method, call the <code class="csharp">SetSecurityInitializer</code><code class="python">set_security_initializer</code> method and then define the security initializer.</p>
 </p>
 <div class="section-example-container">
-<pre class="csharp">// Disable trading fees on each security by passing CustomSecurityInitializer to SetSecurityInitializer function.
+<pre class="csharp">// A custom security initializer can override default models such as setting default fee and fill models for the security.
 SetSecurityInitializer(CustomSecurityInitializer);
 
 private void CustomSecurityInitializer(Security security)
@@ -10,7 +10,7 @@ private void CustomSecurityInitializer(Security security)
     security.SetFeeModel(new ConstantFeeModel(0, "USD"));
 }
 </pre>
-<pre class="python">#Disable trading fees on each security by passing _custom_security_initializer to set_security_initializer method.
+<pre class="python"># A custom security initializer can override default models such as setting default fee and fill models for the security.
 self.set_security_initializer(self._custom_security_initializer)
 
 def _custom_security_initializer(self, security: Security) -&gt; None:
