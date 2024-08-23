9bb048c80ecf483994980986a437f93283bf8952
Jared <jaredbroad@gmail.com>
2024-08-23 09:07:49 -0400

    Update 07 Data Normalization Mode.php

--- 03 Writing Algorithms/12 Universes/02 Settings/07 Data Normalization Mode.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/07 Data Normalization Mode.php
@@ -5,11 +5,11 @@
 <p>In the case of US Equities, the data normalization mode affects how historical data is adjusted for <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-actions'>corporate actions</a>. To view all the available options, see <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#11-Data-Normalization'>Data Normalization</a>. The default value is <code class="csharp">DataNormalizationMode.Adjusted</code><code class="python">DataNormalizationMode.ADJUSTED</code>. To change the data normalization mode, in the <a href='/docs/v2/writing-algorithms/initialization'>Initialize</a> method, adjust the algorithm's <code class="csharp">UniverseSettings</code><code class="python">universe_settings</code> before you add the universe.</p>
 
 <div class="section-example-container">
-    <pre class="csharp"> // Disabling getting historical data with corporate actions (dividends, splits, symbol changes, etc.) accounted for in universe.
+    <pre class="csharp"> // Pipe price data unadjusted for splits and dividends ("raw") into the algorithm. Required for options and useful for more accurately modeling historical periods.
 UniverseSettings.DataNormalizationMode = DataNormalizationMode.Raw;
 // Adds securities to universe that have top 50 highest dollar trading volume.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"> # Disabling getting historical data with corporate actions (dividends, splits, symbol changes, etc.) accounted for in universe.
+    <pre class="python"> # Pipe price data unadjusted for splits and dividends ("raw") into the algorithm. Required for options and useful for more accurately modeling historical periods.
 self.universe_settings.data_normalization_mode = DataNormalizationMode.RAW
 # Adds securities to universe that have top 50 highest dollar trading volume.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
@@ -19,8 +19,8 @@ self.add_universe(self.universe.dollar_volume.top(50))</pre>
 <p>In the case of Futures, the data normalization mode affects how historical data of two contracts is stitched together to form the <a href='/docs/v2/writing-algorithms/universes/futures#12-Continous-Contracts'>continuous contract</a>. To view all the available options, see <a href='/docs/v2/writing-algorithms/securities/asset-classes/futures/requesting-data#09-Data-Normalization'>Data Normalization</a>. The default value is <code class="csharp">DataNormalizationMode.Adjusted</code><code class="python">DataNormalizationMode.ADJUSTED</code>. To change the data normalization mode, in the <a href='/docs/v2/writing-algorithms/initialization'>Initialize</a> method, pass a <code class="csharp">dataNormalizationMode</code><code class="python">data_normalization_mode</code> argument to the <code class="csharp">AddFuture</code><code class="python">add_future</code> method.</p>
 
 <div class="section-example-container">
-    <pre class="csharp"> //
+    <pre class="csharp"> // Set a specific continuous contract data normalization mode with the AddFuture dataNormalizationMode parameter.
 AddFuture(Futures.Currencies.BTC, dataNormalizationMode: DataNormalizationMode.BackwardsRatio);</pre>
-    <pre class="python"> # 
+    <pre class="python"> #  Set a specific continuous contract data normalization mode with the add_future data_normalization_mode parameter.
 self.add_future(Futures.Currencies.BTC, data_normalization_mode=DataNormalizationMode.BACKWARDS_RATIO)</pre>
 </div>
