d433f530a0b0451b943b6c16a6cec1cd6e7c6fc2
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-08-28 21:08:03 +0100

    Adds List of Market to Security Identifier

---
+++ 03 Writing Algorithms/01 Key Concepts/06 Security Identifiers/06 Markets.php
@@ -0,0 +1,4 @@
+<p>The <code class="csharp">Market</code><code class="python">market</code> property distinguishes between tickers that have the same string value but represent different underlying assets. A prime example of this is the various market makers who have different prices for EURUSD. We store this data separately and as they have different fill prices, we treat the execution venues as different markets.</p>
+
+<p>The following table shows the availble markets for different underlying assets.</p>
+<? include(DOCS_RESOURCES."/datasets/supported-securities/markets.html"); ?>
