5371263e4576628f75ba630df3680d6c4c7decb6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-29 11:29:30 -0600

    Update 02 Bars.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Index/02 Handling Data/02 Bars.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/08 Index/02 Handling Data/02 Bars.php
@@ -1,8 +1,6 @@
 <p>You can't trade Indices, but <code>TradeBar</code> objects are bars that represent the open, high, low, and close of an Index price over a period of time.</p>
 <img src='https://cdn.quantconnect.com/i/tu/index-trade-bar.png' class='img-responsive' alt="Trade bar breakdown">
-<p><code>TradeBar</code> objects have the following properties:</p>    
-<div data-tree='QuantConnect.Data.Market.TradeBar'></div>  
-  
+ 
 <p>To get the <code>TradeBar</code> objects in the <code>Slice</code>, index the <code>Slice</code> or index the <code class="csharp">Bars</code><code class="python">bars</code> property of the <code>Slice</code> with the Index <code>Symbol</code>. The <code>Slice</code> may not contain data for your <code>Symbol</code> at every time step. To avoid issues, check if the <code>Slice</code> contains data for your Index before you index the <code>Slice</code> with the Index <code>Symbol</code>.</p>
     
 <div class='section-example-container'>
@@ -19,3 +17,6 @@
         trade_bar = slice.bars[self._symbol]
         value = trade_bar.value</pre>
 </div>
+
+<p><code>TradeBar</code> objects have the following properties:</p>    
+<div data-tree='QuantConnect.Data.Market.TradeBar'></div>  
