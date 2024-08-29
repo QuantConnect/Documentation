c6d67e5c30538a96869fc8e412d5197a6b8a5b5f
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-29 13:34:41 -0600

    Update 07 Data Normalization.php

--- 04 Research Environment/03 Datasets/04 US Equity/07 Data Normalization.php
+++ 04 Research Environment/03 Datasets/04 US Equity/07 Data Normalization.php
@@ -1,9 +1,15 @@
-<?
-$cSharpVariableName = "spy";
-$pyVariableName = "spy";
-$ticker = "SPY";
-include(DOCS_RESOURCES."/securities/data-normalization.php");
-?>
+<p>The data normalization mode defines how historical data is adjusted for <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-actions'>corporate actions</a>. By default, LEAN adjusts US Equity data for splits and dividends to produce a smooth price curve, but the following data normalization modes are available:</p>
+    
+<div data-tree='QuantConnect.DataNormalizationMode' data-fields='Raw,RAW,Adjusted,ADJUSTED,SplitAdjusted,SPLIT_ADJUSTED,TotalReturn,TOTAL_RETURN,ScaledRaw,SCALED_RAW'></div>
+
+<? include(DOCS_RESOURCES."/datasets/data-normalization.php"); ?>
+
+<p>To set the data normalization mode for a security, pass a <code class="csharp">dataNormalizationMode</code><code class="python">data_normalization_mode</code> argument to the <code class="csharp">AddEquity</code><code class="python">add_equity</code> method.</p>    
+
+<div class='section-example-container'>
+    <pre class='csharp'>var spy = qbAddEquity("SPY", dataNormalizationMode: DataNormalizationMode.Raw).Symbol;</pre>
+    <pre class='python'>spy = qb.add_equity("SPY", data_normalization_mode=DataNormalizationMode.RAW).symbol</pre>
+</div>
 
 <p>When you request historical data, the <code class="csharp">History</code><code class="python">history</code> method uses the data normalization of your security subscription. To get historical data with a different data normalization, pass a <code class="csharp">dataNormalizationMode</code><code class="python">data_normalization_mode</code> argument to the <code class="csharp">History</code><code class="python">history</code> method.</p>
 <div class="section-example-container">
