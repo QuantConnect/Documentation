ac46c4314affc1dd619e0bac3462653668a80ec6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-08-30 10:04:54 -0600

    Update 06 Minimum Time in Universe.php

--- 03 Writing Algorithms/34 Algorithm Framework/02 Universe Selection/02 Universe Settings/06 Minimum Time in Universe.php
+++ 03 Writing Algorithms/34 Algorithm Framework/02 Universe Selection/02 Universe Settings/06 Minimum Time in Universe.php
@@ -1,10 +1,10 @@
 <? include(DOCS_RESOURCES."/universes/settings/minimum-time-in-universe.php"); ?> create the Universe Selection model.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">// Keep each security in universe for a minimum of 7 days.
+    <pre class="csharp">// Keep each security in the universe for a minimum of 7 days.
 UniverseSettings.MinimumTimeInUniverse = TimeSpan.FromDays(7);
 AddUniverseSelection(new ETFConstituentsUniverseSelectionModel("QQQ"));</pre>
-    <pre class="python"># Keep each security in universe for a minimum of 7 days.
+    <pre class="python"># Keep each security in the universe for a minimum of 7 days.
 self.universe_settings.minimum_time_in_universe = timedelta(7)
 self.add_universe_selection(ETFConstituentsUniverseSelectionModel("QQQ"))</pre>
 </div>
