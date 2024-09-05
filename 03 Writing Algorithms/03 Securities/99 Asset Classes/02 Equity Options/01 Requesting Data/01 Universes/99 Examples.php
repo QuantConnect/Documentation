766964475aa4a7dd19ae3f7c4ed911f57fb08dca
LouisSzeto <hke0073@icloud.com>
2024-09-05 16:28:25 +0800

    add examples for request equity option universe data

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/01 Universes/99 Examples.php
+++
@@ -1,9 +0,0 @@
-<h4>Example 1: Selecting for 0DTE Contracts</h4>
-
-<p><span class='new-term'>0DTE Options</span> are Option contracts that expire on the same day you trade them. To create a universe with 0DTE Options, call the <code class="csharp">SetFilter</code><code class="python">set_filter</code> method with the following argument:</p>
-
-<div class="section-example-container">
-    <pre class="csharp">option.SetFilter(u => u.IncludeWeeklys().Expiration(0, 0).Strikes(minStrike, maxStrike));</pre>
-    <pre class="python">option.set_filter(lambda u: u.include_weeklys().expiration(0, 0).strikes(min_strike, max_strike))
-</pre>
-</div>
