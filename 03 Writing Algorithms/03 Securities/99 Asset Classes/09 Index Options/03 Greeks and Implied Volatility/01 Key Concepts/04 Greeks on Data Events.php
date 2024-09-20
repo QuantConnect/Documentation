53c6d2960614c8e7a1502992349850e40e3798cb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:28:32 -0600

    Update 04 Greeks on Data Events.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/03 Greeks and Implied Volatility/01 Key Concepts/04 Greeks on Data Events.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/03 Greeks and Implied Volatility/01 Key Concepts/04 Greeks on Data Events.php
@@ -1,7 +1,7 @@
 <? 
-$addOptionPy = "self.add_index_option(\"SPX\")
+$addOptionPy = "self.add_index_option(\"VIX\", \"VIXW\")
 ";
-$addOptionC = "AddIndexOption(\"SPX\");
+$addOptionC = "AddIndexOption(\"VIX\", \"VIXW\");
 ";
 include(DOCS_RESOURCES."/option-pricing/greek-data-event.php"); 
 ?>
