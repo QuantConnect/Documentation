8da913433183e2071d06cbcc80d7e68fbb6a7c07
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 11:19:34 -0600

    Update greeks-and-iv.php

--- Resources/universes/option/greeks-and-iv.php
+++ Resources/universes/option/greeks-and-iv.php
@@ -1,7 +1,6 @@
 <p>
   There are several ways to get the implied volatility and Greeks of contracts.
   The values in the universe filter function are pre-calculated, daily values.
-  If you set a <a href='/docs/v2/writing-algorithms/reality-modeling/options-models/pricing'>price model</a>, it sets the price model of the contracts in the universe.
 </p>
 
 <div class="section-example-container">
@@ -9,7 +8,10 @@
     <pre class="python"># TODO</pre>
 </div>
 
-<p>To override the default <a href="/docs/v2/writing-algorithms/reality-modeling/options-models/pricing">pricing model</a> of the Option, <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/reality-modeling/options-models/pricing#04-Set-Models'>set a pricing model</a>.</p>
+<p>
+  To override the default <a href="/docs/v2/writing-algorithms/reality-modeling/options-models/pricing">pricing model</a> of the Option, <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/reality-modeling/options-models/pricing#04-Set-Models'>set a pricing model</a>.
+  If you set a price model, it sets the price model of the individual contracts in the universe.
+</p>
 
 <div class="section-example-container">
     <pre class="csharp">// Set price_model property to use the Crank-Nicolson finite-difference model to price the Options.
