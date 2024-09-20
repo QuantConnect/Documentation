da08b7c7f3f381bfffb24c7c7057807a6b117fb7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 13:50:29 -0600

    Update 02 Greeks on Universes.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
@@ -3,5 +3,6 @@ $addOptionPy = "self.add_option(\"GOOG\")
 ";
 $addOptionC = "AddOption(\"GOOG\");
 ";
+$indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
 include(DOCS_RESOURCES."/option-pricing/greek-universe.php"); 
 ?>
