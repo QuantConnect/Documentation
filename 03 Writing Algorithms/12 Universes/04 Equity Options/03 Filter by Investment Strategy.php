69f17933f48010f66b2ba8c2e96d5172f0d53d8d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 09:23:15 -0600

    Update 03 Filter by Investment Strategy.php

--- 03 Writing Algorithms/12 Universes/04 Equity Options/03 Filter by Investment Strategy.php
+++ 03 Writing Algorithms/12 Universes/04 Equity Options/03 Filter by Investment Strategy.php
@@ -1,12 +1,7 @@
-<? 
-  include(DOCS_RESOURCES."/universes/option/set-default-filter.php"); 
-  include(DOCS_RESOURCES."/universes/option/set-filter.php"); 
-?>
-<p>The following table describes the filter methods of the <code>OptionFilterUniverse</code> class:</p>
+<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts of the same Equity underlying with different features. The following table describes the filter methods of the <code>OptionFilterUniverse</code> class that select contract for Option strategies.</p>
 
 <?
   $assetClass = "Option";
-  include(DOCS_RESOURCES."/universes/option/option-filter-universe.html"); 
-  include(DOCS_RESOURCES."/universes/option/filter-examples.html"); 
-  include(DOCS_RESOURCES."/universes/option/filter-caveats.php");
+  include(DOCS_RESOURCES."/universes/option/option-filter-universe-strategies.html"); 
+  include(DOCS_RESOURCES."/universes/option/filter-examples-strategies.html");
 ?>
