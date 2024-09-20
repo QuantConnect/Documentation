87a3980e04dac693a2f032f92c6d4756e21814e7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 10:27:22 -0600

    Update and rename 03 Filter Contracts.php to 03 Filter by Investment Strategy.php

---
+++ 03 Writing Algorithms/12 Universes/08 Index Options/03 Filter by Investment Strategy.php
@@ -0,0 +1,7 @@
+<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts on the same underlying Index, but with different features.</p>
+
+<?
+  $assetClass = "Option";
+  include(DOCS_RESOURCES."/universes/option/filter-examples-strategies.html");
+  include(DOCS_RESOURCES."/universes/option/option-filter-universe-strategies.html"); 
+?>
