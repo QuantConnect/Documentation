9e32b3d7a3184828f45855a7ec24f6ea658f4b11
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 10:30:50 -0600

    Create 04 Filter by Implied Volatility and Greeks.php

---
+++ 03 Writing Algorithms/12 Universes/08 Index Options/04 Filter by Implied Volatility and Greeks.php
@@ -0,0 +1,11 @@
+<p>
+  Option price models compute the theoretical price of Option contracts, their implied volatility, and their Greek values. 
+  Theoretical prices can help you detect undervalued and overvalued contracts, implied volatility can provide you insight into the upcoming volatility of the underlying security, and Greek values can help you hedge your portfolio. 
+</p>
+
+<?
+  $assetClass = "Option";
+  include(DOCS_RESOURCES."/universes/option/filter-examples-pricing.html");
+  include(DOCS_RESOURCES."/universes/option/option-filter-universe-pricing.html"); 
+  include(DOCS_RESOURCES."/universes/option/filter-examples-pricing-isolated-filter.html");
+?>
