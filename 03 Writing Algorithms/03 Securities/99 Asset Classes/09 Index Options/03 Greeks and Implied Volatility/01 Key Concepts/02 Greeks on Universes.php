27816b4f46ce744f2d742a84338df7bd047f8dac
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:30:13 -0600

    Update 02 Greeks on Universes.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
@@ -1,7 +1,7 @@
 <? 
-$addOptionPy = "self.add_index_option(\"VIX\", \"VIXW\")
+$addOptionPy = "self.add_index_option(\"SPX\")
 ";
-$addOptionC = "AddIndexOption(\"VIX\", \"VIXW\");
+$addOptionC = "AddIndexOption(\"SPX\");
 ";
 $filterLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/requesting-data/universes#04-Filter-by-Implied-Volatility-and-Greeks";
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/greeks-and-implied-volatility/indicators";
