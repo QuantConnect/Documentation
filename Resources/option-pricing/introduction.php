f94cbebab7986a736a7c51a765a8a9dabdf7acb0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:04:29 -0600

    Update introduction.php

--- Resources/option-pricing/introduction.php
+++ Resources/option-pricing/introduction.php
@@ -1,1 +1,5 @@
-<p>Two different Precalc and Online driven</p>
+<p>
+  LEAN provides multiple ways for you to get the Greeks and implied volatility (IV) of Option contracts.
+  There is a <a href='/datasets/quantconnect-us-equity-option-universe'>daily, pre-calculated dataset</a> based on the end of the previous trading day.
+  There is also a stream of values that are calculated on-the-fly as your algorithm runs.
+</p>
