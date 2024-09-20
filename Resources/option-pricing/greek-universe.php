20ff3d7f12598181b409ea4d9396aa3097caa041
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 13:40:36 -0600

    Update greek-universe.php

--- Resources/option-pricing/greek-universe.php
+++ Resources/option-pricing/greek-universe.php
@@ -3,10 +3,10 @@
 <div class="section-example-container">
     <pre class="csharp">public override void Initialize()
 {
-    var option = {$addOptionC}
+    var option = <?=$addOptionC?>
     option.SetFilter(optionFilterUniverse => optionFilterUniverse.IncludeWeeklys().Delta(0.3m, 0.7m).Expiration(0, 7);
 }</pre>
     <pre class="python">def initialize(self):
-    option = {$addOptionPy}
+    option = <?=$addOptionPy?>
     option.set_filter(lambda option_filter_universe: option_filter_universe.include_weeklys().delta(0.3, 0.7).expiration(0,7))</pre>
 </div>
