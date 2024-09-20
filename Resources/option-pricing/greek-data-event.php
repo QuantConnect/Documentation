1405f9f25eb5cf6f1832bea3fbd15c78e0d9f321
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:36:57 -0600

    Update greek-data-event.php

--- Resources/option-pricing/greek-data-event.php
+++ Resources/option-pricing/greek-data-event.php
@@ -4,14 +4,37 @@
 </p>
 
 <div class="section-example-container">
-    <pre class="csharp">public override Initialize()
-{
-    var option = <?=$addOptionC?>
-    option.PriceModel = OptionPriceModels.CrankNicolsonFD();
+    <pre class="csharp">public class OptionDataEventsAlgorithm : QCAlgorithm
+{        
+    public override void Initialize()
+    {
+        var option = <?=$addOptionC?>
+        option.PriceModel = OptionPriceModels.CrankNicolsonFD();
+    }
+
+    public override void OnData(Slice data)
+    {
+        foreach (var chain in data.OptionChains.Values)
+        {
+            foreach (var contract in chain)
+            {
+                var iv = contract.ImpliedVolatility;
+                var delta = contract.Greeks.Delta;
+            }
+        }
+    }
 }</pre>
-    <pre class="python">def initialize(self):
-    option = <?=$addOptionPy?>
-    option.price_model = OptionPriceModels.crank_nicolson_fd()</pre>
+    <pre class="python">class OptionDataEventsAlgorithm(QCAlgorithm):
+    
+    def initialize(self):
+        option = <?=$addOptionPy?>
+        option.price_model = OptionPriceModels.crank_nicolson_fd()
+    
+    def on_data(self, data):
+        for canonical_symbol, chain in data.option_chains.items():
+            for contract in chain:
+                iv = contract.implied_volatility
+                delta = contract.greeks.delta</pre>
 </div>
 
 <p>To view all the models LEAN supports, see <a href='/docs/v2/writing-algorithms/reality-modeling/options-models/pricing#06-Supported-Models'>Supported Models</a>.</p>
