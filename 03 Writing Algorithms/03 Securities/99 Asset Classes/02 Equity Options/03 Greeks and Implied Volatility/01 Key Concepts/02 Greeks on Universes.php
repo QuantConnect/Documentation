223e88f883feada64fa8d51e72bdd015b5e5aadb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:50:32 -0600

    Update 02 Greeks on Universes.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/02 Greeks on Universes.php
@@ -5,6 +5,6 @@ $addOptionC = "AddOption(\"SPY\");
 ";
 $filterLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/requesting-data/universes#04-Filter-by-Implied-Volatility-and-Greeks";
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
-$calculationMethod = "To calculate the values, we use <a href='/research/16977/greeks-and-iv-implementation/p1'>our implementation</a> the forward tree pricing model, which accounts for the interest rate and dividend payments.";
+$calculationMethod = "To calculate the values, we use <a href='/research/16977/greeks-and-iv-implementation/p1'>our implementation</a> of the forward tree pricing model, which accounts for the interest rate and dividend payments.";
 include(DOCS_RESOURCES."/option-pricing/greek-universe.php"); 
 ?>
