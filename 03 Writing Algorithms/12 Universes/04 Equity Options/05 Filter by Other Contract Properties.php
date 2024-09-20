b42b68711bbc75ca79cac865b919ca76bc75f157
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 09:46:09 -0600

    Update 05 Filter by Other Contract Properties.php

--- 03 Writing Algorithms/12 Universes/04 Equity Options/05 Filter by Other Contract Properties.php
+++ 03 Writing Algorithms/12 Universes/04 Equity Options/05 Filter by Other Contract Properties.php
@@ -1,7 +1,5 @@
-<? include(DOCS_RESOURCES."/universes/option/set-filter.php"); ?>
-<p>The following table describes the filter methods of the <code>OptionFilterUniverse</code> class:</p>
-
 <?
+  include(DOCS_RESOURCES."/universes/option/set-filter.php");
   $assetClass = "Option";
   include(DOCS_RESOURCES."/universes/option/option-filter-universe.html"); 
   include(DOCS_RESOURCES."/universes/option/filter-examples.html"); 
