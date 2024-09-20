f2a23135efe9ebeb1a451541ef103aca91d118cd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 13:51:34 -0600

    Update greek-universe.php

--- Resources/option-pricing/greek-universe.php
+++ Resources/option-pricing/greek-universe.php
@@ -1,12 +1,17 @@
-<p>The Greeks and implied volatility values in the filter function are daily, pre-calculated values based on the end of the previous trading day.</p>
+<p>The Greeks and IV values in the filter function are daily, pre-calculated values based on the end of the previous trading day.</p>
 
 <div class="section-example-container">
     <pre class="csharp">public override void Initialize()
 {
     var option = <?=$addOptionC?>
-    option.SetFilter(optionFilterUniverse => optionFilterUniverse.IncludeWeeklys().Delta(0.3m, 0.7m).Expiration(0, 7);
+    option.SetFilter(universe => universe.IncludeWeeklys().Delta(0.3m, 0.7m).Expiration(0, 7);
 }</pre>
     <pre class="python">def initialize(self):
     option = <?=$addOptionPy?>
-    option.set_filter(lambda option_filter_universe: option_filter_universe.include_weeklys().delta(0.3, 0.7).expiration(0,7))</pre>
+    option.set_filter(lambda universe: universe.include_weeklys().delta(0.3, 0.7).expiration(0,7))</pre>
 </div>
+
+<p>
+    You can't customize the Greeks and IV values that the filter function receives.
+    However, you can create <a href='<?=$indicatorLink?>'>indicators</a> to customize how the Greeks and IV are calculated for the contracts already in your universe.
+</p>
