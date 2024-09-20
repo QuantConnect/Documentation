689e835379f044466fce7a0ec3454a2f46233e4d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:19:31 -0600

    Update 04 Greeks on Data Events.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/04 Greeks on Data Events.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/04 Greeks on Data Events.php
@@ -1,1 +1,7 @@
-<? include(DOCS_RESOURCES."/option-pricing/greek-data-event.php"); ?>
+<? 
+$addOptionPy = "self.add_option(\"GOOG\")
+";
+$addOptionC = "AddOption(\"GOOG\");
+";
+include(DOCS_RESOURCES."/option-pricing/greek-data-event.php"); 
+?>
