953620a7e4d9d0a095875968ed337d482d615fc8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 10:31:12 -0600

    Create 05 Filter by Other Contract Properties.php

---
+++ 03 Writing Algorithms/12 Universes/08 Index Options/05 Filter by Other Contract Properties.php
@@ -0,0 +1,7 @@
+<?
+  include(DOCS_RESOURCES."/universes/option/set-filter.php");
+  $assetClass = "Option";
+  include(DOCS_RESOURCES."/universes/option/option-filter-universe.html"); 
+  include(DOCS_RESOURCES."/universes/option/filter-examples.html"); 
+  include(DOCS_RESOURCES."/universes/option/filter-caveats.php");
+?>
