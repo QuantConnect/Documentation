09651c461504fcc64bb5bafa38c8d85b0c3aa371
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 14:43:43 -0600

    Update 05 Greek Indicators.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/05 Greek Indicators.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/05 Greek Indicators.php
@@ -1,3 +1,4 @@
-<? include(DOCS_RESOURCES."/option-pricing/greek-indicators.php"); ?>
-
-<p>For more information about the Greek Indicators, see <a href='/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators'>Indicators</a>.</p>
+<? 
+$indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
+include(DOCS_RESOURCES."/option-pricing/greek-indicators.php"); 
+?>
