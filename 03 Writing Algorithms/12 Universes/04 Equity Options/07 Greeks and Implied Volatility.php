bd5bec93aa90e6d71b17a892cce423b18f83f3c6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-20 10:16:19 -0600

    Update 07 Greeks and Implied Volatility.php

--- 03 Writing Algorithms/12 Universes/04 Equity Options/07 Greeks and Implied Volatility.php
+++ 03 Writing Algorithms/12 Universes/04 Equity Options/07 Greeks and Implied Volatility.php
@@ -1,1 +1,10 @@
+<p>
+  There are several ways to get the implied volatility and Greeks of contracts.
+  The values in the universe filter function are pre-calculated, daily values.
+  If you set a <a href='/docs/v2/writing-algorithms/reality-modeling/options-models/pricing'>price model</a>, it sets the price model of the contracts in the universe.
+</p>
 
+<div class="section-example-container">
+    <pre class="csharp"></pre>
+    <pre class="python"></pre>
+</div>
