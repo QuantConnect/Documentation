14e9e9280066b631bbc5bf22a30d409bb412551f
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-09-25 00:09:26 +0100

    Create Download Files Section

---
+++ 05 Lean CLI/12 Object Store/04 Download Files.php
@@ -0,0 +1,7 @@
+<? include(DOCS_RESOURCES."/object-store/download-files-from-cloud-permission.php"); ?>
+<p>To download a file or directory from the Object Store, run <code>lean cloud object-store get &lt;key&gt;</code>.</p>
+<div class="cli section-example-container">
+<pre>$ lean cloud object-store get 15710069/adjusted
+Fetching object store download url
+Unzipping object store keys values into: &lt;Current Directory&gt;</pre>
+</div>
