5b1e8119cc808b27fde0559c34e2297c0deb2e1b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-25 09:00:20 -0600

    Update 05 Gamma.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/02 Indicators/05 Gamma.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/02 Indicators/05 Gamma.php
@@ -52,6 +52,6 @@ include(DOCS_RESOURCES."/option-indicators/manual-indicator.php");
 <h4>Volatility Smoothing</h4>
 <?
 $typeName = "gamma";
-$ivLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators#03-Implied-Volatility";
+$ivLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators#02-Parameters";
 include(DOCS_RESOURCES."/option-indicators/iv-smoothing.php");
 ?>
