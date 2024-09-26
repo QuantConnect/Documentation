159996315ea959aefb536a12621de317a006d429
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-26 12:13:39 -0600

    Update greek-history.php

--- Resources/option-pricing/greek-history.php
+++ Resources/option-pricing/greek-history.php
@@ -9,7 +9,7 @@
     public override void Initialize()
     {
         SetStartDate(2020, 1, 1);
-        var option = AddOption("SPY");
+        var option = <=$addOptionC?>
         var history = History&lt;OptionUniverse&gt;(option.Symbol, 5);
         foreach (var chain in history)
         {
@@ -28,6 +28,46 @@
     <pre class="python"># Coming soon</pre>
   </div>
 
+<!-- FOP code when the data is available:
+  
+public class OptionHistoryAlgorithm : QCAlgorithm
+{       
+    public override void Initialize()
+    {
+        SetStartDate(2020, 1, 1);
+        // Subscribe to the underlying Futures contracts.
+        var future = AddFuture(
+            Futures.Indices.SP500EMini,
+            dataMappingMode: DataMappingMode.OpenInterest,
+            dataNormalizationMode: DataNormalizationMode.BackwardsRatio,
+            contractDepthOffset: 0
+        );
+        future.SetFilter(futureFilterUniverse => futureFilterUniverse.FrontMonth());
+        // Subscribe to the Future Options contracts.
+        AddFutureOption(future.Symbol);
+        // Get the Futures contract that's currently in the universe.
+        var futuresContract = FutureChainProvider
+            .GetFutureContractList(future.Symbol, Time)
+            .OrderBy(symbol => symbol.ID.Date)
+            .First();
+        // Get the history of Option contracts for the selected Futures contract.
+        var history = History&lt;OptionUniverse&gt;(futuresContract, 5);
+        foreach (var chain in history)
+        {
+            var endTime = chain.EndTime;
+            var filteredContracts = chain.Data
+                .Select(contract => contract as OptionUniverse)
+                .Where(contract => contract.Greeks.Delta > 0.3m);
+            foreach (var contract in filteredContracts)
+            {
+                var price = contract.Price;
+                var iv = contract.ImpliedVolatility;
+            }
+        }
+    }
+}
+-->
+  
   <p>
     You can't customize the Greeks and IV values that you get from a history request.
     However, you can create <a href='<?=$indicatorLink?>'>indicators</a> to customize how the Greeks and IV are calculated for the contracts already in your universe.
