1c9270932abc2a9b7363a8949f57263bb0925fdb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-26 08:23:54 -0600

    Update index.php

--- Resources/option-indicators/index.php
+++ Resources/option-indicators/index.php
@@ -9,11 +9,13 @@ $underlyingSubscriptionC = "_underlying = AddIndex(\"SPX\").Symbol;
 $dividendYieldProviderConstructorC = "new(_underlying);";
 $scheduleSymbolC = "_underlying";
 $underlyingSymbolC = "_underlying";
+$addContractMethodC = "AddIndexOptionContract";
 $mirrorExtensionC = ".Value";
 $underlyingSubscriptionPy = "self._underlying = self.add_index('SPX').symbol
 ";
 $dividendYieldProviderConstructorPy = "DividendYieldProvider(self._underlying)";
 $scheduleSymbolPy = "self._underlying";
 $underlyingSymbolPy = "self._underlying";
+$addContractMethodC = "add_index_option_contract";
 $mirrorExtensionPy = "";
 ?>
