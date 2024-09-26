bfc86b95312cad5ae11e8c03e93a66fd66f6a5d9
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-09-26 13:51:12 -0600

    Update greek-history.php

--- Resources/option-pricing/greek-history.php
+++ Resources/option-pricing/greek-history.php
@@ -2,9 +2,7 @@
 
 <? include(DOCS_RESOURCES."/option-pricing/greek-history-code.php"); ?>
   
-  <p>
+  <p class='python'>
     You can't customize the Greeks and IV values that you get from a history request.
     However, you can create <a href='<?=$indicatorLink?>'>indicators</a> to customize how the Greeks and IV are calculated for the contracts already in your universe.
   </p>
-  
-</div>
