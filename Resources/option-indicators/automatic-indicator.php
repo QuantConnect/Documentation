8b634b48b9c9360d2eca48b109501a5bc84d17ec
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-10 15:58:04 -0600

    Update automatic-indicator.php

--- Resources/option-indicators/automatic-indicator.php
+++ Resources/option-indicators/automatic-indicator.php
@@ -93,10 +93,12 @@
         # Group the contracts into call/put pairs.
         contracts_pair_sizes = chain.groupby(['expiry', 'strike']).count()['right']
         paired_contracts = contracts_pair_sizes[contracts_pair_sizes == 2].index
-        symbols = chain[
-            chain['expiry'].isin(paired_contracts.levels[0]) & 
-            chain['strike'].isin(paired_contracts.levels[1])
-        ].reset_index().groupby(['expiry', 'strike', 'right', 'symbol']).first().index.levels[-1]
+        symbols = [
+            idx[-1] for idx in chain[
+                chain['expiry'].isin(paired_contracts.levels[0]) & 
+                chain['strike'].isin(paired_contracts.levels[1])
+            ].reset_index().groupby(['expiry', 'strike', 'right', 'symbol']).first().index
+        ]
         pairs = [(symbols[i], symbols[i+1]) for i in range(0, len(symbols), 2)]
 
         for call, put in pairs:
