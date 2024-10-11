2d8f5644b236b66bdf09a264c9750dfcab606b5a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-11 13:24:24 -0600

    Update 10 Historical Data.php

--- 03 Writing Algorithms/12 Universes/04 Equity Options/10 Historical Data.php
+++ 03 Writing Algorithms/12 Universes/04 Equity Options/10 Historical Data.php
@@ -1,6 +1,7 @@
 <? 
 $assetClass = "an Equity Option";
 $addOptionC = "AddOption(\"SPY\");";
+$addOptionPy = "self.add_option('SPY')"
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
 include(DOCS_RESOURCES."/universes/option/historical-data.php"); 
 ?>
