67f00b81ff3ed3a522c0472b9a07f7adf452f17d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-11 13:47:20 -0600

    Update 09 Greeks and Implied Volatility.php

--- 03 Writing Algorithms/12 Universes/08 Index Options/09 Greeks and Implied Volatility.php
+++ 03 Writing Algorithms/12 Universes/08 Index Options/09 Greeks and Implied Volatility.php
@@ -1,1 +1,10 @@
-<? include(DOCS_RESOURCES."/universes/option/greeks-and-iv.php"); ?>
+<? 
+$addOptionPy = "self.add_index_option(\"SPX\")
+";
+$addOptionC = "AddIndexOption(\"SPX\");
+";
+$filterLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/requesting-data/universes#04-Filter-by-Implied-Volatility-and-Greeks";
+$indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/greeks-and-implied-volatility/indicators";
+$calculationMethod = "To calculate the values, we use <a href='/research/16977/greeks-and-iv-implementation/p1'>our implementation</a> of the Black-Scholes pricing model, which accounts for the interest rate and dividend payments when applicable. For example, VIX doesn't have dividends. We use SPY for SPX and QQQ for NDX.";
+include(DOCS_RESOURCES."/universes/option/greeks-and-iv.php"); 
+?>
