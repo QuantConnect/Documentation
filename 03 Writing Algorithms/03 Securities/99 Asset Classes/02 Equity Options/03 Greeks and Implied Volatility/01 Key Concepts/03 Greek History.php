8aceeabe5edc1e60fb7e41c6e8333279db4f5b47
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-11 10:57:08 -0600

    Update 03 Greek History.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/03 Greek History.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/03 Greek History.php
@@ -1,5 +1,6 @@
 <? 
 $addOptionC = "AddOption(\"SPY\");";
+$addOptionPy = "self.add_option('SPY')";
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
 include(DOCS_RESOURCES."/option-pricing/greek-history.php"); 
 ?>
