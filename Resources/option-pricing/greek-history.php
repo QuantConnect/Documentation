e397cfde38b28c2ce30bb09d9670e29214563260
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-11 10:53:03 -0600

    Update greek-history.php

--- Resources/option-pricing/greek-history.php
+++ Resources/option-pricing/greek-history.php
@@ -1,13 +1,9 @@
-<p class='python'><i>This feature is coming soon.</i></p>
-
-<div class='csharp'>
 <p>The Greeks and IV values that you get from a <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/historical-data/history-requests'>history request</a> of the Option universe are the daily, pre-calculated values based on the end of the previous trading day.</p>
   
 <? include(DOCS_RESOURCES."/option-pricing/greek-history-code.php"); ?>
   
-  <p>
+<p>
     You can't customize the Greeks and IV values that you get from a history request.
     However, you can create <a href='<?=$indicatorLink?>'>indicators</a> to customize how the Greeks and IV are calculated for the contracts already in your universe.
-  </p>
+</p>
 
-</div>
