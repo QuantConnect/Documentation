4ec25deb629c48f5bd64f393d53fc91602bac5b4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-11 13:32:06 -0600

    Update 10 Historical Data.php

--- 03 Writing Algorithms/12 Universes/08 Index Options/10 Historical Data.php
+++ 03 Writing Algorithms/12 Universes/08 Index Options/10 Historical Data.php
@@ -1,7 +1,7 @@
 <? 
 $assetClass = "an Index Option";
 $addOptionC = "AddIndexOption(\"SPX\");";
-$addOptionPy = "self.add_index_option('SPX);";
+$addOptionPy = "self.add_index_option('SPX');";
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/greeks-and-implied-volatility/indicators";
 include(DOCS_RESOURCES."/universes/option/historical-data.php"); 
 ?>
