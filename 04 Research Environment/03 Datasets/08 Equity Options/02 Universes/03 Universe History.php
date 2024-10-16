4d9df41aef71f3a1027874b9bc85136d6cae9507
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-16 16:31:13 -0600

    Update 03 Universe History.php

--- 04 Research Environment/03 Datasets/08 Equity Options/02 Universes/03 Universe History.php
+++ 04 Research Environment/03 Datasets/08 Equity Options/02 Universes/03 Universe History.php
@@ -1,6 +1,6 @@
 <? 
 $underlyingAssetClass = "Equity";
 $filterLink = "/docs/v2/writing-algorithms/universes/equity-options#05-Filter-by-Other-Contract-Properties";
-include(DOCS_RESOURCES."/universes/option/option-history-intro.html"); 
+include(DOCS_RESOURCES."/universes/option/option-history-intro.php"); 
 include(DOCS_RESOURCES."/universes/option/option-history-object.html"); 
 ?>
