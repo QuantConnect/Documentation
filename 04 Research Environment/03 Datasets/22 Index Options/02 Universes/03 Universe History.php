272457b0d4056c56b5a1cbac636b14f05ce6ed76
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-16 16:08:14 -0600

    Update 03 Universe History.php

--- 04 Research Environment/03 Datasets/22 Index Options/02 Universes/03 Universe History.php
+++ 04 Research Environment/03 Datasets/22 Index Options/02 Universes/03 Universe History.php
@@ -1,6 +1,6 @@
 <? 
 $underlyingAssetClass = "Index";
 $filterLink = "/docs/v2/writing-algorithms/universes/index-options#05-Filter-by-Other-Contract-Properties";
-include(DOCS_RESOURCES."/universes/option/option-history-intro.html"); 
+include(DOCS_RESOURCES."/universes/option/option-history-intro.php"); 
 include(DOCS_RESOURCES."/universes/option/option-history-object.html"); 
 ?>
