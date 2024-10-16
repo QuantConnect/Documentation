d697e50067c891f5b9edf8743cb2abde1c5428d3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-16 16:52:28 -0600

    Update 02 Create Subscriptions.php

--- 04 Research Environment/03 Datasets/14 Futures Options/03 Individual Contracts/02 Create Subscriptions.php
+++ 04 Research Environment/03 Datasets/14 Futures Options/03 Individual Contracts/02 Create Subscriptions.php
@@ -3,8 +3,7 @@
 <ol>
 <?
 $additionalImports = "using QuantConnect.Data.Market;
-using QuantConnect.Securities.Future;
-using QuantConnect.Securities.FutureOption;
+using QuantConnect.Securities;
 ";
 include(DOCS_RESOURCES."/datasets/research-environment/load-csharp-assemblies.php");
 ?>
