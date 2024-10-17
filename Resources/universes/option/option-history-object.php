0c1967bde158aad1819420c76ac96a88e520f8db
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-17 16:39:29 -0600

    Update option-history-object.php

--- Resources/universes/option/option-history-object.php
+++ Resources/universes/option/option-history-object.php
@@ -2,16 +2,16 @@
 <div class='python section-example-container'>
     <pre class='python'>option_history.data_frame</pre>
 </div>
-<img class='python' src='$<?=$dataFrameImg?>'>
+<img class='python' src='<?=$dataFrameImg?>' alt='DataFrame of Options data'>
 
 <p class='python'>To get the expiration dates of all the contracts in an <code>OptionHistory</code> object, call the <code class='csharp'>GetExpiryDates</code><code class='csharp'>get_expiry_dates</code> method.</p>
 <div class='python section-example-container'>
     <pre class='python'>option_history.get_expiry_dates()</pre>
 </div>
-<img class='python' src='$<?=$expiryDatesImg?>'>
+<img class='python' src='<?=$expiryDatesImg?>' alt='list of expiry dates'>
 
 <p class='python'>To get the strike prices of all the contracts in an <code>OptionHistory</code> object, call the <code class='csharp'>GetStrikes</code><code class='csharp'>get_strikes</code> method.</p>
 <div class='python section-example-container'>
     <pre class='python'>option_history.get_strikes()</pre>
 </div>
-<img class='python' src='$<?=$strikesImg?>'>
+<img class='python' src='<?=$strikesImg?>' alt='List of strike prices'>
