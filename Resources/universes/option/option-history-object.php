e68fdf7b5f288367e0912b36cbef667b99f29953
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-17 16:34:00 -0600

    Update and rename option-history-object.html to option-history-object.php

--- Resources/universes/option/option-history-object.php
+++ Resources/universes/option/option-history-object.php
@@ -2,11 +2,16 @@
 <div class='python section-example-container'>
     <pre class='python'>option_history.data_frame</pre>
 </div>
+<img class='python' src='$<?=$dataFrameImg?>'>
+
 <p class='python'>To get the expiration dates of all the contracts in an <code>OptionHistory</code> object, call the <code class='csharp'>GetExpiryDates</code><code class='csharp'>get_expiry_dates</code> method.</p>
 <div class='python section-example-container'>
     <pre class='python'>option_history.get_expiry_dates()</pre>
 </div>
+<img class='python' src='$<?=$expiryDatesImg?>'>
+
 <p class='python'>To get the strike prices of all the contracts in an <code>OptionHistory</code> object, call the <code class='csharp'>GetStrikes</code><code class='csharp'>get_strikes</code> method.</p>
 <div class='python section-example-container'>
     <pre class='python'>option_history.get_strikes()</pre>
 </div>
+<img class='python' src='$<?=$strikesImg?>'>
