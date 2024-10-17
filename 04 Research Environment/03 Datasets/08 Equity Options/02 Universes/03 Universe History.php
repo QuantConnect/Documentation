2f9ef2ab091072e2dc4531a58b27ada8cd324ba7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-17 16:34:47 -0600

    Update 03 Universe History.php

--- 04 Research Environment/03 Datasets/08 Equity Options/02 Universes/03 Universe History.php
+++ 04 Research Environment/03 Datasets/08 Equity Options/02 Universes/03 Universe History.php
@@ -2,5 +2,9 @@
 $underlyingAssetClass = "Equity";
 $filterLink = "/docs/v2/writing-algorithms/universes/equity-options#05-Filter-by-Other-Contract-Properties";
 include(DOCS_RESOURCES."/universes/option/option-history-intro.php"); 
-include(DOCS_RESOURCES."/universes/option/option-history-object.html"); 
+
+$dataFrameImg = "https://cdn.quantconnect.com/i/tu/equity-options-uiverse-history-data-frame-python.png";
+$expiryDatesImg = "https://cdn.quantconnect.com/i/tu/equity-options-uiverse-history-get-expiry-dates-python.png";
+$strikesImg = "https://cdn.quantconnect.com/i/tu/equity-options-uiverse-history-get-strikes-python.png";
+include(DOCS_RESOURCES."/universes/option/option-history-object.php"); 
 ?>
