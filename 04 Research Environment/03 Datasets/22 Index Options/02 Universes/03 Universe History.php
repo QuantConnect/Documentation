106f0ad9b626fbfcbf8e62fdcbd5714fb188d898
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-10-17 16:35:06 -0600

    Update 03 Universe History.php

--- 04 Research Environment/03 Datasets/22 Index Options/02 Universes/03 Universe History.php
+++ 04 Research Environment/03 Datasets/22 Index Options/02 Universes/03 Universe History.php
@@ -2,5 +2,9 @@
 $underlyingAssetClass = "Index";
 $filterLink = "/docs/v2/writing-algorithms/universes/index-options#05-Filter-by-Other-Contract-Properties";
 include(DOCS_RESOURCES."/universes/option/option-history-intro.php"); 
-include(DOCS_RESOURCES."/universes/option/option-history-object.html"); 
+
+$dataFrameImg = "https://cdn.quantconnect.com/i/tu/index-options-uiverse-history-data-frame-python.png";
+$expiryDatesImg = "https://cdn.quantconnect.com/i/tu/index-options-uiverse-history-get-expiry-dates-python.png";
+$strikesImg = "https://cdn.quantconnect.com/i/tu/index-options-uiverse-history-get-strikes-python.png";
+include(DOCS_RESOURCES."/universes/option/option-history-object.php"); 
 ?>
