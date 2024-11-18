d0c68ef5d2cf7c9e34f13609c2844873bca3be48
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-11-18 14:39:39 -0700

    Update 03 Leverage.php

--- 03 Writing Algorithms/12 Universes/02 Settings/03 Leverage.php
+++ 03 Writing Algorithms/12 Universes/02 Settings/03 Leverage.php
@@ -1,12 +1,12 @@
 <? include(DOCS_RESOURCES."/universes/settings/leverage.php"); ?> add the universe.</p> 
 
 <div class="section-example-container">
-    <pre class="csharp">// Assign 2x leverage for all securities in universe. 
+    <pre class="csharp">// Assign 2x leverage for all securities in the universe. 
 UniverseSettings.Leverage = 2.0m;
-// Add the 50 Equities to the universe that have the most dollar trading volume.
+// Add a universe of the 50 most liquid US Equities.
 AddUniverse(Universe.DollarVolume.Top(50));</pre>
-    <pre class="python"># Assign 2x leverage for all securities in universe.  
+    <pre class="python"># Assign 2x leverage for all securities in the universe.  
 self.universe_settings.leverage = 2.0
-# Add the 50 Equities to the universe that have the most dollar trading volume.
+# Add a universe of the 50 most liquid US Equities.
 self.add_universe(self.universe.dollar_volume.top(50))</pre>
 </div>
