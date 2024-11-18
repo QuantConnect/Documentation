2fa92523565bc3c33af4dbb7ab8e829aac1f7c2b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-11-18 15:48:04 -0700

    Update 03 Filter by Investment Strategy.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Future Options/01 Requesting Data/01 Universes/03 Filter by Investment Strategy.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Future Options/01 Requesting Data/01 Universes/03 Filter by Investment Strategy.php
@@ -1,4 +1,4 @@
-<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts of the same Future underlying with different features. The following table describes the filter methods of the <code>OptionFilterUniverse</code> class that select contract for Option strategies.</p>
+<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts of the same underlying Future with different features. The following table describes the filter methods of the <code>OptionFilterUniverse</code> class that select contract for Option strategies.</p>
 
 <?
   include(DOCS_RESOURCES."/universes/option/option-filter-universe-strategies.html"); 
