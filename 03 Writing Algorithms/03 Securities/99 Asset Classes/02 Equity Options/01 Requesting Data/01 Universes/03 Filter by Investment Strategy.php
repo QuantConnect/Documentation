aea9338db565e9896805746c2dc06428c395b839
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-11-18 15:48:22 -0700

    Update 03 Filter by Investment Strategy.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/01 Universes/03 Filter by Investment Strategy.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/01 Requesting Data/01 Universes/03 Filter by Investment Strategy.php
@@ -1,4 +1,4 @@
-<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts of the same Equity underlying with different features.</p>
+<p><a href="/docs/v2/writing-algorithms/trading-and-orders/option-strategies">Options trading strategies</a> consist of simultaneously buying or selling one or more Option contracts of the same underlying Equity with different features.</p>
 
 <?
   $assetClass = "Option";
