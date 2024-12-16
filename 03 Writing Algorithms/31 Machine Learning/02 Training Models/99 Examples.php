e75376e9ad49658297ed84a58d0be003a2aa661b
LouisSzeto <hke0073@icloud.com>
2024-12-16 21:23:01 +0800

    link training model example

---
+++ 03 Writing Algorithms/31 Machine Learning/02 Training Models/99 Examples.php
@@ -0,0 +1,24 @@
+<p>The following examples demonstrate some common practices for training the machine learning model.</p>
+
+<?
+$number = 1;
+include(DOCS_RESOURCES."/examples/sklearn-example.php"); 
+?>
+
+<h4>Other Examples</h4>
+<p>For more examples, see the following algorithms:</p>
+
+<div class="example-fieldset">
+    <div class="example-legend">Demonstration Algorithms</div>
+
+    <a class="python example-algorithm-link" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Python/TrainingExampleAlgorithm.py" target="_BLANK">
+        TrainingExampleAlgorithm.py
+        <span class="badge-python pull-right">Python</span>
+    </a>
+
+    <a class="csharp example-algorithm-link" href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.CSharp/TrainingExampleAlgorithm.cs" target="_BLANK">
+        TrainingExampleAlgorithm.cs
+        <span class="badge badge-sm badge-csharp pull-right">C#</span>
+    </a>
+
+</div>
