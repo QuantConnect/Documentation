b6613328bcc785d491588aaa82737269ded521af
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-17 13:48:32 -0500

    Update 06 Greeks and IV History.php

--- 04 Research Environment/03 Datasets/14 Futures Options/03 Individual Contracts/06 Greeks and IV History.php
+++ 04 Research Environment/03 Datasets/14 Futures Options/03 Individual Contracts/06 Greeks and IV History.php
@@ -4,5 +4,7 @@ $underlyingNameC = "futuresContractSymbol";
 
 $contractNamePy = "fop_contract_symbol";
 $underlyingNamePy = "futures_contract_symbol";
+
+$imgLinkPy = "https://cdn.quantconnect.com/i/tu/future-options-greeks-and-iv-history.png";
 include(DOCS_RESOURCES."/universes/option/greeks-and-iv-history.php"); 
 ?>
