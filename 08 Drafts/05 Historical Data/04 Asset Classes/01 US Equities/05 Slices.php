30989832a1641fe82925ffb926136487078783b0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-19 15:59:10 -0500

    Update 05 Slices.php

---
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/05 Slices.php
@@ -0,0 +1,12 @@
+<p>
+  To get historical <a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/timeslices'>Slice</a> data, call the <code class='csharp'>History</code><code class='python'>history</code> method without passing any <code>Symbol</code> objects.
+  This method returns <code>Slice</code> objects, which contain data points from all the datasets in your algorithm.
+  If you omit the <code>resolution</code> argument, it uses the resolution that you set for each security and dataset when you created the subscriptions.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Get the historical Slice objects over the last 5 days for all the subcriptions in your algorithm.
+var history = History(5, Resolution.Daily);</pre>
+    <pre class="python"># Get the historical Slice objects over the last 5 days for all the subcriptions in your algorithm.
+history = self.history(5, Resolution.DAILY)</pre>
+</div>
