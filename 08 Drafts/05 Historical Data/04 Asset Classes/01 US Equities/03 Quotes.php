64613f59fb217249bd75487296bcbccb1fcb17d7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-19 11:22:59 -0500

    Update 03 Quotes.php

---
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/03 Quotes.php
@@ -0,0 +1,32 @@
+<?
+$imgLink = "https://cdn.quantconnect.com/i/tu/history-quotebar-dataframe-us-equities.png";
+$resolutionLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#03-Resolutions";
+?>
+
+<p class='csharp'>
+  To get historical trade data, call the <code>History&lt;QuoteBar&gt;</code> method with an asset's <code>Symbol</code>.
+</p>
+
+<p class='python'>
+  To get historical trade data, call the <code>history</code> method with the <code>QuoteBar</code> type and an asset's <code>Symbol</code>.
+  This method returns a DataFrame with columns for the open, high, low, close, and size of the bid and ask quotes.
+  The columns that don't start with "bid" or "ask" are the mean of the quote prices on both sides of the market.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Get the 5 trailing daily QuoteBar objects of an asset. 
+var history = History&lt;TradeBar&gt;(symbol, 5, Resolution.Minute);</pre>
+    <pre class="python"># Get the 5 trailing daily QuoteBar objects of an asset in DataFrame format. 
+history = self.history(QuoteBar, symbol, 5, Resolution.MINUTE)</pre>
+</div>
+
+<img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of open, high, low, close, and volume for an asset.'>
+
+<p class='python'>To get a list of <code>QuoteBar</code> objects instead of a DataFrame, call the <code>history[QuoteBar]</code> method.</p>
+
+<div class="python section-example-container">
+    <pre class="python"># Get the 5 trailing daily TradeBar objects of an asset in TradeBar format. 
+history = self.history[QuoteBar](symbol, 5, Resolution.MINUTE)</pre>
+</div>
+
+<p>The resolution of data you request must support QuoteBar data. Otherwise, the history request won't return any data. To check the resolutions that support QuoteBar data, see <a href='<?=$resolutionLink?>'>Resolutions</a>.</p>
