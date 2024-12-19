a728dffe72497dc8cbf5e932979e4d0613b33cf7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-19 10:34:46 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/01 Introduction.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/01 Introduction.php
@@ -1,1 +1,5 @@
-<p>This page explains how to get historical data for US Equities.</p>
+<?
+$assetClass = "US Equities";
+?>
+
+<p>This page explains how to get historical data for {<?=$assetClass?>}.</p>
