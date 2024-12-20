059c7a9b6752919ecc35a23a26c4b4c7e7e06c34
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 15:43:28 -0500

    Update 08 Brokerage Limitations.php

--- 08 Drafts/05 Historical Data/09 Common Errors/08 Brokerage Limitations.php
+++ 08 Drafts/05 Historical Data/09 Common Errors/08 Brokerage Limitations.php
@@ -1,2 +1,6 @@
-
-E.g. IB is slow and leads to timeout if we request or warm up too many symbols (e.g. options)
+<p>
+  LEAN supports several brokerages that can provide historical data.
+  However, some of them have limitations on historical data.
+  For example, <a href='/docs/v2/cloud-platform/datasets/interactive-brokers'>Interactive Brokers</a> is slow and can timeout if you request historical data for too many assets at once.
+  For more information about these brokerages and the data they provide, see <a href='/docs/v2/cloud-platform/datasets'>Datasets</a>.
+</p>
