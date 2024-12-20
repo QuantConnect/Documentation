ababafbd2549f17295609a267f328fb0af34c7d4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 10:45:07 -0500

    Update 02 DataFrames.php

--- 08 Drafts/05 Historical Data/03 History Responses/02 DataFrames.php
+++ 08 Drafts/05 Historical Data/03 History Responses/02 DataFrames.php
@@ -1,1 +1,12 @@
-<p class='python'>The most popular return type is a <code>DataFrame</code>. If you request a <code>DataFrame</code>, LEAN unpacks the data from <code>Slice</code> objects to populate the <code>DataFrame</code>. If you intend to use the data in the <code>DataFrame</code> to create <code>TradeBar</code> or <code>QuoteBar</code> objects, request that the history request returns the data type you need. Otherwise, LEAN will waste computational resources populating the <code>DataFrame</code>.</p>
+<p class='csharp'>To get a DataFrame from a history request, use Python.</p>
+
+<p class='python'>
+  The most popular return type is a <code>DataFrame</code>. 
+  If you request a <code>DataFrame</code>, LEAN unpacks the data from <code>Slice</code> objects to populate the <code>DataFrame</code>. 
+  If you intend to use the data in the <code>DataFrame</code> to create <code>TradeBar</code> or <code>QuoteBar</code> objects, request that the history request returns the data type you need. 
+  Otherwise, LEAN will waste computational resources populating the <code>DataFrame</code>.
+</p>
+
+<p class='python'>
+  When your history request returns a <code>DataFrame</code>, the timestamps in the <code>DataFrame</code> are based on the <a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/time-zones#05-Data-Time-Zone'>data time zone</a>.
+</p>
