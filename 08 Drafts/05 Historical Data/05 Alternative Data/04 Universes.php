a779e73f5353ffd08cc69823bb26fb442069269b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 18:09:58 -0500

    Update 04 Universes.php

---
+++ 08 Drafts/05 Historical Data/05 Alternative Data/04 Universes.php
@@ -0,0 +1,30 @@
+<?
+$imgLink = "";
+?>
+
+<p class='csharp'>
+  To get historical <a href='/docs/v2/writing-algorithms/universes/equity/alternative-data-universes'>universe data for an alternative dataset</a>, call the <code>History</code> method with the <code>Universe</code> object.
+</p>
+
+<p class='python'>
+  To get historical <a href='/docs/v2/writing-algorithms/universes/equity/alternative-data-universes'>universe data for an alternative dataset</a>, call the <code>history</code> method with the <code>Universe</code> object.
+  Set the <code>flatten</code> argument to <code>True</code> to get a DataFrame that has columns for the data point attributes.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Get the historical universe data over the last 30 days.
+var history = History(universe, TimeSpan.FromDays(30));</pre>
+    <pre class="python"># Get the historical universe data over the last 30 days in DataFrame format.
+history = self.history(universe, timedelta(30), flatten=True)</pre>
+</div>
+
+<b>TODO:</b>
+<img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of universe data for an asset.'>
+
+<p class='python'>To get the data in the format of the objects that you receive in your universe filter function instead of a DataFrame, use <code>flatten=False</code>.</p>
+
+<div class="python section-example-container">
+    <pre class="python"># Get the historical universe data over the last 30 days in a Series where
+# the values in the series are lists of the universe selection objects.
+history = self.history(universe, timedelta(30), flatten=False)</pre>
+</div>
