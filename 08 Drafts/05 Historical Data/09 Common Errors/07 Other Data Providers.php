e7e5d00dc8fa4e8ce0f208914d2de6470a04727b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 15:35:41 -0500

    Update and rename 07 Other Data Providers.php to 07 Other Data Provider Limitations.php

--- 08 Drafts/05 Historical Data/09 Common Errors/07 Other Data Providers.php
+++
@@ -1,4 +0,0 @@
-<p>
-  LEAN supports other data providers, include IQFeed, Polygon, and IEX Cloud.
-  For more information about these other data providers, see the documentation in <a href='/docs/v2/cloud-platform/datasets'>Cloud Platform</a>, <a href='/docs/v2/local-platform/datasets/getting-started'>Local Platform</a>, or the <a href='/docs/v2/lean-cli/live-trading/data-providers'>CLI</a>.
-</p>
