ec2b619a706ed57dc3cde8ff6c2a5d1e9545a96b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 13:50:10 -0500

    Update 03 Trading During Warm Up.php

--- 08 Drafts/05 Historical Data/09 Common Errors/03 Trading During Warm Up.php
+++ 08 Drafts/05 Historical Data/09 Common Errors/03 Trading During Warm Up.php
@@ -1,1 +1,6 @@
+<p>
+  You can't place trades during the warm-up period because they would be operating on fictional fast-forwarded data.
+  If you try to place a trade during warm-up, LEAN throws the following error:
+</p>
 
+<div class='error-messages'>Backtest Handled Error: This operation is not allowed in Initialize or during warm up: OrderRequest.Submit. Please move this code to the OnWarmupFinished() method.</div></p>
