ecb773c340c030a3d8e38e364b6829759787d1af
LouisSzeto <hke0073@icloud.com>
2024-12-20 11:04:49 +0800

    address peer review

--- 03 Writing Algorithms/20 Historical Data/11 Rolling Window/12 Examples.php
+++ 03 Writing Algorithms/20 Historical Data/11 Rolling Window/12 Examples.php
@@ -1,143 +1,7 @@
 <p>The following examples demonstrate some common practices for implementing rolling windows.</p>
 
 <h4>Example 1: Price Actions</h4>
-<p>Using <code>RollingWindow</code> to cache the last 3 trade bars, the following algorithm could identify volume contraction breakout price action pattern and buy SPY accordingly to ride on the capital inflow. We take 2% profit and stop loss at 1%.</p>
-<div class="section-example-container">
-    <pre class="csharp">public class RollingWindowAlgorithm : QCAlgorithm
-{
-    private Symbol _spy;
-    // Set up a rolling window to hold the last 3 trade bars for price action detection as trade signal.
-    private RollingWindow&lt;TradeBar&gt; _windows = new(3);
-
-    public override void Initialize()
-    {
-        SetStartDate(2021, 1, 1);
-        SetEndDate(2022, 1, 1);
-
-        // Request SPY data for signal generation &amp;&amp; trading.
-        _spy = AddEquity("SPY", Resolution.Minute).Symbol;
-
-        // Warm up for the rolling window.
-        var history = History&lt;TradeBar&gt;(_spy, 3, Resolution.Minute);
-        foreach (var bar in history)
-        {
-            _windows.Add(bar);
-        }
-    }
-    
-    public override void OnData(Slice slice)
-    {
-        if (slice.Bars.TryGetValue(_spy, out var bar))
-        {
-            // Trade the price action if the previous bars fulfill a contraction breakout.
-            if (ContractionAction() &amp;&amp; BreakoutAction(bar.Close))
-            {
-                SetHoldings(_spy, 0.5m);
-            }
-
-            // Update the window with the current bar.
-            _windows.Add(bar);
-        }
-    }
-
-    private bool ContractionAction()
-    {
-        // We trade contraction type price action, where the buying preesure is increasing.
-        // 1. The last 3 bars are green.
-        // 2. The price is increasing in trend.
-        // 3. The trading Volume is increasing as well.
-        // 4. The range of the bars are decreasing.
-        return _windows[2].Close &gt; _windows[2].Open &amp;&amp;
-            _windows[1].Close &gt; _windows[1].Open &amp;&amp;
-            _windows[0].Close &gt; _windows[0].Open &amp;&amp;
-            _windows[0].Close &gt; _windows[1].Close &amp;&amp; _windows[1].Close &gt; _windows[2].Close &amp;&amp;
-            _windows[0].Volume &gt; _windows[1].Volume &amp;&amp; _windows[1].Volume &gt; _windows[2].Volume &amp;&amp;
-            _windows[2].Close - _windows[2].Open &gt; _windows[1].Close - _windows[1].Open &amp;&amp;
-            _windows[1].Close - _windows[1].Open &gt; _windows[0].Close - _windows[0].Open;
-    }
-
-    private bool BreakoutAction(decimal currentPrice)
-    {
-        // We trade breakout from contraction: the breakout should be much above the contracted range of the last bar.
-        return currentPrice - _windows[0].Close &gt; (_windows[0].Close - _windows[0].Open) * 2m;
-    }
-
-    public override void OnOrderEvent(OrderEvent orderEvent)
-    {
-        if (orderEvent.Status == OrderStatus.Filled)
-        {
-            if (orderEvent.Ticket.OrderType == OrderType.Market)
-            {
-                // Stop loss order at 1%.
-                var stopPrice = orderEvent.FillQuantity &gt; 0m ? orderEvent.FillPrice * 0.99m : orderEvent.FillPrice * 1.01m;
-                StopMarketOrder(_spy, -Portfolio[_spy].Quantity, stopPrice);
-                // Take profit order at 2%.
-                var takeProfitPrice = orderEvent.FillQuantity &gt; 0m ? orderEvent.FillPrice * 1.02m : orderEvent.FillPrice * 0.98m;
-                LimitOrder(_spy, -Portfolio[_spy].Quantity, takeProfitPrice);
-            }
-            else if (orderEvent.Ticket.OrderType == OrderType.StopMarket || orderEvent.Ticket.OrderType == OrderType.Limit)
-            {
-                // Cancel any open order if stop loss or take profit order filled.
-                Transactions.CancelOpenOrders();
-            }
-        }
-    }
-}</pre>
-    <pre class="python">class RollingWindowAlgorithm(QCAlgorithm):
-    def initialize(self) -&gt; None:
-        self.set_start_date(2021, 1, 1)
-        self.set_end_date(2022, 1, 1)
-        
-        # Request SPY data for signal generation and trading.
-        self.spy = self.add_equity("SPY", Resolution.MINUTE).symbol
-
-        # Set up a rolling window to hold the last 3 trade bars for price action detection as trade signal.
-        self.windows = RollingWindow[TradeBar](3)
-        # Warm up for the rolling window.
-        history = self.history[TradeBar](self.spy, 3, Resolution.MINUTE)
-        for bar in history:
-            self.windows.add(bar)
-
-    def on_data(self, slice: Slice) -&gt; None:
-        bar = slice.bars.get(self.spy)
-        if bar:
-            # Trade the price action if the previous bars fulfill a contraction breakout.
-            if self.contraction_action and self.breakout(bar.close):
-                self.set_holdings(self.spy, 0.5)
-
-            # update the window with the current bar.
-            self.windows.add(bar)
-    
-    def contraction_action(self) -&gt; None:
-        # We trade contraction type price action, where the buying preesure is increasing.
-        # 1. The last 3 bars are green.
-        # 2. The price is increasing in trend.
-        # 3. The trading volume is increasing as well.
-        # 4. The range of the bars are decreasing.
-        return self.windows[2].close &gt; self.windows[2].open and \
-            self.windows[1].close &gt; self.windows[1].open and \
-            self.windows[0].close &gt; self.windows[0].open and \
-            self.windows[0].close &gt; self.windows[1].close &gt; self.windows[2].close and \
-            self.windows[0].volume &gt; self.windows[1].volume &gt; self.windows[2].volume and \
-            self.windows[2].close - self.windows[2].open &gt; self.windows[1].close - self.windows[1].open &gt; self.windows[0].close - self.windows[0].open
-    
-    def breakout(self, current_close: float) -&gt; None:
-        # We trade breakout from contraction: the breakout should be much above the contracted range of the last bar.
-        return current_close - self.windows[0].close &gt; (self.windows[0].close - self.windows[0].open) * 2
-
-    def on_order_event(self, order_event: OrderEvent) -&gt; None:
-        if order_event.status == OrderStatus.FILLED:
-            if order_event.ticket.order_type == OrderType.MARKET:
-                # Stop loss order at 1%.
-                stop_price = order_event.fill_price * (0.99 if order_event.fill_quantity &gt; 0 else 1.01)
-                self.stop_market_order(self.spy, -self.portfolio[self.spy].quantity, stop_price)
-                # Take profit order at 2%.
-                take_profit_price = order_event.fill_price * (1.02 if order_event.fill_quantity &gt; 0 else 0.98)
-                self.limit_order(self.spy, -self.portfolio[self.spy].quantity, take_profit_price)
-            elif order_event.ticket.order_type == OrderType.STOP_MARKET or order_event.ticket.order_type == OrderType.LIMIT:
-                # Cancel any open order if stop loss or take profit order filled.
-                self.transactions.cancel_open_orders()</pre>
-</div>
+<? include(DOCS_RESOURCES."/examples/take-profit-stop-loss-example.html"); ?>
 
 <h4>Example 2: Bid-Ask Spread</h4>
 <p>The following algorithm trades the microeconomy of SPY's supply-demand relationship. We buy SPY if the current bid-ask spread is less than average of that of the last 20 quote bars, indicating demand is approaching supply, while short SPY vice versa. To save the last 20 quote data, we use a rolling window and save the calculated spread.</p>
@@ -215,8 +79,8 @@
                 self.set_holdings(self.spy, 0.5)</pre>
 </div>
 
-<h4>Example 3: Bid-Ask Spread</h4>
-<p>The following algorithm trades the microeconomy of SPY's supply-demand relationship. We buy SPY if the current bid-ask spread is less than average of that of the last 20 quote bars, indicating demand is approaching supply, while short SPY vice versa. To save the last 20 quote data, we use a rolling window and save the calculated spread.</p>
+<h4>Example 3: Trend Acceleration</h4>
+<p>The following algorithm trades the second derivative of EMA indicated trend, more specially, the difference between lagged EMA values. If the daily EMA difference is increasing while being lower than the current price, we estimate the uptrend is accelerating, hence buying SPY. Otherwise, if the price is below the EMA while the daily EMA difference is decelerating, we short SPY.</p>
 <div class="section-example-container">
     <pre class="csharp">public class RollingWindowAlgorithm : QCAlgorithm
 {
