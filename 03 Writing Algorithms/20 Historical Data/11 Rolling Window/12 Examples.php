26d1f88a966a8d6f9973eca182db3437d9489849
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-12-20 22:14:07 +0000

    Update 12 Examples.php

--- 03 Writing Algorithms/20 Historical Data/11 Rolling Window/12 Examples.php
+++ 03 Writing Algorithms/20 Historical Data/11 Rolling Window/12 Examples.php
@@ -4,7 +4,7 @@
 <? include(DOCS_RESOURCES."/examples/take-profit-stop-loss-example.html"); ?>
 
 <h4>Example 2: Bid-Ask Spread</h4>
-<p>The following algorithm trades the microeconomy of SPY's supply-demand relationship. We buy SPY if the current bid-ask spread is less than average of that of the last 20 quote bars, indicating demand is approaching supply, while short SPY vice versa. To save the last 20 quote data, we use a rolling window and save the calculated spread.</p>
+<p>The following algorithm trades the microeconomy of SPY's supply-demand relationship. We buy SPY if the current bid-ask spread is less than the average of the last 20 quote bars, indicating demand is approaching supply during short SPY and vice versa. We use a rolling window to save the last 20 quote data and save the calculated spread.</p>
 <div class="section-example-container">
     <pre class="csharp">public class RollingWindowAlgorithm : QCAlgorithm
 {
@@ -36,7 +36,7 @@
             var spread = bar.Ask.Close - bar.Bid.Close;
             _windows.Add(spread);
 
-            // Buy if the current spread is smaller than average, indicating demand is approaching supply. Buy force will drive up price.
+            // Buy if the current spread is smaller than average, indicating demand is approaching supply. Buy force will drive up prices.
             if (spread &lt; _windows.Average())
             {
                 SetHoldings(_spy, -0.5m);
@@ -71,7 +71,7 @@
             spread = bar.ask.close - bar.bid.close
             self.windows.add(spread)
 
-            # Buy if the current spread is smaller than average, indicating demand is approaching supply. Buy force will drive up price.
+            # Buy if the current spread is smaller than average, indicating demand is approaching supply. Buy force will drive up prices.
             if spread &lt; np.mean(list(self.windows)):
                 self.set_holdings(self.spy, -0.5)
             # Short if the current spread is larger than average, indicating supply is gradually overwhelming demand.
@@ -80,7 +80,7 @@
 </div>
 
 <h4>Example 3: Trend Acceleration</h4>
-<p>The following algorithm trades the second derivative of EMA indicated trend, more specially, the difference between lagged EMA values. If the daily EMA difference is increasing while being lower than the current price, we estimate the uptrend is accelerating, hence buying SPY. Otherwise, if the price is below the EMA while the daily EMA difference is decelerating, we short SPY.</p>
+<p>The following algorithm trades the second derivative of EMA indicated trend, especially the difference between lagged EMA values. If the daily EMA difference increases while being lower than the current price, we estimate the uptrend is accelerating, hence buying SPY. Otherwise, if the price is below the EMA while the daily EMA difference is decelerating, we short SPY.</p>
 <div class="section-example-container">
     <pre class="csharp">public class RollingWindowAlgorithm : QCAlgorithm
 {
@@ -95,12 +95,12 @@
         // Request SPY data for trading.
         _spy = AddEquity("SPY", Resolution.Minute).Symbol;
         
-        // Set up an automatical EMA indicator for trade signal generation.
+        // Set up an automatic EMA indicator for trade signal generation.
         _ema = EMA(_spy, 20, Resolution.Daily);
-        // Making use of the rolling window in the EMA indicator, we adjust it size so it can compare with previous data points.
+        //Using the rolling window in the EMA indicator, we adjust its size so it can compare with previous data points.
         _ema.Window.Size = 3;
 
-        // Schedule an event to rebalance SPY position at daily market open.
+        // Schedule an event to rebalance SPY position at the daily market open.
         Schedule.On(
             DateRules.EveryDay(_spy),
             TimeRules.AfterMarketOpen(_spy, 0),
@@ -114,17 +114,17 @@
     {
         if (_ema.Window.IsReady)
         {
-            // Buy if the current EMA is increasing with acceleration, indicating strong up trend.
+            // Buy if the current EMA increases with acceleration, indicating a strong up trend.
             if (_ema.Window[1] &lt; _ema &amp;&amp; _ema.Window[0] - _ema.Window[1] &gt; _ema.Window[1] - _ema.Window[2])
             {
                 SetHoldings(_spy, 0.5m);
             }
-            // Short if the current EMA is decreasing with acceleration, indicating strong down trend.
+            // Short if the current EMA decreases with acceleration, indicating a strong downtrend.
             else if (_ema.Window[1] &gt; _ema &amp;&amp; _ema.Window[0] - _ema.Window[1] &lt; _ema.Window[1] - _ema.Window[2])
             {
                 SetHoldings(_spy, 0.5m);
             }
-            // Liquidate if no strong trend indicated.
+            // Liquidate if no strong trend is indicated.
             else if (Portfolio.Invested)
             {
                 Liquidate(_spy);
@@ -140,12 +140,12 @@
         # Request SPY data for trading.
         self.spy = self.add_equity("SPY", Resolution.MINUTE).symbol
 
-        # Set up an automatical EMA indicator for trade signal generation.
+        # Set up an automatic EMA indicator for trade signal generation.
         self._ema = self.ema(self.spy, 20, Resolution.DAILY)
-        # Making use of the rolling window in the EMA indicator, we adjust it size so it can compare with previous data points.
+        # Using the rolling window in the EMA indicator, we adjust its size so it can compare with previous data points.
         self._ema.window.size = 3
 
-        # Schedule an event to rebalance SPY position at daily market open.
+        # Schedule an event to rebalance SPY position at the daily market open.
         self.schedule.on(
             self.date_rules.every_day(self.spy),
             self.time_rules.after_market_open(self.spy, 0),
@@ -156,15 +156,15 @@
 
     def rebalance(self) -&gt; None:
         if self._ema.window.is_ready:
-            # Buy if the current EMA is increasing with acceleration, indicating strong up trend.
+            # Buy if the current EMA increases with acceleration, indicating a strong up trend.
             if self._ema.window[1].value &lt; self._ema.current.value and \
             self._ema.window[0].value - self._ema.window[1].value &gt; self._ema.window[1].value - self._ema.window[2].value:
                 self.set_holdings(self.spy, 0.5)
-            # Short if the current EMA is decreasing with acceleration, indicating strong down trend.
+            # Short if the current EMA decreases with acceleration, indicating a strong downtrend.
             elif self._ema.window[1].value &gt; self._ema.current.value and \
             self._ema.window[0].value - self._ema.window[1].value &lt; self._ema.window[1].value - self._ema.window[2].value:
                 self.set_holdings(self.spy, -0.5)
-            # Liquidate if no strong trend indicated.
+            # Liquidate if no strong trend is indicated.
             elif self.portfolio.invested:
                 self.liquidate(self.spy)</pre>
 </div>
