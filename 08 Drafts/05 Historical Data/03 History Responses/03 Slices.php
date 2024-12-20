09c92262954f66c6c23f7c3141496f2b3397e27a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 12:36:22 -0500

    Update 03 Slices.php

--- 08 Drafts/05 Historical Data/03 History Responses/03 Slices.php
+++ 08 Drafts/05 Historical Data/03 History Responses/03 Slices.php
@@ -1,8 +1,29 @@
-<p>If you don't provide <code>Symbol</code> objects when you make a history request, you get <code>Slice</code> objects that contain <?=!$researchEnvironment ? "the entire universe" : "all of the assets you created subscriptions for in the notebook" ?>.</p>
+<p class='csharp'>
+  To request <code>Slice</code> objects of historical data, call the <code>History</code> method.
+  If you pass a list of <code>Symbol</code> objects, it returns data for all the securities and datasets that the <code>Symbol</code> objects reference.
+</p>
 
+<div class="csharp section-example-container">
+      <pre class="csharp">// Get the latest 3 data points of some securities/datasets, packaged into Slice objects.
+var history = History(symbols, 3);</pre>
+</div>
+
+<p class='csharp'>If you don't pass any <code>Symbol</code> objects, you get data for all the data subscriptions in your <?=$writingEnvironment ? "algorithm" : "notebook" ?>.</p>
+
+<p class='python'>
+  To request <code>Slice</code> objects of historical data, call the <code>history</code> method without providing any <code>Symbol</code> objects.
+  It returns data for all the data subscriptions in your <?=$writingEnvironment ? "algorithm" : "notebook" ?>.
+</p>
+
+<div class="section-example-container">
+      <pre class="csharp">// Get the latest 3 data points of all the securities/datasets in the <?=$writingEnvironment ? "algorithm" : "notebook" ?>, packaged into Slice objects.
+history = self.history(symbols, 3)</pre>
+      <pre class="python"># Get the latest 3 data points of all the securities/datasets in the <?=$writingEnvironment ? "algorithm" : "notebook" ?>, packaged into Slice objects.
+history = self.history(3)</pre>
+</div>
 
 <p>
-  When your history request returns <code>Slice</code> objects, the <code class="csharp">Time</code><code class="python">time</code> properties of these objects are based on the <?=$writingAlgorithms ? "algorithm" : "notebook" ?> time zone, but the <code class="csharp">EndTime</code><code class="python">end_time</code> properties of the individual <code>TradeBar</code>, <code>QuoteBar</code>, and <code>Tick</code> objects are based on the <span class='python'>data time zone</span><span class='csharp'><a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/time-zones#05-Data-Time-Zone'>data time zone</a></span>. 
+  When your history request returns <code>Slice</code> objects, the <code class="csharp">Time</code><code class="python">time</code> properties of these objects are based on the <?=$writingAlgorithms ? "algorithm" : "notebook" ?> time zone, but the <code class="csharp">EndTime</code><code class="python">end_time</code> properties of the individual data objects (for example, <code>TradeBar</code>, <code>QuoteBar</code>, and <code>Tick</code> objects) are based on the <span class='python'>data time zone</span><span class='csharp'><a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/time-zones#05-Data-Time-Zone'>data time zone</a></span>. 
   The <code class="csharp">EndTime</code><code class="python">end_time</code> is the end of the sampling period and when the data is actually available. 
   For daily US Equity data, this results in data points appearing on Saturday and skipping Monday.
 </p>
