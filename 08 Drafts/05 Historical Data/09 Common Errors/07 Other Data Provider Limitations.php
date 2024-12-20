0f47be8a2128a5f1f77ecf0285d11d83dac48e07
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 15:36:59 -0500

    Update 07 Other Data Provider Limitations.php

--- 08 Drafts/05 Historical Data/09 Common Errors/07 Other Data Provider Limitations.php
+++ 08 Drafts/05 Historical Data/09 Common Errors/07 Other Data Provider Limitations.php
@@ -1,4 +1,4 @@
 <p>
   LEAN supports other data providers, include IQFeed, Polygon, and IEX Cloud.
-  For more information about the limitations of these other data providers, see the documentation in <a href='/docs/v2/cloud-platform/datasets'>Cloud Platform</a>, <a href='/docs/v2/local-platform/datasets/getting-started'>Local Platform</a>, or the <a href='/docs/v2/lean-cli/live-trading/data-providers'>CLI</a>.
+  For more information about the limitations of these other data providers, see their respective documentation in <a href='/docs/v2/cloud-platform/datasets'>Cloud Platform</a>, <a href='/docs/v2/local-platform/datasets/getting-started'>Local Platform</a>, or the <a href='/docs/v2/lean-cli/live-trading/data-providers'>CLI</a>.
 </p>
