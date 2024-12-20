f4872c16a4a2a05a8183fe8bc89c41bc7bbfc315
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 16:01:37 -0500

    Update 02 Slices.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/02 Slices.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/02 Slices.php
@@ -25,5 +25,4 @@ history = self.history(3)</pre>
 <p>
   When your history request returns <code>Slice</code> objects, the <code class="csharp">Time</code><code class="python">time</code> properties of these objects are based on the <?=$writingAlgorithms ? "algorithm" : "notebook" ?> time zone, but the <code class="csharp">EndTime</code><code class="python">end_time</code> properties of the individual data objects are based on the <span class='python'>data time zone</span><span class='csharp'><a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/time-zones#05-Data-Time-Zone'>data time zone</a></span>. 
   The <code class="csharp">EndTime</code><code class="python">end_time</code> is the end of the sampling period and when the data is actually available. 
-  For daily datasets linked to US Equity assets, this results in data points appearing on Saturday and skipping Monday.
 </p>
