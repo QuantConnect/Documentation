6c4a14cab1fc8991523b1a3ca7478f98705282dd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 18:26:30 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/06 Custom Data/01 Introduction.php
+++ 08 Drafts/05 Historical Data/06 Custom Data/01 Introduction.php
@@ -1,1 +1,4 @@
-<p>This page explains how to get historical data for <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts'>custom datasets</a>.</p>
+<p>
+  This page explains how to get historical data for <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts'>custom datasets</a>.
+  Before you can get historical data for the dataset, define the <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts#04-Set-Data-Sources'><code class='python'>get_source</code><code class='csharp'>GetSource</code></a> and <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts#05-Parse-Custom-Data'><code class='python'>reader</code><code class='csharp'>Reader</code> methods of the custom data class.</a>
+</p>
