34ad5521335025076200078c9a182823332c1bdc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-20 15:19:25 -0500

    Update 06 QC Data Provider Limitations.php

--- 08 Drafts/05 Historical Data/09 Common Errors/06 QC Data Provider Limitations.php
+++ 08 Drafts/05 Historical Data/09 Common Errors/06 QC Data Provider Limitations.php
@@ -1,1 +1,4 @@
-
+<p>
+  In live trading, history requests for Index or Options with the QuantConnect data provider only returns data up to the end of the previous day. 
+  If you make a history request for these asset classes at 1 PM Eastern Time (ET), you won't get any data for the current day.
+</p>
