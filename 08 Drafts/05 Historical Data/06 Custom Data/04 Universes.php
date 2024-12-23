dd45d1dc777f7746c503e56c015d3b79a6153d0a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-23 11:34:45 -0500

    Update 04 Universes.php

--- 08 Drafts/05 Historical Data/06 Custom Data/04 Universes.php
+++ 08 Drafts/05 Historical Data/06 Custom Data/04 Universes.php
@@ -20,12 +20,4 @@ history = qb.history(universe, timedelta(5))</pre>
 
 <img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of universe data for a custom dataset.'>
 
-<!-- 
-<p class='python'>To get the data in the format of the objects that you receive in your universe filter function instead of a DataFrame, use <code>flatten=False</code>.</p>
 
-<div class="python section-example-container">
-    <pre class="python"># Get the historical universe data over the last 30 days in a Series where
-# the values in the series are lists of the universe selection objects.
-history = self.history(universe, timedelta(30), flatten=False)</pre>
-</div>
--->
