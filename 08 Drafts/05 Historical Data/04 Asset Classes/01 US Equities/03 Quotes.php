37120dfba3295fc4a39ed7ab3b6003d517a2d937
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-26 10:38:09 -0500

    Update 03 Quotes.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/03 Quotes.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/03 Quotes.php
@@ -3,14 +3,15 @@ $assetClass = "US Equities";
 $imgLink = "https://cdn.quantconnect.com/i/tu/history-quotebar-dataframe-us-equities.png";
 $resolutionLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#03-Resolutions";
 $dataTypeLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/handling-data#04-Quotes";
+$dataType = "QuoteBar";
 ?>
 
 <p class='csharp'>
-  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>History&lt;QuoteBar&gt;</code> method with an asset's <code>Symbol</code>.
+  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>History&lt;<?=$dataType?>&gt;</code> method with an asset's <code>Symbol</code>.
 </p>
 
 <p class='python'>
-  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>history</code> method with the <code>QuoteBar</code> type and an asset's <code>Symbol</code>.
+  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>history</code> method with the <code><?=$dataType?></code> type and an asset's <code>Symbol</code>.
   This method returns a DataFrame with columns for the open, high, low, close, and size of the bid and ask quotes.
   The columns that don't start with "bid" or "ask" are the mean of the quote prices on both sides of the market.
 </p>
@@ -18,21 +19,26 @@ $dataTypeLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/
 <div class="section-example-container">
     <pre class="csharp">// Get the Symbol of an asset.
 var symbol = AddEquity("SPY").Symbol;
-// Get the 5 trailing minute QuoteBar objects of the asset. 
-var history = History&lt;QuoteBar&gt;(symbol, 5, Resolution.Minute);</pre>
+// Get the 5 trailing minute <?=$dataType?> objects of the asset. 
+var history = History&lt;<?=$dataType?>&gt;(symbol, 5, Resolution.Minute);</pre>
     <pre class="python"># Get the Symbol of an asset.
 symbol = self.add_equity('SPY').symbol
-# Get the 5 trailing minute QuoteBar objects of the asset in DataFrame format. 
-history = self.history(QuoteBar, symbol, 5, Resolution.MINUTE)</pre>
+# Get the 5 trailing minute <?=$dataType?> objects of the asset in DataFrame format. 
+history = self.history(<?=$dataType?>, symbol, 5, Resolution.MINUTE)</pre>
 </div>
 
 <img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of historical quotes for an asset.'>
 
-<p class='python'>To get a list of <code>QuoteBar</code> objects instead of a DataFrame, call the <code>history[QuoteBar]</code> method.</p>
+<p class='python'>
+  If you request a DataFrame, LEAN unpacks the data from <code>Slice</code> objects to populate the DataFrame. 
+  If you intend to use the data in the DataFrame to create <code><?=$dataType?></code> objects, request that the history request returns the data type you need. 
+  Otherwise, LEAN will consume computational resources populating the DataFrame.  
+  To get a list of <code>history[<?=$dataType?>]</code> objects instead of a DataFrame, call the <code>history[<?=$dataType?>]</code> method.
+</p>
 
 <div class="python section-example-container">
-    <pre class="python"># Get the 5 trailing minute QuoteBar objects of an asset in QuoteBar format. 
-history = self.history[QuoteBar](symbol, 5, Resolution.MINUTE)</pre>
+    <pre class="python"># Get the 5 trailing minute <?=$dataType?> objects of an asset in <?=$dataType?> format. 
+history = self.history[<?=$dataType?>](symbol, 5, Resolution.MINUTE)</pre>
 </div>
 
 <p>The resolution of data you request must support <code>QuoteBar</code> data. Otherwise, the history request won't return any data. To check which resolutions for <?=$assetClass?> support <code>QuoteBar</code> data, see <a href='<?=$resolutionLink?>'>Resolutions</a>.</p>
