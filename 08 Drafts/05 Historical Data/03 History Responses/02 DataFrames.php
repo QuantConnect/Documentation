8c6616c5ac822d0826787db27db1280e2f29ef22
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-26 16:40:07 -0500

    Update 02 DataFrames.php

--- 08 Drafts/05 Historical Data/03 History Responses/02 DataFrames.php
+++ 08 Drafts/05 Historical Data/03 History Responses/02 DataFrames.php
@@ -3,7 +3,7 @@
 <div class='python'>
   <p>
     The most popular return type is a DataFrame. 
-    To request a DataFrame of historical data, call the <code>history</code> method with atleast one <code>Symbol</code> object.
+    To request a DataFrame of historical data, call the <code>history</code> method with at least one <code>Symbol</code> object.
   </p>
   
   <div class="section-example-container">
