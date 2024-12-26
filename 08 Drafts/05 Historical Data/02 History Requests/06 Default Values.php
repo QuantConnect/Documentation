8de072ae1b3e24b939b4ae0565d9aa3d95ca225b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-26 15:58:58 -0500

    Update 06 Default Values.php

--- 08 Drafts/05 Historical Data/02 History Requests/06 Default Values.php
+++ 08 Drafts/05 Historical Data/02 History Requests/06 Default Values.php
@@ -13,11 +13,11 @@ $envName = $writingAlgorithms ? "algorithm" : "notebook";
     </thead>
     <tbody>
         <tr>
-            <td>Resolution</td>
+            <td><code>resolution</code></td>
             <td>LEAN guesses the resolution you request by looking at the securities you already have in your <?=$envName?>. If you have a security subscription in your <?=$envName?> with a matching <code>Symbol</code>, the history request uses the same resolution as the subscription. If you don't have a security subscription in your <?=$envName?> with a matching <code>Symbol</code>, <code class="csharp">Resolution.Minute</code><code class="python">Resolution.MINUTE</code> is the default.</td>
         </tr>
         <tr class='csharp'>
-            <td>Bar type</td>
+            <td><code>&lt;T&gt;</code></code></td>
             <td>If you don't specify a type for the history request, <code>TradeBar</code> is the default. If the asset you request data for doesn't have <code>TradeBar</code> data, specify the <code>QuoteBar</code> type to receive history.</td>
         </tr>
     </tbody>
