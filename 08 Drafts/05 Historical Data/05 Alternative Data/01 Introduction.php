2252f02eb5906bf3da6382a7219849b86935f656
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-26 18:25:43 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/01 Introduction.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/01 Introduction.php
@@ -1,5 +1,5 @@
 <p>
-  Alternative datasets are datasets that aren't classified as market or fundamental data.
+  Alternative datasets provide signals to inform trading.
   To view all the alternative datasets available on QuantConnect, see the <a href='/datasets'>Dataset Market</a>.
   This page explains how to get historical data for alternative datasets. 
 </p>
