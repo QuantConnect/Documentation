5f153d5534353711dadb8c651a6d34a365c494fc
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-12-27 20:22:35 +0000

    Improves Information on Authetication Limitation

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Charles Schwab/11 Security and Stability.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Charles Schwab/11 Security and Stability.php
@@ -3,4 +3,4 @@ $brokerageName = "Charles Schwab";
 $statusPageURL = null;
 include(DOCS_RESOURCES."/brokerages/security-and-stability.php");
 ?>
-<p>Charles Schwab only supports authenticating one account at a time per user. Additional deployments will disconnect other running algorithms.</p>
+<p>Charles Schwab only supports authenticating one account at a time per user. If you have an algorithm running with Charles Schwab and then deploy a second one, the first algorithm stops running.</p>
--- 05 Lean CLI/09 Live Trading/01 Brokerages/06 Charles Schwab/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/06 Charles Schwab/01 Introduction.php
@@ -3,5 +3,5 @@ $brokerageName="Charles Schwab";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 ?>
 
-<div class='highlight'><p>Charles Schwab only supports authenticating one account at a time per user. Additional deployments will disconnect other running algorithms.</p></div>
+<p>Charles Schwab only supports authenticating one account at a time per user. If you have an algorithm running with Charles Schwab and then deploy a second one, the first algorithm stops running.</p>
 <p>To view the implementation of the Charles Schwab brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.CharlesSchwab' rel='nofollow' target="_blank">Lean.Brokerages.CharlesSchwab repository</a>.</p>
