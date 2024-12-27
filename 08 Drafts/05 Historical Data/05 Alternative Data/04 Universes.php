ac3879a41a5988ad0f9faa621cc09bbe542694d1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-27 11:34:56 -0500

    Update 04 Universes.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/04 Universes.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/04 Universes.php
@@ -43,7 +43,7 @@ Name: value, dtype: float64</pre>
 <p class='python'>To get the data in the format of the objects that you receive in your universe filter function instead of a DataFrame, use <code>flatten=False</code>.</p>
 
 <div class="python section-example-container">
-    <pre class="python"># Get the historical universe data over the last 30 days in a Series where
+    <pre class="python"># Get the historical universe data over the last 5 days in a Series where
 # the values in the series are lists of the universe selection objects.
 history = self.history(universe, timedelta(5), flatten=False)
 # Select the asset with the greatest value each day.
