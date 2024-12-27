720a7606f7b9e13b1cc8cd06d48d357f359314e2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-27 12:28:06 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/06 Custom Data/01 Introduction.php
+++ 08 Drafts/05 Historical Data/06 Custom Data/01 Introduction.php
@@ -1,6 +1,6 @@
 <p>
-  Custom data is your own external data that's not from the Dataset Market. 
-  You can use <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts'>custom data</a> to inform trading decisions and to simulate trades on unsupported securities.
+  You can import external datasets into your algorithm to use alongside other datasets from the Dataset Market.
   This page explains how to get historical data for custom datasets.
   Before you can get historical data for the dataset, define the <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts#04-Set-Data-Sources'><code class='python'>get_source</code><code class='csharp'>GetSource</code></a> and <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts#05-Parse-Custom-Data'><code class='python'>reader</code><code class='csharp'>Reader</code></a> methods of the custom data class.
+  For examples of custom dataset implementations, see <a href='/docs/v2/writing-algorithms/importing-data/streaming-data/key-concepts'>Key Concepts</a>.
 </p>
