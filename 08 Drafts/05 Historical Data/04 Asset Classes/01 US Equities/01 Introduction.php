ae9b56060fead056ba049a2c742e859f9b830691
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-27 17:39:07 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/01 Introduction.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/01 Introduction.php
@@ -2,4 +2,4 @@
 $assetClass = "US Equities";
 ?>
 
-<p>This page explains how to get historical data for <?=$assetClass?>.</p>
+<p>This page explains how to get historical data for <?=$assetClass?>. Some of the data you can get include prices, fundamentals, corporate actions, and universe selection data.</p>
