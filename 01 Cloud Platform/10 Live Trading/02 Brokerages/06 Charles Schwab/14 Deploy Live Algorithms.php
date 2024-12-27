7935b8a905153e02100acaec11e4a776e429b680
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-12-27 19:13:35 +0000

    Fixes Charles Schwab Deployment Text

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Charles Schwab/14 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/06 Charles Schwab/14 Deploy Live Algorithms.php
@@ -6,6 +6,6 @@ $secondBullet = "";
 $authentication = "<li>Check the <span class=\"box-name\">Authorization</span> check box and then click <span class=\"button-name\">Authenticate</span>.</li>
 <li>On the Charles Schwab website, click <span class=\"button-name\">Allow</span> to grant QuantConnect access to your account information and authorization.</li>";
 $postDeploy = "";
-$dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/charles-schwab'>Charles Schwab data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>.</p>";
+$dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/charles-schwab'>Charles Schwab data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
 ?>
