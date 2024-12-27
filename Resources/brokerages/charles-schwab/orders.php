5864f60601c7152dab992e36782cf405e362f1d1
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2024-12-27 18:03:00 +0000

    Fixes Order Properties Tables

--- Resources/brokerages/charles-schwab/orders.php
+++ Resources/brokerages/charles-schwab/orders.php
@@ -57,6 +57,7 @@
     <tbody>
         <tr>
             <td><code class="csharp">TimeInForce</code><code class="python">time_in_force</code></td>
+            <td><code>TimeInForce</code></td>
             <td>A <a href='/docs/v2/writing-algorithms/trading-and-orders/order-properties#03-Time-In-Force'>TimeInForce</a> instruction to apply to the order. The following instructions are supported:
                 <ul>
                     <li><code class="csharp">Day</code><code class="python">DAY</code></li>
@@ -70,7 +71,7 @@
             <td><code class="csharp">ExtendedRegularTradingHours</code><code class="python">extended_regular_trading_hours</code></td>
             <td><code>bool</code></td>
             <td>If set to true, allows orders to also trigger or fill outside of regular trading hours.</td>
-            <td></td>
+            <td><code class="csharp">false</code><code class="python">False</code></td>
         </tr>
     </tbody>
 </table>
