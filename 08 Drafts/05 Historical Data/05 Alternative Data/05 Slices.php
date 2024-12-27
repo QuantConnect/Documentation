60925fbb0977472d7956abb02f67c689198ca0ff
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-27 10:58:05 -0500

    Update 05 Slices.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/05 Slices.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/05 Slices.php
@@ -4,8 +4,21 @@
 </p>
 
 <div class="csharp section-example-container">
-      <pre class="csharp">// Get the latest 3 data points of some alternative datasets, packaged into Slice objects.
-var history = History(datasetSymbols, 3);</pre>
+      <pre class="csharp">// Add an alternative dataset.
+var symbol = AddCrypto("BTCUSD", Resolution.Daily, Market.Bitfinex).Symbol;
+var datasetSymbol = AddData&lt;BitcoinMetadata&gt;(symbol).Symbol;
+// Get the latest 3 data points of some alternative dataset(s), packaged into Slice objects.
+var history = History(new[] { datasetSymbol }, 3);
+// Iterate through each Slice and get the alternative data points.
+foreach (var slice in history)
+{
+    Log($"Hash rate at {slice.Time}: {slice[datasetSymbol].HashRate}");
+}</pre>
+</div>
+<div class="csharp section-example-container">
+      <pre>Hash rate at 6/23/2023 8:00:00 PM: 325294307.5702
+Hash rate at 6/24/2023 8:00:00 PM: 379943751.242
+Hash rate at 6/25/2023 8:00:00 PM: 398160232.466</pre>
 </div>
 
 <p class='csharp'>If you don't pass any <code>Symbol</code> objects, it returns data for all the data subscriptions in your <?=$writingEnvironment ? "algorithm" : "notebook" ?>, so the result may include more than just alternative data.</p>
@@ -17,9 +30,55 @@ var history = History(datasetSymbols, 3);</pre>
 
 <div class="section-example-container">
       <pre class="csharp">// Get the latest 3 data points of all the securities/datasets in the <?=$writingEnvironment ? "algorithm" : "notebook" ?>, packaged into Slice objects.
-var history = History(3);</pre>
-      <pre class="python"># Get the latest 3 data points of all the securities/datasets in the <?=$writingEnvironment ? "algorithm" : "notebook" ?>, packaged into Slice objects.
-history = self.history(3)</pre>
+var history = History(3);
+// Iterate through each Slice and get the synchronized data points at each moment in time.
+foreach (var slice in history)
+{
+    Log($"Time: {slice.Time}");
+    if (slice.ContainsKey(symbol))
+    {
+        Log($"Price: {slice[symbol].Price}");
+    }
+    if (slice.ContainsKey(datasetSymbol))
+    {
+        Log($"Hash rate: {((BitcoinMetadata)slice[datasetSymbol]).HashRate}");
+    }
+}</pre>
+      <pre class="python"># Add an asset and an alternative dataset.
+symbol = self.add_crypto('BTCUSD', Resolution.DAILY, Market.BITFINEX).symbol
+dataset_symbol = self.add_data(BitcoinMetadata, symbol).symbol
+# Get the latest 3 data points of all the securities/datasets in the <?=$writingEnvironment ? "algorithm" : "notebook" ?>, packaged into Slice objects.
+history = self.history(3)
+# Iterate through each Slice and get the synchronized data points at each moment in time.
+for slice_ in history:
+    self.log(f"Time: {slice_.time}")
+    if symbol in slice_:
+        self.log(f"Price: {slice_[symbol].price}")
+    if dataset_symbol in slice_:
+        self.log(f"Hash rate: {slice_[dataset_symbol].hash_rate}")</pre>
+</div>
+
+<div class="csharp section-example-container">
+      <pre>Time: 6/23/2023 8:00:00 PM
+Price: 30712
+Hash rate: 325294307.5702
+Time: 6/24/2023 8:00:00 PM
+Price: 30556
+Hash rate: 379943751.242
+Time: 6/25/2023 8:00:00 PM
+Price: 30492
+Hash rate: 398160232.466</pre>
+</div>
+<div class="python section-example-container">
+      <pre>Time: 2023-06-23 20:00:00
+Price: 30712.0
+Hash rate: 325294307.5702
+Time: 2023-06-24 20:00:00
+Price: 30556.0
+Hash rate: 379943751.242
+Time: 2023-06-25 20:00:00
+Price: 30492.0
+Hash rate: 398160232.466</pre>
 </div>
 
 <p>
