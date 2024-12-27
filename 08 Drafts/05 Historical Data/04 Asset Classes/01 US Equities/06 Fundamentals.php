332978496560a84d026e54c3f2cceace300929b2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-27 18:29:03 -0500

    Update 06 Fundamentals.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/06 Fundamentals.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/06 Fundamentals.php
@@ -1,1 +1,50 @@
-<? include(DOCS_RESOURCES."/datasets/fundamentals/historical-data.html"); ?>
+<?
+$imgLink = "https://cdn.quantconnect.com/i/tu/history-tradebar-dataframe-us-equities.png";
+?>
+
+<p class='csharp'>
+  To get historical <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-fundamentals'>fundamental data</a>, call the <code>History&lt;<?=Fundamental?>&gt;</code> method with an asset's <code>Symbol</code>.
+</p>
+
+<p class='python'>
+  To get historical <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/corporate-fundamentals'>fundamental data</a>, call the <code>history</code> method with the <code>Fundamental</code> type and an asset's <code>Symbol</code>.
+  This method returns a DataFrame with columns for the <a href='/docs/v2/writing-algorithms/datasets/morningstar/us-fundamental-data#06-Data-Point-Attributes'>data point attributes</a>.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">// Get the Symbol of an asset.
+var symbol = AddEquity("AAPL").Symbol;
+// Get the 3 trailing daily Fundamental objects of the asset. 
+var history = History&lt;Fundamental&gt;(symbol, 3, Resolution.Daily);</pre>
+    <pre class="python"># Get the Symbol of an asset.
+symbol = self.add_equity('AAPL').symbol
+# Get the 3 trailing daily Fundamental objects of the asset in DataFrame format. 
+history = self.history(Fundamental, symbol, 3, Resolution.DAILY)</pre>
+</div>
+
+<img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/fundamental-data-history-dataframe.png' alt='DataFrame of the fundamentals of an asset.'>
+
+<div class="section-example-container">
+    <pre class="csharp"></pre>
+    <pre class="python"># Get the Current Ratio of each row.
+current_ratios = history.apply(lambda row: row.operationratios.current_ratio.value, axis=1)</pre>
+</div>
+
+<div class="python section-example-container">
+    <pre>symbol  time      
+AAPL    2024-12-24    0.867313
+        2024-12-25    0.867313
+        2024-12-27         NaN
+dtype: float64</pre>
+</div>
+
+<p class='python'>
+  If you request a DataFrame, LEAN unpacks the data from <code>Fundamental</code> objects to populate the DataFrame. 
+  To avoid consuming request computational resources populating the DataFrame, you can instead request <code>Fundamental</code> objects. 
+  To get a list of <code>Fundamental</code> objects, call the <code>history[Fundamental]</code> method.
+</p>
+
+<div class="python section-example-container">
+    <pre class="python"># Get the 3 trailing daily Fundamental objects of an asset in Fundamental format. 
+history = self.history[Fundamental](symbol, 3, Resolution.DAILY)</pre>
+</div>
