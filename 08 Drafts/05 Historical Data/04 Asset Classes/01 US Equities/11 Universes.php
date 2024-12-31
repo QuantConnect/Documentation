74a88ef1911d12e681a4f339c1336579c41e3103
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2024-12-31 18:11:30 -0500

    Update 11 Universes.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/11 Universes.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/11 Universes.php
@@ -13,20 +13,31 @@ $dataTypeLink = "/docs/v2/writing-algorithms/universes/equity";
 </p>
 
 <div class="section-example-container">
-    <pre class="csharp">// Add a universe of US Equities based on the constituents of an ETF.
-var universe = AddUniverse(Universe.ETF("SPY"));
-// Get 5 days of history for the universe.
-var history = History(universe, TimeSpan.FromDays(5));
-// Iterate through each day of the universe history.
-foreach (var constituents in history)
+    <pre class="csharp">public class USEquityUniverseHistoryAlgorithm : QCAlgorithm
 {
-    // Select the 2 assets with the smallest weights in the ETF on this day.
-    var dailyLargest = constituents.Select(c => c as ETFConstituentData).OrderByDescending(c => c.Weight).Take(2);
+    public override void Initialize()
+    {
+        SetStartDate(2024, 12, 23);
+        // Add a universe of US Equities based on the constituents of an ETF.
+        var universe = AddUniverse(Universe.ETF("SPY"));
+        // Get 5 days of history for the universe.
+        var history = History(universe, TimeSpan.FromDays(5));
+        // Iterate through each day of the universe history.
+        foreach (var constituents in history)
+        {
+            // Select the 2 assets with the smallest weights in the ETF on this day.
+            var dailyLargest = constituents.Select(c => c as ETFConstituentData).OrderByDescending(c => c.Weight).Take(2);
+        }
+    }
 }</pre>
-    <pre class="python"># Add a universe of US Equities based on the constituents of an ETF.
-universe = self.add_universe(self.universe.etf('SPY'))
-# Get 5 days of history for the universe.
-history = self.history(universe, timedelta(5), flatten=True)</pre>
+    <pre class="python">class USEquityUniverseHistoryAlgorithm(QCAlgorithm):
+
+    def initialize(self) -> None:
+        self.set_start_date(2024, 12, 23)
+        # Add a universe of US Equities based on the constituents of an ETF.
+        universe = self.add_universe(self.universe.etf('SPY'))
+        # Get 5 days of history for the universe.
+        history = self.history(universe, timedelta(5), flatten=True)</pre>
 </div>
 
 <img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of universe data for an asset.'>
