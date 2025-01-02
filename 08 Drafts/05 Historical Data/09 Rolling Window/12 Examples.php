19798119e49c6f6fab4ddf651c2c70b428103029
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-01-02 11:53:26 -0500

    Update 12 Examples.php

--- 08 Drafts/05 Historical Data/09 Rolling Window/12 Examples.php
+++ 08 Drafts/05 Historical Data/09 Rolling Window/12 Examples.php
@@ -1,1 +1,20 @@
+<p>The following examples demonstrate some common practices for rolling windows.</p>
+
+<? 
+$number = 1;
+include(DOCS_RESOURCES."/examples/take-profit-stop-loss-example.php");
+?>
+
+<? 
+$number = 2;
+include(DOCS_RESOURCES."/examples/bid-ask-spread.php"); 
+?>
+
+<?
+$number = 3;
+include(DOCS_RESOURCES."/examples/ema-acceleration.php"); 
+?>
+
+<h4>Other Examples</h4>
+<p>For more examples, see the following algorithms:</p>
 <? include(DOCS_RESOURCES."/rolling-window/examples.html"); ?>
