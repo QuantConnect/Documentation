15e431f1e2eadffc82d3ecf1f5c5a6efa12c6168
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-01-02 10:45:00 -0500

    Update 01 Introduction.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/01 Introduction.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/01 Introduction.php
@@ -1,5 +1,5 @@
 <p>
-  Alternative datasets provide signals to inform trading.
+  Alternative datasets provide signals to inform trading decisions.
   To view all the alternative datasets available on QuantConnect, see the <a href='/datasets'>Dataset Market</a>.
   This page explains how to get historical data for alternative datasets. 
 </p>
