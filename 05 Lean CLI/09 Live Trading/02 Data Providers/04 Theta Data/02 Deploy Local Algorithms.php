f5b727a3bacaa2cef4f8cd18b48dc941f1344106
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-01-02 18:56:54 -0500

    Add Theta Data data provider tutorial

---
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/04 Theta Data/02 Deploy Local Algorithms.php
@@ -0,0 +1,28 @@
+<?
+$brokerageName = "QuantConnect Paper Trading";
+$dataFeedName = "ThetaData";
+$isBrokerage = false;
+
+$brokerageDetails = "";
+
+$dataFeedDetails = "
+    <li>(Optional) Enter the host of the ThetaData Client. 
+    <p>The default host is <span class='live-url'>ws://host.docker.internal:25520/v1/events</span> or <span class='live-url'>http://host.docker.internal:25510</span>.</p>
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+The host of ThetaData Client [ws://host.docker.internal:25520/v1/events]:
+The host of ThetaData Client [http://host.docker.internal:25510]:</pre>
+</div>
+    </li>
+
+    <li>Enter your Theta Data subscription plan. 
+<div class='cli section-example-container'>
+<pre>$ lean live \"My Project\"
+ThetaData subscription price plan (Free, Value, Standard, Pro):</pre>
+</div>
+    </li>
+";
+$supportsIQFeed = false;
+$requiresSubscription = false;
+include(DOCS_RESOURCES."/brokerages/cli-deployment/deploy-local-algorithms.php");
+?>
