39e43a78c28bc438d926af6a2f58450d429c8ee0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-01-02 10:54:22 -0500

    Update 03 Trading During Warm Up.php

--- 08 Drafts/05 Historical Data/10 Common Errors/03 Trading During Warm Up.php
+++ 08 Drafts/05 Historical Data/10 Common Errors/03 Trading During Warm Up.php
@@ -3,4 +3,4 @@
   If you try to place a trade during warm-up, LEAN throws the following error:
 </p>
 
-<div class='error-messages'>Backtest Handled Error: This operation is not allowed in Initialize or during warm up: OrderRequest.Submit. Please move this code to the OnWarmupFinished() method.</div></p>
+<div class='error-messages'>Backtest Handled Error: This operation is not allowed in <span class='csharp'>Initialize</span><span class='csharp'>initialize</span> or during warm up: OrderRequest.Submit. Please move this code to the <span class='csharp'>OnWarmupFinished()</span><span class='csharp'>on_warmup_finished()</span> method.</div></p>
