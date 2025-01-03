a5630d8581961de9f9f5e05a4ab4cac8277376f4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-01-03 16:51:05 -0500

    Update 05 Greek Indicators.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/05 Greek Indicators.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/02 Equity Options/03 Greeks and Implied Volatility/01 Key Concepts/05 Greek Indicators.php
@@ -2,3 +2,5 @@
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
 include(DOCS_RESOURCES."/option-pricing/greek-indicators.php"); 
 ?>
+
+<p>For Equity Options, the <code class="csharp">IndicatorHistory</code><code class="python">indicator_history</code> uses the <code class='python'>SCALED_RAW</code><code class='csharp'>ScaledRaw</code> <a href='/docs/v2/writing-algorithms/securities/asset-classes/us-equity/requesting-data#11-Data-Normalization'>data normalization mode</a> when requesting historical prices.</p>
