8673f1c2b8641ccd481be5169540e4fa01f4b091
GitHub <github-actions@github.com>
2025-01-03 15:03:54 +0000

    Code generated by market-hour-code-generator.py

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/44 FT1/05 Holidays.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/44 FT1/05 Holidays.php
@@ -1,1 +1,1 @@
-<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/generic/holidays.html"); ?>
+<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/FT1/holidays.html"); ?>
--- 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/45 FT5/05 Holidays.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/45 FT5/05 Holidays.php
@@ -1,1 +1,1 @@
-<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/generic/holidays.html"); ?>
+<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/FT5/holidays.html"); ?>
