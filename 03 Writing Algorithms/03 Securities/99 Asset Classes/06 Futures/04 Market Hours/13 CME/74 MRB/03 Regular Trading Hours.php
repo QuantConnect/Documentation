817c2d5f891175fd6c8d266ff3f829454a4847a3
GitHub <github-actions@github.com>
2025-01-10 14:43:44 +0000

    Code generated by market-hour-code-generator.py

---
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/74 MRB/03 Regular Trading Hours.php
@@ -0,0 +1,1 @@
+<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/MRB/regular-trading-hours.html"); ?>
---
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/06 Futures/04 Market Hours/13 CME/74 MRB/04 Post-market Hours.php
@@ -0,0 +1,1 @@
+<?php include(DOCS_RESOURCES."/datasets/market-hours/future/cme/MRB/post-market-hours.html"); ?>
