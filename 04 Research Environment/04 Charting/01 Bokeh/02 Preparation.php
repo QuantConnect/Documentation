5797ccc483e91a2c17aad8d6cdec79de94ba8184
LouisSzeto <hke0073@icloud.com>
2025-02-04 11:41:45 +0800

    bokeh

---
+++ 04 Research Environment/04 Charting/01 Bokeh/02 Preparation.php
@@ -0,0 +1,14 @@
+<?
+$library = "Bokeh";
+$imports = """# Import the Bokeh library for plots and settings.
+from bokeh.plotting import figure, show
+from bokeh.models import BasicTicker, ColorBar, ColumnDataSource, LinearColorMapper
+from bokeh.palettes import Category20c
+from bokeh.transform import cumsum, transform
+from bokeh.io import output_notebook
+
+# Call the `output_notebook` method for displaying the plots in the jupyter notebook.
+output_notebook()"""
+
+include(DOCS_RESOURCES."/research-guide/charting/python-preparation.php");
+?>
---
+++ Resources/research-guide/charting/python-preparation.php
@@ -0,0 +1,12 @@
+
+<h4>Import Libraries</h4>
+
+<p>To research with the <?=$library?> library, import the libraries that you need.</p>
+
+<div class="section-example-container">
+    <pre class="python"><?=$imports?></pre>
+</div>
+
+<h4>Get Historical Data</h4>
+
+<?php include(DOCS_RESOURCES."/charting/get-historical-data.html"); ?>
