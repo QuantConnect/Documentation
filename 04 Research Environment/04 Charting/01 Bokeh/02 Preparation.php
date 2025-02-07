ecf6d94f44957f9c1cd2f9e061c6785f6c81fde6
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2025-02-07 11:30:52 +0800

    Update 02 Preparation.php

--- 04 Research Environment/04 Charting/01 Bokeh/02 Preparation.php
+++ 04 Research Environment/04 Charting/01 Bokeh/02 Preparation.php
@@ -1,6 +1,6 @@
 <?
 $library = "Bokeh";
-$imports = """# Import the Bokeh library for plots and settings.
+$imports = "# Import the Bokeh library for plots and settings.
 from bokeh.plotting import figure, show
 from bokeh.models import BasicTicker, ColorBar, ColumnDataSource, LinearColorMapper
 from bokeh.palettes import Category20c
@@ -8,7 +8,7 @@ from bokeh.transform import cumsum, transform
 from bokeh.io import output_notebook
 
 # Call the `output_notebook` method for displaying the plots in the jupyter notebook.
-output_notebook()"""
+output_notebook()"
 
 include(DOCS_RESOURCES."/research-guide/charting/python-preparation.php");
 ?>
