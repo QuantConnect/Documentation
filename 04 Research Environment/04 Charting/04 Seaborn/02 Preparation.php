f21f2f7cbf9e0d3fea5e2d391746e7efdc6a2c64
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2025-02-07 11:31:30 +0800

    Update 02 Preparation.php

--- 04 Research Environment/04 Charting/04 Seaborn/02 Preparation.php
+++ 04 Research Environment/04 Charting/04 Seaborn/02 Preparation.php
@@ -1,9 +1,9 @@
 <?
 $library = "Seaborn";
-$imports = """import seaborn as sns
+$imports = "import seaborn as sns
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()"""
+register_matplotlib_converters()"
 
 include(DOCS_RESOURCES."/research-guide/charting/python-preparation.php");
 ?>
