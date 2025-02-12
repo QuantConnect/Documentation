a7f9c6844d3e0b52e706e12216c2641930391c8a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-12 15:18:26 -0500

    Update 13 Exceptions and Edge Cases.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/01 Requesting Data/02 Individual Contracts/13 Exceptions and Edge Cases.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/09 Index Options/01 Requesting Data/02 Individual Contracts/13 Exceptions and Edge Cases.php
@@ -1,13 +1,5 @@
 <p>The following sections explain exceptions and edge cases with subscribing to individual Option contracts.</p>
 
-<h4>Default Underlying Subscription Settings</h4>
-
-<p>If you subscribe to an Index Option contract but don't have a subscription to the underlying Index, LEAN automatically subscribes to the underlying Index and sets its <a href='/docs/v2/writing-algorithms/securities/asset-classes/index/requesting-data#05-Fill-Forward'>fill forward</a> property to match that of the Index Option contract. In this case, you still need the Index <code class="csharp">Symbol</code><code class="python">symbol</code> to subscribe to Index Option contracts. If you don't have access to it, create it.</p>
-<div class="section-example-container">
-    <pre class="csharp">_underlying = QuantConnect.Symbol.Create("SPX", SecurityType.Index, Market.USA);</pre>
-    <pre class="python">self._underlying = Symbol.create("SPX", SecurityType.INDEX, Market.USA)</pre>
-</div>
-
 <h4>Manually Creating Option Symbol Objects</h4>
 
 <p>
@@ -30,6 +22,14 @@ self._contract_symbol = Symbol.create_option(self._underlying, Market.USA,
 # Weekly contracts
 self._weekly_contract_symbol = Symbol.create_option(self._underlying, "SPXW", Market.USA,
     OptionStyle.EUROPEAN, OptionRight.CALL, 3650, datetime(2022, 6, 17))</pre>
+</div>
+
+<h4>Default Underlying Subscription Settings</h4>
+
+<p>If you subscribe to an Index Option contract but don't have a subscription to the underlying Index, LEAN automatically subscribes to the underlying Index and sets its <a href='/docs/v2/writing-algorithms/securities/asset-classes/index/requesting-data#05-Fill-Forward'>fill forward</a> property to match that of the Index Option contract. In this case, you still need the Index <code class="csharp">Symbol</code><code class="python">symbol</code> to subscribe to Index Option contracts. If you don't have access to it, create it.</p>
+<div class="section-example-container">
+    <pre class="csharp">_underlying = QuantConnect.Symbol.Create("SPX", SecurityType.Index, Market.USA);</pre>
+    <pre class="python">self._underlying = Symbol.create("SPX", SecurityType.INDEX, Market.USA)</pre>
 </div>
 
 <h4>Overriding the Initial Implied Volatility Guess</h4>
