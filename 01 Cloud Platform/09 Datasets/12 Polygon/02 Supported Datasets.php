fdeba2bf801ae6de7222131d6dc3a08e4159e2c8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:52:29 -0500

    Update 02 Supported Datasets.php

--- 01 Cloud Platform/09 Datasets/12 Polygon/02 Supported Datasets.php
+++ 01 Cloud Platform/09 Datasets/12 Polygon/02 Supported Datasets.php
@@ -1,11 +1,13 @@
-<p>The Polygon data provider serves asset price data directly from Polygon. Our integration supports securities from the following asset classes:</p>
+<p>The Polygon data provider sources asset price data directly from Polygon. Our integration supports securities from the following asset classes:</p>
 
 <? include(DOCS_RESOURCES."/data-providers/supported-assets/polygon.html"); ?>
 
-<p>To supplement the asset price data from Polygon, we provide the following auxiliary datasets from the Dataset Market:</p>
+<p>To supplement the asset price data from Polygon, QuantConnect Cloud provides the following auxiliary datasets from the Dataset Market:</p>
 
 <ul>
     <li><a href="/datasets/quantconnect-us-equity-security-master">US Equity Security Master</a></li>
+    <li><a href="/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-equity-option-universe">US Equity Option Universe</a></li>
+    <li><a href="/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-index-options">US Index Option Universe</a></li>
     <li>Universe selection datasets</li>
     <li>Non-streaming <a href="/docs/v2/cloud-platform/datasets/quantconnect/alternative-data">alternative datasets</a></li>
 </ul>
