02d9fd0ddf61cd3192f7e4348146eae06bd07211
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:46:16 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/08 Alpaca/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/08 Alpaca/01 Introduction.php
@@ -1,5 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/alpaca.html"); ?>
 <p>
-  The Alpaca data feeds are streams of Equity, Equity Option, and Crypto prices directly from <a rel='nofollow' target='_blank' href='https://docs.alpaca.markets/docs/about-market-data-api'>Alpaca's Market Data API</a>. 
+  The Alpaca data provider serves Equity, Equity Option, and Crypto prices directly from <a rel='nofollow' target='_blank' href='https://docs.alpaca.markets/docs/about-market-data-api'>Alpaca's Market Data API</a>. 
   This page explains our integration with their API and its functionality.
 </p>
