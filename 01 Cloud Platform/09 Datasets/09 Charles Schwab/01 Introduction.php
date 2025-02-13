fe82db4770175206731f944ac9d1ab76bab1becd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:10:22 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/09 Charles Schwab/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/09 Charles Schwab/01 Introduction.php
@@ -1,5 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/charles-schwab.html"); ?>
 <p>
-  The Charles Schwab data feeds are streams of Equity, Equity Option, Index, and Index Option prices directly from <a rel='nofollow' target='_blank' href='https://developer.schwab.com/products/trader-api--individual'>Charles Schwab's Trader API</a>. 
-  This page explains our integration with their API and the functionality it provides.
+  The Charles Schwab data provider serves Equity, Equity Option, Index, and Index Option prices directly from <a rel='nofollow' target='_blank' href='https://developer.schwab.com/products/trader-api--individual'>Charles Schwab's Trader API</a>. 
+  This page explains our integration with their API and the its functionality.
 </p>
