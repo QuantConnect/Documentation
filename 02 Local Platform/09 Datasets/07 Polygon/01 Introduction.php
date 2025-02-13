5db0ea7e01cd3e857a45b59dec3db26c21369b39
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:55:47 -0500

    Update 01 Introduction.php

--- 02 Local Platform/09 Datasets/07 Polygon/01 Introduction.php
+++ 02 Local Platform/09 Datasets/07 Polygon/01 Introduction.php
@@ -1,9 +1,8 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/polygon.html"); ?>
 
-<p>To view the implementation of the Polygon integration, see the <a href='https://github.com/QuantConnect/Lean.DataSource.Polygon' rel='nofollow' target='_blank'>Lean.DataSource.Polygon repository</a>.</p>
-
-
 <p>
   The Polygon data provider serves live asset prices from Polygon. 
   This page explains our integration with their API and the its functionality.
 </p>
+
+<p>To view the implementation of the Polygon integration, see the <a href='https://github.com/QuantConnect/Lean.DataSource.Polygon' rel='nofollow' target='_blank'>Lean.DataSource.Polygon repository</a>.</p>
