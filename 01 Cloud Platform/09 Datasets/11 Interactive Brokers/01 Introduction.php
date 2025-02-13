45568a04e863f9f9bbfa4f227c7965aac2ce36cc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:28:34 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/11 Interactive Brokers/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/11 Interactive Brokers/01 Introduction.php
@@ -1,6 +1,6 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/interactive-brokers.html"); ?>
 
 <p>
-  The Interactive Brokers (IB) data provider serves live asset prices from <a rel='nofollow' target='_blank' href='https://developer.schwab.com/products/trader-api--individual'>IB's Trader Workstation API</a>.
+  The Interactive Brokers (IB) data provider serves live asset prices from <a rel='nofollow' target='_blank' href='https://interactivebrokers.github.io/tws-api/'>IB's Trader Workstation API</a>.
   This page explains our integration with their data API and its functionality.
 </p>
