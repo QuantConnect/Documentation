ae670ddf1496a076647dce4d63fb5408d3d5adc8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 18:29:11 -0500

    Update 02 Sourcing.php

--- 01 Cloud Platform/09 Datasets/11 Interactive Brokers/02 Sourcing.php
+++ 01 Cloud Platform/09 Datasets/11 Interactive Brokers/02 Sourcing.php
@@ -1,5 +1,7 @@
-<p>The IB data provider sources data directly from IB. For more information about the data source, see the <a rel="nofollow" target="_blank" href="https://interactivebrokers.github.io/tws-api/">Trader Workstation API documentation</a>.</p>
-
-<p>If you use the IB data provider, IB only provides the security price data. We provide the following auxiliary datasets:</p>
+<p>
+  The IB data provider sources data directly from <a rel='nofollow' target='_blank' href='https://interactivebrokers.github.io/tws-api/'>IB's Trader Workstation API</a>.
+  If you use this data provider, IB only provides the security price data. 
+  QuantConnect Cloud provides the following auxiliary datasets:
+</p>
 
 <? include(DOCS_RESOURCES."/datasets/auxiliary-dataset-list.html"); ?>
