f23081086a933279022a1840689fe16963f23fa8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 16:34:03 -0500

    Update 03 Supported Datasets.php

--- 05 Lean CLI/05 Datasets/11 Polygon/03 Supported Datasets.php
+++ 05 Lean CLI/05 Datasets/11 Polygon/03 Supported Datasets.php
@@ -1,3 +1,8 @@
 <p>Our Polygon integration supports securities from the following asset classes:</p>
 
 <? include(DOCS_RESOURCES."/data-providers/supported-assets/polygon.html"); ?>
+
+<p>
+  The Polygon data provider serves raw data. 
+  To get adjusted Equity data in local deployments, <a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-security-master/cli'>download the US Equity Security Master</a>.
+</p>
