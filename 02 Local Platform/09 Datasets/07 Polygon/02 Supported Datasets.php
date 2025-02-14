30438a5924950a666cbbf1b0490c6eebab018966
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-13 19:03:29 -0500

    Update 02 Supported Datasets.php

--- 02 Local Platform/09 Datasets/07 Polygon/02 Supported Datasets.php
+++ 02 Local Platform/09 Datasets/07 Polygon/02 Supported Datasets.php
@@ -3,6 +3,6 @@
 <? include(DOCS_RESOURCES."/data-providers/supported-assets/polygon.html"); ?>
 
 <p>
-  If you use the Polygon data provider, it only provides the security price data, so you need to <a href='/datasets/quantconnect-us-equity-security-master/cli'>download the US Equity Security Master with the CLI</a>. 
+  This data provider only provides the security price data, so <a href='/datasets/quantconnect-us-equity-security-master/cli'>download the US Equity Security Master with the CLI</a>. 
   For more information about the data source, see the <a rel='nofollow' target='_blank' href='https://polygon.io/docs/stocks'>Polygon API documentation</a>.
 </p>
