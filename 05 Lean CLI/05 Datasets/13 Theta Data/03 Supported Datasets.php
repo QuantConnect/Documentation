4e174e3566d6052fd29752910ab24762e2341582
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 16:38:01 -0500

    Update 03 Supported Datasets.php

--- 05 Lean CLI/05 Datasets/13 Theta Data/03 Supported Datasets.php
+++ 05 Lean CLI/05 Datasets/13 Theta Data/03 Supported Datasets.php
@@ -1,1 +1,6 @@
 <? include(DOCS_RESOURCES."/data-providers/supported-assets/theta-data.html"); ?>
+
+<p>
+  The Theta Data data provider serves raw data. 
+  To get adjusted Equity data in local deployments, <a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-security-master/cli'>download the US Equity Security Master</a>.
+</p>
