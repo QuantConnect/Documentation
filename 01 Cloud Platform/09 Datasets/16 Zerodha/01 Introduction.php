d7aaa5739cc91716c4946a54991fe45729a28f07
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 13:12:57 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/16 Zerodha/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/16 Zerodha/01 Introduction.php
@@ -1,2 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/zerodha.html"); ?>
-<p>The Zerodha data feed streams live asset prices from Zerodha. If you use this data feed and request historical data, the historical data comes from Zerodha.</p>
+<p>
+  The Zerodha data provider serves asset prices from <a rel='nofollow' target='_blank' href='https://developers.stocknote.com/api/#stocknote-api-documentation'>Zerodha's Kite Connect API</a>. 
+  This page explains our integration with their API and its functionality.
+</p>
