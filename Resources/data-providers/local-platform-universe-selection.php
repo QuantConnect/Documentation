5c959587f261334a261f8a85d53b6378e6e82bf1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 16:49:45 -0500

    Update local-platform-universe-selection.php

--- Resources/data-providers/local-platform-universe-selection.php
+++ Resources/data-providers/local-platform-universe-selection.php
@@ -1,8 +1,15 @@
-<p>In local deployments, <a href='/docs/v2/writing-algorithms/universes/equity'>Universe Selection</a> is available with the <?=$dataProviderName?> data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. The dataset listings show how to download the universe selection data with the CLI. For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. For example, the following tutorials explain how to download historical data and download daily updates:</p>
+<p>
+	In local deployments, <a href='/docs/v2/writing-algorithms/universes/key-concepts'>universe selection</a> is available with the <?=$dataProviderName?> data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
+	The dataset listings show how to download the universe selection data with the CLI. 
+	For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. 
+	For example, the following tutorials explain how to download historical data and download daily updates:
+</p>
 
 <ul>
-	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-etf-constituents'>US ETF Constituents</a></li>
 	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-equity-coarse-universe'>US Equity Coarse Universe</a></li>	
+	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-equity-option-universe'>US Equity Option Universe</a></li>
+	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-etf-constituents'>US ETF Constituents</a></li>
+	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-index-option-universe'>US Index Option Universe</a></li>
 </ul>
 
 <p>In cloud deployments, QuantConnect Cloud provides the universe selection dataset.</p>
