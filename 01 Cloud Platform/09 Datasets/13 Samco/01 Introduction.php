e5061b7431796332893c5bf14d7214704f0f19f3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 13:12:25 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/13 Samco/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/13 Samco/01 Introduction.php
@@ -1,5 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/samco.html"); ?>
 <p>
   The Samco data provider serves live asset prices from <a rel='nofollow' target='_blank' href='https://developers.stocknote.com/api/#stocknote-api-documentation'>Samco's Trade API</a>. 
-  This page explains our integration with Polygon and its functionality.
+  This page explains our integration with Samco and its functionality.
 </p>
