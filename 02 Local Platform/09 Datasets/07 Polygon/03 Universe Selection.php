ac4887bcc6f9fdebc146fee1725108e2c3650ad1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 10:30:59 -0500

    Update 03 Universe Selection.php

--- 02 Local Platform/09 Datasets/07 Polygon/03 Universe Selection.php
+++ 02 Local Platform/09 Datasets/07 Polygon/03 Universe Selection.php
@@ -1,5 +1,5 @@
 <p>
-  <a href='/docs/v2/writing-algorithms/universes/key-concepts'>Universe selection</a> is available with the Polygon data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
+  In local deployments, <a href='/docs/v2/writing-algorithms/universes/key-concepts'>universe selection</a> is available with the Polygon data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
   The dataset listings show how to download the universe selection data with the CLI. 
   For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. 
   For example, the following tutorials explain how to download historical data and download daily updates:
@@ -11,3 +11,5 @@
   <li><a href="/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-etf-constituents">US ETF Constituents</a></li>
   <li><a href="/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-index-options">US Index Option Universe</a></li>
 </ul>
+
+<p>In cloud deployments, QuantConnect Cloud provides the universe selection datasets.</p>
