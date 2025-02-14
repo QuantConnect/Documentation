5609cfcc53d70281e4c5c291647f48beac3a2110
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 13:29:53 -0500

    Update 03 Universe Selection.php

--- 02 Local Platform/09 Datasets/05 Alpha Vantage/03 Universe Selection.php
+++ 02 Local Platform/09 Datasets/05 Alpha Vantage/03 Universe Selection.php
@@ -1,4 +1,11 @@
-<? 
-$dataProviderName = "Alpha Vantage";
-include(DOCS_RESOURCES."/data-providers/local-platform-universe-selection.php"); 
-?>
+<p>
+  <a href='/docs/v2/writing-algorithms/universes/equity'>Universe selection</a> is available with the Alpha Vantage data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
+  The dataset listings show how to download the universe selection data with the CLI. 
+  For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. 
+  For example, the following tutorials explain how to download historical data and download daily updates:
+</p>
+
+<ul>
+	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-etf-constituents'>US ETF Constituents</a></li>
+	<li><a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-equity-coarse-universe'>US Equity Coarse Universe</a></li>	
+</ul>
