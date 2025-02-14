b6b0be5e48668963fb31d051ae8f8c849c0c1764
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 13:13:57 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/16 Zerodha/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/16 Zerodha/01 Introduction.php
@@ -1,5 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/zerodha.html"); ?>
 <p>
-  The Zerodha data provider serves asset prices from <a rel='nofollow' target='_blank' href='https://developers.stocknote.com/api/#stocknote-api-documentation'>Zerodha's Kite Connect API</a>. 
+  The Zerodha data provider serves asset prices from <a rel='nofollow' target='_blank' href='https://kite.trade/docs/connect/v3/'>Zerodha's Kite Connect API</a>. 
   This page explains our integration with their API and its functionality.
 </p>
