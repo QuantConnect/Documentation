0b21ba69757e148af2de4c8ddefbf87ce3dea203
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 13:52:53 -0500

    Update 02 Supported Datasets.php

--- 02 Local Platform/09 Datasets/05 Alpha Vantage/02 Supported Datasets.php
+++ 02 Local Platform/09 Datasets/05 Alpha Vantage/02 Supported Datasets.php
@@ -1,7 +1,6 @@
 <p>
-	The Alpha Vantage data provider serves asset price data directly from Alpha Vantage. 
+	The Alpha Vantage data provider serves asset price data directly from <a href='https://www.alphavantage.co/documentation/#time-series-data' rel='nofollow' target='_blank'>Alpha Vantage's Time Series Stock Data APIs</a>. 
 	Our integration supports <a href="https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/us-equity">US Equity</a> securities.
 	It only provides the security price data, so you need to <a href='/datasets/quantconnect-us-equity-security-master/cli'>download the US Equity Security Master with the CLI</a>. 
-	For more information about the Alpha Vantage data source, see the <a rel='nofollow' target='_blank' href='https://www.alphavantage.co/documentation/'>Alpha Vantage documentation</a>.
 </p>
 
