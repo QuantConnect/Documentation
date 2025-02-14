0fea263c89ad23b93c8dbb423638bd352f6298c1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 11:51:21 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/14 TradeStation/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/14 TradeStation/01 Introduction.php
@@ -1,2 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/tradestation.html"); ?>
-<p>The TradeStation data feeds are streams of Equity, Equity Option, and Futures prices directly from TradeStation. If you use this data feed and request historical data, the historical data comes from TradeStation.</p>
+<p>
+  The TradeStation data provider serves Equity, Equity Option, and Futures prices directly from <a rel='nofollow' target='_blank' href='https://api.tradestation.com/docs/'>TradeStation's API</a>. 
+  This page explains our integration with their API and its functionality.
+</p>
