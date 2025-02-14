68269ebc243e0393e463042a69aff0ed2a6cc2c2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-14 16:20:54 -0500

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/12 Polygon/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/12 Polygon/01 Introduction.php
@@ -1,7 +1,7 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/polygon.html"); ?>
 
 <p>
-  The Polygon data provider serves live asset prices from Polygon. 
+  The Polygon data provider serves asset prices from Polygon. 
   Instead of using the data from QuantConnect or your brokerage, you can use data from <a rel='nofollow' target='_blank' href='https://polygon.io/'>Polygon</a> if you're deploying a live algorithm and have an API key. 
   To get an API key, see the <a href='https://polygon.io/dashboard/api-keys' rel='nofollow' target='_blank'>API Keys</a> page on the Polygon website.
   This page explains our integration with Polygon and its functionality.
