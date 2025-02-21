2fa4fee7fb395752c7b326086a39fdd6a5c9a517
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-21 15:27:56 -0500

    Update 03 Data Points.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/03 Data Points.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/03 Data Points.php
@@ -111,9 +111,8 @@ roc = history.pct_change().iloc[1:]</pre>
 
 
 <p class='python'>
-  If you request a DataFrame, LEAN unpacks the data from <code>Slice</code> objects to populate the DataFrame. 
   If you intend to use the data in the DataFrame to create <code><span class='placeholder-text'>alternativeDataClass</span></code> objects, request that the history request returns the data type you need. 
-  Otherwise, LEAN will consume computational resources populating the DataFrame.  
+  Otherwise, LEAN consumes unnecessary computational resources populating the DataFrame.  
   To get a list of dataset objects instead of a DataFrame, call the <code>history[<span class='placeholder-text'>alternativeDataClass</span>]</code> method.
 </p>
 
