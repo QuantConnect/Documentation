709d8c1caea89d450d537861217bd3d119d8edc3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-21 13:29:31 -0500

    Update 04 Ticks.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/04 Ticks.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/01 US Equities/04 Ticks.php
@@ -38,14 +38,144 @@ $dataType = "Tick";
         history = self.history(symbol, timedelta(2), Resolution.TICK)</pre>
 </div>
 
-<img class='python docs-image' src='<?=$imgLink?>' alt='DataFrame of tick data for an asset.'>
+<table border="1" class="dataframe python">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th></th>
+      <th>askprice</th>
+      <th>asksize</th>
+      <th>bidprice</th>
+      <th>bidsize</th>
+      <th>exchange</th>
+      <th>lastprice</th>
+      <th>quantity</th>
+    </tr>
+    <tr>
+      <th>symbol</th>
+      <th>time</th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th rowspan="5" valign="top">SPY</th>
+      <th>2024-12-17 09:30:00.000214</th>
+      <td>0.00</td>
+      <td>0.0</td>
+      <td>604.17</td>
+      <td>1000.0</td>
+      <td>NASDAQ</td>
+      <td>604.17</td>
+      <td>0.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.000214</th>
+      <td>604.19</td>
+      <td>1900.0</td>
+      <td>0.00</td>
+      <td>0.0</td>
+      <td>ARCA</td>
+      <td>604.19</td>
+      <td>0.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001110</th>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>NYSE</td>
+      <td>604.19</td>
+      <td>100.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001480</th>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>NaN</td>
+      <td>ARCA</td>
+      <td>604.19</td>
+      <td>69.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001482</th>
+      <td>0.00</td>
+      <td>0.0</td>
+      <td>604.17</td>
+      <td>1000.0</td>
+      <td>NASDAQ</td>
+      <td>604.17</td>
+      <td>0.0</td>
+    </tr>
+  </tbody>
+</table>
+
 
 <div class="python section-example-container">
     <pre class="python"># Select the rows in the DataFrame that represent trades. Drop the bid/ask columns since they are NaN.
 trade_ticks = history[history.quantity > 0].dropna(axis=1)</pre>
 </div>
 
-<img class='python docs-image' src='https://cdn.quantconnect.com/i/tu/us-equity-trade-tick-dataframe.png' alt='DataFrame of trade tick data for an asset.'>
+<table border="1" class="dataframe python">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th></th>
+      <th>exchange</th>
+      <th>lastprice</th>
+      <th>quantity</th>
+    </tr>
+    <tr>
+      <th>symbol</th>
+      <th>time</th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th rowspan="5" valign="top">SPY</th>
+      <th>2024-12-17 09:30:00.001110</th>
+      <td>NYSE</td>
+      <td>604.19</td>
+      <td>100.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001480</th>
+      <td>ARCA</td>
+      <td>604.19</td>
+      <td>69.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001483</th>
+      <td>ARCA</td>
+      <td>604.19</td>
+      <td>100.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001483</th>
+      <td>ARCA</td>
+      <td>604.19</td>
+      <td>231.0</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 09:30:00.001564</th>
+      <td>NASDAQ</td>
+      <td>604.19</td>
+      <td>100.0</td>
+    </tr>
+  </tbody>
+</table>
+
 
 <p class='python'>
   If you request a DataFrame, LEAN unpacks the data from <code>Slice</code> objects to populate the DataFrame. 
