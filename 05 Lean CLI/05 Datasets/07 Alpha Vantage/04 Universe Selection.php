e10111f4753eecd7200d0257c2a8012e0d0049ab
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-02-25 23:15:58 +0000

    Fixes Broken Links

--- 05 Lean CLI/05 Datasets/07 Alpha Vantage/04 Universe Selection.php
+++ 05 Lean CLI/05 Datasets/07 Alpha Vantage/04 Universe Selection.php
@@ -1,5 +1,5 @@
 <p>
-  <a href='/docs/v2/writing-algorithms/universes/equity'>Universe selection</a> is available with the Alpha Vantage data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
+  <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/universes/equity'>Universe selection</a> is available with the Alpha Vantage data provider if you download the data from the <a href='/datasets'>Dataset Market</a>. 
   The dataset listings show how to download the universe selection data with the CLI. 
   For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. 
   For example, the following tutorials explain how to download historical data and download daily updates:
--- 05 Lean CLI/05 Datasets/12 Terminal Link/04 Universe Selection.php
+++ 05 Lean CLI/05 Datasets/12 Terminal Link/04 Universe Selection.php
@@ -1,5 +1,5 @@
 <p>
-  In local deployments, <a href='/docs/v2/writing-algorithms/universes/key-concepts'>universe selection</a> is available with Terminal Link if you download the data from the <a href='/datasets'>Dataset Market</a>. 
+  In local deployments, <a href='https://www.quantconnect.com/docs/v2/writing-algorithms/universes/key-concepts'>universe selection</a> is available with Terminal Link if you download the data from the <a href='/datasets'>Dataset Market</a>. 
   The dataset listings show how to download the universe selection data with the CLI. 
   For live trading, you'll need to periodically download the new data from QuantConnect Cloud, which you can automate with Python scripts. 
   For example, the following tutorials explain how to download historical data and download daily updates:
