3d91e8fca373bca408eb28e7910e2f301df5afd2
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-27 18:42:03 -0500

    Update 04 Ticks.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/03 Crypto/04 Ticks.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/03 Crypto/04 Ticks.php
@@ -2,7 +2,7 @@
 $symbolC = "AddEquity(\"SPY\", market: Market.Bitfinex).Symbol";
 $symbolPy = "self.add_equity('SPY', market=Market.BITFINEX).symbol";
 $assetClass = "Crypto";
-$dataTypeLink = "/docs/v2/writing-algorithms/securities/asset-classes/us-equity/handling-data#05-Ticks";
+$dataTypeLink = "/docs/v2/writing-algorithms/securities/asset-classes/crypto/handling-data#05-Ticks";
 $dataType = "Tick";
 $dataFrame = "<div class='dataframe-wrapper'>
 <table class='dataframe python'>
