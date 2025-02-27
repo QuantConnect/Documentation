70fbe266deffb973f3943c560f5f6d2d805fc720
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-27 17:48:58 -0500

    Create quotebars.php

---
+++ Resources/history/quotebars.php
@@ -0,0 +1,63 @@
+<p class='csharp'>
+  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>History&lt;<?=$dataType?>&gt;</code> method with an asset's <code>Symbol</code>.
+</p>
+
+<p class='python'>
+  To get historical <a href='<?=$dataTypeLink?>'>quote data</a>, call the <code>history</code> method with the <code><?=$dataType?></code> type and an asset's <code>Symbol</code>.
+  This method returns a DataFrame with columns for the open, high, low, close, and size of the bid and ask quotes.
+  The columns that don't start with "bid" or "ask" are the mean of the quote prices on both sides of the market.
+</p>
+
+<div class="section-example-container">
+    <pre class="csharp">public class <?=$assetClass?><?=$dataType?>Algorithm : QCAlgorithm
+{
+    public override void Initialize()
+    {
+        SetStartDate(2024, 12, 19);
+        // Get the Symbol of an asset.
+        var symbol = <?=$symbolC?>;
+        // Get the 5 trailing minute <?=$dataType?> objects of the asset. 
+        var history = History&lt;<?=$dataType?>&gt;(symbol, 5, Resolution.Minute);
+        // Iterate through the QuoteBar objects and calculate the spread.
+        foreach (var bar in history)
+        {
+            var t = bar.EndTime;
+            var spread = bar.Ask.Close - bar.Bid.Close;
+        }
+    }
+}</pre>
+    <pre class="python">class <?=$assetClass?><?=$dataType?>Algorithm(QCAlgorithm):
+
+    def initialize(self) -> None:
+        self.set_start_date(2024, 12, 19)
+        # Get the Symbol of an asset.
+        symbol = <?=$symbolPy?>
+
+        # Get the 5 trailing minute <?=$dataType?> objects of the asset in DataFrame format. 
+        history = self.history(<?=$dataType?>, symbol, 5, Resolution.MINUTE)</pre>
+</div>
+
+<?=$dataFrame?>
+
+<div class="python section-example-container">
+    <pre class="python"># Calculate the spread at each minute.
+spread = history.askclose - history.bidclose</pre>
+</div>
+
+<div class="python section-example-container">
+    <pre><?=$series?></pre>
+</div>
+
+<p class='python'>
+  If you intend to use the data in the DataFrame to create <code><?=$dataType?></code> objects, request that the history request returns the data type you need. 
+  Otherwise, LEAN consumes unnecessary computational resources populating the DataFrame.  
+  To get a list of <code><?=$dataType?></code> objects instead of a DataFrame, call the <code>history[<?=$dataType?>]</code> method.
+</p>
+
+<div class="python section-example-container">
+    <pre class="python"># Get the 5 trailing minute <?=$dataType?> objects of an asset in <?=$dataType?> format. 
+history = self.history[<?=$dataType?>](symbol, 5, Resolution.MINUTE)
+# Iterate through each QuoteBar and calculate the dollar volume on the bid.
+for quote_bar in history:
+    bid_dollar_volume = quote_bar.last_bid_size * quote_bar.bid.close</pre>
+</div>
