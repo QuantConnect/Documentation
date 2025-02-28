b8dbd0bffd5e9233266c425c7e898cd76e41257e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 16:10:51 -0500

    Update 03 Indicators.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/08 Index/03 Indicators.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/08 Index/03 Indicators.php
@@ -1,1 +1,14 @@
 
+<?
+$symbolC = "AddIndex(\"SPX\").Symbol";
+$targetSymbolC = "AddIndex(\"NDX\").Symbol";
+$symbolPy = "self.add_index('SPX').symbol";
+$targetSymbolPy = "self.add_index('NDX').symbol";
+$assetClass = "Index";
+$supportsTradeData = true;
+$dataFrame = "<div class='dataframe-wrapper'>
+
+</div>";
+
+include(DOCS_RESOURCES."/history/indicators.php");
+?>
