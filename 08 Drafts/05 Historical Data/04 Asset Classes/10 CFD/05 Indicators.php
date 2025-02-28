b349c279ee5bd7d073bbfe6549c3345c27c7e680
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 17:19:35 -0500

    Update 05 Indicators.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/10 CFD/05 Indicators.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/10 CFD/05 Indicators.php
@@ -1,1 +1,13 @@
+<?
+$symbolC = "AddCfd(\"SPX500USD\").Symbol";
+$targetSymbolC = "AddCfd(\"XAUUSD\").Symbol";
+$symbolPy = "self.add_cfd('SPX500USD').symbol";
+$targetSymbolPy = "self.add_cfd('XAUUSD').symbol";
+$assetClass = "CFD";
+$supportsTradeData = false;
+$dataFrame = "<div class='dataframe-wrapper'>
 
+</div>";
+
+include(DOCS_RESOURCES."/history/indicators.php");
+?>
