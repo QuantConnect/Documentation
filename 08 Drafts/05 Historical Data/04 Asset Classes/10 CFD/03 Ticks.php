b98af5bf7271ddf30cfdb6e919ed2c42541c264d
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 16:50:28 -0500

    Update 03 Ticks.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/10 CFD/03 Ticks.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/10 CFD/03 Ticks.php
@@ -1,1 +1,15 @@
+<?
+$symbolC = "AddCfd(\"XAUUSD\").Symbol";
+$symbolPy = "self.add_cfd('XAUUSD').symbol";
+$assetClass = "CFD";
+$dataTypeLink = "/docs/v2/writing-algorithms/securities/asset-classes/cfd/handling-data#03-Ticks";
+$dataType = "Tick";
+$supportsTradeData = false;
+$dataFrame = "<div class='dataframe-wrapper'>
 
+</div>";
+
+$series = "";
+
+include(DOCS_RESOURCES."/history/ticks.php");
+?>
