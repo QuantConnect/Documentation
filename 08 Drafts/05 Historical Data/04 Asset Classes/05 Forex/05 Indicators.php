780167e6299e789dc281f6e543e433df14cef945
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 15:29:51 -0500

    Update 05 Indicators.php

--- 08 Drafts/05 Historical Data/04 Asset Classes/05 Forex/05 Indicators.php
+++ 08 Drafts/05 Historical Data/04 Asset Classes/05 Forex/05 Indicators.php
@@ -1,1 +1,13 @@
+<?
+$symbolC = "AddForex(\"EURUSD\").Symbol";
+$targetSymbolC = "AddForex(\"AUDUSD\").Symbol";
+$symbolPy = "self.add_forex('EURUSD').symbol";
+$targetSymbolPy = "self.add_forex('AUDUSD').symbol";
+$assetClass = "Forex";
+$supportsTradeData = false;
+$dataFrame = "<div class='dataframe-wrapper'>
 
+</div>";
+
+include(DOCS_RESOURCES."/history/indicators.php");
+?>
