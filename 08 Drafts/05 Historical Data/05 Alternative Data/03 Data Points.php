7415ee32e15b981a86d42809db01e9443627567c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 17:52:20 -0500

    Update 03 Data Points.php

--- 08 Drafts/05 Historical Data/05 Alternative Data/03 Data Points.php
+++ 08 Drafts/05 Historical Data/05 Alternative Data/03 Data Points.php
@@ -164,121 +164,264 @@ for data_point in history:
 }</pre>
   </div>
 
-  <div class="dataframe-wrapper">
-    <table class="dataframe python">
-      <thead>
-        <tr style="text-align: right;">
-          <th></th>
-          <th></th>
-          <th>agencies</th>
-          <th>alerttype</th>
-          <th>...</th>
-          <th>time</th>
-          <th>title</th>
-        </tr>
-        <tr>
-          <th>time</th>
-          <th>symbol</th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th rowspan="11" valign="top">2024-12-20</th>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Experian Information Solutions, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Transunion Intermediate Holdings, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Transunion Intermediate Holdings, Inc.</td>
-        </tr>
-        <tr>
-          <th>...</th>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-        <tr>
-          <th>REG.RegalyticsRegulatoryArticles</th>
-          <td>[Consumer Financial Protection Bureau]</td>
-          <td>Complaint</td>
-          <td>...</td>
-          <td>2024-12-21</td>
-          <td>Complaint Filed: Equifax, Inc.</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+<div class="dataframe-wrapper">
+ <table class="dataframe python">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th></th>
+      <th>agencies</th>
+      <th>alerttype</th>
+      <th>announcementurl</th>
+      <th>classification</th>
+      <th>createdat</th>
+      <th>federalregisternumber</th>
+      <th>filingtype</th>
+      <th>id</th>
+      <th>latestupdate</th>
+      <th>originalpublicationdate</th>
+      <th>sector</th>
+      <th>states</th>
+      <th>status</th>
+      <th>summary</th>
+      <th>time</th>
+      <th>title</th>
+    </tr>
+    <tr>
+      <th>time</th>
+      <th>symbol</th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th rowspan="11" valign="top">2024-12-20</th>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11178809</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:33:07.165112</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117880920241216</td>
+      <td>2024-12-18</td>
+      <td>2024-12-16</td>
+      <td>[[[id, 6], [name, Employment]], [[id, 1], [name, Financial]], [[id, 15], [name, Retail]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 16, 2024, via web with the following details: consumer's state: NY; consumer's zip: 10457; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: incorrect information on your report; sub-issue: information belongs to someone else; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11178808</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:33:06.409436</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117880820241216</td>
+      <td>2024-12-18</td>
+      <td>2024-12-16</td>
+      <td>[[[id, 6], [name, Employment]], [[id, 1], [name, Financial]], [[id, 15], [name, Retail]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Experian Information Solutions, Inc., on December 16, 2024, via web with the following details: consumer's state: TX; consumer's zip: 76011; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: incorrect information on your report; sub-issue: account information incorrect; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Experian Information Solutions, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11178805</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:33:04.012876</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117880520241216</td>
+      <td>2024-12-18</td>
+      <td>2024-12-16</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 16, 2024, via web with the following details: consumer's state: FL; consumer's zip: 33056; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11178813</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:33:09.563813</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117881320241216</td>
+      <td>2024-12-18</td>
+      <td>2024-12-16</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Transunion Intermediate Holdings, Inc., on December 16, 2024, via web with the following details: consumer's state: TX; consumer's zip: 77048; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Transunion Intermediate Holdings, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11178811</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:33:08.745803</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117881120241216</td>
+      <td>2024-12-18</td>
+      <td>2024-12-16</td>
+      <td>[[[id, 23], [name, Accounting]], [[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Transunion Intermediate Holdings, Inc., on December 16, 2024, via web with the following details: consumer's state: NY; consumer's zip: 11419; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: incorrect information on your report; sub-issue: information belongs to someone else; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Transunion Intermediate Holdings, Inc.</td>
+    </tr>
+    <tr>
+      <th>...</th>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11175965</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:20:30.693391</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117596520241217</td>
+      <td>2024-12-18</td>
+      <td>2024-12-17</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 17, 2024, via web with the following details: consumer's state: TX; consumer's zip: 78130; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11175956</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:20:29.935351</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117595620241217</td>
+      <td>2024-12-18</td>
+      <td>2024-12-17</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 17, 2024, via web with the following details: consumer's state: FL; consumer's zip: 33304; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11175970</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:20:31.513255</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117597020241217</td>
+      <td>2024-12-18</td>
+      <td>2024-12-17</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 17, 2024, via web with the following details: consumer's state: WA; consumer's zip: 98373; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: credit inquiries on your report that you don't recognize; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11175973</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:20:33.124772</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117597320241217</td>
+      <td>2024-12-18</td>
+      <td>2024-12-17</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 17, 2024, via web with the following details: consumer's state: NJ; consumer's zip: 08846; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+    <tr>
+      <th>REG.RegalyticsRegulatoryArticles</th>
+      <td>[Consumer Financial Protection Bureau]</td>
+      <td>Complaint</td>
+      <td>https://www.consumerfinance.gov/data-research/consumer-complaints/search/detail/11175972</td>
+      <td>Federal</td>
+      <td>2024-12-19 04:20:32.334464</td>
+      <td>None</td>
+      <td>Single</td>
+      <td>19372CFPB1117597220241217</td>
+      <td>2024-12-18</td>
+      <td>2024-12-17</td>
+      <td>[[[id, 1], [name, Financial]], [[id, 15], [name, Retail]], [[id, 22], [name, Taxes]]]</td>
+      <td>[]</td>
+      <td>New</td>
+      <td>The Consumer Financial Protection Bureau received a complaint against Equifax, Inc., on December 17, 2024, via web with the following details: consumer's state: DE; consumer's zip: 19802; product: credit reporting or other personal consumer reports; sub-product: credit reporting; issue: improper use of your report; sub-issue: reporting company used your report improperly; timely response: yes; company response to consumer: in progress; company public response: N/A.</td>
+      <td>2024-12-21</td>
+      <td>Complaint Filed: Equifax, Inc.</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 
 
   <div class="python section-example-container">
