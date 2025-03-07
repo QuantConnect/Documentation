7b8c11d0dd42e325c8cbe26e70c5b186cf89ecb4
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-07 18:03:43 -0500

    Create 07 Indicators.php

---
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/04 Crypto Futures/07 Indicators.php
@@ -0,0 +1,48 @@
+<?
+$symbolC = "AddCryptoFuture(\"BTCUSD\").Symbol";
+$targetSymbolC = "AddCryptoFuture(\"DOGEUSD\").Symbol";
+$symbolPy = "self.add_crypto_future('BTCUSD').symbol";
+$targetSymbolPy = "self.add_crypto('DOGEUSD').symbol";
+$assetClass = "CryptoFuture";
+$supportsTradeData = true;
+$dataFrame = "<div class='dataframe-wrapper'>
+<table class='dataframe python'>
+  <thead>
+    <tr style='text-align: right;'>
+      <th></th>
+      <th>current</th>
+      <th>rollingsum</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>2024-12-15</th>
+      <td>97759.090476</td>
+      <td>2052940.9</td>
+    </tr>
+    <tr>
+      <th>2024-12-16</th>
+      <td>98065.342857</td>
+      <td>2059372.2</td>
+    </tr>
+    <tr>
+      <th>2024-12-17</th>
+      <td>98689.038095</td>
+      <td>2072469.8</td>
+    </tr>
+    <tr>
+      <th>2024-12-18</th>
+      <td>99363.738095</td>
+      <td>2086638.5</td>
+    </tr>
+    <tr>
+      <th>2024-12-19</th>
+      <td>99563.576190</td>
+      <td>2090835.1</td>
+    </tr>
+  </tbody>
+</table>
+</div>";
+
+include(DOCS_RESOURCES."/history/indicators.php");
+?>
