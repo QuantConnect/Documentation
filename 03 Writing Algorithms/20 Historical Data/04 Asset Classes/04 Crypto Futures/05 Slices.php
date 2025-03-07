67c19572bb1cd816d5f5cddcd076a3bc7264eb50
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-07 16:36:44 -0500

    Create 05 Slices.php

---
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/04 Crypto Futures/05 Slices.php
@@ -0,0 +1,7 @@
+<?
+$symbolC = "AddCryptoFuture(\"BTCUSD\").Symbol";
+$symbolPy = "self.add_crypto_future('BTCUSD').symbol";
+$dataType = "TradeBar";
+
+include(DOCS_RESOURCES."/history/slices.php");
+?>
