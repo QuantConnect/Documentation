150e9fcff34684b9fce883b4d32fb7012a3227f3
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-14 11:47:44 -0500

    Create 04 Slices.php

---
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/02 Equity Options/04 Slices.php
@@ -0,0 +1,12 @@
+<?
+$symbolC = "var equity = AddEquity(\"SPY\", dataNormalizationMode: DataNormalizationMode.Raw);
+        var contract = OptionChain(equity.Symbol).OrderBy(c => c.OpenInterest).Last();
+        AddOptionContract(contract.Symbol);
+";
+$symbolPy = "equity = self.add_equity('SPY', data_normalization_mode=DataNormalizationMode.RAW)
+        contract = sorted(self.option_chain(equity.symbol), key=lambda c: c.open_interest)[-1]
+        self.add_option_contract(contract.symbol)";
+$dataType = "TradeBar";
+
+include(DOCS_RESOURCES."/history/slices.php");
+?>
