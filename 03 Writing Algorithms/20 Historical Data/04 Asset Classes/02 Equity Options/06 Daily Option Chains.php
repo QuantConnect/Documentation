f723e9fbfcc28b77e70f1efde8b74e919f391820
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-14 15:34:22 -0500

    Update 06 Daily Option Chains.php

--- 03 Writing Algorithms/20 Historical Data/04 Asset Classes/02 Equity Options/06 Daily Option Chains.php
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/02 Equity Options/06 Daily Option Chains.php
@@ -1,47 +1,12 @@
-<p class='csharp'>
-  To get historical <a href="/docs/v2/writing-algorithms/universes/equity-options#08-Navigate-Daily-Option-Chains">daily Option chain data</a>, call the <code>History</code> method with the Option <code>Symbol</code> object.
-  The data this method returns contains information on all the currently tradable contracts, not just the contracts that pass your filter.
-</p>
-
-<p class='python'>
-  To get historical <a href="/docs/v2/writing-algorithms/universes/equity-options#08-Navigate-Daily-Option-Chains">daily Option chain data</a>, call the <code>history</code> method with the Option <code>Symbol</code> object.
-  The data this method returns contains information on all the currently tradable contracts, not just the contracts that pass your filter.
-  If you pass <code>flatten=True</code>, this method returns a DataFrame with columns for the data point attributes.
-</p>
-
-<div class="section-example-container">
-    <pre class="csharp">public class EquityOpionDailyOptionChainHistoryAlgorithm : QCAlgorithm
-{
-    public override void Initialize()
-    {
-        SetStartDate(2024, 12, 23);
-        // Add an Equity Option universe.
-        var option = AddOption("SPY");
-        // Get the trailing 5 daily Option chains.
-        var history = History&lt;OptionUniverse&gt;(option.Symbol, 5);
-        // Iterate through each day of the history.
-        foreach (var optionUniverse in history)
-        {
-            var t = optionUniverse.EndTime;
-            // Select the 2 contracts with the most volume.
-            var mostTraded = optionUniverse.Select(c => c as OptionUniverse).OrderByDescending(c => c.Volume).Take(2);
-        }
-    }
-}</pre>
-    <pre class="python">class EquityOpionDailyOptionChainHistoryAlgorithm(QCAlgorithm):
-
-    def initialize(self) -> None:
-        self.set_start_date(2024, 12, 23)
-        # Add an Equity Option universe.
-        option = self.add_option('SPY')
-        # Get the trailing 5 daily Option chains in DataFrame format.
-        history = self.history(universe, 5, flatten=True)</pre>
-</div>
-
-<div class="dataframe-wrapper">
-<table class="dataframe python">
+<?
+$optionC = "AddOption(\"SPY\")";
+$optionPy = "self.add_option('SPY')";
+$underlyingAssetClass = "Equity";
+$dataTypeLink = "/docs/v2/writing-algorithms/universes/equity-options#08-Navigate-Daily-Option-Chains";
+$dataFrame = "<div class='dataframe-wrapper'>
+<table class='dataframe python'>
   <thead>
-    <tr style="text-align: right;">
+    <tr style='text-align: right;'>
       <th></th>
       <th></th>
       <th>close</th>
@@ -80,7 +45,7 @@
   </thead>
   <tbody>
     <tr>
-      <th rowspan="5" valign="top">2024-12-13</th>
+      <th rowspan='5' valign='top'>2024-12-13</th>
       <th>SPY YOGVNNCO8QDI|SPY R735QTJ8XC9X</th>
       <td>484.815</td>
       <td>1.000000</td>
@@ -184,7 +149,7 @@
       <td>...</td>
     </tr>
     <tr>
-      <th rowspan="5" valign="top">2024-12-19</th>
+      <th rowspan='5' valign='top'>2024-12-19</th>
       <th>SPY 33899RRUZK23Q|SPY R735QTJ8XC9X</th>
       <td>314.995</td>
       <td>-0.865013</td>
@@ -271,16 +236,9 @@
     </tr>
   </tbody>
 </table>
-</div>
+</div>";
 
-
-<div class="python section-example-container">
-    <pre class="python"># Select the 2 contracts with the greatest volume each day.
-most_traded = history.groupby('time').apply(lambda x: x.nlargest(2, 'volume')).reset_index(level=1, drop=True).volume</pre>
-</div>
-
-<div class="python section-example-container">
-    <pre>time        symbol                            
+$series = "time        symbol                            
 2024-12-13  SPY 32NDZOIHRHLRA|SPY R735QTJ8XC9X    166689.0
             SPY 32NDZOIHXFXT2|SPY R735QTJ8XC9X    160265.0
 2024-12-14  SPY YOCXVCLWHPT2|SPY R735QTJ8XC9X     137597.0
@@ -291,19 +249,7 @@ most_traded = history.groupby('time').apply(lambda x: x.nlargest(2, 'volume')).r
             SPY YOEWRJCELK6E|SPY R735QTJ8XC9X      75769.0
 2024-12-19  SPY 32NKVT60AHJDY|SPY R735QTJ8XC9X    109298.0
             SPY YOFW7MPKOBC6|SPY R735QTJ8XC9X      94823.0
-Name: volume, dtype: float64</pre>
-</div>
+Name: volume, dtype: float64";
 
-<p class='python'>
-  To get the data in the format of <code>OptionUniverse</code> objects instead of a DataFrame, call the <code>history[OptionUniverse]</code> method.
-</p>
-
-<div class="python section-example-container">
-    <pre class="python"># Get the historical OptionUniverse data over the last 30 days.
-history = self.history[OptionUniverse](option.symbol, timedelta(30))
-# Iterate through each daily Option chain.
-for option_universe in history:
-    t = option_universe.end_time
-    # Select the contract with the most volume.
-    most_traded = sorted(option_universe, key=lambda contract: contract.volume)[-1]</pre>
-</div>
+include(DOCS_RESOURCES."/history/daily_option_chains.php");
+?>
