dda842385e6ebb80b06a649cbdadb02f7f582d88
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-14 15:14:32 -0500

    Update 04 Slices.php

--- 03 Writing Algorithms/20 Historical Data/04 Asset Classes/09 Index Options/04 Slices.php
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/09 Index Options/04 Slices.php
@@ -1,1 +1,11 @@
+<?
+$symbolC = "var index = AddIndex(\"SPX\");
+        var contract = OptionChain(index.Symbol).OrderBy(c => c.OpenInterest).Last();
+        AddOptionContract(contract.Symbol);";
+$symbolPy = "index = self.add_index('SPX')
+        contract = sorted(self.option_chain(index.symbol), key=lambda c: c.open_interest)[-1]
+        self.add_option_contract(contract.symbol)";
+$dataType = "TradeBar";
 
+include(DOCS_RESOURCES."/history/slices.php");
+?>
