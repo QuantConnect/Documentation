b7eadbdffda3518d2a1a78f2aba5b856b91e1eb5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-21 16:26:04 -0500

    Update 07 Indicators.php

--- 03 Writing Algorithms/20 Historical Data/04 Asset Classes/09 Index Options/07 Indicators.php
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/09 Index Options/07 Indicators.php
@@ -1,1 +1,46 @@
+<?
+$underlyingSymbolC = "AddIndex(\"SPX\").Symbol";
+$underlyingSymbolPy = "self.add_index('SPX').symbol";
+$assetClass = "IndexOption";
+$indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/index-options/greeks-and-implied-volatility/indicators";
+$dataFrame = "<div class='dataframe-wrapper'>
+<table class='dataframe python'>
+  <thead>
+    <tr style='text-align: right;'>
+      <th></th>
+      <th>current</th>
+      <th>rollingsum</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>2024-12-12 15:15:00</th>
+      <td>0.617857</td>
+      <td>12.975</td>
+    </tr>
+    <tr>
+      <th>2024-12-13 15:15:00</th>
+      <td>0.580952</td>
+      <td>12.200</td>
+    </tr>
+    <tr>
+      <th>2024-12-16 15:15:00</th>
+      <td>0.535714</td>
+      <td>11.250</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 15:15:00</th>
+      <td>0.477381</td>
+      <td>10.025</td>
+    </tr>
+    <tr>
+      <th>2024-12-18 15:15:00</th>
+      <td>0.472619</td>
+      <td>9.925</td>
+    </tr>
+  </tbody>
+</table>
+</div>";
 
+include(DOCS_RESOURCES."/history/option_indicators.php");
+?>
