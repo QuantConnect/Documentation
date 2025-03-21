8c54dea08d9817201b56a05513bed2321159d868
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-21 16:19:16 -0500

    Update 07 Indicators.php

--- 03 Writing Algorithms/20 Historical Data/04 Asset Classes/02 Equity Options/07 Indicators.php
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/02 Equity Options/07 Indicators.php
@@ -4,42 +4,42 @@ $underlyingSymbolPy = "self.add_equity('SPY').symbol";
 $assetClass = "EquityOption";
 $indicatorLink = "/docs/v2/writing-algorithms/securities/asset-classes/equity-options/greeks-and-implied-volatility/indicators";
 $dataFrame = "<div class='dataframe-wrapper'>
-  <table class='dataframe python'>
-    <thead>
-      <tr style='text-align: right;'>
-        <th></th>
-        <th>current</th>
-        <th>rollingsum</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th>2024-12-12 16:00:00</th>
-        <td>599.186667</td>
-        <td>12582.92</td>
-      </tr>
-      <tr>
-        <th>2024-12-13 16:00:00</th>
-        <td>599.520952</td>
-        <td>12589.94</td>
-      </tr>
-      <tr>
-        <th>2024-12-16 16:00:00</th>
-        <td>600.160952</td>
-        <td>12603.38</td>
-      </tr>
-      <tr>
-        <th>2024-12-17 16:00:00</th>
-        <td>601.043810</td>
-        <td>12621.92</td>
-      </tr>
-      <tr>
-        <th>2024-12-18 16:00:00</th>
-        <td>600.954762</td>
-        <td>12620.05</td>
-      </tr>
-    </tbody>
-  </table>
+<table class="dataframe python">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>current</th>
+      <th>rollingsum</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>2024-12-12 16:00:00</th>
+      <td>0.377619</td>
+      <td>7.930</td>
+    </tr>
+    <tr>
+      <th>2024-12-13 16:00:00</th>
+      <td>0.366190</td>
+      <td>7.690</td>
+    </tr>
+    <tr>
+      <th>2024-12-16 16:00:00</th>
+      <td>0.353810</td>
+      <td>7.430</td>
+    </tr>
+    <tr>
+      <th>2024-12-17 16:00:00</th>
+      <td>0.336667</td>
+      <td>7.070</td>
+    </tr>
+    <tr>
+      <th>2024-12-18 16:00:00</th>
+      <td>0.356429</td>
+      <td>7.485</td>
+    </tr>
+  </tbody>
+</table>
 </div>";
 
 include(DOCS_RESOURCES."/history/option_indicators.php");
