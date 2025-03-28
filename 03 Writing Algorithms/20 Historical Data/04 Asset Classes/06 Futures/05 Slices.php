51bf29a66c09e8d75fb4569656f90c362a6ec913
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-03-28 17:37:47 -0500

    Update 05 Slices.php

--- 03 Writing Algorithms/20 Historical Data/04 Asset Classes/06 Futures/05 Slices.php
+++ 03 Writing Algorithms/20 Historical Data/04 Asset Classes/06 Futures/05 Slices.php
@@ -1,12 +1,8 @@
 <?
 $symbolC = "var future = AddFuture(Futures.Indices.SP500EMini);
-        var symbol = FutureChainProvider.GetFutureContractList(future.Symbol, Time)
-            .OrderBy(symbol => symbol.ID.Date).First();
-        AddFutureContract(symbol);";
+        var symbol = FuturesChain(future.Symbol).First().Symbol;";
 $symbolPy = "future = self.add_future(Futures.Indices.SP_500_E_MINI)
-        contract_symbols = self.future_chain_provider.get_future_contract_list(future.symbol, self.time)
-        symbol = sorted(contract_symbols, key=lambda symbol: symbol.id.date)[0]
-        self.add_future_contract(symbol)";
+        symbol = list(self.futures_chain(future.symbol))[0].symbol";
 $dataType = "TradeBar";
 
 include(DOCS_RESOURCES."/history/slices.php");
