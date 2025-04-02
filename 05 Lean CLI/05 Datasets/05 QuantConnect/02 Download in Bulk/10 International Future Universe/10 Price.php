77ef2e0a38b374b1dddf546891e7a611f6256d10
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-02 17:12:20 -0600

    Update and rename 10 Price.html to 10 Price.php

---
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/10 International Future Universe/10 Price.php
@@ -0,0 +1,5 @@
+<p>The following table shows the price of an annual subscription to the US Future Security Master for each organization tier:</p>
+
+<? include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
+
+<p>All of the historical International Future Universe data costs $1,200. An annual subscription to daily updates costs $960/year.</p>
