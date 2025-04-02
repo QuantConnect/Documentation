6deb1c68d0f4d5be26b63b82c54239f5f2219573
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-02 16:38:13 -0600

    Update 10 Price.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/09 US Future Universe/10 Price.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/09 US Future Universe/10 Price.php
@@ -2,4 +2,4 @@
 
 <? include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
 
-<p>All of the historical US Equity Option Universe data costs $1,200. An annual subscription to daily updates costs $960/year.</p>
+<p>All of the historical US Future Universe data costs $1,200. An annual subscription to daily updates costs $960/year.</p>
