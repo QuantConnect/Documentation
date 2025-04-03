23f4775baad6334e8e9dd35bb8e2ab023c3ab94c
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 13:09:19 -0600

    Update 10 Price.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/07 US ETF Constituents/10 Price.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/07 US ETF Constituents/10 Price.php
@@ -1,5 +1,1 @@
-<p>The following table shows the price of an annual subscription to the US Equity Security Master for each organization tier:</p>
-
-<?php include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
-
-<p>All of the historical US ETF Constituents data costs $3,960. An annual subscription to daily updates costs $1,200/year.</p>
+<p>To view the prices of the datasets, see <span class='tab-name'>Pricing</span> tab of the <a href='https://www.quantconnect.com/datasets/quantconnect-us-etf-constituents/pricing'>US ETF Constituents</a> and <a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-security-master/pricing'>US Equity Security Master</a> dataset listings.</p>
