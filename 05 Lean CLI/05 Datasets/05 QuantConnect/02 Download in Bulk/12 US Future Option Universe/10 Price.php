50ef05a4d252eeb93b407d3565fc3a479e9d62b8
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 09:40:21 -0600

    Update and rename 10 Price.html to 10 Price.php

---
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/12 US Future Option Universe/10 Price.php
@@ -0,0 +1,7 @@
+<p>The following table shows the price of an annual subscription to the US Future Security Master for each organization tier:</p>
+
+<? include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
+
+<p>All of the historical US Future Option Universe data costs $1,200. An annual subscription to daily updates costs $960/year.</p>
+
+<p>To view the price of the US Future Universe data, see <a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-future-universe#10-Price'>Price</a>.</p>
