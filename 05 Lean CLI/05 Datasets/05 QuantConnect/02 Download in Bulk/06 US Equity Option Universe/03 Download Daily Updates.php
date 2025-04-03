5e8be070b2769955fa55bdc5d8c44c0b66a51c6e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 11:49:25 -0600

    Update 03 Download Daily Updates.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/06 US Equity Option Universe/03 Download Daily Updates.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/06 US Equity Option Universe/03 Download Daily Updates.php
@@ -4,10 +4,12 @@
 	You need <a href="https://www.quantconnect.com/docs/v2/cloud-platform/organizations/members#09-Permissions">billing permissions</a> to change the organization's subscriptions.
 </p>
 
-<p>
-	After you subscribe to dataset updates, to update your local copy of the US Equity Option Universe dataset, use the <a href="https://www.quantconnect.com/datasets/algoseek-us-equity-option-universe/cli">CLI Command Generator</a> to generate your download command and then run it in a terminal in your <a href="https://www.quantconnect.com/docs/v2/lean-cli/initialization/organization-workspaces">organization workspace</a>. 
-    Alternatively, instead of directly calling the <code>lean data download</code> command, you can place the following Python script in the <span class="public-directory-name">data</span> directory of your organization workspace and run it to update your data files. 
-</p>
+<p>After you subscribe to dataset updates, to update your local copy of the US Equity Option Universe dataset, open a terminal in your <a href="https://www.quantconnect.com/docs/v2/lean-cli/initialization/organization-workspaces">organization workspace</a> and then run.</p>
+<div class="cli section-example-container">
+     <pre>$ lean data download --dataset "US Equity Option Universe" --data-type "bulk" --start "20250403" --end "20250403"</pre> 
+</div>
+
+<p>Alternatively, instead of directly calling the <code>lean data download</code> command, you can place the following Python script in the <span class="public-directory-name">data</span> directory of your organization workspace and run it to update your data files.</p>
 
 <?
 $dataset = "US Equity Option Universe";
