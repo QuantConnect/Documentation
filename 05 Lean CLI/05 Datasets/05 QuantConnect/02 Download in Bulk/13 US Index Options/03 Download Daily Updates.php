b80593aa5e0921b6e9f9d26de8d291109a0393f6
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 15:35:35 -0600

    Update 03 Download Daily Updates.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/13 US Index Options/03 Download Daily Updates.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/13 US Index Options/03 Download Daily Updates.php
@@ -1,8 +1,8 @@
 <p>After you bulk download the US Index Options dataset, new daily updates are available at 8 PM Coordinated Universal Time (UTC) two days after each trading day. For example, the minute resolution data for Monday is available on Wednesday at 8 PM UTC. To unlock local access to the data updates, open the <a href="https://www.quantconnect.com/pricing">Pricing</a> page of your organization and subscribe to at least one of the following data packages:</p>
 <ul>
-    <li>US Index Options Daily Updates by AlgoSeek</li>
-    <li>US Index Options Minute Updates by AlgoSeek</li>
-    <li>US Index Options Hour Updates by AlgoSeek</li>
+    <li><span class="button-name">US Index Options Daily Updates by AlgoSeek</span></li>
+    <li><span class="button-name">US Index Options Minute Updates by AlgoSeek</span></li>
+    <li><span class="button-name">US Index Options Hour Updates by AlgoSeek</span></li>
 </ul>
 <p>You need <a href="https://www.quantconnect.com/docs/v2/cloud-platform/organizations/members#09-Permissions">billing permissions</a> to change the organization's subscriptions.</p>
 
@@ -19,3 +19,5 @@ include(DOCS_RESOURCES."/datasets/download_bulk_data_script.php");
 ?>
 
 <p>The preceding script checks the date of the most recent minute resolution data you have for SPX. If there is new minute data available, it downloads the new data files and overwrites your hourly and daily files. If you don't intend to download all resolutions, adjust this script to your needs.</p>
+
+<p>To update your local copy of the US Index Option Universe data, see <a href='/docs/v2/lean-cli/datasets/quantconnect/download-in-bulk/us-index-option-universe#03-Download-Daily-Updates'>Download Daily Updates</a>.</p>
