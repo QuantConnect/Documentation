b2b3b47472f001600c4c65be3213f983fa0988b5
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 13:51:56 -0600

    Update 10 Price.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/10 International Future Universe/10 Price.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/10 International Future Universe/10 Price.php
@@ -1,5 +1,1 @@
-<p>The following table shows the price of an annual subscription to the US Future Security Master for each organization tier:</p>
-
-<? include(DOCS_RESOURCES."/datasets/us-equity-security-master-price.html"); ?>
-
-<p>All of the historical International Future Universe data costs $1,200. An annual subscription to daily updates costs $960/year.</p>
+<p>To view the prices of the datasets, see <span class='tab-name'>Pricing</span> tab of the <a href='https://www.quantconnect.com/datasets/quantconnect-international-future-universe/pricing'>International Future Universe</a> and <a href='https://www.quantconnect.com/datasets/quantconnect-us-futures-security-master/pricing'>US Futures Security Master</a> dataset listings.</p>
