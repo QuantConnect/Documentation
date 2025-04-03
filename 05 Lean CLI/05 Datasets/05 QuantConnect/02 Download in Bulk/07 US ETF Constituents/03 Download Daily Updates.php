7b5d876484fc6b94365f33ef53ce7e029d5256d1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 13:06:35 -0600

    Update 03 Download Daily Updates.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/07 US ETF Constituents/03 Download Daily Updates.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/07 US ETF Constituents/03 Download Daily Updates.php
@@ -6,11 +6,6 @@
      <pre>$ lean data download --dataset "US ETF Constituents" --data-type "Bulk" --start "20090601" --end "20500101"</pre> 
 </div>
 
-<p>To update your local copy of the US Equity Security Master, run:</p>
-<div class="cli section-example-container">
-     <pre>$ lean data download --dataset "US Equity Security Master"</pre> 
-</div>
-
 <p>Alternatively, instead of directly calling the <code>lean data download</code> command, you can place a Python script in the <span class="public-directory-name">data</span> directory of your organization workspace and run it to update your data files. The following example script updates all of the new data that's missing from your local copy:</p>
 
 <?
@@ -20,3 +15,8 @@ include(DOCS_RESOURCES."/datasets/download_bulk_data_script_universe.php");
 ?>
 
 <p>The preceding script checks the date of the most recent SPY data you have. If there is new data available for SPY, it downloads the new data files for all of the ETFs. You may need to adjust this script to fit your needs.</p>
+
+<p>To update your local copy of the US Equity Security Master, run:</p>
+<div class="cli section-example-container">
+     <pre>$ lean data download --dataset "US Equity Security Master"</pre> 
+</div>
