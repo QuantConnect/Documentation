86ec1ee841d091311c56e24f185769a5d5414255
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 14:50:58 -0600

    Update 10 Price.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/05 US Equity Options/10 Price.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/05 US Equity Options/10 Price.php
@@ -1,1 +1,6 @@
-<p>To view the prices of the datasets, see the <span class='tab-name'>Pricing</span> tab of the <a href='https://www.quantconnect.com/datasets/algoseek-us-equity-options/pricing'>US Equity Options</a> and <a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-security-master/pricing'>US Equity Security Master</a> dataset listings.</p>
+<p>To view the prices of the datasets, see the <span class='tab-name'>Pricing</span> tab of the following dataset listings:</p>
+<ul>
+  <li><a href='https://www.quantconnect.com/datasets/algoseek-us-equity-options/pricing'>US Equity Options</a></li>
+  <li><a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-security-master/pricing'>US Equity Security Master</a></li>
+  <li><a href='https://www.quantconnect.com/datasets/quantconnect-us-equity-option-universe/pricing'>US Equity Option Universe</a></li>
+</ul>
