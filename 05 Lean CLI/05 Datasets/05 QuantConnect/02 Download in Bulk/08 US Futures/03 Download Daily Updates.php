26521d972513f9b84dc46e7f7b3460befff11f94
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 13:16:27 -0600

    Update 03 Download Daily Updates.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/08 US Futures/03 Download Daily Updates.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/08 US Futures/03 Download Daily Updates.php
@@ -1,10 +1,10 @@
 <p>After you bulk download the US Futures dataset, new daily updates are available at 7 AM Eastern Time (ET) after each trading day. To unlock local access to the data updates, open the <a href="https://www.quantconnect.com/pricing">Pricing</a> page of your organization and subscribe to at least one of the following data packages:
 </p><ul>
-    <li>US Futures Daily Updates by AlgoSeek</li>
-    <li>US Futures Hour Updates by AlgoSeek</li>
-    <li>US Futures Minute Updates by AlgoSeek</li>
-    <li>US Futures Second Updates by AlgoSeek</li>
-    <li>US Futures Tick Updates by AlgoSeek</li>
+    <li><span class="button-name">US Futures Daily Updates by AlgoSeek</span></li>
+    <li<span class="button-name">>US Futures Hour Updates by AlgoSeek</span></li>
+    <li><span class="button-name">US Futures Minute Updates by AlgoSeek</span></li>
+    <li><span class="button-name">US Futures Second Updates by AlgoSeek</span></li>
+    <li><span class="button-name">US Futures Tick Updates by AlgoSeek</span></li>
 </ul>
 <p>You need <a href="https://www.quantconnect.com/docs/v2/cloud-platform/organizations/members#09-Permissions">billing permissions</a> to change the organization's subscriptions.</p>
 
@@ -21,3 +21,8 @@ include(DOCS_RESOURCES."/datasets/download_bulk_data_script.php");
 ?>
 
 <p>The preceding script checks the date of the most recent ZC data you have from the CBOT market for tick, second, and minute resolutions. If there is new data available for any of these resolutions, it downloads the new data files and overwrites your hourly and daily files. If you don't intend to download all resolutions and markets, adjust this script to your needs.</p>
+
+<p>To update your local copy of the US Futures Security Master, run:</p>
+<div class="cli section-example-container">
+     <pre>$ lean data download --dataset "US Futures Security Master"</pre> 
+</div>
