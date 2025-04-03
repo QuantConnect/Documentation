daac0432201b41625e7e9c33eed3fac67798fa2a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-03 09:49:21 -0600

    Update 03 Download Daily Updates.php

--- 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/02 FOREX Data/03 Download Daily Updates.php
+++ 05 Lean CLI/05 Datasets/05 QuantConnect/02 Download in Bulk/02 FOREX Data/03 Download Daily Updates.php
@@ -1,13 +1,12 @@
 <p>After you bulk download the Forex dataset, new daily updates are available at 3 PM Coordinated Universal Time (UTC)  after each trading day. To gain access to the data updates, open the <a href="https://www.quantconnect.com/pricing">Pricing</a> page of your organization and subscribe to at least one of the following data packages:</p>
 <ul>
-    <li>OANDA Forex - Daily Updates</li>
-    <li>OANDA Forex - Hour Updates</li>
-    <li>OANDA Forex - Minute Updates</li>
-    <li>OANDA Forex - Second Updates</li>
+    <li><span class="button-name">Forex - Daily Updates</span></li>
+    <li><span class="button-name">Forex - Hour Updates</span></li>
+    <li><span class="button-name">Forex - Minute Updates</span></li>
+    <li><span class="button-name">Forex - Second Updates</span></li>
 </ul>
 
 <p>You need <a href="https://www.quantconnect.com/docs/v2/cloud-platform/organizations/members#09-Permissions">billing permissions</a> to change the organization's subscriptions.</p>
-
 
 <p>After you subscribe to dataset updates, to update your local copy of the Forex dataset, use the <a href="https://www.quantconnect.com/datasets/oanda-forex/cli">CLI Command Generator</a> to generate your download command and then run it in a terminal in your <a href="https://www.quantconnect.com/docs/v2/lean-cli/initialization/organization-workspaces">organization workspace</a>. Alternatively, instead of directly calling the <code>lean data download</code> command, you can place a Python script in the <span class="public-directory-name">data</span> directory of your organization workspace and run it to update your data files. The following example script updates all data resolutions:</p>
 
