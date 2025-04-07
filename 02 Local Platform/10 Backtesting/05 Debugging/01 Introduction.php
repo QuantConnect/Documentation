aae0c38ed28abe1f36a64bbc504678a47829697e
Jared <jaredbroad@gmail.com>
2025-04-07 10:03:13 -0400

    Update 01 Introduction.php

--- 02 Local Platform/10 Backtesting/05 Debugging/01 Introduction.php
+++ 02 Local Platform/10 Backtesting/05 Debugging/01 Introduction.php
@@ -1,1 +1,5 @@
 <? include(DOCS_RESOURCES."/debugging/backtest-intro.html"); ?>
+
+<p>
+  Local debugging only works with local projects. Locally debugging cloud projects is not currently supported.
+</p>
