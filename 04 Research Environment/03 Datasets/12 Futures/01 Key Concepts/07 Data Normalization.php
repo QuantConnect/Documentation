438554776fdbbdf0b28f5960e5142170ef2b7030
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-04-11 11:22:13 -0600

    Update 07 Data Normalization.php

--- 04 Research Environment/03 Datasets/12 Futures/01 Key Concepts/07 Data Normalization.php
+++ 04 Research Environment/03 Datasets/12 Futures/01 Key Concepts/07 Data Normalization.php
@@ -1,5 +1,2 @@
-<? $specificAsset = " for Futures contracts"; $additionalInformation = ""; include(DOCS_RESOURCES."/securities/data-normalization-raw-only.php"); ?>
-
 <p>The following data normalization modes are available for <a href='/docs/v2/writing-algorithms/universes/futures#12-Continous-Contracts'>continuous Futures contracts</a>:</p>
-
 <div data-tree='QuantConnect.DataNormalizationMode' data-fields='ForwardPanamaCanal,BackwardsPanamaCanal,BackwardsRatio,FORWARD_PANAMA_CANAL,BACKWARDS_PANAMA_CANAL,BACKWARDS_RATIO,'></div>
