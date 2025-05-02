cd37d842de17b4dd6e25bda70e93e0bab693ebc4
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-05-03 00:08:10 +0100

    Add Information on How to Get API Token to Cloud API

---
+++ 01 Cloud Platform/99 API Reference/01 Authentication/01 Introduction.php
@@ -0,0 +1,8 @@
+<p>
+Make authenticated REST requests to the QuantConnect API with your User-id and API-Token. Use a simple API endpoint to verify the authentication is working correctly. 
+</p>
+<p>The base URL of QuantConnect API is <code>https://www.quantconnect.com/api/v2</code>.</p>
+
+<? include(DOCS_RESOURCES."/getting-started/organizations/get-api-token.html"); ?>
+
+<? include(DOCS_RESOURCES."/getting-started/organizations/get-organizations-id.html"); ?>
