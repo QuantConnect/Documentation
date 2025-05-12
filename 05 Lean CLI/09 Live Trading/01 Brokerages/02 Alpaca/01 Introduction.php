c718c7934fe1d3f035f90cab2a7bce0452695bec
Jared <jaredbroad@gmail.com>
2025-05-12 15:53:19 -0400

    Update 01 Introduction.php

--- 05 Lean CLI/09 Live Trading/01 Brokerages/02 Alpaca/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/02 Alpaca/01 Introduction.php
@@ -3,6 +3,6 @@ $brokerageName="Alpaca";
 include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 ?>
 
-<p>To view the implementation of the Alpaca brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Alpaca' rel='nofollow' target="_blank">Lean.Brokerages.Alpaca repository</a>.</p>
+<p>To view the implementation of the <a href="http://qnt.co/alpaca" target="_BLANK">Alpaca</a> brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Alpaca' rel='nofollow' target="_blank">Lean.Brokerages.Alpaca repository</a>.</p>
 
 <p>To use the CLI, you must be a member in an <a href='https://www.quantconnect.com/docs/v2/cloud-platform/organizations/tier-features'>organization</a> on a paid tier.</p>
