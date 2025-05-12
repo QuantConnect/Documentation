d23064c41a86de267fb1653c93c17818f46754a8
Jared <jaredbroad@gmail.com>
2025-05-12 15:58:18 -0400

    Update 01 Introduction.php

--- 01 Cloud Platform/09 Datasets/08 Alpaca/01 Introduction.php
+++ 01 Cloud Platform/09 Datasets/08 Alpaca/01 Introduction.php
@@ -1,5 +1,5 @@
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/alpaca.html"); ?>
 <p>
-  The Alpaca data provider serves Equity, Equity Option, and Crypto prices directly from <a rel='nofollow' target='_blank' href='https://docs.alpaca.markets/docs/about-market-data-api'>Alpaca's Market Data API</a>. 
+  The <a rel='nofollow' target='_blank' href='http://qnt.co/alpaca'>Alpaca</a> data provider serves Equity, Equity Option, and Crypto prices directly from <a rel='nofollow' target='_blank' href='https://docs.alpaca.markets/docs/about-market-data-api'>Alpaca's Market Data API</a>. 
   This page explains our integration with their API and its functionality.
 </p>
