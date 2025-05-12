86626a6748769290105ad88af6ad56cb5104221e
Jared <jaredbroad@gmail.com>
2025-05-12 16:02:26 -0400

    Update 08 Slippage.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/08 Slippage.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/08 Slippage.php
@@ -4,4 +4,4 @@ $paperTradingSupported = false;
 include(DOCS_RESOURCES."/brokerages/slippage.php");
 ?>
 
-<p>To view how we model Alpaca slippage, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/alpaca#05-Slippage">Slippage</a>.</p>
+<p>To view how we model <a rel='nofollow' target='_blank' href='http://qnt.co/alpaca'>Alpaca</a> slippage, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/alpaca#05-Slippage">Slippage</a>.</p>
