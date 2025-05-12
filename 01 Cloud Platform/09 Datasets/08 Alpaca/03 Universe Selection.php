ddfd1a9f16b4eb2d9782380c1900f6691be995a9
Jared <jaredbroad@gmail.com>
2025-05-12 16:02:11 -0400

    Update 03 Universe Selection.php

--- 01 Cloud Platform/09 Datasets/08 Alpaca/03 Universe Selection.php
+++ 01 Cloud Platform/09 Datasets/08 Alpaca/03 Universe Selection.php
@@ -5,7 +5,7 @@ include(DOCS_RESOURCES."/data-feeds/universe-selection.php");
 ?>
 
 <p>
-  The Alpaca data provider can stream data for up to 30 assets on the Basic plan. 
+  The <a rel='nofollow' target='_blank' href='http://qnt.co/alpaca'>Alpaca</a> data provider can stream data for up to 30 assets on the Basic plan. 
   If your algorithm adds more than the quota, LEAN logs an error message from Alpaca. 
   To increase the quota, <a href="/docs/v2/cloud-platform/datasets/alpaca#99-Pricing">purchase</a> the Algo Trader Plus plan from Alpaca.
 </p>
