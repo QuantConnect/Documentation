5b5047d8e243e3c96967d08630efdee7c5c4fa3d
Jared <jaredbroad@gmail.com>
2025-05-12 16:01:26 -0400

    Update 05 Orders.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/05 Orders.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/05 Orders.php
@@ -1,3 +1,3 @@
-<p>We model the Alpaca API by supporting several order types, order properties, and order updates. When you deploy live algorithms, you can <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#03-Place-Manual-Trades'>place manual orders</a> through the IDE.</p>
+<p>We model the <a rel='nofollow' target='_blank' href='http://qnt.co/alpaca'>Alpaca</a> API by supporting several order types, order properties, and order updates. When you deploy live algorithms, you can <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#03-Place-Manual-Trades'>place manual orders</a> through the IDE.</p>
 
 <? include(DOCS_RESOURCES."/brokerages/alpaca/orders.php"); ?>
