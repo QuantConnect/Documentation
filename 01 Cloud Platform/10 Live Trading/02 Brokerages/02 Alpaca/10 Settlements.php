83ed4d653458583b24e87f8e4d5c3d02bcfb5cb9
Jared <jaredbroad@gmail.com>
2025-05-12 16:02:47 -0400

    Update 10 Settlements.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/10 Settlements.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/02 Alpaca/10 Settlements.php
@@ -8,5 +8,5 @@ include(DOCS_RESOURCES."/brokerages/settlements.php");
 ?>
 
 
-<p>To view how we model settlement for Alpaca trades, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/alpaca#08-Settlement">Settlement</a>.</p>
+<p>To view how we model settlement for <a rel='nofollow' target='_blank' href='http://qnt.co/alpaca'>Alpaca</a> trades, see <a href="/docs/v2/writing-algorithms/reality-modeling/brokerages/supported-models/alpaca#08-Settlement">Settlement</a>.</p>
 
