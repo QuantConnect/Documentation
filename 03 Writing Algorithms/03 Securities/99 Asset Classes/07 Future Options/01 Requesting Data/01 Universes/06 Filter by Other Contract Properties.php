cddbe8accc73530f96ff22a5c5e51fbd4051cd69
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-05-21 17:03:24 -0600

    Update 06 Filter by Other Contract Properties.php

--- 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Future Options/01 Requesting Data/01 Universes/06 Filter by Other Contract Properties.php
+++ 03 Writing Algorithms/03 Securities/99 Asset Classes/07 Future Options/01 Requesting Data/01 Universes/06 Filter by Other Contract Properties.php
@@ -2,7 +2,7 @@
 
 <?
   $assetClass = "Option";
-  include(DOCS_RESOURCES."/universes/option/option-filter-universe.html"); 
+  include(DOCS_RESOURCES."/universes/option/fop-filter-universe.html"); 
   include(DOCS_RESOURCES."/universes/future-option/filter-examples.html"); 
   include(DOCS_RESOURCES."/universes/option/filter-caveats.php");
 ?>
