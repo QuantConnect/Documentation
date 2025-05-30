9286e98012dfc0a4ff355a54fe83821dede665ae
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-05-30 15:56:15 -0600

    Update 06 Enter the Competition.php

--- 01 Cloud Platform/13 Community/06 Quant League Pro/06 Enter the Competition.php
+++ 01 Cloud Platform/13 Community/06 Quant League Pro/06 Enter the Competition.php
@@ -16,7 +16,7 @@
 <p>Your algorithm must observe the following rules:</p>
 <ul>
   <li>Your algorithm is unique.</li>
-  <li>Your algorithm is invested at all times. </li>
+  <li>Your algorithm is actively trading.</li>
   <li>Your algorithm trades liquid <a href="/docs/v2/writing-algorithms/securities/key-concepts">securities</a>. For Equities, this includes the top 1,000 stocks, Crypto includes the top 10 coins, Forex includes the top 20 pairs, and Futures includes the top 30 contracts.</li>
   <li>The maximum leverage is 2.</li>
 </ul>
