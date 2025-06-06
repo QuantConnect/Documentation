7f92260f1b80a60acc37581817cd6ff38d729497
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-06 15:22:21 -0600

    Update visualization.php

--- Resources/indicators/visualization.php
+++ Resources/indicators/visualization.php
@@ -1,4 +1,4 @@
-<p>The following image shows plot values of selected properties of <code><?=$typeName?></code> using the <a href='/docs/v2/research-environment/charting/plotly'>plotly</a> library.</p>
+<p>The following plot shows values for some of the <code><?=$typeName?></code> indicator properties:</p>
 
 <img class='docs-image' style="width: 100%"
     alt="<? echo "$typeName line plot." ?>"
