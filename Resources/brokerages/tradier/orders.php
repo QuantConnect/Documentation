8a7c07b3ed47456683b97c0b6a1f91507276917e
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-06-06 22:00:56 +0100

    Tradier Supports OutsideRegularTradingHours

--- Resources/brokerages/tradier/orders.php
+++ Resources/brokerages/tradier/orders.php
@@ -41,21 +41,49 @@
 }
 </style>
 
+<h4>Order Properties</h4>
+<p><?=$writingAlgorithms ? "The <code>TradierBrokerageModel</code> supports custom order properties." : "We model the Tradier API." ?> The following table describes the members of the <code>TradierOrderProperties</code> object that you can set to customize order execution.</p>
 
-<h4>Time In Force</h4>
-<p><?= $writingAlgorithms ? "The <code>TradierBrokerageModel</code> supports" : "We model the Tradier API by supporting" ?> the following <a href='/docs/v2/writing-algorithms/trading-and-orders/order-properties#03-Time-In-Force'>TimeInForce</a> instructions:</p>
-
-<ul>
-    <li><code class="csharp">Day</code><code class="python">DAY</code></li>
-    <li><code class="csharp">GoodTilCanceled</code><code class="python">GOOD_TIL_CANCELED</code> (not available for short selling)</li>
-</ul>
-
-<?php if ($writingAlgorithms) { ?>
+<table class="table qc-table">
+    <thead>
+        <tr>
+         <th>Property</th>
+         <th>Data Type</th>
+         <th>Description</th>
+         <th>Default Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code class="csharp">TimeInForce</code><code class="python">time_in_force</code></td>
+            <td><code>TimeInForce</code></td>
+            <td>A <a href='/docs/v2/writing-algorithms/trading-and-orders/order-properties#03-Time-In-Force'>TimeInForce</a> instruction to apply to the order. The following instructions are supported:
+                <ul>
+                    <li><code class="csharp">Day</code><code class="python">DAY</code></li>
+                    <li><code class="csharp">GoodTilCanceled</code><code class="python">GOOD_TIL_CANCELED</code></li>
+                    <li><code class="csharp">GoodTilDate</code><code class="python">good_til_date</code></li>
+                </ul>
+            </td>
+            <td><code class='csharp'>TimeInForce.GoodTilCanceled</code><code class='python'>TimeInForce.GOOD_TIL_CANCELED</code></td>
+        </tr>
+        <tr>
+            <td><code class="csharp">OutsideRegularTradingHours</code><code class="python">outside_regular_trading_hours</code></td>
+            <td><code>bool</code></td>
+            <td>A flag to signal that the order may be triggered and filled outside of regular trading hours.</td>
+            <td><code class='csharp'>false</code><code class='python'>False</code></td>
+        </tr>
+    </tbody>
+</table>
+<? if ($writingAlgorithms) { ?>
 <div class="section-example-container">
     <pre class="csharp">public override void Initialize()
 {
     // Set the default order properties
-    DefaultOrderProperties.TimeInForce = TimeInForce.GoodTilCanceled;
+    DefaultOrderProperties = new TradierOrderProperties
+    {
+        OutsideRegularTradingHours = false,
+        TimeInForce = TimeInForce.GoodTilCanceled
+    };
 }
 
 public override void OnData(Slice slice)
@@ -65,13 +93,21 @@ public override void OnData(Slice slice)
     
     // Override the default order properties
     LimitOrder(_symbol, quantity, limitPrice, 
-               orderProperties: new OrderProperties
+               orderProperties: new TradierOrderProperties
                { 
-                   TimeInForce = TimeInForce.Day 
+                   TimeInForce = TimeInForce.Day
+               });
+    LimitOrder(_symbol, quantity, limitPrice, 
+               orderProperties: new TradierOrderProperties
+               { 
+                   OutsideRegularTradingHours = true,
+                   TimeInForce = TimeInForce.GoodTilDate(new DateTime(year, month, day))
                });
 }</pre>
     <pre class="python">def initialize(self) -&gt; None:
     # Set the default order properties
+    self.default_order_properties = TradierOrderProperties()
+    self.default_order_properties.outside_regular_trading_hours = False
     self.default_order_properties.time_in_force = TimeInForce.GOOD_TIL_CANCELED
 
 def on_data(self, slice: Slice) -&gt; None:
@@ -79,11 +115,15 @@ def on_data(self, slice: Slice) -&gt; None:
     self.limit_order(self._symbol, quantity, limit_price)
     
     # Override the default order properties
-    order_properties = OrderProperties()
+    order_properties = TradierOrderProperties()
     order_properties.time_in_force = TimeInForce.DAY
+    self.limit_order(self._symbol, quantity, limit_price, order_properties=order_properties)
+
+    order_properties.time_in_force = TimeInForce.good_til_date(datetime(year, month, day))
+    order_properties.outside_regular_trading_hours = True
     self.limit_order(self._symbol, quantity, limit_price, order_properties=order_properties)</pre>
 </div>
-<?php } ?>
+<? } ?>
 
 <h4>Updates</h4>
 <p><?= $writingAlgorithms ? "The <code>TradierBrokerageModel</code> supports" : "We model the Tradier API by supporting" ?> most <a href='/docs/v2/writing-algorithms/trading-and-orders/order-management/order-tickets#04-Update-Orders'>order updates</a>. To update the quantity of an order, cancel the order and then submit a new order with the desired quantity. For more information about this workaround, see the <a href='/docs/v2/writing-algorithms/trading-and-orders/order-management/order-tickets#workaround-for-brokerages-that-dont-support-updates'>Workaround for Brokerages That Don’t Support Updates</a>.</p>
