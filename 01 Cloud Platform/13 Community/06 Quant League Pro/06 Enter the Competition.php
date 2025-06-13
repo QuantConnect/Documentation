bcd018527ed56ff901bf9f5b5df7338674813eb1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-13 08:39:11 -0600

    Update 06 Enter the Competition.php

--- 01 Cloud Platform/13 Community/06 Quant League Pro/06 Enter the Competition.php
+++ 01 Cloud Platform/13 Community/06 Quant League Pro/06 Enter the Competition.php
@@ -9,9 +9,18 @@
 <p>You must observe the following rules:</p>
 <ul>
   <li><b>You must be proficient at <a href='/docs/v2/writing-algorithms'>coding with the QuantConnect API</a>.</b></li>
-  <li>You must have completed <a href="/learning/course/1/Boot-Camp-101-US-Equities">Boot Camp</a>.</li>
   <li>Cash deposits during the quarter result in disqualification.</li>
+  <li>Copy QuantConnect into correspondence with investment funds.</li>
+  <li>Keep a QuantConnect public strategy listing if you receive an allocation.</li>
+  <li>Deploy all allocations through the QuantConnect platform.</li>
 </ul>
+
+<p>
+    Our goal isn't to charge users, but to grow institutional clients on QuantConnect and provide opportunities for users. 
+    To do this, we ask that allocations are done on a QuantConnect live server and that QuantConnect is copied into correspondence so we understand the challenges and process for the users. 
+    If you receive an allocation, keep the alpha listed. 
+    Otherwise, all the good ones would be immediately picked off and there would be no incentive for funds to browse the listing and no competing bids to license your alpha.
+</p>
 
 <p>Your algorithm must observe the following rules:</p>
 <ul>
