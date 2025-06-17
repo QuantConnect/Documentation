a95db265985651c40dca95a5a06c6675733cbc51
LouisSzeto <hke0073@icloud.com>
2025-06-17 18:23:24 +0800

    Fix broken links

--- 05 Lean CLI/09 Live Trading/02 Data Providers/03 Polygon/02 Deploy Local Algorithms.php
+++ 05 Lean CLI/09 Live Trading/02 Data Providers/03 Polygon/02 Deploy Local Algorithms.php
@@ -12,7 +12,7 @@ Configure credentials for Polygon
 Your Polygon API Key:</pre>
 </div>
 
-<p>To get your API key, see the <a rel='nofollow' target='_blank' href='https://polygon.io/dashboard/api-keys'>API Keys page</a> on the Polygon website.</p>
+<p>To get your API key, see the <a rel='nofollow' target='_blank' href='https://polygon.io/dashboard/keys'>API Keys page</a> on the Polygon website.</p>
     </li>
 ";
 $supportsIQFeed = false;
