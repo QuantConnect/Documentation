ce995801a5bb4c0583ffa08286f9dbf8bc94dd8d
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-07-08 15:05:14 +0100

    Adds Commands Section to Cloud Platform

---
+++ 01 Cloud Platform/10 Live Trading/08 Algorithm Control/07 Receive Commands.php
@@ -0,0 +1,5 @@
+<? include(DOCS_RESOURCES."/live-trading/commands-introduction.html");?>
+
+<p>
+    For more information, see the <a href='/docs/v2/writing-algorithms/live-trading/commands'>Commands</a> section of the Writing Algorithms documentation.
+</p>
---
+++ 03 Writing Algorithms/40 Live Trading/41 Commands/01 Introduction.php
@@ -0,0 +1,1 @@
+<? include(DOCS_RESOURCES."/live-trading/commands-introduction.html");?>
