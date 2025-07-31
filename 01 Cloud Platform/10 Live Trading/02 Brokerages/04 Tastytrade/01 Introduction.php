0555fe3718a6253c1f66fb1a87c6c397eb4fa174
Jared <jaredbroad@gmail.com>
2025-07-31 15:17:23 -0400

    Update 01 Introduction.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/04 Tastytrade/01 Introduction.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/04 Tastytrade/01 Introduction.php
@@ -1,3 +1,13 @@
 <? include(DOCS_RESOURCES."/landing-page-introductions/individual-brokerages.php"); ?>
 <? include(DOCS_RESOURCES."/brokerages/introduction-by-brokerage/tastytrade.html"); ?>
-<p>To view the implementation of the tastytrade brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.tastytrade' rel='nofollow' target="_blank">Lean.Brokerages.tastytrade repository</a>.</p>
+<p>To view the implementation of the tastytrade brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.tastytrade' rel='nofollow' target="_blank">tastytrade repository</a>.</p>
+<script type="text/javascript">
+  document.addEventListener("DOMContentLoaded", function() {
+    const headings = document.querySelectorAll('h2.page-sub-heading > span');
+    headings.forEach(span => {
+        if (span.textContent.trim() === "Tastytrade") {
+            span.textContent = "tastytrade";
+        }
+    });
+});
+</script>
