4dab61b3e84c820e9c5bf2d7d666eb5959b6e15a
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-31 16:21:25 -0600

    Update 99 Examples.php

--- 02 AI Assistance/03 MCP Server/99 Examples.php
+++ 02 AI Assistance/03 MCP Server/99 Examples.php
@@ -9,11 +9,9 @@
 <p>The client should call the <code>create_project</code> tool.</p>
 
 <h4>Example 2: Search QuantConnect, Create Projects, and Fix Errors</h4>
-<p>The following video shows the conversation with Claude Desktop:</p>
+<p>The following video shows <a href='https://claude.ai/share/7006527e-b665-45b6-970c-97af58296b16' target='_blank' rel='nofollow'>this conversation</a> with Claude Desktop in real-time:</p>
 <? DOCS_VIMEO(1106258759); ?>
-<a href='https://claude.ai/share/7006527e-b665-45b6-970c-97af58296b16' target='_blank' rel='nofollow'>https://claude.ai/share/7006527e-b665-45b6-970c-97af58296b16</a>
 
 <h4>Example 3: Brainstorm Ideas, Add Project Descriptions, and Add Collaborators</h4>
-<p>The following video shows the conversation with Claude Desktop:</p>
+<p>The following video shows <a href='https://claude.ai/share/68784d82-243d-4384-8f19-9f7dde7ba076' target='_blank' rel='nofollow'>this conversation</a> with Claude Desktop in real-time:</p>
 <? DOCS_VIMEO(1106259276); ?>
-<a href='https://claude.ai/share/68784d82-243d-4384-8f19-9f7dde7ba076' target='_blank' rel='nofollow'>https://claude.ai/share/68784d82-243d-4384-8f19-9f7dde7ba076</a>
