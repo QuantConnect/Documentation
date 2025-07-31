3bf59740524c75be46a08da2676fad440406de95
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-31 15:46:43 -0600

    Create 99 Examples.php

---
+++ 01 Cloud Platform/98 MCP Server/99 Examples.php
@@ -0,0 +1,15 @@
+<p>The following examples demonstrate the MCP server.</p>
+
+<h4>Example 1: Hello World</h4>
+<p>To test the server and client are working and connected, enter the following prompt into the client application:</p>
+
+ <div class="section-example-container">
+     <pre>> Create a QuantConnect project.}</pre>
+</div>
+<p>The client should call the <code>create_project</code> tool.</p>
+
+<h4>Example 2: Search QuantConnect, Create Projects, and Fix Errors</h4>
+<? DOCS_VIMEO(1106258759); ?>
+
+<h4>Example 3: Brainstorm Ideas, Add Project Descriptions, and Add Collaborators</h4>
+<? DOCS_VIMEO(1106259276); ?>
