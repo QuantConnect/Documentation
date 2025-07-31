5da62a207e749b2edbcfb2e1bb6195e152f883b5
Jared <jaredbroad@gmail.com>
2025-07-31 14:40:59 -0400

    Update 07 Hybrid Data Provider.php

--- 01 Cloud Platform/09 Datasets/10 Tastytrade/07 Hybrid Data Provider.php
+++ 01 Cloud Platform/09 Datasets/10 Tastytrade/07 Hybrid Data Provider.php
@@ -1,5 +1,5 @@
 <?
-$brokerageName = "Tastytrade";
+$brokerageName = "tastytrade";
 $liveDeployment = "/docs/v2/cloud-platform/live-trading/brokerages/tastytrade#15-Deploy-Live-Algorithms";
 include(DOCS_RESOURCES."/data-feeds/multiple-data-providers.php");
 ?>
