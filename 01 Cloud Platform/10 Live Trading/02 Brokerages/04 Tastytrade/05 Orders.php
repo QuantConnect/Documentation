742036195aa2a483e7f08c225bbd4f4e38eb1140
Jared <jaredbroad@gmail.com>
2025-07-31 14:20:06 -0400

    Update 05 Orders.php

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/04 Tastytrade/05 Orders.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/04 Tastytrade/05 Orders.php
@@ -1,2 +1,2 @@
-<p>We model the Tastytrade API by supporting several order types, the <code>TimeInForce</code> order property, and order updates. When you deploy live algorithms, you can <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#03-Place-Manual-Trades'>place manual orders</a> through the IDE.</p>
+<p>We model the tastytrade API by supporting several order types, the <code>TimeInForce</code> order property, and order updates. When you deploy live algorithms, you can <a href='/docs/v2/cloud-platform/live-trading/algorithm-control#03-Place-Manual-Trades'>place manual orders</a> through the IDE.</p>
 <? include(DOCS_RESOURCES."/brokerages/tastytrade/orders.php"); ?>
