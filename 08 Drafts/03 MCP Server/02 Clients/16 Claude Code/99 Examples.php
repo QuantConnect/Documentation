6dc18113c9f03fc0c0536f6f93f74b51b61edf43
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-08-18 17:06:22 -0600

    Update 99 Examples.php

--- 08 Drafts/03 MCP Server/02 Clients/16 Claude Code/99 Examples.php
+++ 08 Drafts/03 MCP Server/02 Clients/16 Claude Code/99 Examples.php
@@ -1,9 +1,10 @@
 <p>The following examples demonstrate the MCP server with Claude Code.</p>
 
 <h4>Example 1: Hello World</h4>
-<p>To test the server and client are working and connected, enter the following prompt into the client application:</p>
+<p>To test the server and client are working and connected, enter the following prompt into Claude Code:</p>
 
  <div class="cli section-example-container">
      <pre>> Read my QuantConnect account.</pre>
 </div>
-<p>The client should call the <code>read_account</code> tool.</p>
+<p>Claude should call the <code>read_account</code> tool.</p>
+
