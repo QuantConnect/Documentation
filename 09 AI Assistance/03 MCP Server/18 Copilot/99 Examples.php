89549a5b7a72479a161ec0327dd789fe22f98767
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-08-21 14:02:49 -0600

    Create 99 Examples.php

---
+++ 09 AI Assistance/03 MCP Server/18 Copilot/99 Examples.php
@@ -0,0 +1,10 @@
+<p>The following examples demonstrate the MCP server with Copilot.</p>
+
+<h4>Example 1: Hello World</h4>
+<p>To test the server and client are working and connected, enter the following prompt into Copilot:</p>
+
+ <div class="cli section-example-container">
+     <pre>> Read my QuantConnect account.</pre>
+</div>
+<p>Copilot should call the <code>read_account</code> tool.</p>
+
