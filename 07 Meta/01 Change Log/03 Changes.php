a3988f0e90cc7441df75197b1e762de49897e5dd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-09-05 16:09:57 -0600

    Update 03 Changes.php

--- 07 Meta/01 Change Log/03 Changes.php
+++ 07 Meta/01 Change Log/03 Changes.php
@@ -1,14 +1,6 @@
 <?
-// URL of the JSON file
 $jsonUrl = 'https://s3.us-east-1.amazonaws.com/content.quantconnect.com/documentation/change-log.json';
-
-// Fetch JSON data
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $jsonUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$jsonData = curl_exec($ch);
-curl_close($ch);
-
+$jsonData = file_get_contents($jsonUrl);
 echo htmlspecialchars($jsonData !== false ? $jsonData : 'Failed to fetch JSON');
 ?>
-Hello world2
+Hello world3
