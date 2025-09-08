4333b8ee7cccc776212e4879244c096c8e3aefbb
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-09-08 08:41:45 -0600

    Update 03 Changes.php

--- 07 Meta/01 Change Log/03 Changes.php
+++ 07 Meta/01 Change Log/03 Changes.php
@@ -1,4 +1,23 @@
 <?
-echo file_get_contents('https://s3.us-west-1.amazonaws.com/content.quantconnect.com/documentation/change-log.json');
+$changeByDate = json_decode(file_get_contents('https://s3.us-east-1.amazonaws.com/cdn.quantconnect.com/docs/i/change-log.json'), true);
+
+foreach ($changeByDate as $date => $changes) {
+    $changes = json_decode($changes, true);
+    if (empty($changes))) {
+      continue;
+    }
 ?>
-Hello world5
+    <h4><?=$date?></h4>
+    <ul>
+<?
+    foreach ($change in $changes) {
+?>
+      <li><?=$change["summary"]?></li>
+<?
+    }
+?>
+    </ul>
+<? 
+} 
+?>
+
