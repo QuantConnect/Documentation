3c0059ec225b79647ccc55353443324031032b06
Derek Melchin <melchin.derek@gmail.com>
2025-09-10 13:58:22 -0600

    Add Project Workspace Quota

---
+++ 01 Cloud Platform/04 Organizations/03 Resources/12 Project Workspace Quotas.php
@@ -0,0 +1,12 @@
+<p>
+  Each project in your organization produces a <span class='new-term'>project workspace</span>.
+  The project workspace contains all the data of a single project, including its files, backtest results, optimization results, and live deployment results.
+  The following table shows the total amount of space each organization tier can use for project workspaces:
+</p>
+
+<? include(DOCS_RESOURCES."/quotas/project-workspaces.html"); ?>
+
+<p>
+  To avoid reaching the quota, manage your project workspace usage. 
+  Mindful practices like logging sparsely, deleting old projects, and deleting old backtest results ensure your project workspaces stay within reasonable sizes.
+</p>
