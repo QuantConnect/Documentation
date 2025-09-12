9940ee34d9e5651309ee1635c1b9b249d1a54d48
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-09-12 11:52:00 -0600

    Update 10 Customize Reports.php

--- 01 Cloud Platform/08 Backtesting/06 Report/10 Customize Reports.php
+++ 01 Cloud Platform/08 Backtesting/06 Report/10 Customize Reports.php
@@ -8,4 +8,7 @@ include(DOCS_RESOURCES."/backtesting/results/customize-report.php");
 
 <h4>Example</h4>
 
-<p><a href='https://www.quantconnect.com/terminal/clone/25061168/92562921df687db3fc5f28f48826405c/clone-of%3A-Adaptable-Apricot-Cat' target='_blank'>Clone this project</a> to view an example project that creates reports with custom HTML and CSS.</p>
+<p>
+  <a href='https://www.quantconnect.com/terminal/clone/25061168/92562921df687db3fc5f28f48826405c/clone-of%3A-Adaptable-Apricot-Cat' target='_blank'>Clone this project</a> to view an example project that creates reports with custom HTML and CSS.
+  If you backtest it and then generate a report, you'll notice the report has a red banner at the top.
+</p>
