707fe74e43026bbb800c1e21d8e61ee17f6edc50
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-09-12 11:49:05 -0600

    Update 10 Customize Reports.php

--- 01 Cloud Platform/08 Backtesting/06 Report/10 Customize Reports.php
+++ 01 Cloud Platform/08 Backtesting/06 Report/10 Customize Reports.php
@@ -5,3 +5,7 @@ $addCSSFileInstructions = "<a href='/docs/v2/cloud-platform/projects/files#03-Ad
 $crisisEventLink = "/docs/v2/cloud-platform/backtesting/report#08-Crisis-Events";
 include(DOCS_RESOURCES."/backtesting/results/customize-report.php"); 
 ?>
+
+<h4>Example</h4>
+
+<p><a href='https://www.quantconnect.com/terminal/clone/25061168/92562921df687db3fc5f28f48826405c/clone-of%3A-Adaptable-Apricot-Cat' target='_blank'>Clone this project</a> to view an example project that creates reports with custom HTML and CSS.</p>
