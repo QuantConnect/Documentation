66bf66cda335f93cf925210a8fb81938a9c01488
GitHub <github-actions@github.com>
2025-10-03 10:22:02 +0000

    Code generated by writing-algorithm-api-reference-sphinx-code-generator.py

---
+++ 03 Writing Algorithms/98 API Reference/06 Handling Data.php
@@ -0,0 +1,47 @@
+<h4 id="Handling Data">Handling Data</h4>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-cik.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-composite-figi.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-cusip.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-fundamentals.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-isin.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-data.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-delistings.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-dividends.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-end-of-algorithm.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-end-of-day.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-end-of-time-step.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-splits.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-symbol-changed-events.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-on-warmup-finished.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-sedol.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-algorithm-id.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-api.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-available-data-types.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-current-slice.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-date-time.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-end-date.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-object-store.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-run-time-error.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-start-date.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-set-time-zone.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-algorithm-id.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-current-slice.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-end-date.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-name.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-object-store.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-settings.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-start-date.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-status.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-subscription-manager.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-tags.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-time.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-time-zone.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-utc-time.html"); ?>
+<? include(DOCS_RESOURCES."/qcalgorithm-api/qcalgorithm-composite-f-i-g-i.html"); ?>
+<style>
+.code-source        { color: grey; float: right }
+.object-type        { font-style: italic; }
+.arg-name           { font-weight: bold; }
+.subsection-header  { font-weight: bold; background-color: lightgray; }
+.subsection-content { margin-left: 10px; }
+</style>
