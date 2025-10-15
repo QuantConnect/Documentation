4f2da864fc00d9975c201deb1bc8a52bbde9c4b5
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-10-15 18:36:54 +0100

    Documents the ShortMarginInterestRateModel

--- 03 Writing Algorithms/24 Reality Modeling/12 Margin Interest Rate/01 Key Concepts/02 Set Models.php
+++ 03 Writing Algorithms/24 Reality Modeling/12 Margin Interest Rate/01 Key Concepts/02 Set Models.php
@@ -3,20 +3,24 @@
     <pre class="csharp">public override void Initialize()
 {
     var security = AddEquity("SPY");
-    // Null margin interest rate model is non-realistic, you should set it according to your broker information
-    security.MarginInterestRateModel = MarginInterestRateModel.Null;
+    // For US Equity, use the ShortMarginInterestRateModel with IB data
+    security.SetShortableProvider(new InteractiveBrokersShortableProvider());
+    security.MarginInterestRateModel = new ShortMarginInterestRateModel();
 }</pre>
     <pre class="python">def initialize(self) -&gt; None:
     security = self.add_equity("SPY")
-    # Null margin interest rate model is non-realistic, you should set it according to your broker information
-    security.set_margin_interest_rate_model(MarginInterestRateModel.NULL)</pre>
+    # For US Equity, use the ShortMarginInterestRateModel with IB data
+    security.set_shortable_provider(InteractiveBrokersShortableProvider())
+    security.margin_interest_rate_model = ShortMarginInterestRateModel()</pre>
 </div>
 
 <p>You can also set the margin interest rate model in a <a href='/docs/v2/writing-algorithms/initialization#07-Set-Security-Initializer'>security initializer</a>. If your algorithm has a dynamic universe, use the security initializer technique. In order to initialize single security subscriptions with the security initializer, call <code class="csharp">SetSecurityInitializer</code><code class="python">set_security_initializer</code> before you create the subscriptions.</p><p>
 
 <?
-$overwriteCodePy = "security.set_margin_interest_rate_model(MarginInterestRateModel.NULL)";
-$overwriteCodeC = "security.MarginInterestRateModel = MarginInterestRateModel.Null;";
+$overwriteCodePy = "security.set_shortable_provider(InteractiveBrokersShortableProvider())
+        security.margin_interest_rate_model = ShortMarginInterestRateModel()";
+$overwriteCodeC = "security.SetShortableProvider(new InteractiveBrokersShortableProvider());
+        security.MarginInterestRateModel = new ShortMarginInterestRateModel();";
 include(DOCS_RESOURCES."/reality-modeling/brokerage-model-security-init.php");
 ?>
 
