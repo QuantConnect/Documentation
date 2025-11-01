81ee987474f49a224777f8c976085098cc23c399
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2025-11-01 17:17:20 +0000

    Refactors Indicator Page Generator

--- Resources/indicators/visualization.php
+++
@@ -1,5 +0,0 @@
-<p>The following plot shows values for some of the <code><?=$typeName?></code> indicator properties:</p>
-
-<img class='docs-image' style="width: 100%"
-    alt="<? echo "$typeName line plot." ?>"
-    src="<?=$imageSource?>">
