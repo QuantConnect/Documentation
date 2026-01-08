86c6f2bec708a50a586af57ee9e38f8b81198bbc
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2026-01-08 16:56:58 -0700

    Update 99 Examples.php

--- 09 AI Assistance/03 MCP Server/18 Copilot/99 Examples.php
+++ 09 AI Assistance/03 MCP Server/18 Copilot/99 Examples.php
@@ -3,9 +3,7 @@
 <h4>Example 1: Hello World</h4>
 <p>To test the server and client are working and connected, enter the following prompt into Copilot:</p>
 
- <div class="cli section-example-container">
-     <pre>> Read the open project.</pre>
-</div>
+<blockquote><p>> Read the open project.</p></blockquote>
 <p>Copilot should call the <code>read_open_project</code> tool.</p>
 
 <h4>Example 2: Strategy Development and Deployment</h4>
