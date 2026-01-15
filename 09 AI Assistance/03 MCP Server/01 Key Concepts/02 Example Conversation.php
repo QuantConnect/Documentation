76e1e92da9ec60c71c698565c7103f52389f6e59
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2026-01-15 10:31:28 -0700

    Update 02 Example Conversation.php

--- 09 AI Assistance/03 MCP Server/01 Key Concepts/02 Example Conversation.php
+++ 09 AI Assistance/03 MCP Server/01 Key Concepts/02 Example Conversation.php
@@ -1,4 +1,2 @@
-<p>
-  The following <a href='https://claude.ai/share/752c1f3d-cea0-4498-8cc8-5311143dc5b1' rel='nofollow' target='_blank'>example conversation</a> with Claude Desktop demonstrates how the server equips an LLM with the ability to create trading strategies, backtest them, and deploy them to live trading:
-</p>
-<?php DOCS_VIMEO(1107394484); ?>
+<p>The following conversation with Copilot Chat in Local Platform demonstrates how the server equips an LLM with the ability to create trading strategies, backtest them, and deploy them to live trading:</p>
+<? DOCS_VIMEO(1114056119); ?>
