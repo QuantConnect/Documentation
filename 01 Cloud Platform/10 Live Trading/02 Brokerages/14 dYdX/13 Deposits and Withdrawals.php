3e0cd8b6bbf5a83913b7deec8bb6d0aa8bbb7b08
Jared <jaredbroad@gmail.com>
2026-01-16 15:59:17 -0500

    Update Deposits and Withdrawals info for dYdX
    Added information about cash deposits and withdrawals with dYdX.

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/14 dYdX/13 Deposits and Withdrawals.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/14 dYdX/13 Deposits and Withdrawals.php
@@ -1,1 +1,4 @@
-<? include(DOCS_RESOURCES."/brokerages/deposits-and-withdrawals.html");?>
+<p>
+    You can deposit and withdraw cash while you run an algorithm that's connected to a dYdX account. 
+  We sync the algorithm's cash holdings with the cash holdings in dYdX every day at 7:45 AM Eastern Time (ET).
+</p>
