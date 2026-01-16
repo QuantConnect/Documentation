1802c3c57f6f878bab59ed8b08bed71fd60c9837
Jared <jaredbroad@gmail.com>
2026-01-16 15:55:28 -0500

    Update authentication message for dYdX Exchange

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/14 dYdX/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/14 dYdX/16 Deploy Live Algorithms.php
@@ -3,7 +3,7 @@ $brokerageName = "dYdX Exchange";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Enter your private key, wallet address, and sub-account number</li>";
+$authentication = "<li>Enter API your private key, wallet address, and sub-account number</li>";
 $authentication .= file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/dydx.html");
 $postDeploy = "";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
