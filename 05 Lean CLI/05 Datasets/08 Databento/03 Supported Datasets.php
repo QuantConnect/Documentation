27c3da6bc82a7c58ebb52107f1246024437d98b0
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-02-09 19:37:59 +0000

    Adds Databento to CLI Docs

---
+++ 05 Lean CLI/05 Datasets/08 Databento/03 Supported Datasets.php
@@ -0,0 +1,5 @@
+<p>
+	The Databento data provider serves asset price data directly from <a href='https://databento.com/docs/' rel='nofollow' target='_blank'>Databento's API</a>. 
+	Our integration supports <a href="https://www.quantconnect.com/docs/v2/writing-algorithms/securities/asset-classes/futures">US Futures</a> securities.
+	It only provides the security price data, so you need to download the <a href='/datasets/quantconnect-us-futures-security-master'>US Futures Security Master</a> and the <a href='/datasets/quantconnect-us-futures-universe'>US Futures Universe</a> . 
+</p>
