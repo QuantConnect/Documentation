6300596a5855dca0971404d7cce07cdf181ed9ba
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-03-06 22:35:06 +0000

    Updates Third-Party Library Code Generation

--- 02 Local Platform/04 Development Environment/12 Packages and Libraries/03 Default Environment.php
+++ 02 Local Platform/04 Development Environment/12 Packages and Libraries/03 Default Environment.php
@@ -1,5 +1,5 @@
 <p>The default environment supports the following libraries:</p>
-<? 
+<?
 $isCloudPlatform = false;
-include(DOCS_RESOURCES."/libraries/supported-libraries.php"); 
+include(DOCS_RESOURCES."/libraries/supported-libraries.php");
 ?>
