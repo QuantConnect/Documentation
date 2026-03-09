3e4174e1f27bc88305ce7fe04a7bd18ef8897dda
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-03-09 23:38:01 +0000

    Updates Charles Schwab Deployment

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Charles Schwab/11 Security and Stability.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Charles Schwab/11 Security and Stability.php
@@ -3,4 +3,4 @@ $brokerageName = "Charles Schwab";
 $statusPageURL = null;
 include(DOCS_RESOURCES."/brokerages/security-and-stability.php");
 ?>
-<p>Charles Schwab only supports authenticating one account at a time per user. If you have an algorithm running with Charles Schwab and then deploy a second one, the first algorithm stops running.</p>
+<p>Our integration supports authenticating multiple Charles Schwab user accounts, one for each project, with one trading account. If you deploy a second algorithm for the same user, the first algorithm stops. For example, if you authenticate as <span class='field-name'>JohnDoe</span> with trading accounts <span class='field-name'>XXXXX549</span> and <span class='field-name'>XXXXX725</span>, you can deploy to only one of those. To deploy a second algorithm, you must authenticate as a different user, for example <span class='field-name'>RichardRoe</span>, and select trading accounts associated with it.</p>
--- 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Charles Schwab/14 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/07 Charles Schwab/14 Deploy Live Algorithms.php
@@ -3,8 +3,8 @@ $brokerageName = "Charles Schwab";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Check the <span class=\"box-name\">Authorization</span> check box and then click <span class=\"button-name\">Authenticate</span>.</li>
-<li>On the Charles Schwab website, log in, and select your Schwab accounts to link. Click <span class=\"button-name\">Allow</span> to grant QuantConnect access to your account information and authorization.</li>
+$authentication = "<li>Enter your user name (Charles Schwab Login ID) and press <span class=\"button-name\">Enter</span>.</li><li>Check the <span class=\"box-name\">Authorization</span> box and then click <span class=\"button-name\">Authenticate</span>.</li>
+<li>Log in to the Charles Schwab website using the same ID. Select your Schwab accounts to link, then click <span class=\"button-name\">Allow</span> to authorize QuantConnect access.</li>
 <li>Click on the <span class='field-name'>Select Account Id</span> field and then click on one of your accounts.</li>";
 $postDeploy = "";
 $dataProviderDetails = "<p>In most cases, we suggest using the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a>, the <a href='/docs/v2/cloud-platform/datasets/charles-schwab'>Charles Schwab data provider</a>, or both. The order you set them in the deployment wizard defines their order of precedence in Lean.</p>";
