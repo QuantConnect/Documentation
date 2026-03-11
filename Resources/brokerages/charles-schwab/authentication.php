82e1e8b1f708068a58db2d80ea852c8705fb339c
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-03-11 22:05:42 +0000

    Creates Authetication Section for Charles Schwab

--- Resources/brokerages/charles-schwab/authentication.php
+++ Resources/brokerages/charles-schwab/authentication.php
@@ -1,6 +1,1 @@
-<?
-$brokerageName = "Charles Schwab";
-$statusPageURL = null;
-include(DOCS_RESOURCES."/brokerages/security-and-stability.php");
-?>
 <p>Our integration supports authenticating multiple Charles Schwab user accounts, one for each project, with one trading account. If you deploy a second algorithm for the same user, the first algorithm stops. For example, if you authenticate as <span class='field-name'>JohnDoe</span> with trading accounts <span class='field-name'>XXXXX549</span> and <span class='field-name'>XXXXX725</span>, you can deploy to only one of those. To deploy a second algorithm, you must authenticate as a different user, for example <span class='field-name'>RichardRoe</span>, and select trading accounts associated with it.</p>
