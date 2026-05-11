1b094291910a98a3f0a13d27cddffa2d04c60927
Rudy Osuna <ruosuna@ucsd.edu>
2026-05-11 11:57:15 -0700

    change if statement ordering

--- project-templates/python/alternative-data-universe-braincompanyfilinglanguagemetricsuniverseall/main.py
+++ project-templates/python/alternative-data-universe-braincompanyfilinglanguagemetricsuniverseall/main.py
@@ -9,6 +9,7 @@ class BrainCompanyFilingLanguageMetricsUniverseAlgorithm(QCAlgorithm):
         self.set_start_date(2024, 9, 1)
         self.set_end_date(2024, 12, 31)
         self.set_cash(100_000)
+        self.settings.seed_initial_prices = True
         self.universe_settings.resolution = Resolution.DAILY
         # Add a universe of US Equities with positive sentiment in their latest SEC filings.
         self._universe = self.add_universe(BrainCompanyFilingLanguageMetricsUniverseAll, self._select_assets)
@@ -22,10 +23,10 @@ class BrainCompanyFilingLanguageMetricsUniverseAlgorithm(QCAlgorithm):
     def _select_assets(self, data: List[BrainCompanyFilingLanguageMetricsUniverseAll]) -> List[Symbol]:
         # Filter for assets with positive sentiment in both the report and MD&A sections.
         return [d.symbol for d in data
-                    if d.report_sentiment and d.report_sentiment.sentiment and d.report_sentiment.sentiment > 0
-                    and d.management_discussion_analyasis_of_financial_condition_and_results_of_operations
-                    and d.management_discussion_analyasis_of_financial_condition_and_results_of_operations.sentiment
-                    and d.management_discussion_analyasis_of_financial_condition_and_results_of_operations.sentiment > 0]
+                    if d.report_sentiment and d.report_sentiment.sentiment and d.report_sentiment.sentiment > 0 and 
+                    d.management_discussion_analyasis_of_financial_condition_and_results_of_operations and
+                    d.management_discussion_analyasis_of_financial_condition_and_results_of_operations.sentiment and
+                    d.management_discussion_analyasis_of_financial_condition_and_results_of_operations.sentiment > 0]
 
     def _rebalance(self) -> None:
         if not self._universe.selected:
