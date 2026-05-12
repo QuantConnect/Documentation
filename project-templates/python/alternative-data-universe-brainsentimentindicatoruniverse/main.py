4fc38a19413044cdfdef7821b973a5c93f1dd601
Rudy Osuna <ruosuna@ucsd.edu>
2026-05-12 15:41:02 -0700

    Refine asset selection criteria in BrainSentimentIndicator

--- project-templates/python/alternative-data-universe-brainsentimentindicatoruniverse/main.py
+++ project-templates/python/alternative-data-universe-brainsentimentindicatoruniverse/main.py
@@ -4,7 +4,6 @@ from AlgorithmImports import *
 
 
 class BrainSentimentIndicatorUniverseAlgorithm(QCAlgorithm):
-    _fundamental: list[Symbol] = []
 
     def initialize(self) -> None:
         self.set_start_date(2024, 9, 1)
@@ -12,9 +11,7 @@ class BrainSentimentIndicatorUniverseAlgorithm(QCAlgorithm):
         self.set_cash(100_000)
         self.settings.seed_initial_prices = True
         self.universe_settings.resolution = Resolution.DAILY
-        # Add a fundamental universe to track the most liquid US Equities by dollar volume.
-        self.add_universe(self._fundamental_filter)
-        # Add a Brain Sentiment universe, restricted to high-sentiment names within the fundamental list.
+        # Add a Brain Sentiment universe, restricted to high-sentiment names with strong coverage.
         self._universe = self.add_universe(BrainSentimentIndicatorUniverse, self._select_assets)
         # Rebalance every day at 9am.
         self.schedule.on(
@@ -23,16 +20,11 @@ class BrainSentimentIndicatorUniverseAlgorithm(QCAlgorithm):
             self._rebalance
         )
 
-    def _fundamental_filter(self, fundamental: List[Fundamental]) -> Universe.UnchangedUniverse:
-        self._fundamental = [f.symbol for f in sorted(fundamental, key=lambda f: f.dollar_volume)[-100:]]
-        return Universe.UNCHANGED
-
     def _select_assets(self, data: List[BrainSentimentIndicatorUniverse]) -> List[Symbol]:
-        # Keep names with both active mention coverage and positive 7-day sentiment.
-        alt = [d.symbol for d in data
-               if d.total_article_mentions_7_days and d.total_article_mentions_7_days > 0 and
-               d.sentiment_7_days and d.sentiment_7_days > 0]
-        return [s for s in self._fundamental if s in alt]
+        # Keep names with both active mention coverage 15+ articles and positive 7-day sentiment above 0.5.
+        return [d.symbol for d in data
+                if d.total_article_mentions_7_days and d.total_article_mentions_7_days > 15 and
+                d.sentiment_7_days and d.sentiment_7_days > 0.5]
 
     def _rebalance(self) -> None:
         if not self._universe.selected:
