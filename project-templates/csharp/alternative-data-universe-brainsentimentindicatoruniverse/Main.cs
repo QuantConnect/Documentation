5b24c75bfac7abad9e49e50398cf15fb176cb053
Rudy Osuna <ruosuna@ucsd.edu>
2026-05-12 15:40:18 -0700

    remove the 2nd universe, make filtering more strict

--- project-templates/csharp/alternative-data-universe-brainsentimentindicatoruniverse/Main.cs
+++ project-templates/csharp/alternative-data-universe-brainsentimentindicatoruniverse/Main.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using QuantConnect.Algorithm.Framework.Portfolio;
-using QuantConnect.Data.Fundamental;
 using QuantConnect.Data.UniverseSelection;
 using QuantConnect.DataSource;
 
@@ -10,7 +9,6 @@ namespace QuantConnect.Algorithm.CSharp
 {
     public class BrainSentimentIndicatorChainedUniverseAlgorithm : QCAlgorithm
     {
-        private List<Symbol> _fundamental = [];
         private Universe _universe;
 
         public override void Initialize()
@@ -20,24 +18,13 @@ namespace QuantConnect.Algorithm.CSharp
             SetCash(100000);
             Settings.SeedInitialPrices = true;
             UniverseSettings.Resolution = Resolution.Daily;
-            // Add a fundamental universe to track the most liquid US Equities by dollar volume.
-            AddUniverse(fundamental =>
-            {
-                _fundamental = fundamental
-                    .OrderBy(c => c.DollarVolume)
-                    .TakeLast(100)
-                    .Select(c => c.Symbol)
-                    .ToList();
-                return Universe.Unchanged;
-            });
-            // Add a Brain Sentiment universe, restricted to high-sentiment names within the fundamental list.
+            // Add a Brain Sentiment universe, restricted to high-sentiment names with strong coverage.
             _universe = AddUniverse<BrainSentimentIndicatorUniverse>(
-                // Keep names with both active mention coverage and positive 7-day sentiment.
+                // Keep names with both active mention coverage 15+ articles and positive 7-day sentiment above 0.5.
                 altCoarse => altCoarse
                     .OfType<BrainSentimentIndicatorUniverse>()
-                    .Where(d => d.TotalArticleMentions7Days > 0m && d.Sentiment7Days > 0m)
+                    .Where(d => d.TotalArticleMentions7Days > 15m && d.Sentiment7Days > 0.5m)
                     .Select(d => d.Symbol)
-                    .Intersect(_fundamental)
             );
             // Rebalance every day at 9am.
             Schedule.On(DateRules.EveryDay("SPY"), TimeRules.At(9, 0, 0), Rebalance);
