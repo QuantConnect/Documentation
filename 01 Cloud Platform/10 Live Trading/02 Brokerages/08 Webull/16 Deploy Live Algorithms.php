ea839082c8169b57dfe98ea779708f2142661f54
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-06-04 16:49:58 +0100

    Revert "Use OAuth for Webull cloud deployment"
    This reverts commit 78781f2046a83c652e9f0a6178c4dac8f604c7c8.

--- 01 Cloud Platform/10 Live Trading/02 Brokerages/08 Webull/16 Deploy Live Algorithms.php
+++ 01 Cloud Platform/10 Live Trading/02 Brokerages/08 Webull/16 Deploy Live Algorithms.php
@@ -3,9 +3,7 @@ $brokerageName = "Webull";
 $cashState = false;
 $holdingsState = false;
 $secondBullet = "";
-$authentication = "<li>Click the <span class=\"button-name\">Authenticate</span> button.</li>
-<li>On the Webull website, log in to your account to grant QuantConnect access to your account information and authorization.</li>
-<li>Click the <span class='field-name'>Select Account Id</span> field and then click one of your accounts.</li>";
+$authentication = "<li>Enter your Webull App Key, App Secret, and account ID.</li>" . file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/webull.html");
 $postDeploy = "";
 $dataProviderDetails = "<p>Webull doesn't provide a live data feed, so use the <a href='/docs/v2/cloud-platform/datasets'>QuantConnect data provider</a> or another data provider for the securities you trade.</p>";
 include(DOCS_RESOURCES."/live-trading/deploy-live-algorithm.php");
