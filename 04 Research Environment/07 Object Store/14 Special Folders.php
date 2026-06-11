8f19ea545437d20ad85791bf07894d8916ff4417
Derek Melchin <melchin.derek@gmail.com>
2026-06-11 08:46:01 -0600

    Add skills, memories, and templates

---
+++ 04 Research Environment/07 Object Store/14 Special Folders.php
@@ -0,0 +1,3 @@
+<?php include(DOCS_RESOURCES."/object-store/special-folders.html"); ?>
+
+<p>These files are shared across your organization. To add or update them, upload files to the Object Store with the <a href="/docs/v2/cloud-platform/object-store#03-Upload-Files">Algorithm Lab</a>, <a href='/docs/v2/lean-cli/object-store#03-Cloud-Storage'>CLI</a>, or <a href='/docs/v2/cloud-platform/api-reference/object-store-management/upload-object-store-files'>API</a>.</p>
