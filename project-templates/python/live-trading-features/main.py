0d7211fb4c8e488f300c828ac86c3e127547a775
Rudy Osuna <ruosuna@ucsd.edu>
2026-06-18 10:30:44 -0700

    Add manual indicator alternative for live-trading-features

--- project-templates/python/live-trading-features/main.py
+++ project-templates/python/live-trading-features/main.py
@@ -16,6 +16,10 @@ class LiveTradingFeaturesAlgorithm(QCAlgorithm):
         self._spy = self.add_equity("SPY")
         # Create an EMA indicator to generate trade signals.
         self._spy.ema = self.ema(self._spy, 20, Resolution.DAILY)
+        # Alternatively, use a manual indicator.
+        # self._spy.ema = ExponentialMovingAverage(20)
+        # self.warm_up_indicator(self._spy.symbol, self._spy.ema, Resolution.DAILY)
+        # self.register_indicator(self._spy.symbol, self._spy.ema, Resolution.DAILY)
     
     def _notify_all(self, subject: str, message: str) -> None:
         self.notify.email("email@address.com", subject, message)
