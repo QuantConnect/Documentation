59bb027b819f9b082fde61b742de3b811c49edfd
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2026-06-18 13:29:51 -0600

    Update create-consolidator.php

--- Resources/consolidators/consolidator-info/market-hour-aware/create-consolidator.php
+++ Resources/consolidators/consolidator-info/market-hour-aware/create-consolidator.php
@@ -14,17 +14,17 @@
         <tr>
             <td><code class='csharp'>dailyStrictEndTimeEnabled</code><code class='python'>daily_strict_end_time_enabled</code></td>
             <td><code>bool</code></td>
-            <td>Whether daily bars should have an <a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/periods#03-Start-and-End-Time'><span class='csharp'>EndTime</span><span class='python'>end_time</span></a> that matches the market close time (<code class='python'>True</code><code class='csharp'>true</code>) instead of the following midnight (<code class='python'>False</code><code class='csharp'>false</code>). This argument usually matches your <a href='/docs/v2/writing-algorithms/initialization#10-Set-Algorithm-Settings'><code class='csharp'>DailyPreciseEndTime</code><code class='python'>daily_precise_end_time</code></a> setting.</td>
+            <td>Whether daily bars should have an <a href='/docs/v2/writing-algorithms/key-concepts/time-modeling/periods#03-Start-and-End-Time'><span class='csharp'>EndTime</span><span class='python'>end_time</span></a> that matches the market close time (<code class='python'>True</code><code class='csharp'>true</code>) instead of the following midnight (<code class='python'>False</code><code class='csharp'>false</code>). This argument usually matches your <a href='/docs/v2/writing-algorithms/initialization#10-Set-Algorithm-Settings'><code class='csharp'>DailyPreciseEndTime</code><code class='python'>daily_precise_end_time</code></a> <a href='/docs/v2/writing-algorithms/initialization#10-Set-Algorithm-Settings'>setting</a>.</td>
         </tr>
         <tr>
             <td><code>period</code></td>
             <td><code class='csharp'>TimeSpan</code><code class='python'>timedelta</code></td>
-            <td>The consolidation period. The consolidator anchors intraday periods to the market open, so a bar never spans across closed market hours.</td>
+            <td>The duration of the consolidation period. The first consolidation period of each day always starts at the beginning of the regular or extended market hours.</td>
         </tr>
         <tr>
             <td><code class='csharp'>dataType</code><code class='python'>data_type</code></td>
             <td><code class='csharp'>Type</code><code class='python'>type</code></td>
-            <td>The type of the input data. In this case, use <code><?=$this->dataTypeArgPy?></code>.</td>
+            <td>The type of data the consolidator produces. In this case, use <code><?=$this->dataTypeArgPy?></code>.</td>
         </tr>
         <tr>
             <td><code class='csharp'>tickType</code><code class='python'>tick_type</code></td>
@@ -34,7 +34,7 @@
         <tr>
             <td><code class='csharp'>extendedMarketHours</code><code class='python'>extended_market_hours</code></td>
             <td><code>bool</code></td>
-            <td>Whether to consolidate data from extended market hours. If <code class='python'>False</code><code class='csharp'>false</code>, the consolidator ignores data that occurs while the market is closed and anchors bars to the regular market open. If <code class='python'>True</code><code class='csharp'>true</code>, it anchors bars to the start of the extended market hours.</td>
+            <td>Whether or not to consolidate data from extended market hours. If <code class='python'>False</code><code class='csharp'>false</code>, the consolidator ignores data that occurs while the market is closed and anchors bars to the regular market open. If <code class='python'>True</code><code class='csharp'>true</code>, it anchors bars to the start of the extended market hours.</td>
         </tr>
     </tbody>
 </table>
