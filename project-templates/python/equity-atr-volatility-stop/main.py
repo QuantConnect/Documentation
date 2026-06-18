403c7b421090fc7864d48031517bc05c75f8395d
Rudy Osuna <ruosuna@ucsd.edu>
2026-06-18 11:20:47 -0700

    Clarify manual indicator warm-up comments

--- project-templates/python/equity-atr-volatility-stop/main.py
+++ project-templates/python/equity-atr-volatility-stop/main.py
@@ -7,6 +7,7 @@ class AtrChandelierTrailingStopAlgorithm(QCAlgorithm):
         self.set_start_date(2022, 1, 1)
         self.set_end_date(2024, 12, 31)
         self.set_cash(100000)
+        # automatic_indicator_warm_up only supports automatic indicators, not manual indicators.
         self.settings.automatic_indicator_warm_up = True
 
         self._spy = self.add_equity("SPY", Resolution.MINUTE)
