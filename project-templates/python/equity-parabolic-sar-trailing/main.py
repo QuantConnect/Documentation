ed8fd23a3affba1c111f29432a1e0f2fbafffa1d
Rudy Osuna <ruosuna@ucsd.edu>
2026-06-18 10:29:11 -0700

    Add manual indicator alternative for equity-parabolic-sar-trailing

--- project-templates/python/equity-parabolic-sar-trailing/main.py
+++ project-templates/python/equity-parabolic-sar-trailing/main.py
@@ -11,6 +11,10 @@ class EquityParabolicSarTrailingTemplateAlgorithm(QCAlgorithm):
         self.settings.automatic_indicator_warm_up = True
         self._equity = self.add_equity("SPY")
         self._psar = self.psar(self._equity, 0.02, 0.02, 0.2, Resolution.DAILY)
+        # Alternatively, use a manual indicator.
+        # self._psar = ParabolicStopAndReverse(0.02, 0.02, 0.2)
+        # self.warm_up_indicator(self._equity.symbol, self._psar, Resolution.DAILY)
+        # self.register_indicator(self._equity.symbol, self._psar, Resolution.DAILY)
         self.plot_indicator("SPY", self._psar)
 
     def on_data(self, data: Slice) -> None:
