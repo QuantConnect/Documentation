275c5e91cafb8299999d331f0cceedd5c9701332
Rudy Osuna <ruosuna@ucsd.edu>
2026-06-18 10:37:29 -0700

    Use securities in manual indicator comments

--- project-templates/python/intraday-greeks-based-options-selection/main.py
+++ project-templates/python/intraday-greeks-based-options-selection/main.py
@@ -21,8 +21,8 @@ class OptionChainFullExample(QCAlgorithm):
         self.ema(self._index, 60).updated += self._trade_target_delta_contract
         # Alternatively, use a manual indicator.
         # self._ema = ExponentialMovingAverage(60)
-        # self.warm_up_indicator(self._index.symbol, self._ema)
-        # self.register_indicator(self._index.symbol, self._ema)
+        # self.warm_up_indicator(self._index, self._ema)
+        # self.register_indicator(self._index, self._ema)
         # self._ema.updated += self._trade_target_delta_contract
 
         self._option_chain_symbol = Symbol.create_canonical_option(self._index, "RUTW", Market.USA, "?RUTW")
