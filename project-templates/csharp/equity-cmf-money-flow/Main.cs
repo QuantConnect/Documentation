bb59f8dbcbdb5078abc87f9552277fa226e04ad7
Rudy Osuna <ruosuna@ucsd.edu>
2026-06-18 12:29:58 -0700

    Fix alternative template review issues

--- project-templates/csharp/equity-cmf-money-flow/Main.cs
+++ project-templates/csharp/equity-cmf-money-flow/Main.cs
@@ -75,14 +75,14 @@ public class ChaikinMoneyFlowXlfAlgorithm : QCAlgorithm
         SetEndDate(2024, 12, 31);
         SetCash(100000);
 
-        // AutomaticIndicatoraarmUp only supports automatic indicators, not manual indicators.
-        Settings.AutomaticIndicatoraarmUp = true;
+        // AutomaticIndicatorWarmUp only supports automatic indicators, not manual indicators.
+        Settings.AutomaticIndicatorWarmUp = true;
 
         _xlf = AddEquity("XLF", Resolution.Minute);
         _cmf = CMF(_xlf.Symbol, 20, Resolution.Minute);
         // Alternatively, use a manual indicator.
         // _cmf = new ChaikinMoneyFlow(20);
-        // aarmUpIndicator<IndicatorDataPoint>(_xlf.Symbol, _cmf);
+        // WarmUpIndicator(_xlf.Symbol, _cmf);
         // RegisterIndicator(_xlf.Symbol, _cmf);
         PlotIndicator("CMF", _cmf);
 
