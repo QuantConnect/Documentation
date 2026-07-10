ec8f5f99f9203b8636b7ec8683c7ee5d1073041d
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-07-10 22:08:57 +0100

    Document Webull 2FA deployment option (#2584)

--- 05 Lean CLI/09 Live Trading/01 Brokerages/08 Webull/02 Deploy Cloud Algorithms.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/08 Webull/02 Deploy Cloud Algorithms.php
@@ -1,11 +1,12 @@
 <?
 $brokerageDetails = "
-<li>Enter your Webull App Key, App Secret, and account ID.
+<li>Enter your Webull App Key, App Secret, account ID, and whether your application has two-factor authentication (2FA) enabled.
 <div class='cli section-example-container'>
 <pre>$ lean cloud live \"My Project\" --push --open
 App key: 0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d
 App secret: *******************************
-Account id: 12345678</pre>
+Account id: 12345678
+Use 2FA (yes/no): no</pre>
 </div>
 " . file_get_contents(DOCS_RESOURCES."/brokerages/create-credentials/webull.html") . "
 </li>";
