e04c79f33a47f32d2810b1c5f4848dd3c7a8da69
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2026-07-10 19:29:16 +0100

    Comment out link to private Lean.Brokerages.Public repository

--- 05 Lean CLI/09 Live Trading/01 Brokerages/05 Public/01 Introduction.php
+++ 05 Lean CLI/09 Live Trading/01 Brokerages/05 Public/01 Introduction.php
@@ -4,6 +4,6 @@ include(DOCS_RESOURCES."/brokerages/cli-deployment/introduction.php");
 ?>
 
 <p>Public authenticates with a secret key and an account number. Public lets you have more than one account, so the account number picks which one to trade.</p>
-<p>To view the implementation of the Public brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Public' rel='nofollow' target="_blank">Lean.Brokerages.Public repository</a>.</p>
+<!--p>To view the implementation of the Public brokerage integration, see the <a href='https://github.com/QuantConnect/Lean.Brokerages.Public' rel='nofollow' target="_blank">Lean.Brokerages.Public repository</a>.</p-->
 
 <p>To use the CLI, you must be a member in an <a href='https://www.quantconnect.com/docs/v2/cloud-platform/organizations/tier-features'>organization</a> on a paid tier.</p>
